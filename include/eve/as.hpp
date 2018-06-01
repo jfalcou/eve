e38b0444bf5f09fd18e9035e36cec56030fc9708
Joel Falcou <joel.falcou@lri.fr>
2018-06-01 23:03:51 +0200

    Add as() helper

---
+++ include/eve/as.hpp
@@ -0,0 +1,22 @@
+//==================================================================================================
+/**
+  Copyright 2018 Joel FALCOU
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+**/
+//==================================================================================================
+#ifndef EVE_AS_HPP_INCLUDED
+#define EVE_AS_HPP_INCLUDED
+
+#include <eve/detail/abi.hpp>
+#include <type_traits>
+
+namespace eve
+{
+  template<typename T> struct as_ { using type = T; };
+
+  template<typename T> EVE_FORCEINLINE as_<std::decay_t<T>> as(T&&) { return {}; }
+}
+
+#endif
