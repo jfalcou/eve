3b926e2d50902f5152975b18d3f8c43b5523b03f
Joel Falcou <joel.falcou@lri.fr>
2018-06-20 22:19:29 +0200

    Add version header

---
+++ include/eve/version.hpp
@@ -0,0 +1,21 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2018 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_VERSION_HPP_INCLUDED
+#define EVE_VERSION_HPP_INCLUDED
+
+/// semantic: EVE_VERSION / 10000000
+///     year: EVE_VERSION / 100000 % 100
+///    month: EVE_VERSION / 1000 % 100
+///    patch: EVE_VERSION % 1000
+#define EVE_VERSION 41701002
+
+#define EVE_LIB_VERSION "4.17.1.2"
+
+#endif
