b24e767414aea1c1234836ee7e057b1397635b27
Joel Falcou <joel.falcou@lri.fr>
2018-07-09 00:25:01 +0200

    Add TTS specification for pack

---
+++ test/test.hpp
@@ -0,0 +1,31 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2018 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef TEST_TEST_HPP
+#define TEST_TEST_HPP
+
+#include <tts/tts.hpp>
+#include <tts/tests/relation.hpp>
+#include <eve/forward.hpp>
+#include <algorithm>
+
+namespace tts { namespace ext
+{
+  template<typename T, typename N, typename ABI, typename EnableIf>
+  struct equal<eve::pack<T,N,ABI>,eve::pack<T,N,ABI>,EnableIf>
+  {
+    using arg_t = eve::pack<T,N,ABI>;
+    inline bool operator()(arg_t const& l, arg_t const& r) const
+    {
+      return std::equal(l.begin(),l.end(),r.begin());
+    }
+  };
+} }
+
+#endif
