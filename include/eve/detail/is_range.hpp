90734da1065385f961533ff62f3bc4a739125472
Joel Falcou <joel.falcou@lri.fr>
2018-07-08 19:02:04 +0200

    Add detection of Random Access Range

--- include/eve/detail/is_range.hpp
+++ include/eve/detail/is_range.hpp
@@ -15,6 +15,7 @@
 
 namespace eve { namespace detail
 {
+  // Detect if a Type behaves a Range
   template <typename T> struct is_range_impl
   {
     static std::false_type test(...);
@@ -36,6 +37,30 @@ namespace eve { namespace detail
 
   template<typename T>
   using is_range_t = typename is_range<T>::type;
+
+  // Detect if a Type behaves a Random Access Range
+  template <typename T> struct is_rarange_impl
+  {
+    static std::false_type test(...);
+
+    template <  typename U
+              , typename=decltype(std::begin(std::declval<U>()))
+              , typename=decltype(std::end  (std::declval<U>()))
+              , typename=decltype(std::declval<U>()[0])
+              >
+    static std::true_type test(U&&);
+
+    static constexpr auto value = std::is_same_v<decltype(test(std::declval<T>())),std::true_type>;
+  };
+
+  template<typename T>
+  inline constexpr bool is_random_access_range_v = detail::is_rarange_impl<T>::value;
+
+  template<typename T>
+  struct is_random_access_range : std::bool_constant<is_random_access_range_v<T>> {};
+
+  template<typename T>
+  using is_random_access_range_t = typename is_random_access_range<T>::type;
 } }
 
 #endif
