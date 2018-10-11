fa4148c47eedff0c146ea6c13422b7b54a8727c6
Joel Falcou <joel.falcou@lri.fr>
2018-10-11 15:54:51 +0200

    Simpler literals suffix

--- include/eve/literals.hpp
+++ include/eve/literals.hpp
@@ -19,32 +19,20 @@ namespace eve
   {
     template<std::uint64_t N> using idx_ = std::integral_constant<std::uint64_t,N>;
 
-    constexpr int to_int(char c) { return static_cast<int>(c) - 48; }
-
-    template< std::size_t N> constexpr std::uint64_t parse(int, const char (&)[N], idx_<0> const&)
+    template<char... c> constexpr std::uint64_t parse()
     {
-      return 0;
-    }
-
-    template< std::size_t N, std::size_t I>
-    constexpr std::uint64_t parse(int base, const char (&arr)[N], idx_<I> const&)
-    {
-      return to_int(arr[I - 1]) * base + parse<N>(base*10, arr,idx_<I-1>{});
-    }
-
-    // Intermediate trampoline so MSVC doesn't cry
-    template<std::size_t N,char... c> constexpr std::uint64_t parse(int base)
-    {
-      return parse<N>(base, {c...}, idx_<N>{});
+      std::uint64_t value = 0;
+      char arr[] = {c...};
+      for(int i = 0;i<sizeof...(c);++i) value = value*10 + (arr[i] - 48);
+      return value;
     }
   }
 
   namespace literal
   {
-    template <char ...c>
-    constexpr idx_< eve::detail::parse<sizeof...(c),c...>(1)> operator"" _c()
+    template<char ...c> constexpr auto operator"" _c()
     {
-      return {};
+      return detail::idx_<detail::parse<c...>()>{};
     }
   }
 } }
