74507c6c298371f09c948a9a70c0111443fd403d
Joel Falcou <joel.falcou@lri.fr>
2019-01-11 21:47:19 +0100

    Type conversion in popcount

--- include/eve/module/core/function/scalar/popcnt.hpp
+++ include/eve/module/core/function/scalar/popcnt.hpp
@@ -27,53 +27,53 @@ namespace eve { namespace detail
     if constexpr( sizeof(T) == 1 )
     {
       #if defined(EVE_COMP_IS_MSVC)
-      std::uint8_t that = __popcnt16(static_cast<std::int16_t>(a0) & 0xFF);
+      std::uint8_t r = static_cast<std::uint8_t>(__popcnt16(static_cast<std::int16_t>(a0) & 0xFF));
       #else
-      std::uint8_t that = __builtin_popcount(static_cast<std::uint32_t>(a0) & 0xFF);
+      std::uint8_t r = __builtin_popcount(static_cast<std::uint32_t>(a0) & 0xFF);
       #endif
 
-      return that;
+      return r;
     }
 
     if constexpr( sizeof(T) == 2 )
     {
       #if defined(EVE_COMP_IS_MSVC)
-      std::uint16_t that = __popcnt16(a0);
+      std::uint16_t r = __popcnt16(a0);
       #else
-      std::uint16_t that = __builtin_popcount(static_cast<std::uint32_t>(a0) & 0xFFFF);
+      std::uint16_t r = __builtin_popcount(static_cast<std::uint32_t>(a0) & 0xFFFF);
       #endif
 
-      return that;
+      return r;
     }
 
     if constexpr( sizeof(T) == 4 )
     {
       #if defined(EVE_COMP_IS_MSVC)
-      std::uint32_t that = __popcnt(a0);
+      std::uint32_t r = __popcnt(a0);
       #else
-      std::uint32_t that = __builtin_popcount(a0);
+      std::uint32_t r = __builtin_popcount(a0);
       #endif
 
-      return that;
+      return r;
     }
 
     if constexpr( sizeof(T) == 8 )
     {
       #if defined(EVE_COMP_IS_MSVC) && defined(_WIN64)
-      std::uint64_t that = __popcnt64(a0);
+      std::uint64_t r = __popcnt64(a0);
       #else
       std::uint32_t hi0 = (static_cast<std::uint32_t>(a0 >> 32) & 0xFFFFFFFF);
       std::uint32_t lo0 = (a0 & 0xFFFFFFFF);
 
       #if defined(EVE_COMP_IS_MSVC)
-      std::uint64_t that = __popcnt(hi0) + __popcnt(lo0);
+      std::uint64_t r = __popcnt(hi0) + __popcnt(lo0);
       #else
-      std::uint64_t that = __builtin_popcount(hi0) + __builtin_popcount(lo0);
+      std::uint64_t r = __builtin_popcount(hi0) + __builtin_popcount(lo0);
       #endif
 
       #endif
 
-      return that;
+      return r;
     }
   }
 
