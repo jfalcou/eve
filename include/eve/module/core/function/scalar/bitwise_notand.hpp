89c437fdee99d5163fe49fe5a71b67064c51dd63
jtlap <jtlapreste@gmail.com>
2019-01-26 20:35:34 +0100

    bitwise_notand and tests

---
+++ include/eve/module/core/function/scalar/bitwise_notand.hpp
@@ -0,0 +1,35 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Jean-Thierry Lapreste
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_BITWISE_NOTAND_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_BITWISE_NOTAND_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/scalar/bitwise_and.hpp>
+#include <eve/function/scalar/complement.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  // -----------------------------------------------------------------------------------------------
+  // Regular case
+  template<typename T, typename U> EVE_FORCEINLINE
+  constexpr T bitwise_notand_(EVE_SUPPORTS(cpu_), T const& a, U const& b) noexcept
+  {
+    static_assert ( sizeof(T) == sizeof(U)
+                  , "eve::bitwise_notand - Arguments have incompatible size"
+                  );
+
+    return bitwise_and(complement(a), b); 
+  }
+} 
+
+#endif
---
+++ include/eve/module/core/function/simd/common/bitwise_notand.hpp
@@ -0,0 +1,85 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Jean-Thierry Lapreste
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_BITWISE_NOTAND_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_BITWISE_NOTAND_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/bitwise_cast.hpp>
+#include <eve/forward.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  // -----------------------------------------------------------------------------------------------
+  // Aggregation
+  template<typename T, typename N>
+  EVE_FORCEINLINE wide<T,N,aggregated_> bitwise_notand_( EVE_SUPPORTS(simd_)
+                                                    , wide<T,N,aggregated_> const& v0
+                                                    , wide<T,N,aggregated_> const& v1
+                                                    ) noexcept
+  {
+    return aggregate( eve::bitwise_notand, v0, v1);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Emulation with auto-splat inside map for performance purpose
+  template<typename T, typename N>
+  EVE_FORCEINLINE auto bitwise_notand_ ( EVE_SUPPORTS(simd_)
+                                    , wide<T,N,emulated_> const& v0
+                                    , wide<T,N,emulated_> const& v1
+                                    ) noexcept
+  {
+    return map( eve::bitwise_notand, v0, v1);
+  }
+
+  template<typename T, typename N, typename U>
+  EVE_FORCEINLINE auto bitwise_notand_ ( EVE_SUPPORTS(simd_)
+                                    , wide<T,N,emulated_> const& v0, U const& v1
+                                    ) noexcept
+                  requires(wide<T,N,emulated_>, Convertible<U,T>)
+  {
+    return map( eve::bitwise_notand, v0, T(v1) );
+  }
+
+  template<typename T, typename N, typename U>
+  EVE_FORCEINLINE auto bitwise_notand_ ( EVE_SUPPORTS(simd_)
+                                    , U const& v0, wide<T,N,emulated_> const& v1
+                                    ) noexcept
+                  requires(wide<T,N,emulated_>, Convertible<U,T>)
+  {
+    return map( eve::bitwise_notand, T(v0), v1);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Support for mixed type with auto-splat
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto bitwise_notand_ ( EVE_SUPPORTS(simd_)
+                                    , wide<T,N,ABI> const& v0, U const& v1
+                                    ) noexcept
+                  requires(wide<T,N,ABI>, Convertible<U,T>)
+  {
+    return eve::bitwise_notand( v0, wide<T,N,ABI>(v1) );
+  }
+
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto bitwise_notand_ ( EVE_SUPPORTS(simd_)
+                                    , U const& v0, wide<T,N,ABI> const& v1
+                                    ) noexcept
+                  requires(wide<T,N,ABI>, Convertible<U,T>)
+  {
+    return eve::bitwise_notand( wide<T,N,ABI>(v0), v1 );
+  }
+}
+
+
+#endif
