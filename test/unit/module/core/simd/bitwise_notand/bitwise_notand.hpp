f1df39ebf4a8637fb3c705a784bb946ff0df761a
jtlap <jtlapreste@gmail.com>
2019-01-29 14:16:02 +0100

    first version of bitwise_select with no xop intrinsics

--- test/unit/module/core/simd/bitwise_notand/bitwise_notand.hpp
+++ test/unit/module/core/simd/bitwise_notand/bitwise_notand.hpp
@@ -17,7 +17,7 @@
 
 using eve::fixed;
 
-TTS_CASE_TPL( "Check bitwise_and behavior on homogeneous wide"
+TTS_CASE_TPL( "Check bitwise_notand behavior on homogeneous wide"
             , fixed<1>,fixed<2>,fixed<4>,fixed<8>,fixed<16>,fixed<32>,fixed<64>
             )
 {
@@ -29,7 +29,7 @@ TTS_CASE_TPL( "Check bitwise_and behavior on homogeneous wide"
                 , rhs([](int i, int  ) { return i+1; })
                 , ref([](int i, int c) { return eve::bitwise_notand(Type(c-i),Type(i+1)); });
 
-    TTS_SECTION( "supports eve::bitwise_and" )  { TTS_EQUAL(ref, eve::bitwise_notand(lhs,rhs)); }
+    TTS_SECTION( "supports eve::bitwise_notand" )  { TTS_EQUAL(ref, eve::bitwise_notand(lhs,rhs)); }
   }
 }
 
@@ -44,7 +44,7 @@ TTS_CASE_TPL( "Check bitwise_notand behavior on wide + scalar"
     wide<Type,T>  lhs([](int i, int c) { return i%3; })
                 , ref([](int i, int c) { return eve::bitwise_notand( Type(i%3), Type(7) ); });
 
-    TTS_SECTION( "supports eve::bitwise_and" )  { TTS_EQUAL(ref, eve::bitwise_notand(lhs,7)); }
+    TTS_SECTION( "supports eve::bitwise_notand" )  { TTS_EQUAL(ref, eve::bitwise_notand(lhs,7)); }
   }
 }
 
--- test/unit/module/core/simd/bitwise_select/uint64.cpp
+++ test/unit/module/core/simd/bitwise_select/uint64.cpp
@@ -1,17 +1,13 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
   Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_BITWISE_SELECT_HPP_INCLUDED
-#define EVE_FUNCTION_BITWISE_SELECT_HPP_INCLUDED
 
-#include <eve/function/scalar/bitwise_select.hpp>
-#include <eve/function/simd/bitwise_select.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::uint64_t;
+#include "bitwise_select.hpp"
