44f39e08bb88cf0325aac5b31de4e89fbb3e5878
Joel Falcou <joel.falcou@lri.fr>
2019-01-30 20:25:19 +0100

    Add support for IEEE testing

--- test/test.hpp
+++ test/test.hpp
@@ -12,6 +12,7 @@
 
 #include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
 #include <eve/wide.hpp>
 #include <algorithm>
 
@@ -24,6 +25,19 @@ namespace tts::ext
     inline bool operator()(arg_t const& l, arg_t const& r) const
     {
       return std::equal(l.begin(),l.end(),r.begin());
+    }
+  };
+
+  template<typename T, typename N, typename ABI, typename EnableIf>
+  struct ulpdist<eve::wide<T,N,ABI>,eve::wide<T,N,ABI>,EnableIf>
+  {
+    inline double operator()(eve::wide<T,N,ABI> const& l, eve::wide<T,N,ABI> const& r) const
+    {
+      double max_ulp = 0;
+      for(auto i = 0; i < l.size(); ++i)
+        max_ulp = std::max( max_ulp, tts::ulpdist(T(l[i]),T(r[i])) );
+
+      return max_ulp;
     }
   };
 }
