4669430674f291ff8d27010b9b9048ba1c9b2538
jtlap <jtlapreste@gmail.com>
2019-02-12 21:07:50 +0100

    formatting

--- include/eve/constant/mzero.hpp
+++ include/eve/constant/mzero.hpp
@@ -23,8 +23,10 @@ namespace eve
     using t_t = detail::value_type_t<T>;
 
     if constexpr(std::is_same_v<t_t, float>) return T(-0.0f);
-    else if constexpr(std::is_same_v<t_t, double>) return T(-0.0);
-    else return T(0);
+    else if constexpr(std::is_same_v<t_t, double>)
+      return T(-0.0);
+    else
+      return T(0);
   }
 }
 
