83d3617e87509459668ed754d0ba33f159d6f435
jtlap <jtlapreste@gmail.com>
2019-02-18 07:54:23 +0100

    logical_not

--- include/eve/function/simd/logical_not.hpp
+++ include/eve/function/simd/logical_not.hpp
@@ -1,16 +1,16 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2018 Joel FALCOU
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_LOGICAL_NOT_HPP_INCLUDED
-#define EVE_FUNCTION_LOGICAL_NOT_HPP_INCLUDED
+#ifndef EVE_FUNCTION_SIMD_LOGICAL_NOT_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_LOGICAL_NOT_HPP_INCLUDED
 
 #include <eve/function/scalar/logical_not.hpp>
-#include <eve/function/simd/logical_not.hpp>
- 
+#include <eve/module/core/function/simd/logical_not.hpp>
+
 #endif
