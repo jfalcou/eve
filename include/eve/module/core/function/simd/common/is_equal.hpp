fd2d469f78910696e89ea7d4d9193afe680f7339
jtlap <jtlapreste@gmail.com>
2019-02-18 19:21:39 +0100

    mixed support for == and !=

--- include/eve/module/core/function/simd/common/is_equal.hpp
+++ include/eve/module/core/function/simd/common/is_equal.hpp
@@ -87,6 +87,24 @@ namespace eve
   {
     return eve::is_equal(v0, v1);
   }
+
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto operator ==(wide<T, N, ABI> const &v0,
+                                  U const & v1) noexcept requires(wide<T, N, ABI>,
+                                                                  detail::Convertible<U, T>)
+  {
+    return eve::is_equal(v0, wide<T, N, ABI>(static_cast<T>(v1)));
+  }
+  
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto  operator==( U const & v0,
+                                    wide<T, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
+                                                                                 detail::Convertible<U, T>)
+  {
+    return eve::is_equal(wide<T, N, ABI>(static_cast<T>(v0)), v1);
+  }
+   
+  
 }
 
 #endif
