272068e89cae3394d6c9ab5355fb54f0ccae0828
Joel Falcou <joel.falcou@lri.fr>
2019-02-21 14:46:19 +0100

    PPC support

--- include/eve/module/core/function/simd/is_not_equal.hpp
+++ include/eve/module/core/function/simd/is_not_equal.hpp
@@ -24,16 +24,10 @@
 
 #endif
 
-// #if defined(EVE_HW_POWERPC)
-// #  if EVE_HW_POWERPC >= EVE_VMX_VERSION
-// #    include <eve/module/core/function/simd/ppc/vmx/is_not_equal.hpp>
-// #  endif
-// #endif
-
-// #if defined(EVE_HW_ARM)
-// #  if EVE_HW_ARM >= EVE_NEON_VERSION
-// #    include <eve/module/core/function/simd/arm/neon/is_not_equal.hpp>
-// #  endif
-// #endif
+#if defined(EVE_HW_POWERPC)
+#  if EVE_HW_POWERPC >= EVE_VMX_VERSION
+#    include <eve/module/core/function/simd/ppc/vmx/is_not_equal.hpp>
+#  endif
+#endif
 
 #endif
---
+++ include/eve/module/core/function/simd/ppc/vmx/is_not_equal.hpp
@@ -0,0 +1,29 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2018 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_PPC_VMX_IS_NOT_EQUAL_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_PPC_VMX_IS_NOT_EQUAL_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/as_logical.hpp>
+#include <eve/forward.hpp>
+
+namespace eve::detail
+{
+  template<typename T, typename N>
+  EVE_FORCEINLINE auto
+  is_not_equal_(EVE_SUPPORTS(vmx_), wide<T, N, ppc_> const &v0, wide<T, N, ppc_> const &v1) noexcept
+  {
+    using t_t = wide<T, N, ppc_>;
+    return as_logical_t<t_t>(vec_cmpne(v0.storage(), v1.storage()));
+  }
+}
+
+#endif
