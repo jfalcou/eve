3c9ba8eec0f9948ed52263ed20af5e21e1e4b9bc
jtlap <jtlapreste@gmail.com>
2019-02-22 08:33:22 +0100

    is_less, is_greater and signmask : they are intrinsecly intricated

---
+++ include/eve/module/core/function/simd/common/is_greater.hpp
@@ -0,0 +1,98 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019  Jean-Thierry Lapreste
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IS_GREATER_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IS_GREATER_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/bitwise_cast.hpp>
+#include <eve/function/bitwise_not.hpp>
+#include <eve/function/bitwise_xor.hpp>
+#include <eve/forward.hpp>
+#include <eve/constant/true.hpp>
+#include <eve/as_logical.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+
+  // -----------------------------------------------------------------------------------------------
+  // Support for mixed type with auto-splat
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto is_greater_(EVE_SUPPORTS(simd_),
+                                  wide<T, N, ABI> const &v0,
+                                  U const & v1) noexcept requires(wide<T, N, ABI>,
+                                                                  detail::Convertible<U, T>)
+  {
+    return eve::is_greater(v0, wide<T, N, ABI>(static_cast<T>(v1)));
+  }
+  
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto is_greater_(EVE_SUPPORTS(simd_),
+                                  U const & v0,
+                                  wide<T, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
+                                                                               detail::Convertible<U, T>)
+  {
+    return eve::is_greater(wide<T, N, ABI>(static_cast<T>(v0)), v1);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Aggregation
+  template<typename T, typename N>
+  EVE_FORCEINLINE auto is_greater_(EVE_SUPPORTS(simd_),
+                                 wide<T, N, aggregated_> const &v0,
+                                 wide<T, N, aggregated_> const &v1) noexcept
+  {
+    return aggregate(eve::is_greater, v0, v1);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Emulation
+  template<typename T, typename N>
+  EVE_FORCEINLINE auto is_greater_(EVE_SUPPORTS(simd_),
+                                 wide<T, N, emulated_> const &v0,
+                                 wide<T, N, emulated_> const &v1) noexcept
+  {
+    return map(eve::is_greater, v0, v1);
+  }
+
+}
+
+namespace eve
+{
+  // -----------------------------------------------------------------------------------------------
+  // operator > 
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto operator > (wide<T, N, ABI> const &v0, wide<T, N, ABI> const &v1) noexcept
+  {
+    return eve::is_greater(v0, v1);
+  }
+
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto operator > (wide<T, N, ABI> const &v0,
+                                  U const & v1) noexcept requires(wide<T, N, ABI>,
+                                                                  detail::Convertible<U, T>)
+  {
+    return eve::is_greater(v0, wide<T, N, ABI>(static_cast<T>(v1)));
+  }
+  
+  template<typename T, typename N, typename ABI, typename U>
+  EVE_FORCEINLINE auto  operator > ( U const & v0,
+                                    wide<T, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
+                                                                                 detail::Convertible<U, T>)
+  {
+    return eve::is_greater(wide<T, N, ABI>(static_cast<T>(v0)), v1);
+  }
+   
+  
+}
+
+#endif
---
+++ test/unit/module/core/simd/is_less/is_less.hpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Jean-Thierry Lapreste
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef IS_LESS_HPP
+#define IS_LESS_HPP
+
+#include "test.hpp"
+#include <tts/tests/relation.hpp>
+#include <eve/function/simd/is_less.hpp>
+#include <eve/logical.hpp>
+#include <eve/wide.hpp>
+
+using eve::fixed;
+
+TTS_CASE_TPL("Check is_less behavior on homogeneous wide",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>, 
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>
+            )
+{
+  using eve::wide;
+
+  TTS_SETUP("A correctly initialized wide")
+  {
+    wide<Type, T> lhs([](int i, int c) { return c - i; }), rhs([](int i, int c) { return i%2 ? i : c-i; });
+    wide < eve::logical < Type>, T >  ref([](int i, int c) { return eve::is_less(Type(c - i), Type(i%2 ? i : c-i)); });
+    TTS_SECTION("supports eve::is_less") { TTS_EQUAL(ref, eve::is_less(lhs, rhs)); }
+    TTS_SECTION("supports operator < ") { TTS_EQUAL(ref, (lhs <  rhs)); }
+  }
+}
+
+
+#endif
