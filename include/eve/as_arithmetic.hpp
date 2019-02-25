8d5a167b5d621065d45fa8df1d0461a23f39d3f8
Joel Falcou <joel.falcou@lri.fr>
2019-02-26 00:23:42 +0100

    Traits for conversion to arithmetic

---
+++ include/eve/as_arithmetic.hpp
@@ -0,0 +1,52 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_AS_ARITHMETIC_HPP_INCLUDED
+#define EVE_AS_ARITHMETIC_HPP_INCLUDED
+
+#include <eve/forward.hpp>
+#include <eve/logical.hpp>
+
+namespace eve
+{
+  template<typename T>
+  struct as_arithmetic
+  {
+    using type = T ;
+  };
+
+  template<>
+  struct as_arithmetic < bool >
+  {
+    using type = bool ;
+  };
+
+  template<typename T>
+  struct as_arithmetic<logical<T>>
+  {
+    using type = T;
+  };
+
+  template<typename T,  typename N,  typename ABI>
+  struct as_arithmetic<wide<T, N, ABI>>
+  {
+    using type = wide<T, N, ABI>;
+  };
+
+  template<typename T, typename N,  typename ABI>
+  struct as_arithmetic<wide<logical<T>, N, ABI>>
+  {
+    using type = wide<T, N>;
+  };
+
+  template<typename T>
+  using as_arithmetic_t = typename as_arithmetic<T>::type;
+}
+
+#endif
