94faff38ef676df86c7c14b8b1e92c30cc465c7d
jtlap <jtlapreste@gmail.com>
2019-03-01 16:23:11 +0100

    is_unordered

---
+++ include/eve/module/core/function/simd/is_unordered.hpp
@@ -0,0 +1,38 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_IS_UNORDERED_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_IS_UNORDERED_HPP_INCLUDED
+
+#include <eve/arch.hpp>
+#include <eve/module/core/function/simd/common/is_unordered.hpp>
+
+// #if defined(EVE_HW_X86)
+// #  if EVE_HW_X86 >= EVE_SSE2_VERSION
+// #    include <eve/module/core/function/simd/x86/sse2/is_unordered.hpp>
+// #  endif
+// #  if EVE_HW_X86 >= EVE_AVX_VERSION
+// #    include <eve/module/core/function/simd/x86/avx/is_unordered.hpp>
+// #  endif
+// #endif
+
+#if defined(EVE_HW_POWERPC)
+#  if EVE_HW_POWERPC >= EVE_VMX_VERSION
+#    include <eve/module/core/function/simd/ppc/vmx/is_unordered.hpp>
+#  endif
+#endif
+
+#if defined(EVE_HW_ARM)
+#  if EVE_HW_ARM >= EVE_NEON_VERSION
+#    include <eve/module/core/function/simd/arm/neon/is_unordered.hpp>
+#  endif
+#endif
+
+#endif
+ 
---
+++ test/unit/module/core/simd/is_unordered/is_unordered.hpp
@@ -0,0 +1,80 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef IS_UNORDERED_HPP
+#define IS_UNORDERED_HPP
+
+#include "test.hpp"
+#include <tts/tests/relation.hpp>
+#include <eve/function/simd/is_unordered.hpp>
+#include <eve/logical.hpp>
+#include <eve/wide.hpp> 
+
+using eve::fixed;
+
+TTS_CASE_TPL("Check is_unordered behavior on homogeneous wide",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>
+            )
+{
+  using eve::wide;
+
+  TTS_SETUP("A correctly initialized wide")
+  {
+    if constexpr(std::is_integral_v<Type>)
+    {                   
+      wide<Type, T> lhs([](int i, int c) { return c - i; }), rhs([](int i, int c) { return i%2 ? i : c-i; });
+      wide < eve::logical < Type>, T >  ref([](int i, int c) { return eve::is_unordered(Type(c - i), Type(i%2 ? i : c-i)); });
+      TTS_SECTION("supports eve::is_unordered") { TTS_EQUAL(ref, eve::is_unordered(lhs, rhs)); }
+    }
+    else
+    {
+      wide<Type, T> lhs([](int i, int c ) { return c - i; }), rhs([](int i, int ) { return i/Type(i); });
+      wide < eve::logical < Type>, T >  ref([](int i, int c) { return eve::is_unordered(Type(c - i), Type(i/Type(i))); });
+      TTS_SECTION("supports eve::is_unordered") { TTS_EQUAL(ref, eve::is_unordered(lhs, rhs)); }
+    }
+    
+  }
+}
+
+TTS_CASE_TPL("Check plus behavior on wide and scalar",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>)
+{
+  using eve::wide;
+
+  TTS_SETUP("A correctly initialized wide and a scalar")
+  {
+    if constexpr(std::is_integral_v<Type>)
+    {                   
+      wide<Type, T> lhs([](auto i, auto) { return i; });
+      wide<eve::logical<Type>, T> ref([](int i, int) { return eve::is_unordered(Type(i), Type(2)); });
+      TTS_SECTION("supports eve::is_unordered wide/scalar") { TTS_EQUAL(ref, eve::is_unordered(lhs, Type(2))); }
+      TTS_SECTION("supports eve::is_unordered scalar/wide") { TTS_EQUAL(ref, eve::is_unordered(Type(2), lhs)); }
+    }
+    else
+    {
+      wide<Type, T> lhs([](auto i, auto) { return i/i; });
+      wide<eve::logical<Type>, T> ref([](int i, int) { return eve::is_unordered(Type(i), Type(2)); });
+      TTS_SECTION("supports eve::is_unordered wide/scalar") { TTS_EQUAL(ref, eve::is_unordered(lhs, Type(2))); }
+      TTS_SECTION("supports eve::is_unordered scalar/wide") { TTS_EQUAL(ref, eve::is_unordered(Type(2), lhs)); }
+    }
+  }
+}
+#endif
