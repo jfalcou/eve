6257df74d8a4bac1cab9d022b469b39a927b1790
Joel Falcou <joel.falcou@lri.fr>
2019-03-02 19:38:39 +0100

    Better info display

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -9,13 +9,8 @@
 
 mkdir -p build
 cd build
-echo "BUILD=$VARIANT"
-echo "OPTIONS=$OPTIONS"
+
 $1 --version
-
-echo "cmake .. -DCMAKE_BUILD_TYPE=$VARIANT -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS=$OPTIONS"
 cmake .. -DCMAKE_BUILD_TYPE=$VARIANT -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS=$OPTIONS
-
 ninja unit -j 8
-
 ctest -D Experimental -j 8
