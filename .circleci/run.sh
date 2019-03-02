e016588ffb9170749867dae4a19f697930b3c281
Joel Falcou <joel.falcou@lri.fr>
2019-03-02 19:32:25 +0100

    Fix handlign fo options

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -9,10 +9,13 @@
 
 mkdir -p build
 cd build
-echo VARIANT=$VARIANT
-echo OPTIONS=$OPTIONS
+echo "BUILD=$VARIANT"
+echo "OPTIONS=$OPTIONS"
 $1 --version
 
-echo cmake .. -DCMAKE_BUILD_TYPE=$VARIANT -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS=$OPTIONS
+echo "cmake .. -DCMAKE_BUILD_TYPE=$VARIANT -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS=$OPTIONS"
+cmake .. -DCMAKE_BUILD_TYPE=$VARIANT -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS=$OPTIONS
+
 ninja unit -j 8
+
 ctest -D Experimental -j 8
