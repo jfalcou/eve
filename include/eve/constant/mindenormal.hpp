fce8235fb00fd9f514ae875343034b3d4951b8ff
jtlap <jtlapreste@gmail.com>
2019-03-03 22:02:41 +0100

    correcting mindenormal

--- include/eve/constant/mindenormal.hpp
+++ include/eve/constant/mindenormal.hpp
@@ -15,21 +15,19 @@
 #include <eve/constant/constant.hpp>
 #include <eve/as.hpp>
 #include <type_traits>
-#include <limits>
 
 namespace eve
 {
   EVE_MAKE_CALLABLE(mindenormal_, mindenormal_);
 
   template<typename T>
-  EVE_FORCEINLINE auto Mindenormal(as_<T> const & = {}) noexcept
+  EVE_FORCEINLINE T Mindenormal(as_<T> const & = {}) noexcept
   {
     using t_t = detail::value_type_t<T>;
-
+    std::cout << "minden" << std::endl; 
     if constexpr(std::is_integral_v<t_t>)     return T(1);
-    if constexpr(std::is_same_v<t_t, float>)  return Constant<float, 0x1UL>();
-    if constexpr(std::is_same_v<t_t, double>) return Constant<double, 0x1ULL>();                
-      return Allbits<T>();
+    if constexpr(std::is_same_v<t_t, float>)  return Constant<T, 0x1U>();
+    if constexpr(std::is_same_v<t_t, double>) return Constant<T, 0x1ULL>();                
   }
 
   EVE_MAKE_NAMED_CONSTANT(mindenormal_, Mindenormal);
