79c58e60b0f2ce8e6a296e583688f07ac12f9b0c
jtlap <jtlapreste@gmail.com>
2019-03-05 21:12:55 +0100

    is_not_finite with docs

---
+++ include/eve/function/scalar/is_not_finite.hpp
@@ -0,0 +1,16 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_FUNCTION_SCALAR_IS_NOT_FINITE_HPP_INCLUDED
+#define EVE_FUNCTION_SCALAR_IS_NOT_FINITE_HPP_INCLUDED
+
+#include <eve/function/definition/is_not_finite.hpp>
+#include <eve/module/core/function/scalar/is_not_finite.hpp>
+
+#endif
