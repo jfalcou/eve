a0ceba0b326c660bf1e9ed4370af0ddf94bec3c9
jtlap <jtlapreste@gmail.com>
2019-03-06 22:21:28 +0100

    bad copy paste in is_ordered

--- include/eve/module/core/function/simd/x86/avx/is_ordered.hpp
+++ include/eve/module/core/function/simd/x86/avx/is_ordered.hpp
@@ -17,7 +17,7 @@
 #include <eve/forward.hpp>
 #include <eve/as_logical.hpp>
 #include <eve/is_logical.hpp>
-#include <eve/constant/false.hpp>
+#include <eve/constant/true.hpp>
 #include <type_traits>
 
 namespace eve::detail
@@ -34,7 +34,7 @@ namespace eve::detail
 
     if constexpr(std::is_same_v<T, float>)  return l_t(_mm256_cmp_ps(v0, v1, _CMP_ORD_Q));
     if constexpr(std::is_same_v<T, double>) return l_t(_mm256_cmp_pd(v0, v1, _CMP_ORD_Q));
-    if constexpr(std::is_integral_v<T> || eve::is_logical_v<T>) return False<l_t>(); 
+    if constexpr(std::is_integral_v<T> || eve::is_logical_v<T>) return True<l_t>(); 
   }
 
   // -----------------------------------------------------------------------------------------------
@@ -49,7 +49,7 @@ namespace eve::detail
 
     if constexpr(std::is_same_v<T, float>)  return l_t(_mm_cmp_ps(v0, v1, _CMP_ORD_Q));
     if constexpr(std::is_same_v<T, double>) return l_t(_mm_cmp_pd(v0, v1, _CMP_ORD_Q));
-    if constexpr(std::is_integral_v<T> || eve::is_logical_v<T>) return False<l_t>(); 
+    if constexpr(std::is_integral_v<T> || eve::is_logical_v<T>) return True<l_t>(); 
   } 
 }
 
