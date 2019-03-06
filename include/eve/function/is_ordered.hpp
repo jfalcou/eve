a626ac0df0af7d5b4c13d4f417952cf8c7078a38
jtlap <jtlapreste@gmail.com>
2019-03-06 18:54:07 +0100

    is_ordered

---
+++ include/eve/function/is_ordered.hpp
@@ -0,0 +1,17 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine 
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_FUNCTION_IS_ORDERED_HPP_INCLUDED
+#define EVE_FUNCTION_IS_ORDERED_HPP_INCLUDED
+
+#include <eve/function/scalar/is_ordered.hpp>
+#include <eve/function/simd/is_ordered.hpp>
+
+#endif
+ 
---
+++ include/eve/module/core/function/simd/is_ordered.hpp
@@ -0,0 +1,26 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_IS_ORDERED_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_IS_ORDERED_HPP_INCLUDED
+
+#include <eve/arch.hpp>
+#include <eve/module/core/function/simd/common/is_ordered.hpp>
+
+#if defined(EVE_HW_X86)
+#  if EVE_HW_X86 >= EVE_SSE2_VERSION
+#    include <eve/module/core/function/simd/x86/sse2/is_ordered.hpp>
+#  endif
+#  if EVE_HW_X86 >= EVE_AVX_VERSION
+#    include <eve/module/core/function/simd/x86/avx/is_ordered.hpp>
+#  endif
+#endif
+
+#endif
+
