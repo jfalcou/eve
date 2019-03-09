bb364ec9c59a4cc4dd9f562dae8234682a56af26
jtlap <jtlapreste@gmail.com>
2019-03-09 07:39:13 +0100

    ifnot_else

---
+++ include/eve/module/core/function/scalar/ifnot_else.hpp
@@ -0,0 +1,45 @@
+//==================================================================================================
+/** 
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry Lapreste
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_IFNOT_ELSE_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_IFNOT_ELSE_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/is_nez.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  // -----------------------------------------------------------------------------------------------
+  // Regular case
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto
+  ifnot_else_(EVE_SUPPORTS(cpu_), T const &cond
+          , U const &t
+          , U const &f) noexcept requires(U, detail::Arithmetic<U>)
+  {
+    return cond ? f : t;
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // logical case
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto
+  ifnot_else_(EVE_SUPPORTS(cpu_), logical<T> const &cond
+          , U const &t
+          , U const &f) noexcept requires(U, detail::Arithmetic<U>)
+  {
+    return is_nez(cond) ? f : t;
+  }  
+}
+
+#endif
---
+++ include/eve/module/core/function/simd/common/ifnot_else.hpp
@@ -0,0 +1,140 @@
+//==================================================================================================
+/** 
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry Lapreste
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IFNOT_ELSE_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IFNOT_ELSE_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/bitwise_select.hpp>
+#include <eve/function/bitwise_mask.hpp>
+#include <eve/function/is_nez.hpp>
+#include <eve/logical.hpp>
+#include <eve/forward.hpp>
+#include <eve/as_arithmetic.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  
+  //-----------------------------------------------------------------------------------------------
+  // Basic
+  template<typename U, typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                , wide<logical<U>, N, ABI> const& v0
+                                , wide<T, N, ABI> const& v1
+                                , wide<T, N, ABI> const& v2
+                                ) noexcept
+  {
+    return bitwise_select( bitwise_mask(v0), v2, v1); 
+  }
+  
+  template<typename U, typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                , wide<logical<U>, N, ABI> const& v0
+                                , wide<logical<T>, N, ABI> const& v1
+                                , wide<logical<T>, N, ABI> const& v2
+                                ) noexcept
+  {
+    using t_t =  wide<logical<T>, N, ABI>; 
+    return bitwise_cast<t_t>(bitwise_select( bitwise_mask(v0), bitwise_mask(v2), bitwise_mask(v1))); 
+  }
+
+  template<typename U, typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                , wide<U, N, ABI> const& v0
+                                , wide<T, N, ABI> const& v1
+                                , wide<T, N, ABI> const& v2
+                                ) noexcept
+  {
+    return ifnot_else(is_nez(v0), v1, v2); 
+  }
+
+  template<typename U, typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                , wide<U, N, ABI> const& v0
+                                , wide<logical<T>, N, ABI> const& v1
+                                , wide<logical<T>, N, ABI> const& v2
+                                ) noexcept
+  {
+    return ifnot_else(is_nez(v0), v1, v2); 
+  }
+
+  //-----------------------------------------------------------------------------------------------
+  // Aggregation
+  template<typename U, typename T, typename N>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                                        , wide<U,N,aggregated_> const& v0
+                                                        , wide<T,N,aggregated_> const& v1
+                                                        , wide<T,N,aggregated_> const& v2
+                                                 ) noexcept requires(wide<T,N>, Arithmetic<U>, Arithmetic<T>)
+  {
+    return aggregate( eve::ifnot_else, v0, v1, v2);
+  }
+
+  template<typename U, typename T, typename N>
+  EVE_FORCEINLINE auto ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                                        , wide<logical<U>,N,aggregated_> const& v0
+                                                        , wide<T,N,aggregated_> const& v1
+                                                        , wide<T,N,aggregated_> const& v2
+                                                        ) noexcept requires(wide<T,N>, Arithmetic<U>, Arithmetic<T>)
+  {
+    return aggregate( eve::ifnot_else, v0, v1, v2);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Emulation with auto-splat inside map for performance purpose
+  template<typename U, typename T, typename N>
+  EVE_FORCEINLINE  wide<T,N,emulated_> ifnot_else_ ( EVE_SUPPORTS(simd_)
+                                                       , wide<U,N,emulated_> const& v0
+                                                       , wide<T,N,emulated_> const& v1
+                                                       , wide<T,N,emulated_> const& v2
+                                                       ) noexcept
+  {
+    return map( eve::ifnot_else, v0, v1, v2);
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Support for mixed type with auto-splat
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto
+  ifnot_else_(EVE_SUPPORTS(simd_)
+          , wide<T, N, ABI> const &v0
+          , T const &v1
+          , T const &v2) noexcept
+  {
+    return eve::ifnot_else(v0, wide<T, N>(v1), wide<T, N>(v2));
+  }
+
+  template<typename T, typename U, typename N, typename ABI>
+  EVE_FORCEINLINE auto
+  ifnot_else_(EVE_SUPPORTS(simd_)
+          , wide<T, N, ABI> const &v0
+          , wide<U, N, ABI> const &v1
+          , U const &v2) noexcept
+  {
+    return eve::ifnot_else(v0, v1, wide<U, N>(v2));
+  }
+
+  template<typename T, typename U, typename N, typename ABI>
+  EVE_FORCEINLINE auto
+  ifnot_else_(EVE_SUPPORTS(simd_)
+          , wide<T, N, ABI> const &v0
+          , U const &v1
+          , wide<U, N, ABI> const &v2) noexcept
+  {
+    return eve::ifnot_else(v0, wide<U, N>(v1), v2);
+  }
+
+}
+
+#endif
