c6795f5269a00eef77bff7e9cc719ed1849c648e
jtlap <jtlapreste@gmail.com>
2019-03-24 17:01:08 +0100

    abs was forgotten

--- test/unit/module/core/scalar/abs/abs.hpp
+++ test/unit/module/core/scalar/abs/abs.hpp
@@ -14,11 +14,24 @@
 #include <eve/function/scalar/abs.hpp>
 #include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
+#include <eve/as_logical.hpp>
+#include <type_traits>
+
+TTS_CASE("Check abs return type")
+{
+  TTS_EXPR_IS(eve::abs(Type()),  Type);
+}
 
 TTS_CASE("Check eve::abs behavior")
 {
-  TTS_EQUAL(eve::abs(Type{0}), Type{1});
-  TTS_EQUAL(eve::abs(Type{2}), Type{3});
+  TTS_EQUAL(eve::abs(Type{0}), Type(0));
+  TTS_EQUAL(eve::abs(Type{2}), Type(2));
+  if constexpr(std::is_signed_v<Type>)
+  {
+    TTS_EQUAL(eve::abs(static_cast<Type>(-2)), Type(2));
+  }
 }
+
 
 #endif
