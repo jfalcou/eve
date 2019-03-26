4961ea333666b227ea96ed61afab52f2a6f6948f
jtlap <jtlapreste@gmail.com>
2019-03-26 08:45:03 +0100

    some doc ouputs and is_gtz

--- test/unit/module/core/scalar/is_gtz/float.cpp
+++ test/unit/module/core/scalar/is_gtz/float.cpp
@@ -7,10 +7,6 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_IS_GTZ_HPP_INCLUDED
-#define EVE_FUNCTION_IS_GTZ_HPP_INCLUDED
 
-#include <eve/function/scalar/is_gtz.hpp>
-#include <eve/function/simd/is_gtz.hpp>
-
-#endif
+using Type = float;
+#include "is_gtz.hpp"
