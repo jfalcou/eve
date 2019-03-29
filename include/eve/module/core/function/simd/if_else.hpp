b2ea4c52b304e2b3ac2c6bc05120869a182c22f1
Joel Falcou <joel.falcou@lri.fr>
2019-03-29 14:21:54 +0100

    Remove spurrious include in simd/if_else.hpp

--- include/eve/module/core/function/simd/if_else.hpp
+++ include/eve/module/core/function/simd/if_else.hpp
@@ -1,7 +1,7 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU 
+  Copyright 2019 Joel FALCOU
   Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
@@ -15,10 +15,10 @@
 #include <eve/module/core/function/simd/common/if_else.hpp>
 
 #if defined(EVE_HW_X86)
-#  if EVE_HW_X86 >= EVE_SSE2_VERSION 
+#  if EVE_HW_X86 >= EVE_SSE2_VERSION
 #    include <eve/module/core/function/simd/x86/sse2/if_else.hpp>
 #  endif
-#  if EVE_HW_X86 >= EVE_SSE4_1_VERSION 
+#  if EVE_HW_X86 >= EVE_SSE4_1_VERSION
 #    include <eve/module/core/function/simd/x86/sse4_1/if_else.hpp>
 #  endif
 #  if (EVE_HW_X86 >= EVE_AVX_VERSION)
@@ -26,7 +26,7 @@
 #  endif
 #  if EVE_HW_X86 >= EVE_AVX2_VERSION
 #    include <eve/module/core/function/simd/x86/avx2/if_else.hpp>
-#  endif 
+#  endif
 #endif
 
 #if defined(EVE_HW_POWERPC)
@@ -35,9 +35,4 @@
 #  endif
 #endif
 
-#if defined(EVE_HW_ARM)
-#  if EVE_HW_ARM >= EVE_NEON_VERSION
-#    include <eve/module/core/function/simd/arm/neon/if_else.hpp>
-#  endif
-#endif
 #endif
