2f7e6c1e472f01807d0c23f89a543058c29f26b9
Joel Falcou <joel.falcou@lri.fr>
2019-03-29 14:22:06 +0100

    Fix spurrious warnings

--- include/eve/module/core/function/simd/ppc/vmx/if_else.hpp
+++ include/eve/module/core/function/simd/ppc/vmx/if_else.hpp
@@ -11,7 +11,6 @@
 #define EVE_MODULE_CORE_FUNCTION_SIMD_PPC_VMX_IF_ELSE_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
-#include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/forward.hpp>
 
@@ -19,13 +18,10 @@ namespace eve::detail
 {
   template<typename T, typename N>
   EVE_FORCEINLINE wide<T, N, ppc_>
-  if_else_ ( EVE_SUPPORTS(vmx_),
-                    wide<logical<T>, N, ppc_> const& m,
-                    wide<T, N, ppc_> const& v0, wide<T, N, ppc_> const& v1
-                  ) noexcept
+  if_else_( EVE_SUPPORTS(vmx_), wide<logical<T>, N, ppc_> const& m,
+            wide<T, N, ppc_> const& v0, wide<T, N, ppc_> const& v1
+          ) noexcept
   {
-    using i_t = wide<as_integer_t<T, unsigned>,N>;
-
     return vec_sel( v1.storage(), v0.storage(), m.storage() );
   }
 }
