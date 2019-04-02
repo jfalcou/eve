2abacd718fc254b163b751bba14565cd9e8cbdaf
jtlap <jtlapreste@gmail.com>
2019-04-02 08:49:54 +0200

    sub doc example

--- test/doc/sub.cpp
+++ test/doc/sub.cpp
@@ -1,4 +1,5 @@
 #include <eve/function/sub.hpp>
+#include <eve/function/is_greater.hpp>
 #include <eve/literals.hpp>
 #include <eve/wide.hpp>
 
@@ -6,7 +7,12 @@ int main()
 {
   using namespace eve::literal;
   eve::wide<float> w( [](int i, int) { return 1.f+i; } );
+  eve::wide<float> v( [](int i, int) { return 1.f-i; } ); 
 
   std::cout << "sub(3.f, 5.5f) = " << eve::sub(3.f,5.5f) << '\n';
   std::cout << "sub({3,3,3,3}, {1,2,3,4}) = " << eve::sub(3._wf, w ) << '\n';
+  std::cout << "sub[3 > 4](2, 5)" << eve::sub[3 > 4](2, 5) << std::endl; 
+  std::cout << "w " << w << std::endl;
+  std::cout << "v " << v << std::endl;   
+  std::cout << "sub[w > 3.0f](w, v)" << eve::sub[w > 3.0f](w, v) << std::endl;  
 }
