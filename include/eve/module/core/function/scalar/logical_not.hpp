d652d2e6b8a13cf271fd05503489a145f9c471b6
Joel Falcou <joel.falcou@lri.fr>
2019-04-03 10:55:34 +0200

    Reimplements logical_not

--- include/eve/module/core/function/scalar/logical_not.hpp
+++ include/eve/module/core/function/scalar/logical_not.hpp
@@ -13,24 +13,35 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
+#include <eve/concept/vectorizable.hpp>
 #include <eve/as_logical.hpp>
 #include <eve/logical.hpp>
 
 namespace eve::detail
 {
   template<typename T>
-  EVE_FORCEINLINE constexpr as_logical_t<T>
-  logical_not_(EVE_SUPPORTS(cpu_), T const &a) noexcept
+  EVE_FORCEINLINE constexpr auto logical_not_(EVE_SUPPORTS(cpu_), T const &a) noexcept
+                            requires( as_logical_t<T>, Vectorizable<T> )
   {
     return !a;
   }
 
   template<typename T>
-  EVE_FORCEINLINE constexpr as_logical_t<T>
-  logical_not_(EVE_SUPPORTS(cpu_), logical<T> const &a) noexcept
+  EVE_FORCEINLINE constexpr auto logical_not_(EVE_SUPPORTS(cpu_), logical<T> const &a) noexcept
+                            requires( as_logical_t<T>, Vectorizable<T> )
   {
     return a.not_value();
   }
 }
 
+// -------------------------------------------------------------------------------------------------
+// Infix operator support
+namespace eve
+{
+  template<typename T>
+  EVE_FORCEINLINE auto operator!(T const &v) noexcept -> decltype(eve::logical_not(v))
+  {
+    return eve::logical_not(v);
+  }
+}
 #endif
--- include/eve/module/core/function/simd/common/logical_not.hpp
+++ include/eve/module/core/function/simd/common/logical_not.hpp
@@ -11,64 +11,45 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_LOGICAL_NOT_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_LOGICAL_NOT_HPP_INCLUDED
 
+#include <eve/detail/is_native.hpp>
 #include <eve/detail/overload.hpp>
 #include <eve/detail/skeleton.hpp>
 #include <eve/detail/abi.hpp>
+#include <eve/concept/vectorized.hpp>
+#include <eve/constant/zero.hpp>
 #include <eve/function/bitwise_cast.hpp>
 #include <eve/function/bitwise_not.hpp>
 #include <eve/function/is_equal.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/as_logical.hpp>
-#include <eve/logical.hpp>
 #include <eve/forward.hpp>
+#include <eve/logical.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
-  template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto logical_not_(EVE_SUPPORTS(simd_),wide<T, N, ABI> const &v) noexcept
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto logical_not_(EVE_SUPPORTS(cpu_), T const &a) noexcept
+                            requires( as_logical_t<T>,Vectorized<T>)
   {
-    if constexpr( is_native_v<ABI> )
+    if constexpr( is_aggregated_v<typename T::abi_type> )
     {
-      return eve::is_equal(v, as(v));
+      return aggregate( eve::logical_not, a);
+    }
+    else if constexpr( is_emulated_v<typename T::abi_type> )
+    {
+      return map( eve::logical_not, a);
     }
     else
     {
-      if( std::is_same_v<ABI,aggregated_>)  return aggregate(eve::logical_not, v);
-      if( std::is_same_v<ABI,emulated_> )   return map(eve::logical_not, v);
+      return is_equal(a, eve::Zero(as(a)));
     }
   }
 
-  template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto logical_not_(EVE_SUPPORTS(simd_), logical<wide<T, N, ABI>> const &v) noexcept
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto logical_not_( EVE_SUPPORTS(cpu_),logical<T> const &a) noexcept
+                            requires( as_logical_t<T>, Vectorized<T> )
   {
-    if constexpr( is_native_v<ABI> )
-    {
-      using t_t = wide<T, N, ABI>;
-      return eve::bitwise_cast<as_logical_t<t_t>>(eve::bitwise_not(eve::bitwise_cast<t_t>(v)));
-    }
-    else
-    {
-      if( std::is_same_v<ABI,aggregated_> ) return aggregate(eve::logical_not, v);
-      if( std::is_same_v<ABI,emulated_> )   return map(eve::logical_not, v);
-    }
-  }
-}
-
-// -------------------------------------------------------------------------------------------------
-// Infix operator support
-namespace eve
-{
-  template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto operator!(wide<T, N, ABI> const &v) noexcept
-  {
-    return eve::logical_not(v);
-  }
-
-  template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto operator!(logical<wide<T, N, ABI>> const &v) noexcept
-  {
-    return eve::logical_not(v);
+    return eve::bitwise_cast<as_logical_t<T>>(eve::bitwise_not(a.bits()));
   }
 }
 
