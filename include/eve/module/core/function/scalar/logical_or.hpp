6a6ba4adca5a65a2e2467e53e203d63fcafb74ca
Joel Falcou <joel.falcou@lri.fr>
2019-04-03 20:33:26 +0200

    Reimplements logical_or

--- include/eve/module/core/function/scalar/logical_or.hpp
+++ include/eve/module/core/function/scalar/logical_or.hpp
@@ -8,21 +8,41 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_LOGICAL_OR_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_SCALAR_LOGICAL_OR_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_LOGICAL_OR_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_LOGICAL_OR_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
+#include <eve/concept/vectorizable.hpp>
 #include <eve/as_logical.hpp>
-#include <eve/logical.hpp>
+#include <eve/is_logical.hpp>
+#include <type_traits>
 
 namespace eve::detail
 {
-  template<typename T>
-  EVE_FORCEINLINE constexpr as_logical_t<T>
-  logical_or_(EVE_SUPPORTS(cpu_), T const &a, T const &b) noexcept
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto logical_or_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+                            requires( as_logical_t<T>, Vectorizable<T>, Vectorizable<U> )
   {
-    return a || b;
+    if constexpr( is_logical_v<T> || is_logical_v<U>)
+    {
+      return static_cast<bool>(a) || static_cast<bool>(b);
+    }
+    else
+    {
+      return a || b;
+    }
+  }
+}
+
+namespace eve
+{
+    // -----------------------------------------------------------------------------------------------
+  // operator &&
+  template<typename T, typename U>
+  EVE_FORCEINLINE auto operator||(T const& a, U const &b) noexcept -> decltype(eve::logical_or(a, b))
+  {
+    return eve::logical_or(a, b);
   }
 }
 
