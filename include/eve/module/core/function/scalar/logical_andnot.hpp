cb87ae320cd3ea7ef50925def927ebabecf4f28d
Joel Falcou <joel.falcou@lri.fr>
2019-04-03 14:28:11 +0200

    Reimplements logical_andnot

--- include/eve/module/core/function/scalar/logical_andnot.hpp
+++ include/eve/module/core/function/scalar/logical_andnot.hpp
@@ -8,30 +8,41 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_LOGICAL_ANDNOT_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_SCALAR_LOGICAL_ANDNOT_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_LOGICAL_ANDNOT_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_LOGICAL_ANDNOT_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/logical_and.hpp>
-#include <eve/function/logical_not.hpp>
+#include <eve/concept/vectorizable.hpp>
 #include <eve/as_logical.hpp>
-#include <eve/logical.hpp>
+#include <eve/is_logical.hpp>
+#include <type_traits>
 
 namespace eve::detail
 {
-  template<typename T>
-  EVE_FORCEINLINE constexpr as_logical_t<T>
-  logical_andnot_(EVE_SUPPORTS(cpu_), T const &a, T const &b) noexcept
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto logical_andnot_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+                            requires( as_logical_t<T>, Vectorizable<T>, Vectorizable<U> )
   {
-    return a && !b;
+    if constexpr( is_logical_v<T> || is_logical_v<U>)
+    {
+      return static_cast<bool>(a) && !static_cast<bool>(b);
+    }
+    else
+    {
+      return a && !b;
+    }
   }
+}
 
-  template<typename T>
-  EVE_FORCEINLINE constexpr as_logical_t<T>
-  logical_andnot_(EVE_SUPPORTS(cpu_), logical<T> const &a, logical<T> const &b) noexcept
+namespace eve
+{
+    // -----------------------------------------------------------------------------------------------
+  // operator &&
+  template<typename T, typename U>
+  EVE_FORCEINLINE auto operator&&(T const& a, U const &b) noexcept -> decltype(eve::logical_andnot(a, b))
   {
-    return logical_and(a, logical_not(b));
+    return eve::logical_andnot(a, b);
   }
 }
 
--- test/unit/module/core/simd/logical_andnot/arithmetic/logical_andnot.hpp
+++ test/unit/module/core/simd/logical_andnot/arithmetic/logical_andnot.hpp
@@ -2,7 +2,7 @@
 /**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry Lapreste
+  Copyright 2019 Jean-Thierry LAPRESTE
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
@@ -26,17 +26,16 @@ TTS_CASE_TPL("Check logical_andnot behavior on homogeneous wide",
              fixed<8>,
              fixed<16>,
              fixed<32>,
-             fixed<64>
-            )
+             fixed<64>)
 {
   using eve::wide;
+  using eve::logical;
 
-  TTS_SETUP("A correctly initialized wide")
-  {
-    wide<Type, T> lhs([](int i, int c) { return c - i; }), rhs([](int i, int ) { return i%2; });
-    wide < eve::logical < Type>, T >  ref([](int i, int c) { return eve::logical_andnot(Type(c - i), Type(i%2)); });
-    TTS_SECTION("supports eve::logical_andnot") { TTS_EQUAL(ref, eve::logical_andnot(lhs, rhs)); }
-  }
+  wide<Type, T>           lhs([](auto i, auto c)  { return c - i; }),
+                          rhs([](auto i, auto)    { return i % 2; });
+  logical<wide<Type, T>>  ref([](auto i, auto c)  { return eve::logical_andnot(Type(c - i), Type(i % 2)); });
+
+  TTS_EQUAL(ref, eve::logical_andnot(lhs, rhs));
 }
 
 TTS_CASE_TPL("Check plus behavior on wide and scalar",
@@ -49,20 +48,18 @@ TTS_CASE_TPL("Check plus behavior on wide and scalar",
              fixed<64>)
 {
   using eve::wide;
+  using eve::logical;
 
-  TTS_SETUP("A correctly initialized wide and a scalar")
-  {
-    wide<Type, T>               lhs([](auto i, auto) { return i; });
-    wide<eve::logical<Type>, T> ref1([](int i, int) { return eve::logical_andnot(Type(i), Type(2)); });
-    wide<eve::logical<Type>, T> ref2([](int i, int) { return eve::logical_andnot(Type(2), Type(i)); });  
-    wide<eve::logical<Type>, T> ref3([](int i, int) { return eve::logical_andnot(Type(i), Type(0)); });
-    wide<eve::logical<Type>, T> ref4([](int i, int) { return eve::logical_andnot(Type(0), Type(i)); });
-    TTS_SECTION("supports eve::logical_andnot") { TTS_EQUAL(ref1, eve::logical_andnot(lhs, Type(2))); }
-    TTS_SECTION("supports eve::logical_andnot") { TTS_EQUAL(ref2, eve::logical_andnot(Type(2), lhs)); }
-    TTS_SECTION("supports eve::logical_andnot") { TTS_EQUAL(ref3, eve::logical_andnot(lhs, Type(0))); }
-    TTS_SECTION("supports eve::logical_andnot") { TTS_EQUAL(ref4, eve::logical_andnot(Type(0), lhs)); }
-  }
+  wide<Type, T>           lhs([](auto i, auto) { return i; });
+  logical<wide<Type, T>> ref1([](auto i, auto) { return eve::logical_andnot(Type(i), Type(2)); });
+  logical<wide<Type, T>> ref2([](auto i, auto) { return eve::logical_andnot(Type(2), Type(i)); });
+  logical<wide<Type, T>> ref3([](auto i, auto) { return eve::logical_andnot(Type(i), Type(0)); });
+  logical<wide<Type, T>> ref4([](auto i, auto) { return eve::logical_andnot(Type(0), Type(i)); });
+
+  TTS_EQUAL(ref1, eve::logical_andnot(lhs, Type(2)));
+  TTS_EQUAL(ref2, eve::logical_andnot(Type(2), lhs));
+  TTS_EQUAL(ref3, eve::logical_andnot(lhs, Type(0)));
+  TTS_EQUAL(ref4, eve::logical_andnot(Type(0), lhs));
 }
-
 
 #endif
