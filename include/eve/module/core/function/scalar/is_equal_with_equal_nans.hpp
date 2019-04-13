bba78d59881aed843c381e420526ad9e694b423d
Joel Falcou <joel.falcou@lri.fr>
2019-04-13 19:04:27 +0200

    Reimplements is_equal_with_equal_nans

--- include/eve/module/core/function/scalar/is_equal_with_equal_nans.hpp
+++ include/eve/module/core/function/scalar/is_equal_with_equal_nans.hpp
@@ -4,7 +4,6 @@
   Copyright 2019 Joel FALCOU
   Copyright 2019 Jean-Thierry LAPRESTE
 
-
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
@@ -14,19 +13,26 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/logical.hpp>
+#include <eve/concept/vectorizable.hpp>
+#include <eve/as_logical.hpp>
+#include <eve/is_logical.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
-  template<typename T>
-  EVE_FORCEINLINE constexpr logical<T>
-  is_equal_with_equal_nans_(EVE_SUPPORTS(cpu_), T const &a, T const &b) noexcept
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr
+  auto  is_equal_with_equal_nans_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+        requires( as_logical_t<T>, Vectorizable<T>, Vectorizable<U> )
   {
     if constexpr(std::is_floating_point_v<T>)
+    {
       return (a == b) || ((a!= a) && (b!= b));
+    }
     else
+    {
       return a == b;
+    }
   }
 }
 
--- test/unit/module/core/simd/is_equal_with_equal_nans/arithmetic/uint64.cpp
+++ test/unit/module/core/simd/is_equal_with_equal_nans/arithmetic/uint64.cpp
@@ -1,17 +1,13 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2018 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_SCALAR_IS_EQUAL_WITH_EQUAL_NANS_HPP_INCLUDED
-#define EVE_FUNCTION_SCALAR_IS_EQUAL_WITH_EQUAL_NANS_HPP_INCLUDED
 
-#include <eve/function/definition/is_equal_with_equal_nans.hpp>
-#include <eve/module/core/function/scalar/is_equal_with_equal_nans.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::uint64_t;
+#include "is_equal_with_equal_nans.hpp"
