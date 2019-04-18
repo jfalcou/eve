ffe7eb34d1b9545b3edbc869f81f293e516cb4a8
Joel Falcou <joel.falcou@lri.fr>
2019-04-18 09:49:17 +0200

    Fix tests in doc for if(not)_else

--- test/doc/ifnot_else.cpp
+++ test/doc/ifnot_else.cpp
@@ -4,17 +4,17 @@
 
 using iT = std::int32_t;
 using wide_it = eve::wide<iT, eve::fixed<4>>;
-using wide_lt = eve::wide<eve::logical<iT>, eve::fixed<4>>;
+using wide_lt = eve::as_logical_t<wide_it>;
 
 int main() {
   wide_it pi = {3, -2, -10, 0};
   wide_it qi = {4, -1, 0, 5};
   wide_it si = {2, -3, 0, 4};
-  wide_lt lsi= {true, false, false, true}; 
+  wide_lt lsi= {true, false, false, true};
 
   std::cout << "---- simd" << '\n'
             << " <- si =                              " << si << '\n'
-            << " <- lsi =                             " << lsi << '\n'  
+            << " <- lsi =                             " << lsi << '\n'
             << " <- pi =                              " << pi << '\n'
             << " <- qi =                              " << qi << '\n'
             << " -> eve::ifnot_else(si, pi, qi) =     "
@@ -23,7 +23,7 @@ int main() {
             << eve::ifnot_else(lsi, pi, qi) << '\n';
 
   iT ssi = 3, xi = 3, yi = 4;
-  eve::logical<iT>  lssi = false; 
+  eve::logical<iT>  lssi = false;
 
   std::cout << "---- scalar" << '\n'
             << " ssi =                                " << ssi << '\n'
