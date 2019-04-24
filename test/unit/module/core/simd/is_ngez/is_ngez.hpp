7cddaceeacb288281d2218b8a766b419546d364a
jtlap <jtlapreste@gmail.com>
2019-04-24 10:48:46 +0200

    is_ngez

---
+++ test/unit/module/core/simd/is_ngez/is_ngez.hpp
@@ -0,0 +1,58 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef IS_NGEZ_HPP
+#define IS_NGEZ_HPP
+
+#include "test.hpp"
+#include <tts/tests/relation.hpp>
+#include <eve/function/simd/is_ngez.hpp>
+#include <eve/constant/false.hpp>
+#include <eve/logical.hpp>
+#include <eve/wide.hpp>
+
+using eve::fixed;
+
+TTS_CASE_TPL("Check is_ngez behavior on wide",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>)
+{
+  using eve::wide;
+  using eve::logical;
+
+  wide<Type, T>          lhs([](auto i, auto) { return i%2; });
+  logical<wide<Type, T>> ref([](auto i, auto) { return eve::is_ngez(Type(i%2)); });
+
+  TTS_EQUAL(ref, eve::is_ngez(lhs));
+}
+
+TTS_CASE_TPL("Check is_ngez behavior on logical<wide>",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>)
+{
+  using eve::wide;
+  using eve::logical;
+  using t_t = wide<Type, T>; 
+  eve::logical<t_t> lhs([](auto i, auto) { return (i%2 == 0); });
+
+  TTS_EQUAL(eve::False<t_t>(), eve::is_ngez(lhs));
+}
+
+#endif
