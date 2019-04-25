1feb874c31d34dac95b0a59279808dd22e040310
jtlap <jtlapreste@gmail.com>
2019-04-25 15:04:24 +0200

    joel's remarks

---
+++ benchmark/module/core/scalar/is_negative/float.cpp
@@ -0,0 +1,17 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/function/is_negative.hpp>
+
+#define TYPE()        float
+#define FUNCTION()    eve::is_negative
+#define SAMPLES(N)    random<T>(N,-100.f,100.f)
+
+#include "bench.hpp"
---
+++ benchmark/module/core/simd/is_negative/uint16.cpp
@@ -0,0 +1,19 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/function/is_negative.hpp>
+#include <eve/wide.hpp>
+#include <cstddef>
+
+#define TYPE()        eve::wide<std::uint16_t>
+#define FUNCTION()    eve::is_negative
+#define SAMPLES(N)    random<T>(N,0,10000)
+
+#include "bench.hpp"
