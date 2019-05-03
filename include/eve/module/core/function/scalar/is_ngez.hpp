d52ed7068c38393b4ee21bb6b9447cc609d04631
Joel Falcou <joel.falcou@lri.fr>
2019-05-03 09:05:28 +0200

    Clean-up in is_ngez

--- include/eve/module/core/function/scalar/is_ngez.hpp
+++ include/eve/module/core/function/scalar/is_ngez.hpp
@@ -13,27 +13,31 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/concept/vectorizable.hpp>
-#include <eve/as_logical.hpp>
-#include <eve/is_logical.hpp>
 #include <eve/function/scalar/is_nan.hpp>
 #include <eve/function/scalar/is_ltz.hpp>
+#include <eve/as_logical.hpp>
+#include <type_traits>
 
 namespace eve::detail
 {
   template<typename T>
-  EVE_FORCEINLINE constexpr auto is_ngez_(EVE_SUPPORTS(cpu_), T const &a) noexcept
-                            requires( as_logical_t<T>, Vectorizable<T> )
+  EVE_FORCEINLINE constexpr as_logical_t<T> is_ngez_(EVE_SUPPORTS(cpu_), T const &a) noexcept
   {
-    if constexpr(std::is_unsigned_v<T> || is_logical_v<T>)
+    if constexpr(std::is_unsigned_v<T>)
+    {
       return False(as(a));
+    }
     else
     {
-      if constexpr(std::is_floating_point_v<T>)
-        return is_ltz(a).value() || is_nan(a).value();
-      if constexpr(std::is_integral_v<T>)
-        return is_ltz(a); 
+      if constexpr(std::is_floating_point_v<T>) return is_ltz(a) || is_nan(a);
+      else                                      return is_ltz(a);
     }
+  }
+
+  template<typename T>
+  EVE_FORCEINLINE constexpr as_logical_t<T> is_ngez_(EVE_SUPPORTS(cpu_), logical<T> const &a) noexcept
+  {
+    return False(as(a));
   }
 }
 
--- include/eve/module/core/function/simd/common/is_ngez.hpp
+++ include/eve/module/core/function/simd/common/is_ngez.hpp
@@ -12,33 +12,34 @@
 #define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IS_NGEZ_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
-#include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/concept/vectorized.hpp>
-#include <eve/function/is_not_equal.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/constant/false.hpp>
-#include <eve/as_logical.hpp>
-#include <eve/is_logical.hpp>
+#include <eve/constant/zero.hpp>
+#include <eve/function/is_not_greater_equal.hpp>
 #include <eve/function/is_ltz.hpp>
 #include <eve/forward.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
-  template<typename T>
-  EVE_FORCEINLINE auto is_ngez_(EVE_SUPPORTS(simd_), T const &v) noexcept
-                            requires( as_logical_t<T>, Vectorized<T> )
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto is_ngez_(EVE_SUPPORTS(simd_), wide<T, N, ABI> const &v) noexcept
   {
-    if constexpr(std::is_unsigned_v<T> || is_logical_v<T>)
+    if constexpr(std::is_unsigned_v<T>)
+    {
       return False(as(v));
+    }
     else
     {
-      if constexpr(std::is_floating_point_v<T>)
-        return is_not_greater_equal(v, Zero(as(v)));
-      else 
-        return is_ltz(v); 
-    }   
+      if constexpr(std::is_floating_point_v<T>) return is_not_greater_equal(v, Zero(as(v)));
+      else                                      return is_ltz(v);
+    }
+  }
+
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto is_ngez_(EVE_SUPPORTS(simd_), logical<wide<T, N, ABI>> const &v) noexcept
+  {
+    return False(as(v));
   }
 }
 
