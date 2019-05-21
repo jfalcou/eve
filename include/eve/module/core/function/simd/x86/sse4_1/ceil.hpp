5d49bc7c2cf80e778247d429cb14ceaea6a1d617
jtlap <jtlapreste@gmail.com>
2019-05-21 08:57:39 +0200

    ceil

---
+++ include/eve/module/core/function/simd/x86/sse4_1/ceil.hpp
@@ -0,0 +1,42 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine 
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_X86_SSE4_1_CEIL_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_X86_SSE4_1_CEIL_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/forward.hpp>
+#include <type_traits>
+ 
+namespace eve::detail
+{
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE auto ceil_(EVE_SUPPORTS(sse4_1_),
+                            wide<T, N, sse_> const &a0) noexcept
+  requires(wide<T, N, ABI>, Arithmetic<T>)
+  {
+    if constexpr(std::is_floating_point_v<T>)
+    {
+      if constexpr(std::is_same_v<T, double>) return  _mm_ceil_pd(a0);
+      if constexpr(std::is_same_v<T, float>)  return  _mm_ceil_ps(a0);     
+    }
+    else return a0;
+  }
+
+ 
+   
+}
+
+
+
+#endif
