f6f7de04f6ac57163091e6dc947380599ad3b713
jtlap <jtlapreste@gmail.com>
2019-05-21 09:13:41 +0200

    floor

--- include/eve/module/core/function/scalar/floor.hpp
+++ include/eve/module/core/function/scalar/floor.hpp
@@ -8,34 +8,33 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_CEIL_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_CEIL_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_FLOOR_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_FLOOR_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
-#include <eve/detail/skeleton.hpp>
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/inc.hpp>
-#include <eve/function/is_less.hpp>
-#include <eve/function/trunc.hpp>
-#include <eve/forward.hpp>
+#include <eve/function/dec.hpp>
+#include <eve/tags.hpp>
 #include <type_traits>
-#include <cassert>
 
 namespace eve::detail
 {
-  template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto ceil_(EVE_SUPPORTS(simd_),
-                            wide<T, N, ABI> const &a0) noexcept
-  requires(wide<T, N, ABI>, Arithmetic<T>)
+  // -----------------------------------------------------------------------------------------------
+  // Regular case
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto floor_(EVE_SUPPORTS(cpu_)
+                                  , T const &a0) noexcept requires( T, Arithmetic<T>)
   {
     if constexpr(std::is_floating_point_v<T>)
     {
       auto z =  trunc(a0);
-      return inc[z < a0](z);
+      return dec[z > a0](z);
     }
-    else return a0;
+    else return a0; 
   }
+  
 }
+
 
 #endif
