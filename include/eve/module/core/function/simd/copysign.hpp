4e383048893a2723f006fd00394e31bd1fb7bdc8
jtlap <jtlapreste@gmail.com>
2019-05-22 19:07:07 +0200

    copysign

--- include/eve/module/core/function/simd/copysign.hpp
+++ include/eve/module/core/function/simd/copysign.hpp
@@ -1,17 +1,18 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
   Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Joel Falcou
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_COPYSIGN_HPP_INCLUDED
-#define EVE_FUNCTION_COPYSIGN_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COPYSIGN_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COPYSIGN_HPP_INCLUDED
 
-#include <eve/function/scalar/copysign.hpp>
-#include <eve/function/simd/copysign.hpp>
+#include <eve/arch.hpp>
+#include <eve/module/core/function/simd/common/copysign.hpp>
 
 #endif
+ 
--- test/unit/module/core/scalar/copysign/uint32.cpp
+++ test/unit/module/core/scalar/copysign/uint32.cpp
@@ -2,16 +2,13 @@
 /**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_COPYSIGN_HPP_INCLUDED
-#define EVE_FUNCTION_COPYSIGN_HPP_INCLUDED
 
-#include <eve/function/scalar/copysign.hpp>
-#include <eve/function/simd/copysign.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::uint32_t;
+#include "copysign.hpp"
