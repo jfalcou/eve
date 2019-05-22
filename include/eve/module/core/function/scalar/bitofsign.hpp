acafa0d30eabc608ab58c0c382a738881efd8b30
jtlap <jtlapreste@gmail.com>
2019-05-22 08:38:29 +0200

    bitofsign

---
+++ include/eve/module/core/function/scalar/bitofsign.hpp
@@ -0,0 +1,33 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_BITOFSIGN_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_BITOFSIGN_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/bitwise_and.hpp>
+#include <eve/constant/signmask.hpp>
+#include <eve/concept/vectorizable.hpp>
+#include <eve/platform.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto bitofsign_(EVE_SUPPORTS(cpu_)
+                                        , T const &a) noexcept
+  requires(T, Vectorizable<T> )
+  {
+    return bitwise_and(a, Signmask(as(a))); 
+  }
+}
+
+#endif
