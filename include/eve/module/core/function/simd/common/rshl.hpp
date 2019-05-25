264b45d81ab4362b72f4843cddd5e2aeb218ed8a
jtlap <jtlapreste@gmail.com>
2019-05-25 16:01:12 +0200

    revisiting common version

--- include/eve/module/core/function/simd/common/rshl.hpp
+++ include/eve/module/core/function/simd/common/rshl.hpp
@@ -28,46 +28,42 @@ namespace eve::detail
   // Basic
   template<typename T, typename U, typename N, typename ABI>
   EVE_FORCEINLINE auto
+  rshl_(EVE_SUPPORTS(simd_), wide<T, N, ABI> const &v0, wide<U, N, ABI> const &v1) noexcept
+  {
+    EVE_ASSERT(detail::assert_good_shift<T>(abs(v1)),
+               "[ eve::rshl scalar] - at least shift absolute value abs(" << v1 << ") is out of the range [0, "
+                                                                 << sizeof(T) * 8 << "[.");
+    if constexpr(std::is_aggregated_v<ABI>)
+      return aggregate(eve::rshl, v0, v1);
+    else if   constexpr(std::is_emulated_v<ABI>)
+      return map(eve::rshl, v0, v1);
+    else if constexpr(std::is_unsigned_v<U>)
+      return shl(v0, v1);
+    else 
+    {
+#ifndef NDEBUG
+      return if_else(is_gtz(v1), shl(v0, max(Zero(as(v1)), v1)), shr(v0, max(Zero(as(v1)), -v1)));
+#else
+      return if_else(is_gtz(v1), shl(v0, v1), shr(v0, -v1));
+#endif
+    }
+  }
+  
+  
+  template<typename T, typename U, typename N, typename ABI>
+  EVE_FORCEINLINE auto
   rshl_(EVE_SUPPORTS(simd_), wide<T, N, ABI> const &v0, U const &v1) noexcept
   {
     EVE_ASSERT(detail::assert_good_shift<T>(abs(v1)),
                "[ eve::rshl scalar] - Shift absolute value abs(" << v1 << ") is out of the range [0, "
                                                                  << sizeof(T) * 8 << "[.");
-    if constexpr(std::is_unsigned_v<U>)
-      return shl(v0, v1);
-    else
-    {
 #ifndef NDEBUG
-      if constexpr(is_wide_v<U>)
-        return if_else(is_gtz(v1), shl(v0, max(Zero(as(v1)), v1)), shr(v0, max(Zero(as(v1)), -v1)));
-      else
-        return is_gtz(v1) ?  shl(v0, max(Zero(as(v1)), v1)) : shr(v0, max(Zero(as(v1)), -v1));
+    return if_else(is_gtz(v1), shl(v0, max(Zero(as(v1)), v1)), shr(v0, max(Zero(as(v1)), -v1)));
 #else
-      if constexpr(is_vectorized_v<U>)
-        return if_else(is_gtz(v1), shl(v0, v1), shr(v0, -v1));
-      else
-        return is_gtz(v1) ?  shl(v0, v1) : shr(v0, -v1);
+    return if_else(is_gtz(v1), shl(v0, v1), shr(v0, -v1));
 #endif
-    }
   }
 
-  // -----------------------------------------------------------------------------------------------
-  // Aggregation
-  template<typename T, typename U, typename N>
-  EVE_FORCEINLINE auto
-  rshl_(EVE_SUPPORTS(simd_), wide<T, N, aggregated_> const &v0, U const &v1) noexcept
-  {
-    return aggregate(eve::rshl, v0, v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Emulation 
-  template<typename T, typename U, typename N>
-  EVE_FORCEINLINE auto
-  rshl_(EVE_SUPPORTS(simd_), wide<T, N, emulated_> const &v0, U const &v1) noexcept
-  {
-    return map(eve::rshl, v0, v1);
-  }  // -------------------------------------------------------------------------------------
 }
 
 #endif
