25e677a2a33d21eddbd14ad1e2026f161648ee70
jtlap <jtlapreste@gmail.com>
2019-05-26 15:46:24 +0200

    average

--- test/unit/module/core/scalar/average/average.hpp
+++ test/unit/module/core/scalar/average/average.hpp
@@ -8,10 +8,22 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_AVERAGE_HPP_INCLUDED
-#define EVE_FUNCTION_AVERAGE_HPP_INCLUDED
+#ifndef AVERAGE_HPP
+#define AVERAGE_HPP
 
 #include <eve/function/scalar/average.hpp>
-#include <eve/function/simd/average.hpp>
+#include <tts/tts.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <type_traits>
+
+
+TTS_CASE("Check eve::average behavior")
+{
+  TTS_EQUAL(eve::average(Type{0}, Type{0}), Type{0});
+  TTS_EQUAL(eve::average(Type{0}, Type{2}), Type{1});
+  TTS_EQUAL(eve::average(Type{2}, Type{0}), Type{1});
+  TTS_EQUAL(eve::average(Type{1}, Type{1}), Type{1});
+}
 
 #endif
