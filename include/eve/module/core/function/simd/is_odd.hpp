5b9c464ac01879877cd17e43ba2244c4a215b2f7
jtlap <jtlapreste@gmail.com>
2019-05-30 14:44:40 +0200

    is_odd

--- include/eve/module/core/function/simd/is_odd.hpp
+++ include/eve/module/core/function/simd/is_odd.hpp
@@ -8,10 +8,11 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_IS_ODD_HPP_INCLUDED
-#define EVE_FUNCTION_IS_ODD_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_IS_ODD_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_IS_ODD_HPP_INCLUDED
 
-#include <eve/function/scalar/is_odd.hpp>
-#include <eve/function/simd/is_odd.hpp>
+#include <eve/arch.hpp>
+#include <eve/module/core/function/simd/common/is_odd.hpp>
 
 #endif
+
--- test/unit/module/core/simd/is_odd/uint16.cpp
+++ test/unit/module/core/simd/is_odd/uint16.cpp
@@ -1,17 +1,13 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2018 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_IS_ODD_HPP_INCLUDED
-#define EVE_FUNCTION_IS_ODD_HPP_INCLUDED
 
-#include <eve/function/scalar/is_odd.hpp>
-#include <eve/function/simd/is_odd.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::uint16_t;
+#include "is_odd.hpp"
