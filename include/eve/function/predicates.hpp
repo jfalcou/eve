dd810f586a2da9df429c3c6bb069223409f46776
jtlap <jtlapreste@gmail.com>
2019-05-30 14:35:29 +0200

    is_even + is_flint.txt

--- include/eve/function/predicates.hpp
+++ include/eve/function/predicates.hpp
@@ -15,9 +15,9 @@
 #include <eve/function/is_equal.hpp>
 #include <eve/function/is_equal_with_equal_nans.hpp>
 #include <eve/function/is_eqz.hpp>
-// #include <eve/function/is_even.hpp>
+#include <eve/function/is_even.hpp>
 #include <eve/function/is_finite.hpp>
-// #include <eve/function/is_flint.hpp>
+#include <eve/function/is_flint.hpp>
 #include <eve/function/is_gez.hpp>
 #include <eve/function/is_greater_equal.hpp>
 #include <eve/function/is_greater.hpp>
---
+++ include/eve/module/core/function/simd/common/is_even.hpp
@@ -0,0 +1,40 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IS_EVEN_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_IS_EVEN_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/concept/vectorized.hpp>
+#include <eve/constant/one.hpp>
+#include <eve/function/is_eqz.hpp>
+#include <eve/function/is_flint.hpp>
+#include <eve/function/mul.hpp>
+#include <eve/function/bitwise_and.hpp>
+#include <eve/as_logical.hpp>
+#include <eve/forward.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto is_even_(EVE_SUPPORTS(simd_), T const &a) noexcept
+  requires( as_logical_t<T>, Vectorized<T> )
+  {
+    if constexpr(std::is_floating_point_v<typename T::value_type>)
+      return  is_flint(a*Half(as(a)));
+    else
+      return is_eqz(bitwise_and(a, One(as(a))));
+  }
+}
+
+#endif
