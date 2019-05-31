9ce207661a115a7f29c8251fb7919a431874849f
Joel Falcou <joel.falcou@lri.fr>
2019-05-31 16:21:22 +0200

    Remove useless includes and requires clause

--- include/eve/module/core/function/simd/common/frac.hpp
+++ include/eve/module/core/function/simd/common/frac.hpp
@@ -12,8 +12,6 @@
 #define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_FRAC_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
-#include <eve/detail/skeleton.hpp>
-#include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/function/sub.hpp>
 #include <eve/function/trunc.hpp>
@@ -26,7 +24,6 @@ namespace eve::detail
   template<typename T, typename N, typename ABI>
   EVE_FORCEINLINE auto frac_(EVE_SUPPORTS(simd_),
                             wide<T, N, ABI> const &a0) noexcept
-  requires(wide<T, N, ABI>, Arithmetic<T>)
   {
     if constexpr(std::is_floating_point_v<T>)
       return a0-trunc(a0); 
