201af5599f3069638a2be03ce292e044e67a6f8a
Joel Falcou <joel.falcou@lri.fr>
2019-06-02 15:43:04 +0200

    Benchmarks for regular, raw and pedantic rec

---
+++ benchmark/module/core/simd/rec/raw/uint8.cpp
@@ -0,0 +1,19 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/function/rec.hpp>
+#include <eve/wide.hpp>
+#include <cstddef>
+
+#define TYPE()        eve::wide<std::uint8_t>
+#define FUNCTION()    eve::rec[eve::raw_]
+#define SAMPLES(N)    random<T>(N,1,200)
+
+#include "bench.hpp"
