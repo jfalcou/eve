2a5b01785fca90bb73e024b530d22b564f239791
Joel Falcou <joel.falcou@lri.fr>
2019-06-02 23:01:37 +0200

    Correct scalar abs

--- include/eve/module/core/function/scalar/abs.hpp
+++ include/eve/module/core/function/scalar/abs.hpp
@@ -15,7 +15,7 @@
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/tags.hpp>
-#include <eve/function/bitwise_notand.hpp>
+#include <eve/function/bitwise_andnot.hpp>
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
@@ -23,42 +23,40 @@
 
 namespace eve::detail
 {
-  // -----------------------------------------------------------------------------------------------
-  // Regular case
   template<typename T>
-  EVE_FORCEINLINE constexpr auto abs_(EVE_SUPPORTS(cpu_)
-                                  , T const &a) noexcept
-  requires( T, Arithmetic<T>)
+  EVE_FORCEINLINE constexpr
+  auto abs_(EVE_SUPPORTS(cpu_), T const& a) noexcept requires( T, Arithmetic<T>)
   {
-
-    if constexpr(std::is_floating_point_v<T>) return a<T(0) ? -a : a;
-    //return bitwise_notand(Mzero<T>(),a); //TODO TEST if bitwise_and with the mzero complement is speedier ?
-    if constexpr(std::is_integral_v<T> && std::is_unsigned_v<T>)  return a;
-    if constexpr(std::is_integral_v<T> && std::is_signed_v<T>)
+    if constexpr(std::is_floating_point_v<T>)
     {
-      using u_t = eve::detail::as_integer_t<T, unsigned>;
-      u_t mask = a >> (sizeof(T)*8 - 1);
-      return (a + mask) ^ mask;
+      return bitwise_andnot(a, Mzero(as(a)) );
+    }
+    else if constexpr(std::is_unsigned_v<T>)
+    {
+      return a;
+    }
+    else
+    {
+      return a < T(0) ? -a : a;
     }
   }
 
   // -----------------------------------------------------------------------------------------------
   // saturated case
   template<typename T>
-  EVE_FORCEINLINE constexpr auto abs_(EVE_SUPPORTS(cpu_)
-                                  , saturated_type const &     
-                                  , T const &a) noexcept
-  requires( T, Arithmetic<T>)
+  EVE_FORCEINLINE constexpr
+  auto  abs_(EVE_SUPPORTS(cpu_), saturated_type const &, T const &a) noexcept
+        requires( T, Arithmetic<T>)
   {
     if constexpr(std::is_floating_point_v<T>)
       return a<T(0) ? -a : a;
+
     if constexpr(std::is_integral_v<T> && std::is_unsigned_v<T>)
       return a;
-    if constexpr(std::is_integral_v<T> && std::is_signed_v<T>)
-      return (a == Valmin(as(a))) ? Valmax(as(a)) : eve::abs(a); 
-  }
-  
-}
 
+    if constexpr(std::is_integral_v<T> && std::is_signed_v<T>)
+      return (a == Valmin(as(a))) ? Valmax(as(a)) : eve::abs(a);
+  }
+}
 
 #endif
