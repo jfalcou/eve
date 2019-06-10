3e45fb3f020bfe6b8858f018978658b94b1aeb84
jtlap <jtlapreste@gmail.com>
2019-06-10 16:00:09 +0200

    arg+ tests txt for arg and conj

---
+++ include/eve/function/simd/arg.hpp
@@ -0,0 +1,16 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_FUNCTION_SIMD_ARG_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_ARG_HPP_INCLUDED
+
+#include <eve/function/scalar/arg.hpp>
+
+#endif
