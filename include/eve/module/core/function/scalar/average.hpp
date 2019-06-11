2c1fc258caca52201cbb586b4f90f65044e704b9
Joel Falcou <joel.falcou@lri.fr>
2019-06-11 09:16:13 +0200

    Fix scalar overflow

--- include/eve/module/core/function/scalar/average.hpp
+++ include/eve/module/core/function/scalar/average.hpp
@@ -14,27 +14,24 @@
 #include <eve/detail/overload.hpp>
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-// #include <eve/function/scalar/bitwise_and.hpp>
-// #include <eve/function/scalar/bitwise_xor.hpp>
-#include <eve/function/scalar/inc.hpp>
-#include <eve/function/scalar/fma.hpp>
-#include <eve/function/scalar/shr.hpp>
 #include <eve/constant/half.hpp>
+#include <eve/function/scalar/fma.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
-  // -----------------------------------------------------------------------------------------------
-  // Regular case
   template<typename T>
   EVE_FORCEINLINE constexpr auto average_(EVE_SUPPORTS(cpu_), T const &a0, T const &a1) noexcept
   requires(T, Arithmetic<T>)
   {
     if constexpr(std::is_integral_v<T>)
-//      return bitwise_and(a0, a1)+shr(bitwise_xor(a0, a1),1);
-      return shr(inc(a0+a1), 1); 
+    {
+      return (a0 & a1) + ((a0 ^ a1)>>1);
+    }
     else
+    {
       return fma(a0,Half(as(a0)),a1*Half(as(a0)));
+    }
   }
 }
 
