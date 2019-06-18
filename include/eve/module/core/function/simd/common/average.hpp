a759510c1634a1538333b5f28dca0b0b02d895f4
Joel Falcou <joel.falcou@lri.fr>
2019-06-18 21:20:18 +0200

    Cleanup of common avg

--- include/eve/module/core/function/simd/common/average.hpp
+++ include/eve/module/core/function/simd/common/average.hpp
@@ -15,10 +15,7 @@
 #include <eve/detail/skeleton.hpp>
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-// #include <eve/function/bitwise_and.hpp>
-// #include <eve/function/bitwise_xor.hpp>
 #include <eve/function/add.hpp>
-#include <eve/function/inc.hpp>
 #include <eve/function/fma.hpp>
 #include <eve/function/mul.hpp>
 #include <eve/function/shr.hpp>
@@ -63,7 +60,9 @@ namespace eve::detail
             return bitwise_and(a, b)+shr(bitwise_xor(a, b),1);
           }
           else
-            return fma(a,Half<T>(),b*Half<T>()); 
+          {
+            return fma(a,Half<T>(),b*Half<T>());
+          }
         }
       }
       else
