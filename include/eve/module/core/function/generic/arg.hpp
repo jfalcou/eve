824359f3df4d8730e81baa36c136ed231c6588d9
Joel Falcou <joel.falcou@lri.fr>
2019-06-19 10:51:16 +0200

    Use static_assert for arg/conj

--- include/eve/module/core/function/generic/arg.hpp
+++ include/eve/module/core/function/generic/arg.hpp
@@ -13,17 +13,13 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/detail/meta.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/function/is_nan.hpp>
 #include <eve/function/is_negative.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/pi.hpp>
-#include <eve/constant/zero.hpp>  
-#include <eve/assert.hpp>
+#include <eve/constant/zero.hpp>
 #include <eve/tags.hpp>
-#include <eve/as.hpp>
-#include <eve/assert.hpp>
 #include <type_traits>
 
 namespace eve::detail
@@ -32,34 +28,28 @@ namespace eve::detail
   EVE_FORCEINLINE constexpr T arg_(EVE_SUPPORTS(cpu_)
                                   , T const &a) noexcept
   {
-    if constexpr(!std::is_floating_point_v<value_type_t<T>>)
-    {
-      EVE_ASSERT(std::is_floating_point_v<value_type_t<T>>, "[eve::arg] -this function is not to be used with integral types");
-      return{};
-    }
-    else
-      return if_else(is_negative(a),Pi(as(a)), eve::zero_);
+    static_assert ( std::is_floating_point_v<value_type_t<T>>
+                  , "[eve::arg] -this function is not to be used with integral types"
+                  );
+
+    return if_else(is_negative(a),Pi(as(a)), eve::zero_);
   }
-  
+
   template<typename T>
   EVE_FORCEINLINE constexpr T arg_(EVE_SUPPORTS(cpu_)
-                                  , pedantic_type const &  
+                                  , pedantic_type const &
                                   , T const &a) noexcept
   {
-    if constexpr(!std::is_floating_point_v<value_type_t<T>>)
-     {
-     EVE_ASSERT(std::is_floating_point_v<value_type_t<T>>, "[eve::arg[pedantic_]] -this function is notto be used with integral types"); 
-      return{};
-    }
-    else
-    {
-      auto r = arg(a); 
+    static_assert ( std::is_floating_point_v<value_type_t<T>>
+                  , "[eve::arg[pedantic_]] -this function is notto be used with integral types"
+                  );
+
+    auto r = arg(a);
 #ifndef BOOST_SIMD_NO_NANS
-      return if_else(is_nan(a),eve::allbits_, r);
+    return if_else(is_nan(a),eve::allbits_, r);
 #else
-      return r;
+    return r;
 #endif
-    }
   }
 }
 
--- include/eve/module/core/function/generic/conj.hpp
+++ include/eve/module/core/function/generic/conj.hpp
@@ -13,9 +13,6 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/detail/meta.hpp>
-#include <eve/function/sqr.hpp>
-#include <eve/assert.hpp>
 #include <type_traits>
 
 namespace eve::detail
@@ -23,12 +20,10 @@ namespace eve::detail
   template<typename T>
   EVE_FORCEINLINE constexpr T conj_(EVE_SUPPORTS(cpu_), T const &a) noexcept
   {
-    if constexpr(!std::is_floating_point_v<value_type_t<T>>)
-    {
-      EVE_ASSERT(std::is_floating_point_v<value_type_t<T>>, "[eve::conj] -this function is not to be used with integral types");
-      return {}; 
-    }
-    else
+      static_assert ( std::is_floating_point_v<value_type_t<T>>
+                    , "[eve::conj] -this function is not to be used with integral types"
+                    );
+
       return a;
   }
 }
