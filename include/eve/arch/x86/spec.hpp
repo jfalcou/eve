ceb153dfc0db5a6e3461298fde364b40e2391499
Joel Falcou <joel.falcou@lri.fr>
2019-07-26 23:24:08 +0200

    Simplify x86 arch headers

--- include/eve/arch/x86/spec.hpp
+++ include/eve/arch/x86/spec.hpp
@@ -11,38 +11,77 @@
 #ifndef EVE_ARCH_X86_SPEC_HPP_INCLUDED
 #define EVE_ARCH_X86_SPEC_HPP_INCLUDED
 
+#include <eve/detail/compiler.hpp>
 #include <eve/arch/x86/predef.hpp>
 #include <cstdint>
 #include <cstddef>
 
-// Register count
 #if defined(EVE_HW_X86)
 namespace eve
 {
   struct register_count
-
   {
     static constexpr std::size_t general = 2 * sizeof(void *);
     static constexpr std::size_t simd    = 2 * sizeof(void *);
   };
 }
+
+// X86 SIMD ABI
+#  if !defined(EVE_CURRENT_ABI)
+#    if EVE_HW_X86 >= EVE_AVX_VERSION
+#      define EVE_CURRENT_ABI ::eve::avx_
+#      include <immintrin.h>
+#    elif EVE_HW_X86 >= EVE_SSE2_VERSION
+#      define EVE_CURRENT_ABI ::eve::sse_
+#      include <nmmintrin.h>
+#    endif
+#  endif
 #endif
 
-// SSE ABI
-#include <eve/arch/x86/sse2/spec.hpp>
-#include <eve/arch/x86/sse3/spec.hpp>
-#include <eve/arch/x86/ssse3/spec.hpp>
-#include <eve/arch/x86/sse4_1/spec.hpp>
-#include <eve/arch/x86/sse4_2/spec.hpp>
+// TODO: AVX512 ABI
 
-// AVX ABI
-#include <eve/arch/x86/avx/spec.hpp>
-#include <eve/arch/x86/fma3/spec.hpp>
-#include <eve/arch/x86/xop/spec.hpp>
-#include <eve/arch/x86/fma4/spec.hpp>
-#include <eve/arch/x86/avx2/spec.hpp>
+// X86 SIMD API
+#if !defined(EVE_CURRENT_API)
+#  if EVE_HW_X86 == EVE_SSE2_VERSION
+#    define EVE_CURRENT_API ::eve::sse2_
+#  elif EVE_HW_X86 == EVE_SSE3_VERSION
+#    define EVE_CURRENT_API ::eve::sse3_
+#  elif EVE_HW_X86 == EVE_SSSE3_VERSION
+#    define EVE_CURRENT_API ::eve::ssse3_
+#  elif EVE_HW_X86 == EVE_SSE4_1_VERSION
+#    define EVE_CURRENT_API ::eve::sse4_1_
+#  elif EVE_HW_X86 == EVE_SSE4_2_VERSION
+#    define EVE_CURRENT_API ::eve::sse4_2_
+#  elif EVE_HW_X86 == EVE_AVX_VERSION
+#    define EVE_CURRENT_API ::eve::avx_
+#  elif EVE_HW_X86 == EVE_AVX2_VERSION
+#    define EVE_CURRENT_API ::eve::avx2_
+#  endif
+#endif
 
-// AVX512 ABI
-//#include <eve/arch/x86/avx512/spec.hpp>
+// TODO: AVX512 API
+
+// Additionnal ISA support
+#if defined(EVE_SUPPORTS_FMA3)
+#  include <immintrin.h>
+#endif
+
+#if defined(EVE_SUPPORTS_FMA4)
+#  if defined(EVE_COMP_IS_MSVC)
+#    include <intrin.h>
+#  else
+#    include <x86intrin.h>
+#    include <fma4intrin.h>
+#  endif
+#endif
+
+#if defined(EVE_SUPPORTS_XOP)
+#  if defined(EVE_COMP_IS_MSVC)
+#    include <intrin.h>
+#  else
+#    include <x86intrin.h>
+#    include <xopintrin.h>
+#  endif
+#endif
 
 #endif
