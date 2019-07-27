ed1ed762da6c19fcb82eec375be1af738ea31ff5
Joel Falcou <joel.falcou@lri.fr>
2019-07-27 10:35:40 +0200

    Simplify PPC arch headers

--- include/eve/arch/ppc/abi_of.hpp
+++ include/eve/arch/ppc/abi_of.hpp
@@ -12,13 +12,45 @@
 #define EVE_ARCH_PPC_ABI_OF_HPP_INCLUDED
 
 #include <eve/arch/ppc/predef.hpp>
+#include <eve/ext/abi_of.hpp>
+#include <type_traits>
 
+namespace eve
+{
+  struct ppc_;
+}
+
+namespace eve::ext
+{
 #if EVE_HW_POWERPC >= EVE_VMX_VERSION
-#  include <eve/arch/ppc/vmx/abi_of.hpp>
+  template<int N>
+  struct abi_of<float, N, std::enable_if_t<(N <= 4)>>
+  {
+    using type = ::eve::ppc_;
+  };
+
+  template<typename T, int N>
+  struct abi_of<T,
+                N,
+                std::enable_if_t<std::is_integral_v<T> && (sizeof(T) < 8) && (N <= 16 / sizeof(T))>>
+  {
+    using type = ::eve::ppc_;
+  };
 #endif
 
 #if EVE_HW_POWERPC >= EVE_VSX_VERSION
-#  include <eve/arch/ppc/vsx/abi_of.hpp>
+  template<int N>
+  struct abi_of<double, N, std::enable_if_t<(N <= 2)>>
+  {
+    using type = ::eve::ppc_;
+  };
+
+  template<typename T, int N>
+  struct abi_of<T, N, std::enable_if_t<std::is_integral_v<T> && (sizeof(T) == 8) && (N <= 2)>>
+  {
+    using type = ::eve::ppc_;
+  };
 #endif
+}
 
 #endif
