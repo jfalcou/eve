a03625fd0858722a50bcacb4e860e43eddd75c74
jtlap <jtlapreste@gmail.com>
2019-08-10 13:43:29 +0200

    bitwise_xor

--- include/eve/module/core/function/scalar/bitwise_or.hpp
+++ include/eve/module/core/function/scalar/bitwise_or.hpp
@@ -23,7 +23,9 @@ namespace eve::detail
   // -----------------------------------------------------------------------------------------------
   // Regular case
   template<typename T, typename U>
-  EVE_FORCEINLINE constexpr auto bitwise_or_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+  EVE_FORCEINLINE constexpr auto bitwise_or_(EVE_SUPPORTS(cpu_)
+                                            , T const &a
+                                            , U const &b) noexcept
   requires(T,  Vectorizable<T>, Vectorizable<U>)
   {
     if constexpr(sizeof(T) != sizeof(U))
--- test/unit/module/core/simd/bitwise_xor/bitwise_xor.hpp
+++ test/unit/module/core/simd/bitwise_xor/bitwise_xor.hpp
@@ -50,9 +50,36 @@ TTS_CASE_TPL("Check bitwise_xor behavior on wide + scalar",
   wide<Type, T> lhs([](auto i, auto c) { return i % 3; }),
     ref([](auto i, auto c) { return eve::bitwise_xor(Type(i % 3), Type(-1)); });
 
-  TTS_EQUAL(ref, eve::bitwise_xor(lhs, -1));
-  TTS_EQUAL(ref, lhs ^ -1);
+  TTS_EQUAL(ref, eve::bitwise_xor(lhs, Type(-1)));
+  TTS_EQUAL(ref, lhs ^ Type(-1));
+}
 
+TTS_CASE_TPL("Check bitwise_xor with mixed types",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>,
+             fixed<32>,
+             fixed<64>)
+{
+  using eve::wide;
+
+  {
+    wide<float, T> lhs([](auto i, auto c) { return c - i; });
+    wide<int32_t, T> rhs([](auto i, auto) { return i; });
+    wide<float, T>  ref([](auto i, auto c) { return eve::bitwise_xor(float(c - i), int32_t(i)); });
+
+    TTS_IEEE_EQUAL(ref, eve::bitwise_xor(lhs, rhs));
+    TTS_IEEE_EQUAL(ref, lhs | rhs);
+  }
+  {
+    wide<int32_t, T> lhs([](auto i, auto c) { return int32_t(i % 3); }),
+      ref([](auto i, auto c) { return eve::bitwise_xor(int32_t(i % 3), -3.322f); });
+
+    TTS_IEEE_EQUAL(ref, eve::bitwise_xor(lhs, -3.322f));
+    TTS_IEEE_EQUAL(ref, lhs ^ -3.322f);
+  }
 }
 
 #endif
