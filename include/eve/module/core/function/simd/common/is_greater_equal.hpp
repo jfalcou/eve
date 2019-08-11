e3ad4edd1fc44d4de0372abc8a6d9e7bbd811efc
jtlap <jtlapreste@gmail.com>
2019-08-11 19:18:23 +0200

    is_greater_equal

--- include/eve/module/core/function/simd/common/is_greater_equal.hpp
+++ include/eve/module/core/function/simd/common/is_greater_equal.hpp
@@ -27,41 +27,40 @@
 namespace eve::detail
 {
   template<typename T, typename U>
-  EVE_FORCEINLINE constexpr auto is_greater_equal_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+  EVE_FORCEINLINE constexpr auto is_greater_equal_(EVE_SUPPORTS(cpu_)
+                                                  , T const &a
+                                                  , U const &b) noexcept
   requires( as_logical_t<std::conditional_t<is_vectorized_v<T>,T,U>>,
             detail::Either<is_vectorized_v<T>, is_vectorized_v<U>>
           )
   {
-    if constexpr( !is_vectorized_v<U> )
+    using t_abi = abi_type_t<T>;
+    using u_abi = abi_type_t<U>;
+
+    if constexpr( is_emulated_v<t_abi> || is_emulated_v<u_abi> )
     {
-      return is_greater_equal(a, T{b});
+      return map( eve::is_greater_equal, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
     }
-    else if constexpr( !is_vectorized_v<T> )
+    else if constexpr( is_aggregated_v<t_abi> || is_aggregated_v<u_abi> )
     {
-      return is_greater_equal(U{a},b);
+      return aggregate( eve::is_greater_equal, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
     }
-    else
+    else if constexpr( is_vectorized_v<T> & is_vectorized_v<U> )
     {
-      if constexpr(std::is_same_v<T,U>)
-      {
-        if constexpr( is_aggregated_v<typename T::abi_type> )
-        {
-          return aggregate( eve::is_greater_equal, a, b);
-        }
-        else if constexpr( is_emulated_v<typename T::abi_type> )
-        {
-          return map( eve::is_greater_equal, a, b);
-        }
-        else
-        {
-          return is_greater(a, b) || is_equal(a, b);
-        }
-      }
+      if constexpr(std::is_same_v<T, U>)// & is_native_v<T>)
+        return is_greater_equal(a, b);
+//       else
+//         return is_greater(a, b) || is_equal(a, b);
       else
       {
-        static_assert( std::is_same_v<T,U>, "[eve::is_greater_equal] - Incompatible types.");
+        static_assert(std::is_same_v<T, U> 
+                     , "[eve::is_greater_equal] common - cannot test is_greater_equal for wide of different types");
         return {};
       }
+    }
+    else //if constexpr( is_vectorized_v<T> ^ is_vectorized_v<U> )
+    {
+      return eve::is_greater_equal(abi_cast<U>(a), abi_cast<T>(b) );
     }
   }
   
@@ -75,6 +74,16 @@ namespace eve::detail
           )
   {
     return bitwise_cast<logical<T>>( is_greater_equal(a.bits(),b.bits()) );
+  }
+}
+
+namespace eve
+{
+  template<typename T, typename U>
+  EVE_FORCEINLINE auto operator >= (T const &v0, U const &v1) noexcept
+  -> decltype( eve::is_greater_equal(v0,v1) )
+  {
+    return eve::is_greater_equal(v0, v1);
   }
 }
 
