0dbb858f92f7644b7f6252d63cef45cc1c69d687
jtlap <jtlapreste@gmail.com>
2019-08-13 07:57:19 +0200

    is_less

--- include/eve/module/core/function/simd/common/is_less.hpp
+++ include/eve/module/core/function/simd/common/is_less.hpp
@@ -26,49 +26,40 @@
 namespace eve::detail
 {
   template<typename T, typename U>
-  EVE_FORCEINLINE  auto is_less_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+  EVE_FORCEINLINE auto is_less_(EVE_SUPPORTS(cpu_)
+                                  , T const &a
+                                  , U const &b) noexcept
   requires( as_logical_t<std::conditional_t<is_vectorized_v<T>,T,U>>,
             detail::Either<is_vectorized_v<T>, is_vectorized_v<U>>
           )
   {
-    if constexpr( !is_vectorized_v<U> )
+    using t_abi = abi_type_t<T>;
+    using u_abi = abi_type_t<U>;
+
+    if constexpr( is_emulated_v<t_abi> || is_emulated_v<u_abi> )
     {
-      return is_less(a, T{b});
+      return map( eve::is_less, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
     }
-    else if constexpr( !is_vectorized_v<T> )
+    else if constexpr( is_aggregated_v<t_abi> || is_aggregated_v<u_abi> )
     {
-      return is_less(U{a},b);
+      return aggregate( eve::is_less, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
     }
-    else
+    else if constexpr( is_vectorized_v<T> & is_vectorized_v<U> )
     {
-      if constexpr(std::is_same_v<T,U>)
-      {
-        if constexpr( is_aggregated_v<typename T::abi_type> )
-        {
-          return aggregate( eve::is_less, a, b);
-        }
-        else if constexpr( is_emulated_v<typename T::abi_type> )
-        {
-          return map( eve::is_less, a, b);
-        }
-        else
-        {
-          static_assert( wrong<T,U>, "[eve::is_less] - Unsupported ABI.");
-          return {};
-        }
-      }
-      else
-      {
-        static_assert( std::is_same_v<T,U>, "[eve::is_less] - Incompatible types.");
-        return {};
-      }
+      static_assert(wrong<T, U>, "[eve::is_less] - no support for current simd api ");
+      return {};
+    }
+    else //if constexpr( is_vectorized_v<T> ^ is_vectorized_v<U> )
+    {
+      return eve::is_less(abi_cast<U>(a), abi_cast<T>(b) );
     }
   }
   
   template<typename T, typename U>
-  EVE_FORCEINLINE  auto is_less_( EVE_SUPPORTS(cpu_),
-                                  logical<T> const &a, logical<U> const &b
-                                ) noexcept
+  EVE_FORCEINLINE auto is_less_( EVE_SUPPORTS(cpu_),
+                                    logical<T> const &a
+                                  , logical<U> const &b
+                                  ) noexcept
   requires( logical<T>,
             Vectorized<T>, Vectorized<U>,
             EqualCardinal<T,U>
@@ -77,5 +68,17 @@ namespace eve::detail
     return bitwise_cast<logical<T>>( is_less(a.bits(),b.bits()) );
   }
 }
+
+
+namespace eve
+{
+  template<typename T, typename U>
+  EVE_FORCEINLINE auto operator <(T const &v0, U const &v1) noexcept
+  -> decltype( eve::is_less(v0,v1) )
+  {
+    return eve::is_less(v0, v1);
+  }
+}
+
 
 #endif
