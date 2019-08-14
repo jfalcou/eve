ca92216dc5a510709c3e15aa60ade80d558d5144
jtlap <jtlapreste@gmail.com>
2019-08-14 12:48:03 +0200

    is_not_less

--- include/eve/module/core/function/simd/x86/is_not_less.hpp
+++ include/eve/module/core/function/simd/x86/is_not_less.hpp
@@ -8,8 +8,8 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_X86_IS_NOT_GREATER_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_SIMD_X86_IS_NOT_GREATER_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_X86_IS_NOT_LESS_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_X86_IS_NOT_LESS_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/skeleton.hpp>
@@ -17,7 +17,7 @@
 #include <eve/detail/abi.hpp>
 #include <eve/forward.hpp>
 #include <eve/as_logical.hpp>
-#include <eve/function/is_less_equal.hpp>
+#include <eve/function/is_greater.hpp>
 #include <type_traits>
 
 namespace eve::detail
@@ -26,32 +26,32 @@ namespace eve::detail
   // 128 bits implementation
   template<typename T, typename N>
   EVE_FORCEINLINE auto
-  is_not_greater_(EVE_SUPPORTS(sse2_)
+  is_not_less_(EVE_SUPPORTS(sse2_)
                  , wide<T, N, sse_> const &v0
                  , wide<T, N, sse_> const &v1) noexcept
   {
     using t_t = wide<T, N, sse_>;
     using l_t = as_logical_t<t_t>;
     
-    if constexpr(std::is_same_v<T, float>)         return l_t(_mm_cmpngt_ps(v0, v1));
-    else if constexpr(std::is_same_v<T, double>)   return l_t(_mm_cmpngt_pd(v0, v1));
-    else /* if constexpr(std::is_integral_v<T>)*/  return is_less_equal(v0,v1);
+    if constexpr(std::is_same_v<T, float>)         return l_t(_mm_cmpnlt_ps(v0, v1));
+    else if constexpr(std::is_same_v<T, double>)   return l_t(_mm_cmpnlt_pd(v0, v1));
+    else /* if constexpr(std::is_integral_v<T>)*/  return is_greater_equal(v0,v1);
   }
   
   // -----------------------------------------------------------------------------------------------
   // 256 bits implementation
   template<typename T, typename N>
   EVE_FORCEINLINE auto
-  is_not_greater_(EVE_SUPPORTS(avx_)
+  is_not_less_(EVE_SUPPORTS(avx_)
                  , wide<T, N, avx_> const &v0
                  , wide<T, N, avx_> const &v1) noexcept
   {
     using t_t = wide<T, N ,avx_>;
     using l_t = as_logical_t<t_t>;
     
-    if constexpr(std::is_same_v<T, float>)         return l_t(_mm256_cmp_ps(v0, v1, _CMP_NGT_UQ));
-    else if constexpr(std::is_same_v<T, double>  ) return l_t(_mm256_cmp_pd(v0, v1, _CMP_NGT_UQ));
-    else /* if  constexpr(std::is_integral_v<T>)*/ return is_less_equal(v0,v1);
+    if constexpr(std::is_same_v<T, float>)         return l_t(_mm256_cmp_ps(v0, v1, _CMP_NLT_UQ));
+    else if constexpr(std::is_same_v<T, double>  ) return l_t(_mm256_cmp_pd(v0, v1, _CMP_NLT_UQ));
+    else /* if  constexpr(std::is_integral_v<T>)*/ return is_greater_equal(v0,v1);
     
   }
 }
