e2dd3549c2b100417275b880a22806d7384868e7
jtlap <jtlapreste@gmail.com>
2019-08-15 10:36:42 +0200

    min

--- include/eve/module/core/function/scalar/min.hpp
+++ include/eve/module/core/function/scalar/min.hpp
@@ -26,7 +26,7 @@ namespace eve::detail
   EVE_FORCEINLINE constexpr auto min_(EVE_SUPPORTS(cpu_)
                                      , T const &a0
                                      , T const &a1) noexcept
-  requires(T, Arithmetic<T>)
+  requires(T, Vectorizable<T>)
   {
      return (a1 < a0) ? a1 : a0;
   }
@@ -38,7 +38,7 @@ namespace eve::detail
                                      , pedantic_type const &
                                      , T const &a0
                                      , T const &a1) noexcept
-  requires(T, Arithmetic<T>)
+  requires(T, Vectorizable<T>)
   {
     return (a1 < a0) ? a1 : a0;
   }
@@ -50,11 +50,10 @@ namespace eve::detail
                                      , numeric_type const &
                                      , T const &a0
                                      , T const &a1) noexcept
-  requires(T, Arithmetic<T>)
+  requires(T, Vectorizable<T>)
   {
-    if constexpr(std::is_floating_point_v<T>)
-      if (is_nan(a0)) return a1;
-    return eve::min(a0, a1);
+    if constexpr(std::is_floating_point_v<T>) return (is_nan(a0)) ?  a1 :  eve::min(a0, a1);
+    else  return eve::min(a0, a1);
   }
 }
 
--- include/eve/module/core/function/simd/min.hpp
+++ include/eve/module/core/function/simd/min.hpp
@@ -14,34 +14,16 @@
 #include <eve/arch.hpp>
 #include <eve/module/core/function/simd/common/min.hpp>
 
- #if defined(EVE_HW_X86)
-#  if EVE_HW_X86 >= EVE_SSE2_VERSION
-#    include <eve/module/core/function/simd/x86/sse2/min.hpp>
-#  endif
-
-#  if EVE_HW_X86 >= EVE_SSE4_1_VERSION
-#    include <eve/module/core/function/simd/x86/sse4_1/min.hpp>
-#  endif
-
-#  if EVE_HW_X86 >= EVE_AVX_VERSION
-#    include <eve/module/core/function/simd/x86/avx/min.hpp>
-#  endif
-
-#  if EVE_HW_X86 >= EVE_AVX2_VERSION
-#    include <eve/module/core/function/simd/x86/avx2/min.hpp>
-#  endif
+#if defined(EVE_HW_X86)
+#  include <eve/module/core/function/simd/x86/min.hpp>
 #endif
 
 #if defined(EVE_HW_POWERPC)
-#  if EVE_HW_POWERPC >= EVE_VMX_VERSION
-#    include <eve/module/core/function/simd/ppc/vmx/min.hpp>
-#  endif
+#  include <eve/module/core/function/simd/ppc/vmx/min.hpp>
 #endif
 
 #if defined(EVE_HW_ARM)
-#  if EVE_HW_ARM >= EVE_NEON_VERSION
-#    include <eve/module/core/function/simd/arm/neon/min.hpp>
-#  endif
+#  include <eve/module/core/function/simd/arm/neon/min.hpp>
 #endif
 
 #endif
