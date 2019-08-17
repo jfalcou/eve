4e9f0e0f4c58cc15e7a0913447537063e5414a06
jtlap <jtlapreste@gmail.com>
2019-08-17 17:36:44 +0200

    store

--- include/eve/module/core/function/scalar/store.hpp
+++ include/eve/module/core/function/scalar/store.hpp
@@ -14,13 +14,17 @@
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/memory/aligned_ptr.hpp>
+#include <eve/concept/vectorizable.hpp>
 
 namespace eve::detail
 {
   // -----------------------------------------------------------------------------------------------
   // Regular case
   template<typename T>
-  EVE_FORCEINLINE void store_(EVE_SUPPORTS(cpu_), T value, T *ptr) noexcept
+  EVE_FORCEINLINE auto store_(EVE_SUPPORTS(cpu_)
+                             , T value
+                             , T *ptr) noexcept
+  requires(void, Vectorizable<T>)
   {
     *ptr = value;
   }
@@ -28,7 +32,10 @@ namespace eve::detail
   // -----------------------------------------------------------------------------------------------
   // Aligned case
   template<typename T, std::size_t N>
-  EVE_FORCEINLINE void store_(EVE_SUPPORTS(cpu_), T value, aligned_ptr<T, N> ptr) noexcept
+  EVE_FORCEINLINE auto store_(EVE_SUPPORTS(cpu_)
+                             , T value
+                             , aligned_ptr<T, N> ptr) noexcept
+  requires(void, Vectorizable<T>)
   {
     *ptr = value;
   }
