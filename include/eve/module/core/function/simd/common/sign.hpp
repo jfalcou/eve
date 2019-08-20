db39562683b8580a248e282d4470278e95787828
jtlap <jtlapreste@gmail.com>
2019-08-20 20:01:13 +0200

    common of sign need cpu_ for retargetting

--- include/eve/module/core/function/simd/common/sign.hpp
+++ include/eve/module/core/function/simd/common/sign.hpp
@@ -36,7 +36,7 @@
 namespace eve::detail
 {
   template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE auto sign_(EVE_SUPPORTS(simd_)
+  EVE_FORCEINLINE auto sign_(EVE_SUPPORTS(cpu_)
                             ,wide<T, N, ABI> const &a) noexcept
   {
     if constexpr(std::is_floating_point_v<T>)
