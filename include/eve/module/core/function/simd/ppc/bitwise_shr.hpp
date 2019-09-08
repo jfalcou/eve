758bb6f2d8af29b7ceea974fa1cf642d6445a2f8
Joel Falcou <joel.falcou@lri.fr>
2019-09-08 17:42:06 +0200

    Reorganize asserts for bitwise_shr

--- include/eve/module/core/function/simd/ppc/bitwise_shr.hpp
+++ include/eve/module/core/function/simd/ppc/bitwise_shr.hpp
@@ -14,34 +14,26 @@
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/detail/meta.hpp>
-#include <eve/detail/assert_utils.hpp>
+#include <eve/function/bitwise_cast.hpp>
 #include <eve/forward.hpp>
-#include <eve/assert.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
   template<typename T, typename N, typename I>
-  EVE_FORCEINLINE auto bitwise_shr_(EVE_SUPPORTS(vmx_),
-                                    wide<T, N, ppc_> const &v0,
-                                    wide<I, N, ppc_> const &v1) noexcept requires(wide<T, N, ppc_>,
-                                                                                  Integral<I>,
-                                                                                  Integral<T>)
+  EVE_FORCEINLINE auto bitwise_shr_ ( EVE_SUPPORTS(vmx_),
+                                      wide<T, N, ppc_> const &v0,
+                                      wide<I, N, ppc_> const &v1
+                                    ) noexcept
+  requires(wide<T, N, ppc_>, Integral<I>, Integral<T>)
   {
-    using t_t = wide<T, N, ppc_>;
-    EVE_ASSERT(detail::assert_good_shift<t_t>(v1),
-               "[eve::bitwise_shr ppc] - At least one of "
-                   << v1 << " elements is out of the range [0, " << sizeof(T) * 8 << "[.");
-
     using i_t = wide<as_integer_t<T, unsigned>, N>;
-    return t_t(vec_sr(v0.storage(), bitwise_cast<i_t>(v1).storage()));
+    return wide<T, N, ppc_>(vec_sr(v0.storage(), bitwise_cast<i_t>(v1).storage()));
   }
 
   template<typename T, typename N, typename I>
-  EVE_FORCEINLINE auto
-  bitwise_shr_(EVE_SUPPORTS(vmx_),
-               wide<T, N, ppc_> const &v0,
-               I v1) noexcept requires(wide<T, N, ppc_>, Integral<I>, Integral<T>)
+  EVE_FORCEINLINE auto bitwise_shr_(EVE_SUPPORTS(vmx_), wide<T, N, ppc_> const &v0, I v1) noexcept
+  requires(wide<T, N, ppc_>, Integral<I>, Integral<T>)
   {
     using i_t = wide<as_integer_t<T, unsigned>, N>;
     return eve::shr(v0, i_t(v1));
