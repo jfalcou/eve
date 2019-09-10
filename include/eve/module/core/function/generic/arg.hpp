9a12a2f02f59c58166b7ef7564dff0eb82b74eca
Joel Falcou <joel.falcou@lri.fr>
2019-09-10 23:29:57 +0200

    Reorganize assert for arg

--- include/eve/module/core/function/generic/arg.hpp
+++ include/eve/module/core/function/generic/arg.hpp
@@ -19,32 +19,30 @@
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/pi.hpp>
 #include <eve/constant/zero.hpp>
+#include <eve/platform.hpp>
 #include <eve/tags.hpp>
-#include <type_traits>
 
 namespace eve::detail
 {
   template<typename T>
   EVE_FORCEINLINE constexpr T arg_(EVE_SUPPORTS(cpu_), T const &a) noexcept
   {
-    static_assert(std::is_floating_point_v<value_type_t<T>>,
-                  "[eve::arg] - Function undefined for integral types");
-
     return if_else(is_negative(a), Pi(as(a)), eve::zero_);
   }
 
   template<typename T>
-  EVE_FORCEINLINE constexpr T arg_(EVE_SUPPORTS(cpu_), pedantic_type const &, T const &a) noexcept
+  EVE_FORCEINLINE constexpr T arg_(EVE_SUPPORTS(cpu_), pedantic_type const&, T const &a) noexcept
   {
-    static_assert(std::is_floating_point_v<value_type_t<T>>,
-                  "[eve::pedantic_(eve::arg)] - Function undefined for integral types");
-
     auto r = arg(a);
-#ifndef EVE_NO_NANS
-    return if_else(is_nan(a), eve::allbits_, r);
-#else
-    return r;
-#endif
+
+    if constexpr( platform::supports_nans )
+    {
+      return if_else(is_nan(a), eve::allbits_, r);
+    }
+    else
+    {
+      return r;
+    }
   }
 }
 
