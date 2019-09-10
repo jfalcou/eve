9115a8e263faa666f4d2a7f911245b355993a04c
jtlap <jtlapreste@gmail.com>
2019-09-10 13:16:41 +0200

    clang does not want return {]; after assert in Constant

--- include/eve/constant/constant.hpp
+++ include/eve/constant/constant.hpp
@@ -13,11 +13,11 @@
 
 #include <eve/detail/abi.hpp>
 #include <eve/detail/meta.hpp>
-#include <eve/function/bitwise_cast.hpp>
+#include <eve/function/bitwise_cast.hpp> 
 #include <eve/as.hpp>
 #include <type_traits>
 
-namespace eve
+namespace eve 
 {
   template<typename T, auto BitsPattern>
   EVE_FORCEINLINE auto Constant(as_<T> const & = {})
@@ -27,7 +27,15 @@ namespace eve
     if constexpr(std::is_integral_v<t_t>)
       return static_cast<T>(BitsPattern);
     else
-      return static_cast<T>(bitwise_cast<t_t>(BitsPattern));
+    {
+      if constexpr(sizeof(t_t) != sizeof(BitsPattern))
+      {
+        static_assert(sizeof(t_t) == sizeof(BitsPattern),
+                      "[eve::constant] floating_point case - BitsPattern has not the correct size");
+        return T();
+      }
+      else return static_cast<T>(bitwise_cast<t_t>(BitsPattern));
+    }    
   }
 }
 
