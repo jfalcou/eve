962ec1f8f112bd79511183660f0a5ac2c1ca01f2
jtlap <jtlapreste@gmail.com>
2019-09-14 11:41:42 +0200

    some docs modifications

--- test/doc/nearest.cpp
+++ test/doc/nearest.cpp
@@ -2,26 +2,19 @@
 #include <eve/wide.hpp>
 
 using wide_ft = eve::wide<float, eve::fixed<4>>;
-using wide_it = eve::wide<std::int16_t, eve::fixed<4>>;
 
 int main()
 {
-  wide_ft pf = {-1.0f, 2.0f, -3.0f, -32768.0f};
-  wide_it pi = {-1, 2, -3, -32768};
+  wide_ft pf = {-1.2f, 2.5f, 3.5f, 1.6f};
 
   std::cout << "---- simd" << '\n'
             << "<- pf =                            " << pf << '\n'
-            << "-> eve::nearest(pf) =                  " << eve::nearest(pf) << '\n'
-            << "<- pi =                            " << pi << '\n'
-            << "-> eve::nearest(pi) =                  " << eve::nearest(pi) << '\n';
+            << "-> eve::nearest(pf) =              " << eve::nearest(pf) << '\n';
 
-  float        xf = -32768.0f;
-  std::int16_t xi = -32768;
+  float        xf = -1.3f;
 
   std::cout << "---- scalar" << '\n'
             << "<- xf =                            " << xf << '\n'
-            << "-> eve::nearest(xf) =                  " << eve::nearest(xf) << '\n'
-            << "<- xi =                            " << xi << '\n'
-            << "-> eve::nearest(xi) =                  " << eve::nearest(xi) << '\n';
+            << "-> eve::nearest(xf) =              " << eve::nearest(xf) << '\n'; 
   return 0;
 }
