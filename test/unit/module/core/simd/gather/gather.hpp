8e88f5feb56c25a7caa13ac5124356826963163d
Joel Falcou <joel.falcou@lri.fr>
2019-10-05 19:26:26 +0200

    Use correct indexes in reference computation

--- test/unit/module/core/simd/gather/gather.hpp
+++ test/unit/module/core/simd/gather/gather.hpp
@@ -33,7 +33,7 @@ TTS_CASE_TPL( "Check gather behavior: int32 indexes, unaligned pointer",
   Type data[T::value];
   for(std::size_t i = 0;i<T::value;++i) data[i] = Type(1) + i;
 
-  wide<std::int32_t, T> maps([](auto i, auto c) { return i%3 ? 1+i : c-i-1; });
+  wide<std::int32_t, T> maps([](auto i, auto c) { return i%3 ? i : c-i-1; });
   wide<Type,T> ref( [&](auto i, auto) { return data[maps[i]]; } );
 
   TTS_EQUAL( ref, eve::gather(&data[0], maps) );
@@ -54,7 +54,7 @@ TTS_CASE_TPL( "Check gather behavior: int64 indexes, unaligned pointer",
   Type data[T::value];
   for(std::size_t i = 0;i<T::value;++i) data[i] = Type(1) + i;
 
-  wide<std::int64_t, T> maps([](auto i, auto c) { return i%3 ? 1+i : c-i-1; });
+  wide<std::int64_t, T> maps([](auto i, auto c) { return i%3 ? i : c-i-1; });
   wide<Type,T> ref( [&](auto i, auto) { return data[maps[i]]; } );
 
   TTS_EQUAL( ref, eve::gather(&data[0], maps) );
@@ -77,7 +77,7 @@ TTS_CASE_TPL( "Check gather behavior: int32 indexes, aligned pointer",
   alignas(alg) Type data[T::value];
   for(std::size_t i = 0;i<T::value;++i) data[i] = Type(1) + i;
 
-  wide<std::int32_t, T> maps([](auto i, auto c) { return i%3 ? 1+i : c-i-1; });
+  wide<std::int32_t, T> maps([](auto i, auto c) { return i%3 ? i : c-i-1; });
   wide<Type,T> ref( [&](auto i, auto) { return data[maps[i]]; } );
 
   TTS_EQUAL( ref, eve::gather( eve::as_aligned<alg>(&data[0]), maps) );
@@ -100,7 +100,7 @@ TTS_CASE_TPL( "Check gather behavior: int64 indexes, aligned pointer",
   alignas(alg) Type data[T::value];
   for(std::size_t i = 0;i<T::value;++i) data[i] = Type(1) + i;
 
-  wide<std::int64_t, T> maps([](auto i, auto c) { return i%3 ? 1+i : c-i-1; });
+  wide<std::int64_t, T> maps([](auto i, auto c) { return i%3 ? i : c-i-1; });
   wide<Type,T> ref( [&](auto i, auto) { return data[maps[i]]; } );
 
   TTS_EQUAL( ref, eve::gather( eve::as_aligned<alg>(&data[0]), maps) );
