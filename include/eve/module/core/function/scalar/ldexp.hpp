fe9302d33011b6e90e61b2be1ce1bcf5196e5727
jtlap <jtlapreste@gmail.com>
2019-10-06 18:39:18 +0200

    ldexp ok

--- include/eve/module/core/function/scalar/ldexp.hpp
+++ include/eve/module/core/function/scalar/ldexp.hpp
@@ -18,11 +18,17 @@
 #include <eve/function/wide_cast.hpp>
 #include <eve/function/bitwise_cast.hpp>
 #include <eve/function/bitwise_shl.hpp>
+#include <eve/function/rshl.hpp>
+#include <eve/function/trunc.hpp>
+#include <eve/function/is_flint.hpp>
+#include <eve/function/is_not_finite.hpp>
 #include <eve/constant/limitexponent.hpp>
 #include <eve/constant/minexponent.hpp> 
 #include <eve/constant/maxexponent.hpp>
 #include <eve/constant/nbmantissabits.hpp>
 #include <eve/constant/smallestposval.hpp>
+#include <eve/function/pedantic.hpp>
+#include <eve/concept/vectorizable.hpp>
 #include <eve/platform.hpp>
 #include <type_traits>
 
@@ -30,43 +36,46 @@ namespace eve::detail
 {
   // -----------------------------------------------------------------------------------------------
   // Regular case
-  template<typename T>
-  EVE_FORCEINLINE constexpr T ldexp_(EVE_SUPPORTS(cpu_)
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto ldexp_(EVE_SUPPORTS(cpu_)
                                     , T const &a0
                                     , U const &a1) noexcept
+  requires(T, Vectorizable<T>, Vectorizable<U>)
   {
     if constexpr(std::is_integral_v<T>)
     {
       if constexpr(std::is_integral_v<U>)
-        return (a1>0)?(a0<<a1):(a0>>a1);
+        return rshl(a0, a1); //(a1>0)?(a0<<a1):(a0>>a1);
       else
         EVE_ASSERT(false,
-               "[ eve::ldexp] - If argument 1 is integral argument 2 must be integral." );
+                   "[ eve::ldexp] - If argument 1 is integral argument 2 must be integral." );
     }
     else
     {
       if constexpr(std::is_floating_point_v<U>)
       {
-         BOOST_ASSERT_MSG(is_flint(a1)||is_invalid(a1), "argument 2 is not a flint nor is invalid");
-         return ldexp(a0, itrunc(a1));
+        using i_t =  as_integer_t<U, signed>; 
+        EVE_ASSERT(is_flint(a1)||is_not_finite(a1), "argument 2 is not a flint nor is not_finite");
+        return ldexp(a0, i_t(trunc(a1))); // TODO itrunc
       }
       else 
       {
-        using i_t = as_integer_t<T>;
+        using i_t = detail::as_integer_t<T>;
         i_t ik =  a1+Maxexponent<T>();
         ik = bitwise_shl(ik, Nbmantissabits<T>());
-        return a0*bitwise_cast<T>(ik);
+        return a0*bitwise_cast(ik, as(T()));
       }
     }
   }
   
   // -----------------------------------------------------------------------------------------------
   // Pedantic case
-  template<typename T>
-  EVE_FORCEINLINE constexpr T ldexp_(EVE_SUPPORTS(cpu_)
-                                    , pedantic_ const & 
+  template<typename T, typename U>
+  EVE_FORCEINLINE constexpr auto ldexp_(EVE_SUPPORTS(cpu_)
+                                    , pedantic_type const & 
                                     , T const &a0
                                     , U const &a1) noexcept
+  requires(T, Vectorizable<T>, Vectorizable<U>)
   {
     if constexpr(std::is_integral_v<T>)
     {
@@ -76,14 +85,14 @@ namespace eve::detail
     {
       if constexpr(std::is_floating_point_v<U>)
       {
-         return ldexp[pedantic_](a0, itrunc(a1));
+        using i_t =  as_integer_t<U, signed>; 
+        return pedantic_(ldexp)(a0, i_t(trunc(a1))); //TODO itrunc(a1)
       }
       else
       {
         using i_t = as_integer_t<T>;
         i_t e =  static_cast<i_t>(a1);
         T f = One<T>();
-#ifndef BOOST_SIMD_NO_DENORMALS
         if constexpr(platform::supports_denormals)
         {
           if (e < Minexponent<T>())   //(BOOST_UNLIKELY
@@ -92,14 +101,14 @@ namespace eve::detail
             f = Smallestposval<T>();
           }
         }
-#endif
         i_t b = (e == Limitexponent<T>());
         f += static_cast<T>(b);
         e -= b;
         e += Maxexponent<T>();
         e = bitwise_shl(e, Nbmantissabits<T>());
-        return a0*bitwise_cast<T>(e)*f;
+        return a0*bitwise_cast(e, as(T()))*f;
       }
+    }
   }
 }
 
--- include/eve/module/core/function/simd/common/ldexp.hpp
+++ include/eve/module/core/function/simd/common/ldexp.hpp
@@ -1,11 +1,11 @@
 //==================================================================================================
 /**
-  EVE - Expressive Vector Engine
-  Copyright 2019 Jean-Thierry LAPRESTE
-  Copyright 2019 Joel FALCOU
-
-  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-  SPDX-License-Identifier: MIT
+   EVE - Expressive Vector Engine
+   Copyright 2019 Jean-Thierry LAPRESTE
+   Copyright 2019 Joel FALCOU
+   
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_LDEXP_HPP_INCLUDED
@@ -15,7 +15,8 @@
 #include <eve/detail/skeleton.hpp>
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/assert_utils.hpp>
+#include <eve/detail/assert_utils.hpp>
+#include <eve/function/all.hpp>
 #include <eve/function/bitwise_cast.hpp>
 #include <eve/function/bitwise_shl.hpp>
 #include <eve/function/dec.hpp>
@@ -23,7 +24,8 @@
 #include <eve/function/is_equal.hpp>
 #include <eve/function/is_less.hpp>
 #include <eve/function/inc.hpp>
-#include <eve/function/multiplies.hpp>
+#include <eve/function/mul.hpp>
+#include <eve/function/rshl.hpp>
 #include <eve/function/sub.hpp>
 #include <eve/function/wide_cast.hpp>
 #include <eve/constant/limitexponent.hpp>
@@ -31,278 +33,210 @@
 #include <eve/constant/maxexponent.hpp>
 #include <eve/constant/nbmantissabits.hpp>
 #include <eve/constant/smallestposval.hpp>
-#include <eve/tags.hpp>
+#include <eve/function/pedantic.hpp>
 #include <eve/forward.hpp>
+#include <eve/detail/meta.hpp>
 #include <type_traits>
 
 namespace eve::detail
 {
+
   // -----------------------------------------------------------------------------------------------
-  // Basic
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            wide<T, N, ABI> const &v0,
-                            wide<U, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
-                                                           Floating_point<T>, Integral<U>)
+  // regular case
+  template<typename T, typename U>
+  EVE_FORCEINLINE  auto ldexp_(EVE_SUPPORTS(cpu_)
+                            , T const &a
+                            , U const &b) noexcept
+  requires( T, Vectorized<T>)
   {
-    if constexpr(std::is_integral<U>)
+    using t_abi = abi_type_t<T>;
+
+    if constexpr(!std::is_floating_point_v<value_type_t<T>>)
     {
-      if constexpr(std::is_floating_point_v<T>())
+      static_assert(std::is_floating_point_v<value_type_t<T>>, "first parameter must be floating point");
+      return{}; 
+    }
+    else if constexpr( is_vectorizable_v<U>) // U is scalar
+    {
+      if constexpr( is_emulated_v<t_abi>)
       {
-        using t_t = wide<T, N, ABI>;
-        using i_t = as_integer_t<T>;
-        auto ik =  v1+Maxexponent(as(T));
-        ik = shift_left(ik, Nbmantissabits<T>());
-        return a0*bitwise_cast<t_t>(ik);
+        return map( eve::ldexp, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else if constexpr( is_aggregated_v<t_abi>)
+      {
+        return aggregate( eve::ldexp, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
       }
       else
       {
-        return rshl(a0, a1);
-      }
-    }
-    else
-    {
-      using wi_t = as_integer_t<wide<U, N, ABI>>;
-      EVE_ASSERT_MSG(assert_all(is_flint(v1)), "[ ldexp ] : second parameter is not a flint");
-      return ldexp(a0, wide_cast<wi_t>(v1));
-    }
-  }
-  
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                              wide<T, N, ABI> const &v0,
-                              U const &a1) noexcept requires(wide<T, N, ABI>,
-                                                             Floating_point<T>, Integral<U>)
-  {
-    if constexpr(std::is_integral<U>)
-    {
-      if constexpr(std::is_floating_point_v<T>())
-      {
-        using i_t = as_integer_t<T>;
-        auto ik =  a1+Maxexponent(as(T));
-        ik = shift_left(ik, Nbmantissabits<T>());
-        return a0*bitwise_cast<T>(ik);   
-      }
-      else
-      {
-        return rshl(a0, a1);
-      }
-    }
-    else
-    {
-      using i_t = as_integer_t<U>;
-      EVE_ASSERT_MSG(assert_all(is_flint(a1)), "[ ldexp ] : second parameter is not a flint");
-      return ldexp(a0, static_cast<i_t>(a1));
-    }
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Support for mixed type with auto-splat
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            wide<T, N, ABI> const &v0,
-                            U const &              v1) noexcept requires(wide<T, N, ABI>,
-                                                           detail::Convertible<U, T>)
-  {
-    return eve::ldexp(v0, wide<T, N, ABI>(static_cast<T>(v1)));
-  }
-
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            U const &              v0,
-                            wide<T, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
-                                                                         detail::Convertible<U, T>)
-  {
-    return eve::ldexp(wide<T, N, ABI>(static_cast<T>(v0)), v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Aggregation
-  template<typename T, typename N>
-  EVE_FORCEINLINE wide<T, N, aggregated_> ldexp_(EVE_SUPPORTS(simd_),
-                                               wide<T, N, aggregated_> const &v0,
-                                               wide<T, N, aggregated_> const &v1) noexcept
-  {
-    return aggregate(eve::ldexp, v0, v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Emulation with auto-splat inside map for performance purpose
-  template<typename T, typename N>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            wide<T, N, emulated_> const &v0,
-                            wide<T, N, emulated_> const &v1) noexcept
-  {
-    return map(eve::ldexp, v0, v1);
-  }
-
-  template<typename T, typename N, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            wide<T, N, emulated_> const &v0,
-                            U const &v1) noexcept requires(wide<T, N, emulated_>,
-                                                           detail::Convertible<U, T>)
-  {
-    return map(eve::ldexp, v0, static_cast<T>(v1));
-  }
-
-  template<typename T, typename N, typename U>
-  EVE_FORCEINLINE auto
-  ldexp_(EVE_SUPPORTS(simd_),
-       U const &                    v0,
-       wide<T, N, emulated_> const &v1) noexcept requires(wide<T, N, emulated_>,
-                                                          detail::Convertible<U, T>)
-  {
-    return map(eve::ldexp, static_cast<T>(v0), v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Pedantic
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                              pedantic_ const &, 
-                              wide<T, N, ABI> const &v0,
-                              wide<U, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
-                                                                           Floating_point<T>, Integral<U>)
-  {
-    if constexpr(std::is_integral<U>)
-    {
-      if constexpr(std::is_floating_point_v<T>())
-      {
-        using t_t = wide<T, N, ABI>;
-        using i_t = as_integer_t<t_t>;
-        i_t e = v1;  // ceci devrait être widecast<i_t>(a1); 
-        t_t f = One<t_t>();
-#ifndef EVE_NO_DENORMALS
-        auto denormal =  is_less(e, Minexponent<t_t>());
-        e = sub[denormal]( e, Minexponent<t_t>());
-        f = if_else(denormal, Smallestposval<t_t>(), One<t_t>());
-#endif
-        auto test = is_equal(e, Limitexponent<t_t>());
-        f = inc[test](f);
-        e = dec[test](e);
-        e += Maxexponent<t_t>();
-        e = bitwise_shl(e, Nbmantissabits<T>());
-        return a0*bitwise_cast<t_t>(e)*f;
-      }
-      else
-      {
-        return rshl(a0, a1);
-      }
-    }
-    else
-    {
-      using i_t = as_integer_t<U>>;
-      EVE_ASSERT_MSG(assert_all(is_flint(v1)), "[ ldexp ] : second parameter is not a flint");
-      return ldexp[pedantic_](a0, wide_cast<wi_t>(v1, as<i_t>()));
-    }
-  }
-  
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                              pedantic_ const &, 
-                              wide<T, N, ABI> const &v0,
-                              U const &a1) noexcept requires(wide<T, N, ABI>,
-                                                             Floating_point<T>, Integral<U>)
-  {
-    if constexpr(std::is_integral<U>)
-    {
-      if constexpr(std::is_floating_point_v<T>())
-      {
-        using t_t = wide<T, N, ABI>;
-        using i_t = as_integer_t<T>;
-        i_t e = a1;
-        t_t f = One<t_t>();
-#ifndef BOOST_SIMD_NO_DENORMALS
-        auto denormal =  is_less(e, Minexponent<T>());
-        e = sub[denormal](e, Minexponent<T>());
-        f = if_else(denormal, Smallestposval<t_t>(), One<t_t>());
-#endif
-        if (is_equal(e, Limitexponent<T>()))
+        if constexpr(std::is_integral_v<U>)
         {
-          f+= One<t_t>();
-          e-= One<i_t>();
+          using t_t = value_type_t<T>;
+          using i_t = detail::as_integer_t<t_t, signed>; 
+          i_t ik =  b+Maxexponent<t_t>();
+          ik = shl(ik, Nbmantissabits<T>());
+          return a*bitwise_cast(ik, as<t_t>());
+        } 
+        else  // U is floating point
+        {
+            using t_t = value_type_t<T>;
+          EVE_ASSERT(is_flint(b)||is_not_finite(b), "argument 2 is not a flint or is not finite");
+          using i_t =  detail::as_integer_t<t_t, signed>; 
+          return ldexp(a, i_t(trunc(b)));
         }
-        e += Maxexponent<T>();
-        e = shift_left(e, Nbmantissabits<T>());
-        return a0*(bitwise_cast<T>(e)*f);
+      }
+    }
+    else // constexpr( is_vectorized_v<U>)
+    {
+      using u_abi = abi_type_t<U>;
+      if constexpr (U::cardinal_type::value != T::cardinal_type::value)
+      {
+        static_assert(U::cardinal_type::value != T::cardinal_type::value, "[eve::ldexp] - no support for current simd api");
+      }
+      else if constexpr( is_emulated_v<t_abi> || is_emulated_v<u_abi> )
+      {
+        return map( eve::ldexp, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else if constexpr( is_aggregated_v<t_abi> || is_aggregated_v<u_abi> )
+      {
+        return aggregate( eve::ldexp, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else 
+      {
+        if constexpr(std::is_integral_v<value_type_t<U>>)
+        {
+          using t_t = value_type_t<T>; 
+          using i_t = as_integer_t<t_t, signed>;
+          auto ik =  wide_cast(b, as<i_t>())+Maxexponent<t_t>();
+          ik = shl(ik, Nbmantissabits<t_t>());
+          return a*bitwise_cast(ik, as<T>());
+        }
+        else // U is vector of floating
+        {
+          using t_t = value_type_t<T>; 
+          using i_t = as_integer_t<t_t, signed>;
+          EVE_ASSERT(is_flint(b)||is_not_finite(b), "argument 2 is not a flint or is not finite");
+          return ldexp(a, wide_cast(trunc(b), as<i_t>())); 
+        }
+      }
+    }
+  }
+  
+  // -----------------------------------------------------------------------------------------------
+  // pedantic case
+  template<typename T, typename U>
+  EVE_FORCEINLINE  auto ldexp_(EVE_SUPPORTS(cpu_)
+                            , pedantic_type const & pdt 
+                            , T const &a
+                            , U const &b) noexcept
+  requires( T, Vectorized<T>)
+  {
+    using t_abi = abi_type_t<T>;
+
+    if constexpr( is_vectorizable_v<U>) // U is scalar
+    {
+      if constexpr( is_emulated_v<t_abi>)
+      {
+        return map( eve::ldexp, pdt, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else if constexpr( is_aggregated_v<t_abi>)
+      {
+        return aggregate( eve::ldexp, pdt, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
       }
       else
       {
-        return rshl(a0, a1);
+        if constexpr(std::is_integral_v<U>)
+        {
+          if constexpr(std::is_floating_point_v<value_type_t<T>>())
+          {
+            using t_t = value_type_t<T>; 
+            using i_t = detail::as_integer_t<t_t, signed>; 
+            i_t ik =  b+Maxexponent<t_t>();
+            i_t e = b;  
+            auto f = One<T>();
+            if constexpr( eve::platform::supports_denormals)
+            {
+              auto denormal =  is_less(e, Minexponent<t_t>());
+              e = sub[denormal]( e, Minexponent<t_t>());
+              f = if_else(denormal, Smallestposval<t_t>(), One<t_t>());
+            }
+            auto test = is_equal(e, Limitexponent<t_t>());
+            f = inc[test](f);
+            e = dec[test](e);
+            e += Maxexponent<t_t>();
+            e = bitwise_shl(e, Nbmantissabits<T>());
+            return b*bitwise_cast(e, as(t_t()))*f;
+          }
+          else
+          {
+            return rshl(a, b);
+          }
+        } 
+        else  // U is floating point
+        {
+          EVE_ASSERT(is_flint(b)||is_not_finite(b), "argument 2 is not a flint or is not finite");
+          using i_t =  detail::as_integer_t<U, signed>; 
+          return pedantic_(ldexp)(a, wide_cast(trunc(b), as<i_t>())); 
+        }
       }
     }
-    else
+    else // constexpr( is_vectorized_v<U>)
     {
-      using i_t = as_integer_t<U>;
-      EVE_ASSERT_MSG(assert_all(is_flint(v1)), "[ ldexp ] : second parameter is not a flint");
-      return ldexp[pedantic_](a0, wide_cast(v1, as<i_t>()));
+      using u_abi = abi_type_t<U>;
+      if constexpr( is_emulated_v<t_abi> || is_emulated_v<u_abi> )
+      {
+        return map( eve::ldexp, pdt, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else if constexpr( is_aggregated_v<t_abi> || is_aggregated_v<u_abi> )
+      {
+        return aggregate( eve::ldexp, pdt, abi_cast<value_type_t<U>>(a), abi_cast<value_type_t<T>>(b) );
+      }
+      else if (U::cardinal_type::value != T::cardinal_type::value)
+      {      
+        static_assert(wrong<T, U>, "[eve::ldexp] - no support for current simd api");
+        return {};     
+      }
+      else 
+      {
+        if constexpr(std::is_integral_v<value_type_t<U>>)
+        {
+          if constexpr(std::is_floating_point_v<T>())
+          {
+            using t_t = value_type_t<T>; 
+            using i_t = as_integer_t<t_t, signed>;
+            auto e = wide_cast(b, as<i_t>()); 
+            auto f = One<T>();
+            if constexpr( eve::platform::supports_denormals)
+            {
+              auto denormal =  is_less(e, Minexponent<t_t>());
+              e = sub[denormal]( e, Minexponent<t_t>());
+              f = if_else(denormal, Smallestposval<t_t>(), One<t_t>());
+            }
+            auto test = is_equal(e, Limitexponent<t_t>());
+            f = inc[test](f);
+            e = dec[test](e);
+            e += Maxexponent<t_t>();
+            e = bitwise_shl(e, Nbmantissabits<t_t>());
+            return b*bitwise_cast(e, as(t_t()))*f;
+            
+            auto ik =  wide_cast(b, as<i_t>())+Maxexponent<t_t>();
+            ik = shl(ik, Nbmantissabits<t_t>());
+            return b*bitwise_cast(ik, as<T>());
+          }
+          else
+          {
+            return rshl(a, b);
+          }
+        }
+        else // U is vector of floating
+        {
+          using t_t = value_type_t<T>; 
+          using i_t = as_integer_t<t_t, signed>;
+          EVE_ASSERT(is_flint(b)||is_not_finite(b), "argument 2 is not a flint or is not finite");
+          return pedantic_(ldexp)(a, wide_cast(trunc(b), as<i_t>())); 
+        }
+      }
     }
   }
-   
-  // -----------------------------------------------------------------------------------------------
-  // Support for mixed type with auto-splat
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                              pedantic_ const &, 
-                              wide<T, N, ABI> const &v0,
-                              U const &              v1) noexcept requires(wide<T, N, ABI>,
-                                                                           detail::Convertible<U, T>)
-  {
-    return eve::ldexp(v0, wide<T, N, ABI>(static_cast<T>(v1)));
-  }
-
-  template<typename T, typename N, typename ABI, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            pedantic_ const &, 
-                            U const &              v0,
-                            wide<T, N, ABI> const &v1) noexcept requires(wide<T, N, ABI>,
-                                                                         detail::Convertible<U, T>)
-  {
-    return eve::ldexp(pedantic_, wide<T, N, ABI>(static_cast<T>(v0)), v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Aggregation
-  template<typename T, typename N>
-  EVE_FORCEINLINE wide<T, N, aggregated_> ldexp_(EVE_SUPPORTS(simd_),
-                                               wide<T, N, aggregated_> const &v0,
-                                               wide<T, N, aggregated_> const &v1) noexcept
-  {
-    return aggregate(eve::ldexp, pedantic_, v0, v1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // Emulation with auto-splat inside map for performance purpose
-  template<typename T, typename N>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            pedantic_ const &, 
-                            wide<T, N, emulated_> const &v0,
-                            wide<T, N, emulated_> const &v1) noexcept
-  {
-    return map(eve::ldexp, pedantic_, v0, v1);
-  }
-
-  template<typename T, typename N, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            pedantic_ const &, 
-                            wide<T, N, emulated_> const &v0,
-                            U const &v1) noexcept requires(wide<T, N, emulated_>,
-                                                           detail::Convertible<U, T>)
-  {
-    return map(eve::ldexp, pedantic_, v0, static_cast<T>(v1));
-  }
-
-  template<typename T, typename N, typename U>
-  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(simd_),
-                            pedantic_ const &, 
-       U const &                    v0,
-       wide<T, N, emulated_> const &v1) noexcept requires(wide<T, N, emulated_>,
-                                                          detail::Convertible<U, T>)
-  {
-    return map(eve::ldexp, pedantic_, static_cast<T>(v0), v1);
-  }
-
-  
 }
 
 
