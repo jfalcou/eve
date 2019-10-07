90674c60821c7d463e7378d2bcc2b48e6431b538
jtlap <jtlapreste@gmail.com>
2019-10-07 19:00:28 +0200

    exponentbits first pass

--- include/eve/function/definition/exponentbits.hpp
+++ include/eve/function/definition/exponentbits.hpp
@@ -12,9 +12,33 @@
 #define EVE_FUNCTION_DEFINITION_EXPONENTBITS_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/function/pedantic.hpp>
+#include <type_traits>
 
 namespace eve
 {
+  namespace tag { struct exponentbits_; }
+
+  namespace detail
+  {
+    template<typename T>
+    EVE_FORCEINLINE void check(EVE_SUPPORTS(eve::tag::exponentbits_), T const&)
+    {
+      static_assert ( std::is_floating_point_v<value_type_t<T>>,
+                      "[eve::exponentbits] - No support for integral types"
+                    );
+    }
+
+    template<typename T>
+    EVE_FORCEINLINE void check(EVE_SUPPORTS(eve::tag::exponentbits_), pedantic_type const&, T const&)
+    {
+      static_assert ( std::is_floating_point_v<value_type_t<T>>,
+                      "[eve::pedantic_(eve::exponentbits)] - No support for integral types"
+                    );
+    }
+  }
+
   EVE_MAKE_CALLABLE(exponentbits_, exponentbits);
 }
 
