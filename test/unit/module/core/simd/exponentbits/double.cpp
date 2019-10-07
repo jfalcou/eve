8f4efe0c6d58ccb5c0ec11b761e70c1a764c306a
jtlap <jtlapreste@gmail.com>
2019-10-07 19:00:28 +0200

    exponentbits first pass

--- test/unit/module/core/simd/exponentbits/double.cpp
+++ test/unit/module/core/simd/exponentbits/double.cpp
@@ -1,17 +1,12 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
-#define EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
 
-#include <eve/function/definition/exponentbits.hpp>
-#include <eve/module/core/function/generic/exponentbits.hpp>
-
-#endif
+using Type = double;
+#include "exponentbits.hpp"
