1090dc1b57f827a2647251af01b3bf9cc599878c
jtlap <jtlapreste@gmail.com>
2019-10-08 18:01:11 +0200

    mantissa

--- test/unit/module/core/scalar/mantissa/mantissa.hpp
+++ test/unit/module/core/scalar/mantissa/mantissa.hpp
@@ -14,28 +14,32 @@
 #include <eve/function/scalar/mantissa.hpp>
 #include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
 #include <eve/constant/minf.hpp>
 #include <eve/constant/inf.hpp>
+#include <eve/constant/nan.hpp>
 #include <eve/constant/zero.hpp>
 #include <eve/constant/one.hpp>
 #include <eve/constant/mone.hpp> 
 #include <type_traits>
 
-TTS_CASE("Check mantissa return type") { TTS_EXPR_IS(eve::mantissa(Type(0)), eve::detail::as_integer_t<Type>); }
+TTS_CASE("Check mantissa return type") { TTS_EXPR_IS(eve::mantissa(Type(0)), Type); }
 
 TTS_CASE ("checking  mantissa behaviour")
 {
-  // specific values tests
-  TTS_EQUAL(eve::mantissa(eve::Minf<Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::mantissa(eve::Inf <Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::mantissa(eve::Nan <Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::mantissa(eve::Mone<Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::mantissa(eve::One<Type>()),  eve::Zero<Type>());
+  if constexpr(eve::platform::supports_invalids)
+  {
+    TTS_EQUAL(eve::mantissa(eve::Inf<Type>()), eve::Inf<Type>());
+    TTS_EQUAL(eve::mantissa(eve::Minf<Type>()), eve::Minf<Type>());
+    TTS_IEEE_EQUAL(eve::mantissa(eve::Nan<Type>()), eve::Nan<Type>());
+  }
+  TTS_EQUAL(eve::mantissa(eve::Mone<Type>()), eve::Mone<Type>());
+  TTS_EQUAL(eve::mantissa(eve::One<Type>()), eve::One<Type>());
   TTS_EQUAL(eve::mantissa(eve::Zero<Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::mantissa(Type(2)),  eve::One<Type>() );
-  TTS_EQUAL(eve::mantissa(Type(1.5)),        eve::Zero<Type>() );
-  TTS_EQUAL(eve::mantissa(Type(4.5)),        Type(2)); 
+  TTS_EQUAL(eve::mantissa(Type(2)), eve::One<Type>());
+  TTS_EQUAL(eve::mantissa(Type(1.5)), Type(1.5));
+  TTS_EQUAL(eve::mantissa(Type(2.5)), Type(1.25));
 }
 
 #endif
