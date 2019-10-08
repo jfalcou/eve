f6d8d1b5076ee9bb8f619009d3bcaa5fc5eb5fe0
Joel Falcou <joel.falcou@lri.fr>
2019-10-08 19:39:46 +0200

    Fix binarize

--- test/unit/module/core/binarize/regular/binarize.hpp
+++ test/unit/module/core/binarize/regular/binarize.hpp
@@ -8,41 +8,40 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BINARIZE_HPP
-#define BINARIZE_HPP
-
 #include <eve/function/binarize.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
+#include <eve/function/is_greater.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/zero.hpp>
-#include <eve/function/is_greater.hpp>
 #include <eve/logical.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check binarize return type") {
-  TTS_EXPR_IS(eve::binarize(eve::logical<Type>()), Type);
-  TTS_EXPR_IS(eve::binarize(eve::logical<Type>(), int()), Type);
+TTS_CASE("Check binarize return type")
+{
+  using eve::binarize;
+  TTS_EXPR_IS(binarize(eve::logical<Type>()), (Type));
+  TTS_EXPR_IS(binarize(eve::logical<Type>(), int()), (Type));
 }
 
 TTS_CASE("Check eve::binarize on logicals behavior")
 {
-  TTS_EQUAL(eve::binarize(eve::is_greater(Type{1}, Type(2))), Type(0));
-  TTS_EQUAL(eve::binarize(eve::is_greater(Type{2}, Type(1))), Type(1));
+  using eve::binarize;
+  using eve::is_greater;
+  TTS_EQUAL(binarize(is_greater(Type{1} , Type(2))), Type(0));
+  TTS_EQUAL(binarize(is_greater(Type{2} , Type(1))), Type(1));
 
-  if constexpr(std::is_signed_v<Type>) {
-    TTS_EQUAL(eve::binarize(eve::is_greater(Type{2},  Type(1)), Type(-2)), Type(-2));
-    TTS_EQUAL(eve::binarize(eve::is_greater(Type{0}, Type(1)), Type(-2)), Type(0));
-    TTS_EQUAL(eve::binarize(eve::is_greater(Type{1}, Type(2)), eve::mone_), Type(0));
-    TTS_EQUAL(eve::binarize(eve::is_greater(Type{2}, Type(1)), eve::mone_), Type(-1));
+  if constexpr(std::is_signed_v<Value>)
+  {
+    TTS_EQUAL(binarize(is_greater(Type{2}, Type(1)), Value(-2)), Type(-2));
+    TTS_EQUAL(binarize(is_greater(Type{0}, Type(1)), Value(-2)), Type(0));
+    TTS_EQUAL(binarize(is_greater(Type{1}, Type(2)), eve::mone_), Type(0));
+    TTS_EQUAL(binarize(is_greater(Type{2}, Type(1)), eve::mone_), Type(-1));
   }
-  TTS_EQUAL(eve::binarize(eve::is_greater(Type{2}, Type(1)), Type(2)), Type(2));
-  TTS_EQUAL(eve::binarize(eve::is_greater(Type{0}, Type(1)), Type(2)), Type(0));
-  TTS_EQUAL(eve::binarize(eve::is_greater(Type{0}, Type(1)), eve::allbits_), Type(0));
-  TTS_IEEE_EQUAL(eve::binarize(eve::is_greater(Type{2}, Type(1)), eve::allbits_), eve::Allbits<Type>());
+
+  TTS_EQUAL(binarize(is_greater(Type{2}, Type(1)), Value(2)), Type(2));
+  TTS_EQUAL(binarize(is_greater(Type{0}, Type(1)), Value(2)), Type(0));
+  TTS_EQUAL(binarize(is_greater(Type{0}, Type(1)), eve::allbits_), Type(0));
+  TTS_IEEE_EQUAL(binarize(is_greater(Type{2}, Type(1)), eve::allbits_), (eve::Allbits<Type>()));
 }
-
-
-#endif
