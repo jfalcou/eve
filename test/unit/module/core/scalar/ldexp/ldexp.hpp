11c510777c5d3eda7c720038435db95090fd62e1
jtlap <jtlapreste@gmail.com>
2019-10-08 10:48:13 +0200

    doc and tests for ldexp

--- test/unit/module/core/scalar/ldexp/ldexp.hpp
+++ test/unit/module/core/scalar/ldexp/ldexp.hpp
@@ -10,9 +10,50 @@
 #ifndef LDEXP_HPP
 #define LDEXP_HPP
 
-#include <eve/function/scalar/ldexp.hpp>
+#include <eve/function/ldexp.hpp>
 #include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/smallestposval.hpp>
+#include <eve/constant/mindenormal.hpp>
+#include <eve/function/is_positive.hpp>
+#include <eve/function/is_negative.hpp>
+#include <eve/platform.hpp>
+
+TTS_CASE("Check eve::ldexp behavior")
+{
+  TTS_EQUAL(eve::ldexp(Type{0}, 0), Type{0});
+  TTS_EQUAL(eve::ldexp(Type{0}, 1), Type{0});
+  TTS_EQUAL(eve::ldexp(Type{1}, 0), Type{1});
+  TTS_EQUAL(eve::ldexp(Type{1}, 1), Type{2});
+  TTS_EQUAL(eve::ldexp(Type{1}, 2), Type{4});
+  TTS_EQUAL(eve::ldexp(Type{3}, 4), Type{48});
+  if constexpr(std::is_floating_point_v<Type>)
+  {
+    TTS_EQUAL(eve::ldexp(-Type{0}, 1), Type{0});
+    TTS_EXPECT(eve::is_negative(eve::ldexp(-Type{0}, 1)));
+    TTS_EXPECT(eve::is_positive(eve::ldexp(Type{0}, 1)));     
+    if constexpr(eve::platform::supports_infinites)
+    {
+      TTS_EQUAL(eve::ldexp(eve::Minf<Type>(), 1), eve::Minf<Type>());
+      TTS_EQUAL(eve::ldexp(eve::Inf<Type>(),  1), eve::Inf<Type>());
+    }
+    if constexpr(eve::platform::supports_invalids)
+    {
+      TTS_IEEE_EQUAL(eve::ldexp(eve::Nan<Type>(),  1), eve::Nan<Type>());
+    }
+    if constexpr(eve::platform::supports_denormals)
+    {
+      TTS_EQUAL(eve::ldexp(eve::Smallestposval<Type>()/2,  1), eve::Smallestposval<Type>());
+      TTS_EQUAL(eve::ldexp(eve::Smallestposval<Type>()/2, -1), eve::Smallestposval<Type>()/4);
+      TTS_EQUAL(eve::ldexp(eve::Mindenormal<Type>(), 1), eve::Mindenormal<Type>()*2);
+      TTS_EQUAL(eve::ldexp(eve::Mindenormal<Type>(), -1), eve::Mindenormal<Type>()/2);
+    }    
+  }
+}
 
 TTS_CASE("Check eve::ldexp behavior")
 {
@@ -21,6 +62,31 @@ TTS_CASE("Check eve::ldexp behavior")
   TTS_EQUAL(eve::ldexp(Type{1}, Type{0}), Type{1});
   TTS_EQUAL(eve::ldexp(Type{1}, Type{1}), Type{2});
   TTS_EQUAL(eve::ldexp(Type{1}, Type{2}), Type{4});
+  TTS_EQUAL(eve::ldexp(Type{3}, Type{4}), Type{48});
+  if constexpr(std::is_floating_point_v<Type>)
+  {
+    TTS_EQUAL(eve::ldexp(-Type{0}, Type{1}), Type{0});
+    TTS_EXPECT(eve::is_negative(eve::ldexp(-Type{0}, Type{1})));
+    TTS_EXPECT(eve::is_positive(eve::ldexp(Type{0}, Type{1})));     
+    if constexpr(eve::platform::supports_infinites)
+    {
+      TTS_EQUAL(eve::ldexp(eve::Minf<Type>(), Type{1}), eve::Minf<Type>());
+      TTS_EQUAL(eve::ldexp(eve::Inf<Type>(), Type{1}), eve::Inf<Type>());
+    }
+    if constexpr(eve::platform::supports_invalids)
+    {
+      TTS_IEEE_EQUAL(eve::ldexp(eve::Nan<Type>(), Type{1}), eve::Nan<Type>());
+    }
+    if constexpr(eve::platform::supports_denormals)
+    {
+      TTS_EQUAL(eve::ldexp(Type{3}, Type{4.3}), Type{48});
+      TTS_EQUAL(eve::ldexp(eve::Smallestposval<Type>()/2,  Type{1}), eve::Smallestposval<Type>());
+      TTS_EQUAL(eve::ldexp(eve::Smallestposval<Type>(), Type{-1}), eve::Smallestposval<Type>()/2); 
+      TTS_EQUAL(eve::ldexp(eve::Mindenormal<Type>(), Type{1}), eve::Mindenormal<Type>()*2);
+      TTS_EQUAL(eve::ldexp(eve::Mindenormal<Type>(), Type{-1}), eve::Mindenormal<Type>()/2);
+    }    
+  }
+    
 }
 
 #endif
