00584d73345a8e5b6339741487966a1c0776e51e
Joel Falcou <joel.falcou@lri.fr>
2019-10-08 19:55:32 +0200

    Fix bitofsign

--- test/unit/module/core/bitofsign/regular/bitofsign.hpp
+++ test/unit/module/core/bitofsign/regular/bitofsign.hpp
@@ -8,59 +8,62 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BITOFSIGN_HPP
-#define BITOFSIGN_HPP
-
 #include <eve/function/bitofsign.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
-#include <eve/constant/valmax.hpp>
 #include <eve/function/is_positive.hpp>
 #include <eve/function/is_negative.hpp>
+#include <eve/function/all.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/constant/mzero.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/inf.hpp>
 #include <eve/platform.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check bitofsign return type") { TTS_EXPR_IS(eve::bitofsign(Type()), Type); }
+TTS_CASE("Check bitofsign return type")
+{
+  using eve::bitofsign;
+  TTS_EXPR_IS(bitofsign(Type()), (Type));
+}
 
 TTS_CASE("Check eve::bitofsign behavior")
 {
-  if constexpr( std::is_floating_point_v<Type>)
+  using eve::all;
+  using eve::bitofsign;
+  using eve::is_positive;
+  using eve::is_negative;
+
+  if constexpr( std::is_floating_point_v<Value>)
   {
     if constexpr( eve::platform::supports_infinites )
     {
-      TTS_EQUAL(eve::bitofsign(eve::Inf<Type>()), eve::Zero<Type>());
-      TTS_EQUAL(eve::bitofsign(eve::Minf<Type>()), eve::Mzero<Type>());
-      TTS_EXPECT(eve::is_positive(eve::bitofsign(eve::Inf<Type>())));
-      TTS_EXPECT(eve::is_negative(eve::bitofsign(eve::Minf<Type>())));
+      TTS_EQUAL( bitofsign(eve::Inf<Type>() ), Type(0)           );
+      TTS_EQUAL( bitofsign(eve::Minf<Type>()), eve::Mzero<Type>());
+      TTS_EXPECT( all(is_positive(bitofsign( eve::Inf<Type>()))) );
+      TTS_EXPECT( all(is_negative(bitofsign(eve::Minf<Type>()))) );
     }
-    TTS_EQUAL(eve::bitofsign(eve::Zero<Type>()), eve::Zero<Type>());
-    TTS_EQUAL(eve::bitofsign(eve::Mzero<Type>()), eve::Mzero<Type>());
-    TTS_EXPECT(eve::is_positive(eve::bitofsign(eve::Zero<Type>())));
-    TTS_EXPECT(eve::is_negative(eve::bitofsign(eve::Mzero<Type>())));
-    TTS_EQUAL(eve::bitofsign(eve::One<Type>()), eve::Zero<Type>());
-    TTS_EXPECT(eve::is_positive(eve::bitofsign(eve::One<Type>())));
-    TTS_EQUAL(eve::bitofsign(eve::Mone<Type>()), eve::Mzero<Type>());
-    TTS_EXPECT(eve::is_negative(eve::bitofsign(eve::Mone<Type>())));
+
+    TTS_EQUAL (bitofsign(Type(0))           , Type(0));
+    TTS_EQUAL (bitofsign(eve::Mzero<Type>()), eve::Mzero<Type>());
+    TTS_EXPECT( all(is_positive(bitofsign(Type(0)))) );
+    TTS_EXPECT( all(is_negative(bitofsign(eve::Mzero<Type>()))) );
+
+    TTS_EQUAL (bitofsign(Type(1)), Type(0));
+    TTS_EXPECT( all(is_positive(bitofsign(Type(1)))) );
+
+    TTS_EQUAL (bitofsign(Type(-1)), eve::Mzero<Type>());
+    TTS_EXPECT( all(is_negative(bitofsign(Type(-1)))) );
   }
-  else if  constexpr( std::is_unsigned_v<Type>)
+  else if constexpr( std::is_unsigned_v<Value>)
   {
-    TTS_EQUAL(eve::bitofsign(eve::One<Type>()), eve::Zero<Type>());
-    TTS_EQUAL(eve::bitofsign(eve::Valmax<Type>()), Type(1ull << (sizeof(Type)*8-1)));
+    TTS_EQUAL(bitofsign(Type(1)), Type(0));
+    TTS_EQUAL(bitofsign(eve::Valmax<Type>()), Type(1ull << (sizeof(Value)*8-1)));
   }
   else
   {
-    TTS_EQUAL(eve::bitofsign(eve::Mone<Type>()), Type(1ull << (sizeof(Type)*8-1)));
-    TTS_EQUAL(eve::bitofsign(eve::One<Type>()), eve::Zero<Type>());
-    TTS_EQUAL(eve::bitofsign(eve::Zero<Type>()), eve::Zero<Type>());
+    TTS_EQUAL(bitofsign(Type(-1)), Type(1ull << (sizeof(Value)*8-1)));
+    TTS_EQUAL(bitofsign(Type( 1)), Type(0));
+    TTS_EQUAL(bitofsign(Type( 0)), Type(0));
   }
 }
-
-#endif
