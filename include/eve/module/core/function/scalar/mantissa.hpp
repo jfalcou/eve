04f7fc6dd308c22e44fbd4b8d1be4c27e464bdfa
jtlap <jtlapreste@gmail.com>
2019-10-08 15:44:30 +0200

    mantissa en cours

---
+++ include/eve/module/core/function/scalar/mantissa.hpp
@@ -0,0 +1,38 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_MANTISSA_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_MANTISSA_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/is_not_finite.hpp>
+#include <eve/function/bitwise_or.hpp>
+#include <eve/function/bitwise_and.hpp>
+#include <eve/concept/vectorizable.hpp>
+#include <eve/detail/meta.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto mantissa_(EVE_SUPPORTS(cpu_),
+                                           T const &a) noexcept
+  requires(T, Vectorizable<T>)
+  {
+    if(!a0) return a0;
+    if constexpr(eve::platform::supports_invalids)
+      if(is_not_finite(a0)) return a0;
+    return bitwise_or(bitwise_and(a0,Mantissamask<T>()),One<T>());
+  }
+}
+
+#endif
--- test/unit/module/core/simd/mantissa/int16.cpp
+++ test/unit/module/core/simd/mantissa/int16.cpp
@@ -1,17 +1,13 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_MANTISSA_HPP_INCLUDED
-#define EVE_FUNCTION_MANTISSA_HPP_INCLUDED
 
-#include <eve/function/scalar/mantissa.hpp>
-#include <eve/function/simd/mantissa.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::int16_t;
+#include "mantissa.hpp"
