0448aadfcf627fe47a59ea743e9dd0a4d8a16407
Joel Falcou <joel.falcou@lri.fr>
2019-10-10 20:00:06 +0200

    Fix bitwise_select

--- test/unit/module/core/bitwise_select/regular/bitwise_select.hpp
+++ test/unit/module/core/bitwise_select/regular/bitwise_select.hpp
@@ -8,50 +8,38 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BITWISE_SELECT_HPP
-#define BITWISE_SELECT_HPP
-
 #include <eve/function/bitwise_select.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
+#include <eve/function/bitwise_cast.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/zero.hpp>
-#include <eve/function/bitwise_cast.hpp>
-#include <eve/detail/meta.hpp>
-#include <type_traits>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 
-TTS_CASE("Check bitwise_select return type")
+TTS_CASE("Check eve::bitwise_select return type")
 {
   using i_t = eve::detail::as_integer_t<Type, unsigned>;
-  TTS_EXPR_IS(eve::bitwise_select(Type(),Type(),Type()), Type);
-  TTS_EXPR_IS(eve::bitwise_select(i_t(),Type(),Type()), Type);
+  TTS_EXPR_IS(eve::bitwise_select(Type(),Type() ,Type() ), (Type));
+  TTS_EXPR_IS(eve::bitwise_select(Type(),Type() ,Value()), (Type));
+  TTS_EXPR_IS(eve::bitwise_select(Type(),Value(),Type() ), (Type));
+  TTS_EXPR_IS(eve::bitwise_select( i_t(),Type() ,Type() ), (Type));
+  TTS_EXPR_IS(eve::bitwise_select( i_t(),Type() ,Value()), (Type));
+  TTS_EXPR_IS(eve::bitwise_select( i_t(),Value(),Type() ), (Type));
 }
 
-TTS_CASE( "Check bitwise_select behavior")
+TTS_CASE( "Check eve::bitwise_select behavior")
 {
-  using i_t = eve::detail::as_integer_t<Type, unsigned>;
+  using i_t  = eve::detail::as_integer_t<Type, unsigned>;
+  using vi_t = eve::detail::as_integer_t<Value, unsigned>;
 
-  TTS_EXPR_IS(eve::bitwise_select(Type(),Type(),Type()), Type);
-  TTS_EXPR_IS(eve::bitwise_select(i_t(),Type(),Type()), Type);
+  std::uint64_t base  = 0xF5555552F552F5F2ULL;
+  i_t           imask = static_cast<i_t>(base & std::uint64_t(~vi_t(0)));
+  Type          mask  = eve::bitwise_cast(imask, eve::as(Type()));
 
-  std::uint64_t base = 0xF5555552F552F5F2ULL;
-  i_t imask = static_cast<i_t>(base & std::uint64_t(~i_t(0)));
-  Type mask = eve::bitwise_cast(imask, eve::as(Type()));
+  TTS_EQUAL( eve::bitwise_select( imask, eve::Allbits<Type>() , eve::Zero<Type>() ), mask );
+  TTS_EQUAL( eve::bitwise_select( imask, eve::Allbits<Value>(), eve::Zero<Type>() ), mask );
+  TTS_EQUAL( eve::bitwise_select( imask, eve::Allbits<Type>() , eve::Zero<Value>()), mask );
 
-  TTS_EQUAL( eve::bitwise_select( imask
-                                , eve::Allbits<Type>()
-                                , eve::Zero<Type>()
-                                )
-                , mask
-                );
-
-  TTS_EQUAL( eve::bitwise_select( mask
-                                , eve::Allbits<Type>()
-                                , eve::Zero<Type>()
-                                )
-                , mask
-                );
+  TTS_EQUAL( eve::bitwise_select( mask, eve::Allbits<Type>()  , eve::Zero<Type>() ), mask);
+  TTS_EQUAL( eve::bitwise_select( mask, eve::Allbits<Value>() , eve::Zero<Type>() ), mask);
+  TTS_EQUAL( eve::bitwise_select( mask, eve::Allbits<Type>()  , eve::Zero<Value>()), mask);
 }
-
-#endif
