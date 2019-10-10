441b4cc79a244db842fa320ad9dae0d9b3ac5dbc
Joel Falcou <joel.falcou@lri.fr>
2019-10-10 19:40:56 +0200

    Fix bitwise_mask

--- test/unit/module/core/bitwise_mask/regular/bitwise_mask.hpp
+++ test/unit/module/core/bitwise_mask/regular/bitwise_mask.hpp
@@ -8,31 +8,25 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BITWISE_MASK_HPP
-#define BITWISE_MASK_HPP
-
 #include <eve/function/bitwise_mask.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/true.hpp>
 #include <eve/constant/false.hpp>
-#include <type_traits>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 
-TTS_CASE("Check bitwise_mask return type")
+TTS_CASE("Check eve::bitwise_mask return type")
 {
-  TTS_EXPR_IS(eve::bitwise_mask(Type()), Type);
+  TTS_EXPR_IS(eve::bitwise_mask(Type()), (Type));
 }
 
 TTS_CASE("Check eve::bitwise_mask behavior")
 {
-  TTS_EQUAL(eve::bitwise_mask(Type{0}), Type(0));
-  TTS_EQUAL(eve::bitwise_mask(-Type{0}), Type(0));
-  TTS_IEEE_EQUAL(eve::bitwise_mask(Type{1}), eve::Allbits<Type>());
-  TTS_EQUAL(eve::bitwise_mask(eve::False<Type>()), Type(0));
-  TTS_IEEE_EQUAL(eve::bitwise_mask(eve::True<Type>()), eve::Allbits<Type>());
-}
+  TTS_EQUAL(eve::bitwise_mask( Type(0))           , (Type)(0));
+  TTS_EQUAL(eve::bitwise_mask(-Type(0))           , (Type)(0));
+  TTS_EQUAL(eve::bitwise_mask(eve::False<Type>()) , (Type)(0));
 
-#endif
+  TTS_IEEE_EQUAL(eve::bitwise_mask(Type(1))           , (eve::Allbits<Type>()));
+  TTS_IEEE_EQUAL(eve::bitwise_mask(eve::True<Type>()) , (eve::Allbits<Type>()));
+}
