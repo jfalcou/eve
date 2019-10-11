1c3f7187d5cb382246c09419ca79c1d5c6599e14
Joel Falcou <joel.falcou@lri.fr>
2019-10-11 11:18:42 +0200

    Fix acsc* functions

--- test/unit/module/core/acsc/regular/acsc.hpp
+++ test/unit/module/core/acsc/regular/acsc.hpp
@@ -8,48 +8,36 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ACSC_HPP
-#define ACSC_HPP
-
 #include <eve/function/acsc.hpp>
-#include <tts/tts.hpp>
+#include <eve/constant/mzero.hpp>
+#include <eve/constant/pio_2.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/nan.hpp>
+#include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/smallestposval.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
-#include <eve/constant/half.hpp>
-#include <eve/constant/mhalf.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/pio_2.hpp>
-#include <eve/platform.hpp>
-#include <type_traits>
-#include <cmath>
 
-TTS_CASE("Check acsc return type")
+TTS_CASE("Check eve::acsc return type")
 {
-  TTS_EXPR_IS(eve::acsc(Type(0)),  Type);
+  TTS_EXPR_IS(eve::acsc(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::acsc behavior")
 {
   if constexpr( eve::platform::supports_nans )
   {
-    TTS_ULP_EQUAL(eve::acsc(eve::Nan<Type>()),    eve::Nan<Type>(), 0);
-    TTS_ULP_EQUAL(eve::acsc(eve::Zero<Type>()),   eve::Nan<Type>(), 1);
-    TTS_ULP_EQUAL(eve::acsc(eve::Mzero<Type>()),  eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::acsc(Type(0)           ) , eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::acsc(eve::Mzero<Type>()) , eve::Nan<Type>(), 1);
+    TTS_IEEE_EQUAL(eve::acsc(eve::Nan<Type>() ) , (eve::Nan<Type>()) );
   }
-  TTS_ULP_EQUAL(eve::acsc(eve::Inf<Type>()),    0, 0);
-  TTS_ULP_EQUAL(eve::acsc(eve::Minf<Type>()),   0, 0);
-  TTS_ULP_EQUAL(eve::acsc(Type(-2)),            -eve::Pio_2<Type>()/3, 1);
-  TTS_ULP_EQUAL(eve::acsc(Type(2)),             eve::Pio_2<Type>()/3, 1);
-  TTS_ULP_EQUAL(eve::acsc(eve::Mone<Type>()),   -eve::Pio_2<Type>(), 0.5);
-  TTS_ULP_EQUAL(eve::acsc(eve::One<Type>()),    eve::Pio_2<Type>(), 0.5);
-}
 
-#endif
+  TTS_ULP_EQUAL(eve::acsc(Type(-2.)), -eve::Pio_2<Type>()/3 , 1   );
+  TTS_ULP_EQUAL(eve::acsc(Type( 2.)),  eve::Pio_2<Type>()/3 , 1   );
+  TTS_ULP_EQUAL(eve::acsc(Type(-1.)), -eve::Pio_2<Type>()   , 0.5 );
+  TTS_ULP_EQUAL(eve::acsc(Type( 1.)),  eve::Pio_2<Type>()   , 0.5 );
+
+  TTS_IEEE_EQUAL(eve::acsc(eve::Inf<Type>()) , (Type(0)));
+  TTS_IEEE_EQUAL(eve::acsc(eve::Minf<Type>()), (Type(0)));
+}
--- test/unit/module/core/acscpi/regular/acscpi.hpp
+++ test/unit/module/core/acscpi/regular/acscpi.hpp
@@ -8,49 +8,38 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ACSCPI_HPP
-#define ACSCPI_HPP
-
 #include <eve/function/acscpi.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/half.hpp>
-#include <eve/constant/mhalf.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/platform.hpp>
-#include <type_traits>
-#include <cmath>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 
-TTS_CASE("Check acscpi return type")
+TTS_CASE("Check eve::acscpi return type")
 {
-  TTS_EXPR_IS(eve::acscpi(Type(0)),  Type);
+  TTS_EXPR_IS(eve::acscpi(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::acscpi behavior")
 {
   if constexpr( eve::platform::supports_nans )
   {
-    TTS_ULP_EQUAL(eve::acscpi(eve::Nan<Type>()),    eve::Nan<Type>(), 0);
-    TTS_ULP_EQUAL(eve::acscpi(eve::Zero<Type>()),   eve::Nan<Type>(), 1);
-    TTS_ULP_EQUAL(eve::acscpi(eve::Mzero<Type>()),  eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::acscpi(Type(0))            , eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::acscpi(eve::Mzero<Type>()) , eve::Nan<Type>(), 1);
+    TTS_IEEE_EQUAL(eve::acscpi(eve::Nan<Type>())  , (eve::Nan<Type>()) );
   }
+
   if constexpr( eve::platform::supports_infinites )
   {
-    TTS_ULP_EQUAL(eve::acscpi(eve::Inf<Type>()),    0, 0);
-    TTS_ULP_EQUAL(eve::acscpi(eve::Minf<Type>()),   0, 0);
+    TTS_IEEE_EQUAL(eve::acscpi(eve::Inf<Type>()) , (Type(0)));
+    TTS_IEEE_EQUAL(eve::acscpi(eve::Minf<Type>()), (Type(0)));
   }
-  TTS_ULP_EQUAL(eve::acscpi(Type(-2)),            Type(-1)/6, 1);
-  TTS_ULP_EQUAL(eve::acscpi(Type(2)),             Type(1)/6, 1);
-  TTS_ULP_EQUAL(eve::acscpi(eve::Mone<Type>()),   Type(-0.5) , 0.5);
-  TTS_ULP_EQUAL(eve::acscpi(eve::One<Type>()),    Type(0.5) , 0.5);
-}
 
-#endif
+  TTS_ULP_EQUAL(eve::acscpi(Type(-2.)), Type(-1)/6, 1.  );
+  TTS_ULP_EQUAL(eve::acscpi(Type( 2.)), Type(1)/6 , 1.  );
+  TTS_ULP_EQUAL(eve::acscpi(Type(-1.)), Type(-0.5), 0.5 );
+  TTS_ULP_EQUAL(eve::acscpi(Type( 1.)), Type(0.5) , 0.5 );
+}
