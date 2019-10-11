93f04f7cfa379a09b39432260f7f1ce71174163f
Joel Falcou <joel.falcou@lri.fr>
2019-10-11 18:16:29 +0200

    Fix extract

--- test/unit/module/core/extract/regular/extract.hpp
+++ test/unit/module/core/extract/regular/extract.hpp
@@ -8,31 +8,50 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EXTRACT_HPP
-#define EXTRACT_HPP
-
 #include <eve/function/extract.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/detail/meta.hpp>
-#include <type_traits>
 
 TTS_CASE("Check extract return type")
 {
-  using i_t = eve::detail::as_integer_t<Type, signed>;
-  using u_t = eve::detail::as_integer_t<Type, unsigned>;
-  TTS_EXPR_IS(eve::extract(Type(), i_t()), Type);
-  TTS_EXPR_IS(eve::extract(Type(), u_t()), Type);
-  TTS_EXPR_IS(eve::extract(Type(), int()), Type);
+  using i_t = eve::detail::as_integer_t<Value, signed>;
+  using u_t = eve::detail::as_integer_t<Value, unsigned>;
+
+  TTS_EXPR_IS(eve::extract(Type(), i_t()), Value);
+  TTS_EXPR_IS(eve::extract(Type(), u_t()), Value);
+  TTS_EXPR_IS(eve::extract(Type(), int()), Value);
 }
 
-TTS_CASE("Check eve::extract behavior")
+TTS_CASE("Check eve::extract behavior on arithmetic types")
 {
-  TTS_EQUAL(eve::extract(Type{0}, 0), Type{0});
+  TTS_EQUAL(eve::extract(Type(42), 0), Value(42));
+
+#if defined(EVE_SIMD_TESTS)
+  Type arg;
+
+  for(int i=0; i< Cardinal; ++i)
+    arg[i] = static_cast<Value>(1+i);
+
+  for(int i=0; i< Cardinal; ++i)
+    TTS_EQUAL(eve::extract(arg, i), static_cast<Value>(1+i));
+#endif
 }
 
+TTS_CASE("Check eve::extract behavior on logical types")
+{
+  using eve::logical;
+
+  TTS_EQUAL(eve::extract(logical<Type>(42), 0), true  );
+  TTS_EQUAL(eve::extract(logical<Type>( 0), 0), false );
+
+#if defined(EVE_SIMD_TESTS)
+  logical<Type> arg;
+
+  for(int i=0; i< Cardinal; ++i)
+    arg[i] = ((1+i) % 2) == 0;
+
+  for(int i=0; i< Cardinal; ++i)
+    TTS_EQUAL(eve::extract(arg, i), ((1+i) % 2) == 0);
 #endif
+
+}
