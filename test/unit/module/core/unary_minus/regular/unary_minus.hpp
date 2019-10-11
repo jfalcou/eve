f4434ecba6d0a7577f99405a5d316c8f305e522a
Joel Falcou <joel.falcou@lri.fr>
2019-10-11 20:49:23 +0200

    Fix unary plus and minus

--- test/unit/module/core/unary_minus/regular/unary_minus.hpp
+++ test/unit/module/core/unary_minus/regular/unary_minus.hpp
@@ -8,18 +8,17 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef UNARY_MINUS_HPP
-#define UNARY_MINUS_HPP
-
 #include <eve/function/unary_minus.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 
-TTS_CASE("Check unary_minus return type") { TTS_EXPR_IS(eve::unary_minus(Type(0)), Type); }
+TTS_CASE("Check eve::unary_minus return type")
+{
+  TTS_EXPR_IS(eve::unary_minus(Type(0)), (Type));
+}
+
 TTS_CASE("Check eve::unary_minus behavior")
 {
-  TTS_EQUAL(eve::unary_minus(Type{1}), static_cast<Type>(-1));
-  TTS_EQUAL(eve::unary_minus(static_cast<Type>(-2)), Type{2});
+  TTS_EQUAL(eve::unary_minus(Type( 1)) , static_cast<Type>(-1));
+  TTS_EQUAL(eve::unary_minus(Type(-1)) , static_cast<Type>( 1));
 }
-#endif
