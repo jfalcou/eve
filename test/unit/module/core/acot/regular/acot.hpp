9df894f2ed572d9802002c0d0b926fcbf336f1bf
Joel Falcou <joel.falcou@lri.fr>
2019-10-11 09:40:38 +0200

    Fix acot* functions

--- test/unit/module/core/acot/regular/acot.hpp
+++ test/unit/module/core/acot/regular/acot.hpp
@@ -8,52 +8,45 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ACOT_HPP
-#define ACOT_HPP
-
 #include <eve/function/acot.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/smallestposval.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/half.hpp>
-#include <eve/constant/mhalf.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/pio_2.hpp>
 #include <eve/constant/pio_4.hpp>
 #include <eve/platform.hpp>
-#include <type_traits>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
 #include <cmath>
 
-TTS_CASE("Check acot return type")
+TTS_CASE("Check eve::acot return type")
 {
-  TTS_EXPR_IS(eve::acot(Type(0)),  Type);
+  TTS_EXPR_IS(eve::acot(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::acot behavior")
 {
   if constexpr( eve::platform::supports_nans )
   {
-    TTS_ULP_EQUAL(eve::acot(eve::Nan<Type>()),   eve::Nan<Type>(), 0);
+    TTS_ULP_EQUAL(eve::acot(eve::Nan<Type>()) , eve::Nan<Type>(), 0);
   }
+
   if constexpr( eve::platform::supports_infinites )
   {
-    TTS_ULP_EQUAL(eve::acot(eve::Inf<Type>()), 0, 0);
-    TTS_ULP_EQUAL(eve::acot(eve::Minf<Type>()), 0, 0);
+    TTS_ULP_EQUAL(eve::acot(eve::Inf<Type>()) , Type(0), 0);
+    TTS_ULP_EQUAL(eve::acot(eve::Minf<Type>()), Type(0), 0);
   }
-  TTS_ULP_EQUAL(eve::acot(eve::Half<Type>()),   Type(1.107148717794090e+00), 1);
-  TTS_ULP_EQUAL(eve::acot(eve::Mhalf<Type>()),  Type(-1.107148717794090e+00), 1);
-  TTS_ULP_EQUAL(eve::acot(eve::Mone<Type>()),   -eve::Pio_4<Type>(), 0.5);
-  TTS_ULP_EQUAL(eve::acot(eve::One<Type>()),    eve::Pio_4<Type>(), 0.5);
-  TTS_ULP_EQUAL(eve::acot(eve::Zero<Type>()),   eve::Pio_2<Type>(), 1);
-  TTS_ULP_EQUAL(eve::acot(eve::Mzero<Type>()),  -eve::Pio_2<Type>(), 1);
-  TTS_ULP_EQUAL(eve::acot(eve::rec(eve::Smallestposval<Type>())), eve::Smallestposval<Type>(), 0.5);
+
+  TTS_ULP_EQUAL(eve::acot(Type( 0.5))         ,  Type(1.107148717794090e+00)  , 1   );
+  TTS_ULP_EQUAL(eve::acot(Type(-0.5))         ,  Type(-1.107148717794090e+00) , 1   );
+  TTS_ULP_EQUAL(eve::acot(Type(-1. ))         , -eve::Pio_4<Type>()           , 0.5 );
+  TTS_ULP_EQUAL(eve::acot(Type( 1. ))         ,  eve::Pio_4<Type>()           , 0.5 );
+  TTS_ULP_EQUAL(eve::acot(Type( 0. ))         ,  eve::Pio_2<Type>()           , 1   );
+
+  auto inv_smallest = eve::rec(eve::Smallestposval<Type>());
+  TTS_ULP_EQUAL(eve::acot(eve::Mzero<Type>()) , -eve::Pio_2<Type>()           , 1   );
+  TTS_ULP_EQUAL(eve::acot(inv_smallest)       ,  eve::Smallestposval<Type>()  , 0.5 );
 }
-#endif
--- test/unit/module/core/acotd/regular/acotd.hpp
+++ test/unit/module/core/acotd/regular/acotd.hpp
@@ -8,15 +8,8 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ACOTD_HPP
-#define ACOTD_HPP
-
 #include <eve/function/acotd.hpp>
 #include <eve/function/indeg.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/smallestposval.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
@@ -28,30 +21,35 @@
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/platform.hpp>
-#include <type_traits>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
 
-TTS_CASE("Check acotd return type")
+TTS_CASE("Check eve::acotd return type")
 {
-  TTS_EXPR_IS(eve::acotd(Type(0)),  Type);
+  TTS_EXPR_IS(eve::acotd(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::acotd behavior")
 {
   if constexpr( eve::platform::supports_nans )
   {
-    TTS_ULP_EQUAL(eve::acotd(eve::Inf<Type>()), 0, 0);
-    TTS_ULP_EQUAL(eve::acotd(eve::Minf<Type>()), 0, 0);
+    TTS_ULP_EQUAL(eve::acotd(eve::Inf<Type>())  , Type(0), 0);
+    TTS_ULP_EQUAL(eve::acotd(eve::Minf<Type>()) , Type(0), 0);
   }
- if constexpr( eve::platform::supports_infinites )
+
+  if constexpr( eve::platform::supports_infinites )
   {
-    TTS_ULP_EQUAL(eve::acotd(eve::Nan<Type>()),   eve::Nan<Type>(), 0);
+    TTS_ULP_EQUAL(eve::acotd(eve::Nan<Type>()), eve::Nan<Type>(), 0);
   }
-  TTS_ULP_EQUAL(eve::acotd(eve::Half<Type>()),   Type(63.4349488229220), 1);
-  TTS_ULP_EQUAL(eve::acotd(eve::Mhalf<Type>()),  Type(-63.4349488229220), 1);
-  TTS_ULP_EQUAL(eve::acotd(eve::Mone<Type>()),   Type(-45), 0.5);
-  TTS_ULP_EQUAL(eve::acotd(eve::One<Type>()),    Type(45), 0.5);
-  TTS_ULP_EQUAL(eve::acotd(eve::Zero<Type>()),   Type(90), 0.5);
-  TTS_ULP_EQUAL(eve::acotd(eve::Mzero<Type>()),  Type(-90), 0.5);
-  TTS_ULP_EQUAL(eve::acotd(eve::rec(eve::Smallestposval<Type>())), eve::indeg(eve::Smallestposval<Type>()), 0.5);
+
+  TTS_ULP_EQUAL(eve::acotd(Type( 0.5)), Type( 63.4349488229220) , 1   );
+  TTS_ULP_EQUAL(eve::acotd(Type(-0.5)), Type(-63.4349488229220) , 1   );
+  TTS_ULP_EQUAL(eve::acotd(Type(-1. )), Type(-45)               , 0.5 );
+  TTS_ULP_EQUAL(eve::acotd(Type( 1. )), Type( 45)               , 0.5 );
+  TTS_ULP_EQUAL(eve::acotd(Type( 0. )), Type( 90)               , 0.5 );
+
+  auto inv_smallest = eve::rec(eve::Smallestposval<Type>());
+  TTS_ULP_EQUAL(eve::acotd(eve::Mzero<Type>()),  Type(-90)                              , 0.5);
+  TTS_ULP_EQUAL(eve::acotd(inv_smallest)      , eve::indeg(eve::Smallestposval<Type>()) , 0.5);
 }
-#endif
