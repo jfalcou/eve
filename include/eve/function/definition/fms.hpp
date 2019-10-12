3aa3aafa4d537c3784aa487350ce0e55ab7a899e
jtlap <jtlapreste@gmail.com>
2019-10-12 08:57:43 +0200

    suppressing fma family checks

--- include/eve/function/definition/fms.hpp
+++ include/eve/function/definition/fms.hpp
@@ -17,32 +17,7 @@
 
 namespace eve
 {
-  namespace tag { struct fms_; }
-
-  namespace detail
-  {
-    template<typename T, typename U, typename V>
-    EVE_FORCEINLINE void check(EVE_MATCH_CALL(eve::tag::fms_), T const&, U const &, V const &)
-    {
-      constexpr bool is_vec_t = is_vectorized_v<T>;
-      constexpr bool is_vec_u = is_vectorized_v<U>;
-      constexpr bool is_vec_v = is_vectorized_v<V>;
-      if constexpr(is_vec_t && is_vec_u && is_vec_v)
-      {
-        static_assert(std::is_same_v<T, U> && std::is_same_v<T, V>, 
-                    "[eve::fms]  - SIMD arguments have not the same type");
-
-      }
-      if constexpr(!is_vec_t && !is_vec_u && !is_vec_v)
-      {
-        static_assert(std::is_same_v<T, U> && std::is_same_v<T, V>, 
-                    "[eve::fms]  - scalar arguments have not the same type");
-
-      }
-    }
-  }
-
-EVE_MAKE_CALLABLE(fms_, fms);
+  EVE_MAKE_CALLABLE(fms_, fms);
 }
 
 #endif
