ec9abfcbf13c4712cd163f249554df8b6850b4f4
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 22:49:57 +0200

    Fix bitwise_andnot

--- test/unit/module/core/bitwise_andnot/regular/bitwise_andnot.hpp
+++ test/unit/module/core/bitwise_andnot/regular/bitwise_andnot.hpp
@@ -8,50 +8,58 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BITWISE_ANDNOT_HPP
-#define BITWISE_ANDNOT_HPP
-
 #include <eve/function/bitwise_andnot.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/allbits.hpp>
-#include <eve/detail/meta.hpp>
 #include <type_traits>
 
-TTS_CASE("Check bitwise_andnot return type")
+TTS_CASE("Check eve::bitwise_andnot return type")
 {
-  TTS_EXPR_IS(eve::bitwise_andnot(Type(), Type()), Type);
-  TTS_EXPR_IS(eve::bitwise_andnot(Type(), eve::detail::as_integer_t<Type>()), Type);
-  using ui_t = eve::detail::as_integer_t<Type, unsigned>;
-  using si_t = eve::detail::as_integer_t<Type, signed>;
-  TTS_EXPR_IS(eve::bitwise_andnot(Type(),ui_t()) , Type);
-  TTS_EXPR_IS(eve::bitwise_andnot(Type(),si_t()) , Type);
-  TTS_EXPR_IS(eve::bitwise_andnot(ui_t(), Type()), ui_t);
-  TTS_EXPR_IS(eve::bitwise_andnot(si_t(), Type()), si_t);
+  using eve::detail::as_integer_t;
+
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(), Type())  , (Type));
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(), Value()) , (Type));
+
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(),(as_integer_t<Type, unsigned>())) , (Type));
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(),(as_integer_t<Value, unsigned>())), (Type));
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(),(as_integer_t<Type, signed>()))   , (Type));
+  TTS_EXPR_IS(eve::bitwise_andnot(Type(),(as_integer_t<Value, signed>()))  , (Type));
+
+  TTS_EXPR_IS(eve::bitwise_andnot((as_integer_t<Type, unsigned>()) , Type()), (as_integer_t<Type, unsigned>));
+  TTS_EXPR_IS(eve::bitwise_andnot((as_integer_t<Type, signed>())   , Type()), (as_integer_t<Type, signed>));
 }
 
 TTS_CASE( "Check bitwise_andnot behavior")
 {
-  TTS_EQUAL(eve::bitwise_andnot(eve::Zero<Type>(), eve::One<Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::bitwise_andnot(eve::Zero<Type>(), eve::Zero<Type>()), eve::Zero<Type>());
-  TTS_EQUAL(eve::bitwise_andnot(eve::One<Type>(), eve::Zero<Type>()), eve::One<Type>());
-  TTS_IEEE_EQUAL(eve::bitwise_andnot(eve::Allbits<Type>(), eve::Zero<Type>()), eve::Allbits<Type>());
-  if constexpr(std::is_integral_v<Type>)
+  using eve::detail::as_integer_t;
+
+  TTS_EQUAL(eve::bitwise_andnot((Type(0)), (Type(0)))  , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(0)), (Value(0))) , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(0)), (Type(1)))  , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(0)), (Value(1))) , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), (Type(1)))  , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), (Value(1))) , (Type(0)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), (Type(0)))  , (Type(1)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), (Value(0))) , (Type(1)));
+
+  using ui_t = as_integer_t<Type, unsigned>;
+  using vi_t = as_integer_t<Value, unsigned>;
+  using si_t = as_integer_t<Type, signed>;
+  using wi_t = as_integer_t<Value, signed>;
+
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), ui_t(0)), (Type(1)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), vi_t(0)), (Type(1)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), si_t(0)), (Type(1)));
+  TTS_EQUAL(eve::bitwise_andnot((Type(1)), wi_t(0)), (Type(1)));
+
+  TTS_EQUAL(eve::bitwise_andnot(ui_t(1), (Type(0))), ui_t(1));
+  TTS_EQUAL(eve::bitwise_andnot(si_t(1), (Type(0))), si_t(1));
+
+  if constexpr(std::is_integral_v<Value>)
   {
-    TTS_EQUAL(eve::bitwise_andnot(Type(3), Type(2)), Type(1));
-    TTS_EQUAL(eve::bitwise_andnot(Type(3), Type(1)), Type(2));
-    using ui_t = eve::detail::as_integer_t<Type, unsigned>;
-    using si_t = eve::detail::as_integer_t<Type, signed>;
-    TTS_EQUAL(eve::bitwise_andnot(eve::One<Type>(), eve::Zero<ui_t>()), eve::One<Type>());
-    TTS_EQUAL(eve::bitwise_andnot(eve::One<Type>(), eve::Zero<si_t>()), eve::One<Type>());
-    TTS_EQUAL(eve::bitwise_andnot(eve::One<ui_t>(), eve::Zero<Type>()), eve::One<ui_t>());
-    TTS_EQUAL(eve::bitwise_andnot(eve::One<si_t>(), eve::Zero<Type>()), eve::One<si_t>());
+    TTS_EQUAL(eve::bitwise_andnot(Type(3), Type(~2) ) , Type(2));
+    TTS_EQUAL(eve::bitwise_andnot(Type(3), Value(~2)) , Type(2));
+    TTS_EQUAL(eve::bitwise_andnot(Type(3), Type(~1) ) , Type(1));
+    TTS_EQUAL(eve::bitwise_andnot(Type(3), Value(~1)) , Type(1));
   }
 }
-
-#endif
