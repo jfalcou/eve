e65549a967a94fddeba160b07b3ae082fc1e55c9
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 21:47:31 +0200

    Fix minmag

--- test/unit/module/core/minmag/pedantic/minmag.hpp
+++ test/unit/module/core/minmag/pedantic/minmag.hpp
@@ -8,37 +8,59 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef MINMAG_HPP
-#define MINMAG_HPP
-
 #include <eve/function/minmag.hpp>
+#include <eve/function/pedantic.hpp>
 #include <eve/constant/nan.hpp>
-#include <eve/constant/one.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <type_traits>
+#include <tts/tests/types.hpp>
+
+TTS_CASE("Check eve::pedantic_(eve::minmag) return type")
+{
+  TTS_EXPR_IS(eve::pedantic_(eve::minmag)(Type(0)  , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::pedantic_(eve::minmag)(Value(0) , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::pedantic_(eve::minmag)(Type(0)  , Value(0)) , (Type));
+}
 
 TTS_CASE("Check eve::pedantic_(eve::minmag) behavior")
 {
-  TTS_EQUAL(eve::pedantic_(eve::minmag)(Type{0}, Type{0}), Type{0});
-  TTS_EQUAL(eve::pedantic_(eve::minmag)(Type{0}, Type{1}), Type{0});
-  TTS_EQUAL(eve::pedantic_(eve::minmag)(Type{1}, Type{0}), Type{0});
-  TTS_EQUAL(eve::pedantic_(eve::minmag)(Type{1}, Type{2}), Type{1});
-  TTS_EQUAL(eve::pedantic_(eve::minmag)(Type{2}, Type{1}), Type{1});
-  if constexpr(std::is_signed_v<Type>)
-  {
-    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-3), Type{2}), Type{2});
-    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-1), Type{2}), static_cast<Type>(-1));
-    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-1), Type{1}), static_cast<Type>(-1));
-  }
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(0)), (Type(1))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(1)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(1)), (Type(1))), (Type(1)));
+
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Value(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Value(0)), (Type(1))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Value(1)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Value(1)), (Type(1))), (Type(1)));
+
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(0)), (Value(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(0)), (Value(1))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(1)), (Value(0))), (Type(0)));
+  TTS_EQUAL(eve::pedantic_(eve::minmag)((Type(1)), (Value(1))), (Type(1)));
+
   if constexpr(std::is_floating_point_v<Type>)
   {
-    Type n = eve::Nan<Type>();
-    Type o = eve::One<Type>();
-    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)(n, o), n);
-    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)(o, n), o);
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((eve::Nan<Type>() ), (Type(1))) , (eve::Nan<Type>()) );
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((eve::Nan<Value>()), (Type(1))) , (eve::Nan<Type>()) );
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((eve::Nan<Type>() ), (Value(1))), (eve::Nan<Type>()) );
+
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((Type(1)) , (eve::Nan<Type>())  ), (Type(1)) );
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((Value(1)), (eve::Nan<Type>())  ), (Type(1)) );
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::minmag)((Type(1)) , (eve::Nan<Value>()) ), (Type(1)) );
+  }
+
+  if constexpr(std::is_signed_v<Value>)
+  {
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
+
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
+
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::pedantic_(eve::minmag)(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
   }
 }
-
-#endif
