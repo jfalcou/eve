7ae8b8922458ca294f7cafc469a4e610ef09518d
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 15:48:43 +0200

    ALL_EQUAL -> EQUAL

--- test/unit/module/core/add/saturated/add.hpp
+++ test/unit/module/core/add/saturated/add.hpp
@@ -22,24 +22,24 @@ TTS_CASE("Check eve::saturated_(eve::add) return type")
 
 TTS_CASE("Check eve::saturated_(eve::add) behavior")
 {
-  TTS_ALL_EQUAL(eve::saturated_(eve::add)(Type{0}, Type{0}), (Type)(0));
-  TTS_ALL_EQUAL(eve::saturated_(eve::add)(Type{1}, Type{1}), (Type)(2));
-  TTS_ALL_EQUAL(eve::saturated_(eve::add)(Type{2}, Type{2}), (Type)(4));
+  TTS_EQUAL(eve::saturated_(eve::add)(Type{0}, Type{0}), (Type)(0));
+  TTS_EQUAL(eve::saturated_(eve::add)(Type{1}, Type{1}), (Type)(2));
+  TTS_EQUAL(eve::saturated_(eve::add)(Type{2}, Type{2}), (Type)(4));
 
   if constexpr(std::is_integral_v<Value>)
   {
-    TTS_ALL_EQUAL ( (eve::saturated_(eve::add)(eve::Valmax<Type>(),eve::One<Type>())),
+    TTS_EQUAL ( (eve::saturated_(eve::add)(eve::Valmax<Type>(),eve::One<Type>())),
                     (eve::Valmax<Type>())
                   );
 
     if constexpr(std::is_signed_v<Value>)
     {
-      TTS_ALL_EQUAL ( (eve::saturated_(eve::add)(eve::Valmin<Type>(),eve::Mone<Type>())),
+      TTS_EQUAL ( (eve::saturated_(eve::add)(eve::Valmin<Type>(),eve::Mone<Type>())),
                       (eve::Valmin<Type>())
                     );
 
-      TTS_ALL_EQUAL(eve::saturated_(eve::add)(Type(-1), Type(1)),  (Type)(0));
-      TTS_ALL_EQUAL(eve::saturated_(eve::add)(Type(-2), Type(-6)), (Type)(-8));
+      TTS_EQUAL(eve::saturated_(eve::add)(Type(-1), Type(1)),  (Type)(0));
+      TTS_EQUAL(eve::saturated_(eve::add)(Type(-2), Type(-6)), (Type)(-8));
     }
   }
 }
