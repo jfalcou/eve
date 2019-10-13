80169b5e2393c6702bca294e09de8cfecbf218c9
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 14:27:30 +0200

    Fix asec* family

--- test/unit/module/core/asec/regular/asec.hpp
+++ test/unit/module/core/asec/regular/asec.hpp
@@ -8,49 +8,38 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ASEC_HPP
-#define ASEC_HPP
-
 #include <eve/function/asec.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/half.hpp>
-#include <eve/constant/mhalf.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/platform.hpp>
-#include <type_traits>
-#include <cmath>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
 
-TTS_CASE("Check asec return type")
+TTS_CASE("Check eve::asec return type")
 {
-  TTS_EXPR_IS(eve::asec(Type(0)),  Type);
+  TTS_EXPR_IS(eve::asec(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::asec behavior")
 {
   if constexpr( eve::platform::supports_infinites )
   {
-    TTS_ULP_EQUAL(eve::asec(eve::Inf<Type>()),     eve::Pio_2<Type>(), 0);
-    TTS_ULP_EQUAL(eve::asec(eve::Minf<Type>()),    eve::Pio_2<Type>(), 0);
+    TTS_IEEE_EQUAL(eve::asec(eve::Inf<Type>()),  (eve::Pio_2<Type>()) );
+    TTS_IEEE_EQUAL(eve::asec(eve::Minf<Type>()), (eve::Pio_2<Type>()) );
   }
+
   if constexpr( eve::platform::supports_nans )
   {
-    TTS_ULP_EQUAL(eve::asec(eve::Nan<Type>()),     eve::Nan<Type>(), 0);
-    TTS_ULP_EQUAL(eve::asec(eve::Zero<Type>()),    eve::Nan<Type>(), 1);
-    TTS_ULP_EQUAL(eve::asec(eve::Mzero<Type>()),   eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::asec((Type(0)))          , eve::Nan<Type>(), 1);
+    TTS_ULP_EQUAL(eve::asec(eve::Mzero<Type>()) , eve::Nan<Type>(), 1);
+    TTS_IEEE_EQUAL(eve::asec(eve::Nan<Type>())  , (eve::Nan<Type>()) );
   }
-  TTS_ULP_EQUAL(eve::asec(Type(-2)),             2*eve::Pi<Type>()/3, 1);
-  TTS_ULP_EQUAL(eve::asec(Type(2)),              eve::Pi<Type>()/3, 1);
-  TTS_ULP_EQUAL(eve::asec(eve::Mone<Type>()),    eve::Pi<Type>(), 0.5);
-  TTS_ULP_EQUAL(eve::asec(eve::One<Type>()),     eve::Zero<Type>(), 0.5);
-}
 
-#endif
+  TTS_ULP_EQUAL(eve::asec(Type(-2)) , 2*eve::Pi<Type>()/3 , 1   );
+  TTS_ULP_EQUAL(eve::asec(Type( 2)) , eve::Pi<Type>()/3   , 1   );
+  TTS_ULP_EQUAL(eve::asec(Type(-1)) , eve::Pi<Type>()     , 0.5 );
+  TTS_ULP_EQUAL(eve::asec(Type( 1)) , (Type(0))           , 0.5 );
+}
