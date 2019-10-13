bde8d8ae132dab44e0f30eff17adc09da3ccbd23
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 21:47:31 +0200

    Fix minmag

--- test/unit/module/core/minmag/regular/minmag.hpp
+++ test/unit/module/core/minmag/regular/minmag.hpp
@@ -8,29 +8,46 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef MINMAG_HPP
-#define MINMAG_HPP
-
 #include <eve/function/minmag.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <eve/constant/one.hpp>
-#include <type_traits>
+#include <tts/tests/types.hpp>
+
+TTS_CASE("Check eve::minmag return type")
+{
+  TTS_EXPR_IS(eve::minmag(Type(0)  , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::minmag(Value(0) , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::minmag(Type(0)  , Value(0)) , (Type));
+}
 
 TTS_CASE("Check eve::minmag behavior")
 {
-  TTS_EQUAL(eve::minmag(Type{0}, Type{0}), Type{0});
-  TTS_EQUAL(eve::minmag(Type{0}, Type{1}), Type{0});
-  TTS_EQUAL(eve::minmag(Type{1}, Type{0}), Type{0});
-  TTS_EQUAL(eve::minmag(Type{1}, Type{2}), Type{1});
-  TTS_EQUAL(eve::minmag(Type{2}, Type{1}), Type{1});
-  if constexpr(std::is_signed_v<Type>)
+  TTS_EQUAL(eve::minmag((Type(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(0)), (Type(1))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(1)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(1)), (Type(1))), (Type(1)));
+
+  TTS_EQUAL(eve::minmag((Value(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Value(0)), (Type(1))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Value(1)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Value(1)), (Type(1))), (Type(1)));
+
+  TTS_EQUAL(eve::minmag((Type(0)), (Value(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(0)), (Value(1))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(1)), (Value(0))), (Type(0)));
+  TTS_EQUAL(eve::minmag((Type(1)), (Value(1))), (Type(1)));
+
+  if constexpr(std::is_signed_v<Value>)
   {
-    TTS_EQUAL(eve::minmag(static_cast<Type>(-3), Type{2}), Type{2});
-    TTS_EQUAL(eve::minmag(static_cast<Type>(-1), Type{2}), static_cast<Type>(-1));
-    TTS_EQUAL(eve::minmag(static_cast<Type>(-1), Type{1}), static_cast<Type>(-1));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
+
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
+
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-3), (Type(2))), (Type(2)));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-1), (Type(2))), static_cast<Type>(-1));
+    TTS_EQUAL(eve::minmag(static_cast<Type>(-2), (Type(2))), static_cast<Type>(-2));
   }
 }
-
-#endif
