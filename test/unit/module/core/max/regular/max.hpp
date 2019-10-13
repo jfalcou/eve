1008dc2a2215cbefd985c8363a51629746a20a6e
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 19:30:23 +0200

    Fix max

--- test/unit/module/core/max/regular/max.hpp
+++ test/unit/module/core/max/regular/max.hpp
@@ -8,23 +8,31 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef MAX_HPP
-#define MAX_HPP
-
 #include <eve/function/max.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/one.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <type_traits>
+#include <tts/tests/types.hpp>
+
+TTS_CASE("Check eve::max return type")
+{
+  TTS_EXPR_IS(eve::max(Type(0)  , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::max(Value(0) , Type(0) ) , (Type));
+  TTS_EXPR_IS(eve::max(Type(0)  , Value(0)) , (Type));
+}
 
 TTS_CASE("Check eve::max behavior")
 {
-  TTS_EQUAL(eve::max(Type{0}, Type{0}), Type{0});
-  TTS_EQUAL(eve::max(Type{0}, Type{1}), Type{1});
-  TTS_EQUAL(eve::max(Type{1}, Type{0}), Type{1});
-  TTS_EQUAL(eve::max(Type{1}, Type{1}), Type{1});
-}
+  TTS_EQUAL(eve::max((Type(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::max((Type(0)), (Type(1))), (Type(1)));
+  TTS_EQUAL(eve::max((Type(1)), (Type(0))), (Type(1)));
+  TTS_EQUAL(eve::max((Type(1)), (Type(1))), (Type(1)));
 
-#endif
+  TTS_EQUAL(eve::max((Value(0)), (Type(0))), (Type(0)));
+  TTS_EQUAL(eve::max((Value(0)), (Type(1))), (Type(1)));
+  TTS_EQUAL(eve::max((Value(1)), (Type(0))), (Type(1)));
+  TTS_EQUAL(eve::max((Value(1)), (Type(1))), (Type(1)));
+
+  TTS_EQUAL(eve::max((Type(0)), (Value(0))), (Type(0)));
+  TTS_EQUAL(eve::max((Type(0)), (Value(1))), (Type(1)));
+  TTS_EQUAL(eve::max((Type(1)), (Value(0))), (Type(1)));
+  TTS_EQUAL(eve::max((Type(1)), (Value(1))), (Type(1)));
+}
