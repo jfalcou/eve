f49ffd5dec07ac3a05d71f20d876e824056043dc
Joel Falcou <joel.falcou@lri.fr>
2019-10-13 13:02:28 +0200

    Fix oneminus

--- test/unit/module/core/oneminus/regular/oneminus.hpp
+++ test/unit/module/core/oneminus/regular/oneminus.hpp
@@ -8,43 +8,31 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef ONEMINUS_HPP
-#define ONEMINUS_HPP
-
 #include <eve/function/oneminus.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <tts/tests/types.hpp>
-#include <eve/constant/false.hpp>
 #include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/as_logical.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check oneminus return type")
+TTS_CASE("Check eve::oneminus return type")
 {
-  TTS_EXPR_IS(eve::oneminus(Type(0)),  Type);
+  TTS_EXPR_IS(eve::oneminus(Type(0)), (Type));
 }
 
 TTS_CASE("Check eve::oneminus behavior")
 {
-  TTS_EQUAL(eve::oneminus(Type{1}), Type(0));
-  TTS_EQUAL(eve::oneminus[Type(1) > Type(0)](Type(1)),  Type(0));
-  TTS_EQUAL(eve::oneminus[Type(1) > Type(2)](eve::Zero<Type>()),  Type(0));
+  TTS_EQUAL(eve::oneminus(Type(1)), (Type(0)));
 
-   if constexpr(std::is_signed_v<Type>)
+  if constexpr(std::is_signed_v<Value>)
   {
-    TTS_EQUAL(eve::oneminus(Type{2}), Type(-1));
-    TTS_EQUAL(eve::oneminus(static_cast<Type>(-2)), Type(3));
-    TTS_EQUAL(eve::oneminus[Type(-1) > Type(0)](eve::Zero<Type>()),  Type(0));
+    TTS_EQUAL(eve::oneminus(Type(2))              , (Type(-1)));
+    TTS_EQUAL(eve::oneminus(static_cast<Type>(-2)), (Type( 3)));
   }
-  if constexpr(std::is_floating_point_v<Type>)
+
+  if constexpr(std::is_floating_point_v<Value>)
   {
-    TTS_EQUAL(eve::oneminus(eve::Mzero<Type>()), Type(1));
-    TTS_EQUAL(eve::oneminus(eve::Zero<Type>()),  Type(1));
+    TTS_EQUAL(eve::oneminus(eve::Mzero<Type>()) , (Type(1)));
+    TTS_EQUAL(eve::oneminus((Type(0)))          , (Type(1)));
   }
 }
-
-#endif
