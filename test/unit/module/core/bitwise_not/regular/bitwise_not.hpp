25b6f8d20c87e9d6a5e01a2df132bc0ee77ac83f
Joel Falcou <joel.falcou@lri.fr>
2019-10-15 11:10:47 +0200

    Fix bitwise_not

--- test/unit/module/core/bitwise_not/regular/bitwise_not.hpp
+++ test/unit/module/core/bitwise_not/regular/bitwise_not.hpp
@@ -8,37 +8,30 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef BITWISE_NOT_HPP
-#define BITWISE_NOT_HPP
-
 #include <eve/function/bitwise_not.hpp>
-#include <tts/tts.hpp>
+#include <eve/constant/allbits.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/allbits.hpp>
-#include <eve/constant/true.hpp>
-#include <eve/constant/false.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/detail/meta.hpp>
 #include <type_traits>
 
-TTS_CASE("Check bitwise_not return type")
+TTS_CASE("Check eve::bitwise_not return type")
 {
-  TTS_EXPR_IS(eve::bitwise_not(Type()), Type);
+  TTS_EXPR_IS(eve::bitwise_not(Type()), (Type));
 }
 
 TTS_CASE("Check eve::bitwise_not behavior")
 {
-  TTS_IEEE_EQUAL(eve::bitwise_not(Type{0}), eve::Allbits<Type>());
-  if constexpr(std::is_integral_v<Type>)
+  TTS_IEEE_EQUAL(eve::bitwise_not(Type(0)), (eve::Allbits<Type>()));
+
+  if constexpr(std::is_integral_v<Value>)
   {
-    TTS_EQUAL(eve::bitwise_not(static_cast<Type>(0x1234567890ABCDEFULL)), static_cast<Type>(0xEDCBA9876F543210ULL));
-    TTS_EQUAL(eve::bitwise_not(eve::Zero<Type>()), eve::Mone<Type>());
-    TTS_EQUAL(eve::bitwise_not(eve::Mone<Type>()), eve::Zero<Type>());
+    TTS_EQUAL ( eve::bitwise_not( static_cast<Type>(0x1234567890ABCDEFULL))
+              , static_cast<Type>(0xEDCBA9876F543210ULL)
+              );
+
+    TTS_EQUAL(eve::bitwise_not(Type( 0)), (Type(-1)));
+    TTS_EQUAL(eve::bitwise_not(Type(-1)), (Type( 0)));
   }
 }
 
-#endif
