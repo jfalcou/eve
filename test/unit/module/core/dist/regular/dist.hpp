bfccede394de3274779183e6dde39357866c03b8
Joel Falcou <joel.falcou@lri.fr>
2019-10-16 23:30:56 +0200

    Fix dist

--- test/unit/module/core/dist/regular/dist.hpp
+++ test/unit/module/core/dist/regular/dist.hpp
@@ -8,16 +8,15 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef DIST_HPP
-#define DIST_HPP
-
 #include <eve/function/dist.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check dist return type") { TTS_EXPR_IS(eve::dist(Type(), Type()), Type); }
+TTS_CASE("Check eve::dist return type")
+{
+  TTS_EXPR_IS(eve::dist(Type(), Type()), (Type));
+}
 
 TTS_CASE("Check eve::dist behavior")
 {
@@ -25,11 +24,10 @@ TTS_CASE("Check eve::dist behavior")
   TTS_EQUAL(eve::dist(Type{0}, Type{1}), Type{1});
   TTS_EQUAL(eve::dist(Type{1}, Type{0}), Type{1});
   TTS_EQUAL(eve::dist(Type{1}, Type{1}), Type{0});
+
   if constexpr(std::is_signed_v<Type>)
   {
     TTS_EQUAL(eve::dist(Type(-1), Type(1)), Type(2));
     TTS_EQUAL(eve::dist(Type(-2), Type(-6)), Type(4));
   }
 }
-
-#endif
--- test/unit/module/core/dist/saturated/dist.hpp
+++ test/unit/module/core/dist/saturated/dist.hpp
@@ -8,39 +8,35 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef DIST_HPP
-#define DIST_HPP
-
 #include <eve/function/dist.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/mone.hpp>
 #include <type_traits>
 
-TTS_CASE("Check dist return type")
+TTS_CASE("Check saturated_(eve::dist) return type")
 {
-  TTS_EXPR_IS(eve::saturated_(eve::dist)(Type(), Type()), Type);
+  TTS_EXPR_IS(eve::saturated_(eve::dist)(Type(), Type()), (Type));
 }
 
-TTS_CASE("Check eve::saturated_(eve::dist) behavior")
+TTS_CASE("Check saturated_(eve::dist) behavior")
 {
-  TTS_EQUAL(eve::saturated_(eve::dist)(Type{0}, Type{0}), Type(0));
-  TTS_EQUAL(eve::saturated_(eve::dist)(Type{1}, Type{5}), Type(4));
+  TTS_EQUAL(eve::saturated_(eve::dist)(Type(0), Type(0)), (Type(0)));
+  TTS_EQUAL(eve::saturated_(eve::dist)(Type(1), Type(5)), (Type(4)));
+
   if constexpr(std::is_integral_v<Type>)
   {
-    TTS_EQUAL(eve::saturated_(eve::dist)(eve::Valmax<Type>(),eve::Valmin<Type>()), eve::Valmax<Type>());
+    TTS_EQUAL ( eve::saturated_(eve::dist)(eve::Valmax<Type>(),eve::Valmin<Type>())
+              , eve::Valmax<Type>()
+              );
+
     if constexpr(std::is_signed_v<Type>)
     {
-      TTS_EQUAL(eve::saturated_(eve::dist)(eve::Valmin<Type>(),eve::Mone<Type>()), eve::Valmax<Type>());
-      TTS_EQUAL(eve::saturated_(eve::dist)(eve::Valmin<Type>(),eve::Zero<Type>()), eve::Valmax<Type>());
-      TTS_EQUAL(eve::saturated_(eve::dist)(Type(-1), Type(1)), Type(2));
-      TTS_EQUAL(eve::saturated_(eve::dist)(Type(-2), Type(-6)), Type(4));
+      TTS_EQUAL(eve::saturated_(eve::dist)(eve::Valmin<Type>(),Type(-1)), eve::Valmax<Type>());
+      TTS_EQUAL(eve::saturated_(eve::dist)(eve::Valmin<Type>(),Type( 0)), eve::Valmax<Type>());
+      TTS_EQUAL(eve::saturated_(eve::dist)(Type(-1), Type(1) ), (Type(2)));
+      TTS_EQUAL(eve::saturated_(eve::dist)(Type(-2), Type(-6)), (Type(4)));
     }
   }
 }
-
-#endif
