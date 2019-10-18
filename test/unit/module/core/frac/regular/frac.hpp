d7c1fe7e2c4b103a76b37d93fc2d1b725dd4382c
Joel Falcou <joel.falcou@lri.fr>
2019-10-18 15:21:17 +0200

    Fix frac

--- test/unit/module/core/frac/regular/frac.hpp
+++ test/unit/module/core/frac/regular/frac.hpp
@@ -8,38 +8,34 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef FRAC_HPP
-#define FRAC_HPP
-
 #include <eve/function/frac.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check frac return type")
+TTS_CASE("Check eve::frac return type")
 {
-  TTS_EXPR_IS(eve::frac(Type()), Type);
+  TTS_EXPR_IS(eve::frac(Type()), (Type));
 }
 
 TTS_CASE("Check eve::frac behavior")
 {
-  TTS_EQUAL(eve::frac(Type{0}), Type(0));
-  TTS_EQUAL(eve::frac(Type{2}), Type(0));
-  if constexpr(std::is_signed_v<Type>)
+  TTS_EQUAL(eve::frac(Type(0)), Type(0));
+  TTS_EQUAL(eve::frac(Type(2)), Type(0));
+
+  if constexpr(std::is_signed_v<Value>)
   {
-    TTS_EQUAL(eve::frac(static_cast<Type>(-2)), Type(0));
-    TTS_EQUAL(eve::frac(static_cast<Type>(1)) , Type(0));
+    TTS_EQUAL(eve::frac(static_cast<Type>(-2)), (Type(0)) );
+    TTS_EQUAL(eve::frac(static_cast<Type>(1)) , (Type(0)) );
   }
-  if constexpr(std::is_floating_point_v<Type>)
+
+  if constexpr(std::is_floating_point_v<Value>)
   {
-    TTS_EQUAL(eve::frac(Type(3) / Type(-2)), Type(-1) / 2);
-    TTS_EQUAL(eve::frac(Type(3) / Type(2)), Type(1) / 2);
-    TTS_ULP_EQUAL(eve::frac(Type(4) / Type(3)), Type(1) / 3, 0.5);
-    TTS_ULP_EQUAL(eve::frac(Type(4) / Type(-3)), Type(-1) / 3, 0.5);
+    TTS_EQUAL(eve::frac(Type(-3/2.)), Type(-0.5));
+    TTS_EQUAL(eve::frac(Type( 3/2.)), Type( 0.5));
+
+    TTS_ULP_EQUAL(eve::frac(Type( 4/3.)), (Type( 1/3.)), 0.5);
+    TTS_ULP_EQUAL(eve::frac(Type(-4/3.)), (Type(-1/3.)), 0.5);
   }
 }
-
-
-#endif
