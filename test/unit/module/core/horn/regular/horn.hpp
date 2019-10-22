61567d66488ca97fed9eecf732a060d786b0ffc9
Joel Falcou <joel.falcou@lri.fr>
2019-10-22 22:06:00 +0200

    Fix horn/horn1

--- test/unit/module/core/horn/regular/horn.hpp
+++ test/unit/module/core/horn/regular/horn.hpp
@@ -8,12 +8,36 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef HORN1_HPP
-#define HORN1_HPP
-
 #include <eve/module/core/detail/generic/horn.hpp>
-#include "test.hpp"
 #include <tts/tests/relation.hpp>
+#include <type_traits>
 
+#ifndef HORN_INCLUDED
+#define HORN_INCLUDED
+template<typename T> constexpr auto coeff0()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x00000000U;
+  else                                      return 0x00000000ULL;
+}
+
+template<typename T> constexpr auto coeff1()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x3f800000U;
+  else                                      return 0x3FF0000000000000ULL;
+}
+
+template<typename T> constexpr auto coeff2()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x40000000U;
+  else                                      return 0x4000000000000000ULL;
+}
 #endif
- 
+
+TTS_CASE("Check eve::horn behavior")
+{
+  using eve::detail::horn;
+
+  TTS_EQUAL((horn<Type, coeff0<Value>()>(Type(0)))                                  , (Type(0)));
+  TTS_EQUAL((horn<Type, coeff1<Value>(), coeff0<Value>(), coeff1<Value>()>(Type(1))), (Type(2)));
+  TTS_EQUAL((horn<Type, coeff2<Value>(), coeff1<Value>(), coeff1<Value>()>(Type(2))), (Type(8)));
+}
--- test/unit/module/core/horn1/regular/horn1.hpp
+++ test/unit/module/core/horn1/regular/horn1.hpp
@@ -8,14 +8,36 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef HORN1_HPP
-#define HORN1_HPP
- 
 #include <eve/module/core/detail/generic/horn1.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
 #include <type_traits>
 
+#ifndef HORN1_INCLUDED
+#define HORN1_INCLUDED
+template<typename T> constexpr auto coeff0()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x00000000U;
+  else                                      return 0x00000000ULL;
+}
 
+template<typename T> constexpr auto coeff1()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x3f800000U;
+  else                                      return 0x3FF0000000000000ULL;
+}
+
+template<typename T> constexpr auto coeff2()
+{
+  if constexpr( std::is_same_v<T, float> )  return 0x40000000U;
+  else                                      return 0x4000000000000000ULL;
+}
 #endif
+
+TTS_CASE("Check eve::horn1 behavior")
+{
+  using eve::detail::horn1;
+
+  TTS_EQUAL((horn1<Type, coeff0<Value>()>(Type(0)))                                  , (Type(0)));
+  TTS_EQUAL((horn1<Type, coeff1<Value>(), coeff0<Value>(), coeff1<Value>()>(Type(1))), (Type(3)));
+  TTS_EQUAL((horn1<Type, coeff2<Value>(), coeff1<Value>(), coeff1<Value>()>(Type(2))), (Type(16)));
+}
