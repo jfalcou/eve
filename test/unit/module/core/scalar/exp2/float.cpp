a4ff44388495f2717a7caed28822fad9d8f4dc35
jtlap <jtlapreste@gmail.com>
2019-10-22 20:24:02 +0200

    exp2

--- test/unit/module/core/scalar/exp2/float.cpp
+++ test/unit/module/core/scalar/exp2/float.cpp
@@ -1,17 +1,12 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2018 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_EXP2_HPP_INCLUDED
-#define EVE_FUNCTION_EXP2_HPP_INCLUDED
 
-#include <eve/function/scalar/exp2.hpp>
-#include <eve/function/simd/exp2.hpp>
-
-#endif
+using Type = float;
+#include "exp2.hpp"
