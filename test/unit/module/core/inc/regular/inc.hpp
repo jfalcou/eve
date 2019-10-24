675d82d81aaca050c486818eb45bbc2e9af2f1d9
Joel Falcou <joel.falcou@lri.fr>
2019-10-24 10:41:27 +0200

    Fix inc

--- test/unit/module/core/inc/regular/inc.hpp
+++ test/unit/module/core/inc/regular/inc.hpp
@@ -8,33 +8,30 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef INC_HPP
-#define INC_HPP
-
 #include <eve/function/inc.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/constant/mzero.hpp>
+#include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check inc return type") { TTS_EXPR_IS(eve::inc(Type(), Type()), Type); }
+TTS_CASE("Check eve::inc return type")
+{
+  TTS_EXPR_IS(eve::inc(Type()), (Type));
+}
 
 TTS_CASE("Check eve::inc behavior")
 {
-  TTS_EQUAL(eve::inc(Type{1}), Type(2));
-  TTS_EQUAL(eve::inc(Type{2}), Type(3));
+  TTS_EQUAL(eve::inc(Type(1)), (Type(2)));
+  TTS_EQUAL(eve::inc(Type(2)), (Type(3)));
 
-  if constexpr(std::is_signed_v<Type>)
+  if constexpr(std::is_signed_v<Value>)
   {
-    TTS_EQUAL(eve::inc(-Type(2)), -Type(1));
+    TTS_EQUAL(eve::inc(Type(-2)), (Type(-1)));
   }
-  if constexpr(std::is_floating_point_v<Type>)
+
+  if constexpr(std::is_floating_point_v<Value>)
   {
-    TTS_EQUAL(eve::inc(eve::Mzero<Type>()), Type(1));
-    TTS_EQUAL(eve::inc(eve::Zero<Type>()), Type(1));
+    TTS_EQUAL(eve::inc( eve::Mzero<Type>()), (Type(1)));
+    TTS_EQUAL(eve::inc( (Type(0))         ), (Type(1)));
   }
 }
-
-#endif
--- test/unit/module/core/inc/saturated/inc.hpp
+++ test/unit/module/core/inc/saturated/inc.hpp
@@ -8,38 +8,32 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef INC_HPP
-#define INC_HPP
-
 #include <eve/function/inc.hpp>
-#include <tts/tts.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/constant/mzero.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/valmax.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/mzero.hpp>
 #include <type_traits>
 
 TTS_CASE("Check inc return type")
 {
-  TTS_EXPR_IS(eve::saturated_(eve::inc)(Type()), Type);
-  TTS_EXPR_IS(eve::saturated_(eve::inc[eve::logical<Type>()])(Type()), Type);
+  TTS_EXPR_IS(eve::saturated_(eve::inc)(Type()), (Type));
 }
 
 TTS_CASE("Check saturated_(inc) behavior")
 {
   TTS_EQUAL(eve::saturated_(eve::inc)(eve::Valmax<Type>()), eve::Valmax<Type>());
-  TTS_EQUAL(eve::saturated_(eve::inc)(Type{1}), Type(2));
-  TTS_EQUAL(eve::saturated_(eve::inc)(Type{2}), Type(3));
-  if constexpr(std::is_signed_v<Type>)
+  TTS_EQUAL(eve::saturated_(eve::inc)(Type(1)), (Type(2)));
+  TTS_EQUAL(eve::saturated_(eve::inc)(Type(2)), (Type(3)));
+
+if constexpr(std::is_signed_v<Value>)
   {
-    TTS_EQUAL(eve::saturated_(eve::inc)(static_cast<Type>(-2)), Type(-1));
+    TTS_EQUAL(eve::saturated_(eve::inc)(Type(-2)), (Type(-1)));
   }
-  if constexpr(std::is_floating_point_v<Type>)
+
+  if constexpr(std::is_floating_point_v<Value>)
   {
-    TTS_EQUAL(eve::saturated_(eve::inc)(eve::Mzero<Type>()), Type(1));
-    TTS_EQUAL(eve::saturated_(eve::inc)(eve::Zero<Type>()), Type(1));
+    TTS_EQUAL(eve::saturated_(eve::inc)( eve::Mzero<Type>()), (Type(1)));
+    TTS_EQUAL(eve::saturated_(eve::inc)( (Type(0))         ), (Type(1)));
   }
 }
-
-#endif
