05ec0accbf978607e1495f6ffcf2b8b80d3a8362
Joel Falcou <joel.falcou@lri.fr>
2019-10-25 14:46:26 +0200

    Fix signnz

--- test/unit/module/core/signnz/regular/signnz.hpp
+++ test/unit/module/core/signnz/regular/signnz.hpp
@@ -8,21 +8,22 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SIGNNZ_HPP
-#define SIGNNZ_HPP
-
-#include "test.hpp"
 #include <eve/function/signnz.hpp>
-#include <tts/tests/basic.hpp>
+#include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 
-TTS_CASE("Check signnz return type") { TTS_EXPR_IS(eve::signnz(Type()), Type); }
+TTS_CASE("Check eve::signnz return type")
+{
+  TTS_EXPR_IS(eve::signnz(Type()), (Type));
+}
 
 TTS_CASE("Check eve::signnz behavior")
 {
   TTS_EQUAL(eve::signnz(Type{0}), Type(1));
   TTS_EQUAL(eve::signnz(Type{2}), Type(1));
-  if constexpr(std::is_signed_v<Type>) { TTS_EQUAL(eve::signnz(static_cast<Type>(-2)), Type(-1)); }
-}
 
-#endif
+  if constexpr(std::is_signed_v<Value>)
+  {
+    TTS_EQUAL(eve::signnz(static_cast<Type>(-2)), (Type(-1)));
+  }
+}
