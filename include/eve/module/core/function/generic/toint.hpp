d19d34d50b0e4f23f8f470434eccba0ddf060974
Joel Falcou <joel.falcou@lri.fr>
2019-10-25 15:16:30 +0200

    Fix toint

--- include/eve/module/core/function/generic/toint.hpp
+++ include/eve/module/core/function/generic/toint.hpp
@@ -1,5 +1,5 @@
 //==================================================================================================
-/** 
+/**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
   Copyright 2019 Jean-Thierry LAPRESTE
@@ -13,28 +13,34 @@
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/pedantic.hpp>        
-#include <eve/function/saturated.hpp>
 #include <eve/function/convert.hpp>
-#include <eve/detail/meta.hpp>
 
 namespace eve::detail
 {
   template<typename T>
-  EVE_FORCEINLINE constexpr auto toint_(EVE_SUPPORTS(cpu_)
-                                       , T const &a) noexcept
-  requires(as_integer_t<T>, behave_as<floating_point,T>)
+  EVE_FORCEINLINE constexpr auto toint_(EVE_SUPPORTS(cpu_), T const &a) noexcept
   {
-    return convert(a, as_<as_integer_t<value_type_t<T>>>()); ;
+    if constexpr( std::is_integral_v< value_type_t<T> > )
+    {
+      return a;
+    }
+    else
+    {
+      return convert(a, as_<as_integer_t<value_type_t<T>>>());
+    }
   }
-  
+
   template<typename T, typename Tag>
-  EVE_FORCEINLINE constexpr auto toint_(EVE_SUPPORTS(cpu_)
-                                       , Tag const& tag_
-                                       , T const &a) noexcept
-  requires(as_integer_t<T>, behave_as<floating_point,T>)
+  EVE_FORCEINLINE constexpr auto toint_(EVE_SUPPORTS(cpu_), Tag const& tag_, T const &a) noexcept
   {
-    return convert(tag_, a, as_<as_integer_t<value_type_t<T>>>()); ;
+    if constexpr( std::is_integral_v< value_type_t<T> > )
+    {
+      return a;
+    }
+    else
+    {
+      return convert(tag_, a, as_<as_integer_t<value_type_t<T>>>());
+    }
   }
 }
 
