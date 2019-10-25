d55ec0c66458f3ecdd9eff75341c1f35543e11b6
jtlap <jtlapreste@gmail.com>
2019-10-25 11:16:41 +0200

    cbrt aggregated and emulated

--- include/eve/module/core/function/simd/common/cbrt.hpp
+++ include/eve/module/core/function/simd/common/cbrt.hpp
@@ -36,26 +36,30 @@ namespace eve::detail
                                       , T x) noexcept
   requires(T, vectorized<T>, behave_as<floating_point, T>)
   {
-    
-    constexpr double cbtr2 = 1.2599210498948731648;   /* 2^(1/3) */
-    constexpr double sqr_cbtr2 = 1.5874010519681994748;   /* 2^(2/3) */
-    constexpr value_type_t<T> factor[5] =
-      {
-        1.0 / sqr_cbtr2,
-        1.0 / cbtr2,
-        1.0,
-        cbtr2,
-        sqr_cbtr2
-      };
-    
-    /* Reduce X.  XM now is an range  [0.5, 1.0].  */
-    auto [xm, xe] = raw_(ifrexp)(eve::abs (x));
-    T u = cbrt_kernel(xm); 
-    u *= gather(&factor[0], 2 + xe-(xe/3)*3);
-    u = unary_minus[is_ltz(x)](u); 
-    return if_else(is_eqz(x) || is_not_finite(x), x, eve::ldexp(u, xe/3)); 
+   using t_abi = abi_type_t<T>;
+   if constexpr(is_emulated_v<t_abi> ) return map(eve::cbrt, x); 
+   else if constexpr(is_aggregated_v<t_abi> ) return aggregate(eve::cbrt, x);
+   else
+   {    
+     constexpr double cbtr2 = 1.2599210498948731648;   /* 2^(1/3) */
+     constexpr double sqr_cbtr2 = 1.5874010519681994748;   /* 2^(2/3) */
+     constexpr value_type_t<T> factor[5] =
+       {
+         1.0 / sqr_cbtr2,
+         1.0 / cbtr2,
+         1.0,
+         cbtr2,
+         sqr_cbtr2
+       };
+     
+     /* Reduce X.  XM now is an range  [0.5, 1.0].  */
+     auto [xm, xe] = raw_(ifrexp)(eve::abs (x));
+     T u = cbrt_kernel(xm); 
+     u *= gather(&factor[0], 2 + xe-(xe/3)*3);
+     u = unary_minus[is_ltz(x)](u); 
+     return if_else(is_eqz(x) || is_not_finite(x), x, eve::ldexp(u, xe/3)); 
+   }
   }
-
 }
 
 #endif
