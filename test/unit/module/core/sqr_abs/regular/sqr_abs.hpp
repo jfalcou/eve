01d151792db0409f33c4e79cbf08779d40f3a7bd
Joel Falcou <joel.falcou@lri.fr>
2019-10-25 14:21:44 +0200

    Fix sqr_abs

--- test/unit/module/core/sqr_abs/regular/sqr_abs.hpp
+++ test/unit/module/core/sqr_abs/regular/sqr_abs.hpp
@@ -8,32 +8,21 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SQR_ABS_HPP
-#define SQR_ABS_HPP
-
 #include <eve/function/sqr_abs.hpp>
-#include <tts/tts.hpp>
+#include <eve/constant/mzero.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/false.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/function/inc.hpp>
-#include <eve/as_logical.hpp>
-#include <type_traits>
 
-TTS_CASE("Check sqr_abs return type") { TTS_EXPR_IS(eve::sqr_abs(Type(0)), Type); }
+TTS_CASE("Check eve::sqr_abs return type")
+{
+  TTS_EXPR_IS(eve::sqr_abs(Type(0)), (Type));
+}
 
 TTS_CASE("Check eve::sqr_abs behavior")
 {
-  TTS_EQUAL(eve::sqr_abs(Type{1}), Type(1));
-  TTS_EQUAL(eve::sqr_abs(Type{2}), Type(4));
-
-  TTS_EQUAL(eve::sqr_abs(static_cast<Type>(-2)), Type(4));
-  TTS_EQUAL(eve::sqr_abs(eve::Mzero<Type>()), Type(0));
-  TTS_EQUAL(eve::sqr_abs(eve::Zero<Type>()), Type(0));
+  TTS_EQUAL(eve::sqr_abs(Type(1))               , (Type(1)) );
+  TTS_EQUAL(eve::sqr_abs(Type(2))               , (Type(4)) );
+  TTS_EQUAL(eve::sqr_abs(eve::Mzero<Type>())    , (Type(0)) );
+  TTS_EQUAL(eve::sqr_abs(static_cast<Type>(-2)) , (Type(4)) );
 }
-
-#endif
-
