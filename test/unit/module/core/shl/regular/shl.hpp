eb701bedeceb82781536dfc5cd384d7c336b8c98
Joel Falcou <joel.falcou@lri.fr>
2019-10-25 14:59:23 +0200

    Fix shl/shr

--- test/unit/module/core/shl/regular/shl.hpp
+++ test/unit/module/core/shl/regular/shl.hpp
@@ -8,37 +8,36 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SHL_HPP
-#define SHL_HPP
-
 #include <eve/function/shl.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/one.hpp>
 #include <eve/constant/allbits.hpp>
-#include <eve/as_logical.hpp>
 #include <type_traits>
 
-TTS_CASE("Check shl return type")
+TTS_CASE("Check eve::shl return type")
 {
   using i_t = eve::detail::as_integer_t<Type, signed>;
   using u_t = eve::detail::as_integer_t<Type, unsigned>;
-  TTS_EXPR_IS(eve::shl(Type(), Type()), Type);
-  TTS_EXPR_IS(eve::shl(Type(), i_t()), Type);
-  TTS_EXPR_IS(eve::shl(Type(), u_t()), Type);
-  TTS_EXPR_IS(eve::shl(i_t(), Type()), i_t);
-  TTS_EXPR_IS(eve::shl(u_t(), Type()), u_t);
+
+  TTS_EXPR_IS(eve::shl(Type(), Type()), (Type));
+  TTS_EXPR_IS(eve::shl(Type(), i_t() ), (Type));
+  TTS_EXPR_IS(eve::shl(Type(), u_t() ), (Type));
 }
 
 TTS_CASE("Check eve::shl behavior")
 {
-  TTS_EQUAL(eve::shl(eve::One<Type>(), 4), Type(16));
-  TTS_EQUAL(eve::shl(Type(3), 2), Type(12));
-  TTS_EQUAL(eve::shl(eve::Allbits<Type>(), 0), eve::Allbits<Type>());
-  TTS_EQUAL(eve::shl(eve::Zero<Type>(), 3), eve::Zero<Type>());
-  TTS_EQUAL(eve::shl(Type(8), -2), Type(2));
-}
+  TTS_EQUAL(eve::shl(Type(0), 7), (Type( 0)));
+  TTS_EQUAL(eve::shl(Type(1), 4), (Type(16)));
+  TTS_EQUAL(eve::shl(Type(3), 2), (Type(12)));
 
-#endif
+  if constexpr(std::is_signed_v<Value>)
+  {
+    TTS_EQUAL(eve::shl(Type(-1), 4), (Type(-16)));
+    TTS_EQUAL(eve::shl(Type(-3), 2), (Type(-12)));
+    TTS_EQUAL(eve::shl(eve::Allbits<Type>(), 1), Type(-2));
+  }
+  else
+  {
+    TTS_EQUAL(eve::shl(eve::Allbits<Type>(), 1), eve::Allbits<Type>() - 1 );
+  }
+}
