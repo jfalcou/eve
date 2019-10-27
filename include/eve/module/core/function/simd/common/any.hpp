37c6be5d5f20e880a306cd4d2b7f5c141e22771d
jtlap <jtlapreste@gmail.com>
2019-10-27 13:18:41 +0100

    any

--- include/eve/module/core/function/simd/common/any.hpp
+++ include/eve/module/core/function/simd/common/any.hpp
@@ -8,39 +8,45 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_ALL_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_ALL_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_ANY_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_ANY_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/is_nez.hpp>
+#include <eve/function/logical_or.hpp>
 #include <eve/function/extract.hpp>
 #include <eve/function/is_nez.hpp>
 #include <eve/logical.hpp>
+#include <type_traits>
 
 namespace eve::detail
 {
   template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(cpu_), logical<wide<T, N, ABI>> const &v) noexcept
+  EVE_FORCEINLINE bool any_(EVE_SUPPORTS(cpu_)
+                           , logical<wide<T, N, ABI>> const &v) noexcept
   {
     if constexpr(is_aggregated_v<ABI>)
     {
-      return v.storage().apply( [](auto const&... e) { return eve::all((e.bits() & ...)); } );
+     
+      auto [ sl, sh] = v.slice(); 
+      return any(sl) || any(sh);
     }
-    else
+    else 
     {
       for(int i=0; i < N::value ; ++i)
       {
-        if (!extract(v, i)) return false;
+        if (extract(v, i)) return true; 
       }
-      return true;
+      return false; 
     }
   }
 
   template<typename T, typename N, typename ABI>
-  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(cpu_), wide<T, N, ABI> const &v) noexcept
+  EVE_FORCEINLINE bool any_(EVE_SUPPORTS(cpu_)
+                           , wide<T, N, ABI> const &v) noexcept
   {
-    return all(is_nez(v));
+    return any(is_nez(v)); 
   }
 }
 
--- test/unit/module/core/simd/any/uint32.cpp
+++ test/unit/module/core/simd/any/uint32.cpp
@@ -1,17 +1,13 @@
 //==================================================================================================
 /**
   EVE - Expressive Vector Engine
-  Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_ANY_HPP_INCLUDED
-#define EVE_FUNCTION_ANY_HPP_INCLUDED
 
-#include <eve/function/scalar/any.hpp>
-#include <eve/function/simd/any.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::uint32_t;
+#include "any.hpp"
