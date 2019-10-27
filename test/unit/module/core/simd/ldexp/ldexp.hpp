03febe7e7cc7d784cdd097b714b38adaa4e1bccf
jtlap <jtlapreste@gmail.com>
2019-10-27 22:05:26 +0100

    csch

--- test/unit/module/core/simd/ldexp/ldexp.hpp
+++ test/unit/module/core/simd/ldexp/ldexp.hpp
@@ -12,7 +12,15 @@
 
 #include "test.hpp"
 #include <tts/tests/relation.hpp>
+<<<<<<< HEAD:test/unit/module/core/simd/ldexp/ldexp.hpp
 #include <eve/function/ldexp.hpp>
+=======
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
+#include <eve/function/all.hpp>
+#include <eve/function/unary_minus.hpp>
+#include <eve/function/is_negative.hpp>
+>>>>>>> csch:test/unit/module/core/simd/unary_minus/unary_minus.hpp
 #include <eve/wide.hpp>
 
 using eve::fixed;
@@ -27,10 +35,24 @@ TTS_CASE_TPL("Check plus behavior on wide",
              fixed<64>)
 {
   using eve::wide;
+<<<<<<< HEAD:test/unit/module/core/simd/ldexp/ldexp.hpp
   
   wide<Type, T> lhs([](auto i, auto) { return i; }), rhs([](auto i, auto c) { return Type(i); }),
     ref([](auto i, auto) { return  eve::ldexp(Type(i), Type(i)); });
   TTS_EQUAL(ref, eve::ldexp(lhs, rhs)); 
+=======
+  using t_t =  wide<Type, T>; 
+  t_t lhs([](int i, int) { return i + 1; }),
+      ref([](int i, int) { return eve::unary_minus(Type(i + 1)); });
+
+  TTS_EQUAL(ref, eve::unary_minus(lhs));
+  TTS_EQUAL(ref, -lhs);
+  if constexpr(std::is_floating_point_v<T>)
+  {
+    TTS_EXPECT(eve::all(eve::is_negative(eve::unary_minus(t_t(0)))));
+    TTS_EXPECT(eve::all(eve::is_negative(-t_t(0))));
+  }
+>>>>>>> csch:test/unit/module/core/simd/unary_minus/unary_minus.hpp
 }
 
 TTS_CASE_TPL("Check plus behavior on wide",
