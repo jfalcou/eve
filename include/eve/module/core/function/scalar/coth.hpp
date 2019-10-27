8ee40b8c9d96f593e662692a63a8ca76dd205bed
jtlap <jtlapreste@gmail.com>
2019-10-27 09:09:23 +0100

    tanh coth

---
+++ include/eve/module/core/function/scalar/coth.hpp
@@ -0,0 +1,38 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_COTH_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_COTH_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/function/copysign.hpp>
+#include <eve/function/expm1.hpp>
+#include <eve/function/fma.hpp>
+#include <eve/function/rec.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  // Regular case
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto coth_(EVE_SUPPORTS(cpu_)
+                                     , T a0) noexcept
+  requires(T, floating_point<T>)
+  {
+    auto x = eve::abs(a0+a0);
+    auto t = rec(expm1(x));
+    auto r = fma(Type(2), t, Type(1)); 
+    return copysign(r, a0); 
+  }
+}
+
+#endif
---
+++ test/unit/module/core/simd/tanh/tanh.hpp
@@ -0,0 +1,62 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef TANH_HPP
+#define TANH_HPP
+
+#include <eve/function/tanh.hpp>
+#include <eve/wide.hpp>
+#include "test.hpp"
+#include <tts/tests/relation.hpp>
+#include <tts/tests/precision.hpp>
+#include <tts/tests/types.hpp>
+#include <eve/constant/nan.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/zero.hpp>
+#include <eve/constant/maxlog.hpp>
+#include <eve/constant/minlog.hpp>
+#include <eve/platform.hpp>
+#include <cmath>
+#include <iomanip>
+
+using eve::fixed;
+TTS_CASE_TPL("Check tanh behavior on wide",
+             fixed<1>,
+             fixed<2>,
+             fixed<4>,
+             fixed<8>,
+             fixed<16>)
+{
+  using t_t =  eve::wide<Type, T>; 
+  TTS_EXPR_IS ( eve::tanh(t_t(0)), t_t);
+  
+  TTS_ULP_EQUAL(eve::tanh(t_t(1)), t_t(std::tanh(Type(1))), 0.5);
+  TTS_ULP_EQUAL(eve::tanh(t_t(2)), t_t(std::tanh(Type(2))), 0.5);
+  TTS_ULP_EQUAL(eve::tanh(t_t(-2)),t_t(std::tanh(Type(-2))), 0.5);
+  TTS_ULP_EQUAL(eve::tanh(t_t(-1)),t_t(std::tanh(Type(-1))), 0.5);
+  TTS_IEEE_EQUAL(eve::tanh(t_t(0.0)), t_t(Type(0)));
+  TTS_IEEE_EQUAL(eve::tanh(t_t(-0.0)), t_t(Type(0)));
+  if constexpr(eve::platform::supports_invalids)
+  {
+    TTS_IEEE_EQUAL(eve::tanh(eve::Nan<t_t>()), eve::Nan<t_t>()); 
+    TTS_IEEE_EQUAL(eve::tanh(eve::Inf<t_t>()), t_t(1));
+    TTS_IEEE_EQUAL(eve::tanh(eve::Minf<t_t>()), t_t(-1));
+  }
+  
+  for(int i=1; i < eve::Maxlog<Type>(); i *= 3)
+  {
+    TTS_ULP_EQUAL(eve::tanh(t_t(i)), t_t(std::tanh(Type(i))), 1);
+    TTS_ULP_EQUAL(eve::tanh(-t_t(i)), t_t(std::tanh(Type(-i))), 1); 
+    TTS_ULP_EQUAL(eve::tanh(1/t_t(i)), t_t(std::tanh(1/Type(i))), 1);
+    TTS_ULP_EQUAL(eve::tanh(-1/t_t(i)), t_t(std::tanh(1/Type(-i))), 1); 
+  }
+}
+
+#endif
