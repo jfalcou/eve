10ee1b7b71d6bf64046bde8eb013fe782b2cc2dc
jtlap <jtlapreste@gmail.com>
2019-10-27 14:22:26 +0100

    nbtrue

---
+++ include/eve/module/core/function/simd/common/nbtrue.hpp
@@ -0,0 +1,54 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_NBTRUE_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_COMMON_NBTRUE_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/skeleton.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/logical_or.hpp>
+#include <eve/function/extract.hpp>
+#include <eve/function/is_nez.hpp>
+#include <eve/logical.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE size_t nbtrue_(EVE_SUPPORTS(cpu_)
+                           , logical<wide<T, N, ABI>> const &v) noexcept
+  {
+    if constexpr(is_aggregated_v<ABI>)
+    {
+     
+      auto [ sl, sh] = v.slice(); 
+      return nbtrue(sl) + nbtrue(sh);
+    }
+    else 
+    {
+      size_t r = 0; 
+      for(int i=0; i < N::value ; ++i)
+      {
+        if (extract(v, i)) ++r; 
+      }
+      return r; 
+    }
+  }
+
+  template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE size_t nbtrue_(EVE_SUPPORTS(cpu_)
+                           , wide<T, N, ABI> const &v) noexcept
+  {
+    return nbtrue(is_nez(v)); 
+  }
+}
+
+#endif
--- test/unit/module/core/scalar/nbtrue/int8.cpp
+++ test/unit/module/core/scalar/nbtrue/int8.cpp
@@ -2,16 +2,13 @@
 /**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
+  Copyright 2019 Jean-Thierry Lapreste
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_NBTRUE_HPP_INCLUDED
-#define EVE_FUNCTION_NBTRUE_HPP_INCLUDED
 
-#include <eve/function/scalar/nbtrue.hpp>
-#include <eve/function/simd/nbtrue.hpp>
-
-#endif
+#include <cstdint>
+using Type = std::int8_t;
+#include "nbtrue.hpp"
