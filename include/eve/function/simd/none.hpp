979a81e6e451570db26f90ae8c36934f857c65cd
jtlap <jtlapreste@gmail.com>
2019-10-27 13:34:17 +0100

    none

--- include/eve/function/simd/none.hpp
+++ include/eve/function/simd/none.hpp
@@ -8,10 +8,10 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_NONE_HPP_INCLUDED
-#define EVE_FUNCTION_NONE_HPP_INCLUDED
+#ifndef EVE_FUNCTION_SIMD_NONE_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_NONE_HPP_INCLUDED
 
 #include <eve/function/scalar/none.hpp>
-#include <eve/function/simd/none.hpp>
+#include <eve/module/core/function/simd/none.hpp>
 
 #endif
