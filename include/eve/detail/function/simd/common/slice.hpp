d4e01e429d2fcd5bce0eb363333bcbeb10694e2c
Joel Falcou <joel.falcou@lri.fr>
2019-10-28 14:36:15 +0100

    Reimplement slice

--- include/eve/detail/function/simd/common/slice.hpp
+++ include/eve/detail/function/simd/common/slice.hpp
@@ -28,31 +28,125 @@ namespace eve
   {
   };
 
-  inline constexpr upper_slice upper_ = {};
-  inline constexpr lower_slice lower_ = {};
+  inline constexpr upper_slice const upper_ = {};
+  inline constexpr lower_slice const lower_ = {};
 }
 
 namespace eve::detail
 {
   //------------------------------------------------------------------------------------------------
-  // Logical
-  template<typename T, typename N, typename Arch>
-  EVE_FORCEINLINE std::array<logical<wide<T, typename N::split_type>>, 2>
-                  slice(logical<wide<T, N, Arch>> const &a) noexcept
+  // Full slice
+  template<typename P>
+  EVE_FORCEINLINE auto slice_impl(P const &a) noexcept
   {
-    using l_t     = logical<wide<T, typename N::split_type>>;
-    using t_t     = std::array<l_t, 2>;
-    auto [ l, h ] = a.bits().slice();
-    return t_t{l_t(l.storage(), eve::from_bits), l_t(h.storage(), eve::from_bits)};
+    using abi_t   = typename P::abi_type;
+    using card_t  = typename P::cardinal_type;
+    using value_t = typename P::value_type;
+
+    using sub_t = as_wide_t<value_t, typename card_t::split_type>;
+
+    if constexpr(is_emulated_v<abi_t>)
+    {
+      auto eval = [&](auto... I)
+      {
+        using that_t = std::array<sub_t, 2>;
+        return that_t{sub_t{a[ I ]...}, sub_t{a[ I + card_t::value / 2 ]...}};
+      };
+
+      return apply<card_t::value / 2>(eval);
+    }
+    else if constexpr(is_aggregated_v<abi_t>)
+    {
+      std::array<sub_t, 2> that{ a.slice(lower_), a.slice(upper_)};
+      return that;
+    }
+    else
+    {
+      static_assert(wrong<abi_t>, "[eve::slice(a) - Unsupported SIMD ABI");
+    }
   }
 
-  template<typename T, typename N, typename Arch, typename Slice>
-  EVE_FORCEINLINE logical<wide<T, typename N::split_type>> slice(logical<wide<T, N, Arch>> const &a,
-                                                                 Slice const &s) noexcept
+  //------------------------------------------------------------------------------------------------
+  // Partial slice
+  template<typename P, typename Slice>
+  EVE_FORCEINLINE auto slice_impl(P const &a, Slice const &) noexcept
   {
-    using l_t = logical<wide<T, typename N::split_type>>;
-    auto sa   = a.bits().slice(s);
-    return l_t(sa.storage(), eve::from_bits);
+    using abi_t   = typename P::abi_type;
+    using card_t  = typename P::cardinal_type;
+    using value_t = typename P::value_type;
+
+    using sub_t = as_wide_t<value_t, typename card_t::split_type>;
+
+    if constexpr(is_emulated_v<abi_t>)
+    {
+      auto eval = [&](auto... I)
+      {
+        return sub_t{a[ I + (Slice::value * sub_t::static_size) ]...};
+      };
+
+      return apply<card_t::value / 2>(eval);
+    }
+    else if constexpr(is_aggregated_v<abi_t>)
+    {
+      static constexpr auto reps = P::storage_type::replication;
+
+      if constexpr(reps == 2)
+      {
+        return a.storage().segments[Slice::value];
+      }
+      else
+      {
+        sub_t that;
+        static constexpr auto subreps = sub_t::storage_type::replication;
+        static constexpr auto offset  = Slice::value*subreps;
+
+        auto const& out = a.storage().segments;
+        detail::apply<subreps>( [&](auto const&... I)
+                                {
+                                  that.storage().segments = { out[I + offset]... };
+                                }
+                              );
+
+        return that;
+      }
+    }
+    else
+    {
+      static_assert(wrong<abi_t>, "[eve::slice(a,s) - Unsupported SIMD ABI");
+    }
+  }
+
+  //------------------------------------------------------------------------------------------------
+  // Mapping over wide-like type
+template<typename T, typename N, typename ABI>
+  EVE_FORCEINLINE std::array<logical<wide<T, typename N::split_type>>, 2>
+                  slice(logical<wide<T, N, ABI>> const &a) noexcept
+  {
+    if constexpr( is_native_v<ABI> )
+    {
+      using l_t     = logical<wide<T, typename N::split_type>>;
+      using t_t     = std::array<l_t, 2>;
+      auto [ l, h ] = a.bits().slice();
+      return t_t{l_t(l.storage(), eve::from_bits), l_t(h.storage(), eve::from_bits)};
+    }
+    else
+    {
+      return slice_impl(a);
+    }
+  }
+
+  template<typename T, typename N, typename ABI, typename Slice>
+  EVE_FORCEINLINE auto slice(logical<wide<T, N, ABI>> const &a, Slice const & s) noexcept
+  {
+    if constexpr( is_native_v<ABI> )
+    {
+      using l_t = logical<wide<T, typename N::split_type>>;
+      return l_t(a.bits().slice(s).storage(), eve::from_bits);
+    }
+    else
+    {
+      return slice_impl(a,s);
+    }
   }
 
   //------------------------------------------------------------------------------------------------
@@ -60,63 +154,15 @@ namespace eve::detail
   template<typename T, typename N, typename ABI>
   EVE_FORCEINLINE auto slice(wide<T, N, ABI> const &a) noexcept
   {
-    if constexpr(is_emulated_v<ABI>)
-    {
-      auto eval = [&](auto... I) {
-        using wide_t = wide<T, typename N::split_type>;
-        using that_t = std::array<wide_t, 2>;
-        return that_t{wide_t{a[ I ]...}, wide_t{a[ I + N::value / 2 ]...}};
-      };
-
-      return apply<N::value / 2>(eval);
-    }
-    else if constexpr(is_aggregated_v<ABI>)
-    {
-      if constexpr(platform::compiler == compilers::gcc_ && sizeof(a) > 256)
-      {
-        auto eval = [&](auto... I) {
-          using wide_t = wide<T, typename N::split_type>;
-          using that_t = std::array<wide_t, 2>;
-          return that_t{wide_t{a[ I ]...}, wide_t{a[ I + N::value / 2 ]...}};
-        };
-
-        return apply<N::value / 2>(eval);
-      }
-      else
-      {
-        return a.storage();
-      }
-    }
-    else
-    {
-      static_assert(wrong<ABI>, "[eve::slice(a) - Unsupported SIMD ABI");
-    }
+    return slice_impl(a);
   }
 
   //------------------------------------------------------------------------------------------------
   // Arithmetic partial slice
   template<typename T, typename N, typename ABI, typename Slice>
-  EVE_FORCEINLINE auto slice(wide<T, N, ABI> const &a, Slice const &) noexcept
+  EVE_FORCEINLINE auto slice(wide<T, N, ABI> const &a, Slice const & s) noexcept
   {
-    if constexpr(is_emulated_v<ABI>)
-    {
-      auto eval = [&](auto... I) {
-        return wide<T, typename N::split_type>{a[ I + (Slice::value * N::value / 2) ]...};
-      };
-
-      return apply<N::value / 2>(eval);
-    }
-    else if constexpr(is_aggregated_v<ABI>)
-    {
-      if constexpr(Slice::value == 0)
-        return a.storage().lo;
-      else
-        return a.storage().hi;
-    }
-    else
-    {
-      static_assert(wrong<ABI>, "[eve::slice(a,s) - Unsupported SIMD ABI");
-    }
+    return slice_impl(a,s);
   }
 }
 
