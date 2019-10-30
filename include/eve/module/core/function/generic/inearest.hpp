40b71a673c7d175c8746e9b3e56ccbcdded5bace
jtlap <jtlapreste@gmail.com>
2019-10-30 09:58:20 +0100

    inearest

--- include/eve/module/core/function/generic/inearest.hpp
+++ include/eve/module/core/function/generic/inearest.hpp
@@ -8,13 +8,13 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_ICEIL_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_GENERIC_ICEIL_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_INEAREST_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_INEAREST_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/meta.hpp>
 #include <eve/detail/abi.hpp>
-#include <eve/function/ceil.hpp>
+#include <eve/function/nearest.hpp>
 #include <eve/function/toint.hpp>
 #include <type_traits>
 
@@ -23,12 +23,12 @@ namespace eve::detail
   ////////////////////////////////////////////////
   // saturated_
   template<typename T>
-  EVE_FORCEINLINE constexpr auto iceil_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE constexpr auto inearest_(EVE_SUPPORTS(cpu_)
                                       , T const &a) noexcept
   {
     if constexpr(std::is_floating_point_v<value_type_t<T>>)
     {
-      return saturated_(toint)(eve::ceil(a));
+      return saturated_(toint)(eve::nearest(a));
 
     }
     else // if constexpr(std::is_integral_v<value_type_t<T>>)
