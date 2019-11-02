b5b903a4fbb5ba0b35864b390182f91b630b558c
Joel Falcou <joel.falcou@lri.fr>
2019-11-02 21:31:24 +0100

    More cleanup

--- test/unit/module/core/rsqrt/regular/rsqrt.hpp
+++ test/unit/module/core/rsqrt/regular/rsqrt.hpp
@@ -8,37 +8,31 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef RSQRT_HPP
-#define RSQRT_HPP
-
 #include <eve/function/rsqrt.hpp>
-#include <tts/tts.hpp>
+#include <eve/constant/mzero.hpp>
+#include <eve/constant/nan.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/half.hpp>
-#include <eve/platform.hpp>
 #include <type_traits>
 
-TTS_CASE("Check rsqrt return type") { TTS_EXPR_IS(eve::rsqrt(Type(0)), Type); }
+TTS_CASE("Check eve::rsqrt return type")
+{
+  TTS_EXPR_IS(eve::rsqrt(Type(0)), (Type));
+}
 
 TTS_CASE("Check eve::rsqrt behavior")
 {
-  TTS_EQUAL(eve::rsqrt(Type{1}), Type(1));
-  TTS_EQUAL(eve::rsqrt(Type{4}), eve::Half<Type>());
+  TTS_ULP_EQUAL(eve::rsqrt(Type(1)), (Type(1  )), 0.5);
+  TTS_ULP_EQUAL(eve::rsqrt(Type(4)), (Type(0.5)), 0.5);
 
   if constexpr(std::is_floating_point_v<Type> && eve::platform::supports_invalids)
   {
-    TTS_IEEE_EQUAL(eve::rsqrt(eve::Nan<Type>()), eve::Nan<Type>());
-    TTS_IEEE_EQUAL(eve::rsqrt(-eve::Nan<Type>()), eve::Nan<Type>());
-    TTS_EQUAL(eve::rsqrt(eve::Mzero<Type>()), eve::Inf<Type>());
-    TTS_EQUAL(eve::rsqrt(eve::Zero<Type>()), eve::Inf<Type>());
+    TTS_IEEE_EQUAL((eve::rsqrt(eve::Nan<Type>())) , (eve::Nan<Type>()));
+    TTS_EQUAL(eve::rsqrt(eve::Mzero<Type>())      , eve::Inf<Type>());
+    TTS_EQUAL(eve::rsqrt((Type(0)))               , eve::Inf<Type>());
   }
 }
-
-#endif
 
