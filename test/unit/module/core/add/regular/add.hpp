41804c0184140dede7e10977877a9fa5e2dba505
jtlap <jtlapreste@gmail.com>
2019-11-03 18:53:29 +0100

    mixed add tests

--- test/unit/module/core/add/regular/add.hpp
+++ test/unit/module/core/add/regular/add.hpp
@@ -24,9 +24,23 @@ TTS_CASE("Check eve::add behavior")
   TTS_EQUAL(eve::add(Type{1}, Type{1}), Type(2));
   TTS_EQUAL(eve::add(Type{2}, Type{2}), Type(4));
 
+  TTS_EQUAL(eve::add(Value{0}, Type{0}), Type(0));
+  TTS_EQUAL(eve::add(Value{1}, Type{1}), Type(2));
+  TTS_EQUAL(eve::add(Value{2}, Type{2}), Type(4));
+
+  TTS_EQUAL(eve::add(Type{0}, Value{0}), Type(0));
+  TTS_EQUAL(eve::add(Type{1}, Value{1}), Type(2));
+  TTS_EQUAL(eve::add(Type{2}, Value{2}), Type(4));
+
   if constexpr(std::is_signed_v<Value>)
   {
     TTS_EQUAL(eve::add(Type(-1), Type(1)), Type(0));
     TTS_EQUAL(eve::add(Type(-2), Type(-6)), Type(-8));
+
+    TTS_EQUAL(eve::add(Value(-1), Type(1)), Type(0));
+    TTS_EQUAL(eve::add(Value(-2), Type(-6)), Type(-8));
+
+    TTS_EQUAL(eve::add(Type(-1), Value(1)), Type(0));
+    TTS_EQUAL(eve::add(Type(-2), Value(-6)), Type(-8));
   }
 }
