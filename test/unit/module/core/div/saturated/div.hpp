6e9b41f768a3e85b653cd2ebc41770243246e229
jtlap <jtlapreste@gmail.com>
2019-11-03 19:51:46 +0100

    adding div mixed tests and correcting simd saturation

--- test/unit/module/core/div/saturated/div.hpp
+++ test/unit/module/core/div/saturated/div.hpp
@@ -26,26 +26,56 @@ TTS_CASE("Check saturated_(eve::div) behavior")
 
   if constexpr(std::is_integral_v<Value>)
   {
-    auto vmax = eve::Valmax<Type>();
-    auto vmin = eve::Valmin<Type>();
+    auto tvmax = eve::Valmax<Type>();
+    auto tvmin = eve::Valmin<Type>();
+    auto vmax = eve::Valmax<Value>();
+    auto vmin = eve::Valmin<Value>();
 
     if constexpr(std::is_signed_v<Value>)
     {
-      TTS_EQUAL(saturated_(eve::div)(vmax       , (Type(-1))), vmin+(Type(1)));
-      TTS_EQUAL(saturated_(eve::div)(vmin       , (Type(-1))), vmax);
-      TTS_EQUAL(saturated_(eve::div)((Type(-1)) , (Type( 0))), vmin);
-      TTS_EQUAL(saturated_(eve::div)((Type(-2)) , (Type( 0))), vmin);
-      TTS_EQUAL(saturated_(eve::div)(vmin       , (Type( 0))), vmin);
-      TTS_EQUAL(saturated_(eve::div)((Type(1))  , (Type( 0))), vmax);
-      TTS_EQUAL(saturated_(eve::div)((Type(2))  , (Type( 0))), vmax);
-      TTS_EQUAL(saturated_(eve::div)(vmax       , (Type( 0))), vmax);
-    }
+      TTS_EQUAL(saturated_(eve::div)(tvmax       , (Type(-1))), tvmin+(Type(1)));
+      TTS_EQUAL(saturated_(eve::div)(tvmin       , (Type(-1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(-1))  , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Type(-2) ) , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)(tvmin       , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Type(1))   , (Type( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(2))   , (Type( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(tvmax       , (Type( 0))), tvmax);
+
+      TTS_EQUAL(saturated_(eve::div)(tvmax       , (Value(-1))), tvmin+(Type(1)));
+      TTS_EQUAL(saturated_(eve::div)(tvmin       , (Value(-1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(-1))  , (Value( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Type(-2))  , (Value( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)(tvmin       , (Value( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Type(1))   , (Value( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(2))   , (Value( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(tvmax       , (Value( 0))), tvmax);
+
+      TTS_EQUAL(saturated_(eve::div)(vmax        , (Type(-1))), tvmin+(Type(1)));
+      TTS_EQUAL(saturated_(eve::div)(vmin        , (Type(-1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Value(-1)) , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Value(-2)) , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)(vmin        , (Type( 0))), tvmin);
+      TTS_EQUAL(saturated_(eve::div)((Value(1))  , (Type( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Value(2))  , (Type( 0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(vmax        , (Type( 0))), tvmax);
+     }
     else
     {
-      TTS_EQUAL(saturated_(eve::div)(vmax     , (Type(1))), vmax);
-      TTS_EQUAL(saturated_(eve::div)((Type(1)), (Type(0))), vmax);
-      TTS_EQUAL(saturated_(eve::div)((Type(2)), (Type(0))), vmax);
-      TTS_EQUAL(saturated_(eve::div)(vmax     , (Type(0))), vmax);
+      TTS_EQUAL(saturated_(eve::div)(tvmax    , (Type(1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(1)), (Type(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(2)), (Type(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(tvmax    , (Type(0))), tvmax);
+
+      TTS_EQUAL(saturated_(eve::div)(vmax      , (Type(1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Value(1)), (Type(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Value(2)), (Type(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(vmax      , (Type(0))), tvmax);
+
+     TTS_EQUAL(saturated_(eve::div)(tvmax     , (Value(1))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(1)) , (Value(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)((Type(2)) , (Value(0))), tvmax);
+      TTS_EQUAL(saturated_(eve::div)(tvmax     , (Value(0))), tvmax);
     }
   }
   else
@@ -54,5 +84,15 @@ TTS_CASE("Check saturated_(eve::div) behavior")
     TTS_EQUAL(saturated_(eve::div)(Type( 1) , Type(1)), Type(1  ) );
     TTS_EQUAL(saturated_(eve::div)(Type(12) , Type(4)), Type(3  ) );
     TTS_EQUAL(saturated_(eve::div)(Type( 1) , Type(2)), Type(0.5) );
+
+    TTS_EQUAL(saturated_(eve::div)(Value( 0) , Type(1)), Type(0  ) );
+    TTS_EQUAL(saturated_(eve::div)(Value( 1) , Type(1)), Type(1  ) );
+    TTS_EQUAL(saturated_(eve::div)(Value(12) , Type(4)), Type(3  ) );
+    TTS_EQUAL(saturated_(eve::div)(Value( 1) , Type(2)), Type(0.5) );
+
+    TTS_EQUAL(saturated_(eve::div)(Type( 0) , Value(1)), Type(0  ) );
+    TTS_EQUAL(saturated_(eve::div)(Type( 1) , Value(1)), Type(1  ) );
+    TTS_EQUAL(saturated_(eve::div)(Type(12) , Value(4)), Type(3  ) );
+    TTS_EQUAL(saturated_(eve::div)(Type( 1) , Value(2)), Type(0.5) );
   }
 }
