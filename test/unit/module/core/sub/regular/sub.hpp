e5026b1292563dfb037d60e58540bb144b65af79
jtlap <jtlapreste@gmail.com>
2019-11-04 13:54:49 +0100

    sub

--- test/unit/module/core/sub/regular/sub.hpp
+++ test/unit/module/core/sub/regular/sub.hpp
@@ -8,27 +8,39 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SUB_HPP
-#define SUB_HPP
-
 #include <eve/function/sub.hpp>
-#include <tts/tts.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 #include <type_traits>
 
-TTS_CASE("Check sub return type") { TTS_EXPR_IS(eve::sub(Type(), Type()), Type); }
+TTS_CASE("Check eve::sub return type")
+{
+  TTS_EXPR_IS(eve::sub(Type(), Type()), (Type));
+}
 
 TTS_CASE("Check eve::sub behavior")
 {
   TTS_EQUAL(eve::sub(Type{0}, Type{0}), Type(0));
   TTS_EQUAL(eve::sub(Type{1}, Type{1}), Type(0));
-  TTS_EQUAL(eve::sub(Type{2}, Type{2}), Type(0));
-  if constexpr(std::is_signed_v<Type>)
+  TTS_EQUAL(eve::sub(Type{6}, Type{2}), Type(4));
+
+  TTS_EQUAL(eve::sub(Value{0}, Type{0}), Type(0));
+  TTS_EQUAL(eve::sub(Value{1}, Type{1}), Type(0));
+  TTS_EQUAL(eve::sub(Value{6}, Type{2}), Type(4));
+
+  TTS_EQUAL(eve::sub(Type{0}, Value{0}), Type(0));
+  TTS_EQUAL(eve::sub(Type{1}, Value{1}), Type(0));
+  TTS_EQUAL(eve::sub(Type{6}, Value{2}), Type(4));
+
+  if constexpr(std::is_signed_v<Value>)
   {
     TTS_EQUAL(eve::sub(Type(-1), Type(1)), Type(-2));
     TTS_EQUAL(eve::sub(Type(-2), Type(-6)), Type(4));
+
+    TTS_EQUAL(eve::sub(Value(-1), Type(1)), Type(-2));
+    TTS_EQUAL(eve::sub(Value(-2), Type(-6)), Type(4));
+
+    TTS_EQUAL(eve::sub(Type(-1), Value(1)), Type(-2));
+    TTS_EQUAL(eve::sub(Type(-2), Value(-6)), Type(4));
   }
 }
-
-#endif
--- test/unit/module/core/sub/regular_if/sub.hpp
+++ test/unit/module/core/sub/regular_if/sub.hpp
@@ -8,40 +8,46 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SUB_HPP
-#define SUB_HPP
-
 #include <eve/function/sub.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
 #include <eve/constant/true.hpp>
 #include <eve/constant/false.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/logical.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 #include <type_traits>
+#include <algorithm>
 
-TTS_CASE("Check conditional sub return type")
+TTS_CASE("Check eve::sub[condition] return type")
 {
-  TTS_EXPR_IS(eve::sub[ Type() ](Type(), Type()), Type);
-  TTS_EXPR_IS(eve::sub[ eve::logical<Type>() ](Type(), Type()), Type);
-  TTS_EXPR_IS(eve::sub[ true ](Type(), Type()), Type);
+  TTS_EXPR_IS( (eve::sub[ Type() ](Type(), Type())), (Type));
+  TTS_EXPR_IS( (eve::sub[ eve::logical<Type>() ](Type(), Type())), (Type));
+  TTS_EXPR_IS( (eve::sub[ true ](Type(), Type())), (Type));
 }
 
-TTS_CASE("Check conditional sub behavior")
+TTS_CASE("Check eve::sub[condition] behavior")
 {
-  Type tv{2};
+  Type tv{5};
   Type fv{3};
   auto t = eve::True<Type>();
   auto f = eve::False<Type>();
 
-  TTS_EQUAL(eve::sub[ 1 ](tv, fv), tv - fv);
-  TTS_EQUAL(eve::sub[ 1.0 ](tv, fv), tv - fv);
-  TTS_EQUAL(eve::sub[ true ](tv, fv), tv - fv);
-  TTS_EQUAL(eve::sub[ t ](tv, fv), tv - fv);
-  TTS_EQUAL(eve::sub[ 0 ](tv, fv), tv);
-  TTS_EQUAL(eve::sub[ 0.0 ](tv, fv), tv);
-  TTS_EQUAL(eve::sub[ false ](tv, fv), tv);
-  TTS_EQUAL(eve::sub[ f ](tv, fv), tv);
-}
+  // All basic TRUE
+  TTS_EQUAL(eve::sub[ 1 ](tv, fv)     , eve::sub(tv,fv));
+  TTS_EQUAL(eve::sub[ 1.0 ](tv, fv)   , eve::sub(tv,fv));
+  TTS_EQUAL(eve::sub[ true ](tv, fv)  , eve::sub(tv,fv));
+  TTS_EQUAL(eve::sub[ t ](tv, fv)     , eve::sub(tv,fv));
 
-#endif
+  // All basic FALSE
+  TTS_EQUAL(eve::sub[ 0 ](tv, fv)     , tv);
+  TTS_EQUAL(eve::sub[ 0.0 ](tv, fv)   , tv);
+  TTS_EQUAL(eve::sub[ false ](tv, fv) , tv);
+  TTS_EQUAL(eve::sub[ f ](tv, fv)     , tv);
+
+  // Mixed case
+  eve::as_logical_t<Type> m;
+  std::for_each ( tts::detail::begin(m), tts::detail::end(m)
+                , [k = true](auto& e) mutable { e = k; k = !k; }
+                );
+
+  TTS_EQUAL(eve::sub[ m ](tv, fv) , eve::if_else(m,eve::sub(tv,fv), tv) );
+}
