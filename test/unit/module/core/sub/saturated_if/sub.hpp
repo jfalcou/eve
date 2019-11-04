cf7f22bcafe37a22f4da05f6c9abe5c1fc7cfacd
jtlap <jtlapreste@gmail.com>
2019-11-04 13:54:49 +0100

    sub

--- test/unit/module/core/sub/saturated_if/sub.hpp
+++ test/unit/module/core/sub/saturated_if/sub.hpp
@@ -8,27 +8,22 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef SUB_HPP
-#define SUB_HPP
-
 #include <eve/function/sub.hpp>
-#include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
 #include <eve/constant/true.hpp>
 #include <eve/constant/false.hpp>
 #include <eve/logical.hpp>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 #include <type_traits>
 
 TTS_CASE("Check sub return type")
 {
-  TTS_EXPR_IS(eve::saturated_(eve::sub[ Type() ])(Type(), Type()), Type);
-  TTS_EXPR_IS(eve::saturated_(eve::sub[ eve::logical<Type>() ])(Type(), Type()), Type);
-  TTS_EXPR_IS(eve::saturated_(eve::sub[ true ])(Type(), Type()), Type);
+  TTS_EXPR_IS( (eve::saturated_(eve::sub[ Type()              ])(Type(), Type())), (Type));
+  TTS_EXPR_IS( (eve::saturated_(eve::sub[ eve::logical<Type>()])(Type(), Type())), (Type));
+  TTS_EXPR_IS( (eve::saturated_(eve::sub[ true                ])(Type(), Type())), (Type));
 }
-
 
 TTS_CASE("Check saturated conditional sub behavior")
 {
@@ -37,14 +32,24 @@ TTS_CASE("Check saturated conditional sub behavior")
   auto t = eve::True<Type>();
   auto f = eve::False<Type>();
 
-  TTS_EQUAL(eve::saturated_(eve::sub[ 1 ])(tv, fv), eve::saturated_(eve::sub)(tv,fv));
-  TTS_EQUAL(eve::saturated_(eve::sub[ 1.0 ])(tv, fv), eve::saturated_(eve::sub)(tv,fv));
-  TTS_EQUAL(eve::saturated_(eve::sub[ true ])(tv, fv), eve::saturated_(eve::sub)(tv,fv));
-  TTS_EQUAL(eve::saturated_(eve::sub[ t ])(tv, fv), eve::saturated_(eve::sub)(tv,fv));
-  TTS_EQUAL(eve::saturated_(eve::sub[ 0 ])(tv, fv), tv);
-  TTS_EQUAL(eve::saturated_(eve::sub[ 0.0 ])(tv, fv), tv);
-  TTS_EQUAL(eve::saturated_(eve::sub[ false ])(tv, fv), tv);
-  TTS_EQUAL(eve::saturated_(eve::sub[ f ])(tv, fv), tv);
-}
+  TTS_EQUAL(eve::saturated_(eve::sub[ 1 ])(tv, fv)    , eve::saturated_(eve::sub)(tv,fv));
+  TTS_EQUAL(eve::saturated_(eve::sub[ 1.0 ])(tv, fv)  , eve::saturated_(eve::sub)(tv,fv));
+  TTS_EQUAL(eve::saturated_(eve::sub[ true ])(tv, fv) , eve::saturated_(eve::sub)(tv,fv));
+  TTS_EQUAL(eve::saturated_(eve::sub[ t ])(tv, fv)    , eve::saturated_(eve::sub)(tv,fv));
 
-#endif
+  TTS_EQUAL(eve::saturated_(eve::sub[ 0 ])(tv, fv)    , tv);
+  TTS_EQUAL(eve::saturated_(eve::sub[ 0.0 ])(tv, fv)  , tv);
+  TTS_EQUAL(eve::saturated_(eve::sub[ false ])(tv, fv), tv);
+  TTS_EQUAL(eve::saturated_(eve::sub[ f ])(tv, fv)    , tv);
+
+  // Mixed case
+  eve::as_logical_t<Type> m;
+  std::for_each ( tts::detail::begin(m), tts::detail::end(m)
+                , [k = true](auto& e) mutable { e = k; k = !k; }
+                );
+
+  TTS_EQUAL ( eve::saturated_(eve::sub[ m ])(tv, fv),
+                  eve::if_else(m, eve::saturated_(eve::sub)(tv,fv), tv)
+                );
+
+}
