c20071066b3d21f05f053414b83776a145362b46
jtlap <jtlapreste@gmail.com>
2019-11-06 16:38:27 +0100

    tanh tests

--- test/unit/module/core/tanh/regular/tanh.hpp
+++ test/unit/module/core/tanh/regular/tanh.hpp
@@ -8,7 +8,8 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#include <eve/function/sinh.hpp>
+#include <eve/function/tanh.hpp>
+#include <eve/function/rec.hpp>
 #include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/inf.hpp>
@@ -21,25 +22,23 @@
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
 
-TTS_CASE("Check eve::sinh return type")
+TTS_CASE("Check eve::tanh return type")
 {
-  TTS_EXPR_IS(eve::sinh(Type(0)), (Type));
+  TTS_EXPR_IS(eve::tanh(Type(0)), (Type));
 }
 
-TTS_CASE("Check eve::eve::sinh behavior")
+TTS_CASE("Check eve::eve::tanh behavior")
 {
-
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::sinh(eve::Nan<Type>()) , (eve::Nan<Type>()) );
-    TTS_IEEE_EQUAL(eve::sinh(eve::Inf<Type>()) , (eve::Inf<Type>()) );
-    TTS_IEEE_EQUAL(eve::sinh(eve::Minf<Type>()), (eve::Minf<Type>()) );   
+    TTS_IEEE_EQUAL(eve::tanh(eve::Nan<Type>()) , (eve::Nan<Type>()) );
+    TTS_IEEE_EQUAL(eve::tanh(eve::Inf<Type>()) , (Type(1)) );
+    TTS_IEEE_EQUAL(eve::tanh(eve::Minf<Type>()), (Type(-1)) );   
   }
-  TTS_ULP_EQUAL(eve::sinh(Type(1)), Type(std::sinh(1.0)), 0.5);
-  TTS_ULP_EQUAL(eve::sinh(Type(-1)),Type(std::sinh(-1.0)), 0.5);
-  TTS_IEEE_EQUAL((eve::sinh(Type(0))), (Type(0)));
-  TTS_IEEE_EQUAL((eve::sinh(eve::Mzero<Type>())), (Type(0)));
-  TTS_EXPECT(eve::all(eve::is_negative(eve::sinh(eve::Mzero<Type>()))) );
-  TTS_EXPECT(eve::all(eve::is_positive(eve::sinh(Type(0))))            );
-
+  TTS_ULP_EQUAL(eve::tanh(Type(1)), Type(std::tanh(Value(1))), 0.5);
+  TTS_ULP_EQUAL(eve::tanh(Type(-1)),Type(std::tanh(Value(-1))), 0.5); 
+  TTS_IEEE_EQUAL((eve::tanh(Type(0))), (eve::Zero<Type>()));
+  TTS_IEEE_EQUAL((eve::tanh(eve::Mzero<Type>())), (eve::Zero<Type>()));
+  TTS_EXPECT(eve::all(eve::is_negative(eve::tanh(eve::Mzero<Type>()))) );
+  TTS_EXPECT(eve::all(eve::is_positive(eve::tanh(Type(0))))            );
 }
