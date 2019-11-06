b83b7d4e42becac479ece458d606f84080c57259
Joel Falcou <joel.falcou@lri.fr>
2019-11-06 10:09:13 +0100

    Stupdi fix for stupid errors

--- test/unit/module/core/mul/saturated/mul.hpp
+++ test/unit/module/core/mul/saturated/mul.hpp
@@ -30,39 +30,39 @@ TTS_CASE("Check saturated_(eve::mul) behavior")
     auto tvmin = eve::Valmin<Type>();
     auto vmax = eve::Valmax<Value>();
     auto vmin = eve::Valmin<Value>();
-    
+
     if constexpr(std::is_signed_v<Value>)
     {
-      TTS_EQUAL(saturated_(eve::mul)(tvmax       , (Type(-2))), tvmin);
       TTS_EQUAL(saturated_(eve::mul)(tvmin       , (Type(-2))), tvmax);
+      TTS_EQUAL(saturated_(eve::mul)(tvmax       , (Type(-2))), tvmin);
       TTS_EQUAL(saturated_(eve::mul)((Type(-1))  , (Type( 0))), Type(0));
       TTS_EQUAL(saturated_(eve::mul)(tvmax       ,      tvmax), tvmax);
       TTS_EQUAL(saturated_(eve::mul)(tvmax       ,      tvmin), tvmin);
-      
+
       TTS_EQUAL(saturated_(eve::mul)(vmax       , (Type(-2))), tvmin);
       TTS_EQUAL(saturated_(eve::mul)(vmin       , (Type(-2))), tvmax);
       TTS_EQUAL(saturated_(eve::mul)((Value(-1)), (Type( 0))), Type(0));
       TTS_EQUAL(saturated_(eve::mul)(vmax       ,      tvmax), tvmax);
       TTS_EQUAL(saturated_(eve::mul)(vmax       ,      tvmin), tvmin);
-      
+
       TTS_EQUAL(saturated_(eve::mul)(tvmax       , (Value(-2))), tvmin);
       TTS_EQUAL(saturated_(eve::mul)(tvmin       , (Value(-2))), tvmax);
       TTS_EQUAL(saturated_(eve::mul)((Type(-1))  , (Value( 0))), Type(0));
       TTS_EQUAL(saturated_(eve::mul)(tvmax       ,        vmax), tvmax);
       TTS_EQUAL(saturated_(eve::mul)(tvmax       ,        vmin), tvmin);
-      
+
     }
     else
     {
       TTS_EQUAL(saturated_(eve::mul)(tvmax    , (Type(2))), tvmax);
       TTS_EQUAL(saturated_(eve::mul)((Type(2)), tvmax)    , tvmax);
-      
+
       TTS_EQUAL(saturated_(eve::mul)(vmax      , (Type(2))), tvmax);
       TTS_EQUAL(saturated_(eve::mul)((Value(2)), tvmax)    , tvmax);
-      
+
       TTS_EQUAL(saturated_(eve::mul)(tvmax     , (Value(2))), tvmax);
       TTS_EQUAL(saturated_(eve::mul)((Type(2)) , vmax )     , tvmax);
-      
+
     }
   }
   else
@@ -70,11 +70,11 @@ TTS_CASE("Check saturated_(eve::mul) behavior")
     TTS_EQUAL(saturated_(eve::mul)(Type( 0) , Type(1)), Type(0  ) );
     TTS_EQUAL(saturated_(eve::mul)(Type( 1) , Type(1)), Type(1  ) );
     TTS_EQUAL(saturated_(eve::mul)(Type(12) , Type(4)), Type(48  ) );
-    
+
     TTS_EQUAL(saturated_(eve::mul)(Value( 0) , Type(1)), Type(0  ) );
     TTS_EQUAL(saturated_(eve::mul)(Value( 1) , Type(1)), Type(1  ) );
     TTS_EQUAL(saturated_(eve::mul)(Value(12) , Type(4)), Type(48  ) );
-    
+
     TTS_EQUAL(saturated_(eve::mul)(Type( 0) , Value(1)), Type(0  ) );
     TTS_EQUAL(saturated_(eve::mul)(Type( 1) , Value(1)), Type(1  ) );
     TTS_EQUAL(saturated_(eve::mul)(Type(12) , Value(4)), Type(48  ) );
