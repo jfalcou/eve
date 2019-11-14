bfda64371633a16dc479d19ee6704f04fc3ddce3
jtlap <jtlapreste@gmail.com>
2019-11-14 10:51:32 +0100

    cosh

--- include/eve/function/simd/cosh.hpp
+++ include/eve/function/simd/cosh.hpp
@@ -2,16 +2,15 @@
 /**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_COSH_HPP_INCLUDED
-#define EVE_FUNCTION_COSH_HPP_INCLUDED
-
+#ifndef EVE_FUNCTION_SIMD_COSH_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_COSH_HPP_INCLUDED
+ 
 #include <eve/function/scalar/cosh.hpp>
-#include <eve/function/simd/cosh.hpp>
+#include <eve/module/core/function/simd/cosh.hpp>
 
 #endif
