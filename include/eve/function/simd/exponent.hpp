3ce9e21a753bf8d477251c8db20eb392b2858ea6
jtlap <jtlapreste@gmail.com>
2019-11-15 17:13:15 +0100

    exponent and mantissa + tests

--- include/eve/function/simd/exponent.hpp
+++ include/eve/function/simd/exponent.hpp
@@ -8,10 +8,10 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
-#define EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
+#ifndef EVE_FUNCTION_SIMD_EXPONENT_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_EXPONENT_HPP_INCLUDED
 
-#include <eve/function/definition/exponentbits.hpp>
-#include <eve/module/core/function/generic/exponentbits.hpp>
+#include <eve/function/scalar/exponent.hpp>
+#include <eve/module/core/function/simd/exponent.hpp>
 
 #endif
--- include/eve/module/core/function/simd/exponent.hpp
+++ include/eve/module/core/function/simd/exponent.hpp
@@ -8,10 +8,10 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
-#define EVE_FUNCTION_SCALAR_EXPONENTBITS_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_EXPONENT_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_EXPONENT_HPP_INCLUDED
 
-#include <eve/function/definition/exponentbits.hpp>
-#include <eve/module/core/function/generic/exponentbits.hpp>
+#include <eve/arch.hpp>
+#include <eve/module/core/function/simd/common/exponent.hpp>
 
 #endif
