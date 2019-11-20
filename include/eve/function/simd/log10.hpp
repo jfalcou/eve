1f157afef546021f6a1eba293345a56c5131a44f
jtlap <jtlapreste@gmail.com>
2019-11-20 17:20:08 +0100

    log10

--- include/eve/function/simd/log10.hpp
+++ include/eve/function/simd/log10.hpp
@@ -2,16 +2,15 @@
 /**
   EVE - Expressive Vector Engine
   Copyright 2019 Joel FALCOU
-  Copyright 2019 Jean-Thierry LAPRESTE
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_FUNCTION_LOG10_HPP_INCLUDED
-#define EVE_FUNCTION_LOG10_HPP_INCLUDED
-
+#ifndef EVE_FUNCTION_SIMD_LOG10_HPP_INCLUDED
+#define EVE_FUNCTION_SIMD_LOG10_HPP_INCLUDED
+ 
 #include <eve/function/scalar/log10.hpp>
-#include <eve/function/simd/log10.hpp>
+#include <eve/module/core/function/simd/log10.hpp>
 
 #endif
