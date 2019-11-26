f7f98a360c1c8d968ad6160741fb9543cac4e1be
Joel Falcou <joel.falcou@lri.fr>
2019-11-26 19:23:44 +0100

    Reorganize tests & measures for local tests

--- test/test.hpp
+++ test/test.hpp
@@ -13,64 +13,7 @@
 
 #define TTS_USE_CUSTOM_DRIVER
 #include <tts/tts.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/precision.hpp>
-#include <eve/wide.hpp>
-#include <algorithm>
-
-#define EVE_WIDE_SIZE_RANGE()                                                                      \
-  eve::fixed<1>, eve::fixed<2>, eve::fixed<4>, eve::fixed<8>, eve::fixed<16>, eve::fixed<32>,      \
-      eve::fixed<64> /**/
-
-namespace tts::ext
-{
-  template<typename T, typename N, typename ABI, typename EnableIf>
-  struct equal<eve::wide<T, N, ABI>, eve::wide<T, N, ABI>, EnableIf>
-  {
-    using arg_t = eve::wide<T, N, ABI>;
-    inline bool operator()(arg_t const &l, arg_t const &r) const
-    {
-      return std::equal(l.begin(), l.end(), r.begin());
-    }
-  };
-
-  template<typename T, typename EnableIf>
-  struct equal<eve::logical<T>, eve::logical<T>, EnableIf>
-  {
-    using arg_t = eve::logical<T>;
-    inline bool operator()(arg_t const &l, arg_t const &r) const
-    {
-      using b_t = typename arg_t::bits_type;
-      return equal<b_t, b_t>{}(l.bits(), r.bits());
-    }
-  };
-
-  template<typename T, typename N, typename ABI, typename EnableIf>
-  struct ulpdist<eve::wide<T, N, ABI>, eve::wide<T, N, ABI>, EnableIf>
-  {
-    inline double operator()(eve::wide<T, N, ABI> const &l, eve::wide<T, N, ABI> const &r) const
-    {
-      double max_ulp = 0;
-      for(auto i = 0; i < l.size(); ++i)
-        max_ulp = std::max(max_ulp, tts::ulpdist(T(l[ i ]), T(r[ i ])));
-
-      return max_ulp;
-    }
-  };
-
-  template<typename T, typename N, typename ABI, typename EnableIf>
-  struct reldist<eve::wide<T, N, ABI>, eve::wide<T, N, ABI>, EnableIf>
-  {
-    inline double operator()(eve::wide<T, N, ABI> const &l, eve::wide<T, N, ABI> const &r) const
-    {
-      double max_rel = 0;
-      for(auto i = 0; i < l.size(); ++i)
-        max_rel = std::max(max_rel, tts::reldist(T(l[ i ]), T(r[ i ])));
-
-      return max_rel;
-    }
-  };
-}
+#include "measures.hpp"
 
 int main(int argc, char **argv)
 {
