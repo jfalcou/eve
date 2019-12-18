0e1089695b5a7316dec7dca13e90f359b7afd799
jtlap <jtlapreste@gmail.com>
2019-12-18 11:01:21 +0100

    bug of unary_minus for simd zero

--- test/unit/module/core/unary_minus/regular/unary_minus.hpp
+++ test/unit/module/core/unary_minus/regular/unary_minus.hpp
@@ -11,6 +11,11 @@
 #include <eve/function/unary_minus.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
+#include <eve/function/all.hpp>
+#include <eve/function/is_negative.hpp>
+#include <eve/function/is_positive.hpp>
+#include <eve/constant/zero.hpp>
+#include <eve/constant/mzero.hpp>
 
 TTS_CASE("Check eve::unary_minus return type")
 {
@@ -21,4 +26,10 @@ TTS_CASE("Check eve::unary_minus behavior")
 {
   TTS_EQUAL(eve::unary_minus(Type( 1)) , static_cast<Type>(-1));
   TTS_EQUAL(eve::unary_minus(Type(-1)) , static_cast<Type>( 1));
+  if (std::is_floating_point_v<Value>)
+  {
+    TTS_EXPECT(eve::all(eve::is_negative(eve::unary_minus(eve::Zero<Type>()))));
+    TTS_EXPECT(eve::all(eve::is_positive(eve::unary_minus(eve::Mzero<Type>())))); 
+  }
+  
 }
