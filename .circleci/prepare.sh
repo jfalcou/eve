204567a08abe30247938c49c9d97972b2a0db59a
Joel Falcou <joel.falcou@lri.fr>
2019-12-19 14:18:57 +0100

    Download Boost.Math in CI

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -9,8 +9,16 @@
 #!/usr/bin/env bash
 
 apt-get update && apt-get install -y sudo && rm -rf /var/lib/apt/lists/*
+apt-get update && sudo apt-get install -y wget
 apt-get update && sudo apt-get install -y cmake
 apt-get update && sudo apt-get install -y ninja-build
+
+wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
+tar -zxvf boost_1_72_0.tar.gz
+cd boost_1_72_0 && ./bootstrap.sh
+LOCALBOOST=`pwd`
+echo "BOOST INSTALLED AT " $LOCALBOOST
+exit 1
 
 if hash "git" 2>/dev/null; then
   git --version;
