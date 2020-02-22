2c16f23cffb048addf32115bea8c9877c52337b8
jtlap <jtlapreste@gmail.com>
2020-02-22 09:32:30 +0100

    random exhaustive for cos

--- test/random/cos/medium/cos.hpp
+++ test/random/cos/medium/cos.hpp
@@ -9,7 +9,7 @@
 **/
 //==================================================================================================
 #include <eve/function/cos.hpp>
-#include <eve/constant/maxlog.hpp>
+#include <eve/constant/reduce_medium_limits.hpp>
 #include <tts/tests/range.hpp>
 #include "measures.hpp"
 #include "producers.hpp"
@@ -19,6 +19,6 @@ TTS_CASE("wide random check on cos")
 {
   auto std_cos = tts::vectorize<Type>( [](auto e) { return std::cos(e); } );
 
-  eve::rng_producer<Type> p(-Value(10000), Value(10000));
+  eve::rng_producer<Type> p(-eve::Reduce_medium_limits<Value>(), eve::Reduce_medium_limits<Value>());
   TTS_RANGE_CHECK(p, std_cos, eve::medium_(eve::cos)); 
 }
