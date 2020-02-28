449471982adb0b402078edc0c1c5a86acd5921d3
Joel Falcou <joel.falcou@lri.fr>
2020-02-28 10:10:08 +0100

    Use correct FMA/FMS intrinsinc on ARM

---
+++ include/eve/module/core/function/simd/arm/neon/fnma.hpp
@@ -0,0 +1,69 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_FNMA_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_FNMA_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/forward.hpp>
+
+namespace eve::detail
+{
+  template<typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon64_> fnma_(EVE_SUPPORTS(neon128_),
+                                           wide<T, N, neon64_> const &v0,
+                                           wide<T, N, neon64_> const &v1,
+                                           wide<T, N, neon64_> const &v2) noexcept
+  {
+#if defined(__aarch64__)
+    if constexpr(std::is_same_v<T, double>) return vfms_f64(v2, v1, v0);
+#endif
+
+    if constexpr(std::is_same_v<T, float>) return vfms_f32(v2, v1, v0);
+    else return v2-v0*v1;
+  }
+
+  template<typename D, typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon64_> fnma_(EVE_SUPPORTS(neon128_),
+                                           D const &,
+                                           wide<T, N, neon64_> const &v0,
+                                           wide<T, N, neon64_> const &v1,
+                                           wide<T, N, neon64_> const &v2) noexcept
+  {
+    return fnma(v0, v1, v2);
+  }
+
+  template<typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon128_> fnma_(EVE_SUPPORTS(neon128_),
+                                            wide<T, N, neon128_> const &v0,
+                                            wide<T, N, neon128_> const &v1,
+                                            wide<T, N, neon128_> const &v2) noexcept
+  {
+#if defined(__aarch64__)
+    if constexpr(std::is_same_v<T, double>) return vfmsq_f64(v2, v1, v0);
+#endif
+
+    if constexpr(std::is_same_v<T, float>) return vfmsq_f32(v2, v1, v0);
+    else return v2-v0*v1;
+  }
+
+  template<typename D, typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon128_> fnma_(EVE_SUPPORTS(neon128_),
+                                            D const &,
+                                            wide<T, N, neon128_> const &v0,
+                                            wide<T, N, neon128_> const &v1,
+                                            wide<T, N, neon128_> const &v2) noexcept
+  {
+    return fnma(v0, v1, v2);
+  }
+}
+
+#endif
---
+++ include/eve/module/core/function/simd/arm/neon/fnms.hpp
@@ -0,0 +1,60 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_FNMS_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_FNMS_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/unary_minus.hpp>
+#include <eve/forward.hpp>
+
+namespace eve::detail
+{
+  template<typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon64_> fnms_(EVE_SUPPORTS(neon128_),
+                                           wide<T, N, neon64_> const &v0,
+                                           wide<T, N, neon64_> const &v1,
+                                           wide<T, N, neon64_> const &v2) noexcept
+  {
+    return -fma(v0,v1,v2);
+  }
+
+  template<typename D, typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon64_> fnms_(EVE_SUPPORTS(neon128_),
+                                           D const &,
+                                           wide<T, N, neon64_> const &v0,
+                                           wide<T, N, neon64_> const &v1,
+                                           wide<T, N, neon64_> const &v2) noexcept
+  {
+    return -fma(v0,v1,v2);
+  }
+
+  template<typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon128_> fnms_(EVE_SUPPORTS(neon128_),
+                                            wide<T, N, neon128_> const &v0,
+                                            wide<T, N, neon128_> const &v1,
+                                            wide<T, N, neon128_> const &v2) noexcept
+  {
+    return -fma(v0,v1,v2);
+  }
+
+  template<typename D, typename T, typename N>
+  EVE_FORCEINLINE wide<T, N, neon128_> fnms_(EVE_SUPPORTS(neon128_),
+                                            D const &,
+                                            wide<T, N, neon128_> const &v0,
+                                            wide<T, N, neon128_> const &v1,
+                                            wide<T, N, neon128_> const &v2) noexcept
+  {
+    return -fma(v0,v1,v2);
+  }
+}
+
+#endif
