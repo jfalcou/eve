3a426796faaaa52cca23e189d58ba56d4c5dda58
jtlap <jtlapreste@gmail.com>
2020-03-04 11:32:59 +0100

    negatenz

---
+++ include/eve/module/core/function/scalar/negatenz.hpp
@@ -0,0 +1,35 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2019 Joel FALCOU
+  Copyright 2019 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_SCALAR_NEGATENZ_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_SCALAR_NEGATENZ_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/if_else.hpp>
+#include <eve/function/signnz.hpp>
+#include <eve/concept/vectorizable.hpp>
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<typename T>
+  EVE_FORCEINLINE constexpr auto
+  negatenz_(EVE_SUPPORTS(cpu_), T const &a0, T const &a1) noexcept
+  requires(T, vectorizable<T>)
+  {
+    if constexpr(std::is_signed_v<T>)
+      return a0*signnz(a1);
+    else
+      return a0;
+  }
+}
+
+#endif
