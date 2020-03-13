6f9ba692819e0489847ccf276448c518bf69bb60
jtlap <jtlapreste@gmail.com>
2020-03-13 11:41:42 +0100

    bad std call in test

--- test/unit/module/core/sincos/restricted/sincos.hpp
+++ test/unit/module/core/sincos/restricted/sincos.hpp
@@ -33,8 +33,8 @@ TTS_CASE("Check (eve::sincos behavior")
   for(int i=0; i < 4 ; ++i)
   {
     auto [p0, p1] = eve::restricted_(eve::sincos)(EVE_TYPE(x[i]));
-    TTS_ULP_EQUAL(p0, std::sin(EVE_TYPE(x[i])), 0.5);
-    TTS_ULP_EQUAL(p1, std::cos(EVE_TYPE(x[i])), 0.5);
+    TTS_ULP_EQUAL(p0, EVE_TYPE(std::sin(x[i])), 0.5);
+    TTS_ULP_EQUAL(p1, EVE_TYPE(std::cos(x[i])), 0.5);
   }
   for(int i=4; i < 6 ; ++i)
   {
