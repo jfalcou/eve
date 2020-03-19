23c80339c901bfe1e6f8436e0882956ab246b97e
Joel Falcou <joel.falcou@lri.fr>
2020-03-19 16:40:44 +0100

    Simplify code to appease aarch64-g++-9 and its new tantrums

--- test/unit/module/core/sinpicospi/big/sinpicospi.hpp
+++ test/unit/module/core/sinpicospi/big/sinpicospi.hpp
@@ -10,7 +10,7 @@
 //==================================================================================================
 #include <eve/function/sinpicospi.hpp>
 #include <eve/function/cospi.hpp>
-#include <eve/function/sinpi.hpp>  
+#include <eve/function/sinpi.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
 #include <type_traits>
@@ -23,24 +23,25 @@
 TTS_CASE("Check  eve::big_(eve::sinpicospi) return type")
 {
   TTS_EXPR_IS(eve::big_(eve::sinpicospi)(EVE_TYPE()), (std::tuple<EVE_TYPE,EVE_TYPE>));
-} 
+}
 
 TTS_CASE("Check  eve::big_(eve::sinpicospi) behavior")
 {
-  static const int N = 16; 
-  EVE_VALUE x[N] = {  eve::One<EVE_VALUE>()/8, -eve::One<EVE_VALUE>()/8
-                  , eve::One<EVE_VALUE>()/4, -eve::One<EVE_VALUE>()/4
-                  , EVE_VALUE(1), EVE_VALUE(-1)
-                  , EVE_VALUE(10), EVE_VALUE(-10)
-                  , EVE_VALUE(1000000), EVE_VALUE(-1000000)
-                  , EVE_VALUE(1000000000), EVE_VALUE(-1000000000)
-                  , eve::Valmax<EVE_VALUE>(), eve::Valmin<EVE_VALUE>()    
-                  , eve::Valmax<EVE_VALUE>()/100000, eve::Valmin<EVE_VALUE>()/10000}; 
-  
-  for(int i=0; i < N ; ++i)
+  EVE_VALUE values[] =  { EVE_VALUE(1)/8          , -EVE_VALUE(1)/8
+                        , EVE_VALUE(1)/4          , -EVE_VALUE(1)/4
+                        , EVE_VALUE(1)            , EVE_VALUE(-1)
+                        , EVE_VALUE(10)           , EVE_VALUE(-10)
+                        , EVE_VALUE(1000000)      , EVE_VALUE(-1000000)
+                        , EVE_VALUE(1000000000)   , EVE_VALUE(-1000000000)
+                        , eve::Valmax<EVE_VALUE>(), eve::Valmin<EVE_VALUE>()
+                        , eve::Valmax<EVE_VALUE>()/100000, eve::Valmin<EVE_VALUE>()/10000
+                        };
+
+  for(auto v : values)
   {
-    auto [p0, p1] = eve::big_(eve::sinpicospi)(EVE_TYPE(x[i]));
-    TTS_ULP_EQUAL(p0, eve::sinpi(EVE_TYPE(x[i])), 0.5);
-    TTS_ULP_EQUAL(p1, eve::cospi(EVE_TYPE(x[i])), 0.5);
+    EVE_TYPE e(v);
+    auto [p0, p1] = eve::big_(eve::sinpicospi)(e);
+    TTS_ULP_EQUAL(p0, eve::sinpi(e), 0.5);
+    TTS_ULP_EQUAL(p1, eve::cospi(e), 0.5);
   }
 }
