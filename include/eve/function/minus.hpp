a191595f4cf2377f28a2db3573f9c2ce846c7d85
jtlap <jtlapreste@gmail.com>
2020-03-24 15:19:09 +0100

    treating plus minus

--- include/eve/function/minus.hpp
+++ include/eve/function/minus.hpp
@@ -11,7 +11,8 @@
 #ifndef EVE_FUNCTION_MINUS_HPP_INCLUDED
 #define EVE_FUNCTION_MINUS_HPP_INCLUDED
 
-#include <eve/function/scalar/minus.hpp>
-#include <eve/function/simd/minus.hpp>
+#include <eve/function/definition/minus.hpp>
+#include <eve/module/core/function/scalar/minus.hpp>
+#include <eve/module/core/function/simd/minus.hpp>
 
 #endif
