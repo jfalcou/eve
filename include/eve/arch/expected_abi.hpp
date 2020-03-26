5a712c02d896b16f64c133886e9675c29955a000
Joel Falcou <joel.falcou@lri.fr>
2020-03-26 16:27:37 +0100

    Better expected_abi

--- include/eve/arch/expected_abi.hpp
+++ include/eve/arch/expected_abi.hpp
@@ -11,51 +11,55 @@
 #ifndef EVE_ARCH_EXPECTED_ABI_HPP_INCLUDED
 #define EVE_ARCH_EXPECTED_ABI_HPP_INCLUDED
 
-#include <eve/arch/abi_of.hpp>
 #include <eve/arch/expected_cardinal.hpp>
+#include <eve/concept/stdconcepts.hpp>
+#include <eve/ext/abi_of.hpp>
 #include <eve/forward.hpp>
 
 namespace eve::detail
 {
-  // Is this type ABI emulated_ ?
-  template<typename Type>
-  constexpr bool require_emulation()
-  {
-    using abi_t = abi_of_t<Type, expected_cardinal_v<Type>>;
-    return std::is_same_v<abi_t, eve::emulated_>;
-  }
-
   // Types that are too big and are not emulated require aggregation
   template<typename Type, typename Size>
-  constexpr bool require_aggregation()
-  {
-    return (Size::value > expected_cardinal_v<Type>)&&!require_emulation<Type>();
-  }
+  inline constexpr bool require_aggregation =     (Size::value > expected_cardinal_v<Type>)
+                                              && !std::is_same_v
+                                                        < ext::abi_of_t < Type
+                                                                        , expected_cardinal_v<Type>
+                                                                        >
+                                                        , eve::emulated_
+                                                        >;
 }
 
 namespace eve
 {
   // Find proper ABI for wide
-  template<typename Type, typename Size, typename Enable = void>
-  struct expected_abi;
+  template<typename Type, typename Size> struct expected_abi {};
 
   template<typename Type, typename Size>
-  struct expected_abi<Type, Size, std::enable_if_t<std::is_arithmetic_v<Type>>>
+  requires( std::arithmetic<Type> && detail::require_aggregation<Type, Size> )
+  struct expected_abi<Type, Size>
   {
-    using type = std::conditional_t<detail::require_aggregation<Type, Size>(),
-                                    eve::aggregated_,
-                                    abi_of_t<Type, Size::value>>;
+    using type = eve::aggregated_;
   };
+
+  template<typename Type, typename Size>
+  requires( std::arithmetic<Type> && !detail::require_aggregation<Type, Size> )
+  struct expected_abi<Type, Size> : ext::abi_of<Type, Size::value>
+  {};
 
   // Wrapper for SIMD registers holding logical type
   template<typename Type, typename Size>
-  struct expected_abi<logical<Type>, Size, std::enable_if_t<std::is_arithmetic_v<Type>>>
+  requires( std::arithmetic<Type> && detail::require_aggregation<Type, Size> )
+  struct expected_abi<logical<Type>, Size>
   {
-    using type = std::conditional_t<detail::require_aggregation<Type, Size>(),
-                                    eve::aggregated_,
-                                    abi_of_t<logical<Type>, Size::value>>;
+    using type = eve::aggregated_;
   };
 
+  template<typename Type, typename Size>
+  requires( std::arithmetic<Type> && !detail::require_aggregation<Type, Size> )
+  struct expected_abi<logical<Type>, Size> : ext::abi_of<logical<Type>, Size::value>
+  {};
+
+  // Type short-cut
   template<typename Type, typename Size>
   using expected_abi_t = typename expected_abi<Type, Size>::type;
 }
--- include/eve/concept/stdconcepts.hpp
+++ include/eve/concept/stdconcepts.hpp
@@ -18,6 +18,7 @@
 namespace std
 {
   template<typename T> concept floating_point          = std::is_floating_point_v<T>;
+  template<typename T> concept arithmetic              = std::is_arithmetic_v<T>;
   template<typename T> concept integral                = std::is_integral_v<T>;
   template<typename T> concept signed_type             = std::is_signed_v<T>;
   template<typename T> concept unsigned_integral       = std::is_unsigned_v<T>;
