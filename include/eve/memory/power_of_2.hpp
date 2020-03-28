5dd3515b13107702c54c6b855e57482b5161ace1
Joel Falcou <joel.falcou@lri.fr>
2020-03-28 19:58:05 +0100

    Conceptified power of 2 related functions

--- include/eve/memory/power_of_2.hpp
+++ include/eve/memory/power_of_2.hpp
@@ -11,20 +11,18 @@
 #ifndef EVE_MEMORY_POWER_OF_2_HPP_INCLUDED
 #define EVE_MEMORY_POWER_OF_2_HPP_INCLUDED
 
+#include <eve/concept/stdconcepts.hpp>
 #include <climits>
 #include <cstdint>
-#include <type_traits>
 
 namespace eve
 {
-  template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-  constexpr bool is_power_of_2(T value) noexcept
+  template<std::integral T> constexpr bool is_power_of_2(T value) noexcept
   {
     return (!(value & (value - 1)) && value);
   }
 
-  template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-  constexpr T next_power_of_2(T value) noexcept
+  template<std::integral T> constexpr T next_power_of_2(T value) noexcept
   {
     value--;
     for(std::size_t i = 1; i < sizeof(value) * CHAR_BIT; i *= 2) value |= value >> i;
@@ -32,8 +30,7 @@ namespace eve
     return ++value;
   }
 
-  template<typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-  constexpr T prev_power_of_2(T value) noexcept
+  template<std::integral T> constexpr T prev_power_of_2(T value) noexcept
   {
     for(std::size_t i = 1; i < sizeof(value) * CHAR_BIT; i *= 2) value |= value >> i;
 
