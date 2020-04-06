0ba1b277bc9ba4320e3fb9fbe62ed151e5383feb
Joel Falcou <joel.falcou@lri.fr>
2020-04-06 21:54:28 +0200

    Cleaner scalar_value

--- include/eve/concept/vectorizable.hpp
+++ include/eve/concept/vectorizable.hpp
@@ -12,7 +12,9 @@
 #define EVE_CONCEPT_VECTORIZABLE_HPP_INCLUDED
 
 #include <eve/forward.hpp>
-#include <eve/concept/stdconcepts.hpp> // to replace by <concepts> when libc++ is uptodate  
+#include <eve/concept/stdconcepts.hpp>
+#include <eve/concept/rebindable.hpp>
+#include <eve/detail/is_wide.hpp>
 #include <eve/element_type.hpp>
 #include <eve/is_logical.hpp>
 #include <type_traits>
@@ -66,15 +68,44 @@ namespace eve
   template<typename Type>
   using Vectorizable = std::enable_if_t<is_Vectorizable_v<Type>>;
 
+
+  namespace detail
+  {
+    //==============================================================================================
+    // Check if something is a scalar_value
+    //==============================================================================================
+    template<typename T> struct is_scalar_value : std::is_arithmetic<T>
+    {};
+
+    template<typename T>
+    struct is_scalar_value<eve::logical<T>> : std::is_arithmetic<T>
+    {};
+
+    template<typename Type>
+    requires( rebindable<Type> && !detail::is_wide<Type>::value )
+    struct is_scalar_value<Type>
+    {
+      template<typename Idx>      struct eval_n;
+      template<std::size_t... N>  struct eval_n<std::index_sequence<N...>>
+      {
+        static constexpr bool value = (is_scalar_value<std::tuple_element_t<N,Type>>::value && ...);
+      };
+
+      using size = std::tuple_size<Type>;
+      static constexpr bool value = eval_n<std::make_index_sequence<size::value>>::value;
+    };
+  }
+
   template<typename Type> concept vectorizable = is_Vectorizable_v<Type>;
 
-  template<typename T> concept scalar_value                   = is_Vectorizable_v<T>;
+  template<typename T> concept scalar_value                   = detail::is_scalar_value<T>::value;
+
   template<typename T> concept integral_scalar_value          = scalar_value<T> && std::integral<T>;
   template<typename T> concept signed_scalar_value            = scalar_value<T> && std::is_signed_v<T>;
   template<typename T> concept unsigned_scalar_value          = scalar_value<T> && std::unsigned_integral<T>;
   template<typename T> concept signed_integral_scalar_value   = scalar_value<T> && std::signed_integral<T>;
   template<typename T> concept floating_scalar_value          = scalar_value<T> && std::floating_point<T>;
-  template<typename T> concept logical_scalar_value           = scalar_value<T> && is_logical_v<T>; 
+  template<typename T> concept logical_scalar_value           = scalar_value<T> && is_logical_v<T>;
   template<typename T> concept real_scalar_value              = scalar_value<T> && std::same_as< detail::value_type_t<T>, element_type_t<T>>;
   template<typename T> concept floating_real_scalar_value     = real_scalar_value<T> && std::floating_point<detail::value_type_t<T>>;
   template<typename T> concept integral_real_scalar_value     = real_scalar_value<T> && std::integral<detail::value_type_t<T>>;
---
+++ test/unit/meta/value.cpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/logical.hpp>
+#include <eve/wide.hpp>
+#include <tts/tests/types.hpp>
+#include <tuple>
+#include <string>
+
+TTS_CASE("Check validation of the scalar_value concept for actual scalar types" )
+{
+  using eve::logical;
+
+  TTS_EXPECT( eve::scalar_value<int>   );
+  TTS_EXPECT( eve::scalar_value<char>  );
+  TTS_EXPECT( eve::scalar_value<float> );
+  TTS_EXPECT( eve::scalar_value<double>);
+
+  TTS_EXPECT( eve::scalar_value<logical<short>>);
+  TTS_EXPECT( eve::scalar_value<logical<float>>);
+
+  TTS_EXPECT( (eve::scalar_value<std::pair<int,float>>) );
+  TTS_EXPECT( (eve::scalar_value<std::tuple<double,char,logical<float>>>) );
+}
+
+TTS_CASE("Check validation of the scalar_value concept for non-scalar types" )
+{
+  using eve::logical;
+  using eve::fixed;
+  using eve::wide;
+
+  TTS_EXPECT_NOT( (eve::scalar_value<wide<int>>           ) );
+  TTS_EXPECT_NOT( (eve::scalar_value<logical<wide<int>>>  ) );
+  TTS_EXPECT_NOT( (eve::scalar_value<wide<char,fixed<16>>>) );
+  TTS_EXPECT_NOT( (eve::scalar_value<std::pair<wide<int>,wide<float>>>) );
+}
