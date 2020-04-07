fdaad247c1d0f52ab2f49dc6c4ff58fc9c78586d
Joel Falcou <joel.falcou@lri.fr>
2020-04-07 14:36:42 +0200

    Update to simd_value concept
    A type satisfies sidm_value iff its cardinal is not
    scalar_cardinal

--- include/eve/concept/vectorized.hpp
+++ include/eve/concept/vectorized.hpp
@@ -11,7 +11,7 @@
 #ifndef EVE_CONCEPT_VECTORIZED_HPP_INCLUDED
 #define EVE_CONCEPT_VECTORIZED_HPP_INCLUDED
 
-#include <eve/concept/stdconcepts.hpp> // to replace by <concepts> when libc++ is uptodate  
+#include <eve/concept/stdconcepts.hpp> // to replace by <concepts> when libc++ is uptodate
 #include <eve/element_type.hpp>
 #include <eve/cardinal.hpp>
 #include <eve/is_logical.hpp>
@@ -80,13 +80,17 @@ namespace eve
 
   template<typename Type> concept vectorized = is_Vectorized_v<Type>;
 
-  template<typename T> concept simd_value                 = is_Vectorized_v<T>;
+  //================================================================================================
+  // A type satisfies simd_value iff its cardinal is not scalar_cardinal
+  //================================================================================================
+  template<typename T> concept simd_value = !std::same_as<eve::cardinal_t<T>, scalar_cardinal>;
+
   template<typename T> concept integral_simd_value        = simd_value<T> && std::integral<detail::value_type_t<T>>;
   template<typename T> concept signed_simd_value          = simd_value<T> && std::is_signed_v<detail::value_type_t<T>>;
   template<typename T> concept unsigned_simd_value        = simd_value<T> && std::unsigned_integral<detail::value_type_t<T>>;
   template<typename T> concept signed_integral_simd_value = simd_value<T> && std::signed_integral<detail::value_type_t<T>>;
   template<typename T> concept floating_simd_value        = simd_value<T> && std::floating_point<detail::value_type_t<T>>;
-  template<typename T> concept logical_simd_value         = simd_value<T> && is_logical_v<T>; 
+  template<typename T> concept logical_simd_value         = simd_value<T> && is_logical_v<T>;
   template<typename T> concept real_simd_value            = simd_value<T> && std::same_as< detail::value_type_t<T>, element_type_t<T>>;
   template<typename T> concept floating_real_simd_value   = real_simd_value<T> && std::floating_point<detail::value_type_t<T>>;
   template<typename T> concept integral_real_simd_value   = real_simd_value<T> && std::integral<detail::value_type_t<T>>;
--- test/unit/meta/value.cpp
+++ test/unit/meta/value.cpp
@@ -15,9 +15,11 @@
 #include <tuple>
 #include <string>
 
-TTS_CASE("Check validation of the scalar_value concept for actual scalar types" )
+TTS_CASE("Check validation of the scalar_value concept" )
 {
   using eve::logical;
+  using eve::fixed;
+  using eve::wide;
 
   TTS_EXPECT( eve::scalar_value<int>   );
   TTS_EXPECT( eve::scalar_value<char>  );
@@ -29,16 +31,32 @@ TTS_CASE("Check validation of the scalar_value concept for actual scalar types" 
 
   TTS_EXPECT( (eve::scalar_value<std::pair<int,float>>) );
   TTS_EXPECT( (eve::scalar_value<std::tuple<double,char,logical<float>>>) );
-}
-
-TTS_CASE("Check validation of the scalar_value concept for non-scalar types" )
-{
-  using eve::logical;
-  using eve::fixed;
-  using eve::wide;
 
   TTS_EXPECT_NOT( (eve::scalar_value<wide<int>>           ) );
   TTS_EXPECT_NOT( (eve::scalar_value<logical<wide<int>>>  ) );
   TTS_EXPECT_NOT( (eve::scalar_value<wide<char,fixed<16>>>) );
   TTS_EXPECT_NOT( (eve::scalar_value<std::pair<wide<int>,wide<float>>>) );
+}
+
+TTS_CASE("Check validation of the simd_value" )
+{
+  using eve::logical;
+  using eve::fixed;
+  using eve::wide;
+
+  TTS_EXPECT_NOT( eve::simd_value<int>   );
+  TTS_EXPECT_NOT( eve::simd_value<char>  );
+  TTS_EXPECT_NOT( eve::simd_value<float> );
+  TTS_EXPECT_NOT( eve::simd_value<double>);
+
+  TTS_EXPECT_NOT( eve::simd_value<logical<short>>);
+  TTS_EXPECT_NOT( eve::simd_value<logical<float>>);
+
+  TTS_EXPECT_NOT( (eve::simd_value<std::pair<int,float>>) );
+  TTS_EXPECT_NOT( (eve::simd_value<std::tuple<double,char,logical<float>>>) );
+
+  TTS_EXPECT( (eve::simd_value<wide<int>>           ) );
+  TTS_EXPECT( (eve::simd_value<logical<wide<int>>>  ) );
+  TTS_EXPECT( (eve::simd_value<wide<char,fixed<16>>>) );
+  TTS_EXPECT( (eve::simd_value<std::pair<wide<int>,wide<float>>>) );
 }
