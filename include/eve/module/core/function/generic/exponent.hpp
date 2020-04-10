a1330ad9d25941704cae9873fc81b760ab5d8d30
jtlap <jtlapreste@gmail.com>
2020-04-10 22:25:14 +0200

    exponent and suppressing exponentbits

---
+++ include/eve/module/core/function/generic/exponent.hpp
@@ -0,0 +1,58 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_EXPONENT_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_EXPONENT_HPP_INCLUDED
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/function/bit_and.hpp>
+#include <eve/function/is_not_finite.hpp>
+#include <eve/function/is_eqz.hpp>
+#include <eve/function/is_nez.hpp>
+#include <eve/function/binarize.hpp>
+#include <eve/function/shr.hpp>
+#include <eve/function/sub.hpp>
+#include <eve/constant/expobits_mask.hpp>
+#include <eve/constant/maxexponent.hpp>
+#include <eve/constant/nbmantissabits.hpp>
+#include <eve/detail/meta.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/apply_over.hpp>
+
+
+namespace eve::detail
+{
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr as_integer_t<T> exponent_(EVE_SUPPORTS(cpu_)
+                                                     , T const &a) noexcept
+  {
+    if constexpr(native<T>)
+    {
+      auto z =  bit_and(Expobits_mask<T>(), a);
+      if constexpr(scalar_value<T>)
+      {
+        if (is_not_finite(a)) return as_integer_t<T>(0);
+        auto x = shr(z, Nbmantissabits<T>());
+        return sub[is_nez(a)](x, Maxexponent<T>());
+      }
+      else
+      {
+        auto x = shr(z, Nbmantissabits<T>());
+        return if_else(is_not_finite(a), eve::zero_, sub[is_nez(a)](x, Maxexponent<T>()));
+      }
+    }
+    else return apply_over(exponent, a);
+  }
+
+}
+
+#endif
