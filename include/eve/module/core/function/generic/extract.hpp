355b846d05887524dec1e8cc61ac4065d96875cc
jtlap <jtlapreste@gmail.com>
2020-04-11 08:50:49 +0200

    extract

---
+++ include/eve/module/core/function/generic/extract.hpp
@@ -0,0 +1,32 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_EXTRACT_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_EXTRACT_HPP_INCLUDED
+
+
+#include <eve/detail/overload.hpp>
+#include <eve/detail/abi.hpp>
+#include <eve/forward.hpp>
+#include <eve/concept/value.hpp>
+
+namespace eve::detail
+{
+  template<value T, integral_scalar_value I>
+  EVE_FORCEINLINE auto extract_(EVE_SUPPORTS(cpu_)
+                               , T const &a
+                               , I i) noexcept
+  {
+    if constexpr(scalar_value<T>) return a;
+    else                          return a[i];
+  }
+}
+
+#endif
