c8eb5ea6882e38f1478f174311fb4d59ca172360
jtlap <jtlapreste@gmail.com>
2020-04-13 10:28:07 +0200

    file deleted by mistake

---
+++ include/eve/function/is_even.hpp
@@ -0,0 +1,17 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef EVE_FUNCTION_IS_EVEN_HPP_INCLUDED
+#define EVE_FUNCTION_IS_EVEN_HPP_INCLUDED
+
+#include <eve/function/definition/is_even.hpp>
+#include <eve/module/core/function/generic/is_even.hpp>
+
+#endif
