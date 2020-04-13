2d00c50d6e7350840cfc6aa4dd0e9e0d1f3b3a0e
jtlap <jtlapreste@gmail.com>
2020-04-13 17:26:48 +0200

    is_less_equal on archs

--- include/eve/module/core/function/simd/ppc/is_less_equal.hpp
+++ include/eve/module/core/function/simd/ppc/is_less_equal.hpp
@@ -16,21 +16,18 @@
 #include <eve/traits/as_logical.hpp>
 #include <eve/function/is_greater.hpp>
 #include <eve/forward.hpp>
+#include <eve/concept/value.hpp>
 
 namespace eve::detail
 {
-  template<typename T, typename N>
+  template<real_scalar_value T, typename N>
   EVE_FORCEINLINE auto is_less_equal_(EVE_SUPPORTS(vmx_),
                                       wide<T, N, ppc_> const &v0,
                                       wide<T, N, ppc_> const &v1) noexcept
   {
     using t_t = wide<T, N, ppc_>;
-    if constexpr(std::is_floating_point_v<T>)
-    { return as_logical_t<t_t>(vec_cmple(v0.storage(), v1.storage())); }
-    else
-    {
-      return !eve::is_greater(v0, v1);
-    }
+    if constexpr(std::is_floating_point_v<T>) return as_logical_t<t_t>(vec_cmple(v0.storage(), v1.storage()));
+    else                                      return !eve::is_greater(v0, v1);
   }
 }
 
