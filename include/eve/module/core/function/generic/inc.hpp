cacd80e52d9a0dc688c66151ae0880faa2ba2e35
jtlap <jtlapreste@gmail.com>
2020-04-14 19:09:55 +0200

    if_else -> is_equal

--- include/eve/module/core/function/generic/inc.hpp
+++ include/eve/module/core/function/generic/inc.hpp
@@ -11,10 +11,10 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_INC_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_GENERIC_INC_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/meta.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/function/bit_cast.hpp>
+#include <eve/function/bit_mask.hpp>
+#include <eve/function/is_not_equal.hpp>
 #include <eve/function/saturated.hpp>
 #include <eve/function/logical_and.hpp>
 #include <eve/function/if_else.hpp>
