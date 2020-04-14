fa4228a0a24652dfb0b9c16f3d3730d4ce2d6f2a
jtlap <jtlapreste@gmail.com>
2020-04-14 08:02:56 +0200

    is_nez

--- include/eve/module/core/function/generic/is_nez.hpp
+++ include/eve/module/core/function/generic/is_nez.hpp
@@ -8,13 +8,13 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_IS_EQZ_HPP_INCLUDED
-#define EVE_MODULE_CORE_FUNCTION_GENERIC_IS_EQZ_HPP_INCLUDED
+#ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_IS_NEZ_HPP_INCLUDED
+#define EVE_MODULE_CORE_FUNCTION_GENERIC_IS_NEZ_HPP_INCLUDED
 
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/function/is_equal.hpp>
-#include <eve/function/logical_not.hpp>
+#include <eve/function/is_not_equal.hpp>
 #include <eve/traits/as_logical.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/detail/apply_over.hpp>
@@ -22,15 +22,15 @@
 namespace eve::detail
 {
   template<real_value T>
-  EVE_FORCEINLINE constexpr as_logical_t<T> is_eqz_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE constexpr as_logical_t<T> is_nez_(EVE_SUPPORTS(cpu_)
                                                    , T const &a) noexcept
   {
     if constexpr(native<T>)
     {
-      if constexpr(scalar_value<T> || is_logical_v<T>) return !a;
-      else                                             return is_equal(a, Zero(as(a)));
+      if constexpr(is_logical_v<T>) return a;
+      else                          return is_not_equal(a, Zero(as(a)));
     }
-    else                                               return apply_over(is_eqz, a);
+    else                            return apply_over(is_nez, a);
   }
 }
 
