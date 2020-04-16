15b05e2af0983624239e950dd363799995e479ab
Joel Falcou <joel.falcou@lri.fr>
2020-04-16 17:23:08 +0200

    Cleanup for: all

--- include/eve/module/core/function/generic/all.hpp
+++ include/eve/module/core/function/generic/all.hpp
@@ -11,37 +11,31 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_ALL_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_GENERIC_ALL_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/detail/has_abi.hpp>
-#include <eve/function/is_nez.hpp>
-#include <eve/function/extract.hpp>
 #include <eve/concept/value.hpp>
+#include <eve/detail/has_abi.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/function/is_nez.hpp>
 
 namespace eve::detail
 {
-  template<value T>
-  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(cpu_)
-                           , T const &v) noexcept
+  template<value T> EVE_FORCEINLINE bool all_(EVE_SUPPORTS(cpu_), T const &v) noexcept
   {
-    if constexpr(scalar_value<T>)
+    if constexpr( scalar_value<T> )
     {
       return bool(v);
     }
-    else if constexpr(logical_simd_value<T>)
+    else if constexpr( logical_simd_value<T> )
     {
-      if constexpr(has_aggregated_abi_v<T>)
+      if constexpr( has_aggregated_abi_v<T> )
       {
-        return v.storage().apply( [](auto const&... e) { return eve::all((e.bits() & ...)); } );
+        return v.storage().apply([](auto const &... e) { return eve::all((e.bits() & ...)); });
       }
       else
       {
         bool r = true;
 
-        [&]<std::size_t... I>( std::index_sequence<I...> const& )
-        {
-          r = (r && ... && get<I>(v));
-        }( std::make_index_sequence<cardinal_v<T>>{});
+        [&]<std::size_t... I>(std::index_sequence<I...> const &) { r = (r && ... && get<I>(v)); }
+        (std::make_index_sequence<cardinal_v<T>> {});
 
         return r;
       }
--- include/eve/module/core/function/simd/x86/all.hpp
+++ include/eve/module/core/function/simd/x86/all.hpp
@@ -11,79 +11,78 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_X86_ALL_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_X86_ALL_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
 #include <eve/arch/limits.hpp>
+#include <eve/concept/stdconcepts.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+
 #include <type_traits>
 
 namespace eve::detail
 {
-  // -----------------------------------------------------------------------------------------------
+  //================================================================================================
   // 128 bits implementation
-  template<typename T, typename N>
-  EVE_FORCEINLINE bool  all_(EVE_SUPPORTS(sse2_)
-                            , logical<wide<T, N, sse_>> const &v) noexcept
+  //================================================================================================
+  template<real_scalar_value T, typename N>
+  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(sse2_), logical<wide<T, N, sse_>> const &v) noexcept
   {
     static constexpr int Bytes = limits<eve::sse2_>::bytes;
 
-    if constexpr(std::is_same_v<T, float>)
+    if constexpr( std::is_same_v<T, float> )
     {
-      using i8_t = typename wide<T, N, sse_>::template rebind<int8_t, fixed<Bytes>>;
+      using i8_t         = typename wide<T, N, sse_>::template rebind<int8_t, fixed<Bytes>>;
+      constexpr int Card = Bytes / sizeof(T);
+      constexpr int SH   = (Card - N::value);
+      constexpr int mask = 0xF >> SH;
 
-      static constexpr int Card = Bytes/sizeof(T);
-      static constexpr int SH = (Card-N::value);
-      static constexpr int mask = 0xF >> SH;
-
-      if constexpr(N::value*sizeof(T) != Bytes) // "small" wide types
+      if constexpr( N::value * sizeof(T) != Bytes ) // "small" wide types
       {
-        using t_t  = wide<float, fixed<Card>, sse_>;
-        static constexpr int sv = SH*sizeof(T);
-        static constexpr int smask = mask << SH;
-        i8_t z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t>()), sv);
+        using t_t           = wide<float, fixed<Card>, sse_>;
+        constexpr int sv    = SH * sizeof(T);
+        constexpr int smask = mask << SH;
 
-        return _mm_movemask_ps(bit_cast(z,as_<t_t>())) == smask;
+        i8_t z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t>()), sv);
+        return _mm_movemask_ps(bit_cast(z, as_<t_t>())) == smask;
       }
       else
       {
         return _mm_movemask_ps(v.mask()) == mask;
       }
     }
-    else if constexpr(std::is_same_v<T, double>)
+    else if constexpr( std::is_same_v<T, double> )
     {
-      using i8_t = typename wide<T, N, sse_>::template rebind<int8_t, fixed<Bytes>>;
+      using i8_t         = typename wide<T, N, sse_>::template rebind<int8_t, fixed<Bytes>>;
+      constexpr int Card = Bytes / sizeof(T);
+      constexpr int SH   = (Card - N::value);
+      constexpr int mask = 0x3 >> SH;
 
-      static constexpr int Card = Bytes/sizeof(T);
-      static constexpr int SH = (Card-N::value);
-      static constexpr int mask = 0x3 >> SH;
-
-      if constexpr(N::value*sizeof(T) != Bytes) // "small" wide types
+      if constexpr( N::value * sizeof(T) != Bytes ) // "small" wide types
       {
-        using t_t  = wide<double, fixed <Card>, sse_>;
-        static constexpr int sv = SH*sizeof(T);
-        static constexpr int smask = mask << SH;
-        i8_t z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t>()), sv);
+        using t_t           = wide<double, fixed<Card>, sse_>;
+        constexpr int sv    = SH * sizeof(T);
+        constexpr int smask = mask << SH;
 
-        return _mm_movemask_pd(bit_cast(z,as_<t_t>())) == smask;
+        i8_t z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t>()), sv);
+        return _mm_movemask_pd(bit_cast(z, as_<t_t>())) == smask;
       }
       else
       {
         return _mm_movemask_pd(v.mask()) == mask;
       }
     }
-    else if constexpr(integral_value<T>)
+    else if constexpr( std::integral<T> )
     {
-      static constexpr int SH   = (Bytes-sizeof(T)*N::value);
-      static constexpr int mask = 0xFFFF >> SH;
+      constexpr int SH   = (Bytes - sizeof(T) * N::value);
+      constexpr int mask = 0xFFFF >> SH;
 
-      if constexpr(sizeof(T) == 1)
+      if constexpr( sizeof(T) == 1 )
       {
-        if constexpr(N::value*sizeof(T) != Bytes) // "small" wide types
+        if constexpr( N::value * sizeof(T) != Bytes ) // "small" wide types
         {
-          static constexpr int smask = mask <<  SH;
-          static constexpr int sv = SH;
-          auto z = _mm_bslli_si128(v.mask(), sv);
+          constexpr int smask = mask << SH;
+          constexpr int sv    = SH;
 
+          auto z = _mm_bslli_si128(v.mask(), sv);
           return _mm_movemask_epi8(z) == smask;
         }
         else
@@ -95,51 +94,50 @@ namespace eve::detail
       {
         using i8_t = typename wide<T, N, sse_>::template rebind<int8_t, fixed<Bytes>>;
 
-        if constexpr(N::value*sizeof(T) != Bytes) // "small" wide types
+        if constexpr( N::value * sizeof(T) != Bytes ) // "small" wide types
         {
-          static constexpr int smask = mask <<  SH;
-          static constexpr int sv = SH;
-          auto z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t >()), sv);
+          constexpr int smask = mask << SH;
+          constexpr int sv    = SH;
+
+          auto z = _mm_bslli_si128(bit_cast(v.mask(), as_<i8_t>()), sv);
           return _mm_movemask_epi8(z) == smask;
         }
         else
         {
-          return _mm_movemask_epi8(bit_cast(v.mask(), as_<i8_t >())) == mask;
+          return _mm_movemask_epi8(bit_cast(v.mask(), as_<i8_t>())) == mask;
         }
       }
     }
   }
 
-  // -----------------------------------------------------------------------------------------------
+  //================================================================================================
   // 256 bits implementation
-  template<typename T, typename N>
-  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(avx_)
-                           , logical<wide<T, N, avx_>> const &v) noexcept
+  //================================================================================================
+  template<real_scalar_value T, typename N>
+  EVE_FORCEINLINE bool all_(EVE_SUPPORTS(avx_), logical<wide<T, N, avx_>> const &v) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr( std::is_same_v<T, float> )
     {
-      if constexpr(std::is_same_v<T, float>)
-      {
-        return _mm256_movemask_ps(v.mask()) == int(0xFF);
-      }
-      else
-      {
-        return _mm256_movemask_pd(v.mask()) ==int( 0xF);
-      }
+      return _mm256_movemask_ps(v.mask()) == static_cast<int>(0xFF);
+    }
+    else if constexpr( std::is_same_v<T, double> )
+    {
+      return _mm256_movemask_pd(v.mask()) == static_cast<int>(0xF);
     }
     else
     {
-      if constexpr(current_api >= avx2)
+      if constexpr( current_api >= avx2 )
       {
-        if constexpr(sizeof(T) == 1)
+        if constexpr( sizeof(T) == 1 )
         {
-          return _mm256_movemask_epi8(v.mask()) == int(0xFFFFFFFF);
+          return _mm256_movemask_epi8(v.mask()) == static_cast<int>(0xFFFFFFFF);
         }
         else
         {
           using i8_t = typename wide<T, N, avx_>::template rebind<int8_t, fixed<32>>;
 
-          return _mm256_movemask_epi8(bit_cast(v.mask(), as_<i8_t >())) == int(0xFFFFFFFF);
+          auto that = _mm256_movemask_epi8(bit_cast(v.mask(), as_<i8_t>()));
+          return that == static_cast<int>(0xFFFFFFFF);
         }
       }
       else
