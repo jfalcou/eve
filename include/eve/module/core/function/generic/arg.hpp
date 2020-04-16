417845a1587547ec04a7a21d5303a6fc52030e02
Joel Falcou <joel.falcou@lri.fr>
2020-04-16 18:24:20 +0200

    Cleanup for: arg

--- include/eve/module/core/function/generic/arg.hpp
+++ include/eve/module/core/function/generic/arg.hpp
@@ -11,45 +11,48 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_ARG_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_GENERIC_ARG_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/function/if_else.hpp>
-#include <eve/function/is_nan.hpp>
-#include <eve/function/is_negative.hpp>
+#include <eve/concept/value.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/pi.hpp>
 #include <eve/constant/zero.hpp>
-#include <eve/platform.hpp>
+#include <eve/detail/has_abi.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/detail/meta/traits.hpp>
+#include <eve/function/if_else.hpp>
+#include <eve/function/is_nan.hpp>
+#include <eve/function/is_negative.hpp>
 #include <eve/function/pedantic.hpp>
 #include <eve/function/regular.hpp>
-#include <eve/detail/meta.hpp>
-#include <eve/concept/value.hpp>
+#include <eve/platform.hpp>
 
 namespace eve::detail
 {
   template<floating_real_value T, typename D>
-  EVE_FORCEINLINE constexpr auto arg_(EVE_SUPPORTS(cpu_)
-                                     , D const &
-                                     , T const &a) noexcept
-  requires std::same_as<D, regular_type> || std::same_as<D, pedantic_type>
+  EVE_FORCEINLINE constexpr auto arg_(EVE_SUPPORTS(cpu_), D const &decorator, T const &a) noexcept
+      requires(contains<D>(types<regular_type, pedantic_type> {}))
   {
-    if constexpr(native<T>)
+    if constexpr( has_native_abi_v<T> )
     {
-      auto z =  if_else(is_negative(a), Pi(as(a)), eve::zero_);
+      auto z = if_else(is_negative(a), Pi(as(a)), eve::zero_);
       if constexpr( platform::supports_nans && std::same_as<D, pedantic_type> )
       {
-           return if_else(is_nan(a), eve::allbits_, z);
+        return if_else(is_nan(a), eve::allbits_, z);
       }
-      else return z;
+      else
+      {
+        return z;
+      }
     }
-    else   return apply_over(D()(arg), a);
+    else
+    {
+      return apply_over(decorator(arg), a);
+    }
   }
 
   template<floating_real_value T>
-  EVE_FORCEINLINE constexpr auto arg_(EVE_SUPPORTS(cpu_)
-                                     , T const &a) noexcept
+  EVE_FORCEINLINE constexpr auto arg_(EVE_SUPPORTS(cpu_), T const &a) noexcept
   {
-    return arg(regular_type(), a);
+    return arg(regular_type {}, a);
   }
 }
 
