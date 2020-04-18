cf5e2268820800f745fff3c5d6fc791af44dd7ab
jtlap <jtlapreste@gmail.com>
2020-04-18 18:47:21 +0200

    rec

--- include/eve/module/core/function/simd/ppc/rec.hpp
+++ include/eve/module/core/function/simd/ppc/rec.hpp
@@ -11,8 +11,7 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_PPC_REC_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_PPC_REC_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
+#include <eve/detail/implementation.hpp>
 #include <eve/function/refine_rec.hpp>
 #include <eve/function/bit_or.hpp>
 #include <eve/function/if_else.hpp>
@@ -20,27 +19,18 @@
 #include <eve/forward.hpp>
 #include <eve/function/raw.hpp>
 #include <eve/function/pedantic.hpp>
-
-#if !defined(EVE_NO_INFINITIES)
-#  include <eve/constant/mzero.hpp>
-#  include <eve/function/bit_and.hpp>
-#  include <eve/function/is_infinite.hpp>
-#endif
+#include <eve/concept/value.hpp>
 
 namespace eve::detail
 {
-  template<typename T, typename N>
+  template<floating_real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, ppc_>
                   rec_(EVE_SUPPORTS(vmx_), raw_type const &mode, wide<T, N, ppc_> const &v0) noexcept
   {
-    if constexpr(std::is_floating_point_v<T>) { return vec_re(v0.storage()); }
-    else
-    {
-      return rec_(EVE_RETARGET(cpu_), mode, v0);
-    }
+    return vec_re(v0.storage());
   }
 
-  template<typename T, typename N>
+  template<floating_real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, ppc_> rec_(EVE_SUPPORTS(vmx_), wide<T, N, ppc_> const &v0) noexcept
   {
     if constexpr(std::is_same_v<double, T>)
@@ -52,32 +42,21 @@ namespace eve::detail
     {
       return refine_rec(v0, raw_(rec)(v0));
     }
-    else
-    {
-      return rec_(EVE_RETARGET(cpu_), v0);
-    }
   }
 
-  template<typename T, typename N>
+  template<floating_real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, ppc_>
                   rec_(EVE_SUPPORTS(vmx_), pedantic_type const &mode, wide<T, N, ppc_> const &v0) noexcept
   {
-    if constexpr(std::is_floating_point_v<T>)
-    {
-      auto estimate = rec(v0);
+    auto estimate = rec(v0);
 
-      estimate = if_else(is_eqz(v0), bit_or(v0, Inf(as(v0))), estimate);
+    estimate = if_else(is_eqz(v0), bit_or(v0, Inf(as(v0))), estimate);
 
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_PPC_REC_HPP_INCLUDED
-      estimate = if_else(is_infinite(v0), bit_and(v0, Mzero(as(v0))), estimate);
+    estimate = if_else(is_infinite(v0), bit_and(v0, Mzero(as(v0))), estimate);
 #endif
 
-      return estimate;
-    }
-    else
-    {
-      return rec_(EVE_RETARGET(cpu_), mode, v0);
-    }
+    return estimate;
   }
 }
 
