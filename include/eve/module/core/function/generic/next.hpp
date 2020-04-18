5dc9f98908549666cf317b747e51324b8cdb9464
jtlap <jtlapreste@gmail.com>
2020-04-18 08:33:36 +0200

    negatenz next

--- include/eve/module/core/function/generic/next.hpp
+++ include/eve/module/core/function/generic/next.hpp
@@ -11,25 +11,20 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_GENERIC_NEXT_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_GENERIC_NEXT_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/meta.hpp>
-#include <eve/detail/abi.hpp>
+#include <eve/detail/implementation.hpp>
 #include <eve/module/core/detail/generic/next_kernel.hpp>
 #include <eve/function/saturated.hpp>
+#include <eve/function/pedantic.hpp>
 #include <eve/function/regular.hpp>
 #include <eve/function/logical_and.hpp>
-#include <eve/function/add.hpp>
 #include <eve/function/convert.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/function/is_eqz.hpp>
-#include <eve/function/is_ltz.hpp>
 #include <eve/function/is_negative.hpp>
 #include <eve/function/is_positive.hpp>
 #include <eve/function/is_nan.hpp>
 #include <eve/function/inc.hpp>
 #include <eve/function/dec.hpp>
-#include <eve/function/add.hpp>
-#include <eve/function/prev.hpp>
 #include <eve/constant/allbits.hpp>
 #include <eve/constant/zero.hpp>
 #include <eve/constant/mzero.hpp>
@@ -45,59 +40,80 @@ namespace eve::detail
   EVE_FORCEINLINE constexpr auto next_(EVE_SUPPORTS(cpu_)
                                       , T const &a) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T>)
     {
-      return bitfloating(inc(bitinteger(a))); 
+      if constexpr(floating_value<T>)
+      {
+        return bitfloating(inc(bitinteger(a)));
+      }
+      else if constexpr(integral_value<T>)
+      {
+        return inc(a);
+      }
     }
-    else if constexpr(integral_value<T>)
+    else
     {
-      return inc(a); 
+      return apply_over(next, a);
     }
   }
-  
+
   //////////////////////////////////////////////////////////////
   // pedantic call
   template<real_value T>
   EVE_FORCEINLINE constexpr auto next_(EVE_SUPPORTS(cpu_)
-                                      , pedantic_type const & 
+                                      , pedantic_type const &
                                       , T const &a) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T>)
     {
-      auto pz = bitinteger(a); 
-      auto z  = bitfloating( inc(pz) );
-      auto test = is_negative(a) && is_positive(z);
-      if constexpr(scalar_value<T>)
+      if constexpr(floating_value<T>)
       {
-        return test ? (z == 0 ? T(-0.) : bitfloating(pz)) : z;
+        auto pz = bitinteger(a);
+        auto z  = bitfloating( inc(pz) );
+        auto test = is_negative(a) && is_positive(z);
+        if constexpr(scalar_value<T>)
+        {
+          return test ? (z == 0 ? T(-0.) : bitfloating(pz)) : z;
+        }
+        else
+        {
+          return if_else(test, if_else(is_eqz(z), Mzero<T>(), bitfloating(pz)), z);
+        }
       }
-      else
+      else if constexpr(integral_value<T>)
       {
-        return if_else(test, if_else(is_eqz(z), Mzero<T>(), bitfloating(pz)), z);
+        return saturated_(inc)(a);
       }
     }
-    else if constexpr(integral_value<T>)
+    else
     {
-      return saturated_(inc)(a); 
+      return apply_over(pedantic_(next), a);
     }
   }
-  
+
   ////////////////////////////////////////////////
   // saturated_ call
   template< real_value T>
   EVE_FORCEINLINE constexpr auto next_(EVE_SUPPORTS(cpu_)
-                                      , saturated_type const & 
+                                      , saturated_type const &
                                       , T const &a) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T>)
     {
-      auto z =  next(a); 
-      if constexpr(eve::platform::supports_nans) return if_else(is_nan(a), eve::allbits_, z);
-      else                                       return z;
+      if constexpr(floating_value<T>)
+      {
+        auto z =  next(a);
+        if constexpr(eve::platform::supports_nans) return if_else(is_nan(a), eve::allbits_, z);
+        else                                       return z;
+      }
+      else if constexpr(integral_value<T>)
+      {
+        return saturated_(inc)(a);
+      }
     }
-    else if constexpr(integral_value<T>)
+    else
     {
-      return saturated_(inc)(a);
+      return apply_over(saturated_(next), a);
     }
   }
 
@@ -110,61 +126,82 @@ namespace eve::detail
                                       , T const &a
                                       , U const &n) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T> && has_native_abi_v<U>)
     {
-      using i_t = as_integer_t<value_type_t<T>>; 
-      return bitfloating(bitinteger(a)+  convert(n, as<i_t>()));
+      if constexpr(floating_value<T>)
+      {
+        using i_t = as_integer_t<value_type_t<T>>;
+        return bitfloating(bitinteger(a)+  convert(n, as<i_t>()));
+      }
+      else if constexpr(integral_value<T>)
+      {
+        return add(a, convert(n, as<value_type_t<T>>()));
+      }
     }
-    else if constexpr(integral_value<T>)
+    else
     {
-      return add(a, convert(n, as<value_type_t<T>>()));
+      return apply_over(next, a, n);
     }
   }
   //////////////////////////////////////////////////////////////
   // pedantic_ call
   template<real_value T, integral_real_value U>
   EVE_FORCEINLINE constexpr auto next_(EVE_SUPPORTS(cpu_)
-                                     ,  pedantic_type const & 
+                                     ,  pedantic_type const &
                                       , T const &a
                                       , U const &n) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T> && has_native_abi_v<U>)
     {
-      using i_t = as_integer_t<value_type_t<T>>; 
-      auto pz = dec(bitinteger(a) +  convert(n, as<i_t>()));
-      auto z  = bitfloating( inc(pz) );
-      auto test = is_negative(a) && is_positive(z);
-      if constexpr(scalar_value<T> && scalar_value<U>)
+      if constexpr(floating_value<T>)
       {
-        return test ? (z == 0 ? T(-0.) : bitfloating(pz)) : z;
+        using i_t = as_integer_t<value_type_t<T>>;
+        auto pz = dec(bitinteger(a) +  convert(n, as<i_t>()));
+        auto z  = bitfloating( inc(pz) );
+        auto test = is_negative(a) && is_positive(z);
+        if constexpr(scalar_value<T> && scalar_value<U>)
+        {
+          return test ? (z == 0 ? T(-0.) : bitfloating(pz)) : z;
+        }
+        else
+        {
+          return if_else(test, if_else(is_eqz(z), Mzero<T>(), bitfloating(pz)), z);
+        }
       }
-      else
+      else if constexpr(integral_value<T>)
       {
-        return if_else(test, if_else(is_eqz(z), Mzero<T>(), bitfloating(pz)), z);
+        return saturated_(add)(a, convert(n, as<value_type_t<T>>()));
       }
-    }
-    else if constexpr(integral_value<T>)
+   }
+    else
     {
-      return saturated_(add)(a, convert(n, as<value_type_t<T>>()));
+      return apply_over(pedantic_(next), a, n);
     }
   }
 
 
   template<real_value T, integral_value U>
   EVE_FORCEINLINE constexpr auto next_(EVE_SUPPORTS(cpu_)
-                                      , saturated_type const & 
+                                      , saturated_type const &
                                       , T const &a
                                       , U const &n) noexcept
   {
-    if constexpr(floating_value<T>)
+    if constexpr(has_native_abi_v<T> && has_native_abi_v<U>)
     {
-      auto z =  next(a, n); 
-      if constexpr(eve::platform::supports_nans) return if_else(is_nan(a), eve::allbits_, z);
-      else                                       return z;
+      if constexpr(floating_value<T>)
+      {
+        auto z =  next(a, n);
+        if constexpr(eve::platform::supports_nans) return if_else(is_nan(a), eve::allbits_, z);
+        else                                       return z;
+      }
+      else if constexpr(integral_value<T>)
+      {
+        return saturated_(add)(a, convert(n, as<value_type_t<T>>()));
+      }
     }
-    else if constexpr(integral_value<T>)
+    else
     {
-      return saturated_(add)(a, convert(n, as<value_type_t<T>>()));
+      return apply_over(pedantic_(next), a, n);
     }
   }
 }
