6cf72abbe948e1e9ac89c3d02eb272002ce8925f
Joel Falcou <joel.falcou@lri.fr>
2020-04-21 23:43:49 +0200

    Cleanup: ppc store

--- include/eve/module/core/function/simd/ppc/store.hpp
+++ include/eve/module/core/function/simd/ppc/store.hpp
@@ -11,30 +11,29 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_PPC_STORE_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_PPC_STORE_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/meta.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/memory/aligned_ptr.hpp>
 #include <eve/arch/limits.hpp>
-#include <eve/concept/vectorizable.hpp>
-#include <eve/forward.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/memory/aligned_ptr.hpp>
 
 namespace eve::detail
 {
-  template<typename T, typename N>
-  EVE_FORCEINLINE auto store_(EVE_SUPPORTS(vmx_),
-                              wide<T, N, ppc_> const &value,
-                              T *                     ptr) noexcept Requires(void, Vectorizable<T>)
+  template<scalar_value T, typename N>
+  EVE_FORCEINLINE void store_(EVE_SUPPORTS(vmx_), wide<T, N, ppc_> const &value, T *ptr) noexcept
   {
-    if constexpr(N::value * sizeof(T) == limits<vmx_>::bytes)
+    if constexpr( N::value * sizeof(T) == limits<vmx_>::bytes )
     {
-      if constexpr(current_api == eve::vmx)
-      { *((typename wide<T, N, ppc_>::storage_type *)(ptr)) = value; }
-      else if constexpr(current_api == eve::vsx)
+      if constexpr( current_api == eve::vmx )
+      {
+        *((typename wide<T, N, ppc_>::storage_type *)(ptr)) = value;
+      }
+      else if constexpr( current_api == eve::vsx )
       {
         // 64bits integrals are not supported by vec_vsx_st on some compilers
-        if constexpr(sizeof(T) == 8 && std::is_integral_v<T>)
-        { vec_vsx_st((__vector double)(value.storage()), 0, (double *)(ptr)); }
+        if constexpr( sizeof(T) == 8 && std::is_integral_v<T> )
+        {
+          vec_vsx_st((__vector double)(value.storage()), 0, (double *)(ptr));
+        }
         else
         {
           vec_vsx_st(value.storage(), 0, ptr);
@@ -42,16 +41,17 @@ namespace eve::detail
       }
     }
     else
-      apply<N::value>([&](auto... I) { ((*ptr++ = value[ I ]), ...); });
+      apply<N::value>([&](auto... I) { ((*ptr++ = value[I]), ...); });
   }
 
-  template<typename T, typename S, std::size_t N>
-  EVE_FORCEINLINE auto store_(EVE_SUPPORTS(vmx_),
-                              wide<T, S, ppc_> const &value,
-                              aligned_ptr<T, N>       ptr) noexcept Requires(void, Vectorizable<T>)
+  template<scalar_value T, typename S, std::size_t N>
+  EVE_FORCEINLINE void
+  store_(EVE_SUPPORTS(vmx_), wide<T, S, ppc_> const &value, aligned_ptr<T, N> ptr)
   {
-    if constexpr((N >= limits<vmx_>::bytes) && current_api == eve::vmx)
-    { vec_st(value.storage(), 0, ptr.get()); }
+    if constexpr( (N >= limits<vmx_>::bytes) && current_api == eve::vmx )
+    {
+      vec_st(value.storage(), 0, ptr.get());
+    }
     else
     {
       store(value, ptr.get());
