8676d2756a5ffe4d9c10cde921cee259ff1b9a75
Joel Falcou <joel.falcou@lri.fr>
2020-04-21 17:53:53 +0200

    Fix doc usage of fixed

--- test/doc/core/div.cpp
+++ test/doc/core/div.cpp
@@ -5,9 +5,9 @@
 int main()
 {
   using namespace eve::literal;
-  eve::wide<float, eve::fixed<4>> w([](auto i, auto) { return 1.f + i; });
+  eve::wide<float, eve::fixed<4>> w([](auto i, auto) { return 1.f + i; }), y(3.f);
   std::cout << "div(3.f, 5.5f) = " << eve::div(3.f, 5.5f) << '\n';
-  std::cout << "3._wf  = " << 3._wf << std::endl;
+  std::cout << "3._wf  = " << y << std::endl;
   std::cout << "w      = " << w << std::endl;
-  std::cout << "div(3._wf, w) = " << eve::div(3._wf, w) << '\n';
+  std::cout << "div(3._wf, w) = " << eve::div(y, w) << '\n';
 }
