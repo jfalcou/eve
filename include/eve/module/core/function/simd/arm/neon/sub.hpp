53a6558e580406e4836a688d6a8e7696232241a3
jtlap <jtlapreste@gmail.com>
2020-04-22 18:34:28 +0200

    sub

--- include/eve/module/core/function/simd/arm/neon/sub.hpp
+++ include/eve/module/core/function/simd/arm/neon/sub.hpp
@@ -11,22 +11,21 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_SUB_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_ARM_NEON_SUB_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
 
 namespace eve::detail
 {
-  template<typename T, typename N>
+  template<real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, neon64_>
-  sub_(EVE_SUPPORTS(neon128_), wide<T, N, neon64_> v0, wide<T, N, neon64_> const &v1) noexcept
+                  sub_(EVE_SUPPORTS(neon128_), wide<T, N, neon64_> v0, wide<T, N, neon64_> const &v1) noexcept
   {
     return v0 -= v1;
   }
 
-  template<typename T, typename N>
+  template<real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, neon128_>
-  sub_(EVE_SUPPORTS(neon128_), wide<T, N, neon128_> v0, wide<T, N, neon128_> const &v1) noexcept
+                  sub_(EVE_SUPPORTS(neon128_), wide<T, N, neon128_> v0, wide<T, N, neon128_> const &v1) noexcept
   {
     return v0 -= v1;
   }
--- include/eve/module/core/function/simd/x86/sub.hpp
+++ include/eve/module/core/function/simd/x86/sub.hpp
@@ -11,84 +11,100 @@
 #ifndef EVE_MODULE_CORE_FUNCTION_SIMD_X86_SUB_HPP_INCLUDED
 #define EVE_MODULE_CORE_FUNCTION_SIMD_X86_SUB_HPP_INCLUDED
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
 #include <eve/function/saturated.hpp>
+
 #include <type_traits>
 
 namespace eve::detail
 {
   // -----------------------------------------------------------------------------------------------
   // 128 bits implementation
-  template<typename T, typename N>
+  template<real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, sse_>
-  sub_(EVE_SUPPORTS(sse2_), wide<T, N, sse_> v0, wide<T, N, sse_> const &v1) noexcept
+                  sub_(EVE_SUPPORTS(sse2_), wide<T, N, sse_> v0, wide<T, N, sse_> const &v1) noexcept
   {
     return v0 -= v1;
   }
 
   // -----------------------------------------------------------------------------------------------
   // 256 bits implementation
-  template<typename T, typename N>
+  template<real_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N, avx_>
-  sub_(EVE_SUPPORTS(avx_), wide<T, N, avx_> v0, wide<T, N, avx_> const &v1) noexcept
+                  sub_(EVE_SUPPORTS(avx_), wide<T, N, avx_> v0, wide<T, N, avx_> const &v1) noexcept
   {
     return v0 -= v1;
   }
 
   // -----------------------------------------------------------------------------------------------
   // 128 bits saturated_ implementation
-  template<typename T, typename N>
-  EVE_FORCEINLINE wide<T, N, sse_> sub_(EVE_SUPPORTS(sse2_)
-                                       , saturated_type const & st
-                                       , wide<T, N, sse_> const &v0
-                                       , wide<T, N, sse_> const &v1) noexcept
+  template<real_scalar_value T, typename N>
+  EVE_FORCEINLINE wide<T, N, sse_> sub_(EVE_SUPPORTS(sse2_),
+                                        saturated_type const &  st,
+                                        wide<T, N, sse_> const &v0,
+                                        wide<T, N, sse_> const &v1) noexcept
   {
-    if constexpr(std::is_floating_point_v<T>)  return sub(v0, v1);
-    else if constexpr(current_api >= avx2)
+    if constexpr( std::is_floating_point_v<T> )
+      return sub(v0, v1);
+    else if constexpr( current_api >= avx2 )
     {
-      if constexpr(std::is_signed_v<T>)
+      if constexpr( std::is_signed_v<T> )
       {
-        if constexpr(sizeof(T) == 1) return _mm_subs_epi8(v0, v1);
-        else if constexpr(sizeof(T) == 2) return _mm_subs_epi16(v0, v1);
-        else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+        if constexpr( sizeof(T) == 1 )
+          return _mm_subs_epi8(v0, v1);
+        else if constexpr( sizeof(T) == 2 )
+          return _mm_subs_epi16(v0, v1);
+        else
+          return sub_(EVE_RETARGET(cpu_), st, v0, v1);
       }
-      else // if constexpr(std::is_unsigned_v<T>)
+      else if constexpr( std::is_unsigned_v<T> )
       {
-        if constexpr(sizeof(T) == 1) return _mm_subs_epu8(v0, v1);
-        else if constexpr(sizeof(T) == 2) return _mm_subs_epu16(v0, v1);
-        else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+        if constexpr( sizeof(T) == 1 )
+          return _mm_subs_epu8(v0, v1);
+        else if constexpr( sizeof(T) == 2 )
+          return _mm_subs_epu16(v0, v1);
+        else
+          return sub_(EVE_RETARGET(cpu_), st, v0, v1);
       }
     }
-    else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+    else
+      return sub_(EVE_RETARGET(cpu_), st, v0, v1);
   }
 
   // -----------------------------------------------------------------------------------------------
   // 256 bits saturated_ implementation
-  template<typename T, typename N>
-  EVE_FORCEINLINE wide<T, N, avx_> sub_(EVE_SUPPORTS(avx2_)
-                                       , saturated_type const & st
-                                       , wide<T, N, avx_> const &v0
-                                       , wide<T, N, avx_> const &v1) noexcept
+  template<real_scalar_value T, typename N>
+  EVE_FORCEINLINE wide<T, N, avx_> sub_(EVE_SUPPORTS(avx2_),
+                                        saturated_type const &  st,
+                                        wide<T, N, avx_> const &v0,
+                                        wide<T, N, avx_> const &v1) noexcept
   {
-    if constexpr(std::is_floating_point_v<T>)  return sub(v0, v1);
-    else if constexpr(current_api >= avx2)
+    if constexpr( std::is_floating_point_v<T> )
+      return sub(v0, v1);
+    else if constexpr( current_api >= avx2 )
     {
-      if constexpr(std::is_signed_v<T>)
+      if constexpr( std::is_signed_v<T> )
       {
-        if constexpr(sizeof(T) == 1) return _mm256_subs_epi8(v0, v1);
-        else if constexpr(sizeof(T) == 2) return _mm256_subs_epi16(v0, v1);
-        else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+        if constexpr( sizeof(T) == 1 )
+          return _mm256_subs_epi8(v0, v1);
+        else if constexpr( sizeof(T) == 2 )
+          return _mm256_subs_epi16(v0, v1);
+        else
+          return sub_(EVE_RETARGET(cpu_), st, v0, v1);
       }
-      else // if constexpr(std::is_unsigned_v<T>)
+      else if constexpr( std::is_unsigned_v<T> )
       {
-        if constexpr(sizeof(T) == 1) return _mm256_subs_epu8(v0, v1);
-        else if constexpr(sizeof(T) == 2) return _mm256_subs_epu16(v0, v1);
-        else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+        if constexpr( sizeof(T) == 1 )
+          return _mm256_subs_epu8(v0, v1);
+        else if constexpr( sizeof(T) == 2 )
+          return _mm256_subs_epu16(v0, v1);
+        else
+          return sub_(EVE_RETARGET(cpu_), st, v0, v1);
       }
     }
-    else return sub_(EVE_RETARGET(cpu_),  st, v0, v1);
+    else
+      return sub_(EVE_RETARGET(cpu_), st, v0, v1);
   }
 }
 
