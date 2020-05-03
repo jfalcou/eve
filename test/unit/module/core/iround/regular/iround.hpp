3e9a8dd9f4c66fb4038e1b363fb7457ff839fd4a
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 00:21:10 +0200

    Cleanup iround

--- test/unit/module/core/iround/regular/iround.hpp
+++ test/unit/module/core/iround/regular/iround.hpp
@@ -19,20 +19,20 @@
 
 TTS_CASE_TPL("Check eve::upward_(eve::iround) return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::upward_(eve::iround)(T(0)), (eve::detail::as_integer_t<T>));
+  TTS_EXPR_IS(eve::upward_(eve::iround)(T(0)), eve::detail::as_integer_t<T>);
 }
 
 TTS_CASE_TPL("Check eve::upward_(eve::iround) behavior", EVE_TYPE)
 {
-  TTS_EQUAL(eve::upward_(eve::iround)((T(1.7))), eve::iceil   ((T(1.7))) );
-  TTS_EQUAL(eve::downward_(eve::iround)((T(1.7))), eve::ifloor  ((T(1.7))) );
-  TTS_EQUAL(eve::toward_zero_(eve::iround)((T(1.7))), eve::itrunc  ((T(1.7))) );
-  TTS_EQUAL(eve::to_nearest_(eve::iround)((T(1.7))), eve::inearest((T(1.7))) );
-  TTS_EQUAL(eve::iround((T(1.7))), eve::inearest((T(1.7))) );
+  TTS_EQUAL(eve::iround(T(1.7)), eve::inearest((T(1.7))) );
+  TTS_EQUAL(eve::upward_     (eve::iround)( T(1.7) ), eve::iceil   ( T(1.7) ) );
+  TTS_EQUAL(eve::downward_   (eve::iround)( T(1.7) ), eve::ifloor  ( T(1.7) ) );
+  TTS_EQUAL(eve::toward_zero_(eve::iround)( T(1.7) ), eve::itrunc  ( T(1.7) ) );
+  TTS_EQUAL(eve::to_nearest_ (eve::iround)( T(1.7) ), eve::inearest( T(1.7) ) );
 
-  TTS_EQUAL(eve::upward_(eve::iround)((T(1.3))), eve::iceil   ((T(1.3))) );
-  TTS_EQUAL(eve::downward_(eve::iround)((T(1.3))), eve::ifloor  ((T(1.3))) );
-  TTS_EQUAL(eve::toward_zero_(eve::iround)((T(1.3))), eve::itrunc  ((T(1.3))) );
-  TTS_EQUAL(eve::to_nearest_(eve::iround)((T(1.3))), eve::inearest((T(1.3))) );
-  TTS_EQUAL(eve::iround((T(1.3))), eve::inearest((T(1.3))) );
+  TTS_EQUAL(eve::iround(T(1.3)), eve::inearest(T(1.3)) );
+  TTS_EQUAL(eve::upward_     (eve::iround)(T(1.3)), eve::iceil   (T(1.3)) );
+  TTS_EQUAL(eve::downward_   (eve::iround)(T(1.3)), eve::ifloor  (T(1.3)) );
+  TTS_EQUAL(eve::toward_zero_(eve::iround)(T(1.3)), eve::itrunc  (T(1.3)) );
+  TTS_EQUAL(eve::to_nearest_ (eve::iround)(T(1.3)), eve::inearest(T(1.3)) );
 }
