a294a00bc3e9e6354194e0a884bad5c7e406e751
Joel Falcou <joel.falcou@lri.fr>
2020-05-03 23:58:58 +0200

    Cleanup: coth

--- test/unit/module/core/coth/regular/coth.hpp
+++ test/unit/module/core/coth/regular/coth.hpp
@@ -10,14 +10,13 @@
 //==================================================================================================
 #include <eve/function/coth.hpp>
 #include <eve/function/rec.hpp>
-#include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
 #include <eve/platform.hpp>
-#include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
+#include <cmath>
 
 TTS_CASE_TPL("Check eve::coth return type", EVE_TYPE)
 {
@@ -26,14 +25,17 @@ TTS_CASE_TPL("Check eve::coth return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::eve::coth behavior", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
+
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::coth(eve::Nan<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::coth(eve::Inf<T>()) , (T(1)) );
-    TTS_IEEE_EQUAL(eve::coth(eve::Minf<T>()), (T(-1)) );
+    TTS_IEEE_EQUAL(eve::coth(eve::Nan<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::coth(eve::Inf<T>()) , T( 1)         );
+    TTS_IEEE_EQUAL(eve::coth(eve::Minf<T>()), T(-1)         );
   }
-  TTS_ULP_EQUAL(eve::coth(T(1)), T(eve::rec(std::tanh(v_t(1)))), 0.5);
-  TTS_ULP_EQUAL(eve::coth(T(-1)),T(eve::rec(std::tanh(v_t(-1)))), 0.5);
-  TTS_IEEE_EQUAL((eve::coth(T(0))), (eve::Inf<T>()));
-  TTS_IEEE_EQUAL((eve::coth(T(-0.))), (eve::Minf<T>()));
+
+  TTS_ULP_EQUAL (eve::coth(T( 1 )), T(eve::rec(std::tanh(v_t(1)))), 0.5);
+  TTS_ULP_EQUAL (eve::coth(T(-1 )), T(eve::rec(std::tanh(v_t(-1)))), 0.5);
+  TTS_IEEE_EQUAL(eve::coth(T( 0 )), eve::Inf<T>() );
+  TTS_IEEE_EQUAL(eve::coth(T(-0.)), eve::Minf<T>());
 }
