4649b5e4278f538c0d94554f15c33f95c66f9364
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 00:28:40 +0200

    Cleanup: is_equal

--- test/unit/module/core/is_equal/numeric/is_equal.hpp
+++ test/unit/module/core/is_equal/numeric/is_equal.hpp
@@ -18,18 +18,20 @@
 
 TTS_CASE_TPL("Check eve::numeric_(eve::is_equal) return type", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
   using eve::logical;
 
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(T()          , T()          ), (logical<T>));
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(T()          , v_t()         ), (logical<T>));
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(v_t()         , T()          ), (logical<T>));
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<T>() , logical<T>() ), (logical<T>));
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<T>() , logical<v_t>()), (logical<T>));
-  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<v_t>(), logical<T>() ), (logical<T>));
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(  T()         ,   T()         ) , logical<T>);
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(  T()         , v_t()         ) , logical<T>);
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(v_t()         ,   T()         ) , logical<T>);
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<T>()  , logical<T>()  ) , logical<T>);
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<T>()  , logical<v_t>()) , logical<T>);
+  TTS_EXPR_IS( eve::numeric_(eve::is_equal)(logical<v_t>(), logical<T>()  ) , logical<T>);
 }
 
 TTS_CASE_TPL("Check eve::numeric_(eve::is_equal) behavior", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
   using eve::logical;
 
   if constexpr(eve::platform::supports_nans && eve::floating_value<T>)
@@ -38,10 +40,10 @@ TTS_CASE_TPL("Check eve::numeric_(eve::is_equal) behavior", EVE_TYPE)
     TTS_EQUAL(eve::numeric_(eve::is_equal)(eve::Nan<T>(), T(4))            , eve::False<T>());
   }
 
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(1)           , T(1) )           , eve::True<T>() );
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(1)           , v_t(1))           , eve::True<T>() );
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(3)           , T(1) )           , eve::False<T>());
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(3)           , v_t(1))           , eve::False<T>());
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(eve::True<T>() , eve::True<T>())  , eve::True<T>() );
-  TTS_EQUAL(eve::numeric_(eve::is_equal)(eve::True<T>() , eve::False<T>()) , eve::False<T>());
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(1)           ,   T(1) )        , eve::True<T>() );
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(1)           , v_t(1))         , eve::True<T>() );
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(3)           ,   T(1) )        , eve::False<T>());
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(T(3)           , v_t(1))         , eve::False<T>());
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(eve::True<T>() , eve::True<T>()) , eve::True<T>() );
+  TTS_EQUAL(eve::numeric_(eve::is_equal)(eve::True<T>() , eve::False<T>()), eve::False<T>());
 }
