2af7defb031110d73963aab1d85633f30688c6a9
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 00:13:13 +0200

    Cleanup inearest

--- test/unit/module/core/inearest/regular/inearest.hpp
+++ test/unit/module/core/inearest/regular/inearest.hpp
@@ -15,12 +15,13 @@
 
 TTS_CASE_TPL("Check inearest return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::inearest(T()), (eve::detail::as_integer_t<T>));
+  TTS_EXPR_IS(eve::inearest(T()), eve::detail::as_integer_t<T>);
 }
 
 TTS_CASE_TPL("Check eve::inearest behavior", EVE_TYPE)
 {
   using i_t = eve::detail::as_integer_t<T>;
+
   TTS_EQUAL(eve::inearest(T(0)), i_t(0));
   TTS_EQUAL(eve::inearest(T(1)), i_t(1));
   TTS_EQUAL(eve::inearest(T(2)), i_t(2));
@@ -36,14 +37,14 @@ TTS_CASE_TPL("Check eve::inearest behavior", EVE_TYPE)
    TTS_EQUAL(eve::inearest(T(-1.3)), i_t(-1));
    TTS_EQUAL(eve::inearest(T(-1.5)), i_t(-2));
    TTS_EQUAL(eve::inearest(T(-1.6)), i_t(-2));
-   TTS_EQUAL(eve::inearest(T(1.3)) , i_t(1));
-   TTS_EQUAL(eve::inearest(T(1.5)) , i_t(2));
-   TTS_EQUAL(eve::inearest(T(1.6)) , i_t(2));
+   TTS_EQUAL(eve::inearest(T( 1.3)) , i_t( 1));
+   TTS_EQUAL(eve::inearest(T( 1.5)) , i_t( 2));
+   TTS_EQUAL(eve::inearest(T( 1.6)) , i_t( 2));
    TTS_EQUAL(eve::inearest(T(-2.3)), i_t(-2));
    TTS_EQUAL(eve::inearest(T(-2.5)), i_t(-2));
    TTS_EQUAL(eve::inearest(T(-2.6)), i_t(-3));
-   TTS_EQUAL(eve::inearest(T(2.3)) , i_t(2));
-   TTS_EQUAL(eve::inearest(T(2.5)) , i_t(2));
-   TTS_EQUAL(eve::inearest(T(2.6)) , i_t(3));
+   TTS_EQUAL(eve::inearest(T( 2.3)) , i_t( 2));
+   TTS_EQUAL(eve::inearest(T( 2.5)) , i_t( 2));
+   TTS_EQUAL(eve::inearest(T( 2.6)) , i_t( 3));
   }
 }
