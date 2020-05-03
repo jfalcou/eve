f4799c4345a5b187fc39f1ca1f6469d6476535bf
Joel Falcou <joel.falcou@lri.fr>
2020-05-03 13:45:03 +0200

    Cleanup for: nb_values

--- test/unit/module/core/nb_values/regular/nb_values.hpp
+++ test/unit/module/core/nb_values/regular/nb_values.hpp
@@ -8,28 +8,20 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/false.hpp>
-#include <eve/constant/true.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
+#include <eve/function/nb_values.hpp>
+#include <eve/function/is_even.hpp>
+#include <eve/function/is_odd.hpp>
+#include <eve/function/minus.hpp>
+#include <eve/function/inc.hpp>
+#include <eve/function/dec.hpp>
+#include <eve/function/all.hpp>
 #include <eve/constant/mindenormal.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
 #include <eve/constant/eps.hpp>
 #include <eve/constant/nan.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/function/inc.hpp>
-#include <eve/function/dec.hpp>
-#include <eve/function/is_odd.hpp>
-#include <eve/function/is_even.hpp>
-#include <eve/function/all.hpp>
-#include <eve/function/nb_values.hpp>
-#include <eve/function/minus.hpp>
-#include <type_traits>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 
 TTS_CASE_TPL("Check eve::nb_values return type", EVE_TYPE)
 {
@@ -39,41 +31,33 @@ TTS_CASE_TPL("Check eve::nb_values return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::nb_values  behavior", EVE_TYPE)
 {
-  using eve::nb_values;
-  TTS_EXPR_IS( nb_values(T(), T()), (eve::detail::as_integer_t<T, unsigned>));
   using r_t = eve::detail::as_integer_t<T, unsigned>;
+
   if constexpr(eve::floating_value<T>)
   {
     if constexpr(eve::platform::supports_invalids)
     {
-      TTS_EQUAL(nb_values(eve::Inf<T>(), eve::Inf<T>()), eve::Zero<r_t>());
-      TTS_EQUAL(nb_values(eve::Minf<T>(), eve::Minf<T>()), eve::Zero<r_t>());
-      TTS_EQUAL(nb_values(eve::Nan<T>(), eve::Nan<T>()), eve::Valmax<r_t>());
+      TTS_EQUAL(eve::nb_values(eve::Inf<T>()  , eve::Inf<T>())  , r_t(0)            );
+      TTS_EQUAL(eve::nb_values(eve::Minf<T>() , eve::Minf<T>()) , r_t(0)            );
+      TTS_EQUAL(eve::nb_values(eve::Nan<T>()  , eve::Nan<T>())  , eve::Valmax<r_t>());
     }
 
-    TTS_EQUAL( nb_values(eve::One<T>(), eve::inc(eve::Eps<T>()))
-             , r_t(1)
-             );
+    auto eps = eve::Eps<T>();
 
-    TTS_EQUAL( nb_values(eve::One<T>(), T(-eve::dec(eve::Eps<T>())))
-             , r_t(2)
-             );
+    TTS_EQUAL( eve::nb_values(T(1)  ,  eve::inc(eps)  ) , r_t(1));
+    TTS_EQUAL( eve::nb_values(T(1)  , -eve::dec(eps)  ) , r_t(2));
+    TTS_EQUAL( eve::nb_values(T(1)  , -eve::dec(eps/2)) , r_t(1));
+    TTS_EQUAL( eve::nb_values(T(-0.), T(0)            ) , r_t(1));
 
-    TTS_EQUAL( nb_values(eve::One<T>(), T(-eve::dec(eve::Eps<T>()/2)))
-             , r_t(1)
-             );
-    TTS_EQUAL( nb_values(T(-0.), T(T( 0 )))
-             , r_t(1)
-             );
-    TTS_EXPECT(eve::all(eve::is_odd(nb_values(T(-10), T(10)))));
+    TTS_EXPECT(eve::all(eve::is_odd(eve::nb_values(T(-10), T(10)))));
   }
   else if constexpr(eve::signed_value<T>)
   {
-    TTS_EXPECT(eve::all(eve::is_even(nb_values(T(-10), T(10)))));
+    TTS_EXPECT(eve::all(eve::is_even(eve::nb_values(T(-10), T(10)))));
   }
   else
   {
-    TTS_EQUAL((nb_values(eve::One<T>(), T(10))), (r_t(9)));
-    TTS_EQUAL(nb_values(T( 0 ), T( 0 )), eve::Zero<r_t>());
+    TTS_EQUAL(eve::nb_values(T(1), T(10)), r_t(9));
+    TTS_EQUAL(eve::nb_values(T(0), T( 0)), r_t(0));
   }
 }
