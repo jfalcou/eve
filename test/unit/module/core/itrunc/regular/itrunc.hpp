4e7e01d9e6efbc96b6e1148e075f6b23f682c8b5
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 17:34:28 +0200

    Cleanup: itrunc

--- test/unit/module/core/itrunc/regular/itrunc.hpp
+++ test/unit/module/core/itrunc/regular/itrunc.hpp
@@ -15,12 +15,13 @@
 
 TTS_CASE_TPL("Check itrunc return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::itrunc(T()), (eve::detail::as_integer_t<T>));
+  TTS_EXPR_IS(eve::itrunc(T()), eve::detail::as_integer_t<T>);
 }
 
 TTS_CASE_TPL("Check eve::itrunc behavior", EVE_TYPE)
 {
   using i_t = eve::detail::as_integer_t<T>;
+
   TTS_EQUAL(eve::itrunc(T(0)), i_t(0));
   TTS_EQUAL(eve::itrunc(T(1)), i_t(1));
   TTS_EQUAL(eve::itrunc(T(2)), i_t(2));
@@ -36,8 +37,8 @@ TTS_CASE_TPL("Check eve::itrunc behavior", EVE_TYPE)
    TTS_EQUAL(eve::itrunc(T(-1.3)), i_t(-1));
    TTS_EQUAL(eve::itrunc(T(-1.5)), i_t(-1));
    TTS_EQUAL(eve::itrunc(T(-1.6)), i_t(-1));
-   TTS_EQUAL(eve::itrunc(T(1.3)) , i_t(1));
-   TTS_EQUAL(eve::itrunc(T(1.5)) , i_t(1));
-   TTS_EQUAL(eve::itrunc(T(1.6)) , i_t(1));
+   TTS_EQUAL(eve::itrunc(T( 1.3)), i_t( 1));
+   TTS_EQUAL(eve::itrunc(T( 1.5)), i_t( 1));
+   TTS_EQUAL(eve::itrunc(T( 1.6)), i_t( 1));
   }
 }
