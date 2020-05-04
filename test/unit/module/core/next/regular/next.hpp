f7ad73d439ddb9f4b445cb31e3041eb41c4e9e28
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 16:54:16 +0200

    Cleanup: next

--- test/unit/module/core/next/regular/next.hpp
+++ test/unit/module/core/next/regular/next.hpp
@@ -9,115 +9,108 @@
 **/
 //==================================================================================================
 #include <eve/function/next.hpp>
+#include <eve/constant/mindenormal.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/eps.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/nan.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/eps.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/mindenormal.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/constant/valmax.hpp>
-#include <eve/constant/valmin.hpp>
-#include <eve/function/all.hpp>
-#include <eve/function/is_negative.hpp>
-#include <eve/function/is_positive.hpp>
-#include <type_traits>
 
 TTS_CASE_TPL("Check next return type", EVE_TYPE)
 {
-  using i_t = eve::detail::as_integer_t<T, signed>;
-  using u_t = eve::detail::as_integer_t<T, unsigned>;
-  using si_t = eve::detail::as_integer_t<v_t, signed>;
-  using su_t = eve::detail::as_integer_t<v_t, unsigned>;
+  using v_t   = eve::element_type_t<T>;
+  using i_t   = eve::detail::as_integer_t<  T, signed>;
+  using u_t   = eve::detail::as_integer_t<  T, unsigned>;
+  using si_t  = eve::detail::as_integer_t<v_t, signed>;
+  using su_t  = eve::detail::as_integer_t<v_t, unsigned>;
 
-  TTS_EXPR_IS(eve::next(T()), T);
-  TTS_EXPR_IS((eve::next(T(), i_t())), T);
-  TTS_EXPR_IS((eve::next(T(), u_t())), T);
-  TTS_EXPR_IS((eve::next(T(), si_t())), T);
-  TTS_EXPR_IS((eve::next(T(), su_t())), T);
+  TTS_EXPR_IS(eve::next(T())        , T);
+  TTS_EXPR_IS(eve::next(T(), i_t()) , T);
+  TTS_EXPR_IS(eve::next(T(), u_t()) , T);
+  TTS_EXPR_IS(eve::next(T(), si_t()), T);
+  TTS_EXPR_IS(eve::next(T(), su_t()), T);
 }
 
 TTS_CASE_TPL("Check eve::next one parameter behavior", EVE_TYPE)
 {
   if constexpr(eve::integral_value<T>)
   {
-    TTS_EQUAL(eve::next(T{0}), T(1));
-    TTS_EQUAL(eve::next(T{1}), T(2));
+    TTS_EQUAL(eve::next(T(0)), T(1));
+    TTS_EQUAL(eve::next(T(1)), T(2));
+
     if constexpr(eve::signed_value<T>)
     {
-      TTS_EQUAL(eve::next(T(-1)), T(0));
+      TTS_EQUAL(eve::next(T(-1)), T( 0));
       TTS_EQUAL(eve::next(T(-2)), T(-1));
     }
   }
   else
   {
-    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>()), (eve::Nan<T>()));
-    TTS_EQUAL(eve::next(eve::Valmax<T>())  , (eve::Inf<T>()));
-    TTS_EQUAL(eve::next(eve::Minf<T>())    , (eve::Valmin<T>()));
-    TTS_EQUAL(eve::next(eve::Mone<T>())    , (eve::Mone<T>()+eve::Eps<T>()/2));
-    TTS_EQUAL(eve::next(eve::One<T>())     , (eve::One<T>()+eve::Eps<T>()));
-    TTS_EQUAL(eve::next(-eve::Mindenormal<T>())   , (T(-0.)));
-    TTS_EQUAL(eve::next(T( 0 ))    , (eve::Mindenormal<T>()));
-    TTS_EQUAL(eve::next(T(-0.))   , (eve::Mindenormal<T>()));
+    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>())     , eve::Nan<T>()         );
+    TTS_EQUAL(eve::next(eve::Valmax<T>())       , eve::Inf<T>()         );
+    TTS_EQUAL(eve::next(eve::Minf<T>())         , eve::Valmin<T>()      );
+    TTS_EQUAL(eve::next(T(-1))                  , T(-1)+eve::Eps<T>()/2 );
+    TTS_EQUAL(eve::next(T(1))                   , T( 1)+eve::Eps<T>()   );
+    TTS_EQUAL(eve::next(-eve::Mindenormal<T>()) , T(-0.)                );
+    TTS_EQUAL(eve::next(T( 0 ))                 , eve::Mindenormal<T>() );
+    TTS_EQUAL(eve::next(T(-0.))                 , eve::Mindenormal<T>() );
   }
 }
 
 TTS_CASE_TPL("Check eve::next two parameters behavior", EVE_TYPE)
 {
-  using i_t = eve::detail::as_integer_t<T, signed>;
-  using si_t = eve::detail::as_integer_t<v_t, signed>;
+  using v_t   = eve::element_type_t<T>;
+  using i_t   = eve::detail::as_integer_t<T, signed>;
+  using si_t  = eve::detail::as_integer_t<v_t, signed>;
 
   if constexpr(eve::integral_value<T>)
   {
-    TTS_EQUAL(eve::next(T{0}, i_t(0)), T(0));
-    TTS_EQUAL(eve::next(T{0}, i_t(2)), T(2));
-    TTS_EQUAL(eve::next(T{1}, i_t(2)), T(3));
-    TTS_EQUAL(eve::next(T{0}, si_t(0)),T(0));
-    TTS_EQUAL(eve::next(T{0}, si_t(2)),T(2));
-    TTS_EQUAL(eve::next(T{1}, si_t(2)),T(3));
+    TTS_EQUAL(eve::next(T(0),  i_t(0)), T(0));
+    TTS_EQUAL(eve::next(T(0),  i_t(2)), T(2));
+    TTS_EQUAL(eve::next(T(1),  i_t(2)), T(3));
+    TTS_EQUAL(eve::next(T(0), si_t(0)), T(0));
+    TTS_EQUAL(eve::next(T(0), si_t(2)), T(2));
+    TTS_EQUAL(eve::next(T(1), si_t(2)), T(3));
 
-    TTS_EQUAL(eve::next(v_t{0}, i_t(0)), T(0));
-    TTS_EQUAL(eve::next(v_t{0}, i_t(2)), T(2));
-    TTS_EQUAL(eve::next(v_t{1}, i_t(2)), T(3));
+    TTS_EQUAL(eve::next(v_t(0), i_t(0)), T(0));
+    TTS_EQUAL(eve::next(v_t(0), i_t(2)), T(2));
+    TTS_EQUAL(eve::next(v_t(1), i_t(2)), T(3));
 
     if constexpr(eve::signed_value<T>)
     {
-      TTS_EQUAL(eve::next(T(-1), i_t(2)), T(1));
-      TTS_EQUAL(eve::next(T(-2), i_t(2)), T(0));
-      TTS_EQUAL(eve::next(T(-1), si_t(2)),T(1));
-      TTS_EQUAL(eve::next(T(-2), si_t(2)),T(0));
-
-      TTS_EQUAL(eve::next(v_t(-1), i_t(2)), T(1));
-      TTS_EQUAL(eve::next(v_t(-2), i_t(2)), T(0));
+      TTS_EQUAL(eve::next(T(-1)   ,  i_t(2)), T(1));
+      TTS_EQUAL(eve::next(T(-2)   ,  i_t(2)), T(0));
+      TTS_EQUAL(eve::next(T(-1)   , si_t(2)), T(1));
+      TTS_EQUAL(eve::next(T(-2)   , si_t(2)), T(0));
+      TTS_EQUAL(eve::next(v_t(-1) ,  i_t(2)), T(1));
+      TTS_EQUAL(eve::next(v_t(-2) ,  i_t(2)), T(0));
     }
   }
   else
   {
-    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>(), i_t(2))   , (eve::Nan<T>()));
-    TTS_IEEE_EQUAL(eve::next(eve::Valmax<T>(), i_t(2)), (eve::Nan<T>()));
-    TTS_EQUAL(eve::next(eve::Mone<T>(), i_t(2))       , (eve::Mone<T>()+eve::Eps<T>()));
-    TTS_EQUAL(eve::next(eve::One<T>(), i_t(2))        , (eve::One<T>()+eve::Eps<T>()*2));
+    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>()      ,  i_t(2) ) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::next(eve::Valmax<T>()   ,  i_t(2) ) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>()      , si_t(2) ) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::next(eve::Valmax<T>()   , si_t(2) ) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::next(eve::Inf<v_t>()    ,  i_t(2) ) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::next(eve::Valmax<v_t>() ,  i_t(2) ) , eve::Nan<T>() );
 
-    TTS_IEEE_EQUAL(eve::next(eve::Inf<T>(), si_t(2))   , (eve::Nan<T>()));
-    TTS_IEEE_EQUAL(eve::next(eve::Valmax<T>(), si_t(2)), (eve::Nan<T>()));
-    TTS_EQUAL(eve::next(eve::Mone<T>(), si_t(2))       , (eve::Mone<T>()+eve::Eps<T>()));
-    TTS_EQUAL(eve::next(eve::One<T>(), si_t(2))        , (eve::One<T>()+eve::Eps<T>()*2));
+    TTS_EQUAL(eve::next(  T(-1),  i_t(2)) , T(-1)+eve::Eps<T>()   );
+    TTS_EQUAL(eve::next(  T( 1),  i_t(2)) , T( 1)+eve::Eps<T>()*2 );
+    TTS_EQUAL(eve::next(  T(-1), si_t(2)) , T(-1)+eve::Eps<T>()   );
+    TTS_EQUAL(eve::next(  T( 1), si_t(2)) , T( 1)+eve::Eps<T>()*2 );
+    TTS_EQUAL(eve::next(v_t(-1),  i_t(2)) , T(-1)+eve::Eps<T>()   );
+    TTS_EQUAL(eve::next(v_t( 1),  i_t(2)) , T( 1)+eve::Eps<T>()*2 );
+    TTS_EQUAL(eve::next(  T( 0), 1      ) , eve::Mindenormal<T>() );
+    TTS_EQUAL(eve::next(  T(-0), 1      ) , eve::Mindenormal<T>() );
+    TTS_EQUAL(eve::next(  T( 0), 0      ) , T( 0 )                );
+    TTS_EQUAL(eve::next(  T(-0), 0      ) , T(-0.)                );
 
-    TTS_IEEE_EQUAL(eve::next(eve::Inf<v_t>(), i_t(2))   , (eve::Nan<T>()));
-    TTS_IEEE_EQUAL(eve::next(eve::Valmax<v_t>(), i_t(2)), (eve::Nan<T>()));
-    TTS_EQUAL(eve::next(eve::Mone<v_t>(), i_t(2))       , (eve::Mone<T>()+eve::Eps<T>()));
-    TTS_EQUAL(eve::next(eve::One<v_t>(), i_t(2))        , (eve::One<T>()+eve::Eps<T>()*2));
-
-    TTS_EQUAL(eve::next(T( 0 ), 1)             , (eve::Mindenormal<T>()));
-    TTS_EQUAL(eve::next(T(-0.), 1)            , (eve::Mindenormal<T>()));
-    TTS_EQUAL(eve::next(-eve::Mindenormal<T>(), 1)     , (T( 0 )));
-
-    TTS_EQUAL(eve::next(T( 0 ), 0)             , (T( 0 )));
-    TTS_EQUAL(eve::next(T(-0.), 0)            , (T(-0.)));
+    TTS_EQUAL(eve::next(-eve::Mindenormal<T>(), 1), T( 0 )        );
   }
 }
+
