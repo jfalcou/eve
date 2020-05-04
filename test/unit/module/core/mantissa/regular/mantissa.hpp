acb3466b6a6c5acc397168a680a4eedc5c23494d
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 17:33:23 +0200

    Cleanup: mantissa

--- test/unit/module/core/mantissa/regular/mantissa.hpp
+++ test/unit/module/core/mantissa/regular/mantissa.hpp
@@ -9,36 +9,30 @@
 **/
 //==================================================================================================
 #include <eve/function/mantissa.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/false.hpp>
-#include <eve/constant/true.hpp>
-#include <tts/tests/relation.hpp>
-#include <tts/tests/types.hpp>
-#include <eve/constant/mone.hpp>
-#include <eve/constant/one.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
 #include <eve/constant/nan.hpp>
-#include <type_traits>
+#include <tts/tests/relation.hpp>
+#include <tts/tests/types.hpp>
 
 TTS_CASE_TPL("Check eve::mantissa return type", EVE_TYPE)
 {
-  TTS_EXPR_IS((eve::mantissa(T(0))), T);
+  TTS_EXPR_IS(eve::mantissa(T(0)), T);
 }
 
 TTS_CASE_TPL("Check eve::mantissa  behavior", EVE_TYPE)
 {
   if constexpr(eve::platform::supports_invalids)
   {
-    TTS_EQUAL((eve::mantissa(eve::Inf<T>())), (eve::Inf<T>()));
-    TTS_EQUAL((eve::mantissa(eve::Minf<T>())), (eve::Minf<T>()));
-    TTS_IEEE_EQUAL((eve::mantissa(eve::Nan<T>())), (eve::Nan<T>()));
+    TTS_EQUAL(eve::mantissa(eve::Inf<T>())      , eve::Inf<T>() );
+    TTS_EQUAL(eve::mantissa(eve::Minf<T>())     , eve::Minf<T>());
+    TTS_IEEE_EQUAL(eve::mantissa(eve::Nan<T>()) , eve::Nan<T>() );
   }
-  TTS_EQUAL((eve::mantissa(eve::Mone<T>())), (eve::Mone<T>()));
-  TTS_EQUAL((eve::mantissa(eve::One<T>())), (eve::One<T>()));
-  TTS_EQUAL((eve::mantissa(T( 0 ))), (T( 0 )));
-  TTS_EQUAL((eve::mantissa(T(2))), eve::One<T>());
-  TTS_EQUAL((eve::mantissa(T(1.5))), T(1.5));
-  TTS_EQUAL((eve::mantissa(T(2.5))), T(1.25));
+  TTS_EQUAL(eve::mantissa(T(-1  )), T(-1    ));
+  TTS_EQUAL(eve::mantissa(T( 1  )), T( 1    ));
+  TTS_EQUAL(eve::mantissa(T( 0  )), T( 0    ));
+  TTS_EQUAL(eve::mantissa(T( 2  )), T( 1    ));
+  TTS_EQUAL(eve::mantissa(T( 1.5)), T( 1.5  ));
+  TTS_EQUAL(eve::mantissa(T( 2.5)), T( 1.25 ));
 }
 
