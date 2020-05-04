7c45641eb5bb3bfe8ff4a16a6ae5fab18919011a
Joel Falcou <joel.falcou@lri.fr>
2020-05-04 17:27:50 +0200

    Cleanup: manhattan

--- test/unit/module/core/manhattan/pedantic/manhattan.hpp
+++ test/unit/module/core/manhattan/pedantic/manhattan.hpp
@@ -29,17 +29,17 @@ TTS_CASE_TPL("Check eve::pedantic_(eve::manhattan) behavior", EVE_TYPE)
 {
   if constexpr(eve::platform::supports_invalids)
   {
-    TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Nan<T>(), eve::Inf<T>()), eve::Inf<T>(), 0);
-    TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Inf<T>(), eve::Nan<T>()), eve::Inf<T>(), 0);
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(eve::Nan<T>(), eve::Inf<T>()), eve::Inf<T>());
+    TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(eve::Inf<T>(), eve::Nan<T>()), eve::Inf<T>());
   }
 
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Valmax<T>(), (T(0)))          , eve::Valmax<T>(), 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T(0))          , eve::Valmax<T>()), eve::Valmax<T>(), 0);
+  TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(eve::Valmax<T>(), T(0))             , eve::Valmax<T>());
+  TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(T(0)            , eve::Valmax<T>()) , eve::Valmax<T>());
 
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T(-1)), (T(-1)))                  , T(2)                     , 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T( 1)), (T( 1)))                  , T(2)                     , 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T( 0)), (T( 0)))                  , (T(0))                   , 0  );
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Sqrt_2<T>(), eve::Sqrt_2<T>()), T(2)* eve::Sqrt_2<T>(), 0.5);
+  TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(T(-1), T(-1)), T(2));
+  TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(T( 1), T( 1)), T(2));
+  TTS_IEEE_EQUAL(eve::pedantic_(eve::manhattan)(T( 0), T( 0)), T(0));
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Sqrt_2<T>() , eve::Sqrt_2<T>()) , T(2)* eve::Sqrt_2<T>(), 0.5);
 }
 
 TTS_CASE_TPL("Check 3 params eve::pedantic_(eve::manhattan) behavior", EVE_TYPE)
@@ -50,11 +50,11 @@ TTS_CASE_TPL("Check 3 params eve::pedantic_(eve::manhattan) behavior", EVE_TYPE)
     TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Inf<T>(), eve::Nan<T>(), eve::Inf<T>()), eve::Inf<T>(), 0);
   }
 
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Valmax<T>(), (T(0)),              (T(0))), eve::Valmax<T>(), 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T(0))          , (eve::Valmax<T>()),  (T(0))), eve::Valmax<T>(), 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(eve::Valmax<T>(), T(0),              T(0)), eve::Valmax<T>(), 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(T(0)          , (eve::Valmax<T>()),  T(0)), eve::Valmax<T>(), 0);
 
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T(-1)), (T(-1)), T(-1)), T(3) , 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T( 1)), (T( 1)), T(-1)), T(3) , 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T( 0)), (T( 0)), (T( 0))), T(0), 0);
-  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)((T( 1)), (T( 1)), (T( 1))), T(3), 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(T(-1), T(-1), T(-1)), T(3) , 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(T( 1), T( 1), T(-1)), T(3) , 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(T( 0), T( 0), T( 0)), T(0) , 0);
+  TTS_ULP_EQUAL(eve::pedantic_(eve::manhattan)(T( 1), T( 1), T( 1)), T(3) , 0);
 }
