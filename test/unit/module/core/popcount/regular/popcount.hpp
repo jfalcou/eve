c086e3873956a91ab4a42580bef075a19d2c3df8
Joel Falcou <joel.falcou@lri.fr>
2020-05-05 16:29:05 +0200

    Cleanup: popcount

--- test/unit/module/core/popcount/regular/popcount.hpp
+++ test/unit/module/core/popcount/regular/popcount.hpp
@@ -9,28 +9,26 @@
 **/
 //==================================================================================================
 #include <eve/function/popcount.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
-#include <eve/constant/mzero.hpp>
 #include <eve/constant/valmax.hpp>
 #include <eve/platform.hpp>
 #include <tts/tests/precision.hpp>
-#include <tts/tests/sequence.hpp>
 #include <tts/tests/types.hpp>
-#include <type_traits>
 
 TTS_CASE_TPL("Check eve::popcount return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::popcount(T(0)), (eve::detail::as_integer_t<T, unsigned>));
+  TTS_EXPR_IS(eve::popcount(T(0)), (eve::detail::as_integer_t<T, unsigned>) );
 }
 
 TTS_CASE_TPL("Check eve::popcount behavior", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
   using r_t = eve::detail::as_integer_t<T, unsigned>;
-  TTS_EQUAL(eve::popcount( T(0)), (eve::Zero<r_t>()));
-  TTS_EQUAL(eve::popcount( T(1)), (r_t(1))          );
-  TTS_EQUAL(eve::popcount( T(2)), (r_t(1))          );
-  TTS_EQUAL(eve::popcount( T(3)), (r_t(2))          );
-  TTS_EQUAL(eve::popcount( T(11)), (r_t(3))          );
+
+  TTS_EQUAL(eve::popcount( T(0) ) , r_t(0));
+  TTS_EQUAL(eve::popcount( T(1) ) , r_t(1));
+  TTS_EQUAL(eve::popcount( T(2) ) , r_t(1));
+  TTS_EQUAL(eve::popcount( T(3) ) , r_t(2));
+  TTS_EQUAL(eve::popcount( T(11)) , r_t(3));
+
   TTS_EQUAL(eve::popcount( eve::Valmax<T>()), r_t(sizeof(v_t)*8-eve::signed_value<T>));
 }
