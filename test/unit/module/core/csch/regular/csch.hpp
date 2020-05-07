43f414d1e7b012ae227a762996ea7e920c5fe8c5
Joel Falcou <joel.falcou@lri.fr>
2020-05-07 20:35:45 +0200

    Cleanup csch

--- test/unit/module/core/csch/regular/csch.hpp
+++ test/unit/module/core/csch/regular/csch.hpp
@@ -10,38 +10,40 @@
 //==================================================================================================
 #include <eve/function/csch.hpp>
 #include <eve/function/all.hpp>
+#include <eve/function/is_positive.hpp>
+#include <eve/function/is_negative.hpp>
 #include <eve/function/rec.hpp>
-#include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/function/is_positive.hpp>
-#include <eve/function/is_negative.hpp>
 #include <cmath>
 
 TTS_CASE_TPL("Check eve::csch return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::csch(T(0)), T);
+  TTS_EXPR_IS(eve::csch(T()), T);
 }
 
 TTS_CASE_TPL("Check eve::csch behavior", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
 
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::csch(eve::Nan<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::csch(eve::Inf<T>()) , (T( 0 )) );
-    TTS_IEEE_EQUAL(eve::csch(eve::Minf<T>()), (T( 0 )) );
-    TTS_EXPECT(eve::all(eve::is_positive(eve::csch(eve::Inf<T>()))));
+    TTS_IEEE_EQUAL(eve::csch(eve::Nan<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::csch(eve::Inf<T>()) , T(0) );
+    TTS_IEEE_EQUAL(eve::csch(eve::Minf<T>()), T(0) );
+
+    TTS_EXPECT(eve::all(eve::is_positive(eve::csch(eve::Inf<T>()))) );
     TTS_EXPECT(eve::all(eve::is_negative(eve::csch(eve::Minf<T>()))));
   }
-  TTS_ULP_EQUAL(eve::csch(T(1)), T(eve::rec(std::sinh(v_t(1)))), 0.5);
-  TTS_ULP_EQUAL(eve::csch(T(-1)),T(eve::rec(std::sinh(v_t(-1)))), 0.5);
-  TTS_IEEE_EQUAL((eve::csch(T(0))), (eve::Inf<T>()));
-  TTS_IEEE_EQUAL((eve::csch(T(-0.))), (eve::Minf<T>()));
-  }
+
+  TTS_ULP_EQUAL(eve::csch(T(1)) , T(eve::rec(std::sinh(v_t(1))) ), 0.5);
+  TTS_ULP_EQUAL(eve::csch(T(-1)), T(eve::rec(std::sinh(v_t(-1)))), 0.5);
+
+  TTS_IEEE_EQUAL(eve::csch(T( 0 )), eve::Inf<T>()  );
+  TTS_IEEE_EQUAL(eve::csch(T(-0.)), eve::Minf<T>() );
+}
