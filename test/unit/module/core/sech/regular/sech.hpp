452605d648b4a5314b5abb50b1711c7caee5e178
Joel Falcou <joel.falcou@lri.fr>
2020-05-07 20:24:54 +0200

    Cleanup sech

--- test/unit/module/core/sech/regular/sech.hpp
+++ test/unit/module/core/sech/regular/sech.hpp
@@ -9,39 +9,41 @@
 **/
 //==================================================================================================
 #include <eve/function/sech.hpp>
+#include <eve/function/is_positive.hpp>
+#include <eve/function/is_negative.hpp>
 #include <eve/function/all.hpp>
 #include <eve/function/rec.hpp>
-#include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/function/is_positive.hpp>
-#include <eve/function/is_negative.hpp>
 #include <cmath>
 
 TTS_CASE_TPL("Check eve::sech return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::sech(T(0)), T);
+  TTS_EXPR_IS(eve::sech(T()), T);
 }
 
 TTS_CASE_TPL("Check eve::sech behavior", EVE_TYPE)
 {
+  using v_t = eve::element_type_t<T>;
 
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::sech(eve::Nan<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::sech(eve::Inf<T>()) , (T( 0 )) );
-    TTS_IEEE_EQUAL(eve::sech(eve::Minf<T>()), (T( 0 )) );
+    TTS_IEEE_EQUAL(eve::sech(eve::Nan<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::sech(eve::Inf<T>()) , T(0)          );
+    TTS_IEEE_EQUAL(eve::sech(eve::Minf<T>()), T(0)          );
+
     TTS_EXPECT(eve::all(eve::is_positive(eve::sech(eve::Inf<T>()))));
     TTS_EXPECT(eve::all(eve::is_positive(eve::sech(eve::Minf<T>()))));
   }
-  TTS_ULP_EQUAL(eve::sech(T(1)), T(eve::rec(std::cosh(v_t(1)))), 0.5);
-  TTS_ULP_EQUAL(eve::sech(T(-1)),T(eve::rec(std::cosh(v_t(-1)))), 0.5);
-  TTS_IEEE_EQUAL((eve::sech(T(0))), (T(1)));
-  TTS_IEEE_EQUAL((eve::sech(T(-0.))), (T(1)));
-  }
+
+  TTS_ULP_EQUAL(eve::sech( T(1)), T(eve::rec(std::cosh(v_t(1))))  , 0.5);
+  TTS_ULP_EQUAL(eve::sech(-T(1)), T(eve::rec(std::cosh(v_t(-1)))) , 0.5);
+
+  TTS_IEEE_EQUAL(eve::sech(T( 0 )), T(1));
+  TTS_IEEE_EQUAL(eve::sech(T(-0.)), T(1));
+}
