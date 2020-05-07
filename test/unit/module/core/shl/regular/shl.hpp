f7d09ae4719483b5d279210b499994377c7d5da9
Joel Falcou <joel.falcou@lri.fr>
2020-05-07 11:47:43 +0200

    Cleanup for shl/shr

--- test/unit/module/core/shl/regular/shl.hpp
+++ test/unit/module/core/shl/regular/shl.hpp
@@ -19,21 +19,21 @@ TTS_CASE_TPL("Check eve::shl return type", EVE_TYPE)
   using i_t = eve::detail::as_integer_t<T, signed>;
   using u_t = eve::detail::as_integer_t<T, unsigned>;
 
-  TTS_EXPR_IS(eve::shl(T(), T()), T);
-  TTS_EXPR_IS(eve::shl(T(), i_t() ), T);
-  TTS_EXPR_IS(eve::shl(T(), u_t() ), T);
+  TTS_EXPR_IS(eve::shl(T(), T())    , T);
+  TTS_EXPR_IS(eve::shl(T(), i_t() ) , T);
+  TTS_EXPR_IS(eve::shl(T(), u_t() ) , T);
 }
 
 TTS_CASE_TPL("Check eve::shl behavior", EVE_TYPE)
 {
-  TTS_EQUAL(eve::shl(T(0), 7), (T( 0)));
-  TTS_EQUAL(eve::shl(T(1), 4), (T(16)));
-  TTS_EQUAL(eve::shl(T(3), 2), (T(12)));
+  TTS_EQUAL(eve::shl(T(0), 7), T( 0));
+  TTS_EQUAL(eve::shl(T(1), 4), T(16));
+  TTS_EQUAL(eve::shl(T(3), 2), T(12));
 
   if constexpr(eve::signed_value<T>)
   {
-    TTS_EQUAL(eve::shl(T(-1), 4), (T(-16)));
-    TTS_EQUAL(eve::shl(T(-3), 2), (T(-12)));
+    TTS_EQUAL(eve::shl(T(-1), 4), T(-16));
+    TTS_EQUAL(eve::shl(T(-3), 2), T(-12));
     TTS_EQUAL(eve::shl(eve::Allbits<T>(), 1), T(-2));
   }
   else
--- test/unit/module/core/shr/regular/shr.hpp
+++ test/unit/module/core/shr/regular/shr.hpp
@@ -9,10 +9,9 @@
 **/
 //==================================================================================================
 #include <eve/function/shr.hpp>
+#include <eve/constant/allbits.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/constant/allbits.hpp>
-#include <type_traits>
 
 TTS_CASE_TPL("Check eve::shr return type", EVE_TYPE)
 {
@@ -26,14 +25,14 @@ TTS_CASE_TPL("Check eve::shr return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::shr behavior", EVE_TYPE)
 {
-  TTS_EQUAL(eve::shr(T( 0), 7), (T(0)));
-  TTS_EQUAL(eve::shr(T(16), 4), (T(1)));
-  TTS_EQUAL(eve::shr(T(12), 2), (T(3)));
+  TTS_EQUAL(eve::shr(T( 0), 7), T(0));
+  TTS_EQUAL(eve::shr(T(16), 4), T(1));
+  TTS_EQUAL(eve::shr(T(12), 2), T(3));
 
   if constexpr(eve::signed_value<T>)
   {
-    TTS_EQUAL(eve::shr(T(-16), 4), (T(-1)));
-    TTS_EQUAL(eve::shr(T(-12), 2), (T(-3)));
+    TTS_EQUAL(eve::shr(T(-16), 4), T(-1));
+    TTS_EQUAL(eve::shr(T(-12), 2), T(-3));
     TTS_EQUAL(eve::shr(eve::Allbits<T>(), 1), eve::Allbits<T>());
   }
   else
