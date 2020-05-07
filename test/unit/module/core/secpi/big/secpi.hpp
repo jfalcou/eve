70e53926f4474bc0b0d15c00ca65d01edd5b8519
Joel Falcou <joel.falcou@lri.fr>
2020-05-07 19:46:30 +0200

    Cleanup secpi

--- test/unit/module/core/secpi/big/secpi.hpp
+++ test/unit/module/core/secpi/big/secpi.hpp
@@ -9,46 +9,37 @@
 **/
 //==================================================================================================
 #include <eve/function/secpi.hpp>
-#include <eve/function/all.hpp>
-#include <eve/function/is_eqz.hpp>
-#include <eve/constant/mzero.hpp>
 #include <eve/constant/nan.hpp>
-#include <eve/constant/pi.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/function/is_flint.hpp>
-#include <eve/function/rec.hpp>
 #include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
 
-
-TTS_CASE_TPL("Check eve::secpi return type", EVE_TYPE)
+TTS_CASE_TPL("Check eve::big_(eve::secpi) return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::secpi(T(0)), T);
+  TTS_EXPR_IS(eve::big_(eve::secpi)(T()), T);
 }
 
-TTS_CASE_TPL("Check eve::eve::secpi behavior", EVE_TYPE)
+TTS_CASE_TPL("Check eve::big_(eve::secpi) behavior", EVE_TYPE)
 {
-
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Nan<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Inf<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Minf<T>()), (eve::Nan<T>()) );
+    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Nan<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Inf<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::big_(eve::secpi)(eve::Minf<T>()), eve::Nan<T>() );
   }
-  TTS_ULP_EQUAL(eve::big_(eve::secpi)(T(1)), (T(-1)), 0.5);
-  TTS_ULP_EQUAL(eve::big_(eve::secpi)(T(-1)),(T(-1)), 0.5);
-  TTS_IEEE_EQUAL(eve::big_(eve::secpi)(T(0)),(T(1)));
-  TTS_IEEE_EQUAL(eve::big_(eve::secpi)(T(-0.)), (T(1)));
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(T(22.5))), (eve::Nan<T>()), 0.5);
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(-T(22.5))),(eve::Nan<T>()), 0.5);
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(T(100000.0))), T(1), 0.5);
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(T(-100000.0))),T(1), 0.5);
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(T(1000001.0))), T(-1), 0.5);
-  TTS_ULP_EQUAL((eve::big_(eve::secpi)(T(-100001.0))),T(-1), 0.5);
 
+  TTS_IEEE_EQUAL(eve::big_(eve::secpi)(T( 0 )), T(1));
+  TTS_IEEE_EQUAL(eve::big_(eve::secpi)(T(-0.)), T(1));
+
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)( T( 1))       ,-T(1)          , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)(-T( 1))       ,-T(1)          , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)( T(22.5))     , eve::Nan<T>() , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)(-T(22.5))     , eve::Nan<T>() , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)( T(100000.0)) , T(1)          , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)(-T(100000.0)) , T(1)          , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)( T(1000001.0)),-T(1)          , 0.5);
+  TTS_ULP_EQUAL(eve::big_(eve::secpi)(-T(100001.0)) ,-T(1)          , 0.5);
 }
--- test/unit/module/core/secpi/restricted/secpi.hpp
+++ test/unit/module/core/secpi/restricted/secpi.hpp
@@ -9,37 +9,32 @@
 **/
 //==================================================================================================
 #include <eve/function/secpi.hpp>
-#include <eve/function/cos.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/inf.hpp>
 #include <eve/constant/minf.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/nan.hpp>
 #include <eve/constant/pi.hpp>
-#include <eve/constant/mzero.hpp>
-#include <eve/constant/zero.hpp>
-#include <eve/function/is_flint.hpp>
-#include <eve/function/rec.hpp>
 #include <eve/platform.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/types.hpp>
-#include <cmath>
 
 TTS_CASE_TPL("Check eve::restricted_(eve::secpi) return type", EVE_TYPE)
 {
-  TTS_EXPR_IS(eve::restricted_(eve::secpi)(T(0)), T);
+  TTS_EXPR_IS(eve::restricted_(eve::secpi)(T()), T);
 }
 
-TTS_CASE_TPL("Check eve::eve::secpi behavior", EVE_TYPE)
+TTS_CASE_TPL("Check eve::restricted_(eve::secpi) behavior", EVE_TYPE)
 {
   if constexpr( eve::platform::supports_invalids )
   {
-    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Nan<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Inf<T>()) , (eve::Nan<T>()) );
-    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Minf<T>()), (eve::Nan<T>()) );
+    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Nan<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Inf<T>()) , eve::Nan<T>() );
+    TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(eve::Minf<T>()), eve::Nan<T>() );
   }
-  TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(T(0)), (T(1)));
-  TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(T(-0.)), (T(1)));
-  TTS_ULP_EQUAL((eve::restricted_(eve::secpi)(T(0.125))), (T(1.08239220029239396879944641073277884012214412675604)), 0.5);
-  TTS_ULP_EQUAL((eve::restricted_(eve::secpi)(-T(0.125))),(T(1.08239220029239396879944641073277884012214412675604)), 0.5);
+
+  TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(T(0))   , T(1));
+  TTS_IEEE_EQUAL(eve::restricted_(eve::secpi)(T(-0.)) , T(1));
+
+  TTS_ULP_EQUAL(eve::restricted_(eve::secpi)(T(0.125))  , T(1.08239220029239396879944641073277884012214412675604), 0.5);
+  TTS_ULP_EQUAL(eve::restricted_(eve::secpi)(-T(0.125)) , T(1.08239220029239396879944641073277884012214412675604), 0.5);
 }
