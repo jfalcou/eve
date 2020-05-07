a6f821c8435d67389420c2ec9cde3c859bddcaa6
jtlap <jtlapreste@gmail.com>
2020-05-07 08:01:47 +0200

    derniers arithmetiques

--- test/doc/core/sqrt.cpp
+++ test/doc/core/sqrt.cpp
@@ -1,21 +1,24 @@
 #include <eve/function/sqrt.hpp>
+#include <eve/constant/inf.hpp>
 #include <eve/wide.hpp>
 
 using wide_ft = eve::wide<float, eve::fixed<4>>;
 
 int main()
 {
-  wide_ft pf = {-1.0f, 2.0f, -3.0f, -32768.0f};
+  wide_ft pf = {1.0f, 2.0f, -3.0f, eve::Inf<float>()};
 
   std::cout << "---- simd" << '\n'
-            << "<- pf =                            " << pf << '\n'
-            << "-> eve::sqrt(pf) =                  " << eve::sqrt(pf) << '\n'; 
+            << "<- pf              = " << pf << '\n'
+            << "-> sqrt(pf)        = " << eve::sqrt(pf) << '\n'
+            << "-> raw_(sqrt)(pf)  = " << eve::raw_(eve::sqrt)(pf) << '\n';
 
 
-  float        xf = -32768.0f;
+  float xf = 32.768f;
 
   std::cout << "---- scalar" << '\n'
-            << "<- xf =                            " << xf << '\n'
-            << "-> eve::sqrt(xf) =                  " << eve::sqrt(xf) << '\n'; 
+            << "<- xf              = " << xf << '\n'
+            << "-> eve::sqrt(xf)   = " << eve::sqrt(xf) << '\n';
+
   return 0;
 }
