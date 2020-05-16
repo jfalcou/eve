e17e6ca38060bceb2ee06c7549570cd53a0376d1
jtlap <jtlapreste@gmail.com>
2020-05-16 16:32:19 +0200

    inv trigo doc 1

--- test/doc/core/atan.cpp
+++ test/doc/core/atan.cpp
@@ -1,33 +1,27 @@
 #include <eve/function/atan.hpp>
 #include <eve/wide.hpp>
 #include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/mindenormal.hpp>
 #include <iostream>
 
-using wide_ft = eve::wide <float, eve::fixed<8>>;
+using wide_ft = eve::wide <float, eve::fixed<4>>;
 
 int main()
 {
-  using eve::pedantic_;
-  
-  wide_ft pf = { 0.0f, 1.0f, -1.0f, -2.0f
-                , eve::Mindenormal<float>(), eve::Inf<float>(), eve::Minf<float>(), eve::Nan<float>() };
+  wide_ft pf = { 0.0f, 2.0f, -1.0f, -0.5f};
 
   std::cout
     << "---- simd" << '\n'
-    << "<- pf =                      " << pf << '\n'
-    << "-> eve::atan(pf) =            " << eve::atan(pf) << '\n'; 
+    << "<- pf =            " << pf << '\n'
+    << "-> eve::atan(pf) = " << eve::atan(pf) << '\n';
 
   float xf = 1.0f;
-  float yf = eve::Nan<float>();
+  float yf = eve::Inf<float>();
 
   std::cout
     << "---- scalar"  << '\n'
-    << "<- xf =                      " << xf << '\n'
-    << "-> eve::atan(xf) =            " << eve::atan(xf) << '\n'
-    << "<- yf =                      " << yf << '\n'
-    << "-> eve::atan(yf) =            " << eve::atan(yf) << '\n'; 
+    << "<- xf =            " << xf << '\n'
+    << "-> eve::atan(xf) = " << eve::atan(xf) << '\n'
+    << "<- yf =            " << yf << '\n'
+    << "-> eve::atan(yf) = " << eve::atan(yf) << '\n';
   return 0;
 }
