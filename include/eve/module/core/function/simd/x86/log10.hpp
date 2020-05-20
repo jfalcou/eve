201c651b97d0889f17663ebdcd7c2e9f91433611
Joel Falcou <joel.falcou@lri.fr>
2020-05-20 11:45:00 +0200

    Fix typo in log10

--- include/eve/module/core/function/simd/x86/log10.hpp
+++ include/eve/module/core/function/simd/x86/log10.hpp
@@ -44,7 +44,7 @@ namespace eve::detail
 {
   // this implementation is better only if we are in avx_ without avx2_
   template<floating_real_simd_value T>
-  EVE_FORCEINLINE auto log_(EVE_SUPPORTS(cpu_), plain_type const &, const T &a0) noexcept
+  EVE_FORCEINLINE auto log10_(EVE_SUPPORTS(cpu_), plain_type const &, const T &a0) noexcept
   {
     T Invlog_10hi = Ieee_constant<T, 0x3ede6000U, 0x3fdbcb7b15200000ULL>();
     T Invlog_10lo = Ieee_constant<T, 0xb804ead9U, 0x3dbb9438ca9aadd5ULL>();
