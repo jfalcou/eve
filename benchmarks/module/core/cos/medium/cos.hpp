21e37a28a69f91fc7e09d8aece3cb4aa69e8fec1
jtlap <jtlapreste@gmail.com>
2020-05-23 18:47:12 +0200

    Rempio2_limits update

--- benchmarks/module/core/cos/medium/cos.hpp
+++ benchmarks/module/core/cos/medium/cos.hpp
@@ -15,7 +15,7 @@
 int main(int argc, char** argv)
 {
   using EVE_VALUE = eve::detail::value_type_t<EVE_TYPE>;
-  auto lmax = eve::detail::Rempio2_limit(eve::medium_type(), EVE_VALUE());
+  auto lmax = eve::detail::Rempio2_limit(eve::medium_type(), eve::as_<EVE_VALUE>());
   auto lmin = -lmax;
   EVE_REGISTER_BENCHMARK(eve::medium_(eve::cos), EVE_TYPE
                         , eve::bench::random<EVE_TYPE>(lmin,lmax));
