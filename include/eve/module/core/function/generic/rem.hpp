400aaae27303a765c3bc2cba526d612c7ba6c686
jtlap <jtlapreste@gmail.com>
2020-06-07 08:02:35 +0200

    adding masked version to all rem flavours

--- include/eve/module/core/function/generic/rem.hpp
+++ include/eve/module/core/function/generic/rem.hpp
@@ -59,5 +59,72 @@ namespace eve::detail
   {
     return if_else(is_nez(b), d(rem)(a,b), a);
   }
-}
 
+  //================================================================================================
+  // Masked case
+  //================================================================================================
+  template<value T, real_value U, real_value V>
+  EVE_FORCEINLINE auto rem_(EVE_SUPPORTS(cpu_), T const &cond, U const &t, V const &f) noexcept
+  //      requires compatible_values<U, V>
+  {
+    using r_t = decltype(rem(t, f));
+    if constexpr( scalar_value<T> )
+    {
+      return cond ? rem(t, f) : r_t(t);
+    }
+    else if constexpr( simd_value<T> )
+    {
+      return if_else(cond, rem(r_t(t), r_t(f)), r_t(t));
+    }
+  }
+
+  template<value T, real_value U, real_value V, decorator D>
+  EVE_FORCEINLINE auto
+  rem_(EVE_SUPPORTS(cpu_), T const &cond, D const &, U const &t, V const &f) noexcept
+  requires compatible_values<U, V>
+  {
+    using r_t = decltype(D()(rem)(t, f));
+    if constexpr( scalar_value<T> )
+    {
+      return cond ? D()(rem)(t, f) : r_t(t);
+    }
+    else if constexpr( simd_value<T> )
+    {
+      return if_else(cond, D()(rem)(r_t(t), r_t(f)), r_t(t));
+    }
+  }
+
+  template<value T, real_value U, real_value V>
+  EVE_FORCEINLINE auto
+  rem_(EVE_SUPPORTS(cpu_), not_t<T> const &cond, U const &t, V const &f) noexcept
+  requires compatible_values<U, V>
+  {
+    using r_t = decltype(rem(t, f));
+    if constexpr( scalar_value<T> )
+    {
+      return cond.value ? r_t(t) : rem(t, f);
+    }
+    else if constexpr( simd_value<T> )
+    {
+      return if_else(cond, r_t(t), rem(r_t(t), r_t(f)));
+    }
+  }
+
+  template<value T, real_value U, real_value V, decorator D>
+  EVE_FORCEINLINE auto rem_(EVE_SUPPORTS(cpu_),
+                            not_t<T> const &cond,
+                            D const &,
+                            U const &t,
+                            V const &f) noexcept requires compatible_values<U, V>
+  {
+    using r_t = decltype(rem(t, f));
+    if constexpr( scalar_value<T> )
+    {
+      return cond.value ? r_t(t) : D()(rem)(t, f);
+    }
+    else if constexpr( simd_value<T> )
+    {
+      return if_else(cond, r_t(t), D()(rem)(r_t(t), r_t(f)));
+    }
+  }
+}
