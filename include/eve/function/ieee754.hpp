e66cd369896e82de2298f35e0d9664aa2ca2c48e
jtlap <jtlapreste@gmail.com>
2020-06-14 19:42:30 +0200

    iee754

---
+++ include/eve/function/ieee754.hpp
@@ -0,0 +1,27 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+// **=======================================================
+// helper file to include all ieee754 related functions
+#include <eve/function/bitofsign.hpp>
+#include <eve/function/bit_exponent.hpp>
+#include <eve/function/bit_mantisaa.hpp>
+#include <eve/function/exponent.hpp>
+#include <eve/function/frexp.hpp>
+#include <eve/function/ifrexp.hpp>
+#include <eve/function/ldexp.hpp>
+#include <eve/function/mantissa.hpp>
+#include <eve/function/nb_values.hpp>
+#include <eve/function/next.hpp>
+#include <eve/function/nextafter.hpp>
+#include <eve/function/prev.hpp>
+#include <eve/function/two_add.hpp>
+#include <eve/function/two_prod.hpp>
+#include <eve/function/ulpdist.hpp>
