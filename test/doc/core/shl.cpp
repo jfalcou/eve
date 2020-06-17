34577fef831d1e916f63ead481aabbc99f102784
jtlap <jtlapreste@gmail.com>
2020-06-17 11:22:59 +0200

    docs for operators

--- test/doc/core/shl.cpp
+++ test/doc/core/shl.cpp
@@ -10,17 +10,17 @@ int main()
   wide_it qi = {1, 2, 3, 2};
 
   std::cout << "---- simd" << '\n'
-            << "<- pi =              " << pi << '\n'
-            << "<- qi =              " << qi << '\n'
+            << "<- pi               = " << pi << '\n'
+            << "<- qi               = " << qi << '\n'
             << "-> eve::shl(pi, qi) = " << eve::shl(pi, qi) << '\n';
 
   iT xi = 2, mxi = -2, yi = 3;
 
   std::cout << "---- scalar" << '\n'
-            << "<- xi  =                      " << xi << '\n'
-            << "<- mxi =                      " << mxi << '\n'
-            << "<- yi  =                      " << yi << '\n'
-            << "-> eve::shl(xi, yi) = " << eve::shl(xi, yi) << '\n'
-            << "-> eve::shl(mxi, yi)= " << eve::shl(mxi, yi) << '\n';
+            << "<- xi                = " << xi << '\n'
+            << "<- mxi               = " << mxi << '\n'
+            << "<- yi                = " << yi << '\n'
+            << "-> eve::shl(xi, yi)  = " << eve::shl(xi, yi) << '\n'
+            << "-> eve::shl(mxi, yi) = " << eve::shl(mxi, yi) << '\n';
   return 0;
 }
