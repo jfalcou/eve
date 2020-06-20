af7853d5f9aa47756e5770be1c360776a158527a
jtlap <jtlapreste@gmail.com>
2020-06-20 10:03:29 +0200

    new decorators of conversions

--- include/eve/function/converter.hpp
+++ include/eve/function/converter.hpp
@@ -13,6 +13,7 @@
 #include <eve/function/convert.hpp>
 #include <eve/detail/overload.hpp>
 #include <eve/detail/abi.hpp>
+#include <eve/detail/meta.hpp>
 
 namespace eve
 {
@@ -21,7 +22,7 @@ namespace eve
   template <typename T>
   struct converter_type : decorator_
   {
-    using value_type = T;
+    using value_type = element_type_t<T>;
     template<value Val>
     constexpr EVE_FORCEINLINE auto operator()(Val const & val) const noexcept
     {
@@ -61,4 +62,78 @@ namespace eve
   // Function decorator for template conversion
   template<typename T>
   inline constexpr converter_type<T> const to_ = {};
+
+  template<typename T>
+  inline constexpr converter_type<element_type_t<T>> const to_elt = {};
+
+  template<typename T>
+  inline constexpr converter_type<detail::as_integer_t<element_type_t<T>, signed>> const toint_ = {};
+
+  template<typename T>
+  inline constexpr converter_type<detail::as_integer_t<element_type_t<T>, unsigned>> const touint_ = {};
+
+  template<typename T>
+  inline constexpr converter_type<detail::as_floating_point_t<element_type_t<T>>> const tofloating_ = {};
+
+
+  //================================================================================================
+  // Function saturated decorators mark-up used in function overloads
+  template <typename T>
+  struct s_converter_type : decorator_
+  {
+    using value_type = element_type_t<T>;
+    template<value Val>
+    constexpr EVE_FORCEINLINE auto operator()(Val const & val) const noexcept
+    {
+      return saturated_(convert)(val, as<T>());
+    }
+    template<typename Function>
+    EVE_FORCEINLINE constexpr  auto operator()(Function f) const noexcept
+    {
+      return  [f]<typename... Ts>(Ts&&... args)
+      {
+        if constexpr( supports_optimized_conversion<typename Function::tag_type>::value )
+        {
+          return f(s_converter_type<T>(), std::forward<Ts>(args)...);
+        }
+        else
+        {
+          return saturated_(convert)(f(std::forward<Ts>(args)...), as_<T>());
+        }
+      };
+    }
+  };
+
+
+  //================================================================================================
+  // Function saturated decorators for all basic type
+  inline constexpr converter_type<float>          const s_single_   = {};
+  inline constexpr converter_type<double>         const s_double_   = {};
+  inline constexpr converter_type<std::uint8_t >  const s_uint8_    = {};
+  inline constexpr converter_type<std::uint16_t>  const s_uint16_   = {};
+  inline constexpr converter_type<std::uint32_t>  const s_uint32_   = {};
+  inline constexpr converter_type<std::uint64_t>  const s_uint64_   = {};
+  inline constexpr converter_type<std::int8_t >   const s_int8_     = {};
+  inline constexpr converter_type<std::int16_t>   const s_int16_    = {};
+  inline constexpr converter_type<std::int32_t>   const s_int32_    = {};
+  inline constexpr converter_type<std::int64_t>   const s_int64_    = {};
+
+  //================================================================================================
+  // Function decorator for template conversion
+  template<typename T>
+  inline constexpr s_converter_type<T> const to_ = {};
+
+  template<typename T>
+  inline constexpr s_converter_type<element_type_t<T>> const to_elt = {};
+
+  template<typename T>
+  inline constexpr s_converter_type<detail::as_integer_t<element_type_t<T>, signed>> const s_toint_ = {};
+
+  template<typename T>
+  inline constexpr s_converter_type<detail::as_integer_t<element_type_t<T>, unsigned>> const s_touint_ = {};
+
+  template<typename T>
+  inline constexpr s_converter_type<detail::as_floating_point_t<element_type_t<T>>> const s_tofloating_ = {};
+
+
 }
