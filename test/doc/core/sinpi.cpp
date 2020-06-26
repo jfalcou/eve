47745e3f0b587d47ac298849d0573f84ae24fa48
jtlap <jtlapreste@gmail.com>
2020-06-26 16:21:46 +0200

    more docs correction/adding

--- test/doc/core/sinpi.cpp
+++ test/doc/core/sinpi.cpp
@@ -11,13 +11,13 @@ int main()
   wide_ft pf = {1.0f, 0.0f, eve::Inf<float>(), 0.5f};
 
   std::cout << "---- simd" << '\n'
-            << "<- pf       = " << pf << '\n'
+            << "<- pf        = " << pf << '\n'
             << "-> sinpi(pf) = " << eve::sinpi(pf) << '\n';
 
   float xf = 3.0f;
 
   std::cout << "---- scalar" << '\n'
-            << "<- xf       = " << xf << '\n'
+            << "<- xf        = " << xf << '\n'
             << "-> sinpi(xf) = " << eve::sinpi(xf) << '\n';
   return 0;
 }
