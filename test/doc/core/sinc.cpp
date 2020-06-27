2f478da5ec3941fb91c3816b34ec5f5b8c2c98ca
jtlap <jtlapreste@gmail.com>
2020-06-27 16:36:01 +0200

    html generation

---
+++ test/doc/core/sinc.cpp
@@ -0,0 +1,23 @@
+#include <eve/function/sinc.hpp>
+#include <eve/wide.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/constant/pi.hpp>
+#include <iostream>
+
+using wide_ft = eve::wide<float, eve::fixed<4>>;
+
+int main()
+{
+  wide_ft pf = {1.0f, 0.0f, eve::Inf<float>(), eve::Pi<float>()};
+
+  std::cout << "---- simd" << '\n'
+            << "<- pf       = " << pf << '\n'
+            << "-> sinc(pf) = " << eve::sinc(pf) << '\n';
+
+  float xf = 3.0f;
+
+  std::cout << "---- scalar" << '\n'
+            << "<- xf       = " << xf << '\n'
+            << "-> sinc(xf) = " << eve::sinc(xf) << '\n';
+  return 0;
+}
