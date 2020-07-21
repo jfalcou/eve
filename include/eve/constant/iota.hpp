fdec4522ab3e7f99fafa61acfb282d91538e98c8
Joel Falcou <joel.falcou@lri.fr>
2020-07-21 17:14:21 +0200

    Add iota constant that generates (0 1 ... N-1)

---
+++ include/eve/constant/iota.hpp
@@ -0,0 +1,36 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/as.hpp>
+
+namespace eve
+{
+  EVE_MAKE_CALLABLE(iota_, iota_);
+
+  template<typename T>
+  EVE_FORCEINLINE auto Iota(as_<T> const & = {}) noexcept
+  {
+    if constexpr( scalar_value<T> )
+    {
+      return T(0);
+    }
+    else
+    {
+      return T([](auto i, auto c) { return i; } );
+    }
+  }
+
+  EVE_MAKE_NAMED_CONSTANT(iota_, Iota);
+}
+
