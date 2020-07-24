895885dfac423588b5e819d15489fcb853f9f00b
Joel Falcou <joel.falcou@lri.fr>
2020-07-24 22:03:14 +0200

    Reinstate missing scalar lookup

--- include/eve/function/lookup.hpp
+++ include/eve/function/lookup.hpp
@@ -12,22 +12,13 @@
 
 #include <eve/concept/value.hpp>
 #include <eve/detail/abi.hpp>
-// #include <eve/detail/overload.hpp>
-
-// namespace eve
-// {
-//   EVE_MAKE_CALLABLE(lookup_, lookup);
-// }
-
-
-// namespace eve::detail
-// {
-//   template<real_scalar_value T, integral_value I>
-//   EVE_FORCEINLINE constexpr T lookup_(EVE_SUPPORTS(cpu_), T const &a, I const &i)
-//   {
-//     return (i == static_cast<I>(-1)) ? 0 : a;
-//   }
-// }
-
 #include <eve/detail/function/lookup.hpp>
 
+namespace eve::detail
+{
+  template<real_scalar_value T, integral_value I>
+  EVE_FORCEINLINE constexpr T lookup_(EVE_SUPPORTS(cpu_), T const &a, I const &i)
+  {
+    return (i == static_cast<I>(-1)) ? 0 : a;
+  }
+}
