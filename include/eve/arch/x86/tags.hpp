6fd9340557152a55f87185cf2051e2972f5d3e4f
Joel Falcou <joel.falcou@lri.fr>
2020-07-26 17:12:30 +0200

    Generalized CPUID detection for x86

--- include/eve/arch/x86/tags.hpp
+++ include/eve/arch/x86/tags.hpp
@@ -56,75 +56,24 @@ namespace eve
   inline constexpr auto fma3   = fma3_{};
   inline constexpr auto fma4   = fma4_{};
 
-  // clang-format on
   //================================================================================================
   // Runtime detection of CPU support
   //================================================================================================
   template<auto Version> inline bool is_supported(spy::x86_simd_info<Version> const &) noexcept
   {
-    if constexpr( Version == sse2.version )
-    {
-      static const bool detected = detail::detect_feature(25, 0x00000001, detail::edx);
-      return detected;
-    }
-    else if constexpr( Version == sse3.version )
-    {
-      static const bool detected = detail::detect_feature(0, 0x00000001, detail::ecx);
-      return detected;
-    }
-    else if constexpr( Version == ssse3.version )
-    {
-      static const bool detected = detail::detect_feature(9, 0x00000001, detail::ecx);
-      return detected;
-    }
-    else if constexpr( Version == sse4_1.version )
-    {
-      static const bool detected = detail::detect_feature(19, 0x00000001, detail::ecx);
-      return detected;
-    }
-    else if constexpr( Version == sse4_2.version )
-    {
-      static const bool detected = detail::detect_feature(20, 0x00000001, detail::ecx);
-      return detected;
-    }
-    else if constexpr( Version == avx.version )
-    {
-      static const bool detected =
-          detail::detect_features((1 << 28) | (1 << 27) | (1 << 26), 0x00000001, detail::ecx);
-      return detected;
-    }
-    else if constexpr( Version == avx2.version )
-    {
-      static const bool detected =
-          detail::detect_features((1 << 28) | (1 << 27) | (1 << 26), 0x00000001, detail::ecx);
-      return detected;
-    }
-    else
-    {
-      return false;
-    }
+          if constexpr( Version == sse2.version   ) return detail::cpuid_states.supports_sse2();
+    else  if constexpr( Version == sse3.version   ) return detail::cpuid_states.supports_sse3();
+    else  if constexpr( Version == ssse3.version  ) return detail::cpuid_states.supports_ssse3();
+    else  if constexpr( Version == sse4_1.version ) return detail::cpuid_states.supports_sse4_1();
+    else  if constexpr( Version == sse4_2.version ) return detail::cpuid_states.supports_sse4_2();
+    else  if constexpr( Version == avx.version    ) return detail::cpuid_states.supports_avx();
+    else  if constexpr( Version == avx2.version   ) return detail::cpuid_states.supports_avx2();
+    else                                            return false;
   }
 
-  inline bool is_supported(xop_ const &) noexcept
-  {
-    static const bool detected = detail::detect_feature(11, 0x80000001, detail::ebx)
-                                 && detail::detect_feature(27, 0x80000001, detail::ecx);
-    return detected;
-  }
-
-  inline bool is_supported(fma3_ const &) noexcept
-  {
-    static const bool detected = detail::detect_feature(12, 0x00000001, detail::ecx)
-                                 && detail::detect_feature(27, 0x00000001, detail::ecx);
-    return detected;
-  }
-
-  inline bool is_supported(fma4_ const &) noexcept
-  {
-    static const bool detected = detail::detect_feature(16, 0x80000001, detail::ecx)
-                                 && detail::detect_feature(27, 0x80000001, detail::ecx);
-    return detected;
-  }
+  inline bool is_supported(xop_ const &)  noexcept  { return detail::cpuid_states.supports_xop(); }
+  inline bool is_supported(fma3_ const &) noexcept  { return detail::cpuid_states.supports_fma3();}
+  inline bool is_supported(fma4_ const &) noexcept  { return detail::cpuid_states.supports_fma4();}
 
   //================================================================================================
   // x86 ABI concept
