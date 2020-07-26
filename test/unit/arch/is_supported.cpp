d51798e48ab4818806cc836a69ab905bdafb9fbd
Joel Falcou <joel.falcou@lri.fr>
2020-07-26 17:47:09 +0200

    More ARM support

--- test/unit/arch/is_supported.cpp
+++ test/unit/arch/is_supported.cpp
@@ -21,25 +21,25 @@ int main()
   std::cout << "Current API: " << eve::current_api << "\n";
   std::cout << "========================\n";
   std::cout << "X86-like SIMD extensions\n";
-  std::cout << "SSE2   : " << std::boolalpha << (eve::current_api == eve::sse2  ) << "\n";
-  std::cout << "SSE3   : " << std::boolalpha << (eve::current_api == eve::sse3  ) << "\n";
-  std::cout << "SSSE3  : " << std::boolalpha << (eve::current_api == eve::ssse3 ) << "\n";
-  std::cout << "SSE4.1 : " << std::boolalpha << (eve::current_api == eve::sse4_1) << "\n";
-  std::cout << "SSE4.2 : " << std::boolalpha << (eve::current_api == eve::sse4_2) << "\n";
-  std::cout << "AVX    : " << std::boolalpha << (eve::current_api == eve::avx   ) << "\n";
+  std::cout << "SSE2   : " << std::boolalpha << (eve::current_api >= eve::sse2  ) << "\n";
+  std::cout << "SSE3   : " << std::boolalpha << (eve::current_api >= eve::sse3  ) << "\n";
+  std::cout << "SSSE3  : " << std::boolalpha << (eve::current_api >= eve::ssse3 ) << "\n";
+  std::cout << "SSE4.1 : " << std::boolalpha << (eve::current_api >= eve::sse4_1) << "\n";
+  std::cout << "SSE4.2 : " << std::boolalpha << (eve::current_api >= eve::sse4_2) << "\n";
+  std::cout << "AVX    : " << std::boolalpha << (eve::current_api >= eve::avx   ) << "\n";
   std::cout << "FMA3   : " << std::boolalpha << eve::supports_fma3                << "\n";
   std::cout << "XOP    : " << std::boolalpha << eve::supports_fma4                << "\n";
   std::cout << "FMA4   : " << std::boolalpha << eve::supports_xop                 << "\n";
-  std::cout << "AVX2   : " << std::boolalpha << (eve::current_api == eve::avx2  ) << "\n";
+  std::cout << "AVX2   : " << std::boolalpha << (eve::current_api >= eve::avx2  ) << "\n";
   std::cout << "\n";
   std::cout << "========================\n";
   std::cout << "PPC SIMD extensions\n";
-  std::cout << "VSX   : " << std::boolalpha << (eve::current_api == eve::vsx   ) << "\n";
-  std::cout << "VMX   : " << std::boolalpha << (eve::current_api == eve::vmx   ) << "\n";
+  std::cout << "VSX   : " << std::boolalpha << (eve::current_api >= eve::vsx   ) << "\n";
+  std::cout << "VMX   : " << std::boolalpha << (eve::current_api >= eve::vmx   ) << "\n";
   std::cout << "\n";
   std::cout << "========================\n";
   std::cout << "ARM SIMD extensions\n";
-  std::cout << "NEON   : " << std::boolalpha << (eve::current_api == eve::neon   ) << "\n";
+  std::cout << "NEON   : " << std::boolalpha << (eve::current_api >= eve::neon   ) << "\n";
   std::cout << "AARCH64: " << std::boolalpha << eve::supports_aarch64              << "\n";
   std::cout << "\n";
 
@@ -65,6 +65,7 @@ int main()
   std::cout << "========================\n";
   std::cout << "ARM SIMD extensions\n";
   std::cout << "NEON  : " << std::boolalpha << eve::is_supported(eve::neon) << "\n";
+  std::cout << "ASIMD : " << std::boolalpha << eve::is_supported(eve::aarch64) << "\n";
 
   return 0;
 }
