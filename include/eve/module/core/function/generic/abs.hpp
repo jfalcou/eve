bbc23a4dba79425dc54426cb07d4a9047093aa64
jtlap <jtlapreste@gmail.com>
2020-08-21 07:58:30 +0200

    abs max not ok for scalars

--- include/eve/module/core/function/generic/abs.hpp
+++ include/eve/module/core/function/generic/abs.hpp
@@ -29,9 +29,10 @@ namespace eve::detail
   {
     if constexpr( has_native_abi_v<T> )
     {
-            if constexpr( floating_value<T> ) return bit_andnot(a, Mzero(as(a)));
-      else  if constexpr( unsigned_value<T> ) return a;
-      else                                    return eve::max(a, -a);
+            if constexpr( floating_value<T> )               return bit_andnot(a, Mzero(as(a)));
+      else  if constexpr( unsigned_value<T> )               return a;
+      else  if constexpr( signed_integral_scalar_value<T> ) return a < T(0) ? -a : a;
+      else                                                  return eve::max(a, -a);
     }
     else
     {
