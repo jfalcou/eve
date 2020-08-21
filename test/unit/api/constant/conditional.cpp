f923c3bbe8e2d513fcac31dcad96113610dea682
Joel Falcou <joel.falcou@lri.fr>
2020-08-21 14:31:08 +0200

    Added test for ignore_first

--- test/unit/api/constant/conditional.cpp
+++ test/unit/api/constant/conditional.cpp
@@ -76,3 +76,33 @@ TTS_CASE("ignore_last behavior")
   TTS_EQUAL( ignore_last(value.size()).mask(as_<type>())          , eve::False( as_<type>() ) );
   TTS_EQUAL( (if_else(ignore_last(value.size()),value, type(69))) , type(69)                  );
 }
+
+TTS_CASE("ignore_first behavior")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::ignore_first;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::ignore_first>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  TTS_EQUAL( ignore_first(0).mask(as_<type>())         , eve::True( as_<type>() ));
+  TTS_EQUAL( (if_else(ignore_first(0),value, type(69))), value                   );
+
+  for(int i = 1;i < value.size();i++)
+  {
+    logical<type> mref  = [i](auto j, auto) { return j >= i; };
+    type          ref   = [i,value](auto j, auto) { return (j >= i) ? value[j] : 69.f; };
+
+    TTS_EQUAL( ignore_first(i).mask(as_<type>()), mref );
+    TTS_EQUAL( (if_else(ignore_first(i),value, type(69))), ref);
+  }
+
+  TTS_EQUAL( ignore_first(value.size()).mask(as_<type>())          , eve::False( as_<type>() ) );
+  TTS_EQUAL( (if_else(ignore_first(value.size()),value, type(69))) , type(69)                  );
+}
