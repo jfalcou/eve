714aef55b3ac6ffe8078e52649738d0c1f301989
Joel Falcou <joel.falcou@lri.fr>
2020-08-21 16:41:04 +0200

    [WIP] Clarify {ignore/keep}_between

--- include/eve/conditional.hpp
+++ include/eve/conditional.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <eve/as.hpp>
+#include <eve/detail/abi.hpp>
 #include <eve/traits.hpp>
 #include <eve/concept/value.hpp>
 #include <iosfwd>
@@ -39,7 +40,7 @@ namespace eve
 
     if_or_(C const& c, V const& v) : condition_(c), alternative(v) {}
 
-    template<typename T> auto mask(as_<T> const&)  const { return condition_; }
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&)  const { return condition_; }
 
     friend std::ostream& operator<<(std::ostream& os, if_or_ const& c)
     {
@@ -61,7 +62,7 @@ namespace eve
 
     if_not_or_(C const& c, V const& v) : condition_(c), alternative(v) {}
 
-    template<typename T> auto mask(as_<T> const&) const { return condition_;  }
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const { return condition_;  }
 
     friend std::ostream& operator<<(std::ostream& os, if_not_or_ const& c)
     {
@@ -83,10 +84,8 @@ namespace eve
 
     if_(C const& c) : condition_(c) {}
 
-    template<typename V>
-    auto else_(V v) const { return if_or_(condition_,v); }
-
-    template<typename T> auto mask(as_<T> const&)  const { return condition_; }
+    template<typename V> EVE_FORCEINLINE auto else_(V v) const { return if_or_(condition_,v); }
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&)  const { return condition_; }
 
     friend std::ostream& operator<<(std::ostream& os, if_ const& c)
     {
@@ -107,10 +106,8 @@ namespace eve
 
     if_not_(C const& c) : condition_(c) {}
 
-    template<typename V>
-    auto else_(V v) const { return if_not_or_(condition_,v); }
-
-    template<typename T> auto mask(as_<T> const&)  const { return condition_;  }
+    template<typename V> EVE_FORCEINLINE auto else_(V v) const { return if_not_or_(condition_,v); }
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&)  const { return condition_;  }
 
     friend std::ostream& operator<<(std::ostream& os, if_not_ const& c)
     {
@@ -129,7 +126,7 @@ namespace eve
     static constexpr bool is_inverted     = false;
     static constexpr bool is_complete     = true;
 
-    template<typename T> auto mask(as_<T> const&) const
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       return eve::as_logical_t<T>(false);
     }
@@ -148,7 +145,7 @@ namespace eve
     static constexpr bool is_inverted     = true;
     static constexpr bool is_complete     = true;
 
-    template<typename T> auto mask(as_<T> const&) const
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       return eve::as_logical_t<T>(true);
     }
@@ -164,21 +161,21 @@ namespace eve
   //================================================================================================
   // Helper structure to encode ignoring the last N elements
   //================================================================================================
-  struct ignore_last
+  struct ignore_last_
   {
     static constexpr bool has_alternative = false;
     static constexpr bool is_inverted     = false;
     static constexpr bool is_complete     = false;
 
-    constexpr ignore_last(std::ptrdiff_t n) noexcept : count_(n) {}
+    constexpr ignore_last_(std::ptrdiff_t n) noexcept : count_(n) {}
 
-    template<typename T> auto mask(as_<T> const&) const
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       constexpr auto card = cardinal_v<T>;
       return Iota(eve::as_<T>()) < (card-count_);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, ignore_last const& c)
+    friend std::ostream& operator<<(std::ostream& os, ignore_last_ const& c)
     {
       return os << "ignore_last( " << c.count_ << " )";
     }
@@ -186,23 +183,58 @@ namespace eve
     std::ptrdiff_t count_;
   };
 
+  EVE_FORCEINLINE ignore_last_ ignore_last(std::ptrdiff_t n) noexcept
+  {
+    return {n};
+  }
+
   //================================================================================================
-  // Helper structure to encode ignoring the first N elements
+  // Helper structure to encode keeping the last N elements
   //================================================================================================
-  struct ignore_first
+  struct keep_last_
   {
     static constexpr bool has_alternative = false;
     static constexpr bool is_inverted     = false;
     static constexpr bool is_complete     = false;
 
-    constexpr ignore_first(std::ptrdiff_t n) noexcept : count_(n) {}
+    constexpr EVE_FORCEINLINE keep_last_(std::ptrdiff_t n) noexcept : count_(n) {}
 
     template<typename T> auto mask(as_<T> const&) const
+    {
+      constexpr auto card = cardinal_v<T>;
+      return Iota(eve::as_<T>()) >= (card-count_);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, keep_last_ const& c)
+    {
+      return os << "keep_last( " << c.count_ << " )";
+    }
+
+    std::ptrdiff_t count_;
+  };
+
+  EVE_FORCEINLINE keep_last_ keep_last(std::ptrdiff_t n) noexcept
+  {
+    return {n};
+  }
+
+  //================================================================================================
+  // Helper structure to encode ignoring the first N elements
+  //================================================================================================
+  struct ignore_first_
+  {
+    static constexpr bool has_alternative = false;
+    static constexpr bool is_inverted     = false;
+    static constexpr bool is_complete     = false;
+
+    constexpr ignore_first_(std::ptrdiff_t n) noexcept : count_(n) {}
+
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       return Iota(eve::as_<T>()) >= count_;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, ignore_first const& c)
+    friend std::ostream& operator<<(std::ostream& os, ignore_first_ const& c)
     {
       return os << "ignore_first( " << c.count_ << " )";
     }
@@ -210,100 +242,130 @@ namespace eve
     std::ptrdiff_t count_;
   };
 
+  EVE_FORCEINLINE ignore_first_ ignore_first(std::ptrdiff_t n) noexcept
+  {
+    return {n};
+  }
+
+  //================================================================================================
+  // Helper structure to encode keep the first N elements
+  //================================================================================================
+  struct keep_first_
+  {
+    static constexpr bool has_alternative = false;
+    static constexpr bool is_inverted     = false;
+    static constexpr bool is_complete     = false;
+
+    constexpr keep_first_(std::ptrdiff_t n) noexcept : count_(n) {}
+
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
+    {
+      return Iota(eve::as_<T>()) < count_;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, keep_first_ const& c)
+    {
+      return os << "keep_first( " << c.count_ << " )";
+    }
+
+    std::ptrdiff_t count_;
+  };
+
+  EVE_FORCEINLINE keep_first_ keep_first(std::ptrdiff_t n) noexcept
+  {
+    return {n};
+  }
+
   //================================================================================================
   // Helper structure to encode ignoring elements in between two index
   //================================================================================================
-  template<bool isRelative> struct ignore_between
+  struct ignore_between_
   {
     static constexpr bool has_alternative = false;
     static constexpr bool is_inverted     = false;
     static constexpr bool is_complete     = false;
 
-    constexpr ignore_between(std::ptrdiff_t b, std::ptrdiff_t e) noexcept : begin_(b), end_(e) {}
+    constexpr ignore_between_(std::ptrdiff_t b, std::ptrdiff_t e) noexcept : begin_(b), end_(e) {}
 
-    constexpr ignore_between(ignore_first b, ignore_last e) noexcept
-            : begin_(b.count_), end_(e.count_)
-    {}
-
-    template<typename T> auto mask(as_<T> const&) const
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       auto const i = Iota(eve::as_<T>());
-
-      if constexpr(isRelative)
-      {
-        constexpr auto card = cardinal_v<T> - 1;
-        return (i < begin_) || (i > (card-end_));
-      }
-      else
-      {
-        return (i < begin_) || (i > end_);
-      }
+      return (i < begin_) || (i > end_);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, ignore_between const& c)
+    friend std::ostream& operator<<(std::ostream& os, ignore_between_ const& c)
     {
-      if constexpr(isRelative)
-      {
-        return os << "ignore_between( first(" << c.begin_ << "), last(" << c.end_ << ") )";
-      }
-      else
-      {
-        return os << "ignore_between( " << c.begin_ << ", " << c.end_ << " )";
-      }
+      return os << "ignore_between( " << c.begin_ << ", " << c.end_ << " )";
     }
 
     std::ptrdiff_t begin_, end_;
   };
 
-  ignore_between(std::ptrdiff_t,std::ptrdiff_t)                   -> ignore_between<false>;
-  ignore_between(ignore_first, ignore_last) -> ignore_between<true>;
+  EVE_FORCEINLINE ignore_between_ ignore_between(std::ptrdiff_t b, std::ptrdiff_t e)
+  {
+    return {b,e};
+  }
 
   //================================================================================================
   // Helper structure to encode keeping elements in between two index and ignoring the rest
-  // with optional remembrance of relative indexing
   //================================================================================================
-  template<bool isRelative> struct keep_between
+  struct keep_between_
   {
     static constexpr bool has_alternative = false;
     static constexpr bool is_inverted     = false;
     static constexpr bool is_complete     = false;
 
-    constexpr keep_between(std::ptrdiff_t b, std::ptrdiff_t e) noexcept : begin_(b), end_(e) {}
+    constexpr keep_between_(std::ptrdiff_t b, std::ptrdiff_t e) noexcept : begin_(b), end_(e) {}
 
-    constexpr keep_between(ignore_first b, ignore_last e) noexcept
-            : begin_(b.count_), end_(e.count_)
-    {}
-
-    template<typename T> auto mask(as_<T> const&) const
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
     {
       auto const i = Iota(eve::as_<T>());
-
-      if constexpr(isRelative)
-      {
-        constexpr auto card = cardinal_v<T> - 1;
-        return (i >= begin_) && (i <= (card-end_));
-      }
-      else
-      {
-        return (i >= begin_) && (i <= end_);
-      }
+      return (i >= begin_) && (i <= end_);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, keep_between const& c)
+    friend std::ostream& operator<<(std::ostream& os, keep_between_ const& c)
     {
-      if constexpr(isRelative)
-      {
-        return os << "keep_between( first(" << c.begin_ << "), last(" << c.end_ << ") )";
-      }
-      else
-      {
-        return os << "keep_between( " << c.begin_ << ", " << c.end_ << " )";
-      }
+      return os << "keep_between( " << c.begin_ << ", " << c.end_ << " )";
     }
 
     std::ptrdiff_t begin_, end_;
   };
 
-  keep_between(std::ptrdiff_t,std::ptrdiff_t)                   -> keep_between<false>;
-  keep_between(ignore_first, ignore_last) -> keep_between<true>;
+  EVE_FORCEINLINE keep_between_ keep_between(std::ptrdiff_t b, std::ptrdiff_t e)
+  {
+    return {b,e};
+  }
+
+  //================================================================================================
+  // Helper structure to encode ignoring N elements on both extrema.
+  //================================================================================================
+  struct ignore_extrema_
+  {
+    static constexpr bool has_alternative = false;
+    static constexpr bool is_inverted     = false;
+    static constexpr bool is_complete     = false;
+
+    constexpr ignore_extrema_(std::ptrdiff_t b, std::ptrdiff_t e) noexcept
+            : first_count_(b), last_count_(e)
+    {}
+
+    template<typename T> EVE_FORCEINLINE auto mask(as_<T> const&) const
+    {
+      auto const i = Iota(eve::as_<T>());
+      constexpr auto card = cardinal_v<T> - 1;
+      return (i >= first_count_) && (i <= (card-last_count_));
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, ignore_extrema_ const& c)
+    {
+      return os << "ignore( first(" << c.first_count_ << "), last(" << c.last_count_ << ") )";
+    }
+
+    std::ptrdiff_t first_count_, last_count_;
+  };
+
+  constexpr EVE_FORCEINLINE ignore_extrema_ operator&&( ignore_first_ a, ignore_last_ b) noexcept
+  {
+    return {a.count_, b.count_};
+  }
 }
--- test/unit/api/constant/conditional.cpp
+++ test/unit/api/constant/conditional.cpp
@@ -27,6 +27,7 @@ TTS_CASE("ignore_all behavior")
 
   using type = wide<float>;
 
+  std::cout << ignore_all << "\n";
   TTS_EQUAL( ignore_all.mask( as_<type>() )           , eve::False( as_<type>() ) );
   TTS_EQUAL( (if_else(ignore_all,type(42), type(69))) , type(69)                  );
 }
@@ -43,6 +44,7 @@ TTS_CASE("ignore_none behavior")
 
   using type = wide<float>;
 
+  std::cout << ignore_none << "\n";
   TTS_EQUAL( ignore_none.mask( as_<type>() )            , eve::True( as_<type>() )  );
   TTS_EQUAL( (if_else(ignore_none,type(42), type(69)))  , type(42)                  );
 }
@@ -56,11 +58,12 @@ TTS_CASE("ignore_last behavior")
   using eve::if_else;
   using eve::as_;
 
-  TTS_EXPECT( conditional_expr<eve::ignore_last>  );
+  TTS_EXPECT( conditional_expr<eve::ignore_last_>  );
 
   using type = wide<float>;
   type value = [](auto i, auto) { return 1.f+i; };
 
+  std::cout << ignore_last(0) << "\n";
   TTS_EQUAL( ignore_last(0).mask(as_<type>())         , eve::True( as_<type>() ));
   TTS_EQUAL( (if_else(ignore_last(0),value, type(69))), value                   );
 
@@ -69,12 +72,47 @@ TTS_CASE("ignore_last behavior")
     logical<type> mref  = [i](auto j, auto c) { return j < c-i; };
     type          ref   = [i,value](auto j, auto c) { return (j < c-i) ? value[j] : 69.f; };
 
+    std::cout << ignore_last(i) << "\n";
     TTS_EQUAL( ignore_last(i).mask(as_<type>()), mref );
     TTS_EQUAL( (if_else(ignore_last(i),value, type(69))), ref);
   }
 
+    std::cout << ignore_last(type::static_size) << "\n";
   TTS_EQUAL( ignore_last(type::static_size).mask(as_<type>())          , eve::False( as_<type>() ) );
   TTS_EQUAL( (if_else(ignore_last(type::static_size),value, type(69))) , type(69)                  );
+}
+
+TTS_CASE("keep_last behavior")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::keep_last;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::keep_last_>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  std::cout << keep_last(type::static_size) << "\n";
+  TTS_EQUAL( keep_last(type::static_size).mask(as_<type>())         , eve::True( as_<type>() ));
+  TTS_EQUAL( (if_else(keep_last(type::static_size),value, type(69))), value                   );
+
+  for(int i = 1;i < type::static_size;i++)
+  {
+    logical<type> mref  = [i](auto j, auto c) { return j >= c-i; };
+    type          ref   = [i,value](auto j, auto c) { return (j >= c-i) ? value[j] : 69.f; };
+
+    std::cout << keep_last(i) << "\n";
+    TTS_EQUAL( keep_last(i).mask(as_<type>()), mref );
+    TTS_EQUAL( (if_else(keep_last(i),value, type(69))), ref);
+  }
+
+    std::cout << keep_last(0) << "\n";
+  TTS_EQUAL( keep_last(0).mask(as_<type>())          , eve::False( as_<type>() ) );
+  TTS_EQUAL( (if_else(keep_last(0),value, type(69))) , type(69)                  );
 }
 
 TTS_CASE("ignore_first behavior")
@@ -86,11 +124,12 @@ TTS_CASE("ignore_first behavior")
   using eve::if_else;
   using eve::as_;
 
-  TTS_EXPECT( conditional_expr<eve::ignore_first>  );
+  TTS_EXPECT( conditional_expr<eve::ignore_first_>  );
 
   using type = wide<float>;
   type value = [](auto i, auto) { return 1.f+i; };
 
+  std::cout << ignore_first(0) << "\n";
   TTS_EQUAL( ignore_first(0).mask(as_<type>())         , eve::True( as_<type>() ));
   TTS_EQUAL( (if_else(ignore_first(0),value, type(69))), value                   );
 
@@ -99,10 +138,164 @@ TTS_CASE("ignore_first behavior")
     logical<type> mref  = [i](auto j, auto) { return j >= i; };
     type          ref   = [i,value](auto j, auto) { return (j >= i) ? value[j] : 69.f; };
 
+    std::cout << ignore_first(i) << "\n";
     TTS_EQUAL( ignore_first(i).mask(as_<type>()), mref );
     TTS_EQUAL( (if_else(ignore_first(i),value, type(69))), ref);
   }
 
+  std::cout << ignore_first(type::static_size) << "\n";
   TTS_EQUAL( ignore_first(type::static_size).mask(as_<type>())          , eve::False( as_<type>() ) );
   TTS_EQUAL( (if_else(ignore_first(type::static_size),value, type(69))) , type(69)                  );
+}
+
+TTS_CASE("keep_first behavior")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::keep_first;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::keep_first_>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  std::cout << keep_first(type::static_size) << "\n";
+  TTS_EQUAL( keep_first(type::static_size).mask(as_<type>())         , eve::True( as_<type>() ));
+  TTS_EQUAL( (if_else(keep_first(type::static_size),value, type(69))), value                   );
+
+  for(int i = 1;i < type::static_size;i++)
+  {
+    logical<type> mref  = [i](auto j, auto) { return j < i; };
+    type          ref   = [i,value](auto j, auto) { return (j < i) ? value[j] : 69.f; };
+
+    std::cout << keep_first(i) << "\n";
+    TTS_EQUAL( keep_first(i).mask(as_<type>()), mref );
+    TTS_EQUAL( (if_else(keep_first(i),value, type(69))), ref);
+  }
+
+  std::cout << keep_first(0) << "\n";
+  TTS_EQUAL( keep_first(0).mask(as_<type>())          , eve::False( as_<type>() ) );
+  TTS_EQUAL( (if_else(keep_first(0),value, type(69))) , type(69)                  );
+}
+
+TTS_CASE("ignore_between behavior")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::ignore_between;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::ignore_between_>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  std::cout << ignore_between(type::static_size,type::static_size) << "\n";
+  TTS_EQUAL ( ignore_between(type::static_size,type::static_size).mask(as_<type>())
+            , eve::True( as_<type>() )
+            );
+  TTS_EQUAL( (if_else(ignore_between(type::static_size,type::static_size),value,type(69))), value);
+
+  std::cout << ignore_between(0,type::static_size) << "\n";
+  TTS_EQUAL( ignore_between(0,type::static_size).mask(as_<type>())          , eve::False( as_<type>() ) );
+  TTS_EQUAL( (if_else(ignore_between(0,type::static_size),value, type(69))) , type(69)                  );
+
+  for(int fi = 0;fi < type::static_size;fi++)
+  {
+    for(int li = 0;li < type::static_size;li++)
+    {
+      logical<type> mref  = [=](auto j, auto)       { return !(j >= fi && j <= li); };
+      type          ref   = [=](auto j, auto) { return !(j >= fi && j <= li) ? value[j] : 69.f; };
+
+      std::cout << ignore_between(fi,li) << "\n";
+      TTS_EQUAL( ignore_between(fi,li).mask(as_<type>()), mref );
+      TTS_EQUAL( (if_else(ignore_between(fi,li),value, type(69))), ref);
+    }
+  }
+}
+
+TTS_CASE("keep_between behavior")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::keep_between;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::keep_between_>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  std::cout << keep_between(0,type::static_size) << "\n";
+  TTS_EQUAL ( keep_between(0,type::static_size).mask(as_<type>())
+            , eve::True( as_<type>() )
+            );
+  TTS_EQUAL( (if_else(keep_between(0,type::static_size),value,type(69))), value);
+
+  std::cout << keep_between(type::static_size,type::static_size) << "\n";
+  TTS_EQUAL( keep_between(type::static_size,type::static_size).mask(as_<type>())          , eve::False( as_<type>() ) );
+  TTS_EQUAL( (if_else(keep_between(type::static_size,type::static_size),value, type(69))) , type(69)                  );
+
+  for(int fi = 0;fi < type::static_size;fi++)
+  {
+    for(int li = 0;li < type::static_size;li++)
+    {
+      logical<type> mref  = [=](auto j, auto)       { return (j >= fi && j <= li); };
+      type          ref   = [=](auto j, auto) { return (j >= fi && j <= li) ? value[j] : 69.f; };
+
+      std::cout << keep_between(fi,li) << "\n";
+      TTS_EQUAL( keep_between(fi,li).mask(as_<type>()), mref );
+      TTS_EQUAL( (if_else(keep_between(fi,li),value, type(69))), ref);
+    }
+  }
+}
+
+TTS_CASE("ignore_first/last combination")
+{
+  using eve::wide;
+  using eve::logical;
+  using eve::conditional_expr;
+  using eve::ignore_first;
+  using eve::ignore_last;
+  using eve::if_else;
+  using eve::as_;
+
+  TTS_EXPECT( conditional_expr<eve::ignore_extrema_>  );
+
+  using type = wide<float>;
+  type value = [](auto i, auto) { return 1.f+i; };
+
+  constexpr auto sz = type::static_size / 2;
+  std::cout << (ignore_first(sz) && ignore_last(sz)) << "\n";
+  TTS_EQUAL ( (ignore_first(sz) && ignore_last(sz)).mask(as_<type>())
+            , eve::False( as_<type>() )
+            );
+  TTS_EQUAL( (if_else(ignore_first(sz) && ignore_last(sz),value,type(69))), type(69));
+
+  std::cout << (ignore_first(0) && ignore_last(0)) << "\n";
+  TTS_EQUAL ( (ignore_first(0) && ignore_last(0)).mask(as_<type>())
+            , eve::True( as_<type>() )
+            );
+  TTS_EQUAL( (if_else(ignore_first(0) && ignore_last(0),value,type(69))), value);
+
+  for(int fi = 0;fi < type::static_size;fi++)
+  {
+    for(int li = 0;li < type::static_size;li++)
+    {
+      logical<type> mref  = [=](auto j, auto c) { return (j >= fi) && (j <= (c-li-1)); };
+      type          ref   = [=](auto j, auto c) { return (j >= fi) && (j <= (c-li-1)) ? value[j] : 69.f; };
+
+      std::cout << (ignore_first(fi) && ignore_last(li)) << "\n";
+
+      TTS_EQUAL( (ignore_first(fi) && ignore_last(li)).mask(as_<type>()), mref );
+      TTS_EQUAL( (if_else(ignore_first(fi) && ignore_last(li),value, type(69))), ref);
+    }
+  }
 }
