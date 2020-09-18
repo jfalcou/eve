5425d4210b5d4e49a396117f9073afa799d2d343
jtlap <jtlapreste@gmail.com>
2020-09-18 09:04:00 +0200

    test for radindeg and acoth

--- test/random/module/math/radindeg/regular/radindeg.hpp
+++ test/random/module/math/radindeg/regular/radindeg.hpp
@@ -9,8 +9,11 @@
 **/
 //==================================================================================================
 #include <eve/function/radindeg.hpp>
+#include <eve/function/sqr.hpp>
+#include <eve/function/sqrt.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
+#include <eve/constant/eps.hpp>
 #include <eve/constant/pi.hpp>
 #include <tts/tests/range.hpp>
 #include "measures.hpp"
@@ -20,8 +23,17 @@
 TTS_CASE_TPL("wide random check on radindeg", EVE_TYPE)
 {
   using v_t = eve::element_type_t<T>;
-  auto std_indeg = tts::vectorize<T>( [](auto e) { return e*180/eve::pi(eve::as<v_t>()); } );
+  auto rid =  [](auto e)
+    {
+      if (eve::abs(e) < eve::eps(eve::as(e)))
+        return (eve::sqr((eve::sqrt(e)/eve::sqrt(eve::pi(eve::as<v_t>())))*eve::sqrt(v_t(180))));
+      else
+        return (e/eve::pi(eve::as<v_t>()))*v_t(180);
+    };
+
+
+  auto std_indeg = tts::vectorize<T>( rid );
 
   eve::rng_producer<T> p(eve::valmin(eve::as<v_t>()), eve::valmax(eve::as<v_t>()));
-  TTS_RANGE_CHECK(p, std_indeg, eve::radindeg);
+  TTS_ULP_RANGE_CHECK(p, std_indeg, eve::radindeg, 4);
 }
