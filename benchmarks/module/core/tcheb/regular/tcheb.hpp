e7f71b70dc4ae3f0945503e311180fd52575e638
jtlap <jtlapreste@gmail.com>
2020-09-29 18:55:27 +0200

    tcheb unit test and skeleton

---
+++ benchmarks/module/core/tcheb/regular/tcheb.hpp
@@ -0,0 +1,18 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/module/core/detail/generic/tcheb.hpp>
+#include <eve/wide.hpp>
+#include <eve/constant/zero.hpp>
+
+int main()
+{
+  // TO DO
+}
