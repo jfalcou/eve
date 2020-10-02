9980d594e9e56de9db71e58e6061ed34e3fdc9ab
jtlap <jtlapreste@gmail.com>
2020-10-02 21:47:30 +0200

    math functions

--- include/eve/function/ieee754.hpp
+++ include/eve/function/ieee754.hpp
@@ -11,17 +11,15 @@
 // **=======================================================
 // helper file to include all ieee754 related functions
 #include <eve/function/bitofsign.hpp>
-#include <eve/function/bit_exponent.hpp>
-#include <eve/function/bit_mantisaa.hpp>
+// #include <eve/function/bit_exponent.hpp>
+// #include <eve/function/bit_mantisaa.hpp>
 #include <eve/function/exponent.hpp>
 #include <eve/function/frexp.hpp>
 #include <eve/function/ifrexp.hpp>
 #include <eve/function/ldexp.hpp>
 #include <eve/function/mantissa.hpp>
 #include <eve/function/nb_values.hpp>
-#include <eve/function/next.hpp>
 #include <eve/function/nextafter.hpp>
-#include <eve/function/prev.hpp>
-#include <eve/function/two_add.hpp>
-#include <eve/function/two_prod.hpp>
+// #include <eve/function/two_add.hpp>
+// #include <eve/function/two_prod.hpp>
 #include <eve/function/ulpdist.hpp>
--- include/eve/function/trigonometric.hpp
+++ include/eve/function/trigonometric.hpp
@@ -9,13 +9,24 @@
 //==================================================================================================
 #pragma once
 
-// helper file to include all trigonometric functions
-#include <eve/detail/overload.hpp>
-
-namespace eve
-{
-  EVE_MAKE_CALLABLE(trigonometric_, trigonometric);
-}
-
-
-
+// helper file to include all inverse trigonometric functions
+#include <eve/function/cos.hpp>
+#include <eve/function/cosd.hpp>
+#include <eve/function/cospi.hpp
+#include <eve/function/cot.hpp>
+#include <eve/function/cotd.hpp>
+#include <eve/function/cotpi.hpp>
+#include <eve/function/csc.hpp>
+#include <eve/function/cscd.hpp>
+#include <eve/function/cscpi.hpp>
+#include <eve/function/sec.hpp>
+#include <eve/function/secd.hpp>
+#include <eve/function/secpi.hpp>
+#include <eve/function/sin.hpp>
+#include <eve/function/sinc.hpp
+#include <eve/function/sind.hpp>
+#include <eve/function/sinpi.hpp>
+#include <eve/function/sinpic.hpp>
+#include <eve/function/tan.hpp>
+#include <eve/function/tand.hpp>
+#include <eve/function/tanpi.hpp>
