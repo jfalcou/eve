2fc20b831ee41a4bfb38830ebc1a70bf6be3a606
jtlap <jtlapreste@gmail.com>
2020-10-02 21:59:00 +0200

    advanced functions

---
+++ include/eve/function/memory.hpp
@@ -0,0 +1,15 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+// **=======================================================
+// helper file to include all arithmetic functions
+#include <eve/function/load.hpp>
+#include <eve/function/gather.hpp>
+#include <eve/function/store.hpp>
