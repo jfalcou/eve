9ec6d8887505ec6a5d417f4b2766845d4dde2870
jtlap <jtlapreste@gmail.com>
2020-10-04 16:54:30 +0200

    hz_device revisited with no state nor object

--- test/unit/module/core/essai_hz/regular/essai_hz.hpp
+++ test/unit/module/core/essai_hz/regular/essai_hz.hpp
@@ -11,44 +11,55 @@
 #include <eve/wide.hpp>
 #include <eve/constant/nan.hpp>
 #include <eve/constant/inf.hpp>
+#include <eve/constant/true.hpp>
 #include <tts/tests/precision.hpp>
 #include <tts/tests/relation.hpp>
 #include <tts/tests/types.hpp>
-#include <eve/detail/horizontal_choices.hpp>
+#include <eve/detail/hz_device.hpp>
+#include <eve/function/any.hpp>
+#include <eve/function/if_else.hpp>
 
 TTS_CASE_TPL("Check eve::arg return type", EVE_TYPE)
 {
-  using eve::detail::hz_device;
+  using eve::any;
   using elt_t = eve::element_type_t < T >;
   using w_t = eve::wide<elt_t, eve::fixed<8>>;
-  auto f0 = [](auto x){return x; };
-  auto f1 = [](auto x){return x+100; };
-  auto f2 = [](auto x){return x+2000; };
-  auto f3 = [](auto x){return x+30000; };
-  auto f4 = [](auto x){return x+400000;  };
+  auto f0 = [](auto ){return 0; };
+  auto f1 = [](auto ){return 100; };
+  auto f2 = [](auto ){return 2000; };
+  auto f3 = [](auto ){return 30000; };
+  auto f4 = [](auto ){return 400000;  };
   elt_t lim0 = 1;
   elt_t lim1 = 2;
   elt_t lim2 = 4;
   elt_t lim3 = 6;
 //  elt_t lim4 = eve::inf(eve::as(lim0));
-  w_t x{2.5, 1.3, 1.3, 2.5, 4.5, 7.8, 0.1, 3.4};
-  hz_device < w_t> hz(36);
-  hz.first_interval(f0, lim0, x) &&
-    hz.next_interval(f1, lim1)&&
-    hz.next_interval(f2, lim2)&&
-    hz.last_interval(f3, f4, lim3);
+  w_t x{6.5, 1.3, 1.3, 2.5, 4.5, 7.8, 0.1, 3.4};
+  w_t r(36);
+  auto notdone = eve::true_(as(r));
+  notdone = first_interval(f0, notdone, x < lim0, r, x);
+  if(any(notdone)) notdone = next_interval(f1,  notdone, x < lim1, r, x);
+//  std::cout << notdone << std::endl;
+  if(any(notdone)) notdone = next_interval(f2,  notdone, x < lim2, r, x);
+//   std::cout <<  notdone << std::endl;
+  if(any(notdone)) notdone = next_interval(f3,  notdone, x < lim3, r, x);
+//   std::cout << "bef kast " <<  notdone << std::endl;
+  if(any(notdone)) last_interval(f4, notdone, r, x) ;
+//   std::cout <<  notdone << std::endl;
+//   std::cout << " ============================= " << std::endl;
 
-  std::cout << hz.result() << std::endl;
+  std::cout << "r " << r << std::endl;
   std::cout << " ============================= " << std::endl;
-  w_t r{2002.5, 101.3, 101.3, 2002.5, 30004.5, 400007.8, 0.1, 2003.4};
-  TTS_IEEE_EQUAL(r,hz.result());
-
-  hz_device < elt_t> hz1;
-  hz1.first_interval(f0, lim0, 2.5);
-  if (hz1.notdone()) {std::cout << 1 << std::endl; hz1.next_interval(f1, lim1); }
-  if (hz1.notdone()) {std::cout << 2 << std::endl; hz1.next_interval(f2, lim2); }
-  if (hz1.notdone()) {std::cout << 3 << std::endl; hz1.last_interval(f3, f4, lim3); }
-  std::cout << hz1.result() << std::endl;
+  w_t r2{400000, 100, 100, 2000, 30000, 400000, 0, 2000};
+  TTS_IEEE_EQUAL(r2,r);
+//   w_t r1(36);
+//   (first_interval(f0, notdone, x < lim0, r1, x)&&
+//    (next_interval(f2,  notdone, x < lim2, r1, x))&&
+//    (next_interval(f3,  notdone, x < lim3, r1, x))&&
+//    (last_interval(f4, notdone, r1, x)));
+//   std::cout << "r1 " << r1 << std::endl;
+//   std::cout << " ============================= " << std::endl;
+//   TTS_IEEE_EQUAL(r2,r1);
 
 
 }
