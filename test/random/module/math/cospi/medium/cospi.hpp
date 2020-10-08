95b28332946479d69695e4048d69878f665dadd1
jtlap <jtlapreste@gmail.com>
2020-10-08 18:31:27 +0200

    avoiding bad limits with cospi medium test

--- test/random/module/math/cospi/medium/cospi.hpp
+++ test/random/module/math/cospi/medium/cospi.hpp
@@ -10,14 +10,23 @@
 //==================================================================================================
 #include <eve/detail/function/tmp/boost_math_cospi.hpp>
 #include <eve/function/cospi.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
 #include "producers.hpp"
 #include <cmath>
 #include <type_traits>
 
 TTS_CASE("wide random check on cospi")
 {
-  auto my_stdcospi = [](auto x) -> EVE_VALUE {return boost::math::cos_pi(x); };
-
-  eve::uniform_prng<EVE_VALUE> p(EVE_VALUE(-100000.0), EVE_VALUE(100000.0));
-  TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  auto my_stdcospi = [](auto x) {return EVE_VALUE(boost::math::cos_pi(x)); };
+  if constexpr(eve::floating_value<EVE_TYPE>)
+  {
+    eve::uniform_prng<EVE_VALUE> p(EVE_VALUE(-100000), EVE_VALUE(100000));
+    TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  }
+  else
+  {
+    eve::uniform_prng<EVE_VALUE> p(eve::valmin(eve::as<EVE_VALUE>()), eve::valmax(eve::as<EVE_VALUE>()));
+    TTS_RANGE_CHECK(p, my_stdcospi, eve::medium(eve::cospi));
+  }
 }
