d1e22f2d5994db7bbf0981e3a953e64a9b721a62
jtlap <jtlapreste@gmail.com>
2020-11-09 21:18:36 +0100

    jacobi_elliptic test

--- test/unit/module/elliptic/jacobi_elliptic/regular/jacobi_elliptic.hpp
+++ test/unit/module/elliptic/jacobi_elliptic/regular/jacobi_elliptic.hpp
@@ -14,8 +14,6 @@
 #include <eve/constant/one.hpp>
 #include <eve/constant/pio_2.hpp>
 #include <eve/constant/zero.hpp>
-#include <eve/constant/inf.hpp>
-#include <eve/constant/minf.hpp>
 #include <eve/wide.hpp>
 
 
@@ -27,24 +25,11 @@ TTS_CASE_TPL("Check eve::jacobi_elliptic behavior", EVE_TYPE)
   using elt_t = eve::element_type_t<T>;
   using eve::as;
 
-//  auto inf   =  eve::inf(as<elt_t>());
-//   auto minf  =  eve::minf(as<elt_t>());
   auto nan   =  eve::nan(as<elt_t>());
   auto pio_2 =  eve::pio_2(as<elt_t>());
   auto one   =  eve::one(as<elt_t>());
   auto zero  =  eve::zero(as<elt_t>());
 
-// #ifndef boost_simd_no_invalids
-//   TTS_ULP_EQUAL(eve::jacobi_elliptic(inf, elt_t(0)), inf, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(minf, elt_t(0)), minf, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(nan, elt_t(0)), nan, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(inf, elt_t(0.5)), nan, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(minf, elt_t(0.5)), nan, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(nan, elt_t(0.5)), nan, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(inf, elt_t(1)), pio_2, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(minf, elt_t(1)), -pio_2, 1.0);
-//   TTS_ULP_EQUAL(jacobi_elliptic(nan, elt_t(1)), nan, 1.0);
-// #endif
   auto refje = []<typename U>(U x,  U y){
     U cn, dn;
     U sn = boost::math::jacobi_elliptic(y, x, &cn, &dn);
@@ -122,10 +107,4 @@ TTS_CASE_TPL("Check eve::jacobi_elliptic behavior", EVE_TYPE)
     TTS_ULP_EQUAL(ecn, nan, 1);
     TTS_ULP_EQUAL(edn, nan, 1);
   }
-//   {
-//     auto [esn, ecn, edn] = jacobi_elliptic(inf,elt_t(1.0));
-//     TTS_ULP_EQUAL(esn, elt_t(1), 1);
-//     TTS_ULP_EQUAL(ecn, elt_t(0), 1);
-//     TTS_ULP_EQUAL(edn, elt_t(0), 1);
-//   }
 }
