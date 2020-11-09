f917c58b74a70c9c13df9fe970c426ab6eec1b93
Joel Falcou <joel.falcou@lri.fr>
2020-11-10 00:02:59 +0100

    More unpack tests

--- test/unit/api/wide/swizzle/unpack/swizzle.hpp
+++ test/unit/api/wide/swizzle/unpack/swizzle.hpp
@@ -15,37 +15,47 @@
 template<int N, int O>
 inline constexpr auto partial_unpack = eve::swizzler<N>([](int i, int){ return (i/2)+O; });
 
-template<typename T, typename Env, typename Filler>
-void test_unpack(Env& runtime, bool verbose, Filler filler)
+template<int N, int O, bool Left>
+inline constexpr auto unpack_with_zero  =  eve::swizzler<N>([](int i, int)
+                                            {
+                                              bool s = i%2 == 0;
+                                              if constexpr(Left) return s ? -1 : (i/2)+O;
+                                              else               return s ? (i/2)+O : -1;
+                                            });
+
+template<typename T, typename Env, typename Filler, typename Pattern>
+void test_unpack(Env& runtime, bool verbose, Filler filler, Pattern pt)
 {
-  if constexpr(eve::cardinal_v<T> == 1) TTS_PASS("Type is too small to swizzle.");
+  if constexpr(eve::cardinal_v<T> < 1) TTS_PASS("Type is too small to swizzle.");
 
   T simd(filler);
   [&]<std::size_t... I>( std::index_sequence<I...>)
   {
     auto f  = [&]<std::size_t N>(std::integral_constant<std::size_t,N>)
               {
-                constexpr std::size_t sz = 1ULL << (N+1);
+                constexpr std::size_t sz = 1ULL << (N+2);
                 if constexpr(sz <= EVE_CARDINAL)
                 {
                   [&]<std::size_t... V>(std::index_sequence<V...>)
                   {
                     ([&]()
                     {
-                      constexpr auto pt = partial_unpack<sz,V>;
-                      std::cout << "using pattern " << pt << "\n";
+                      auto upt = pt ( std::integral_constant<std::size_t,sz>()
+                                    , std::integral_constant<std::size_t,V>()
+                                    );
+                      std::cout << "using pattern " << upt << "\n";
 
                       using type = typename T::template reshape<eve::fixed<sz>>;
 
                       // constexpr inside lambda leads to strange error :/
-                      type ref([sz=sz,pt,&simd](int i, int)
+                      type ref([sz=sz,p=upt,&simd](int i, int)
                               {
-                                auto idx = pt(i,sz);
+                                auto idx = p(i,sz);
                                 bool check = idx != -1;
                                 return check ? simd[check ? idx : 0] : typename type::value_type(0);
                               }
                               );
-                      TTS_EQUAL((simd[pt]), ref);
+                      TTS_EQUAL((simd[upt]), ref);
                     }(), ...);
 
                   }( std::make_index_sequence<EVE_CARDINAL-sz/2+1>{} );
@@ -53,15 +63,22 @@ void test_unpack(Env& runtime, bool verbose, Filler filler)
               };
 
     ( f(std::integral_constant<std::size_t,I>{}), ... );
-  }( std::make_index_sequence<6>{} );
+  }( std::make_index_sequence<5>{} );
 }
 
 TTS_CASE_TPL( "Check unpack swizzle for arithmetic type", EVE_TYPE )
 {
-  test_unpack<T>(runtime, verbose, [](int i, int) { return 1+i; } );
+  auto f =  [](int i, int) { return 1+i; };
+  test_unpack<T>(runtime, verbose, f, [](auto sz, auto v) { return partial_unpack<sz,v>; } );
+  test_unpack<T>(runtime, verbose, f, [](auto sz, auto v) { return unpack_with_zero<sz,v,true>; } );
+  test_unpack<T>(runtime, verbose, f, [](auto sz, auto v) { return unpack_with_zero<sz,v,false  >; } );
 }
 
 TTS_CASE_TPL("Check unpack swizzle for logical type", EVE_TYPE )
 {
-  test_unpack<eve::logical<T>>(runtime, verbose, [](int i, int) { return i%3==0; } );
+  using L = eve::logical<T>;
+  auto  f =  [](int i, int) { return i%3==0; };
+  test_unpack<L>(runtime, verbose, f, [](auto sz, auto v) { return partial_unpack<sz,v>; } );
+  test_unpack<L>(runtime, verbose, f, [](auto sz, auto v) { return unpack_with_zero<sz,v,true>; } );
+  test_unpack<L>(runtime, verbose, f, [](auto sz, auto v) { return unpack_with_zero<sz,v,false  >; } );
 }
