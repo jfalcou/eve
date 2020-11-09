420f0a5f286bed1fed7429690d517cbd4e9882d7
Joel Falcou <joel.falcou@lri.fr>
2020-11-09 19:39:53 +0100

    Added unpack test (non-zeroed)

---
+++ test/unit/api/wide/swizzle/unpack/swizzle.hpp
@@ -0,0 +1,67 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+#include <eve/logical.hpp>
+#include <eve/wide.hpp>
+
+template<int N, int O>
+inline constexpr auto partial_unpack = eve::swizzler<N>([](int i, int){ return (i/2)+O; });
+
+template<typename T, typename Env, typename Filler>
+void test_unpack(Env& runtime, bool verbose, Filler filler)
+{
+  if constexpr(eve::cardinal_v<T> == 1) TTS_PASS("Type is too small to swizzle.");
+
+  T simd(filler);
+  [&]<std::size_t... I>( std::index_sequence<I...>)
+  {
+    auto f  = [&]<std::size_t N>(std::integral_constant<std::size_t,N>)
+              {
+                constexpr std::size_t sz = 1ULL << (N+1);
+                if constexpr(sz <= EVE_CARDINAL)
+                {
+                  [&]<std::size_t... V>(std::index_sequence<V...>)
+                  {
+                    ([&]()
+                    {
+                      constexpr auto pt = partial_unpack<sz,V>;
+                      std::cout << "using pattern " << pt << "\n";
+
+                      using type = typename T::template reshape<eve::fixed<sz>>;
+
+                      // constexpr inside lambda leads to strange error :/
+                      type ref([sz=sz,pt,&simd](int i, int)
+                              {
+                                auto idx = pt(i,sz);
+                                bool check = idx != -1;
+                                return check ? simd[check ? idx : 0] : typename type::value_type(0);
+                              }
+                              );
+                      TTS_EQUAL((simd[pt]), ref);
+                    }(), ...);
+
+                  }( std::make_index_sequence<EVE_CARDINAL-sz/2+1>{} );
+                }
+              };
+
+    ( f(std::integral_constant<std::size_t,I>{}), ... );
+  }( std::make_index_sequence<6>{} );
+}
+
+TTS_CASE_TPL( "Check unpack swizzle for arithmetic type", EVE_TYPE )
+{
+  test_unpack<T>(runtime, verbose, [](int i, int) { return 1+i; } );
+}
+
+TTS_CASE_TPL("Check unpack swizzle for logical type", EVE_TYPE )
+{
+  test_unpack<eve::logical<T>>(runtime, verbose, [](int i, int) { return i%3==0; } );
+}
