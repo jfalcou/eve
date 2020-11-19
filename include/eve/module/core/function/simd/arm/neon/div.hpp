26565c51001977c8d27c6dac2a17af73a8449fc2
jtlap <jtlapreste@gmail.com>
2020-11-19 18:24:50 +0100

    div

--- include/eve/module/core/function/simd/arm/neon/div.hpp
+++ include/eve/module/core/function/simd/arm/neon/div.hpp
@@ -17,7 +17,7 @@ namespace eve::detail
 {
   template<real_scalar_value T, typename N, arm_abi ABI>
   EVE_FORCEINLINE wide<T, N, ABI> div_(EVE_SUPPORTS(neon128_)
-                                      , wide<T, N, ABI> const &v0
+                                      , wide<T, N, ABI> v0
                                       , wide<T, N, ABI> const &v1) noexcept
   {
     return v0 /= v1;
