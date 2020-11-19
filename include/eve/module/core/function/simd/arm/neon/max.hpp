b1cac54628b5e33e81a1c340c9e2477528d810f1
jtlap <jtlapreste@gmail.com>
2020-11-19 22:37:02 +0100

    min max

--- include/eve/module/core/function/simd/arm/neon/max.hpp
+++ include/eve/module/core/function/simd/arm/neon/max.hpp
@@ -10,61 +10,44 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/implementation.hpp>
 #include <eve/concept/value.hpp>
-#include <eve/function/logical_and.hpp>
-#include <eve/function/bit_and.hpp>
-#include <eve/function/is_eqz.hpp>
-#include <eve/platform.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
 
 namespace eve::detail
 {
-  template<real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> max_(EVE_SUPPORTS(neon128_),
-                                           wide<T, N, arm_64_> const &v0,
-                                           wide<T, N, arm_64_> const &v1) noexcept
+  template<typename T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> max_(EVE_SUPPORTS(neon128_),
+                                       wide<T, N, ABI> const &v0,
+                                       wide<T, N, ABI> const &v1) noexcept
   {
-    constexpr bool is_signed_int   = std::is_integral_v<T> && std::is_signed_v<T>;
-    constexpr bool is_unsigned_int = std::is_integral_v<T> && std::is_unsigned_v<T>;
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
 
-         if constexpr(std::is_same_v<T, float>)          return vmax_f32(v0, v1);
+          if constexpr( cat == category::int32x4  ) return vmaxq_s32(v0, v1);
+    else  if constexpr( cat == category::int16x8  ) return vmaxq_s16(v0, v1);
+    else  if constexpr( cat == category::int8x16  ) return vmaxq_s8(v0, v1);
+    else  if constexpr( cat == category::uint32x4 ) return vmaxq_u32(v0, v1);
+    else  if constexpr( cat == category::uint16x8 ) return vmaxq_u16(v0, v1);
+    else  if constexpr( cat == category::uint8x16 ) return vmaxq_u8(v0, v1);
+    else  if constexpr( cat == category::float32x4) return vmaxq_f32(v0, v1);
+    else  if constexpr( cat == category::int32x2  ) return vmax_s32(v0, v1);
+    else  if constexpr( cat == category::int16x4  ) return vmax_s16(v0, v1);
+    else  if constexpr( cat == category::int8x8   ) return vmax_s8(v0, v1);
+    else  if constexpr( cat == category::uint32x2 ) return vmax_u32(v0, v1);
+    else  if constexpr( cat == category::uint16x4 ) return vmax_u16(v0, v1);
+    else  if constexpr( cat == category::uint8x8  ) return vmax_u8(v0, v1);
+    else  if constexpr( cat == category::float32x2) return vmax_f32(v0, v1);
 #if defined(__aarch64__)
-    else if constexpr(std::is_same_v<T, double>)         return vmax_f64(v0, v1);
+    else  if constexpr( cat == category::float64x1) return vmax_f64(v0, v1);
+    else  if constexpr( cat == category::float64x2) return vmaxq_f64(v0, v1);
+#else
+    else  if constexpr( sizeof(T) == 8 )            return map(max, v0, v1);
 #endif
-    else if constexpr(is_signed_int && sizeof(T) == 8)   return map(eve::max, v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 4)   return vmax_s32(v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 2)   return vmax_s16(v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 1)   return vmax_s8(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 8) return map(eve::max, v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 4) return vmax_u32(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 2) return vmax_u16(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 1) return vmax_u8(v0, v1);
-  }
-
-  template<real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> max_(EVE_SUPPORTS(neon128_),
-                                            wide<T, N, arm_128_> const &v0,
-                                            wide<T, N, arm_128_> const &v1) noexcept
-  {
-    constexpr bool is_signed_int   = std::is_integral_v<T> && std::is_signed_v<T>;
-    constexpr bool is_unsigned_int = std::is_integral_v<T> && std::is_unsigned_v<T>;
-
-         if constexpr(std::is_same_v<T, float>)         return vmaxq_f32(v0, v1);
-#if defined(__aarch64__)
-    else if constexpr(std::is_same_v<T, double>)         return vmaxq_f64(v0, v1);
-#endif
-    else if constexpr(is_signed_int && sizeof(T) == 8)   return map(eve::max, v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 4)   return vmaxq_s32(v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 2)   return vmaxq_s16(v0, v1);
-    else if constexpr(is_signed_int && sizeof(T) == 1)   return vmaxq_s8(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 8) return map(eve::max, v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 4) return vmaxq_u32(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 2) return vmaxq_u16(v0, v1);
-    else if constexpr(is_unsigned_int && sizeof(T) == 1) return vmaxq_u8(v0, v1);
   }
 
   template<typename T, typename N, arm_abi ABI>
-  EVE_FORCEINLINE wide<T, N, ABI> max_( EVE_SUPPORTS(neon128_), pedantic_type const &
+  EVE_FORCEINLINE wide<T, N, ABI> max_( EVE_SUPPORTS(neon128_)
+                                      , pedantic_type const &
                                       , wide<T, N, ABI> const &a0
                                       , wide<T, N, ABI> const &a1
                                       ) noexcept
@@ -74,3 +57,68 @@ namespace eve::detail
     return if_else(is_eqz(a0) && is_eqz(a1), bit_and(a0, a1), tmp);
   }
 }
+
+// #include <eve/detail/implementation.hpp>
+// #include <eve/concept/value.hpp>
+// #include <eve/function/logical_and.hpp>
+// #include <eve/function/bit_and.hpp>
+// #include <eve/function/is_eqz.hpp>
+// #include <eve/platform.hpp>
+
+// namespace eve::detail
+// {
+//   template<real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> max_(EVE_SUPPORTS(neon128_),
+//                                            wide<T, N, arm_64_> const &v0,
+//                                            wide<T, N, arm_64_> const &v1) noexcept
+//   {
+//     constexpr bool is_signed_int   = std::is_integral_v<T> && std::is_signed_v<T>;
+//     constexpr bool is_unsigned_int = std::is_integral_v<T> && std::is_unsigned_v<T>;
+
+//          if constexpr(std::is_same_v<T, float>)          return vmax_f32(v0, v1);
+// #if defined(__aarch64__)
+//     else if constexpr(std::is_same_v<T, double>)         return vmax_f64(v0, v1);
+// #endif
+//     else if constexpr(is_signed_int && sizeof(T) == 8)   return map(eve::max, v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 4)   return vmax_s32(v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 2)   return vmax_s16(v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 1)   return vmax_s8(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 8) return map(eve::max, v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 4) return vmax_u32(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 2) return vmax_u16(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 1) return vmax_u8(v0, v1);
+//   }
+
+//   template<real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_> max_(EVE_SUPPORTS(neon128_),
+//                                             wide<T, N, arm_128_> const &v0,
+//                                             wide<T, N, arm_128_> const &v1) noexcept
+//   {
+//     constexpr bool is_signed_int   = std::is_integral_v<T> && std::is_signed_v<T>;
+//     constexpr bool is_unsigned_int = std::is_integral_v<T> && std::is_unsigned_v<T>;
+
+//          if constexpr(std::is_same_v<T, float>)         return vmaxq_f32(v0, v1);
+// #if defined(__aarch64__)
+//     else if constexpr(std::is_same_v<T, double>)         return vmaxq_f64(v0, v1);
+// #endif
+//     else if constexpr(is_signed_int && sizeof(T) == 8)   return map(eve::max, v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 4)   return vmaxq_s32(v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 2)   return vmaxq_s16(v0, v1);
+//     else if constexpr(is_signed_int && sizeof(T) == 1)   return vmaxq_s8(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 8) return map(eve::max, v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 4) return vmaxq_u32(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 2) return vmaxq_u16(v0, v1);
+//     else if constexpr(is_unsigned_int && sizeof(T) == 1) return vmaxq_u8(v0, v1);
+//   }
+
+//   template<typename T, typename N, arm_abi ABI>
+//   EVE_FORCEINLINE wide<T, N, ABI> max_( EVE_SUPPORTS(neon128_), pedantic_type const &
+//                                       , wide<T, N, ABI> const &a0
+//                                       , wide<T, N, ABI> const &a1
+//                                       ) noexcept
+//   {
+//     auto tmp = eve::max(a0, a1);
+//     if constexpr(eve::platform::supports_invalids) tmp = if_else(is_nan(a1), a0, tmp);
+//     return if_else(is_eqz(a0) && is_eqz(a1), bit_and(a0, a1), tmp);
+//   }
+// }
