92d88ff77a09c4fd3266a28c9ddd1993700cd682
jtlap <jtlapreste@gmail.com>
2020-11-19 17:37:53 +0100

    ceil div extract

--- include/eve/module/core/function/simd/arm/neon/ceil.hpp
+++ include/eve/module/core/function/simd/arm/neon/ceil.hpp
@@ -10,46 +10,67 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
-#include <type_traits>
 #include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
 
 namespace eve::detail
 {
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> ceil_(EVE_SUPPORTS(neon128_),
-                                            wide<T, N, arm_64_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> ceil_(EVE_SUPPORTS(neon128_)
+                                       , wide<T, N, ABI> const& v) noexcept
   {
-#if __ARM_ARCH >= 8
-    if constexpr(std::is_same_v<T, double>)
-#  if defined(__aarch64__)
-      return vrndp_f64(v0);
-#  else
-      return map(ceil, v0);
-#  endif
-    if constexpr(std::is_same_v<T, float>) return vrndp_f32(v0);
-#else
-    if constexpr(std::is_floating_point_v<T>) return map(ceil, v0);
-#endif
-  }
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
 
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> ceil_(EVE_SUPPORTS(neon128_),
-                                             wide<T, N, arm_128_> const &v0) noexcept
-  {
-#if __ARM_ARCH >= 8
-    if constexpr(std::is_same_v<T, double>)
-#  if defined(__aarch64__)
-      return vrndpq_f64(v0);
+#  if defined(__aarch64__) && __ARM_ARCH >= 8
+         if constexpr( cat == category::float64x1) return vrndp_f64(v0);
+    else if constexpr( cat == category::float64x2) return vrndpq_f64(v0);
+    else                                           return map(ceil, v0);
 #  else
-      return map(ceil, v0);
-#  endif
-    if constexpr(std::is_same_v<T, float>) return vrndpq_f32(v0);
-#else
-    if constexpr(std::is_floating_point_v<T>) return map(ceil, v0);
-#endif
+    return map(ceil, v0);
+#  endif 
   }
 }
 
+// #include <eve/detail/overload.hpp>
+// #include <eve/detail/abi.hpp>
+// #include <eve/forward.hpp>
+// #include <type_traits>
+// #include <eve/concept/value.hpp>
+
+// namespace eve::detail
+// {
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> ceil_(EVE_SUPPORTS(neon128_),
+//                                             wide<T, N, arm_64_> const &v0) noexcept
+//   {
+// #if __ARM_ARCH >= 8
+//     if constexpr(std::is_same_v<T, double>)
+// #  if defined(__aarch64__)
+//       return vrndp_f64(v0);
+// #  else
+//       return map(ceil, v0);
+// #  endif
+//     if constexpr(std::is_same_v<T, float>) return vrndp_f32(v0);
+// #else
+//     if constexpr(std::is_floating_point_v<T>) return map(ceil, v0);
+// #endif
+//   }
+
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_> ceil_(EVE_SUPPORTS(neon128_),
+//                                              wide<T, N, arm_128_> const &v0) noexcept
+//   {
+// #if __ARM_ARCH >= 8
+//     if constexpr(std::is_same_v<T, double>)
+// #  if defined(__aarch64__)
+//       return vrndpq_f64(v0);
+// #  else
+//       return map(ceil, v0);
+// #  endif
+//     if constexpr(std::is_same_v<T, float>) return vrndpq_f32(v0);
+// #else
+//     if constexpr(std::is_floating_point_v<T>) return map(ceil, v0);
+// #endif
+//   }
+// }
--- include/eve/module/core/function/simd/arm/neon/div.hpp
+++ include/eve/module/core/function/simd/arm/neon/div.hpp
@@ -15,18 +15,29 @@
 
 namespace eve::detail
 {
-  template<real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_>
-                  div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> v0, wide<T, N, arm_64_> const &v1) noexcept
+  template<real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, arm_abi>
+                  div_(EVE_SUPPORTS(neon128_)
+                      , wide<T, N, arm_64_> const &v0
+                      , wide<T, N, arm_64_> const &v1) noexcept
   {
     return v0 /= v1;
   }
 
-  template<real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_>
-                  div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> v0, wide<T, N, arm_128_> const &v1) noexcept
-  {
-    return v0 /= v1;
-  }
 }
+// namespace eve::detail
+// {
+//   template<real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_>
+//                   div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> v0, wide<T, N, arm_64_> const &v1) noexcept
+//   {
+//     return v0 /= v1;
+//   }
 
+//   template<real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_>
+//                   div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> v0, wide<T, N, arm_128_> const &v1) noexcept
+//   {
+//     return v0 /= v1;
+//   }
+// }
