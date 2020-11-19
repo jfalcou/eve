d00fcd3e34b06a0a43c52dff6ec08117f99d4a4e
jtlap <jtlapreste@gmail.com>
2020-11-19 17:57:18 +0100

    floor fma

--- include/eve/module/core/function/simd/arm/neon/floor.hpp
+++ include/eve/module/core/function/simd/arm/neon/floor.hpp
@@ -10,46 +10,67 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/overload.hpp>
-#include <eve/detail/abi.hpp>
-#include <eve/forward.hpp>
-#include <type_traits>
 #include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
 
 namespace eve::detail
 {
-  template<typename floating_scalar_value, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> floor_(EVE_SUPPORTS(neon128_),
-                                             wide<T, N, arm_64_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> floor_(EVE_SUPPORTS(neon128_)
+                                       , wide<T, N, ABI> const& v) noexcept
   {
-#if __ARM_ARCH >= 8
-    if constexpr(std::is_same_v<T, double>)
-#  if defined(__aarch64__)
-      return vrndm_f64(v0);
-#  else
-      return map(floor, v0);
-#  endif
-    if constexpr(std::is_same_v<T, float>) return vrndm_f32(v0);
-#else
-    if constexpr(std::is_floating_point_v<T>) return map(floor, v0);
-#endif
-  }
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
 
-  template<typename floating_scalar_value, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> floor_(EVE_SUPPORTS(neon128_),
-                                              wide<T, N, arm_128_> const &v0) noexcept
-  {
-#if __ARM_ARCH >= 8
-    if constexpr(std::is_same_v<T, double>)
-#  if defined(__aarch64__)
-      return vrndmq_f64(v0);
+#  if defined(__aarch64__) && __ARM_ARCH >= 8
+         if constexpr( cat == category::float64x1) return vrndm_f64(v0);
+    else if constexpr( cat == category::float64x2) return vrndmq_f64(v0);
+    else                                           return map(floor, v0);
 #  else
-      return map(floor, v0);
+    return map(floor, v0);
 #  endif
-    if constexpr(std::is_same_v<T, float>) return vrndmq_f32(v0);
-#else
-    if constexpr(std::is_floating_point_v<T>) return map(floor, v0);
-#endif
   }
 }
 
+// #include <eve/detail/overload.hpp>
+// #include <eve/detail/abi.hpp>
+// #include <eve/forward.hpp>
+// #include <type_traits>
+// #include <eve/concept/value.hpp>
+
+// namespace eve::detail
+// {
+//   template<typename floating_scalar_value, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> floor_(EVE_SUPPORTS(neon128_),
+//                                              wide<T, N, arm_64_> const &v0) noexcept
+//   {
+// #if __ARM_ARCH >= 8
+//     if constexpr(std::is_same_v<T, double>)
+// #  if defined(__aarch64__)
+//       return vrndm_f64(v0);
+// #  else
+//       return map(floor, v0);
+// #  endif
+//     if constexpr(std::is_same_v<T, float>) return vrndm_f32(v0);
+// #else
+//     if constexpr(std::is_floating_point_v<T>) return map(floor, v0);
+// #endif
+//   }
+
+//   template<typename floating_scalar_value, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_> floor_(EVE_SUPPORTS(neon128_),
+//                                               wide<T, N, arm_128_> const &v0) noexcept
+//   {
+// #if __ARM_ARCH >= 8
+//     if constexpr(std::is_same_v<T, double>)
+// #  if defined(__aarch64__)
+//       return vrndmq_f64(v0);
+// #  else
+//       return map(floor, v0);
+// #  endif
+//     if constexpr(std::is_same_v<T, float>) return vrndmq_f32(v0);
+// #else
+//     if constexpr(std::is_floating_point_v<T>) return map(floor, v0);
+// #endif
+//   }
+// }
