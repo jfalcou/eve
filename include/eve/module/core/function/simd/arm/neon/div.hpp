14339be7b993be355c2424fe87ccb147068c8d3b
jtlap <jtlapreste@gmail.com>
2020-11-20 11:25:40 +0100

    suppressing comments on updated files

--- include/eve/module/core/function/simd/arm/neon/div.hpp
+++ include/eve/module/core/function/simd/arm/neon/div.hpp
@@ -23,19 +23,3 @@ namespace eve::detail
     return v0 /= v1;
   }
 }
-// namespace eve::detail
-// {
-//   template<real_scalar_value T, typename N>
-//   EVE_FORCEINLINE wide<T, N, arm_64_>
-//                   div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> v0, wide<T, N, arm_64_> const &v1) noexcept
-//   {
-//     return v0 /= v1;
-//   }
-
-//   template<real_scalar_value T, typename N>
-//   EVE_FORCEINLINE wide<T, N, arm_128_>
-//                   div_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> v0, wide<T, N, arm_128_> const &v1) noexcept
-//   {
-//     return v0 /= v1;
-//   }
-// }
--- include/eve/module/core/function/simd/arm/neon/mul.hpp
+++ include/eve/module/core/function/simd/arm/neon/mul.hpp
@@ -38,42 +38,4 @@ namespace eve::detail
   {
     return v1 *= v0;
   }
-
-//   template<real_scalar_value T, typename N>
-//   EVE_FORCEINLINE wide<T, N, arm_64_>
-//                   mul_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> v0, wide<T, N, arm_64_> const &v1) noexcept
-//   {
-//     return v0 *= v1;
-//   }
-
-//   template<real_scalar_value T, typename N, real_scalar_value U>
-//   EVE_FORCEINLINE auto mul_(EVE_SUPPORTS(neon128_), U const &v0, wide<T, N, arm_64_> v1)
-//   {
-//     return v1 *= v0;
-//   }
-
-//   template<real_scalar_value T, typename N, real_scalar_value U>
-//   EVE_FORCEINLINE auto mul_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> v0, U const &v1) noexcept
-//   {
-//     return v0 *= v1;
-//   }
-
-//   template<real_scalar_value T, typename N>
-//   EVE_FORCEINLINE wide<T, N, arm_128_>
-//                   mul_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> v0, wide<T, N, arm_128_> const &v1) noexcept
-//   {
-//     return v0 *= v1;
-//   }
-
-//   template<real_scalar_value T, typename N, real_scalar_value U>
-//   EVE_FORCEINLINE auto mul_(EVE_SUPPORTS(neon128_), U const &v0, wide<T, N, arm_128_> v1)
-//   {
-//     return v1 *= v0;
-//   }
-
-//   template<real_scalar_value T, typename N, real_scalar_value U>
-//   EVE_FORCEINLINE auto mul_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> v0, U const &v1) noexcept
-//   {
-//     return v0 *= v1;
-//   }
 }
