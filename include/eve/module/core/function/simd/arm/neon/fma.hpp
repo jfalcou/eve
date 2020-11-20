174cb64a5157af103dcb8db3d13df5b893d97b27
jtlap <jtlapreste@gmail.com>
2020-11-20 18:28:08 +0100

    fma

--- include/eve/module/core/function/simd/arm/neon/fma.hpp
+++ include/eve/module/core/function/simd/arm/neon/fma.hpp
@@ -36,12 +36,12 @@ namespace eve::detail
     else  if constexpr( cat == category::int32x2  ) return  vmla_s32(v2, v1, v0);
     else  if constexpr( cat == category::int16x4  ) return  vmla_s16(v2, v1, v0);
     else  if constexpr( cat == category::int8x8   ) return   vmla_s8(v2, v1, v0);
-    else  if constexpr( cat == category::int32x4  ) return vmlaq_u32(v2, v1, v0);
-    else  if constexpr( cat == category::int16x8  ) return vmlaq_u16(v2, v1, v0);
-    else  if constexpr( cat == category::int8x16  ) return  vmlaq_u8(v2, v1, v0);
-    else  if constexpr( cat == category::int32x2  ) return  vmla_u32(v2, v1, v0);
-    else  if constexpr( cat == category::int16x4  ) return  vmla_u16(v2, v1, v0);
-    else  if constexpr( cat == category::int8x8   ) return   vmla_u8(v2, v1, v0);
+    else  if constexpr( cat == category::uint32x4 ) return vmlaq_u32(v2, v1, v0);
+    else  if constexpr( cat == category::uint16x8 ) return vmlaq_u16(v2, v1, v0);
+    else  if constexpr( cat == category::uint8x16 ) return  vmlaq_u8(v2, v1, v0);
+    else  if constexpr( cat == category::uint32x2 ) return  vmla_u32(v2, v1, v0);
+    else  if constexpr( cat == category::uint16x4 ) return  vmla_u16(v2, v1, v0);
+    else  if constexpr( cat == category::uint8x8  ) return   vmla_u8(v2, v1, v0);
     else  if constexpr( sizeof(T) == 8 )            return  map(fma, v0, v1, v2);
   }
 
