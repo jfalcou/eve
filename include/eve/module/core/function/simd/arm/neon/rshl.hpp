a123eaed6cf9bc8d9a6ad634a4069cd2f30abee4
jtlap <jtlapreste@gmail.com>
2020-11-20 20:39:51 +0100

    sqrt restored

--- include/eve/module/core/function/simd/arm/neon/rshl.hpp
+++ include/eve/module/core/function/simd/arm/neon/rshl.hpp
@@ -14,39 +14,57 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/module/core/function/simd/arm/neon/detail/shift.hpp>
 
-#include <type_traits>
-
 namespace eve::detail
 {
-  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
-  EVE_FORCEINLINE wide<T, N, arm_64_> rshl_(EVE_SUPPORTS(neon128_),
-                                            wide<T, N, arm_64_> const &v0,
-                                            wide<I, N, arm_64_> const &v1) noexcept
+  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> rshl_(EVE_SUPPORTS(neon128_),
+                                        wide<T, N, ABI> const &v0,
+                                        wide<I, N, ABI> const &v1) noexcept
   {
     return neon_shifter(v0, v1);
   }
 
-  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
-  EVE_FORCEINLINE wide<T, N, arm_64_>
-                  rshl_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> const &v0, I v1) noexcept
+  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> rshl_(EVE_SUPPORTS(neon128_),
+                                        wide<T, N, ABI> const &v0,
+                                        I const &v1) noexcept
   {
     using i_t = wide<as_integer_t<T, signed>, N>;
-    return eve::rshl(v0, i_t(v1));
+    return neon_shifter(v0, i_t(v1));
   }
 
-  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
-  EVE_FORCEINLINE auto rshl_(EVE_SUPPORTS(neon128_),
-                             wide<T, N, arm_128_> const &v0,
-                             wide<I, N, arm_128_> const &v1) noexcept
-  {
-    return neon_shifter(v0, v1);
-  }
+// #include <type_traits>
 
-  template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
-  EVE_FORCEINLINE auto rshl_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> const &v0, I v1) noexcept
-  {
-    using i_t = wide<as_integer_t<T, signed>, N>;
-    return eve::rshl(v0, i_t(v1));
-  }
-}
+// namespace eve::detail
+// {
+//   template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> rshl_(EVE_SUPPORTS(neon128_),
+//                                             wide<T, N, arm_64_> const &v0,
+//                                             wide<I, N, arm_64_> const &v1) noexcept
+//   {
+//     return neon_shifter(v0, v1);
+//   }
 
+//   template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
+//   EVE_FORCEINLINE wide<T, N, arm_64_>
+//                   rshl_(EVE_SUPPORTS(neon128_), wide<T, N, arm_64_> const &v0, I v1) noexcept
+//   {
+//     using i_t = wide<as_integer_t<T, signed>, N>;
+//     return eve::rshl(v0, i_t(v1));
+//   }
+
+//   template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
+//   EVE_FORCEINLINE auto rshl_(EVE_SUPPORTS(neon128_),
+//                              wide<T, N, arm_128_> const &v0,
+//                              wide<I, N, arm_128_> const &v1) noexcept
+//   {
+//     return neon_shifter(v0, v1);
+//   }
+
+//   template<integral_real_scalar_value T, typename N, integral_real_scalar_value I>
+//   EVE_FORCEINLINE auto rshl_(EVE_SUPPORTS(neon128_), wide<T, N, arm_128_> const &v0, I v1) noexcept
+//   {
+//     using i_t = wide<as_integer_t<T, signed>, N>;
+//     return eve::rshl(v0, i_t(v1));
+//   }
+// }
