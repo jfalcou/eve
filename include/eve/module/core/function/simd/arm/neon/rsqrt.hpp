48e10eed83c30bebcb49420e42c2d754dadb0c02
jtlap <jtlapreste@gmail.com>
2020-11-20 21:17:53 +0100

    rsqrt

--- include/eve/module/core/function/simd/arm/neon/rsqrt.hpp
+++ include/eve/module/core/function/simd/arm/neon/rsqrt.hpp
@@ -20,134 +20,199 @@
 #include <eve/function/ldexp.hpp>
 #include <eve/concept/value.hpp>
 
+#include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/function/rsqrt.hpp>
+#include <eve/function/if_else.hpp>
+#include <eve/function/is_not_finite.hpp>
+
 namespace eve::detail
 {
-  //------------------------------------------------------------------------------------------------
-  // Raw version
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_>
-                  rsqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_64_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> rsqrt_(EVE_SUPPORTS(neon128_)
+                                        , raw_type const &
+                                        , wide<T, N, ABI> const& v0) noexcept
   {
-    using that_t = wide<T, N, arm_64_>;
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
 
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return that_t(vrsqrte_f64(v0)); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      return that_t(vrsqrte_f32(v0));
-    }
-    else
-    {
-      static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
-    }
+         if constexpr( cat == category::float32x2) return vrsqrte_f32(v);
+    else if constexpr( cat == category::float32x4) return vrsqrteq_f32(v);
+#  if defined(__aarch64__)
+    else if constexpr( cat == category::float64x1) return vrsqrte_f64(v);
+    else if constexpr( cat == category::float64x2) return vrsqrteq_f64(v);
+#  endif
+    else                                           return map(rsqrt, v0);
   }
 
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_>
-                  rsqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_128_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> rsqrt_(EVE_SUPPORTS(neon128_)
+                                        , wide<T, N, ABI> const& v0) noexcept
   {
-    using that_t = wide<T, N, arm_128_>;
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
 
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return that_t(vrsqrteq_f64(v0)); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      return that_t(vrsqrteq_f32(v0));
-    }
-    else
-    {
-      static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
-    }
-  }
-
-  //------------------------------------------------------------------------------------------------
-  // Basic version
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> rsqrt_(EVE_SUPPORTS(neon128_),
-                                             wide<T, N, arm_64_> const &v0) noexcept
-  {
-    using that_t = wide<T, N, arm_64_>;
-
-    if constexpr(std::is_same_v<T, double> && supports_aarch64)
-    {
-      that_t inv  = vrsqrte_f64(v0);
-      inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
-      inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
-      return that_t(vmul_f64(vrsqrts_f64(v0, inv * inv), inv));
-    }
-    else if constexpr(std::is_same_v<T, float>)
+          if constexpr( cat == category::float32x2)
     {
       that_t inv  = vrsqrte_f32(v0);
       inv         = vmul_f32(vrsqrts_f32(v0, inv * inv), inv);
       return that_t(vmul_f32(vrsqrts_f32(v0, inv * inv), inv));
     }
-    else
-    {
-      static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
-    }
-  }
-
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> rsqrt_(EVE_SUPPORTS(neon128_),
-                                             wide<T, N, arm_128_> const &v0) noexcept
-  {
-    using that_t = wide<T, N, arm_128_>;
-
-    if constexpr(std::is_same_v<T, double> && supports_aarch64)
-    {
-      that_t inv  = vrsqrteq_f64(v0);
-      inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
-      inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
-      return that_t(vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv));
-    }
-    else if constexpr(std::is_same_v<T, float>)
+    else if constexpr( cat == category::float32x4)
     {
       that_t inv  = vrsqrteq_f32(v0);
       inv         = vmulq_f32(vrsqrtsq_f32(v0, inv * inv), inv);
       return that_t(vmulq_f32(vrsqrtsq_f32(v0, inv * inv), inv));
     }
-    else
+#  if defined(__aarch64__)
+    else if constexpr( cat == category::float64x1)
     {
-      static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
+      using that_t = wide<T, N, ABI>;
+      that_t inv  = vrsqrte_f64(v0);
+      inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
+      inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
+      return that_t(vmul_f64(vrsqrts_f64(v0, inv * inv), inv));
     }
-  }
-
-
-  //------------------------------------------------------------------------------------------------
-  // Pedantic version
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> rsqrt_(EVE_SUPPORTS(neon128_),
-                                             pedantic_type const &,
-                                             wide<T, N, arm_64_> const &v00) noexcept
-  {
-    if (any(is_denormal(v00)))
+    else if constexpr( cat == category::float64x2)
     {
-      using that_t = wide<T, N, arm_64_>;
-      auto[v0, nn] =  pedantic(eve::ifrexp)(v00);
-      auto tst = is_odd(nn);
-      nn  = dec[tst](nn);
-      v0 = mul[tst](v0,2);
-      auto inv = rsqrt(v0);
-      return that_t(pedantic(eve::ldexp)(inv, -nn/2));
+      using that_t = wide<T, N, ABI>;
+      that_t inv  = vrsqrteq_f64(v0);
+      inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
+      inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
+      return that_t(vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv));
     }
-    else return rsqrt(v00);
-  }
-
-
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> rsqrt_(EVE_SUPPORTS(neon128_),
-                                             pedantic_type const &,
-                                             wide<T, N, arm_128_> const &v00) noexcept
-  {
-    if (any(is_denormal(v00)))
-    {
-      using that_t = wide<T, N, arm_128_>;
-      auto[v0, nn] =  pedantic(eve::ifrexp)(v00);
-      auto tst = is_odd(nn);
-      nn  = dec[tst](nn);
-      v0 = mul[tst](v0,2);
-      auto inv = rsqrt(v0);
-      return that_t(pedantic(eve::ldexp)(inv, -nn/2));
-    }
-    else return rsqrt(v00);
+#  endif
+    else return map(rsqrt, v0);
   }
 }
 
+
+// namespace eve::detail
+// {
+//   //------------------------------------------------------------------------------------------------
+//   // Raw version
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_>
+//                   rsqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_64_> const &v0) noexcept
+//   {
+//     using that_t = wide<T, N, arm_64_>;
+
+//     if constexpr(std::is_same_v<T, double> && supports_aarch64) { return that_t(vrsqrte_f64(v0)); }
+//     else if constexpr(std::is_same_v<T, float>)
+//     {
+//       return that_t(vrsqrte_f32(v0));
+//     }
+//     else
+//     {
+//       static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
+//     }
+//   }
+
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_>
+//                   rsqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_128_> const &v0) noexcept
+//   {
+//     using that_t = wide<T, N, arm_128_>;
+
+//     if constexpr(std::is_same_v<T, double> && supports_aarch64) { return that_t(vrsqrteq_f64(v0)); }
+//     else if constexpr(std::is_same_v<T, float>)
+//     {
+//       return that_t(vrsqrteq_f32(v0));
+//     }
+//     else
+//     {
+//       static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
+//     }
+//   }
+
+//   //------------------------------------------------------------------------------------------------
+//   // Basic version
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> rsqrt_(EVE_SUPPORTS(neon128_),
+//                                              wide<T, N, arm_64_> const &v0) noexcept
+//   {
+//     using that_t = wide<T, N, arm_64_>;
+
+//     if constexpr(std::is_same_v<T, double> && supports_aarch64)
+//     {
+//       that_t inv  = vrsqrte_f64(v0);
+//       inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
+//       inv         = vmul_f64(vrsqrts_f64(v0, inv * inv), inv);
+//       return that_t(vmul_f64(vrsqrts_f64(v0, inv * inv), inv));
+//     }
+//     else if constexpr(std::is_same_v<T, float>)
+//     {
+//       that_t inv  = vrsqrte_f32(v0);
+//       inv         = vmul_f32(vrsqrts_f32(v0, inv * inv), inv);
+//       return that_t(vmul_f32(vrsqrts_f32(v0, inv * inv), inv));
+//     }
+//     else
+//     {
+//       static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
+//     }
+//   }
+
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_> rsqrt_(EVE_SUPPORTS(neon128_),
+//                                              wide<T, N, arm_128_> const &v0) noexcept
+//   {
+//     using that_t = wide<T, N, arm_128_>;
+
+//     if constexpr(std::is_same_v<T, double> && supports_aarch64)
+//     {
+//       that_t inv  = vrsqrteq_f64(v0);
+//       inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
+//       inv         = vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv);
+//       return that_t(vmulq_f64(vrsqrtsq_f64(v0, inv * inv), inv));
+//     }
+//     else if constexpr(std::is_same_v<T, float>)
+//     {
+//       that_t inv  = vrsqrteq_f32(v0);
+//       inv         = vmulq_f32(vrsqrtsq_f32(v0, inv * inv), inv);
+//       return that_t(vmulq_f32(vrsqrtsq_f32(v0, inv * inv), inv));
+//     }
+//     else
+//     {
+//       static_assert(std::is_floating_point_v<T>, "[eve::rsqrt] - Unsupported integral parameters");
+//     }
+//   }
+
+
+//   //------------------------------------------------------------------------------------------------
+//   // Pedantic version
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_64_> rsqrt_(EVE_SUPPORTS(neon128_),
+//                                              pedantic_type const &,
+//                                              wide<T, N, arm_64_> const &v00) noexcept
+//   {
+//     if (any(is_denormal(v00)))
+//     {
+//       using that_t = wide<T, N, arm_64_>;
+//       auto[v0, nn] =  pedantic(eve::ifrexp)(v00);
+//       auto tst = is_odd(nn);
+//       nn  = dec[tst](nn);
+//       v0 = mul[tst](v0,2);
+//       auto inv = rsqrt(v0);
+//       return that_t(pedantic(eve::ldexp)(inv, -nn/2));
+//     }
+//     else return rsqrt(v00);
+//   }
+
+
+//   template<floating_real_scalar_value T, typename N>
+//   EVE_FORCEINLINE wide<T, N, arm_128_> rsqrt_(EVE_SUPPORTS(neon128_),
+//                                              pedantic_type const &,
+//                                              wide<T, N, arm_128_> const &v00) noexcept
+//   {
+//     if (any(is_denormal(v00)))
+//     {
+//       using that_t = wide<T, N, arm_128_>;
+//       auto[v0, nn] =  pedantic(eve::ifrexp)(v00);
+//       auto tst = is_odd(nn);
+//       nn  = dec[tst](nn);
+//       v0 = mul[tst](v0,2);
+//       auto inv = rsqrt(v0);
+//       return that_t(pedantic(eve::ldexp)(inv, -nn/2));
+//     }
+//     else return rsqrt(v00);
+//   }
+// }
