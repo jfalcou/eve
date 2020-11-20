7002618c21c1339db34029ed9caa46d6b62e01dd
jtlap <jtlapreste@gmail.com>
2020-11-20 19:10:36 +0100

    sqrt

--- include/eve/module/core/function/simd/arm/neon/sqrt.hpp
+++ include/eve/module/core/function/simd/arm/neon/sqrt.hpp
@@ -10,86 +10,63 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/function/rsqrt.hpp>
+#include <eve/function/if_else.hpp>
 #include <eve/function/is_not_finite.hpp>
-#include <eve/function/raw.hpp>
-#include <eve/concept/value.hpp>
 
 namespace eve::detail
 {
-  //------------------------------------------------------------------------------------------------
-  // Raw version
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_>
-                  sqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_64_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> sqrt_(EVE_SUPPORTS(neon128_)
+                                       , raw_type const &
+                                       , wide<T, N, ABI> const& v0) noexcept
   {
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return vsqrt_f64(v0); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      if constexpr(supports_aarch64) { return vsqrt_f32(v0); }
-      else
-      {
-        return if_else(v0, v0 * raw(rsqrt)(v0), v0);
-      }
-    }
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
+
+#  if defined(__aarch64__)
+         if constexpr( cat == category::float32x2) return vsqrt_f32(v);
+    else if constexpr( cat == category::float32x4) return vsqrtq_f32(v);
+    else if constexpr( cat == category::float64x1) return vsqrt_f64(v);
+    else if constexpr( cat == category::float64x2) return vsqrtq_f64(v);
+#  else
+         if constexpr( cat == category::float32x2) return if_else(v0, v0 * raw(rsqrt)(v0), v0);
+    else if constexpr( cat == category::float32x4) return if_else(v0, v0 * raw(rsqrt)(v0), v0);
+#  endif
+    else                                           return map(sqrt, v0);
   }
 
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_>
-                  sqrt_(EVE_SUPPORTS(neon128_), raw_type const &, wide<T, N, arm_128_> const &v0) noexcept
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> rec_(EVE_SUPPORTS(neon128_),
+                                       wide<T, N, ABI> const &v0) noexcept
   {
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return vsqrtq_f64(v0); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      if constexpr(supports_aarch64) { return vsqrtq_f32(v0); }
-      else
-      {
-        return if_else(v0, v0 * raw(rsqrt)(v0), v0);
-      }
-    }
-  }
-
-  //------------------------------------------------------------------------------------------------
-  // Basic version
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_64_> sqrt_(EVE_SUPPORTS(neon128_),
-                                            wide<T, N, arm_64_> const &v0) noexcept
-  {
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return vsqrt_f64(v0); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      if constexpr(supports_aarch64) { return vsqrt_f32(v0); }
-      else
-      {
-        auto that = if_else(v0, v0 * rsqrt(v0), v0);
-        if constexpr(platform::supports_invalids) { return if_else(is_not_finite(v0), v0, that); }
-        else
-        {
-          return that;
-        }
-      }
-    }
-  }
-
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, arm_128_> sqrt_(EVE_SUPPORTS(neon128_),
-                                             wide<T, N, arm_128_> const &v0) noexcept
-  {
-    if constexpr(std::is_same_v<T, double> && supports_aarch64) { return vsqrtq_f64(v0); }
-    else if constexpr(std::is_same_v<T, float>)
-    {
-      if constexpr(supports_aarch64) { return vsqrtq_f32(v0); }
-      else
-      {
-        auto that = if_else(v0, v0 * rsqrt(v0), v0);
-        if constexpr(platform::supports_invalids) { return if_else(is_not_finite(v0), v0, that); }
-        else
-        {
-          return that;
-        }
-      }
-    }
+      // estimate 1/x with an extra NR step for full precision
+      auto a = refine_rec(v0, raw(rec)(v0));
+      return refine_rec(v0, a);
   }
 }
 
+
+  template<floating_real_scalar_value T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> sqrt_(EVE_SUPPORTS(neon128_)
+                                       , wide<T, N, ABI> const& v) noexcept
+  {
+    constexpr auto cat = categorize<wide<T, N, ABI>>();
+
+#  if defined(__aarch64__)
+    if constexpr( cat == category::float32x2) return vsqrt_f32(v);
+    else if constexpr( cat == category::float32x4) return vsqrtq_f32(v);
+    else if constexpr( cat == category::float64x1) return vsqrt_f64(v);
+    else if constexpr( cat == category::float64x2) return vsqrtq_f64(v);
+#  else
+    if constexpr( (cat == category::float32x2) ||  (cat == category::float32x4))
+    {
+      auto that = if_else(v0, v0 * raw(rsqrt)(v0), v0);
+      if constexpr(platform::supports_invalids) { return if_else(is_not_finite(v0), v0, that); }
+    }
+#  endif
+    else                                           return map(ceil, v);
+  }
+}
