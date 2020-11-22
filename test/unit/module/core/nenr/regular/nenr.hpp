05207ba6997b2643b150f7ad12305bc1c700c23d
jtlap <jtlapreste@gmail.com>
2020-11-22 13:34:13 +0100

    nenr unit test

--- test/unit/module/core/nenr/regular/nenr.hpp
+++ test/unit/module/core/nenr/regular/nenr.hpp
@@ -22,19 +22,9 @@ TTS_CASE_TPL("Check eve::nenr return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::(eve::nenr) behavior", EVE_TYPE)
 {
-  using v_t = eve::element_type_t<T>;
-
   TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(1)), T(27.6666)                           , 0 );
   TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(0)), T(12.73)                             , 0 );
-  TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(0.5)), eve::average(T(12.73), T(27.6666)) , 0 );
-
-  auto e0 = eve::nenr(v_t(1), v_t(2), eve::prev(1 - 10 * eve::eps(eve::as<v_t>())));
-
-  for( v_t  i = 1 - 10 * eve::eps(eve::as<v_t>()); i <= 1 + 10 * eve::eps(eve::as<v_t>());
-            i = eve::next(i) )
-  {
-    auto e1 = eve::nenr(v_t(1), v_t(2), i);
-    TTS_LESS_EQUAL(e0, e1);
-    e0 = e1;
-  }
+  TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(0.4)), T(12.73)                           , 0 );
+  TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(0.5)), T(12.73)                           , 0 );
+  TTS_ULP_EQUAL(eve::nenr(T(12.73), T(27.6666), T(0.6)), T(27.6666)                         , 0 );
 }
