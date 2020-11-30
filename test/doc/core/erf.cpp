4fa7be7738448877f86ced47ed33bf9e9e3739e5
jtlap <jtlapreste@gmail.com>
2020-11-30 15:10:02 +0100

    spurious using

--- test/doc/core/erf.cpp
+++ test/doc/core/erf.cpp
@@ -9,10 +9,8 @@ using wide_ft = eve::wide<float, eve::fixed<8>>;
 
 int main()
 {
-  using eve::pedantic;
-
-wide_ft pf = {0.0f, -0.0f, -1.0f, 1.0f, 2.0f,
-               eve::inf(eve::as<float>()), eve::minf(eve::as<float>()), eve::nan(eve::as<float>())};
+  wide_ft pf = {0.0f, -0.0f, -1.0f, 1.0f, 2.0f,
+                eve::inf(eve::as<float>()), eve::minf(eve::as<float>()), eve::nan(eve::as<float>())};
 
   std::cout << "---- simd" << '\n'
             << "<- pf      = " << pf << '\n'
