0f14cc2ec1ea4c7fa09fe3b8009d6a952c341c0c
jtlap <jtlapreste@gmail.com>
2020-11-30 15:27:54 +0100

    neon min max

---
+++ include/eve/module/core/function/pedantic/simd/arm/neon/max.hpp
@@ -0,0 +1,31 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/function/pedantic.hpp>
+
+namespace eve::detail
+{
+  template<typename T, typename N, arm_abi ABI>
+  EVE_FORCEINLINE wide<T, N, ABI> max_( EVE_SUPPORTS(neon128_)
+                                      , pedantic_type const &
+                                      , wide<T, N, ABI> const &a0
+                                      , wide<T, N, ABI> const &a1
+                                      ) noexcept
+  {
+    auto tmp = eve::max(a0, a1);
+    if constexpr(eve::platform::supports_invalids) tmp = if_else(is_nan(a1), a0, tmp);
+    return if_else(is_eqz(a0) && is_eqz(a1), bit_and(a0, a1), tmp);
+  }
+}
