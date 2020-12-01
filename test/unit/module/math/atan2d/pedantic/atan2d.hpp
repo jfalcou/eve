b6676d1fc4b29076b8e53ef6301ac648dcfefde9
jtlap <jtlapreste@gmail.com>
2020-12-01 09:03:09 +0100

    atan2XX

--- test/unit/module/math/atan2d/pedantic/atan2d.hpp
+++ test/unit/module/math/atan2d/pedantic/atan2d.hpp
@@ -8,7 +8,7 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#include <eve/function/atan2d.hpp>
+#include <eve/function/pedantic/atan2d.hpp>
 #include <eve/function/is_positive.hpp>
 #include <eve/function/is_negative.hpp>
 #include <eve/function/pedantic.hpp>
