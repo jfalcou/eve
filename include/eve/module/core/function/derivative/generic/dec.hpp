037165f834d4e66e922370f08d502fb5db529bbc
jtlap <jtlapreste@gmail.com>
2020-12-05 17:08:08 +0100

    more arithmetic derivatives

---
+++ include/eve/module/core/function/derivative/generic/dec.hpp
@@ -0,0 +1,41 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/dec.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/function/derivative/plus.hpp>
+
+namespace eve::detail
+{
+  template<floating_real_value T, unsigned_value N>
+  EVE_FORCEINLINE constexpr T dec_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<1> const &
+                                   , T x
+                                   , N n) noexcept
+  {
+    if constexpr( has_native_abi_v<T> )
+    {
+      return if_else(is_eqz(n), dec(x), if_else(is_equal(n, 1u), one(as(x)), zero));
+    }
+    else
+      return apply_over(derivative1(dec), x, n);
+  }
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr T dec_(EVE_SUPPORTS(cpu_)
+                                    , derivative_type<1> const &
+                                    , T x) noexcept
+  {
+
+    return one(as(x));
+  }
+}
