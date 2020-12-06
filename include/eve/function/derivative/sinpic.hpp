c9011b2d57c4d13fd75c1480fc8ade96e73673d3
jtlap <jtlapreste@gmail.com>
2020-12-06 22:30:39 +0100

    derivative headers for math

---
+++ include/eve/function/derivative/sinpic.hpp
@@ -0,0 +1,14 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/sinpic.hpp>
+#include <eve/module/core/function/derivative/generic/sinpic.hpp>
