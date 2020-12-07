a0fcd31ae1ee1f376b138d714e841b7388f819c0
jtlap <jtlapreste@gmail.com>
2020-12-07 11:35:08 +0100

    derivative for math c-files

---
+++ include/eve/module/math/function/derivative/generic/cotd.hpp
@@ -0,0 +1,28 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/cscd.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/function/sqr.hpp>
+#include <eve/function/deginrad.hpp>
+
+namespace eve::detail
+{
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr T cotd_(EVE_SUPPORTS(cpu_)
+                                  , derivative_type<1> const &
+                                  , T const &x) noexcept
+  {
+    return -deginrad(sqr(cscd(x)));
+  }
+}
