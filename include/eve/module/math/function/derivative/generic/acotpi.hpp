acb82e669960edd3246aa1b999d815a992077f9c
jtlap <jtlapreste@gmail.com>
2020-12-07 08:32:36 +0100

    derivative for math a-files

---
+++ include/eve/module/math/function/derivative/generic/acotpi.hpp
@@ -0,0 +1,28 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/derivative/acot.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/function/radinpi.hpp>
+
+
+namespace eve::detail
+{
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr T acotpi_(EVE_SUPPORTS(cpu_)
+                                  , derivative_type<1> const &
+                                  , T const &x) noexcept
+  {
+    return  radinpi(derivative(acotd)(x));
+  }
+}
---
+++ include/eve/module/math/function/derivative/generic/asecpi.hpp
@@ -0,0 +1,27 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/derivative/asec.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/function/radinpi.hpp>
+
+namespace eve::detail
+{
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr T asecpi_(EVE_SUPPORTS(cpu_)
+                                  , derivative_type<1> const &
+                                  , T const &x) noexcept
+  {
+    return radinpi(derivative(asec)(x));
+  }
+}
