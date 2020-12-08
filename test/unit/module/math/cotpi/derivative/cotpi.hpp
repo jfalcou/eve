894ca784f502814ca471a608ee80b94604ae5d17
jtlap <jtlapreste@gmail.com>
2020-12-08 17:42:34 +0100

    der unit test cotxxx

--- test/unit/module/math/cotpi/derivative/cotpi.hpp
+++ test/unit/module/math/cotpi/derivative/cotpi.hpp
@@ -8,22 +8,22 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#include <eve/function/derivative/cosd.hpp>
+#include <eve/function/derivative/cotpi.hpp>
 #include <eve/function/derivative/sin.hpp>
 #include <eve/constant/eps.hpp>
 #include <eve/constant/inf.hpp>
 #include <eve/function/sqrt.hpp>
 #include <type_traits>
 
-TTS_CASE_TPL("Check derivative(cosd) return type", EVE_TYPE)
+TTS_CASE_TPL("Check derivative(cotpi) return type", EVE_TYPE)
 {
   if constexpr(eve::floating_value<T>)
   {
-    TTS_EXPR_IS(eve::derivative(eve::cosd)(T()), T);
+    TTS_EXPR_IS(eve::derivative(eve::cotpi)(T()), T);
   }
 }
 
-TTS_CASE_TPL("Check eve::derivative(eve::cosd) behavior", EVE_TYPE)
+TTS_CASE_TPL("Check eve::derivative(eve::cotpi) behavior", EVE_TYPE)
 {
   if constexpr(eve::floating_value<T>)
   {
@@ -31,8 +31,9 @@ TTS_CASE_TPL("Check eve::derivative(eve::cosd) behavior", EVE_TYPE)
     auto ulp =  (sizeof(elt_t) == 4) ? 1.0e4 : 1.0e8;
     auto e = eve::sqrt(eve::eps(eve::as<T>()));
     auto df = [e](auto f, auto x){return (f(x+e)-f(x-e))/(2*e); };
-    TTS_ULP_EQUAL(eve::derivative(eve::cosd)(T{180.0}), df(eve::cosd, T(180.0))  , 100*ulp);
-    std::cout << eve::derivative(eve::cosd)(T{180}) << std::endl;
-    std::cout << df(eve::cosd, T(180)) << std::endl;
+    TTS_ULP_EQUAL(eve::derivative(eve::cotpi)(T{0.2}), df(eve::cotpi, T(0.2))  , ulp);
+    std::cout << eve::derivative(eve::cotpi)(T{0.2}) << std::endl;
+    std::cout << df(eve::cotpi, T(0.2)) << std::endl;
+    TTS_ULP_EQUAL(eve::derivative(eve::cotpi)(T{0.5}), df(eve::cotpi, T(0.5))  , ulp);
   }
 }
