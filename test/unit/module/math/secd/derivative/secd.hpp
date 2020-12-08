b3b6288dc1f8d26ebb3623a3179097b8a211dfc6
jtlap <jtlapreste@gmail.com>
2020-12-08 20:19:05 +0100

    der unit test secxxx

---
+++ test/unit/module/math/secd/derivative/secd.hpp
@@ -0,0 +1,39 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/function/derivative/secd.hpp>
+#include <eve/function/derivative/sin.hpp>
+#include <eve/constant/eps.hpp>
+#include <eve/constant/inf.hpp>
+#include <eve/function/sqrt.hpp>
+#include <type_traits>
+
+TTS_CASE_TPL("Check derivative(secd) return type", EVE_TYPE)
+{
+  if constexpr(eve::floating_value<T>)
+  {
+    TTS_EXPR_IS(eve::derivative(eve::secd)(T()), T);
+  }
+}
+
+TTS_CASE_TPL("Check eve::derivative(eve::secd) behavior", EVE_TYPE)
+{
+  if constexpr(eve::floating_value<T>)
+  {
+    using elt_t = eve::element_type_t<T>;
+    auto ulp =  (sizeof(elt_t) == 4) ? 1.0e4 : 1.0e8;
+    auto e = eve::sqrt(eve::eps(eve::as<T>()));
+    auto df = [e](auto f, auto x){return (f(x+e)-f(x-e))/(2*e); };
+    TTS_ULP_EQUAL(eve::derivative(eve::secd)(T{135.0}), df(eve::secd, T(135.0))  , 30*ulp);
+    std::cout << eve::derivative(eve::secd)(T{135.0}) << std::endl;
+    std::cout << df(eve::secd, T(135.0)) << std::endl;
+    TTS_ULP_EQUAL(eve::derivative(eve::secd)(T{101}), df(eve::secd, T(101.0))  , 30*ulp);
+  }
+}
