0d135658d253f9d39c9485e559ee1c41e1773fec
jtlap <jtlapreste@gmail.com>
2020-12-09 09:33:39 +0100

    cpp test for der special generation

---
+++ include/eve/module/special/function/derivative/cyl_bessel_j0.hpp
@@ -0,0 +1,26 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/cyl_bessel_j1.hpp>
+#include <eve/function/derivative.hpp>
+
+namespace eve::detail
+{
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE constexpr T cyl_bessel_j0_(EVE_SUPPORTS(cpu_)
+                                  , derivative_type<1> const &
+                                  , T const &x) noexcept
+  {
+    return -cyl_bessel_j1(x);
+  }
+}
