4d1f24cb7de69f1b608c3b3feef46dc4427c5c9b
jtlap <jtlapreste@gmail.com>
2020-12-12 08:35:08 +0100

    clamp frac

---
+++ include/eve/module/core/function/derivative/generic/clamp.hpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/constant/one.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/function/derivative/min.hpp>
+#include <eve/function/derivative/max.hpp>
+
+namespace eve::detail
+{
+  template<floating_value T, floating_value U, floating_value V, auto N>
+  EVE_FORCEINLINE  auto clamp_(EVE_SUPPORTS(cpu_)
+                            , derivative_type<N> const &
+                            , T const &a
+                            , U const &b
+                            , V const &c) noexcept
+  requires compatible_values<T, U>&&compatible_values<T, V>
+  {
+    return arithmetic_call(derivative_type<N>()(clamp), a, b, c);
+  }
+
+  template<floating_value T, auto N>
+  EVE_FORCEINLINE  auto clamp_(EVE_SUPPORTS(cpu_)
+                            , derivative_type<N> const &
+                            , T const &a
+                            , T const &b
+                            , T const &c) noexcept
+  requires(has_native_abi_v<T>)
+  {
+    if constexpr(N == 1) return derivative_1st(min)(max(a, b), c)*derivative_1st(max)(a, b);
+    if constexpr(N == 2) return derivative_1st(min)(max(a, b), c)*derivative_2nd(max)(a, b);
+    if constexpr(N == 3) return derivative_2nd(min)(max(a, b), c);
+  }
+}
