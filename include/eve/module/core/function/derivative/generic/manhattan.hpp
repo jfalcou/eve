16b2739127dff2ec6816a3c7ecc938c88311a4c2
jtlap <jtlapreste@gmail.com>
2020-12-12 07:54:32 +0100

    max min

--- include/eve/module/core/function/derivative/generic/manhattan.hpp
+++ include/eve/module/core/function/derivative/generic/manhattan.hpp
@@ -17,9 +17,66 @@
 #include <eve/function/derivative/sub.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/function/sign.hpp>
+#include <eve/concept/compatible.hpp>
 
 namespace eve::detail
 {
+  template<floating_real_value T, floating_real_value U, auto N>
+  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , U const &y) noexcept
+  requires(compatible_values<T, U>)
+  {
+    return arithmetic_call(derivative_type<N>()(manhattan), x, y);
+  }
+
+  template<floating_real_value T, floating_real_value U, floating_real_value V, auto N>
+  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , U const &y
+                                   , V const &z) noexcept
+  requires(compatible_values<T, U> && compatible_values<T, V>)
+  {
+    return arithmetic_call(derivative_type<N>()(manhattan), x, y, z);
+  }
+
+  template<floating_real_value T, auto N>
+  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , T const &y) noexcept
+  {
+   if constexpr( has_native_abi_v<T> )
+    {
+     auto k = rec(manhattan(x, y));
+     if constexpr(N == 1) return sign(x);
+     if constexpr(N == 2) return sign(y);
+    }
+   else
+     return apply_over(derivative_type<N>()(manhattan), x, y);
+  }
+
+  template<floating_real_value T, auto N>
+  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , T const &y
+                                   , T const &z) noexcept
+  {
+   if constexpr( has_native_abi_v<T> )
+    {
+     auto k = rec(manhattan(x, y, z));
+     if constexpr(N == 1) return sign(x);
+     if constexpr(N == 2) return sign(y);
+     if constexpr(N == 3) return sign(z);
+    }
+   else
+     return apply_over(derivative_type<N>()(manhattan), x, y, z);
+  }
+
+
   template<floating_real_value T, unsigned_value N, unsigned_value P>
   EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
                                    , derivative_type<1> const &
@@ -42,21 +99,4 @@ namespace eve::detail
       return apply_over(derivative_1st(manhattan), x, y, n, p);
   }
 
-  template<floating_real_value T>
-  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
-                                    , derivative_type<1> const &
-                                    , T x, T ) noexcept
-  {
-
-    return sign(x);
-  }
-
-  template<floating_real_value T>
-  EVE_FORCEINLINE constexpr T manhattan_(EVE_SUPPORTS(cpu_)
-                                    , derivative_type<2> const &
-                                    , T , T y) noexcept
-  {
-
-    return sign(y);
-  }
 }
---
+++ include/eve/module/core/function/derivative/generic/max.hpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/function/if_else.hpp>
+#include <eve/function/derivative.hpp>
+#include <eve/concept/compatible.hpp>
+#include <eve/constant/one.hpp>
+
+namespace eve::detail
+{
+  template<floating_real_value T, floating_real_value U, auto N>
+  EVE_FORCEINLINE constexpr T max_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , U const &y) noexcept
+  requires(compatible_values<T, U>)
+  {
+    return arithmetic_call(derivative_type<N>()(max), x, y);
+  }
+
+  template<floating_real_value T, auto N>
+  EVE_FORCEINLINE constexpr T max_(EVE_SUPPORTS(cpu_)
+                                   , derivative_type<N> const &
+                                   , T const &x
+                                   , T const &y) noexcept
+  {
+   if constexpr( has_native_abi_v<T> )
+    {
+      if constexpr(N == 1) return if_else(x > y,  one(as(x)), zero);
+      if constexpr(N == 2) return if_else(x < y,  one(as(x)), zero);
+    }
+   else
+     return apply_over(derivative_type<N>()(max), x, y);
+  }
+}
