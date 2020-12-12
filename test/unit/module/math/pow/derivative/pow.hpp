f882c9e5bf474fdfb606d3768afa72bb81b9264b
jtlap <jtlapreste@gmail.com>
2020-12-12 20:01:35 +0100

    test for pow pow_abs

--- test/unit/module/math/pow/derivative/pow.hpp
+++ test/unit/module/math/pow/derivative/pow.hpp
@@ -16,9 +16,8 @@ TTS_CASE_TPL("Check derivative(pow) return type", EVE_TYPE)
 {
   if constexpr(eve::floating_value<T>)
   {
-    using ui_t = eve::detail::as_integer_t<T, unsigned>;
-    TTS_EXPR_IS(eve::derivative(eve::pow)(T(), T(), unsigned()), T);
-    TTS_EXPR_IS(eve::derivative(eve::pow)(T(), T(), ui_t()), T);
+    TTS_EXPR_IS(eve::derivative(eve::pow)(T(), T()), T);
+    TTS_EXPR_IS(eve::derivative(eve::pow)(T(), T()), T);
   }
 }
 
@@ -27,13 +26,7 @@ TTS_CASE_TPL("Check eve::derivative(eve::pow) behavior", EVE_TYPE)
 
   if constexpr(eve::floating_value<T>)
   {
-    TTS_EQUAL(eve::derivative_1st(eve::pow)(T{2},T{3}, 0u), T(8));
-    TTS_EQUAL(eve::derivative_1st(eve::pow)(T{2},T{3}, 1u), T(12));
-    TTS_EQUAL(eve::derivative_1st(eve::pow)(T{2},T{3}, 2u), T(12));
-    TTS_EQUAL(eve::derivative_2nd(eve::pow)(T{2},T{3}, 1u), T(8*eve::log_2(eve::as<T>())));
-    TTS_EQUAL(eve::derivative_2nd(eve::pow)(T{2},T{3}, 2u), T(8*eve::sqr(eve::log_2(eve::as<T>()))));
-
     TTS_EQUAL(eve::derivative_1st(eve::pow)(T{2},T{3}), T(12));
     TTS_EQUAL(eve::derivative_2nd(eve::pow)(T{2},T{3}), T(8*eve::log_2(eve::as<T>())));
- }
+  }
 }
--- test/unit/module/math/pow_abs/derivative/pow_abs.hpp
+++ test/unit/module/math/pow_abs/derivative/pow_abs.hpp
@@ -16,9 +16,7 @@ TTS_CASE_TPL("Check derivative(pow_abs) return type", EVE_TYPE)
 {
   if constexpr(eve::floating_value<T>)
   {
-    using ui_t = eve::detail::as_integer_t<T, unsigned>;
-    TTS_EXPR_IS(eve::derivative(eve::pow_abs)(T(), T(), unsigned()), T);
-    TTS_EXPR_IS(eve::derivative(eve::pow_abs)(T(), T(), ui_t()), T);
+    TTS_EXPR_IS(eve::derivative(eve::pow_abs)(T(), T()), T);
   }
 }
 
@@ -27,12 +25,6 @@ TTS_CASE_TPL("Check eve::derivative(eve::pow_abs) behavior", EVE_TYPE)
 
   if constexpr(eve::floating_value<T>)
   {
-    TTS_EQUAL(eve::derivative_1st(eve::pow_abs)(T{2},T{3}, 0u), T(8));
-    TTS_EQUAL(eve::derivative_1st(eve::pow_abs)(T{2},T{3}, 1u), T(12));
-    TTS_EQUAL(eve::derivative_1st(eve::pow_abs)(T{2},T{3}, 2u), T(12));
-    TTS_EQUAL(eve::derivative_2nd(eve::pow_abs)(T{2},T{3}, 1u), T(8*eve::log_2(eve::as<T>())));
-    TTS_EQUAL(eve::derivative_2nd(eve::pow_abs)(T{2},T{3}, 2u), T(8*eve::sqr(eve::log_2(eve::as<T>()))));
-
     TTS_EQUAL(eve::derivative_1st(eve::pow_abs)(T{2},T{3}), T(12));
     TTS_EQUAL(eve::derivative_2nd(eve::pow_abs)(T{2},T{3}), T(8*eve::log_2(eve::as<T>())));
  }
