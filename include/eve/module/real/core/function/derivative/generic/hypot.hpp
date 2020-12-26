83a450f5fddac4ae73b4bacb135f4432ca8a4852
jtlap <jtlapreste@gmail.com>
2020-12-26 19:03:06 +0100

    suppress comment

--- include/eve/module/real/core/function/derivative/generic/hypot.hpp
+++ include/eve/module/real/core/function/derivative/generic/hypot.hpp
@@ -17,38 +17,6 @@
 
 namespace eve::detail
 {
-//   template<int N, typename T0, typename T1, typename... Ts>
-//   auto hypot_(EVE_SUPPORTS(cpu_), derivative_type<N>
-//              , T0 arg0, T1 arg1, Ts... args) noexcept
-//   {
-//     using r_t = common_compatible_t<T0,T1, Ts...>;
-//     auto getNth = []<std::size_t... I>(std::index_sequence<I...>, auto& that, auto... vs)
-//       {
-//         auto iff = [&that]<std::size_t J>(auto val, std::integral_constant<std::size_t,J>)
-//         {
-//           if constexpr(J==N) that = val;
-//         };
-
-//         ((iff(vs, std::integral_constant<std::size_t,I+3>{})),...);
-//       };
-//     if constexpr(N > sizeof...(Ts)+2)
-//     {
-//       return zero(as<r_t >());
-//     }
-//     else
-//     {
-//       auto h = hypot(arg0, arg1, args...);
-//       if constexpr(N==1)       return arg0/h;
-//       else if constexpr(N==2)  return  arg1/h;
-//       else
-//       {
-//         r_t that(0);
-//         getNth(std::make_index_sequence<sizeof...(args)>{},that,args...);
-//         return that/h;
-//       }
-//     }
-//   }
-
   template<int N, typename T0, typename T1, typename... Ts>
   auto hypot_(EVE_SUPPORTS(cpu_), derivative_type<N>
              , T0 arg0, T1 arg1, Ts... args) noexcept
