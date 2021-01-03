d9c636e5d6de5627bacdc39a31085c9a97cbefd7
Joel Falcou <joel.falcou@lri.fr>
2021-01-03 11:20:56 +0100

    Hold you horses, element_type
    element_type was trying to do some overly smart
    things and triggered invalid code paths.

--- include/eve/traits/element_type.hpp
+++ include/eve/traits/element_type.hpp
@@ -20,17 +20,13 @@ namespace eve
   //================================================================================================
   template<typename T> struct element_type { using type = T; };
 
-  template<template<class> class B, typename T>
-  struct element_type<B<T>>
-  {
-    using type = B<typename element_type<T>::type>;
-  };
-
   template<typename T, typename N, typename A> struct element_type<wide<T,N,A>> { using type = T; };
   template<typename T, typename N>             struct element_type<wide<T,N>>   { using type = T; };
   template<typename T>                         struct element_type<wide<T>>     { using type = T; };
 
   template<typename T>
+  struct element_type<logical<T>>     { using type = logical<typename element_type<T>::type>; };
+
+  template<typename T>
   using element_type_t = typename element_type<T>::type;
 }
-
