373fccda29cdd95c012ce05f085aa4c14a9f0195
jtlap <jtlapreste@gmail.com>
2021-01-17 21:55:13 +0100

    extending test

--- test/unit/module/real/combinatorial/nth_prime/regular/nth_prime.hpp
+++ test/unit/module/real/combinatorial/nth_prime/regular/nth_prime.hpp
@@ -27,7 +27,7 @@ TTS_CASE_TPL("Check eve::nth_prime behavior", EVE_TYPE)
   if constexpr( sizeof(elt_t) >= 2) TTS_EQUAL(eve::nth_prime(T(54)),  T(257));
   else                              TTS_EQUAL(eve::nth_prime(T(54)),  T(0));
 
-  if constexpr( sizeof(elt_t) == 4)
+  if constexpr( sizeof(elt_t) >= 4)
   {
     using r_t = eve::detail::as_floating_point_t<T>;
     TTS_EQUAL(eve::float32(eve::nth_prime)(T(1000)), r_t(7927));
