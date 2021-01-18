6c1085f27e86598027066e0dd114b0fa56dc8697
jtlap <jtlapreste@gmail.com>
2021-01-18 09:39:10 +0100

    Fix #460 - using uninitialized variable in rem_pio2

--- include/eve/module/real/math/detail/scalar/ieee_754_rem_pio2.hpp
+++ include/eve/module/real/math/detail/scalar/ieee_754_rem_pio2.hpp
@@ -366,7 +366,8 @@ namespace eve::detail
       twon24  =  5.96046447753906250000e-08; /* 0x3E700000, 0x00000000 */
 
     std::int32_t jz,jx,jv,jp,jk,carry,n,iq[20],i,j,k,m,q0,ih;
-    double z,fw,f[20],fq[20],q[20];
+    double z,fw,f[20],fq[20]={},q[20];
+
 
     /* initialize jk*/
     jk = init_jk[prec];
