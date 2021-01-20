15a7bf89f1bff8599b4bb705c22439f5f436e598
jtlap <jtlapreste@gmail.com>
2021-01-20 10:24:02 +0100

    logspace add diff

--- include/eve/module/real/core/function/diff/generic/hypot.hpp
+++ include/eve/module/real/core/function/diff/generic/hypot.hpp
@@ -28,27 +28,23 @@ namespace eve::detail
     }
     else
     {
-      if constexpr(N > sizeof...(Ts)+2) return zero(as<r_t>());
+      auto h = hypot(arg0, arg1, args...);
+      if constexpr(N==1)       return arg0/h;
+      else if constexpr(N==2)  return  arg1/h;
       else
       {
-        auto h = hypot(arg0, arg1, args...);
-        if constexpr(N==1)       return arg0/h;
-        else if constexpr(N==2)  return  arg1/h;
-        else
-        {
-          auto getNth = []<std::size_t... I>(std::index_sequence<I...>, auto& that, auto... vs)
+        auto getNth = []<std::size_t... I>(std::index_sequence<I...>, auto& that, auto... vs)
+          {
+            auto iff = [&that]<std::size_t J>(auto val, std::integral_constant<std::size_t,J>)
             {
-              auto iff = [&that]<std::size_t J>(auto val, std::integral_constant<std::size_t,J>)
-              {
-                if constexpr(J==N) that = val;
-              };
-
-              ((iff(vs, std::integral_constant<std::size_t,I+3>{})),...);
+              if constexpr(J==N) that = val;
             };
-          r_t that(0);
-          getNth(std::make_index_sequence<sizeof...(args)>{},that,args...);
-          return that/h;
-        }
+
+            ((iff(vs, std::integral_constant<std::size_t,I+3>{})),...);
+          };
+        r_t that(0);
+        getNth(std::make_index_sequence<sizeof...(args)>{},that,args...);
+        return that/h;
       }
     }
   }
