04ce9428c53d6c56ed7fb1c2c436363183ee35c0
jtlap <jtlapreste@gmail.com>
2021-01-22 16:05:45 +0100

    spurious file

--- test/unit/module/real/stats/cauchy/attributes/cauchy.hpp
+++ test/unit/module/real/stats/cauchy/attributes/cauchy.hpp
@@ -23,24 +23,22 @@ TTS_CASE_TPL("Check eve::cauchy return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::cauchy behavior", EVE_TYPE)
 {
+ auto inf  = eve::inf(eve::as<T>());
+//   if constexpr( eve::platform::supports_infinites )
+//   {
+//     auto minf = eve::minf(eve::as<T>());
 
-  if constexpr( eve::platform::supports_infinites )
-  {
-    auto inf  = eve::inf(eve::as<T>());
-    auto minf = eve::minf(eve::as<T>());
+//     TTS_ULP_EQUAL(eve::cauchy(inf , T(1.), T(4)) , T(1) , 0.5);
+//     TTS_ULP_EQUAL(eve::cauchy(inf , T(-1.), T(2)), T(1) , 0.5);
+//     TTS_ULP_EQUAL(eve::cauchy(minf, T(1.), T(4)) , T(0) , 0.5);
+//     TTS_ULP_EQUAL(eve::cauchy(minf, T(-1.), T(2)), T(0) , 0.5);
+//    }
 
-    TTS_ULP_EQUAL(eve::cauchy(inf , T(1.), T(4)) , T(1) , 0.5);
-    TTS_ULP_EQUAL(eve::cauchy(inf , T(-1.), T(2)), T(1) , 0.5);
-    TTS_ULP_EQUAL(eve::cauchy(minf, T(1.), T(4)) , T(0) , 0.5);
-    TTS_ULP_EQUAL(eve::cauchy(minf, T(-1.), T(2)), T(0) , 0.5);
-   }
+//   if constexpr( eve::platform::supports_nans )
+//   {
+//     TTS_ULP_EQUAL(eve::cauchy(eve::nan(eve::as<T>())) , eve::nan(eve::as<T>()), 0.5);
+//   }
 
-  if constexpr( eve::platform::supports_nans )
-  {
-    TTS_ULP_EQUAL(eve::cauchy(eve::nan(eve::as<T>())) , eve::nan(eve::as<T>()), 0.5);
-  }
-
-  TTS_ULP_EQUAL(eve::cauchy(T(0))                 , T(0.5)  , 0.5);
-  TTS_ULP_EQUAL(eve::cauchy(T(1), T(1))           , T(0.5)  , 0.5);
-  TTS_ULP_EQUAL(eve::cauchy(T(1), T(1), T(2))     , T(0.5)  , 0.5);
+  TTS_ULP_EQUAL(eve::median(eve::cauchy)(1, 2))                 , T(1)  , 0.5);
+  TTS_ULP_EQUAL(eve::mean  (eve::cauchy)(1, 2))                 , inf   , 0.5);
 }
