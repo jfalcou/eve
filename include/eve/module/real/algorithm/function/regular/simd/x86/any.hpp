df9eb4d6b4bc48bc87875ab64838104aeaa9e2aa
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-01-23 11:18:34 +0000

    FIX-#472: any x86

--- include/eve/module/real/algorithm/function/regular/simd/x86/any.hpp
+++ include/eve/module/real/algorithm/function/regular/simd/x86/any.hpp
@@ -11,9 +11,8 @@
 #pragma once
 
 #include <eve/concept/value.hpp>
-#include <eve/detail/category.hpp>
 #include <eve/detail/concepts.hpp>
-#include <eve/detail/implementation.hpp>
+#include <eve/detail/top_bits.hpp>
 
 namespace eve::detail
 {
@@ -23,18 +22,21 @@ namespace eve::detail
   template<real_scalar_value T, typename N, x86_abi ABI>
   EVE_FORCEINLINE bool any_(EVE_SUPPORTS(sse2_), logical<wide<T, N, ABI>> const &v) noexcept
   {
-    constexpr auto cat = categorize<wide<T, N, ABI>>();
+    if constexpr( N::value == 1) return v[0];
+    else                         return eve::detail::any(eve::detail::top_bits{v});
+  }
 
-    if constexpr(cat == category::int16x8 || cat == category::uint16x8)
-    {
-      // Using bitmap directly on short is pessimistic, we do a custom check here
-      using type  = logical<wide<T, N, ABI>>;
-      using tgt   = typename type::template rebind<std::uint8_t,typename N::combined_type>;
-      return bit_cast(v,as_<tgt>()).bitmap().any();
-    }
-    else
-    {
-      return v.bitmap().any();
-    }
+  template<real_scalar_value T, typename N, x86_abi ABI, relative_conditional_expr C>
+  EVE_FORCEINLINE bool any_(EVE_SUPPORTS(sse2_), C const &cond,
+                            logical<wide<T, N, ABI>> const &v) noexcept
+  {
+    return eve::detail::any(eve::detail::top_bits{v, cond});
+  }
+
+  template<real_scalar_value T, typename N, relative_conditional_expr C>
+  EVE_FORCEINLINE bool any_(EVE_SUPPORTS(sse2_), C const &cond,
+                            logical<wide<T, N, aggregated_>> const &v) noexcept
+  {
+    return eve::detail::any(eve::detail::top_bits{v, cond});
   }
 }
