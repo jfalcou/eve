b305d40d6768b3d8d0bfa8fd36c1087b95b73e93
Joel Falcou <joel.falcou@lri.fr>
2021-01-24 16:51:46 +0100

    PPC induced fixes

--- include/eve/module/real/algorithm/function/regular/generic/any.hpp
+++ include/eve/module/real/algorithm/function/regular/generic/any.hpp
@@ -65,7 +65,7 @@ namespace eve::detail
 
       std::ptrdiff_t first = cond.offset(eve::as_<T>{});
       std::ptrdiff_t last = first + cond.count(eve::as_<T>{});
-      while (first != last) res = res || v[first++];
+      while (first != last) res = res || v.get(first++);
 
       return res;
     }
