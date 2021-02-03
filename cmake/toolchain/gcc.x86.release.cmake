4bfa65f99ab4d1ee8b644f3efe624b992954dd85
Joel Falcou <joel.falcou@lri.fr>
2021-02-03 22:29:30 +0100

    Add new compilers toolchain settings

---
+++ cmake/toolchain/gcc.x86.release.cmake
@@ -0,0 +1,11 @@
+##==================================================================================================
+##  EVE - Expressive Vector Engine
+##  Copyright 2018-2021 Joel FALCOU
+##  Copyright 2018-2021 Jean-Thierry LAPRESTE
+##
+##  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+##  SPDX-License-Identifier: MIT
+##==================================================================================================
+set(CMAKE_C_COMPILER    gcc-10  )
+set(CMAKE_CXX_COMPILER  g++-10  )
+set(CMAKE_BUILD_TYPE    Release )
