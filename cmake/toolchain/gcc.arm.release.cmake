e7a17faa5d2fa9504cf749c5ea6b24cb836f0b40
Joel Falcou <joel.falcou@lri.fr>
2021-02-04 18:41:03 +0100

    Update Contiguous Integration infrastructure
     - Simpler yml
     - Use new docker

---
+++ cmake/toolchain/gcc.arm.release.cmake
@@ -0,0 +1,17 @@
+##==================================================================================================
+##  EVE - Expressive Vector Engine
+##  Copyright 2018-2021 Joel FALCOU
+##  Copyright 2018-2021 Jean-Thierry LAPRESTE
+##
+##  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+##  SPDX-License-Identifier: MIT
+##==================================================================================================
+set(CMAKE_SYSTEM_NAME Linux     )
+set(CMAKE_SYSTEM_PROCESSOR arm  )
+
+set(CMAKE_C_COMPILER    arm-linux-gnueabihf-gcc-10  )
+set(CMAKE_CXX_COMPILER  arm-linux-gnueabihf-g++-10  )
+set(CMAKE_BUILD_TYPE    Release                     )
+set(CMAKE_CXX_FLAGS     "-Wno-psabi"                )
+
+set(CMAKE_CROSSCOMPILING_CMD ../run_arm     )
