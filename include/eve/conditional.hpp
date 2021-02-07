7880917a99a2db0e696e15b7b142056f991dc0f6
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-02-07 16:05:00 +0000

    Revert "Fix #449: first true for x86/emulated/arm v7 (#536)"
    This reverts commit 39f341e5d828fa84e6ce3b7d89a865e0312080e9.

--- include/eve/conditional.hpp
+++ include/eve/conditional.hpp
@@ -604,9 +604,4 @@ namespace eve
   {
     return {a.count_, b.count_};
   }
-
-  constexpr EVE_FORCEINLINE ignore_extrema_ operator&&( ignore_last_ b, ignore_first_ a) noexcept
-  {
-    return a && b;
-  }
 }
