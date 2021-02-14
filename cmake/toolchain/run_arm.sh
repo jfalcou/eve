2455c79825bc72bc7ad8cfa5e42d0eb7b802e8f9
Joel Falcou <joel.falcou@lri.fr>
2021-02-14 21:24:59 +0100

    Helper for ARM v7 runs

---
+++ cmake/toolchain/run_arm.sh
@@ -0,0 +1,11 @@
+##==================================================================================================
+##  EVE - Expressive Vector Engine
+##  Copyright 2018-2021 Joel FALCOU
+##  Copyright 2018-2021 Jean-Thierry LAPRESTE
+##
+##  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+##  SPDX-License-Identifier: MIT
+##==================================================================================================
+#!/bin/sh
+
+qemu-arm -cpu cortex-a15 \$@
