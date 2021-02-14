972aba4453b2240287938de1ff3a2c1323658c26
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-02-14 00:46:24 +0000

    FIX-#538: first_true, arm-v8, <= 8 bytes

--- include/eve/module/real/algorithm/function/regular/simd/arm/neon/first_true.hpp
+++ include/eve/module/real/algorithm/function/regular/simd/arm/neon/first_true.hpp
@@ -11,22 +11,33 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/detail/top_bits.hpp>
 #include <eve/function/any.hpp>
 #include <eve/function/convert.hpp>
-
 
 namespace eve::detail
 {
 
-  template<real_scalar_value T, typename N, relative_conditional_expr C>
+  template<real_scalar_value T, typename N, arm_abi ABI, relative_conditional_expr C>
   EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(
-    EVE_SUPPORTS(neon128_), C const &cond, logical<wide<T, N, arm_128_>> const &v0) noexcept
+    EVE_SUPPORTS(neon128_), C const &cond, logical<wide<T, N, ABI>> const &v0) noexcept
   {
-    // We override the top_bits implementation here
-    // because any will cast to smaller type anyways
+
          if constexpr ( C::is_complete && !C::is_inverted ) return {};
+    else if constexpr ( N() * sizeof(T) <= 4 )              return first_true(top_bits{v0, cond});
+    // for chars better do any first
+    else if constexpr ( sizeof(T) == 1 )                    return first_true_(EVE_RETARGET(cpu_), cond, v0);
+    else if constexpr ( std::same_as<ABI, arm_64_> )
+    {
+      // top bits for anything but chars on asimd are optimal
+      // but without it we better do any first
+      if constexpr ( eve::current_api >= eve::asimd )      return first_true(top_bits{v0, cond});
+      else                                                 return first_true_(EVE_RETARGET(cpu_), cond, v0);
+    }
     else if constexpr ( sizeof(T) >= 2 )
     {
+      // We override the top_bits implementation here
+      // because any will cast to smaller type anyways
       using half_e_t = make_integer_t<sizeof(T) / 2, unsigned>;
       auto halved = eve::convert(v0, eve::as_<eve::logical<half_e_t>>{});
       return eve::first_true[cond](halved);
@@ -35,7 +46,7 @@ namespace eve::detail
     {
       // For chars on arm-v7 I am not sure what's the best approach is
       //  - will stick with default.
-      return detail::first_true_(EVE_RETARGET(cpu_), cond, v0);
+      return first_true_(EVE_RETARGET(cpu_), cond, v0);
     }
   }
 }
