354999e4c2cdc87a84168f3b2733ce9f95694c8c
Joel Falcou <joel.falcou@lri.fr>
2021-02-14 13:09:18 +0100

    Remove useless toolchain

--- cmake/toolchain/clang.x86.cmake
+++ cmake/toolchain/clang.x86.cmake
@@ -9,3 +9,5 @@
 set(CMAKE_C_COMPILER    clang-10   )
 set(CMAKE_CXX_COMPILER  clang++-10 )
 set(CMAKE_BUILD_TYPE    Debug      )
+
+set(CMAKE_CXX_FLAGS     "-DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
--- cmake/toolchain/gcc.aarch64.cmake
+++ cmake/toolchain/gcc.aarch64.cmake
@@ -6,12 +6,13 @@
 ##  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 ##  SPDX-License-Identifier: MIT
 ##==================================================================================================
-set(CMAKE_SYSTEM_NAME Linux         )
-set(CMAKE_SYSTEM_PROCESSOR powerpc  )
+set(CMAKE_SYSTEM_NAME Linux     )
+set(CMAKE_SYSTEM_PROCESSOR arm  )
 
-set(CMAKE_C_COMPILER    powerpc64-linux-gnu-gcc-10  )
-set(CMAKE_CXX_COMPILER  powerpc64-linux-gnu-g++-10  )
-set(CMAKE_BUILD_TYPE    Release                     )
-set(CMAKE_CXX_FLAGS     "-static"                   )
+set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-10  )
+set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-10  )
+set(CMAKE_BUILD_TYPE    Debug                     )
 
-set(CMAKE_CROSSCOMPILING_CMD qemu-ppc64)
+set(CMAKE_CXX_FLAGS     "-DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
+
+set(CMAKE_CROSSCOMPILING_CMD qemu-aarch64)
