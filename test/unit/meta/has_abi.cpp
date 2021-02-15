3a9fc91606a5aa88f87e13fd6a32332c3f21f0d9
Joel Falcou <joel.falcou@lri.fr>
2021-02-15 12:12:46 +0100

    No native double on ARM v7

--- test/unit/meta/has_abi.cpp
+++ test/unit/meta/has_abi.cpp
@@ -15,34 +15,38 @@
 
 TTS_CASE( "Check for detection of native ABI")
 {
-  TTS_EXPECT_NOT(( eve::has_native_abi_v<eve::wide<double, eve::fixed<32>>>              ));
-  TTS_EXPECT_NOT(( eve::has_native_abi_v<eve::logical<eve::wide<double, eve::fixed<32>>>>));
+  constexpr auto native = eve::wide<float>::static_size;
+
+  TTS_EXPECT_NOT(( eve::has_native_abi_v<eve::wide<float, eve::fixed<2*native>>>              ));
+  TTS_EXPECT_NOT(( eve::has_native_abi_v<eve::logical<eve::wide<float, eve::fixed<2*native>>>>));
 
 #if defined(EVE_NO_SIMD)
-  TTS_EXPECT_NOT( eve::has_native_abi_v<eve::wide<double>>                );
-  TTS_EXPECT_NOT( eve::has_native_abi_v<eve::logical<eve::wide<double>>>  );
+  TTS_EXPECT_NOT( eve::has_native_abi_v<eve::wide<float>>                );
+  TTS_EXPECT_NOT( eve::has_native_abi_v<eve::logical<eve::wide<float>>>  );
 #else
-  TTS_EXPECT( eve::has_native_abi_v<eve::wide<double>>                );
-  TTS_EXPECT( eve::has_native_abi_v<eve::logical<eve::wide<double>>>  );
+  TTS_EXPECT( eve::has_native_abi_v<eve::wide<float>>                );
+  TTS_EXPECT( eve::has_native_abi_v<eve::logical<eve::wide<float>>>  );
 #endif
 
-  TTS_EXPECT( eve::has_native_abi_v<double>                           );
-  TTS_EXPECT( eve::has_native_abi_v<eve::logical<double>>             );
+  TTS_EXPECT( eve::has_native_abi_v<float>                           );
+  TTS_EXPECT( eve::has_native_abi_v<eve::logical<float>>             );
 }
 
 TTS_CASE( "Check for detection of aggregated ABI")
 {
+  constexpr auto native = eve::wide<float>::static_size;
+
 #if defined(EVE_NO_SIMD)
-  TTS_EXPECT_NOT(( eve::has_aggregated_abi_v<eve::wide<double, eve::fixed<32>>>              ));
-  TTS_EXPECT_NOT(( eve::has_aggregated_abi_v<eve::logical<eve::wide<double, eve::fixed<32>>>>));
+  TTS_EXPECT_NOT(( eve::has_aggregated_abi_v<eve::wide<float, eve::fixed<2*native>>>              ));
+  TTS_EXPECT_NOT(( eve::has_aggregated_abi_v<eve::logical<eve::wide<float, eve::fixed<2*native>>>>));
 #else
-  TTS_EXPECT(( eve::has_aggregated_abi_v<eve::wide<double, eve::fixed<32>>>              ));
-  TTS_EXPECT(( eve::has_aggregated_abi_v<eve::logical<eve::wide<double, eve::fixed<32>>>>));
+  TTS_EXPECT(( eve::has_aggregated_abi_v<eve::wide<float, eve::fixed<2*native>>>              ));
+  TTS_EXPECT(( eve::has_aggregated_abi_v<eve::logical<eve::wide<float, eve::fixed<2*native>>>>));
 #endif
 
-  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::wide<double>>                );
-  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::logical<eve::wide<double>>>  );
+  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::wide<float>>                );
+  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::logical<eve::wide<float>>>  );
 
-  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<double>                           );
-  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::logical<double>>             );
+  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<float>                           );
+  TTS_EXPECT_NOT( eve::has_aggregated_abi_v<eve::logical<float>>             );
 }
