df9c8f41d66f0f6e81cb9fd2eff9c5d1bff73862
jtlap <jtlapreste@gmail.com>
2021-02-18 09:32:42 +0100

    rising

--- include/eve/module/real/combinatorial/function/regular/generic/lrising_factorial.hpp
+++ include/eve/module/real/combinatorial/function/regular/generic/lrising_factorial.hpp
@@ -14,6 +14,7 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/detail/skeleton_calls.hpp>
 #include <eve/function/tgamma.hpp>
+#include <eve/function/floor.hpp>
 #include <eve/function/is_eqz.hpp>
 #include <eve/function/is_gtz.hpp>
 #include <eve/function/is_lez.hpp>
@@ -23,6 +24,7 @@
 #include <eve/function/lgamma.hpp>
 #include <eve/function/log.hpp>
 #include <eve/function/log1p.hpp>
+#include <eve/function/oneminus.hpp>
 #include <eve/function/pow.hpp>
 #include <eve/function/sinpi.hpp>
 #include <eve/constant/half.hpp>
@@ -63,7 +65,8 @@ namespace eve::detail
   EVE_FORCEINLINE auto lrising_factorial_(EVE_SUPPORTS(cpu_)
                                          , T n,  T x, std::false_type const & ) noexcept
   {
-    return n+x;
+    std::cout << "TODO" << std::endl;
+    return nan(as(n+x));
   }
 
   template<floating_real_value T>
@@ -81,6 +84,7 @@ namespace eve::detail
 
     auto lp1 = [](auto n,  auto x)
       {
+        std::cout << "lp1 "<< " x " << x << " n " << n << std::endl;
         const auto xovern = x/n;
         const auto den = inc(xovern);
         const auto d3 = den*den*den;
@@ -109,17 +113,18 @@ namespace eve::detail
 
     auto lp2 = [](auto n,  auto x)
       {
+        std::cout << "lp2 "<< " x " << x << " n " << n << std::endl;
         return lrising_factorial(n, x, std::false_type());
       };
 
     if( eve::any(notdone) )
     {
       auto test0 = (10*absx > n) || (10*absx*log(eve::max(n, T(2))) > one(as(x)));
+      std::cout << "---x " << x << std::endl;
       notdone = next_interval(lp0,  notdone, test0, r, x, n);
-      std::cout << "r " << r << std::endl;
       if( eve::any(notdone) )
       {
-        auto test1 =  (10*absx <= n) || (n > T(15));
+        auto test1 =  (10*absx <= n) && (n > T(15));
         notdone = next_interval(lp1,  notdone, test1, r, x, n);
         if( eve::any(notdone) )
         {
@@ -135,11 +140,13 @@ namespace eve::detail
   EVE_FORCEINLINE auto lrising_factorial_(EVE_SUPPORTS(cpu_)
                                         , T x, T n) noexcept
   {
+    std::cout << "entre x " << x <<  "  n " << n << std::endl;
     if constexpr(has_native_abi_v<T>)
     {
-      auto xnez =  is_nez(x);
-      auto r = if_else(xnez, nan(as(x)), zero);
-      auto notdone = is_not_nan(x) || xnez;
+      auto nnez =  is_nez(n);
+      //   auto xnez =  is_nez(x);
+      auto r = if_else(nnez, nan(as(x)), minf(as(x)));
+      auto notdone = is_not_nan(x) || nnez;
 
       auto lpos = [](auto n,  auto x)
         {
@@ -153,17 +160,19 @@ namespace eve::detail
           /* Use the reflection formula AMS6.1.17
              poch(-a,-x) = (-1)^x (a/(a+x)) 1/poch(a,x) */
           auto r = lrising_factorial(-n, -x, std::true_type());
-          auto f = log (n / (n + x));
+          auto f = -eve::log1p(x/n);
           return f - r;
         };
       auto lr3 = [](auto n,  auto)
         {
-          /* Handle a+x = 0 i.e. Gamma(0)/Gamma(a) */
+           std::cout << "lr3" << std::endl;
+         /* Handle a+x = 0 i.e. Gamma(0)/Gamma(a) */
           /* poch (-a,a) == (-1)^a Gamma(a+1) */
-          return tgamma(oneminus(n));
+           return lgamma(inc(n));
         };
       auto lr4 = [](auto n,  auto )
         {
+          std::cout << "lr4" << std::endl;
           //a < 0.0 && a+x < 0.0
           /* Handle finite numerator, Gamma(a+x) for a+x != 0 or neg int */
           return minf(as(n));
@@ -171,6 +180,7 @@ namespace eve::detail
 
       auto lr5 = [](auto n ,  auto x)
         {
+          std::cout << "lr5" << std::endl;
           // Reduce to positive case using reflection.
           auto omn = oneminus(n);
           auto sin_1 = sinpi(omn);
@@ -182,9 +192,18 @@ namespace eve::detail
           //      *sgn = GSL_SIGN(sin_1*sin_2);
         };
 
-      auto lr6 = [](auto n ,  auto x)
+      auto lr6 = [](auto n,  auto x)
         {
-          return lgamma(n+x)-lgamma(n);
+          std::cout << "lr6" << std::endl;
+          std::cout << "x " << x <<  "  n " << n << std::endl;
+          std::cout << "res " << eve::lgamma(n+x)-eve::lgamma(n)<< std::endl;
+          //          return eve::lgamma(n+x)-eve::lgamma(n);
+          std::cout << "eve::lgamma(n+x) " << eve::lgamma(n+x)<< std::endl;
+          std::cout << "eve::lgamma(n)   " << eve::lgamma(n  )<< std::endl;
+          std::cout << "log(eve::abs(tgamma(n+x)) " << log(eve::abs(tgamma(n+x))) << std::endl;
+          std::cout << "log(eve::abs(tgamma(n)) " << log(eve::abs(tgamma(n))) << std::endl;
+          return eve::log(eve::abs(tgamma(n+x))-eve::log(eve::abs(tgamma(n))));
+          //   return eve::lgamma(n+x)-eve::lgamma(n);
         };
 
       if( eve::any(notdone) )
@@ -193,7 +212,7 @@ namespace eve::detail
         notdone = next_interval(lpos,  notdone, ispos, r, x, n);
         if( eve::any(notdone) )
         {
-          auto testz = is_lez(n) && is_flint(n);
+          auto testz = is_lez(n) && (eve::floor(n) == n);
           auto xltz = is_ltz(x);
           notdone = next_interval(lr2, notdone, testz && xltz, r, x, n);
           if( eve::any(notdone) )
@@ -209,13 +228,13 @@ namespace eve::detail
               }
               if( eve::any(notdone) )
               {
-                notdone = last_interval(lr6, notdone, r, x, n);//(n < 0.0 && n+x < 0.0)
+                notdone = last_interval(lr6, notdone, r, x, n);
               }
             }
           }
         }
       }
-      return r;
+      return if_else(is_nez(n), r, minf(as(x)));
     }
     else
       return apply_over(lrising_factorial, n, x);
--- test/unit/module/real/combinatorial/lrising_factorial/regular/lrising_factorial.hpp
+++ test/unit/module/real/combinatorial/lrising_factorial/regular/lrising_factorial.hpp
@@ -12,6 +12,13 @@
 #include <boost/math/special_functions/factorials.hpp>
 #include <eve/constant/valmin.hpp>
 #include <type_traits>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_result.h>
+
+extern "C" {
+  double gsl_sf_lnpoch(double, double);
+  int gsl_sf_lnpoch_sgn_e(double, double, gsl_sf_result*, double*);
+};
 
 TTS_CASE_TPL("Check eve::lrising_factorial return type", EVE_TYPE)
 {
@@ -20,8 +27,23 @@ TTS_CASE_TPL("Check eve::lrising_factorial return type", EVE_TYPE)
 
 TTS_CASE_TPL("Check eve::lrising_factorial behavior", EVE_TYPE)
 {
-  TTS_ULP_EQUAL(eve::lrising_factorial(10, T(0.1)) , T(boost::math::lrising_factorial(0.1, 10)), 20);
-  TTS_ULP_EQUAL(eve::lrising_factorial(5,  T(0.1)) , T(boost::math::lrising_factorial(0.1, 5)) , 20);
-  TTS_ULP_EQUAL(eve::lrising_factorial(0,  T(0.1)) , T(boost::math::lrising_factorial(0.1, 0)) , 20);
-  TTS_ULP_EQUAL(eve::lrising_factorial(1,  T(0.1)) , T(boost::math::lrising_factorial(0.1, 1)) , 20);
+  auto gsl_lnr = [](auto n, auto x){
+   gsl_sf_result res;
+   double sgn;
+   gsl_sf_lnpoch_sgn_e(x, n, &res, &sgn);
+   return res.val;
+  };
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T( 20),   T(2.0)) , T(gsl_lnr( 20.,  2.)), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(10, T(0.1)) , T(gsl_sf_lnpoch(0.1, 10.0)), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(5,  T(0.1)) , T(gsl_sf_lnpoch(0.1, 5.0)) , 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(0,  T(0.1)) , T(gsl_sf_lnpoch(0.1, 0.0)) , 20);
+//   //  TTS_ULP_EQUAL(eve::lrising_factorial(1,  T(0.1)) , T(gsl_lnr(1.0, 0.1)) , 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T(20.3), T(10.2)) , T(gsl_lnr(20.3, 10.2)), 20);
+//   //  TTS_ULP_EQUAL(eve::lrising_factorial(T(-20),  T(-2.0)) , T(gsl_lnr(-20., -2.)), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T(20),  T(-20))   , T(gsl_lnr(20., -20.)), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T(-20),  T(20))   , eve::nan(eve::as<T>()), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T(0), T(10))      , T(gsl_lnr(0., 10.)), 20);
+//   TTS_ULP_EQUAL(eve::lrising_factorial(T(10), T(0))      , T(gsl_lnr(10., 0.)), 20);
+  TTS_ULP_EQUAL(eve::lrising_factorial(T(-10.3), T(-5.2)), T(gsl_lnr(-10.3, -5.2)), 20);
+  //lgamma doit etre verolé en négatif
 }
