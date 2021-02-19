c50bb6864c0b67ac09bc1a148da2ece67513eb68
jtlap <jtlapreste@gmail.com>
2021-02-19 20:46:32 +0100

    dispatching pedantic and tests

---
+++ include/eve/module/real/combinatorial/function/pedantic/generic/lrising_factorial.hpp
@@ -0,0 +1,145 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright 2020 Joel FALCOU
+  Copyright 2020 Jean-Thierry LAPRESTE
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/detail/skeleton_calls.hpp>
+#include <eve/function/digamma.hpp>
+#include <eve/function/tgamma.hpp>
+#include <eve/function/dist.hpp>
+#include <eve/function/floor.hpp>
+#include <eve/function/fma.hpp>
+#include <eve/function/is_eqz.hpp>
+#include <eve/function/is_gtz.hpp>
+#include <eve/function/is_lez.hpp>
+#include <eve/function/is_ltz.hpp>
+#include <eve/function/is_nlez.hpp>
+#include <eve/function/is_nltz.hpp>
+#include <eve/function/is_nan.hpp>
+#include <eve/function/is_not_nan.hpp>
+#include <eve/function/lgamma.hpp>
+#include <eve/function/log.hpp>
+#include <eve/function/log1p.hpp>
+#include <eve/function/oneminus.hpp>
+#include <eve/function/pow.hpp>
+#include <eve/function/raw.hpp>
+#include <eve/function/regular.hpp>
+#include <eve/function/sinpi.hpp>
+#include <eve/function/sqr.hpp>
+#include <eve/constant/half.hpp>
+#include <eve/constant/one.hpp>
+#include <eve/constant/minf.hpp>
+#include <eve/constant/nan.hpp>
+#include <eve/function/convert.hpp>
+#include <eve/function/exp.hpp>
+#include <eve/detail/apply_over.hpp>
+#include <eve/traits/common_compatible.hpp>
+#include <eve/detail/hz_device.hpp>
+
+namespace eve::detail
+{
+  // pedantic computes also for negative values and even negative integer values
+  template<floating_real_value T>
+  EVE_FORCEINLINE auto lrising_factorial_(EVE_SUPPORTS(cpu_)
+                                         , pedantic_type const &
+                                         , T a,  T x) noexcept
+  {
+   if constexpr(has_native_abi_v<T>)
+    {
+      auto r = nan(as(a));
+      auto notdone = is_not_nan(a)&&is_not_nan(x);
+
+      auto lr0 = []()
+        {
+          return zero(as(T()));
+        };
+
+      auto lrpos = [](auto a, auto x)
+        {
+          return lrising_factorial(a, x, std::true_type());
+        };
+
+      auto lrnegint = [](auto a,  auto x)
+        // a and a+x are negative integers uses reflection.
+        {
+          auto r = lrising_factorial(-a, -x, std::true_type());
+          return -eve::log1p(x/a)-r;
+        };
+
+      auto lraeqmx = [](auto a,  auto )
+        // a+x = 0
+        {
+         return lgamma(inc(a));
+        };
+
+      auto lraneqmx = [](auto a,  auto )
+        //a < 0.0 && a+x < 0.0
+       {
+          return minf(as(a));
+       };
+
+      auto lrneg = [](auto a,  auto x)
+        {
+          // Reduce to positive case using reflection.
+          auto oma = oneminus(a);
+          auto spioma = sinpi(oma);
+          auto spiomamx = sinpi(oma-x);
+          auto r = lrising_factorial(oma, -x, std::true_type());
+          auto lnterm = eve::log(eve::abs(spioma/spiomamx));
+          return if_else(is_nez(spiomamx*spioma), lnterm - r, allbits);
+          //      *sgn = GSL_SIGN(spioma*spiomamx);
+        };
+
+      auto lrlast = [](auto a,  auto x)
+        {
+         return eve::lgamma(a+x)-eve::lgamma(a);
+        };
+
+      if( eve::any(notdone) )
+      {
+        notdone = next_interval(lr0,  notdone, is_eqz(x), r);
+        if( eve::any(notdone) )
+        {
+          auto testpos = is_nlez(a) && is_nlez(a+x);
+          notdone = next_interval(lrpos, notdone,  testpos, r, a, x);
+          if( eve::any(notdone) )
+          {
+            // from here a+x <= 0 ||  x <= 0
+            auto aneg = is_ltz(a);
+            auto aflint = is_flint(a);
+            auto testnegint = aflint && is_flint(a+x) && aneg && is_ltz(a+x) ;
+            notdone = next_interval(lrnegint, notdone, testnegint, r, a, x);
+            if( eve::any(notdone) )
+            {
+              notdone = next_interval(lraeqmx, notdone, is_eqz(a+x), r, a, x);
+              if( eve::any(notdone) )
+              {
+                notdone = next_interval(lraneqmx, notdone, aflint && !is_eqz(a+x), r, a, x);
+                if( eve::any(notdone) )
+                {
+                  notdone = next_interval(lrneg, notdone, aneg && is_ltz(a+x), r, a, x);
+                  if( eve::any(notdone) )
+                  {
+                    notdone = last_interval(lrlast, notdone, r, a, x);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      return r;
+    }
+    else
+      return apply_over(pedantic(lrising_factorial), a, x);
+  }
+}
--- test/unit/module/real/combinatorial/lrising_factorial/pedantic/lrising_factorial.hpp
+++ test/unit/module/real/combinatorial/lrising_factorial/pedantic/lrising_factorial.hpp
@@ -8,7 +8,7 @@
   SPDX-License-Identifier: MIT
 **/
 //==================================================================================================
-#include <eve/function/lrising_factorial.hpp>
+#include <eve/function/pedantic/lrising_factorial.hpp>
 #include <boost/math/special_functions/factorials.hpp>
 #include <eve/constant/eps.hpp>
 
@@ -21,10 +21,10 @@ extern "C" {
   int gsl_sf_lnpoch_sgn_e(double, double, gsl_sf_result*, double*);
 };
 
-// TTS_CASE_TPL("Check eve::lrising_factorial return type", EVE_TYPE)
-// {
-//   TTS_EXPR_IS(eve::raw(eve::lrising_factorial)(int(), T()), T);
-// }
+TTS_CASE_TPL("Check eve::lrising_factorial return type", EVE_TYPE)
+{
+  TTS_EXPR_IS(eve::pedantic(eve::lrising_factorial)(int(), T()), T);
+}
 
 TTS_CASE_TPL("Check eve::lrising_factorial behavior", EVE_TYPE)
 {
@@ -36,19 +36,17 @@ TTS_CASE_TPL("Check eve::lrising_factorial behavior", EVE_TYPE)
    return res.val;
   };
   auto ulp = sizeof(eve::element_type_t<T>) == 4 ? 70 : 20;
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T( 20),   T(2.0)) , T(gsl_lnr( 20.,  2.)), ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(10, T(0.1)) , T(gsl_lnr(10.0, 0.1)), ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(5,  T(0.1)) , T(gsl_lnr(5.0, 0.1)) , ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(0,  T(0.1)) , T(gsl_lnr(0.0, 0.1)) , ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(0,  T(0.1)) ,  T(gsl_lnr(0.0, 0.1)) , ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(2,  T(0.1)) , T(gsl_lnr(2.0, 0.1)) , ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(20.3), T(10.2)) , T(gsl_lnr(20.3, 10.2)), ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(-2.0)) , T(gsl_lnr(-20., -2.0)), ulp);
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(20),  T(-20)) , T(eve::lgamma(T(21))), ulp);
-
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(20)) , eve::inf(eve::as<T>()), ulp);
-
-  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(10.3)), eve::minf(eve::as<T>()), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T( 20),   T(2.0))   , T(gsl_lnr( 20.,  2.)), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(10, T(0.1))         , T(gsl_lnr(10.0, 0.1)), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(5,  T(0.1))         , T(gsl_lnr(5.0, 0.1)) , ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(0,  T(0.1))         , T(gsl_lnr(0.0, 0.1)) , ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(0,  T(0.1))         ,  T(gsl_lnr(0.0, 0.1)) , ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(2,  T(0.1))         , T(gsl_lnr(2.0, 0.1)) , ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(20.3), T(10.2))   , T(gsl_lnr(20.3, 10.2)), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(-2.0))   , T(gsl_lnr(-20., -2.0)), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(20),  T(-20))     , T(eve::lgamma(T(21))), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(20))     , eve::inf(eve::as<T>()), ulp);
+  TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20),  T(10.3))   , eve::minf(eve::as<T>()), ulp);
   TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20.2),  T(-10.3)), T(gsl_lnr(-20.2, -10.3)), ulp);
   TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20.2),  T(-10.3)), T(gsl_lnr(-20.2, -10.3)), ulp);
   TTS_ULP_EQUAL(eve::pedantic(eve::lrising_factorial)(T(-20.2),  T( 10.3)), T(gsl_lnr(-20.2, 10.3)), ulp);
