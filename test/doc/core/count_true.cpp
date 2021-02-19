02bd4db14fa762782dc26c2f9b9374dde7b60b93
Joel Falcou <joel.falcou@lri.fr>
2021-02-19 09:53:56 +0100

    Updated doc examples with ignore cases

--- test/doc/core/count_true.cpp
+++ test/doc/core/count_true.cpp
@@ -3,27 +3,21 @@
 #include <iostream>
 
 using wide_ft = eve::logical<eve::wide<float, eve::fixed<4>>>;
-using wide_it = eve::logical<eve::wide<std::int16_t, eve::fixed<4>>>;
 
 int main()
 {
+  eve::logical<float>  xf = true;
+
+  std::cout << "---- scalar" << '\n'
+            << "<- xf         = " << xf << '\n'
+            << "-> count_true(xf) = " << eve::count_true(xf) << '\n';
+
   wide_ft pf = {true,false,true,false};
-  wide_it qf = {false,true,true,true};
 
   std::cout << "---- simd" << '\n'
             << "<- pf         = " << pf << '\n'
             << "-> count_true(pf) = " << eve::count_true(pf) << '\n'
-            << "<- qf         = " << qf << '\n'
-            << "-> count_true(qf) = " << eve::count_true(qf) << '\n';
-
-  eve::logical<float>  xf = true;
-  eve::logical<int>    yf = false;
-
-  std::cout << "---- scalar" << '\n'
-            << "<- xf         = " << xf << '\n'
-            << "-> count_true(xf) = " << eve::count_true(xf) << '\n'
-            << "<- yf         = " << yf << '\n'
-            << "-> count_true(yf) = " << eve::count_true(yf) << '\n';
+            << "-> count_true[ignore_first(2)](pf) = " << eve::count_true[eve::ignore_first(2)](pf) << '\n';
 
   return 0;
 }
