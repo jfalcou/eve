be62517c537dd97b388aee9637a7cb12367bad53
Joel Falcou <joel.falcou@lri.fr>
2021-02-23 18:31:07 +0100

    Use na_ everywhere

--- include/eve/detail/function/simd/common/patterns.hpp
+++ include/eve/detail/function/simd/common/patterns.hpp
@@ -12,13 +12,14 @@
 
 #include <eve/traits/cardinal.hpp>
 #include <eve/traits/as_wide.hpp>
+#include <eve/pattern.hpp>
 #include <ostream>
 
 namespace eve::detail
 {
   //================================================================================================
   // Detects <-1,...,-1> as zeroing
-  template<int... I> inline constexpr bool is_zero = ((I==-1) && ...);
+  template<int... I> inline constexpr bool is_zero = ((I == na_) && ...);
 
   //================================================================================================
   // Detects <0,1,...,N-1> as identity
@@ -64,7 +65,7 @@ namespace eve::detail
     }
 
     template<typename Wide, typename Cardinal>
-    EVE_FORCEINLINE auto operator()(Wide v, Cardinal) const
+    EVE_FORCEINLINE auto operator()(Wide, Cardinal) const
     {
       return as_wide_t<Wide,Cardinal>{0};
     }
