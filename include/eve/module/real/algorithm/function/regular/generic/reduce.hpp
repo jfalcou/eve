b6c346a9cd05b6aced5fb8f669dd23dad7e3a6b9
Joel Falcou <joel.falcou@lri.fr>
2021-02-28 17:28:11 +0100

    Basic reduce implementation

---
+++ include/eve/module/real/algorithm/function/regular/generic/reduce.hpp
@@ -0,0 +1,59 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/vectorized.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/detail/is_native.hpp>
+#include <eve/function/swap_adjacent_group.hpp>
+#include <eve/function/splat.hpp>
+#include <eve/pattern.hpp>
+#include <bit>
+
+namespace eve::detail
+{
+  //================================================================================================
+  // reduce toward wide
+  template<real_scalar_value T, typename N, typename ABI, typename Callable>
+  EVE_FORCEINLINE auto reduce_( EVE_SUPPORTS(cpu_), splat_type const&
+                              , wide<T,N,ABI> v, Callable f
+                              ) noexcept
+  {
+    if constexpr( !is_aggregated_v<wide<T,N,ABI>> )
+    {
+      //============================================================================================
+      // We have exactly log2(cardinal) - 1 butterfly steps to perform
+      constexpr auto depth = std::bit_width(std::size_t{N::value}) - 1;
+
+      //============================================================================================
+      // Iterate over all levels
+      return [&]<std::size_t... I>(std::index_sequence<I...>) mutable
+      {
+        ((v = f(v,swap_adjacent_group(v, fixed<(1<<I)>{} ))),...);
+        return v;
+      }(std::make_index_sequence<depth>{});
+    }
+    else
+    {
+      //===========================================================================================
+      // Slice, do f once, then reduce the result
+      auto[l,h] = f.slice();
+      return reduce( f(l,h) );
+    }
+  }
+
+  //================================================================================================
+  // reduce toward scalar
+  template<real_scalar_value T, typename N, typename ABI, typename Callable>
+  EVE_FORCEINLINE
+  auto reduce_(EVE_SUPPORTS(cpu_), wide<T,N,ABI> v, Callable f) noexcept
+  {
+    auto that = splat(reduce)(v,f);
+    return that.get(0);
+  }
+}
---
+++ test/unit/module/real/algorithm/reduce/regular/reduce.hpp
@@ -0,0 +1,34 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include <eve/function/reduce.hpp>
+#include <eve/function/plus.hpp>
+#include <type_traits>
+
+TTS_CASE_TPL("Check eve::reduce return type", EVE_TYPE)
+{
+  TTS_EXPR_IS( (eve::reduce(T{}, eve::plus)            ) , EVE_VALUE );
+  TTS_EXPR_IS( (eve::splat(eve::reduce)(T{}, eve::plus)) , T         );
+}
+
+TTS_CASE_TPL("Check eve::reduce behavior on wide", EVE_TYPE)
+{
+  T data = [](auto i, auto ) { return 1+i; };
+
+  auto ref = (EVE_CARDINAL/2)*(EVE_CARDINAL+1);
+
+  TTS_EQUAL( (eve::reduce(data, [](auto a, auto b) { return a+b; } )), EVE_VALUE(ref) );
+}
+
+TTS_CASE_TPL("Check eve::reduce behavior on wide", EVE_TYPE)
+{
+  T data = [](auto i, auto ) { return 1+i; };
+
+  T ref( (EVE_CARDINAL/2)*(EVE_CARDINAL+1) );
+
+  TTS_EQUAL( (eve::splat(eve::reduce)(data, [](auto a, auto b) { return a+b; } )), ref);
+}
