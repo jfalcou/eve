0c9bc267dd8dcdd53d2cd8ef3f070647c0de1fc1
jtlap <jtlapreste@gmail.com>
2021-03-01 10:20:44 +0100

    html docs

--- test/doc/core/lo.cpp
+++ test/doc/core/lo.cpp
@@ -12,32 +12,24 @@ using wide_ui8t= eve::wide<std::uint8_t , eve::fixed<8>>;
 int main()
 {
   wide_uit i32 = {0, 1, 2, 3, 65536+1, 65536+8, 65536+16, 65536+32};
-  wide_ui8t i8  = {0, 1, 2, 3, 16+0, 16+2, 16+4, 16+8};
+  wide_ui8t i8 = {0, 1, 2, 3, 16+0, 16+2, 16+4, 16+8};
 
-  auto [m, e]    = eve::lo(i32);
-  auto [m8, e8]  = eve::lo(i8);
+  auto m   = eve::lo(i32);
+  auto m8  = eve::lo(i8);
   std::cout << "---- simd (hexadecimal output)" << '\n'
             << std::hex
             << "<- i32             = " << i32 << '\n'
-            << "-> lo(i32)       = [" << '\n'
-            << "                     " << m << ", \n"
-            << "                     " << e << '\n'
-            << "                     ]\n"
-            << std::hex << "<- i8  = " << i8 << '\n'
-            << "-> lo(i8 )       = [" << '\n'
-            << "                     " << m8 << ", \n"
-            << "                     " << e8 << '\n'
-            << "                     ]\n";
+            << "-> lo(i32)         = " << m   << '\n'
+            << std::hex << "<- i8  = " << i8  << '\n'
+            << "-> lo(i8 )         = " << m8  << '\n';
 
   std::int32_t x = 100000;
   std::int8_t  y = 100;
-  auto [sm0, se0] =  eve::lo(x);
-  auto [sm1, se1] =  eve::lo(y);
   std::cout << "---- scalar (hexadecimal output)" << '\n'
-            <<  std::hex << "<- x        = " << x << '\n'
-            << "-> lo(x)  = [" << sm0 << ", " << se0 << "]\n"
-            << "<- y        = " << y << '\n'
-            << "-> lo(y)  = [" << int(sm1) << ", " << int(se1) << "]\n"
+            <<  std::hex << "<- x      = " << x   << '\n'
+            << "-> lo(x)  = " << eve::lo(x) << '\n'
+            << "<- y      = " << y   << '\n'
+            << "-> lo(y)  = " << int(eve::lo(y)) << '\n'
 
     ;
   return 0;
