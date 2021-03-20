3c42b29bf2b7dd4ea0ca11f1b846cfd6f941ffe2
jtlap <jtlapreste@gmail.com>
2021-03-20 17:53:07 +0100

    add

--- test/unit/module/real/core/add.cpp
+++ test/unit/module/real/core/add.cpp
@@ -6,7 +6,7 @@
 //==================================================================================================
 #include "test.hpp"
 #include <eve/constant/valmax.hpp>
-#include <eve/constant/valmin.hpp>    
+#include <eve/constant/valmin.hpp>
 #include <eve/function/add.hpp>
 #include <eve/function/saturated/add.hpp>
 #include <eve/function/is_gtz.hpp>
@@ -14,7 +14,11 @@
 //==================================================================================================
 // Types tests
 //==================================================================================================
-auto types_tests = []<typename T>(auto& runtime, bool verbose, auto const&, T)
+EVE_TEST( "Check return types of add"
+          , eve::test::simd::all_types
+          , eve::test::generate(eve::test::no_data)
+          )
+<typename T>(T)
 {
   using v_t = eve::element_type_t<T>;
 
@@ -63,18 +67,17 @@ auto types_tests = []<typename T>(auto& runtime, bool verbose, auto const&, T)
   TTS_EXPR_IS( eve::saturated(eve::add)(v_t(), v_t(), v_t()) , v_t);
 };
 
-EVE_TEST_BED( "Check return types of add"
-            , eve::test::simd::all_types
-            , eve::test::generate(eve::test::no_data)
-            , types_tests
-            );
-
 //==================================================================================================
 // add tests
 //==================================================================================================
-auto simd_tests = []<typename T>( auto& runtime, bool verbose, auto const&
-                                , T const& a0, T const& a1, T const& a2
-                                )
+EVE_TEST( "Check behavior of add on signed types"
+            , eve::test::simd::all_types
+            , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
+                                  , eve::test::randoms(eve::valmin, eve::valmax)
+                                  , eve::test::randoms(eve::valmin, eve::valmax)
+                                  )
+            )
+<typename T>(T const& a0, T const& a1, T const& a2)
 {
   using eve::add;
   using eve::saturated;
@@ -85,30 +88,19 @@ auto simd_tests = []<typename T>( auto& runtime, bool verbose, auto const&
 
 };
 
-EVE_TEST_BED( "Check behavior of add on signed types"
-            , eve::test::simd::signed_types
-            , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
-                                  , eve::test::randoms(eve::valmin, eve::valmax)
-                                  , eve::test::randoms(eve::valmin, eve::valmax)
-                                  )
-            , simd_tests
-            );
-
-EVE_TEST_BED( "Check behavior of add on unsigned types"
-            , eve::test::simd::unsigned_types
-            , eve::test::generate ( eve::test::randoms(0, eve::valmax)
-                                  , eve::test::randoms(0, eve::valmax)
-                                  , eve::test::randoms(0, eve::valmax)
-                                  )
-            , simd_tests
-            );
-
 //==================================================================================================
-// conditional add tests
+//==  conditional add tests
 //==================================================================================================
-auto simd_tests_if = []<typename T>( auto& runtime, bool verbose, auto const&
-                                , T const& a0, T const& a1, T const& a2
-                                )
+auto mini = [] < typename T > (eve::as_<T> const &){ return std::is_signed_v<eve::element_type_t<T>> ? -128 : 0;};
+
+EVE_TEST( "Check behavior of add on signed types"
+        , eve::test::simd::signed_types
+        , eve::test::generate ( eve::test::randoms(mini, 127)
+                              , eve::test::randoms(mini, 127)
+                              , eve::test::randoms(mini, 127)
+                              )
+        )
+<typename T>( T const& a0, T const& a1, T const& a2)
 {
   using eve::add;
   using eve::saturated;
@@ -116,21 +108,3 @@ auto simd_tests_if = []<typename T>( auto& runtime, bool verbose, auto const&
   TTS_EQUAL( add[a2 > T(64)](a0, a1), T([&](auto i, auto) {return a2.get(i) > 64 ? add(a0.get(i), a1.get(i)) :a0.get(i) ; }));
   TTS_EQUAL( saturated(add[a2 > T(64)])(a0, a1), T([&](auto i, auto) { return  a2.get(i) > 64 ? saturated(add)(a0.get(i), a1.get(i)):a0.get(i); }));
 };
-
-EVE_TEST_BED( "Check behavior of add on signed types"
-            , eve::test::simd::signed_types
-            , eve::test::generate ( eve::test::randoms(-128, 127)
-                                  , eve::test::randoms(-128, 127)
-                                  , eve::test::randoms(-128, 127)
-                                  )
-            , simd_tests_if
-            );
-
-EVE_TEST_BED( "Check behavior of add on unsigned types"
-            , eve::test::simd::unsigned_types
-            , eve::test::generate ( eve::test::randoms(0, 127)
-                                  , eve::test::randoms(0, 127)
-                                  , eve::test::randoms(0, 127)
-                                  )
-            , simd_tests
-            );
