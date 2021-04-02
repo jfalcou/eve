b01a17a2cbb98608e7a2af05cde4aa2b0ca60095
jtlap <jtlapreste@gmail.com>
2021-04-02 10:19:49 +0200

    gegen derivative

--- include/eve/module/real/polynomial/function/diff/generic/gegenbauer.hpp
+++ include/eve/module/real/polynomial/function/diff/generic/gegenbauer.hpp
@@ -18,14 +18,14 @@ namespace eve::detail
 {
 
   template<integral_value N, floating_real_value T, floating_real_value U>
-  EVE_FORCEINLINE constexpr T gegenbauer_(EVE_SUPPORTS(cpu_)
-                                  , diff_type<1> const &
-                                  , N const &n
-                                  , T const &l
-                                  , U const &x) noexcept
-  requires index_compatible_values<N, T> && compatible_values<T, U>
+  EVE_FORCEINLINE constexpr auto gegenbauer_(EVE_SUPPORTS(cpu_)
+                                            , diff_type<1> const &
+                                            , N const &n
+                                            , T const &l
+                                            , U const &x) noexcept
+  //  requires index_compatible_values<N, T> && compatible_values<T, U>
   {
-    if (eve::all(n < one(as(n))))   return zero(as(x));
-    return 2*l*gegenbauer(dec(n), inc(l), x);
+    auto iseqzn = is_eqz(n); 
+    return if_else(iseqzn, zero, 2*l*gegenbauer(dec[if_not_(iseqzn)](n), inc(l), x));
   }
 }
