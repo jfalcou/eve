5486ebd66894029a0373bac9d467540e6f16de59
jtlap <jtlapreste@gmail.com>
2021-04-05 20:20:44 +0200

    arg pas fini

--- test/unit/module/real/core/arg.cpp
+++ test/unit/module/real/core/arg.cpp
@@ -6,91 +6,119 @@
 **/
 //==================================================================================================
 #include "test.hpp"
-#include <eve/concept/value.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
-#include <eve/constant/nan.hpp>
-#include <eve/constant/zero.hpp>
 #include <eve/function/arg.hpp>
 #include <eve/function/diff/arg.hpp>
-#include <eve/function/pedantic/arg.hpp>
+#include <eve/function/saturated/arg.hpp>
 #include <eve/logical.hpp>
-#include <type_traits>
 #include <cmath>
 
 //==================================================================================================
 // Types tests
 //==================================================================================================
-EVE_TEST_TYPES( "Check return types of arg on wide"
+EVE_TEST_TYPES( "Check return types of eve::arg(scalar)"
+              , eve::test::scalar::ieee_reals
+              )
+<typename T>(eve::as_<T>)
+{
+  TTS_EXPR_IS( eve::arg(T())                    , T   );
+  TTS_EXPR_IS( eve::pedantic(eve::arg)(T())     , T   );
+  TTS_EXPR_IS( eve::diff(eve::arg)(T())         , T   );
+
+};
+
+EVE_TEST_TYPES( "Check return types of eve::arg(simd)"
               , eve::test::simd::ieee_reals
               )
 <typename T>(eve::as_<T>)
 {
+  TTS_EXPR_IS( eve::arg(T())                    , T   );
+  TTS_EXPR_IS( eve::pedantic(eve::arg)(T())     , T   );
+  TTS_EXPR_IS( eve::diff(eve::arg)(T())         , T   );
+
+};
+
+
+//==================================================================================================
+// Tests for eve::arg
+//==================================================================================================
+EVE_TEST( "Check behavior of eve::arg(eve::wide)"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate ( eve::test::ramp(-1.0, 1.0))
+        )
+  <typename T>(T const& a0)
+{
+  using eve::detail::map;
   using v_t = eve::element_type_t<T>;
 
-  TTS_EXPR_IS( eve::arg(T())  , T);
-  TTS_EXPR_IS( eve::arg(v_t()), v_t);
+  TTS_EQUAL(eve::arg(a0), map([](auto e) -> v_t { return  e >= 0 ? 0 : pi(as(v_t())); }, a0);
+};
 
-  TTS_EXPR_IS( eve::pedantic(eve::arg)(T())  , T);
-  TTS_EXPR_IS( eve::pedantic(eve::arg)(v_t()), v_t);
+EVE_TEST( "Check behavior of eve::arg(eve::scalar)"
+        , eve::test::scalar::ieee_reals
+        , eve::test::generate ( eve::test::ramp(-1.0, 1.0))
+        )
+  <typename T>(T const& a0)
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
 
-  TTS_EXPR_IS( eve::diff(eve::arg)(T())  , T);
-  TTS_EXPR_IS( eve::diff(eve::arg)(v_t()), v_t);
+  TTS_EQUAL(eve::arg(a0), map([](auto e) -> v_t { return  e >= 0 ? 0 : pi(as(v_t())); }, a0);
+};
+
+
+//==================================================================================================
+// Tests for eve::pedantic(eve::arg)
+//==================================================================================================
+EVE_TEST( "Check behavior of eve::pedantic(eve::arg)(eve::wide)"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate ( eve::test::ramp(-1.0, 1.0))
+        , eve::test::generate(eve::test::limits())
+        )
+  <typename T>(T a0, T a1 )
+{
+  TTS_EQUAL(eve::pedantic(eve::arg)(a0), map([](auto e) -> v_t { return  e >= 0 ? 0 : pi(as(v_t())); }, a0);
+  TTS_IEEE_EQUAL( eve::pedantic(eve::arg)(cases.nan    ), cases.nan   );
+};
+
+EVE_TEST( "Check behavior of eve::pedantic(eve::arg)(eve::scalar)"
+        , eve::test::scalar::ieee_reals
+        , eve::test::generate ( eve::test::ramp(-1.0, 1.0))
+        , eve::test::generate(eve::test::limits())
+        )
+  <typename T>(T a0, T a1 )
+{
+  TTS_EQUAL(eve::pedantic(eve::arg)(a0), map([](auto e) -> v_t { return  e >= 0 ? 0 : pi(as(v_t())); }, a0);
+  TTS_IEEE_EQUAL( eve::pedantic(eve::arg)(cases.nan    ), cases.nan   );
 };
 
 //==================================================================================================
-// arg tests
+// Test for eve::diff(eve::arg)
 //==================================================================================================
-EVE_TEST( "Check behavior of arg on wide"
+EVE_TEST( "Check behavior of eve::diff(eve::arg) on wide"
         , eve::test::simd::ieee_reals
-        , eve::test::generate(eve::test::randoms(eve::valmin, eve::valmax))
+        , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax))
         )
 <typename T>(T const& a0)
 {
+  using eve::detail::map;
   using v_t = eve::element_type_t<T>;
-  using eve::zero;
-  using eve::pi;
-  using eve::as;
-  using eve::arg;
-  using eve::pedantic;
-  using eve::diff;
-  auto ref_arg = [&](auto i, auto) { return a0.get(i) >= 0 ? zero(as(v_t())) : pi(as(v_t())); };
-  TTS_IEEE_EQUAL( arg(a0), T(ref_arg));
-  TTS_IEEE_EQUAL( pedantic(arg)(a0), T(ref_arg));
-  TTS_IEEE_EQUAL( diff(arg)(a0), T(0));
+
+  TTS_EQUAL ( eve::diff(eve::arg)(a0)
+            , map([](auto e) -> v_t { return e > 0 ? 1 : ((e <  0) ? -1 : 0); }, a0)
+            );
 };
 
-EVE_TEST_TYPES( "Check  with nans and infs"
-              , eve::test::simd::ieee_reals
-              )
-<typename T>(eve::as_<T>)
+EVE_TEST( "Check behavior of eve::diff(eve::arg) on scalar"
+        , eve::test::scalar::ieee_reals
+        , eve::test::generate(eve::test::randoms(eve::valmin, eve::valmax))
+        )
+<typename T>(T const& a0 )
 {
-  using eve::pi;
-  using eve::zero;
-  using eve::as;
-  using eve::arg;
-  using eve::diff;
-  using eve::pedantic;
-  // Corners cases
-  if constexpr(eve::platform::supports_nans)
-  {
-    using eve::nan;
-    TTS_IEEE_EQUAL(arg(nan(as<T>())), pi(as<T>()));
-    TTS_IEEE_EQUAL(arg(-nan(as<T>())), zero(as<T>()));
-    TTS_IEEE_EQUAL(pedantic(arg)(nan(as<T>())), nan(as<T>()));
-    TTS_IEEE_EQUAL(pedantic(arg)(-nan(as<T>())), nan(as<T>()));
-    TTS_IEEE_EQUAL(diff(arg)(nan(as<T>())), zero(as<T>()));
-    TTS_IEEE_EQUAL(diff(arg)(-nan(as<T>())), zero(as<T>()));
-  }
-  if constexpr(eve::platform::supports_infinites)
-  {
-    using eve::inf;
-    using eve::minf;
-    TTS_IEEE_EQUAL(arg(minf(as<T>())), pi(as<T>()));
-    TTS_IEEE_EQUAL(arg(inf(as<T>())), zero(as<T>()));
-    TTS_IEEE_EQUAL(pedantic(arg)(minf(as<T>())), pi(as<T>()));
-    TTS_IEEE_EQUAL(pedantic(arg)(inf(as<T>())), zero(as<T>()));
-    TTS_IEEE_EQUAL(diff(arg)(minf(as<T>())), zero(as<T>()));
-    TTS_IEEE_EQUAL(diff(arg)(inf(as<T>())), zero(as<T>()));
-  }
+  using eve::detail::map;
+
+  TTS_EQUAL ( eve::diff(eve::arg)(a0)
+            , (a0 > 0 ? T(1) : (a0 < 0 ? T(-1) : T(0)))
+            );
 };
