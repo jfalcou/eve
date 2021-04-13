c8084d50f9e56dabb28523b988b69de3513ca79d
jtlap <jtlapreste@gmail.com>
2021-04-13 17:10:00 +0200

    bit_notand

--- test/unit/module/real/core/bit_notand.cpp
+++ test/unit/module/real/core/bit_notand.cpp
@@ -6,19 +6,19 @@
 **/
 //==================================================================================================
 #include "test.hpp"
+#include <eve/traits/as_integer.hpp>
 #include <eve/constant/valmax.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/function/bit_notand.hpp>
 #include <eve/function/bit_cast.hpp>
-#include <eve/function/is_gtz.hpp>
 
 //==================================================================================================
 // Types tests
 //==================================================================================================
 EVE_TEST_TYPES( "Check return types of bit_notand"
-              , eve::test::simd::all_types
-              )
-<typename T>(eve::as_<T>)
+        , eve::test::simd::all_types
+        )
+  <typename T>(eve::as_<T>)
 {
   using v_t = eve::element_type_t<T>;
 
@@ -43,32 +43,72 @@ EVE_TEST_TYPES( "Check return types of bit_notand"
 //  bit_notand tests
 //==================================================================================================
 EVE_TEST( "Check behavior of bit_notand on integral types"
-        , eve::test::simd::integers
-        , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
-                              , eve::test::randoms(eve::valmin, eve::valmax)
-                              )
-        )
-<typename T>(T const& a0, T const& a1 )
+            , eve::test::simd::integers
+            , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
+                                  , eve::test::randoms(eve::valmin, eve::valmax)
+                                  )
+            )
+  <typename T>( T const& a0, T const& a1)
 {
   using eve::bit_notand;
-  TTS_EQUAL( bit_notand(a0, a1), T([&](auto i, auto) { return ~a0.get(i)&a1.get(i); }));
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+  TTS_EQUAL( bit_notand(a0, a1), map([](auto e, auto f) -> v_t { return ~e & f; }, a0, a1));
 };
 
-EVE_TEST( "Check behavior of bit_notand on integral types"
+EVE_TEST( "Check behavior of bit_notand on floating types"
         , eve::test::simd::ieee_reals
         , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
                               , eve::test::randoms(eve::valmin, eve::valmax)
                               )
         )
-<typename T>( T const& a0, T const& a1 )
+  <typename T>(T const& a0, T const& a1)
 {
   using eve::as;
-  using eve::bit_cast;
   using eve::bit_notand;
+  using eve::bit_cast;
+  using eve::detail::map;
   using i_t = eve::as_integer_t<eve::element_type_t<T>, signed>;
   using v_t = eve::element_type_t<T>;
-  TTS_IEEE_EQUAL( bit_notand(a0, a1), T([&](auto i, auto)
-                                { return bit_cast(~bit_cast(a0.get(i), as(i_t()))
-                                                 & bit_cast(a1.get(i), as(i_t())), as(v_t())); }
-                               ));
+  TTS_IEEE_EQUAL( bit_notand(a0, a1), map([](auto e, auto f) -> v_t
+                                       { return  bit_cast(~bit_cast(e, as(i_t()))
+                                                          & bit_cast(f, as(i_t())), as(v_t())); }
+                                     , a0, a1));
+};
+
+
+EVE_TEST( "Check behavior of bit_notand on floating types"
+        , eve::test::scalar::ieee_reals
+        , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
+                              , eve::test::randoms(eve::valmin, eve::valmax)
+                              )
+        )
+  <typename T>(T const& a0, T const& a1)
+{
+  using eve::as;
+  using eve::bit_notand;
+  using eve::bit_cast;
+  using v_t = typename T::value_type;
+  using i_t = eve::as_integer_t<v_t, signed>;
+
+  for(std::size_t i = 0;  i < a0.size(); ++i)
+   TTS_IEEE_EQUAL( bit_notand(a0[i], a1[i]), bit_cast(~bit_cast(a0[i], as(i_t()))
+                                                   & bit_cast(a1[i], as(i_t())), as(v_t())
+                                                  )
+                 );
+};
+
+EVE_TEST( "Check behavior of bit_notand on floating types"
+        , eve::test::scalar::integers
+        , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
+                              , eve::test::randoms(eve::valmin, eve::valmax)
+                              )
+        )
+  <typename T>(T const& a0, T const& a1)
+{
+  using eve::as;
+  using eve::bit_notand;
+
+  for(std::size_t i = 0;  i < a0.size(); ++i)
+    TTS_IEEE_EQUAL( bit_notand(a0[i], a1[i]), ~a0[i] & a1[i]);
 };
