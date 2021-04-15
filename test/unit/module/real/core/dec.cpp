7cec05269d2188ba0521996eb7468785b0296acf
jtlap <jtlapreste@gmail.com>
2021-04-15 20:41:11 +0200

    dec

--- test/unit/module/real/core/dec.cpp
+++ test/unit/module/real/core/dec.cpp
@@ -53,28 +53,73 @@ EVE_TEST_TYPES( "Check return types of dec"
   }
 };
 
-
-
 //==================================================================================================
-// dec  tests
+// dec(simd)  tests
 //==================================================================================================
 auto valminp1 = []< typename T>(eve::as_<T> const &){return eve::inc(eve::valmin(eve::as(eve::element_type_t<T>())));};
 
-EVE_TEST( "Check behavior of dec on signed wide"
+EVE_TEST( "Check behavior of dec(wide) and dec[cond](wide)"
         , eve::test::simd::all_types
         , eve::test::generate(eve::test::randoms(valminp1, eve::valmax))
         )
 <typename T>(T const& a0 )
 {
-  TTS_EQUAL( eve::dec(a0), T([&](auto i, auto) { return a0.get(i)-1; }));
+  using v_t = eve::element_type_t<T>;
+  using eve::diff;
+  TTS_EQUAL( eve::dec(a0), map([](auto e) ->v_t{ return v_t(e-1); }, a0));
+  TTS_EQUAL( eve::dec[a0 > 64](a0), map([](auto e) ->v_t{ return v_t((e > 64) ?e-1 : e); }, a0));
+  bool z = (a0.get(0) > 64);
+  TTS_EQUAL( eve::dec[z](a0), map([&](auto e) ->v_t{ return v_t((z) ?e-1 : e); }, a0));
+  if constexpr(eve::floating_real_value<T>)
+    TTS_EQUAL( diff(eve::dec)(a0), eve::one(eve::as(a0)));
+
 };
 
-EVE_TEST( "Check behavior of saturated dec on integral wide"
+EVE_TEST( "Check behavior of saturated(dec)(wide) on integral types"
         , eve::test::simd::integers
         , eve::test::generate(eve::test::randoms(eve::valmin, valminp1))
         )
 <typename T>(T const& a0 )
 {
-  TTS_EQUAL( eve::saturated(eve::dec)(a0), T([&](auto i, auto) { auto x = a0.get(i);
-                                               return x == eve::valmin(eve::as(x)) ? x : x-1; }));
+  using v_t = eve::element_type_t<T>;
+  TTS_EQUAL( eve::saturated(eve::dec)(a0), map([](auto e) ->v_t {
+                                                 return v_t(e == eve::valmin(eve::as(e)) ? e : e-1); }, a0));
+  TTS_EQUAL( eve::saturated(eve::dec[a0 > 64])(a0)
+           , map([](auto e) ->v_t{ return v_t((e > 64
+                                               && e != eve::valmin(eve::as(e)) ? e-1 : e)); }, a0));
+  bool z = (a0.get(0) > 64);
+   TTS_EQUAL( eve::saturated(eve::dec[z])(a0)
+           , map([&](auto e) ->v_t{ return v_t((z
+                                               && e != eve::valmin(eve::as(e)) ? e-1 : e)); }, a0));
+ };
+
+//==================================================================================================
+// dec(scalar)  tests
+//==================================================================================================
+EVE_TEST( "Check behavior of dec(wide) and dec[cond](scalar)"
+        , eve::test::scalar::all_types
+        , eve::test::generate(eve::test::randoms(valminp1, eve::valmax))
+        )
+<typename T>(T const& a0 )
+{
+  using v_t =  typename T::value_type;
+  using eve::diff;
+  for (auto a : a0)
+  {
+    TTS_EQUAL( eve::dec(a), v_t(a-1));
+    TTS_EQUAL( eve::dec[a > 64](a), v_t((a > 64) ?a-1 : a));
+    if constexpr(eve::floating_real_value<T>)
+      TTS_EQUAL( diff(eve::dec)(a), eve::one(eve::as(a)));
+  }
+};
+
+EVE_TEST( "Check behavior of saturated(dec)(wide) on integral types"
+        , eve::test::simd::integers
+        , eve::test::generate(eve::test::randoms(eve::valmin, valminp1))
+        )
+<typename T>(T const& a0 )
+{
+  using v_t = typename T::value_type;
+  for (auto a : a0)
+  TTS_EQUAL( eve::saturated(eve::dec)(a), v_t(a == eve::valmin(eve::as(a)) ? a : a-1));
 };
