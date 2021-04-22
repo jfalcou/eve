109410571774a10ac7b8b2d9a080f56538688d6d
jtlap <jtlapreste@gmail.com>
2021-04-22 10:12:15 +0200

    inc

--- test/unit/module/real/core/inc.cpp
+++ test/unit/module/real/core/inc.cpp
@@ -9,7 +9,7 @@
 #include <eve/concept/value.hpp>
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
-#include <eve/function/inc.hpp>
+#include <eve/function/dec.hpp>
 #include <eve/function/inc.hpp>
 #include <eve/function/diff/inc.hpp>
 #include <eve/function/saturated/inc.hpp>
@@ -53,28 +53,54 @@ EVE_TEST_TYPES( "Check return types of inc"
   }
 };
 
-
-
 //==================================================================================================
-// inc  tests
+// inc(simd)  tests
 //==================================================================================================
-auto valmaxm1 = []< typename T>(eve::as_<T> const &){return eve::dec(eve::valmax(eve::as(eve::element_type_t<T>())));};
+auto valminp1 = []< typename T>(eve::as_<T> const &){return eve::inc(eve::valmin(eve::as(eve::element_type_t<T>())));};
 
-EVE_TEST( "Check behavior of inc on signed wide"
+EVE_TEST( "Check behavior of inc(wide) and inc[cond](wide)"
         , eve::test::simd::all_types
-        , eve::test::generate(eve::test::randoms(eve::valmin, valmaxm1))
+        , eve::test::generate(eve::test::randoms(valminp1, eve::valmax))
         )
 <typename T>(T const& a0 )
 {
-  TTS_EQUAL( eve::inc(a0), T([&](auto i, auto) { return a0.get(i)+1; }));
+  using v_t = eve::element_type_t<T>;
+  using eve::diff;
+  TTS_EQUAL( eve::inc(a0), map([](auto e) ->v_t{ return v_t(e+1); }, a0));
+  TTS_EQUAL( eve::inc[a0 > 64](a0), map([](auto e) ->v_t{ return v_t((e > 64) ?e+1 : e); }, a0));
+  bool z = (a0.get(0) > 64);
+  TTS_EQUAL( eve::inc[z](a0), map([&](auto e) ->v_t{ return v_t((z) ?e+1 : e); }, a0));
+  if constexpr(eve::floating_real_value<T>)
+    TTS_EQUAL( diff(eve::inc)(a0), eve::one(eve::as(a0)));
+
 };
 
-EVE_TEST( "Check behavior of saturated inc on integral wide"
+EVE_TEST( "Check behavior of saturated(inc)(wide) on integral types"
         , eve::test::simd::integers
-        , eve::test::generate(eve::test::randoms(valmaxm1, eve::valmax))
+        , eve::test::generate(eve::test::randoms(eve::valmin, valminp1))
         )
 <typename T>(T const& a0 )
 {
-  TTS_EQUAL( eve::saturated(eve::inc)(a0), T([&](auto i, auto) { auto x = a0.get(i);
-                                               return x == eve::valmax(eve::as(x)) ? x : x+1; }));
+  using v_t = eve::element_type_t<T>;
+  TTS_EQUAL( eve::saturated(eve::inc)(a0), map([](auto e) ->v_t {
+                                                 return v_t(e == eve::valmax(eve::as(e)) ? e : e+1); }, a0));
+  TTS_EQUAL( eve::saturated(eve::inc[a0 > 64])(a0)
+           , map([](auto e) ->v_t{ return v_t((e > 64
+                                               && e != eve::valmin(eve::as(e)) ? e+1 : e)); }, a0));
+  bool z = (a0.get(0) > 64);
+   TTS_EQUAL( eve::saturated(eve::inc[z])(a0)
+           , map([&](auto e) ->v_t{ return v_t((z
+                                               && e != eve::valmin(eve::as(e)) ? e+1 : e)); }, a0));
+ };
+
+
+EVE_TEST( "Check behavior of saturated(inc)(wide) on integral types"
+        , eve::test::simd::integers
+        , eve::test::generate(eve::test::randoms(eve::valmin, valminp1))
+        )
+<typename T>(T const& a0 )
+{
+  using v_t = typename T::value_type;
+  for (auto a : a0)
+  TTS_EQUAL( eve::saturated(eve::inc)(a), v_t(a == eve::valmax(eve::as(a)) ? a : a+1));
 };
