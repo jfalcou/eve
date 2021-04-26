a3a3a96ca3f9d380ba1d2bc958cc6ddf22e0a300
Joel Falcou <joel.falcou@lri.fr>
2021-04-26 23:28:31 +0200

    Setup minimal set of options for PPC compilation

--- cmake/toolchain/gcc.ppc64.cmake
+++ cmake/toolchain/gcc.ppc64.cmake
@@ -10,6 +10,6 @@ set(CMAKE_SYSTEM_PROCESSOR powerpc  )
 set(CMAKE_C_COMPILER    powerpc64-linux-gnu-gcc-10  )
 set(CMAKE_CXX_COMPILER  powerpc64-linux-gnu-g++-10  )
 set(CMAKE_BUILD_TYPE    Debug                       )
-set(CMAKE_CXX_FLAGS     "-static -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
+set(CMAKE_CXX_FLAGS     "-static -mpower8-vector -mvsx -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
 
 set(CMAKE_CROSSCOMPILING_CMD qemu-ppc64)
