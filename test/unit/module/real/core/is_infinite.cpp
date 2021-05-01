e32047cfe5da147446d23f6a0825c02e943403fb
jtlap <jtlapreste@gmail.com>
2021-05-01 09:16:56 +0200

    is_infinite

---
+++ test/unit/module/real/core/is_infinite.cpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/function/is_infinite.hpp>
+#include <eve/logical.hpp>
+#include <cmath>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of eve::is_infinite(simd)"
+              , eve::test::simd::all_types
+              )
+<typename T>(eve::as_<T>)
+{
+  using eve::logical;
+  using v_t = eve::element_type_t<T>;
+  TTS_EXPR_IS( eve::is_infinite(T())                    , logical<T>   );
+  TTS_EXPR_IS( eve::is_infinite(v_t())                  , logical<v_t> );
+};
+
+//==================================================================================================
+// Tests for eve::is_infinite
+//==================================================================================================
+
+EVE_TEST( "Check behavior of eve::is_infinite(simd)"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate ( eve::test::ramp(0))
+        )
+<typename T>(T const& a0)
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EQUAL(eve::is_infinite(a0), map([](auto e) -> eve::logical<v_t> { return  (e == e) && (e-e != 0); }, a0));
+};
