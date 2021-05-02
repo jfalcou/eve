43379c298ced4854a7f513aefbe5e73fa63a1ce5
jtlap <jtlapreste@gmail.com>
2021-05-02 10:00:01 +0200

    is_not_imag

---
+++ test/unit/module/real/core/is_not_imag.cpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/function/is_not_imag.hpp>
+#include <eve/constant/smallestposval.hpp>
+#include <eve/logical.hpp>
+#include <cmath>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of eve::is_not_imag(simd)"
+              , eve::test::simd::ieee_reals
+              )
+<typename T>(eve::as_<T>)
+{
+  using eve::logical;
+  using v_t = eve::element_type_t<T>;
+  TTS_EXPR_IS( eve::is_not_imag(T())                    , logical<T>   );
+  TTS_EXPR_IS( eve::is_not_imag(v_t())                  , logical<v_t> );
+};
+
+//==================================================================================================
+// Tests for eve::is_not_imag
+//==================================================================================================
+
+EVE_TEST( "Check behavior of eve::is_not_imag(simd)"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate ( eve::test::ramp(0))
+        )
+<typename T>(T const& a0)
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EQUAL(eve::is_not_imag(a0), map([](auto e) -> eve::logical<v_t> { return  e != v_t(0); }, a0));
+};
