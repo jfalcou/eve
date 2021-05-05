aea1ab1a28ee2809355bfadb1b481248a6d7276c
jtlap <jtlapreste@gmail.com>
2021-05-05 06:09:56 +0200

    hi

--- test/unit/module/real/core/hi.cpp
+++ test/unit/module/real/core/hi.cpp
@@ -10,37 +10,46 @@
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
 #include <eve/function/hi.hpp>
-#include <eve/function/firstbitset.hpp>
+#include <eve/function/hi.hpp>
+#include <eve/function/shl.hpp>
+#include <eve/function/shr.hpp>
+#include <eve/function/combine.hpp>
 #include <bit>
 
 //==================================================================================================
-//== Types tests
+// Types tests
 //==================================================================================================
 EVE_TEST_TYPES( "Check return types of hi on wide"
-            , eve::test::simd::all_types
+            , eve::test::simd::unsigned_integers
             )
 <typename T>(eve::as_<T>)
 {
   using v_t = eve::element_type_t<T>;
-  using vi_t = eve::as_integer_t<v_t, unsigned>;
-  using sdi_t = eve::detail::downgrade_t<vi_t>;
-  using si_t  = eve::wide<sdi_t, eve::cardinal_t<T> >;
-  TTS_EXPR_IS( eve::hi(T())  , si_t);
-  TTS_EXPR_IS( eve::hi(v_t()), sdi_t);
+  using sd_t= eve::detail::downgrade_t<v_t>;
+  using d_t = eve::wide<sd_t, eve::cardinal_t<T>>;
+  TTS_EXPR_IS( eve::hi(T())  , d_t);
+  TTS_EXPR_IS( eve::hi(v_t()), sd_t);
 };
 
 
 //==================================================================================================
-//== hi(simd) tests
+// hi(simd) tests
 //==================================================================================================
 EVE_TEST( "Check behavior of hi(wide) on unsigned integral "
         , eve::test::simd::unsigned_integers
-        , eve::test::generate(eve::test::ramp(1))
+        , eve::test::generate(eve::test::randoms(eve::valmin, eve::valmax))
         )
 <typename T>(T const& a0)
 {
   using v_t = eve::element_type_t<T>;
-  using vi_t = eve::as_integer_t<v_t>;
-  using sdi_t = eve::detail::downgrade_t<vi_t>;
-  TTS_EQUAL( eve::hi(a0), map([](auto e) ->sdi_t{ return eve::hi(e); }, a0));
+  using d_t = eve::detail::downgrade_t<v_t>;
+  constexpr int s = sizeof(v_t)*4;
+  if constexpr(s == 4)
+  {
+    TTS_EQUAL(eve::hi(a0),  map([&](auto e) ->v_t{ return d_t(eve::shr(e, s)); }, a0));
+  }
+  else
+  {
+    TTS_EQUAL(eve::hi(a0),  map([&](auto e) ->d_t{ return d_t(eve::shr(e, s)); }, a0));
+  }
 };
