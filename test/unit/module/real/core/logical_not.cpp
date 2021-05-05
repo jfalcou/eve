de965ee7e5eff7d8f15c8aa16c06f3053f561dc7
jtlap <jtlapreste@gmail.com>
2021-05-05 16:20:51 +0200

    lnot

---
+++ test/unit/module/real/core/logical_not.cpp
@@ -0,0 +1,43 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/function/logical_not.hpp>
+#include <eve/constant/smallestposval.hpp>
+#include <eve/logical.hpp>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of eve::logical_not(simd)"
+              , eve::test::simd::all_types
+              )
+<typename T>(eve::as_<T>)
+{
+  using eve::logical;
+  using v_t = eve::element_type_t<T>;
+  TTS_EXPR_IS( eve::logical_not(T())                    , logical<T>   );
+  TTS_EXPR_IS( eve::logical_not(v_t())                  , logical<v_t> );
+};
+
+//==================================================================================================
+// Tests for eve::logical_not
+//==================================================================================================
+
+EVE_TEST( "Check behavior of eve::logical_not(simd)"
+        , eve::test::simd::all_types
+        , eve::test::generate (eve::test::logicals(0, 3))
+        )
+<typename T>(T const& a0)
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EQUAL(eve::logical_not(a0), map([](auto e) -> v_t { return  !e; }, a0));
+};
