f8296fbe79186731b118bde80102157a1f9bcfd1
jtlap <jtlapreste@gmail.com>
2021-05-06 11:41:59 +0200

    more tests for land and lor

--- test/unit/module/real/core/logical_and.cpp
+++ test/unit/module/real/core/logical_and.cpp
@@ -9,6 +9,7 @@
 #include <eve/constant/valmin.hpp>
 #include <eve/constant/valmax.hpp>
 #include <eve/function/logical_and.hpp>
+#include <eve/function/convert.hpp>
 #include <eve/logical.hpp>
 #include <cmath>
 
@@ -39,18 +40,25 @@ EVE_TEST_TYPES( "Check return types of eve::logical_and(simd)"
 EVE_TEST( "Check behavior of eve::logical_and(simd)"
         , eve::test::simd::all_types
         , eve::test::generate (eve::test::logicals(0, 3)
-                              , eve::test::logicals(1, 2))
+                              , eve::test::logicals(1, 2)
+                              , eve::test::randoms(0, 6))
         )
-<typename M>(M const& l0, M const& l1)
+<typename M, typename T>(M const& l0, M const& l1, T const & a0)
 {
   using eve::detail::map;
-  using v_t = eve::element_type_t<M>;
+  using l_t = eve::element_type_t<M>;
 
-    TTS_EQUAL(eve::logical_and(l0, true), map([](auto e) -> v_t { return  e; }, l0));
-    TTS_EQUAL(eve::logical_and(true, l1), map([](auto e) -> v_t { return  e; }, l1));
-    TTS_EQUAL(eve::logical_and(l0, true), map([](auto e) -> v_t { return  e; }, l0));
-    TTS_EQUAL(eve::logical_and(l0, false), eve::false_(eve::as<M>()));
-    TTS_EQUAL(eve::logical_and(l0, l1), map([](auto e, auto f) -> v_t { return  e && f; }, l0, l1));
-    TTS_EQUAL(eve::logical_and(l0, l1.get(0)), map([&](auto e) -> v_t { return  e && l1.get(0); }, l0));
-    TTS_EQUAL(eve::logical_and(l0.get(0), l1), map([&](auto f) -> v_t { return  l0.get(0) && f; }, l1));
+  TTS_EQUAL(eve::logical_and(l0, true), map([](auto e) -> l_t { return  e; }, l0));
+  TTS_EQUAL(eve::logical_and(true, l1), map([](auto e) -> l_t { return  e; }, l1));
+  TTS_EQUAL(eve::logical_and(l0, true), map([](auto e) -> l_t { return  e; }, l0));
+  TTS_EQUAL(eve::logical_and(l0, false), eve::false_(eve::as<M>()));
+  TTS_EQUAL(eve::logical_and(l0, l1), map([](auto e, auto f) -> l_t { return  e && f; }, l0, l1));
+  TTS_EQUAL(eve::logical_and(l0, l1.get(0)), map([&](auto e) -> l_t { return  e && l1.get(0); }, l0));
+  TTS_EQUAL(eve::logical_and(l0.get(0), l1), map([&](auto f) -> l_t { return  l0.get(0) && f; }, l1));
+  using v_t = eve::element_type_t<T>;
+  using d_t = eve::detail::downgrade_t<v_t>;
+  auto da0 = eve::convert(a0, eve::as<d_t>());
+  using dl_t = eve::as_logical_t<d_t>;
+  TTS_EQUAL(eve::logical_and(l1, da0 > 1), map([](auto e, auto f) -> l_t { return (f > 1) && e; }, l1, da0));
+  TTS_EQUAL(eve::logical_and( da0 > 1, l1), map([](auto e, auto f) -> dl_t { return bool((e > 1) && f); }, da0, l1));
 };
--- test/unit/module/real/core/logical_or.cpp
+++ test/unit/module/real/core/logical_or.cpp
@@ -39,18 +39,25 @@ EVE_TEST_TYPES( "Check return types of eve::logical_or(simd)"
 EVE_TEST( "Check behavior of eve::logical_or(simd)"
         , eve::test::simd::all_types
         , eve::test::generate (eve::test::logicals(0, 3)
-                              , eve::test::logicals(1, 2))
+                              , eve::test::logicals(1, 2)
+                              , eve::test::randoms(0, 2))
         )
-<typename M>(M const& l0, M const& l1)
+<typename M, typename T>(M const& l0, M const& l1, T const & a0)
 {
   using eve::detail::map;
-  using v_t = eve::element_type_t<M>;
+  using l_t = eve::element_type_t<M>;
 
-    TTS_EQUAL(eve::logical_or(l0, true), eve::true_(eve::as<M>()));
-    TTS_EQUAL(eve::logical_or(true, l1), eve::true_(eve::as<M>()));
-    TTS_EQUAL(eve::logical_or(false, l1), map([](auto e) -> v_t { return  e; }, l1));
-    TTS_EQUAL(eve::logical_or(l0, false), map([](auto e) -> v_t { return  e; }, l0));
-    TTS_EQUAL(eve::logical_or(l0, l1), map([](auto e, auto f) -> v_t { return  e || f; }, l0, l1));
-    TTS_EQUAL(eve::logical_or(l0, l1.get(0)), map([&](auto e) -> v_t { return  e || l1.get(0); }, l0));
-    TTS_EQUAL(eve::logical_or(l0.get(0), l1), map([&](auto f) -> v_t { return  l0.get(0) || f; }, l1));
+  TTS_EQUAL(eve::logical_or(l0, true), eve::true_(eve::as<M>()));
+  TTS_EQUAL(eve::logical_or(true, l1), eve::true_(eve::as<M>()));
+  TTS_EQUAL(eve::logical_or(false, l1), map([](auto e) -> l_t { return  e; }, l1));
+  TTS_EQUAL(eve::logical_or(l0, false), map([](auto e) -> l_t { return  e; }, l0));
+  TTS_EQUAL(eve::logical_or(l0, l1), map([](auto e, auto f) -> l_t { return  e || f; }, l0, l1));
+  TTS_EQUAL(eve::logical_or(l0, l1.get(0)), map([&](auto e) -> l_t { return  e || l1.get(0); }, l0));
+  TTS_EQUAL(eve::logical_or(l0.get(0), l1), map([&](auto f) -> l_t { return  l0.get(0) || f; }, l1));
+  using v_t = eve::element_type_t<T>;
+  using d_t = eve::detail::downgrade_t<v_t>;
+  auto da0 = eve::convert(a0, eve::as<d_t>());
+  using dl_t = eve::as_logical_t<d_t>;
+  TTS_EQUAL(eve::logical_or(l1, da0 > 1), map([](auto e, auto f) -> l_t { return (f > 1) || e; }, l1, da0));
+  TTS_EQUAL(eve::logical_or( da0 > 1, l1), map([](auto e, auto f) -> dl_t { return bool((e > 1) || f); }, da0, l1));
 };
