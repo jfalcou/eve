15da6cda577cd511f5f2ebc992f3cf0b0e315b59
Joel Falcou <joel.falcou@lri.fr>
2021-05-11 08:02:39 +0200

    Fixed ramp -> between

--- test/unit/module/real/polynomial/gegenbauer.cpp
+++ test/unit/module/real/polynomial/gegenbauer.cpp
@@ -33,7 +33,7 @@ EVE_TEST_TYPES( "Check return types of gegenbauer on wide"
 //==================================================================================================
 EVE_TEST( "Check behavior of gegenbauer on wide"
         , eve::test::simd::ieee_reals
-        , eve::test::generate(eve::test::ramp(0.0, 1.0), eve::test::as_integer(eve::test::ramp(0)))
+        , eve::test::generate(eve::test::between(0.0, 1.0), eve::test::as_integer(eve::test::ramp(0)))
         )
   <typename T, typename I>(T const& a0,I const & i0)
 {
@@ -67,7 +67,7 @@ EVE_TEST( "Check behavior of gegenbauer on wide"
 //==================================================================================================
 EVE_TEST( "Check behavior of gegenbauer diff on wide"
         , eve::test::simd::ieee_reals
-        , eve::test::generate(eve::test::ramp(0.0, 1.0), eve::test::as_integer(eve::test::ramp(0)))
+        , eve::test::generate(eve::test::between(0.0, 1.0), eve::test::as_integer(eve::test::ramp(0)))
         )
   <typename T, typename I>(T const& a0,I const & i0)
 {
