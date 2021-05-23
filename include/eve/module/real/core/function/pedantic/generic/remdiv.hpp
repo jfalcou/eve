f1c55f3b9ae93df02adccdd0ebed21679bdd8e0e
jtlap <jtlapreste@gmail.com>
2021-05-23 10:51:13 +0200

    remdiv

--- include/eve/module/real/core/function/pedantic/generic/remdiv.hpp
+++ include/eve/module/real/core/function/pedantic/generic/remdiv.hpp
@@ -8,30 +8,29 @@
 #pragma once
 
 #include <eve/concept/value.hpp>
+#include <eve/detail/function/conditional.hpp>
 #include <eve/detail/implementation.hpp>
+#include <eve/function/binarize_not.hpp>
+#include <eve/function/bitofsign.hpp>
 #include <eve/function/div.hpp>
 #include <eve/function/fnma.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/function/is_nez.hpp>
-#include <eve/function/is_eqz.hpp>
-#include <eve/function/is_unordered.hpp>
-#include <eve/function/trunc.hpp>
-#include <eve/function/decorator.hpp>
-#include <eve/detail/skeleton_calls.hpp>
-#include <eve/detail/apply_over.hpp>
-#include <tuple>
+#include <eve/function/roundings.hpp>
+#include <eve/function/pedantic.hpp>
 
 namespace eve::detail
 {
   template<real_value T, real_value U>
-  EVE_FORCEINLINE auto
-  remdiv_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+  EVE_FORCEINLINE auto remdiv_ ( EVE_SUPPORTS(cpu_)
+                            , pedantic_type const&,T const &a, U const &b
+                            ) noexcept
   requires compatible_values<T, U>
   {
     using r_t = common_compatible_t<T, U>;
     if constexpr(has_native_abi_v<T>&& has_native_abi_v<U>)
     {
-      auto d = trunc(div(a,b));
+      auto d = pedantic(div)(a,b));
       if constexpr(floating_real_value<T>)
       {
         auto r = if_else(is_unordered(a, b) || is_infinite(a) || is_eqz(b)
@@ -45,10 +44,20 @@ namespace eve::detail
       }
       else
       {
-        auto r = fnma(b, d, a);
+        auto r = if_else(is_nez(b), fnma(b, d, a), a);
         return std::tuple<r_t, r_t>{r, d};
       }
     }
     else return apply_over2t(remdiv, r_t(a), r_t(b));
+
+    return if_else(is_nez(b), rem(a,b), a);
+  }
+
+  template<conditional_expr C, real_value U, real_value V>
+  EVE_FORCEINLINE auto
+  rem_(EVE_SUPPORTS(cpu_), C const &cond, pedantic_type const &, U const &t, V const &f) noexcept
+      requires compatible_values<U, V>
+  {
+    return mask_op(  cond, pedantic(rem), t, f);
   }
 }
--- test/unit/module/real/core/remdiv.cpp
+++ test/unit/module/real/core/remdiv.cpp
@@ -38,7 +38,7 @@ EVE_TEST_TYPES( "Check return types of remdiv"
 auto mini = [] < typename T > (eve::as_<T> const &){ return std::is_signed_v<eve::element_type_t<T>> ? -100 : 1;};
 
 EVE_TEST( "Check behavior of remdiv on wide"
-        , eve::test::simd::ieee_reals//all_types
+        , eve::test::simd::all_types
         , eve::test::generate ( eve::test::randoms(mini, 100)
                               , eve::test::randoms(mini, 100)
                               )
