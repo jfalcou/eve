0dedb734781c7baf495c5f5a3ea8b6f17578c10f
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-05-29 12:16:06 +0100

    FIX-670: x86 average

--- include/eve/module/real/core/function/regular/simd/x86/average.hpp
+++ include/eve/module/real/core/function/regular/simd/x86/average.hpp
@@ -16,32 +16,24 @@
 
 namespace eve::detail
 {
-  // -----------------------------------------------------------------------------------------------
-  // 128 bits implementation
   template<unsigned_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, x86_128_> average_(EVE_SUPPORTS(sse2_)
-                                           , wide<T, N, x86_128_> const &a
-                                           , wide<T, N, x86_128_> const &b) noexcept
+  EVE_FORCEINLINE wide<T, N> average_(EVE_SUPPORTS(sse2_), wide<T, N> a, wide<T, N> b) noexcept
+    requires x86_abi<abi_t<T, N>>
   {
-         if constexpr(sizeof(T) == 1) return _mm_avg_epu8(a, b);
-    else if constexpr(sizeof(T) == 2) return _mm_avg_epu16(a, b);
-    else                              return (a & b) + ((a ^ b) >> 1);
-  }
-
-  // -----------------------------------------------------------------------------------------------
-  // 256 bits implementation
-  template<unsigned_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, x86_256_> average_(EVE_SUPPORTS(avx_)
-                                           , wide<T, N, x86_256_> const &a
-                                           , wide<T, N, x86_256_> const &b) noexcept
-  {
-    if constexpr(current_api >= avx2)
+    if constexpr ( sizeof(T) == 2 )
     {
-             if constexpr(sizeof(T) == 1) return _mm256_avg_epu8(a, b);
-        else if constexpr(sizeof(T) == 2) return _mm256_avg_epu16(a, b);
-        else                              return (a & b) + ((a ^ b) >> 1);
+           if constexpr (                        std::same_as<abi_t<T, N>, x86_512_> ) return _mm512_avg_epu16(a, b);
+      else if constexpr ( current_api >= avx2 && std::same_as<abi_t<T, N>, x86_256_> ) return _mm256_avg_epu16(a, b);
+      else if constexpr (                        std::same_as<abi_t<T, N>, x86_128_> ) return _mm_avg_epu16   (a, b);
+      else                                                                             return average_        (EVE_RETARGET(cpu_), a, b);
     }
-    else                                   return average_(EVE_RETARGET(cpu_), a, b);
-
+    if constexpr ( sizeof(T) == 1 )
+    {
+           if constexpr (                        std::same_as<abi_t<T, N>, x86_512_> ) return _mm512_avg_epu8 (a, b);
+      else if constexpr ( current_api >= avx2 && std::same_as<abi_t<T, N>, x86_256_> ) return _mm256_avg_epu8 (a, b);
+      else if constexpr (                        std::same_as<abi_t<T, N>, x86_128_> ) return _mm_avg_epu8    (a, b);
+      else                                                                             return average_        (EVE_RETARGET(cpu_), a, b);
+    }
+    else return average_(EVE_RETARGET(cpu_), a, b);
   }
 }
