f12c8d3d73d4df450573bcf810b0ba5b4f7b8d72
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-05-30 12:03:00 +0100

    FIX-670: sqrt

--- include/eve/module/real/core/function/regular/simd/x86/sqrt.hpp
+++ include/eve/module/real/core/function/regular/simd/x86/sqrt.hpp
@@ -14,22 +14,16 @@
 namespace eve::detail
 {
   template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, x86_128_> sqrt_(EVE_SUPPORTS(sse2_), wide<T, N, x86_128_> const &a0) noexcept
+  EVE_FORCEINLINE wide<T, N> sqrt_(EVE_SUPPORTS(sse2_), wide<T, N> a0) noexcept
+    requires x86_abi<abi_t<T, N>>
   {
-    if constexpr(std::is_same_v<T, float>)
-      return _mm_sqrt_ps(a0);
-    else if constexpr(std::is_same_v<T, double>)
-      return _mm_sqrt_pd(a0);
-  }
+    constexpr auto c = categorize<wide<T, N>>();
 
-  template<floating_real_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N, x86_256_> sqrt_(EVE_SUPPORTS(avx2_), wide<T, N, x86_256_> const &a0) noexcept
-  {
-    if constexpr(std::is_same_v<T, float>)
-      return _mm256_sqrt_ps(a0);
-    else if constexpr(std::is_same_v<T, double>)
-      return _mm256_sqrt_pd(a0);
+         if constexpr ( c == category::float64x8  ) return _mm512_sqrt_pd(a0);
+    else if constexpr ( c == category::float32x16 ) return _mm512_sqrt_ps(a0);
+    else if constexpr ( c == category::float64x4  ) return _mm256_sqrt_pd(a0);
+    else if constexpr ( c == category::float32x8  ) return _mm256_sqrt_ps(a0);
+    else if constexpr ( c == category::float64x2  ) return _mm_sqrt_pd   (a0);
+    else if constexpr ( c == category::float32x4  ) return _mm_sqrt_ps   (a0);
   }
-
 }
-
