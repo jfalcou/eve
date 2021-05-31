8932fec239117168fb221e039ae4ff12bf66f5b8
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-05-31 13:32:14 +0100

    FIX-670: inline namespace around wide (#756)

--- include/eve/arch/cpu/spec.hpp
+++ include/eve/arch/cpu/spec.hpp
@@ -13,6 +13,7 @@
 #  define EVE_CURRENT_ABI ::eve::emulated_
 #  define EVE_CURRENT_API ::eve::cpu_
 #  define EVE_STRICT_EMULATION
+#  define EVE_ABI_NAMESPACE cpu_abi_v0
 #endif
 
 //==================================================================================================
