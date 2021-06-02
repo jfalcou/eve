2e96583ec4154275914a10bf883dfb20d8a7224d
jtlap <jtlapreste@gmail.com>
2021-06-02 12:08:15 +0200

    ldexp regular and pedantic

--- include/eve/module/real/core/function/pedantic/generic/ldexp.hpp
+++ include/eve/module/real/core/function/pedantic/generic/ldexp.hpp
@@ -10,10 +10,13 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/function/bit_cast.hpp>
 #include <eve/function/converter.hpp>
+#include <eve/function/convert.hpp>
+#include <eve/function/any.hpp>
 #include <eve/function/dec.hpp>
 #include <eve/function/inc.hpp>
 #include <eve/function/is_less.hpp>
 #include <eve/function/if_else.hpp>
+#include <eve/function/sub.hpp>
 #include <eve/function/trunc.hpp>
 #include <eve/function/pedantic.hpp>
 #include <eve/function/sub.hpp>
@@ -28,42 +31,62 @@
 
 namespace eve::detail
 {
-  template<real_value T, real_value U>
-  EVE_FORCEINLINE T ldexp_(EVE_SUPPORTS(cpu_)
-                               , pedantic_type const &
-                               , T const &a
-                               , U const &b) noexcept
+  template<floating_real_value T, floating_real_value U>
+  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(cpu_)
+                             , pedantic_type const &
+                             , T a
+                             , U b) noexcept
+  requires(std::same_as<element_type_t<T>, element_type_t<U>>)
   {
-    if constexpr(has_native_abi_v<T> && has_native_abi_v<U>)
+    return pedantic(ldexp)(a, int_(trunc(b)));
+  }
+
+  template<floating_real_value T, integral_real_value U>
+  EVE_FORCEINLINE auto ldexp_(EVE_SUPPORTS(cpu_)
+                               , pedantic_type const &
+                               , T a
+                               , U b) noexcept
+  {
+    if constexpr(floating_real_scalar_value<T> && integral_simd_value<U>)
+    {
+      using i_t =  as_integer_t<T>;
+      using w_t =  wide<T,  cardinal_t<U>>;
+      auto bb = convert(b, as<i_t>());
+      w_t aa(a);
+      return pedantic(ldexp)(aa, bb);
+    }
+    else if constexpr(floating_real_simd_value<T> && integral_scalar_value<U>)
     {
       using elt_t = element_type_t<T>;
-      using eli_t =  as_integer_t<elt_t>;
-      if constexpr(integral_value<U>)
-      {
-        using i_t =  as_integer_t<T>;
-        auto e = to_<i_t>(b);
-        auto f = one(eve::as<T>());
-        if constexpr( eve::platform::supports_denormals)
-        {
-          auto denormal =  is_less(e, minexponent(eve::as<elt_t>()));
-          e = sub[denormal]( e, minexponent(eve::as<elt_t>()));
-          f = if_else(denormal, smallestposval(eve::as<elt_t>()), eve::one);
-        }
-        auto test = (e == limitexponent(eve::as<elt_t>()));
-        f = inc[test](f);
-        e = dec[test](e);
-        e += maxexponent(eve::as<elt_t>());
-        e <<=  nbmantissabits(eve::as<elt_t>());
-        if constexpr(scalar_value<decltype(e)>)
-          return a*bit_cast(e, as(elt_t()))*f;
-        else
-          return a*bit_cast(e, as(T()))*f;
-      }
-      else if constexpr(floating_value<U>)
-      {
-       return pedantic(ldexp)(a, convert(trunc(b), as<eli_t>()));
-      }
+      using i_t =  as_integer_t<elt_t>;
+      using wi_t = wide<i_t, cardinal_t<T>>;
+      i_t bb = convert(trunc(b), as<i_t>());
+      return pedantic(ldexp)(a, wi_t(bb));
     }
-    else  return apply_over(pedantic(ldexp), a, b);
+    else
+    {
+      if constexpr(has_native_abi_v<T> && has_native_abi_v<U>)
+      {
+        using elt_t = element_type_t<T>;
+        auto denormal =  is_less(b, minexponent(eve::as<elt_t>()));
+        auto f = one(eve::as<T>());
+        if (eve::any(denormal))
+        {
+          if constexpr( eve::platform::supports_denormals)
+          {
+            b = sub[denormal]( b, decltype(b)(minexponent(eve::as<elt_t>())));
+            f = if_else(denormal, smallestposval(eve::as<elt_t>()), eve::one);
+          }
+          auto test = (b == decltype(b)(limitexponent(eve::as<elt_t>())));
+          f = inc[test](f);
+          b = dec[test](b);
+          b += maxexponent(eve::as<elt_t>());
+          b <<=  nbmantissabits(eve::as<elt_t>());
+          return a*bit_cast(b, as(T()))*f;
+        }
+        else return ldexp(a, b);
+      }
+      else  return apply_over(pedantic(ldexp), a, b);
+    }
   }
 }
