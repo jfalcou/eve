20cc93d566815e3cb6bac2086ddedba87ec735cc
jtlap <jtlapreste@gmail.com>
2021-06-15 11:23:11 +0200

    manhattan

---
+++ test/unit/module/real/core/manhattan.cpp
@@ -0,0 +1,55 @@
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/constant/valmax.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/mone.hpp>
+#include <eve/function/abs.hpp>
+#include <eve/function/manhattan.hpp>
+#include <eve/function/pedantic/manhattan.hpp>
+#include <algorithm>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of manhattan"
+              , eve::test::simd::ieee_reals
+              )
+<typename T>(eve::as_<T>)
+{
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EXPR_IS( eve::manhattan(T(), T(), T()  )  , T);
+  TTS_EXPR_IS( eve::manhattan(T(), v_t(), T())  , T);
+  TTS_EXPR_IS( eve::manhattan(v_t(), T(), T())  , T);
+  TTS_EXPR_IS( eve::manhattan(T(), T(), v_t() ) , T);
+  TTS_EXPR_IS( eve::manhattan(v_t(), v_t(), T()) , T);
+  TTS_EXPR_IS( eve::manhattan(v_t(), T(), v_t()) , T);
+  TTS_EXPR_IS( eve::manhattan(v_t(), v_t(), v_t()) , v_t);
+};
+
+//==================================================================================================
+// manhattan tests
+//==================================================================================================
+
+EVE_TEST( "Check behavior of manhattan on all types full range"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate (  eve::test::randoms(eve::valmin, eve::valmax)
+                              ,  eve::test::randoms(eve::valmin, eve::valmax)
+                              ,  eve::test::randoms(eve::valmin, eve::valmax)
+                              )
+        )
+<typename T>(  T const& a0, T const& a1, T const& a2)
+{
+  using eve::manhattan;
+  using eve::detail::map;
+  using eve::abs;
+  using v_t = eve::element_type_t<T>;
+  auto m = [](auto a, auto b, auto c)-> v_t {return abs(a)+abs(b)+abs(c); };
+  TTS_ULP_EQUAL(manhattan((a0), (a1), (a2)), map(m, a0, a1, a2), 2);
+  TTS_ULP_EQUAL(eve::pedantic(manhattan)((a0), (a1), (a2)), map(m, a0, a1, a2), 2);
+};
