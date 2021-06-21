a5172f7b5868b3e746d3d8d41f25d81d6bfd5efe
jtlap <jtlapreste@gmail.com>
2021-06-21 20:14:58 +0200

    rshr/l doc

--- test/doc/core/rshl.cpp
+++ test/doc/core/rshl.cpp
@@ -2,26 +2,26 @@
 #include <eve/wide.hpp>
 #include <iostream>
 
-using iT      = std::int32_t;
-using wide_it = eve::wide<iT, eve::fixed<4>>;
+using iT       = std::int32_t;
+using uiT      = std::uint32_t;
+using wide_it  = eve::wide<iT, eve::fixed<4>>;
+using wide_uit = eve::wide<uiT, eve::fixed<4>>;
 
 int main()
 {
-  wide_it pi = {100, 200, -2, 3};
-  wide_it qi = {1, -2, 3, -1};
+  wide_uit pi = {100, 200, 2, 3};
+  wide_it  qi = {1, -2, 3, -1};
 
   std::cout << "---- simd" << '\n'
             << "<- pi            = " << pi << '\n'
             << "<- qi            = " << qi << '\n'
             << "-> shl(pi, qi)   = " << eve::rshl(pi, qi) << '\n';
 
-  iT xi = 2, mxi = -2, yi = 3;
+  uiT xi = 2, yi = 3;
 
   std::cout << "---- scalar" << '\n'
             << "<- xi            = " << xi << '\n'
-            << "<- mxi           = " << mxi << '\n'
             << "<- yi            = " << yi << '\n'
-            << "-> rshl(xi, yi)  = " << eve::rshl(xi, yi) << '\n'
-            << "-> rshl(mxi, yi) = " << eve::rshl(mxi, yi) << '\n';
+            << "-> rshl(xi, yi)  = " << eve::rshl(xi, yi) << '\n';
   return 0;
 }
