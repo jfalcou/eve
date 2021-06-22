8aabf86f439b3537bd7669489a05e7b728953661
jtlap <jtlapreste@gmail.com>
2021-06-22 08:53:21 +0200

    sign

--- include/eve/module/real/core/function/regular/generic/sign.hpp
+++ include/eve/module/real/core/function/regular/generic/sign.hpp
@@ -72,5 +72,12 @@ namespace eve::detail
       return apply_over(sign, a);
     }
   }
-}
 
+  // -----------------------------------------------------------------------------------------------
+  // Masked case
+  template<conditional_expr C, real_value U>
+  EVE_FORCEINLINE auto sign_(EVE_SUPPORTS(cpu_), C const &cond, U const &t) noexcept
+  {
+    return mask_op( cond, sign, t);
+  }
+}
