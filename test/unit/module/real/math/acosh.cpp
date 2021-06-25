314afc7279388299a6af687973e80a462b900950
jtlap <jtlapreste@gmail.com>
2021-06-25 10:04:46 +0200

    math a*.cpp

---
+++ test/unit/module/real/math/acosh.cpp
@@ -0,0 +1,46 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/concept/value.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/function/acosh.hpp>
+#include <eve/function/diff/acosh.hpp>
+#include <cmath>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of acosh"
+            , eve::test::simd::ieee_reals
+            )
+<typename T>(eve::as_<T>)
+{
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EXPR_IS( eve::acosh(T())  , T);
+  TTS_EXPR_IS( eve::acosh(v_t()), v_t);
+};
+
+//==================================================================================================
+// acosh  tests
+//==================================================================================================
+EVE_TEST( "Check behavior of acosh on wide"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate(eve::test::randoms(1.0, eve::valmax)
+                             , eve::test::randoms(1.0, 100.0))
+        )
+<typename T>(T const& a0, T const& a1 )
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+
+  TTS_ULP_EQUAL(eve::acosh(a0)      , map([](auto e) -> v_t { return std::acosh(e); }, a0), 2);
+  TTS_ULP_EQUAL(eve::acosh(a1)      , map([](auto e) -> v_t { return std::acosh(e); }, a1), 2);
+  TTS_ULP_EQUAL(eve::diff(eve::acosh)(a0), map([](auto e) -> v_t { return  v_t(1)/std::sqrt(e*e-1); }, a0), 2);
+  TTS_ULP_EQUAL(eve::diff(eve::acosh)(a1), map([](auto e) -> v_t { return  v_t(1)/std::sqrt(e*e-1); }, a1), 2);
+};
---
+++ test/unit/module/real/math/atan2d.cpp
@@ -0,0 +1,53 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+#include <eve/concept/value.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
+#include <eve/function/radindeg.hpp>
+#include <eve/function/diff/atan2d.hpp>
+#include <cmath>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of atan2d"
+            , eve::test::simd::ieee_reals
+            )
+<typename T>(eve::as_<T>)
+{
+  using v_t = eve::element_type_t<T>;
+
+  TTS_EXPR_IS( eve::atan2d(T(), T())  , T);
+  TTS_EXPR_IS( eve::atan2d(v_t(), v_t()), v_t);
+  TTS_EXPR_IS( eve::atan2d(T(), v_t()), T);
+  TTS_EXPR_IS( eve::atan2d(v_t(), T()), T);
+};
+
+//==================================================================================================
+// atan2d  tests
+//==================================================================================================
+EVE_TEST( "Check behavior of atan2d on wide"
+        , eve::test::simd::ieee_reals
+        , eve::test::generate( eve::test::randoms(eve::valmin, eve::valmax)
+                             , eve::test::randoms(eve::valmin, eve::valmax)
+                             , eve::test::randoms(-1.0, 1.0)
+                             , eve::test::randoms(-1.0, 1.0))
+        )
+<typename T>(T const& a0, T const& a1, T const& a2, T const& a3)
+{
+  using eve::detail::map;
+  using v_t = eve::element_type_t<T>;
+
+  TTS_ULP_EQUAL(eve::atan2d(a0, a1)      , map([](auto e, auto f) -> v_t { return eve::radindeg(std::atan2(e, f)); }, a0, a1), 2);
+  TTS_ULP_EQUAL(eve::atan2d(a2, a3)      , map([](auto e, auto f) -> v_t { return eve::radindeg(std::atan2(e, f)); }, a2, a3), 2);
+  TTS_ULP_EQUAL(eve::diff_1st(eve::atan2d)(a0, a1), map([](auto e, auto f) -> v_t { return  eve::radindeg(f/(e*e+f*f)); }, a0, a1), 2);
+  TTS_ULP_EQUAL(eve::diff_1st(eve::atan2d)(a2, a3), map([](auto e, auto f) -> v_t { return  eve::radindeg(f/(e*e+f*f)); }, a2, a3), 2);
+  TTS_ULP_EQUAL(eve::diff_2nd(eve::atan2d)(a0, a1), map([](auto e, auto f) -> v_t { return  eve::radindeg(e/(e*e+f*f)); }, a0, a1), 2);
+  TTS_ULP_EQUAL(eve::diff_2nd(eve::atan2d)(a2, a3), map([](auto e, auto f) -> v_t { return  eve::radindeg(e/(e*e+f*f)); }, a2, a3), 2);
+};
