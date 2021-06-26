14d8821b0fc527024b42d78a858a80a07c1ebf0e
jtlap <jtlapreste@gmail.com>
2021-06-26 19:40:18 +0200

    cscd

--- test/unit/module/real/math/cscd.cpp
+++ test/unit/module/real/math/cscd.cpp
@@ -67,9 +67,11 @@ EVE_TEST( "Check behavior of cscd on wide"
   TTS_ULP_EQUAL(eve::big(cscd)(a0)             , map(ref, a0), 2);
   TTS_ULP_EQUAL(eve::big(cscd)(a1)             , map(ref, a1), 2);
   TTS_ULP_EQUAL(eve::big(cscd)(a2)             , map(ref, a2), 300);
+  TTS_ULP_EQUAL(eve::big(cscd)(a3)             , map(ref, a3), 300);
   TTS_ULP_EQUAL(cscd(a0)                       , map(ref, a0), 2);
   TTS_ULP_EQUAL(cscd(a1)                       , map(ref, a1), 2);
   TTS_ULP_EQUAL(cscd(a2)                       , map(ref, a2), 300);
+  TTS_ULP_EQUAL(cscd(a3)                       , map(ref, a3), 300);
   auto dinr = 1.7453292519943295769236907684886127134428718885417e-2l;
 
   TTS_ULP_EQUAL(diff(cscd)(a0), map([dinr](auto e) -> v_t { return  -dinr*cscd(e)*eve::cotd(e); }, a0), 2);
