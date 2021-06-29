4a77932ca1bb58e36aab9953dace2fd790a13bf2
jtlap <jtlapreste@gmail.com>
2021-06-29 08:09:36 +0200

    powm1

--- test/unit/module/real/math/powm1.cpp
+++ test/unit/module/real/math/powm1.cpp
@@ -58,60 +58,51 @@ EVE_TEST( "Check behavior of powm1 on wide"
 
 
 EVE_TEST_TYPES( "Check  powm1"
-            , eve::test::simd::all_types
+            , eve::test::simd::ieee_reals
             )
 <typename T>(eve::as_<T>)
 {
   using eve::dec;
-  if constexpr(eve::floating_value<T>)
-  {
-    TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  T(-1)          )          , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  T(-1)          )          , eve::minf(eve::as<T>()));
-    TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  T(-2)          )          , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  T(-2)          )          , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1(-T(1)          ,  eve::minf(eve::as<T>()) ) , T(0)          );
-    TTS_IEEE_EQUAL(eve::powm1(-T(1)          ,  eve::inf(eve::as<T>())  ) , T(0)          );
-    TTS_IEEE_EQUAL(eve::powm1( T(1)          ,  eve::nan(eve::as<T>())  ) , T(0)          );
-    TTS_IEEE_EQUAL(eve::powm1( eve::nan(eve::as<T>()) ,  T(0)           ) , T(0)          );
-    TTS_IEEE_EQUAL(eve::powm1( eve::nan(eve::as<T>()) , -T(0)           ) , T(0)          );
-    TTS_IEEE_EQUAL(eve::powm1( T(0.5)        ,  eve::inf(eve::as<T>())  ) , T(-1 )        );
-    TTS_IEEE_EQUAL(eve::powm1( T(2)          ,  eve::inf(eve::as<T>())  ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1( T(0.5)        ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1( T(2)          ,  eve::minf(eve::as<T>()) ) , T(-1 )        );
-    TTS_IEEE_EQUAL(eve::powm1(-T(0.5)        ,  eve::inf(eve::as<T>())  ) , T(-1 )        );
-    TTS_IEEE_EQUAL(eve::powm1(-T(2)          ,  eve::inf(eve::as<T>())  ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1(-T(0.5)        ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1(-T(2)          ,  eve::minf(eve::as<T>()) ) , T(-1)        );
-    TTS_IEEE_EQUAL(eve::powm1( eve::minf(eve::as<T>()), -T(3)           ) , T(-1)        );
-    TTS_IEEE_EQUAL(eve::powm1( eve::minf(eve::as<T>()), -T(4)           ) , T(-1)        );
-    TTS_IEEE_EQUAL(eve::powm1( eve::inf(eve::as<T>()) ,  T(4)           ) , eve::inf(eve::as<T>()) );
-    TTS_IEEE_EQUAL(eve::powm1( eve::inf(eve::as<T>()) , -T(4)           ) , T( -1)        );
-  }
+  TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  T(-1)          )          , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  T(-1)          )          , eve::minf(eve::as<T>()));
+  TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  T(-2)          )          , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  T(-2)          )          , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1( T(0)          ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1(-T(0)          ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1(-T(1)          ,  eve::minf(eve::as<T>()) ) , T(0)          );
+  TTS_IEEE_EQUAL(eve::powm1(-T(1)          ,  eve::inf(eve::as<T>())  ) , T(0)          );
+  TTS_IEEE_EQUAL(eve::powm1( T(1)          ,  eve::nan(eve::as<T>())  ) , T(0)          );
+  TTS_IEEE_EQUAL(eve::powm1( eve::nan(eve::as<T>()) ,  T(0)           ) , T(0)          );
+  TTS_IEEE_EQUAL(eve::powm1( eve::nan(eve::as<T>()) , -T(0)           ) , T(0)          );
+  TTS_IEEE_EQUAL(eve::powm1( T(0.5)        ,  eve::inf(eve::as<T>())  ) , T(-1 )        );
+  TTS_IEEE_EQUAL(eve::powm1( T(2)          ,  eve::inf(eve::as<T>())  ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1( T(0.5)        ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1( T(2)          ,  eve::minf(eve::as<T>()) ) , T(-1 )        );
+  TTS_IEEE_EQUAL(eve::powm1(-T(0.5)        ,  eve::inf(eve::as<T>())  ) , T(-1 )        );
+  TTS_IEEE_EQUAL(eve::powm1(-T(2)          ,  eve::inf(eve::as<T>())  ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1(-T(0.5)        ,  eve::minf(eve::as<T>()) ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1(-T(2)          ,  eve::minf(eve::as<T>()) ) , T(-1)        );
+  TTS_IEEE_EQUAL(eve::powm1( eve::minf(eve::as<T>()), -T(3)           ) , T(-1)        );
+  TTS_IEEE_EQUAL(eve::powm1( eve::minf(eve::as<T>()), -T(4)           ) , T(-1)        );
+  TTS_IEEE_EQUAL(eve::powm1( eve::inf(eve::as<T>()) ,  T(4)           ) , eve::inf(eve::as<T>()) );
+  TTS_IEEE_EQUAL(eve::powm1( eve::inf(eve::as<T>()) , -T(4)           ) , T( -1)        );
 
-  if constexpr(!eve::real_scalar_value<T>)
+  using v_t =  eve::element_type_t<T>;
+  using w8_t =  eve::wide<v_t, eve::fixed<8>>;
+  auto Inf =  eve::inf(eve::as<v_t>());
   {
-    using v_t = eve::element_type_t<T>;
-    if constexpr(std::is_floating_point_v < v_t > )
-    {
-      using w8_t =  eve::wide<v_t, eve::fixed<8>>;
-      auto Inf =  eve::inf(eve::as<v_t>());
-      {
-        w8_t a(-0.0, -0.0,-0.0,-0.0,-1.0,-1.0, -Inf, -Inf);
-        w8_t b(-Inf, -3.0,-4.0,-4.5,-Inf, Inf, -3.0, -4.0);
-        w8_t r( Inf, -Inf, Inf, Inf, 1.0, 1.0, -0.0,  0.0);
-        w8_t c = (eve::powm1)(a, b);
-        TTS_ULP_EQUAL(c ,dec(r),2);
-      }
-      {
-        using w4_t =  eve::wide<v_t, eve::fixed<4>>;
-        w4_t a(-Inf, -Inf, -Inf, 0.0);
-        w4_t b(-4.5,  4.0,  4.5, 0.0);
-        w4_t r( 0.0,  Inf,  Inf, 1.0);
-        w4_t c = (eve::powm1)(a, b);
-        TTS_ULP_EQUAL(c ,dec(r),2);
-      }
-    }
+    w8_t a(-0.0, -0.0,-0.0,-0.0,-1.0,-1.0, -Inf, -Inf);
+    w8_t b(-Inf, -3.0,-4.0,-4.5,-Inf, Inf, -3.0, -4.0);
+    w8_t r( Inf, -Inf, Inf, Inf, 1.0, 1.0, -0.0,  0.0);
+    w8_t c = (eve::powm1)(a, b);
+    TTS_ULP_EQUAL(c ,dec(r),2);
+  }
+  {
+    using w4_t =  eve::wide<v_t, eve::fixed<4>>;
+    w4_t a(-Inf, -Inf, -Inf, 0.0);
+    w4_t b(-4.5,  4.0,  4.5, 0.0);
+    w4_t r( 0.0,  Inf,  Inf, 1.0);
+    w4_t c = (eve::powm1)(a, b);
+    TTS_ULP_EQUAL(c ,dec(r),2);
   }
 };
