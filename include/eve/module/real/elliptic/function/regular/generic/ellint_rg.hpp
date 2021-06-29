2d74bd0c68286c2f7184bf70104bf0256a5f189c
jtlap <jtlapreste@gmail.com>
2021-06-29 21:15:11 +0200

    ellint_rc/d/f/g

--- include/eve/module/real/elliptic/function/regular/generic/ellint_rg.hpp
+++ include/eve/module/real/elliptic/function/regular/generic/ellint_rg.hpp
@@ -24,7 +24,7 @@
 namespace eve::detail
 {
   template<floating_real_value T, floating_real_value U,  floating_real_value V>
-  EVE_FORCEINLINE T ellint_rg_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE auto ellint_rg_(EVE_SUPPORTS(cpu_)
                               , T x
                               , U y
                               , V z) noexcept
@@ -34,7 +34,7 @@ namespace eve::detail
   }
 
   template<floating_real_value T, floating_real_value U,  floating_real_value V>
-  EVE_FORCEINLINE T ellint_rg_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE auto ellint_rg_(EVE_SUPPORTS(cpu_)
                               , raw_type const &
                               , T x
                               , U y
@@ -50,46 +50,36 @@ namespace eve::detail
                               , T x
                               , T y
                               , T z) noexcept
+  requires has_native_abi_v<T>
   {
-    if constexpr(has_native_abi_v<T>)
-    {
-      auto cond_swap = [](auto cond, auto &a,  auto &b){
-        auto aa = if_else(cond, a, b);
-        auto bb = if_else(cond, b, a);
-        a = aa;
-        b = bb;
-      };
-      cond_swap(x < y, x, y);
-      cond_swap(x < z, x, z);
-      cond_swap(y > z, y, z);
-      // now all(x >= z) and all(z >= y)
-      return (z * ellint_rf(x, y, z)
-              - (x-z)*(y-z)*ellint_rd(x, y, z)/3
-              + sqrt(x * y / z))*half(as(x));
-    }
-    else
-      return apply_over(raw(ellint_rg), x, y, z);
+    auto cond_swap = [](auto cond, auto &a,  auto &b){
+      auto aa = if_else(cond, a, b);
+      auto bb = if_else(cond, b, a);
+      a = aa;
+      b = bb;
+    };
+    cond_swap(x < y, x, y);
+    cond_swap(x < z, x, z);
+    cond_swap(y > z, y, z);
+    // now all(x >= z) and all(z >= y)
+    return (z * ellint_rf(x, y, z)
+            - (x-z)*(y-z)*ellint_rd(x, y, z)/3
+            + sqrt(x * y / z))*half(as(x));
   }
+
 
   template<floating_real_value T>
   EVE_FORCEINLINE T ellint_rg_(EVE_SUPPORTS(cpu_)
                               , T x
                               , T y
                               , T z) noexcept
+  requires has_native_abi_v<T>
   {
-    if constexpr(has_native_abi_v<T>)
-    {
-      auto r = nan(as(x));
-      auto notdone = is_nltz(x) && is_nltz(y) && is_nltz(z);
-      // any parameter nan or less than zero implies nan
-      auto br0 = [x, y, z](){return raw(ellint_rg)(x, y, z);};
-      last_interval(br0, notdone, r);
-      return r;
-    }
-     else
-      return apply_over(ellint_rg, x, y, z);
-   }
-
-
-
+    auto r = nan(as(x));
+    auto notdone = is_nltz(x) && is_nltz(y) && is_nltz(z);
+    // any parameter nan or less than zero implies nan
+    auto br0 = [x, y, z](){return raw(ellint_rg)(x, y, z);};
+    last_interval(br0, notdone, r);
+    return r;
+  }
 }
