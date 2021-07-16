91aa1629c02b7fcb2bea375317044c6009420431
jtlap <jtlapreste@gmail.com>
2021-07-16 12:59:35 +0200

    bit_ceil/floor

--- include/eve/function/bit_floor.hpp
+++ include/eve/function/bit_floor.hpp
@@ -39,8 +39,9 @@ namespace eve
   //!
   //! **Return value**
   //!
-  //!If `x` is not zero, calculates [element-wise](../../../glossary.html#element-wise)  the largest integral power of two that is not greater than `x`.
+  //!If `x` is not zero, computes [element-wise](../../../glossary.html#element-wise)  the largest integral power of two that is not greater than `x`.
   //!If an  [element](../../../glossary.html#element) of `x` is zero, the corresponding result  [element](../../../glossary.html#element) is zero.
+  //!
   //! ---
   //!
   //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
