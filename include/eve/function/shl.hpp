1efec3257603dd2e3b1957fb6098df87c870ec2c
jtlap <jtlapreste@gmail.com>
2021-07-17 10:50:10 +0200

    bit_shl bit_shr

--- include/eve/function/shl.hpp
+++ include/eve/function/shl.hpp
@@ -8,8 +8,6 @@
 #pragma once
 
 #include <eve/detail/overload.hpp>
-#include <eve/detail/assert_utils.hpp>
-#include <eve/assert.hpp>
 #include <type_traits>
 
 namespace eve
@@ -105,19 +103,6 @@ namespace eve
   }
 
   EVE_MAKE_CALLABLE(shl_, shl);
-
-  namespace detail
-  {
-    template<integral_value T, integral_value U>
-    EVE_FORCEINLINE auto shl_(EVE_SUPPORTS(cpu_), T a, U s) noexcept
-    {
-      if constexpr( scalar_value<T> && scalar_value<U> )  return  static_cast<T>(a << s);
-      else if constexpr( scalar_value<T>)
-      {
-        using w_t = as_wide_t<T, cardinal_t<U>>;
-                                                          return w_t(a) << s;
-      }
-      else                                                return a << s;
-    }
-  }
 }
+
+#include <eve/module/real/core/function/regular/generic/shl.hpp>
