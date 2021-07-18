98f46da0d48fb14f80cfed2bc664d3657784728d
jtlap <jtlapreste@gmail.com>
2021-07-18 13:53:26 +0200

    bit_ornot

--- include/eve/module/real/core/function/regular/generic/bit_ornot.hpp
+++ include/eve/module/real/core/function/regular/generic/bit_ornot.hpp
@@ -13,6 +13,7 @@
 #include <eve/function/bit_cast.hpp>
 #include <eve/function/bit_not.hpp>
 #include <eve/function/bit_and.hpp>
+#include <eve/function/bit_or.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/concept/compatible.hpp>
