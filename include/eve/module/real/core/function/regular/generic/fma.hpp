d20bfb481ca887142dce7378d44ee722ff5f6b88
jtlap <jtlapreste@gmail.com>
2021-07-20 10:09:48 +0200

    fma try

--- include/eve/module/real/core/function/regular/generic/fma.hpp
+++ include/eve/module/real/core/function/regular/generic/fma.hpp
@@ -45,7 +45,8 @@ namespace eve::detail
   EVE_FORCEINLINE auto fma_(EVE_SUPPORTS(cpu_), C const &cond, T const &a, U const &b, V const &c) noexcept
   requires properly_convertible<U, V, T>
   {
-    return mask_op(  cond, eve::fma, a, b, c);
+    using r_t =  common_compatible_t<T, U, V>;
+    return mask_op(  cond, eve::fma, r_t(a), r_t(b), r_t(c));
   }
 
 }
