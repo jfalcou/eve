844b2c02cea274bbf6c407de90110eafd61ec4f9
jtlap <jtlapreste@gmail.com>
2021-07-21 18:36:33 +0200

    restricting popcount to unsigned

--- test/unit/module/real/core/popcount.cpp
+++ test/unit/module/real/core/popcount.cpp
@@ -10,27 +10,25 @@
 #include <eve/constant/valmax.hpp>
 #include <eve/function/popcount.hpp>
 #include <bit>
-#include <eve/function/converter.hpp>
+#include <eve/function/bit_cast.hpp>
 //==================================================================================================
 // Types tests
 //==================================================================================================
 EVE_TEST_TYPES( "Check return types of eve::popcount(simd)"
-              , eve::test::simd::integers
+              , eve::test::simd::unsigned_integers
               )
 <typename T>(eve::as<T>)
 {
-  using i_t = eve::as_integer_t<T, unsigned>;
   using v_t = eve::element_type_t<T>;
-  using vi_t = eve::element_type_t<i_t>;
-  TTS_EXPR_IS( eve::popcount(T()), i_t  );
-  TTS_EXPR_IS( eve::popcount(v_t()), vi_t );
+  TTS_EXPR_IS( eve::popcount(T()), T  );
+  TTS_EXPR_IS( eve::popcount(v_t()), v_t );
 };
 
 //==================================================================================================
 // Tests for eve::popcount
 //==================================================================================================
 EVE_TEST( "Check behavior of eve::popcount(simd)"
-        , eve::test::simd::integers
+        , eve::test::simd::unsigned_integers
         , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
                               , eve::test::logicals(0, 3))
         )
@@ -41,5 +39,5 @@ EVE_TEST( "Check behavior of eve::popcount(simd)"
   using vi_t = eve::element_type_t<i_t>;
 
   TTS_EQUAL(eve::popcount(a0), map([](auto e) -> vi_t { return std::popcount(std::bit_cast<vi_t>(e)); }, a0));
-  TTS_EQUAL(eve::popcount[t](a0), eve::if_else(t, eve::popcount(a0), eve::uint_(a0)));
+  TTS_EQUAL(eve::popcount[t](a0), eve::if_else(t, eve::popcount(a0), a0));
 };
