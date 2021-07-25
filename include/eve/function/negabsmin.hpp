c012be38445c4cfd550ae8f3a6147f6d8e560886
jtlap <jtlapreste@gmail.com>
2021-07-25 17:33:48 +0200

    negabsmin/max

---
+++ include/eve/function/negabsmin.hpp
@@ -0,0 +1,113 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/detail/overload.hpp>
+
+namespace eve
+{
+  //================================================================================================
+  //! @addtogroup arithmetic
+  //! @{
+  //! @var negabsmin
+  //!
+  //! @brief Callable object computing the negabsmin operation.
+  //!
+  //! **Required header:** `#include <eve/function/negabsmin.hpp>`
+  //!
+  //! #### Members Functions
+  //!
+  //! | Member       | Effect                                                     |
+  //! |:-------------|:-----------------------------------------------------------|
+  //! | `operator()` | the negabsmin operation   |
+  //! | `operator[]` | Construct a conditional version of current function object |
+  //!
+  //! ---
+  //!
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+  //!  template< value T, value ...Ts> auto operator()( T x,Ts... args ) const noexcept
+  //!                                       requires (compatible_values< T, Ts > && ...);
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!
+  //! **Parameters**
+  //!
+  //!`x`, `args`:   [values](../../concepts.html#value)
+  //!
+  //! **Return value**
+  //!
+  //!the greatest absolute value is returned.
+  //!
+  //!For instance for two elemnts:
+  //!
+  //!  * If `x >  y`,  `-|x|` is returned.
+  //!  * If `x <  y`,  `-|y|` is returned.
+  //!  * Otherwise `-max(|x|, |y|)` is returned.
+  //!
+  //!For n parameters the result is computed as if this scheme was recursively used.
+  //!
+  //!The result type is the [compatibility result](../../concept.html#compatibility) of the parameters.
+  //!
+  //!@warning
+  //!   If any element of the inputs is a `Nan`, the corresponding output element is system-dependent.
+  //!
+  //! ---
+  //!
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+  //!  auto operator[]( conditional_expression auto cond ) const noexcept;
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!
+  //!  Higher-order function generating a masked version of eve::negabsmin
+  //!
+  //!  **Parameters**
+  //!
+  //!  `cond` : conditional expression
+  //!
+  //!  **Return value**
+  //!
+  //!  A Callable object so that the expression `negabsmin[cond](x, ...)` is equivalent to `if_else(cond,negabsmin(x, ...),x)`
+  //!
+  //! ---
+  //!
+  //! #### Supported decorators
+  //!
+  //!  * `pedantic`
+  //!
+  //!     **Required header:**  #include <eve/function/pedantic/negabsmin.hpp>
+  //!
+  //!     The behaviour is the same except that if  `|x|` or `|y|` is a nan, `pedantic(max)` is used.
+  //!
+  //!  * `numeric`
+  //!
+  //!     **Required header:**  #include <eve/function/numeric/negabsmin.hpp>
+  //!
+  //!      The behaviour is the same except that if   `|x|` or `|y|` is a nan, `numeric(max)` is used.
+  //!
+  //!  * `diff`
+  //!
+  //!     **Required header:**  #include <eve/function/diff/negabsmin.hpp>
+  //!
+  //!     The expression `diff< N >(negabsmin)(x,args,...)` computes the partial
+  //!      derivative relative to the Nth parameter. If the parameters are \f$x_1, ..., x_n\f$ and
+  //!      their negabsmin is \f$m\f$, the value returned is elementwise \f$\-mathrm{sign}(x_N)\f$ if \f$m\f$ is equal to \f$|x_N|\f$ else 0.
+  //!
+  //! #### Example
+  //!
+  //! [**See it live on Compiler Explorer**](https://godbolt.org/z/TODO)
+  //!
+  //! @include{lineno} doc/core/negabsmin.cpp
+  //!
+  //!  @}
+  //================================================================================================
+  EVE_MAKE_CALLABLE(negabsmin_, negabsmin);
+}
+
+#include <eve/module/real/core/function/regular/generic/negabsmin.hpp>
+
+#if defined(EVE_INCLUDE_X86_HEADER)
+#  include <eve/module/real/core/function/regular/simd/x86/negabsmin.hpp>
+#endif
---
+++ include/eve/module/real/core/function/regular/generic/negabsmin.hpp
@@ -0,0 +1,66 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/compatible.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/apply_over.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/detail/skeleton_calls.hpp>
+#include <eve/function/abs.hpp>
+#include <eve/function/if_else.hpp>
+#include <eve/function/is_nan.hpp>
+#include <eve/function/is_not_greater_equal.hpp>
+#include <eve/function/max.hpp>
+#include <eve/traits/common_compatible.hpp>
+
+#include <type_traits>
+
+namespace eve::detail
+{
+  template<real_value T, real_value U>
+  EVE_FORCEINLINE auto negabsmin_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept
+      requires compatible_values<T, U>
+  {
+    return arithmetic_call(negabsmin, a, b);
+  }
+
+  template<real_value T>
+  EVE_FORCEINLINE auto negabsmin_(EVE_SUPPORTS(cpu_), T const &a, T const &b) noexcept
+  requires has_native_abi_v<T>
+  {
+    return  -eve::abs(eve::min(a, b));
+  }
+
+  //================================================================================================
+  // Masked case
+  //================================================================================================
+  template<decorator D, conditional_expr C, real_value U, real_value V>
+  EVE_FORCEINLINE auto negabsmin_(EVE_SUPPORTS(cpu_), C const &cond, D const &
+                              , U const &t, V const &f) noexcept
+      requires compatible_values<U, V>
+  {
+    return mask_op(  cond, D()(eve::negabsmin), t, f);
+  }
+
+  template<conditional_expr C, real_value U, real_value V>
+  EVE_FORCEINLINE auto negabsmin_(EVE_SUPPORTS(cpu_), C const &cond, U const &t, V const &f) noexcept
+      requires compatible_values<U, V>
+  {
+    return mask_op(  cond, eve::negabsmin, t, f);
+  }
+
+  //================================================================================================
+  // N parameters
+  //================================================================================================
+  template<real_value T0, real_value T1, real_value ...Ts>
+  common_compatible_t<T0,T1,Ts...> negabsmin_(EVE_SUPPORTS(cpu_), T0 a0, T1 a1, Ts... args)
+  {
+    return eve::abs(eve::min(a0, a1, args...));
+  }
+}
