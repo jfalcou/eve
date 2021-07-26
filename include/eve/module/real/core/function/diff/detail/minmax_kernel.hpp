3535cb9b84febc38707669ce7bfb728147946ab4
jtlap <jtlapreste@gmail.com>
2021-07-26 11:58:21 +0200

    suppressing comments in minmax_kernel

--- include/eve/module/real/core/function/diff/detail/minmax_kernel.hpp
+++ include/eve/module/real/core/function/diff/detail/minmax_kernel.hpp
@@ -89,41 +89,4 @@ namespace eve::detail
       }
     }
   }
-
-//   template<int N, typename F, typename T0, typename T1, typename... Ts>
-//   auto minmaxabs_kernel(F const & op, T0 arg0, T1 arg1, Ts... args) noexcept
-//   {
-//     using r_t = common_compatible_t<T0,T1, Ts...>;
-//     auto getNth = []<std::size_t... I>(std::index_sequence<I...>, auto& that, auto... vs)
-//       {
-//         auto iff = [&that]<std::size_t J>(auto val, std::integral_constant<std::size_t,J>)
-//         {
-//           if constexpr(J==N) that = val;
-//         };
-
-//         ((iff(vs, std::integral_constant<std::size_t,I+3>{})),...);
-//       };
-//     if constexpr(N > sizeof...(Ts)+2)
-//     {
-//       return zero(as<r_t >());
-//     }
-//     else
-//     {
-//       auto m = op(arg0, arg1, args...);
-//       if constexpr(N==1)
-//       {
-//         return if_else(m == eve::abs(arg0), eve::sign(arg0), zero);
-//       }
-//       else if constexpr(N==2)
-//       {
-//         return  if_else(m == eve::abs(arg1), eve::sign(arg1), zero);
-//       }
-//       else
-//       {
-//         r_t that(0);
-//         getNth(std::make_index_sequence<sizeof...(args)>{},that,args...);
-//         return if_else(m == eve::abs(that), eve::sign(that), zero);
-//       }
-//     }
-//   }
 }
