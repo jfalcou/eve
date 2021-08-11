c2d3e98405501fd50eb0aac75ad84f6be233e0a0
jtlap <jtlapreste@gmail.com>
2021-08-11 15:53:30 +0200

    roundings

--- include/eve/function/roundings.hpp
+++ include/eve/function/roundings.hpp
@@ -16,25 +16,25 @@ namespace eve
   struct upward_
   {
     template<typename D> static constexpr auto combine( D const& ) noexcept =delete;
-     static const  auto value = _MM_FROUND_TO_POS_INF | _MM_FROUND_NO_EXC;
+    static const  auto value = 0x02 | 0x08; //_MM_FROUND_TO_POS_INF | _MM_FROUND_NO_EXC;
   };
 
   struct downward_
   {
     template<typename D> static constexpr auto combine( D const& ) noexcept =delete;
-     static const auto value = _MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC;
+     static const auto value = 0x01 | 0x08; //_MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC;
   };
 
   struct to_nearest_
   {
     template<typename D> static constexpr auto combine( D const& ) noexcept =delete;
-    static const auto value = _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC;
+    static const auto value = 0x00 | 0x08; // _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC;
   };
 
   struct toward_zero_
   {
     template<typename D> static constexpr auto combine( D const& ) noexcept =delete;
-    static const auto value = _MM_FROUND_TO_ZERO |_MM_FROUND_NO_EXC;
+    static const auto value = 0x03 | 0x08; // _MM_FROUND_TO_ZERO |_MM_FROUND_NO_EXC;
   };
 
   using upward_type      = decorated<upward_()>;
