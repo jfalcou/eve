7011a3c3293bbe3070ea9eae1082d7f933dfd4b1
jtlap <jtlapreste@gmail.com>
2021-08-11 15:32:43 +0200

    fracscale/roundscale

---
+++ include/eve/function/fracscale.hpp
@@ -0,0 +1,107 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/detail/overload.hpp>
+#include <type_traits>
+
+namespace eve
+{
+  //================================================================================================
+  //! @addtogroup arithmetic
+  //! @{
+  //! @var fracscale
+  //!
+  //! @brief Callable object computing the square root.
+  //!
+  //! **Required header:** `#include <eve/function/fracscale.hpp>`
+  //!
+  //! #### Members Functions
+  //!
+  //! | Member       | Effect                                                     |
+  //! |:-------------|:-----------------------------------------------------------|
+  //! | `operator()` | the  computation of the fractional scaled part             |
+  //! | `operator[]` | Construct a conditional version of current function object |
+  //!
+  //! ---
+  //!
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+  //!  auto operator()(floating_value auto x, int scale) const noexcept;
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!
+  //! **Parameters**
+  //!
+  //!`x`:      [floating real value](../../concepts.html#value).
+  //!`scale` : int or std::integral_constant of int type limited to the range [0, 15].
+  //!
+  //! **Return value**
+  //!
+  //! Returns the [element-wise](../../../glossary.html#elment-wise) reduced part of the scaled input.
+  //! The number of fraction bits retained is specified by scale. By default the internal rounding after scaling is done to nearest integral.
+  //! The call is equivalent to `a0-roundscal(a0, scale)`
+  //!
+  //! ---
+  //!
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+  //!  auto operator[]( conditional_expression auto cond ) const noexcept;
+  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!
+  //!  Higher-order function generating a masked version of eve::fracscale
+  //!
+  //!  **Parameters**
+  //!
+  //!  `cond` : conditional expression
+  //!
+  //!  **Return value**
+  //!
+  //!  A Callable object so that the expression `fracscale[cond](x, ...)` is equivalent to `if_else(cond,fracscale(x, ...),x)`
+  //!
+  //! ---
+  //!
+  //! #### Supported decorators
+  //!
+  //!  * `to_nearest, toward_zero, upward,  downward`
+  //!     If xxx is one of these 4 decorators
+  //!     The call `xxx(fracscale)(x)`, call is equivalent to  `a0-xxx(roundscale)(a0, scale)`
+  //!
+  //! #### Example
+  //!
+  //! [**See it live on Compiler Explorer**](https://godbolt.org/z/TODO)
+  //!
+  //! @include{lineno} doc/core/fracscale.cpp
+  //!
+  //!  @}
+  //================================================================================================
+  namespace tag
+  {
+    struct fracscale_;
+  }
+
+  namespace detail
+  {
+    template<typename T>
+    EVE_FORCEINLINE void check(EVE_MATCH_CALL(eve::tag::fracscale_), T const &, [[maybe_unused]] int s)
+    {
+        EVE_ASSERT(s >= 0 && s < 16, "[eve::fracscale] -  parameter s out of range [0, 15]: " << s);
+    }
+    template<int S, typename T>
+    EVE_FORCEINLINE void check(EVE_MATCH_CALL(eve::tag::fracscale_), T const &, std::integral_constant<int, S> const & )
+    {
+        EVE_ASSERT(S >= 0 && S < 16, "[eve::fracscale] -  integral constant out of range [0, 15]: " << S);
+    }
+  }
+
+  EVE_MAKE_CALLABLE(fracscale_, fracscale);
+}
+
+#include <eve/arch.hpp>
+#include <eve/module/real/core/function/regular/generic/fracscale.hpp>
+
+// #if defined(EVE_INCLUDE_X86_HEADER)
+// #  include <eve/module/real/core/function/regular/simd/x86/fracscale.hpp>
+// #endif
---
+++ include/eve/module/real/core/function/regular/generic/fracscale.hpp
@@ -0,0 +1,83 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/detail/has_abi.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/function/ldexp.hpp>
+#include <eve/function/round.hpp>
+#include <eve/concept/value.hpp>
+
+namespace eve::detail
+{
+  template<int S, real_value T>
+  EVE_FORCEINLINE constexpr T fracscale_(EVE_SUPPORTS(cpu_)
+                                         , T const &a0, std::integral_constant<int, S> ) noexcept
+  {
+    return fracscale(a0, S);
+  }
+
+  template<real_value T>
+  EVE_FORCEINLINE constexpr T fracscale_(EVE_SUPPORTS(cpu_)
+                                  , T const &a0, int scale) noexcept
+  {
+    if constexpr(has_native_abi_v<T>)
+    {
+      return a0-ldexp(nearest(ldexp(a0, scale)), -scale);
+    }
+    else
+    {
+      return apply_over(fracscale, a0, scale);
+    }
+  }
+
+  template<int S, decorator D, real_value T>
+  EVE_FORCEINLINE constexpr T fracscale_(EVE_SUPPORTS(cpu_)
+                                  , D const &
+                                  , T const &a0, std::integral_constant<int, S> ) noexcept
+  requires(is_one_of<D>(types<toward_zero_type, downward_type, to_nearest_type, upward_type> {}))
+  {
+    return D()(fracscale)(a0, S);
+  }
+
+  template<decorator D, real_value T>
+  EVE_FORCEINLINE constexpr T fracscale_(EVE_SUPPORTS(cpu_)
+                                  , D const &
+                                  , T const &a0, int scale) noexcept
+  requires(is_one_of<D>(types<toward_zero_type, downward_type, to_nearest_type, upward_type> {}))
+  {
+    if constexpr(has_native_abi_v<T>)
+    {
+      return a0-ldexp(D()(round)(ldexp(a0, scale)), -scale);
+    }
+    else
+    {
+      return apply_over(fracscale, D(), a0, scale);
+    }
+  }
+
+  // -----------------------------------------------------------------------------------------------
+  // Masked case
+  template<conditional_expr C, real_value U>
+  EVE_FORCEINLINE auto fracscale_(EVE_SUPPORTS(cpu_), C const &cond, U const &t, int scale) noexcept
+  {
+    return mask_op(  cond, eve::fracscale, t, scale);
+  }
+
+
+  //================================================================================================
+  // Rounded masked case
+  //================================================================================================
+  template<conditional_expr C, decorator D, real_value T>
+  EVE_FORCEINLINE T fracscale_(EVE_SUPPORTS(cpu_), C const &cond, D const &, T a, int scale) noexcept
+  requires  has_native_abi_v<T>
+  && (is_one_of<D>(types<toward_zero_type, downward_type, to_nearest_type, upward_type> {}))
+  {
+    return mask_op( cond, D()(eve::fracscale), a, scale);
+  }
+}
