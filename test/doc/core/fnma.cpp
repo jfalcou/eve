e100675b0b3b9a6700d27cccc36a5bfa741e163f
jtlap <jtlapreste@gmail.com>
2021-08-12 16:12:39 +0200

    fnma fnms fanm fsnm

--- test/doc/core/fnma.cpp
+++ test/doc/core/fnma.cpp
@@ -15,9 +15,10 @@ int main()
     << "<- pf                          = " << pf << '\n'
     << "<- qf                          = " << qf << '\n'
     << "<- rf                          = " << rf << '\n'
-    << "-> diff_1st(fnma)(pf, qf, rf) = " << eve::diff_1st(eve::fnma)(pf, qf, rf) << '\n'
-    << "-> diff_2nd(fnma)(pf, qf, rf) = " << eve::diff_2nd(eve::fnma)(pf, qf, rf) << '\n'
-    << "-> diff_3rd(fnma)(pf, qf, rf) = " << eve::diff_3rd(eve::fnma)(pf, qf, rf) << '\n';
+    << "-> fnma(pf, qf, rf)            = " << eve::fnma(pf, qf, rf) << '\n'
+    << "-> diff_1st(fnma)(pf, qf, rf)  = " << eve::diff_1st(eve::fnma)(pf, qf, rf) << '\n'
+    << "-> diff_2nd(fnma)(pf, qf, rf)  = " << eve::diff_2nd(eve::fnma)(pf, qf, rf) << '\n'
+    << "-> diff_3rd(fnma)(pf, qf, rf)  = " << eve::diff_3rd(eve::fnma)(pf, qf, rf) << '\n';
 
 
   float xf = 0.5f;
@@ -25,9 +26,10 @@ int main()
   float zf = 0.1f;
   std::cout
     << "---- scalar"  << '\n'
-    << "<- xf                      = " << xf << '\n'
-    << "<- yf                      = " << yf << '\n'
-    << "<- zf                      = " << yf << '\n'
+    << "<- xf                         = " << xf << '\n'
+    << "<- yf                         = " << yf << '\n'
+    << "<- zf                         = " << yf << '\n'
+    << "-> fnma(xf, yf, zf)           = " << eve::fnma(xf, yf, zf) << '\n'
     << "-> diff_1st(fnma)(xf, yf, zf) = " << eve::diff_1st(eve::fnma)(xf, yf, zf) << '\n'
     << "-> diff_2nd(fnma)(xf, yf, zf) = " << eve::diff_2nd(eve::fnma)(xf, yf, zf) << '\n'
     << "-> diff_3rd(fnma)(xf, yf, zf) = " << eve::diff_3rd(eve::fnma)(xf, yf, zf) << '\n';
