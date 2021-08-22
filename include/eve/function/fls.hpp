287face0c48a6eacc1bc8e6bc08b24a30bd46b77
jtlap <jtlapreste@gmail.com>
2021-08-22 13:09:01 +0200

    :pencil2: More fixes in documentation : F Files

--- include/eve/function/fls.hpp
+++ include/eve/function/fls.hpp
@@ -31,12 +31,12 @@ namespace eve
   //! ---
   //!
   //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-  //!  template< unsigned_value T > auto operator()( T x ) const noexcept;
+  //!  auto operator()( unsigned_value auto x ) const noexcept;
   //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //!
   //! **Parameters**
   //!
-  //!`x`:   [unsigned value](@ref eve::value).
+  //!`x`:   [unsigned value](@ref eve::unsigned_value).
   //!
   //! **Return value**
   //!
@@ -67,7 +67,7 @@ namespace eve
   //!  * eve::pedantic
   //!
   //!     **Required header:** `#include <eve/function/pedantic/abs.hpp>`
-  //!  
+  //!
   //!     The expression `pedantic(fls)(x)` returns the size of the element type (an out of range index) if
   //!      the input is zero.
   //!
