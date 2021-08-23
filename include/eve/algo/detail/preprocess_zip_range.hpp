fe5b96e79e515fbb7628c8e712fe86794332473f
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-08-23 22:58:57 +0100

    FIX-719: zip_iterator_common improvements

---
+++ include/eve/algo/detail/preprocess_zip_range.hpp
@@ -0,0 +1,56 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/detail/kumi.hpp>
+
+namespace eve::algo
+{
+  template <typename ...Is>
+  struct zip_iterator;
+
+  namespace detail
+  {
+    template<typename Ranges>
+    struct preprocessed_zip_result
+    {
+      Ranges ranges;
+
+      auto traits() const { return get<0>(ranges).traits(); }
+
+      auto begin() const
+      {
+        return zip_iterator(kumi::map([](auto r) { return r.begin(); }, ranges));
+      }
+
+      auto end() const
+      {
+        return zip_iterator(kumi::map([](auto r) { return r.end(); }, ranges));
+      }
+
+      template<typename I> auto to_output_iterator(I i) const
+      {
+        return zip_iterator(
+            kumi::map([](auto r_i, auto i_i) { return r_i.to_output_iterator(i_i); }, ranges, i));
+      }
+    };
+
+    template <typename Ranges>
+    preprocessed_zip_result(Ranges) -> preprocessed_zip_result<Ranges>;
+
+    template <typename Traits, typename ...Rngs>
+    auto preprocess_zip_range(Traits traits, kumi::tuple<Rngs...> rngs) {
+      return preprocessed_zip_result{
+        kumi::map([traits](auto rng) { return preprocess_range(traits, rng); }, rngs)
+      };
+    }
+
+
+  }
+
+}
--- test/unit/algo/preprocess_zip_range.cpp
+++ test/unit/algo/preprocess_zip_range.cpp
@@ -18,25 +18,87 @@
 
 #include <vector>
 
-TTS_CASE("zip_iterator, preprocess range")
+TTS_CASE("zip_iterator, preprocess range, scalar end")
 {
   std::vector<int> v1{1, 2, 3};
   std::vector<int> v2{1, 2, 4};
-  eve::algo::zip_iterator zf{v1.begin(), v2.begin()};
+
+  using v_i = std::vector<int>::iterator;
+  using zip_vi = eve::algo::zip_iterator<v_i, v_i>;
+
+  using N = eve::fixed<eve::expected_cardinal_v<int>>;
+  using ui_it = eve::algo::unaligned_ptr_iterator<int, N>;
+  using zip_ui = eve::algo::zip_iterator<ui_it, ui_it>;
+
+  zip_vi zf{v1.begin(), v2.begin()};
 
   auto processed = eve::algo::preprocess_range(eve::algo::traits{}, eve::algo::as_range(zf, v1.end()));
 
-  using ui_it = eve::algo::unaligned_ptr_iterator<int, eve::fixed<eve::expected_cardinal_v<int>>>;
-  using zip_ui = eve::algo::zip_iterator<ui_it, ui_it>;
   zip_ui processed_f = processed.begin();
   zip_ui processed_l = processed.end();
 
   TTS_EQUAL((processed_l - processed_f), 3);
 
-  using v_i = std::vector<int>::iterator;
-  using zip_vi = eve::algo::zip_iterator<v_i, v_i>;
   zip_vi back_zf = processed.to_output_iterator(processed_f);
   TTS_EQUAL(back_zf, zf);
+}
+
+TTS_CASE("zip_iterator, preprocess range, zip end")
+{
+  using u      = int const*;
+  using a      = eve::aligned_ptr<int const>;
+  using zip_au = eve::algo::zip_iterator<a, u>;
+  using zip_uu = eve::algo::zip_iterator<u, u>;
+
+  using N = eve::fixed<eve::expected_cardinal_v<int>>;
+  using a_it = eve::algo::aligned_ptr_iterator  <int const, N>;
+  using u_it = eve::algo::unaligned_ptr_iterator<int const, N>;
+  using zip_au_it = eve::algo::zip_iterator<a_it, u_it>;
+  using zip_uu_it = eve::algo::zip_iterator<u_it, u_it>;
+
+  // au_uu
+  {
+    using zip_au_uu_rng = decltype(eve::algo::as_range(zip_au{}, zip_uu{}));
+
+    using processed =
+      decltype(eve::algo::preprocess_range(eve::algo::traits{}, zip_au_uu_rng{}));
+
+    TTS_TYPE_IS(decltype(std::declval<processed>().begin()), zip_au_it);
+    TTS_TYPE_IS(decltype(std::declval<processed>().end()),   zip_uu_it);
+  }
+
+  // au_au
+  {
+    using zip_au_au_rng = decltype(eve::algo::as_range(zip_au{}, zip_au{}));
+
+    using processed =
+      decltype(eve::algo::preprocess_range(eve::algo::traits{}, zip_au_au_rng{}));
+
+    TTS_TYPE_IS(decltype(std::declval<processed>().begin()), zip_au_it);
+    TTS_TYPE_IS(decltype(std::declval<processed>().end()),   zip_au_it);
+  }
+
+  // au_u
+  {
+    using zip_au_u_rng = decltype(eve::algo::as_range(zip_au{}, u{}));
+
+    using processed =
+      decltype(eve::algo::preprocess_range(eve::algo::traits{}, zip_au_u_rng{}));
+
+    TTS_TYPE_IS(decltype(std::declval<processed>().begin()), zip_au_it);
+    TTS_TYPE_IS(decltype(std::declval<processed>().end()),   zip_uu_it);
+  }
+
+  // au_a
+  {
+    using zip_au_a_rng = decltype(eve::algo::as_range(zip_au{}, a{}));
+
+    using processed =
+      decltype(eve::algo::preprocess_range(eve::algo::traits{}, zip_au_a_rng{}));
+
+    TTS_TYPE_IS(decltype(std::declval<processed>().begin()), zip_au_it);
+    TTS_TYPE_IS(decltype(std::declval<processed>().end()),   zip_au_it);
+  }
 }
 
 TTS_CASE("missmatch prototype")
