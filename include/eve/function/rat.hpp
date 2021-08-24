ead020eeb866fbaf2afa0a0615466da5dddea286
jtlap <jtlapreste@gmail.com>
2021-08-24 16:17:28 +0200

    rat

--- include/eve/function/rat.hpp
+++ include/eve/function/rat.hpp
@@ -24,7 +24,7 @@ namespace eve
   //!
   //! | Member       | Effect                                                     |
   //! |:-------------|:-----------------------------------------------------------|
-  //! | `operator()` | the computation of the rational approximation   |
+  //! | `operator()` | the computation of the rational approximation              |
   //!
   //! ---
   //!
@@ -38,7 +38,7 @@ namespace eve
   //! **Parameters**
   //!
   //!`x`:   [floating_real_value](@ref eve::value).
-  //!`tol`: [floating_real_value](@ref eve::value) default to T(1.0e-6)*eve::abs(x).
+  //!`tol`: [floating_real_value](@ref eve::value). By  default `tol` is equal to `T(1.0e-6)*eve::abs(x)`.
   //!
   //! **Return values**
   //!
