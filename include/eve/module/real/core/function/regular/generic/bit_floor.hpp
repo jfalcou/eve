b614277f22dc73e5a7f0dce14a127e0071c230b6
jtlap <jtlapreste@gmail.com>
2021-08-28 16:00:51 +0200

    proper generic algos for <bit> operation. Must bench to see if map is not sometimes better

--- include/eve/module/real/core/function/regular/generic/bit_floor.hpp
+++ include/eve/module/real/core/function/regular/generic/bit_floor.hpp
@@ -10,7 +10,9 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/constant/one.hpp>
 #include <eve/constant/zero.hpp>
+#include <eve/function/bit_cast.hpp>
 #include <eve/function/bit_shr.hpp>
+#include <eve/function/bit_width.hpp>
 #include <eve/function/dec.hpp>
 #include <eve/function/if_else.hpp>
 #include <eve/function/ifrexp.hpp>
@@ -23,7 +25,7 @@
 namespace eve::detail
 {
   template<real_value T>
-  EVE_FORCEINLINE auto bit_floor_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE T bit_floor_(EVE_SUPPORTS(cpu_)
                                  , T const &v) noexcept
   {
     if constexpr(has_native_abi_v<T>)
@@ -40,17 +42,19 @@ namespace eve::detail
       }
       else
       {
-        using elt_t =  element_type_t<T>;
-        auto a = v;
-        a |= bit_shr(a, 1);
-        a |= bit_shr(a, 2);
-        a |= bit_shr(a, 4);
-        if constexpr( sizeof(elt_t) >= 2 )  a |= bit_shr(a,  8);
-        if constexpr( sizeof(elt_t) >= 4 )  a |= bit_shr(a, 16);
-        if constexpr( sizeof(elt_t) >= 8 )  a |= bit_shr(a, 32);
-        a -= (a >> 1);
-        if constexpr(scalar_value<T>) return a;
-        else                          return if_else(vlt1, eve::zero, a);
+        using u_t = as_integer_t<T, unsigned>;
+        return if_else(is_eqz(v), zero, T{1} << dec(bit_width(bit_cast(v, as<u_t>()))));
+//         using elt_t =  element_type_t<T>;
+//         auto a = v;
+//         a |= bit_shr(a, 1);
+//         a |= bit_shr(a, 2);
+//         a |= bit_shr(a, 4);
+//         if constexpr( sizeof(elt_t) >= 2 )  a |= bit_shr(a,  8);
+//         if constexpr( sizeof(elt_t) >= 4 )  a |= bit_shr(a, 16);
+//         if constexpr( sizeof(elt_t) >= 8 )  a |= bit_shr(a, 32);
+//         a -= (a >> 1);
+//         if constexpr(scalar_value<T>) return a;
+//         else                          return if_else(vlt1, eve::zero, a);
       }
     }
     else return apply_over(bit_floor, v);
--- include/eve/module/real/core/function/regular/generic/popcount.hpp
+++ include/eve/module/real/core/function/regular/generic/popcount.hpp
@@ -25,23 +25,48 @@ namespace eve::detail
 {
   template<unsigned_value T>
   EVE_FORCEINLINE auto popcount_(EVE_SUPPORTS(cpu_)
-                                , T const &v) noexcept
+                                , T const &x) noexcept
   {
     using r_t = as_integer_t<T, unsigned>;
     if constexpr(has_native_abi_v<T>)
     {
       if constexpr(scalar_value<T>)
       {
-        return r_t(std::popcount(v));
+        return r_t(std::popcount(x));
       }
       else
       {
-        return map(eve::popcount, v);
+//        return map(eve::popcount, v);
+        constexpr auto siz = sizeof(eve::element_type_t<T>)*8;
+        if constexpr(siz == 8)
+        {
+          x -= ((x >> 1) & T(0x55));
+          x = ((x >> 2) & T(0x33)) + (x & T(0x33));
+          return ((x + (x >> 4)) & T(0xF));
+        }
+        else if constexpr(siz <= 16)
+        {
+          x -= ((x >> 1) & T(0x5555));
+          x = ((x >> 2) & T(0x3333)) + (x & T(0x3333));
+          return ((((x + (x >> 4)) & T(0xF0F)) * T(0x101)) >> 8);
+        }
+        else if constexpr(siz == 32)
+        {
+          x -= ((x >> 1) & T(0x55555555));
+          x = ((x >> 2) & T(0x33333333)) + (x & T(0x33333333));
+          return ((((x + (x >> 4)) & T(0xF0F0F0F)) * T(0x1010101)) >> 24);
+        }
+        else if constexpr(siz == 64)
+        {
+          x -= (x >> 1) & T(0x5555555555555555ULL);
+          x = ((x >> 2) & T(0x3333333333333333ULL)) + (x & T(0x3333333333333333ULL));
+          return ((((x + (x >> 4)) & T(0xF0F0F0F0F0F0F0FULL)) * T(0x101010101010101ULL)) >> 56);
+        }
       }
     }
     else
     {
-      return apply_over(popcount, v);
+      return apply_over(popcount, x);
     }
   }
 
