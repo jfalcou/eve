f8872736243837ba02f1e2892af54d20efb2b087
jtlap <jtlapreste@gmail.com>
2021-08-30 17:22:06 +0200

    :sparkles: Improve laguerre functions API

--- include/eve/module/real/polynomial/function/diff/generic/laguerre.hpp
+++ include/eve/module/real/polynomial/function/diff/generic/laguerre.hpp
@@ -9,32 +9,33 @@
 
 #include <eve/function/laguerre.hpp>
 #include <eve/function/dec.hpp>
+#include <eve/function/if_else.hpp>
+#include <eve/function/inc.hpp>
+#include <eve/function/cospi.hpp>
 #include <eve/function/derivative.hpp>
 #include <eve/detail/apply_over.hpp>
-// temporary measure
-#include <eve/detail/diff_div.hpp>
-// temporary measure
 
 namespace eve::detail
 {
 
-  template<integral_value N, floating_real_value T>
+  template<integral_real_value N, floating_value T>
   EVE_FORCEINLINE constexpr T laguerre_(EVE_SUPPORTS(cpu_)
                                   , diff_type<1> const &
                                   , N const & n
-                                  , T const &x) noexcept
+                                       , T const &x) noexcept
   requires index_compatible_values<N, T>
   {
-    // TODO
-    // as there is no available closed formule for laguerre derivative at the first order
-    // will use vand algo as soon as complex will be available ie:
-    //     complex<T> cx = {x, eps(as(x))};
-    //     return = imag(laguerre(cx))/eps(as(x));
-    // and will tested in time
-    auto df2 = [](auto n, auto x){
-      auto y =  [&n](auto a){ return eve::laguerre(n, a);  };
-      return eve::detail::centered_diffdiv(y, x);
-    };
-    return df2(n, x); // this is not accurate
+    return -laguerre(1, n, x);
+  }
+
+  template<integral_real_value M, integral_real_value N, floating_value T>
+  EVE_FORCEINLINE constexpr T laguerre_(EVE_SUPPORTS(cpu_)
+                                       , diff_type<1> const &
+                                       , M const & m
+                                       , N const & n
+                                       , T const &x) noexcept
+  requires index_compatible_values<N, T> && index_compatible_values<M, T>
+  {
+    return if_else(is_eqz(n), zero, cospi(m)*laguerre(inc(m), saturated(dec)(n), x));
   }
 }
