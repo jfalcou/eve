dc24d9488edb1ca9cd8d03c518ea2cd9d8969d6f
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-09-02 14:15:26 +0100

    FIX-896: value_type_t

---
+++ include/eve/algo/concepts/value_type.hpp
@@ -0,0 +1,37 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/algo/concepts/relaxed.hpp>
+
+#include <eve/traits.hpp>
+
+#include <type_traits>
+
+namespace eve::algo
+{
+  namespace detail
+  {
+    template <typename T>
+    constexpr auto value_type_impl()
+    {
+           if constexpr ( relaxed_range<T>            ) return value_type_impl<decltype(std::declval<T>().begin())>();
+      else if constexpr ( std::contiguous_iterator<T> ) return std::type_identity<typename std::iterator_traits<T>::value_type>{};
+      else                                              return std::type_identity<typename eve::pointer_traits<T>::value_type>{};
+    }
+  }
+
+  template <typename T>
+  struct value_type
+  {
+    using type = typename decltype(detail::value_type_impl<T>())::type;
+  };
+
+  template <typename T>
+  using value_type_t = typename value_type<T>::type;
+}
