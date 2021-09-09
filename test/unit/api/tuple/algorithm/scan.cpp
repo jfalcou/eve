a35ca8848989e64f2a2bebe475fa0d12cefd5560
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-09-09 23:23:32 +0100

    FIX-894: scan bundle test

---
+++ test/unit/api/tuple/algorithm/scan.cpp
@@ -0,0 +1,36 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+
+#include "test.hpp"
+
+#include <eve/function/scan.hpp>
+
+EVE_TEST_TYPES( "Check behavior of scan", eve::test::scalar::all_types)
+<typename T>(eve::as<T>)
+{
+  using s_t = kumi::tuple<std::int8_t,T,double>;
+  using w_t = eve::wide<s_t>;
+
+  w_t x = [](auto i, auto) { return  s_t { static_cast<std::int8_t>(65+i)
+                                          , static_cast<T>(i + 1)
+                                          , 1.5*(1+i)
+                                          };
+                            };
+
+  w_t expected = kumi::map(eve::scan, x);
+
+  auto plus = [](auto a, auto b) {
+    return kumi::map(eve::plus, a, b);
+  };
+
+  w_t actual_1 = eve::scan(x, plus, eve::zero);
+  TTS_EQUAL(expected, actual_1);
+
+  w_t actual_2 = eve::scan(x, plus, s_t{0});
+  TTS_EQUAL(expected, actual_2);
+};
