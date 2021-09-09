0e03b7c432f5509922d40bf2b368f174de0e0556
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-09-09 09:20:56 +0100

    FIX-894: slide right for 2 wides. (#926)
    * FIX-894: slide right 2
    
    * FIX-894: slide right 2 x86_128
    
    * FIX-894: slide right 2 x86_256
    
    * FIX-894: slide right 2 x86_512
    
    * FIX-894: merge slide_right_2 into slide right
    
    * FIX-894: arm solution
    
    * FIX-894: logicals
    
    * FIX-894: slide right bundle
    
    * FIX-894: fixes

--- include/eve/detail/function/simd/common/slide_right.hpp
+++ include/eve/detail/function/simd/common/slide_right.hpp
@@ -7,6 +7,7 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/concept/abi.hpp>
 #include <eve/detail/abi.hpp>
 #include <eve/pattern.hpp>
 
@@ -39,10 +40,63 @@ namespace eve::detail
     }
   }
 
+  template <simd_value Wide, std::ptrdiff_t Shift>
+  EVE_FORCEINLINE Wide slide_right_(EVE_SUPPORTS(cpu_), Wide x, Wide y, index_t<Shift>) noexcept
+  requires(Shift <= Wide::size() )
+  {
+         if constexpr ( Shift == 0                 ) return y;
+    else if constexpr ( Shift == Wide::size()      ) return x;
+    else if constexpr( is_bundle_v<typename Wide::abi_type> )
+    {
+      return Wide( kumi::map ( []<typename T>(T mx, T my) { return slide_right(mx, my, index<Shift>); }, x, y) );
+    }
+    else if constexpr ( has_aggregated_abi_v<Wide> )
+    {
+      // [aa bb] [cc dd] => [bccd], [abbc]
+      auto [aa, bb] = x.slice();
+      auto [cc, dd] = y.slice();
+
+      constexpr std::ptrdiff_t half = Wide::size() / 2;
+
+      if constexpr ( Shift <= half )
+      {
+        auto s = index<Shift>;
+        return Wide{ slide_right(bb, cc, s), slide_right(cc, dd, s) };
+      }
+      else
+      {
+        auto s = index<Shift - half>;
+        return Wide{ slide_right(aa, bb, s), slide_right(bb, cc, s) };
+      }
+    }
+    else if constexpr ( !native_simd_for_abi<Wide, typename Wide::abi_type> )
+    {
+      return slide_right( eve::combine(x, y), index<Shift>).slice(upper_);
+    }
+    else
+    {
+      Wide res{ [&](int i, int size) {
+        if (i < Shift) return x.get(size - Shift + i);
+        else           return y.get(i - Shift);
+      }};
+      return res;
+    }
+  }
+
   template<simd_value Wide, std::ptrdiff_t Shift>
   EVE_FORCEINLINE auto slide_right_(EVE_SUPPORTS(cpu_), logical<Wide> v, index_t<Shift> s) noexcept
   requires(Shift <= Wide::size() )
   {
     return bit_cast(slide_right(v.bits(),s), as<logical<Wide>>{});
+  }
+
+  template<simd_value Wide, std::ptrdiff_t Shift>
+  EVE_FORCEINLINE auto slide_right_(EVE_SUPPORTS(cpu_)
+                                   , logical<Wide> x
+                                   , logical<Wide> y
+                                   , index_t<Shift> s) noexcept
+  requires(Shift <= Wide::size() )
+  {
+    return bit_cast(slide_right(x.bits(), y.bits(), s), as<logical<Wide>>{});
   }
 }
---
+++ test/unit/api/regular/shuffle/slide_right.cpp
@@ -0,0 +1,62 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#include "test.hpp"
+
+#include <eve/function/slide_right.hpp>
+
+//==================================================================================================
+// slide_right test
+//==================================================================================================
+
+EVE_TEST_TYPES("Check behavior of slide_right shuffle", eve::test::simd::all_types)
+<typename T>(eve::as<T>)
+{
+  T x{[](int i, int size) { return i - size; }};
+  T y{[](int i, int     ) { return i; }};
+
+  eve::logical<T> lx = x < -3;
+  eve::logical<T> ly = y < 4;
+
+  eve::detail::for_<0, 1, T::size()>(
+    [&]<int Shift>(std::integral_constant<int, Shift>) {
+      T expected([](int i, int) { return i - Shift; });
+      T actual = eve::slide_right(x, y, eve::index<Shift>);
+      TTS_EQUAL(expected, actual);
+
+      eve::logical<T> lexpected([](int i, int) {
+        int full_i = i - Shift;
+        if (full_i < 0) return full_i < -3;
+        else            return full_i < 4;
+      });
+      eve::logical<T> lactual = eve::slide_right(lx, ly, eve::index<Shift>);
+
+      TTS_EQUAL(lexpected, lactual);
+    }
+  );
+};
+
+TTS_CASE("Check behaviour of slide_right, 4 ints")
+{
+  // Special case to check if everything makes sense
+  using T = eve::wide<int, eve::fixed<4>>;
+
+  T x{-4, -3, -2, -1};
+  T y{0,   1,  2,  3};
+
+  T shift_0 = {0,   1,  2,   3};
+  T shift_1 = {-1,  0,  1,   2};
+  T shift_2 = {-2, -1,  0,   1};
+  T shift_3 = {-3, -2,  -1,  0};
+  T shift_4 = {-4, -3,  -2, -1};
+
+  TTS_EQUAL(shift_0, (eve::slide_right(x, y, eve::index<0>)));
+  TTS_EQUAL(shift_1, (eve::slide_right(x, y, eve::index<1>)));
+  TTS_EQUAL(shift_2, (eve::slide_right(x, y, eve::index<2>)));
+  TTS_EQUAL(shift_3, (eve::slide_right(x, y, eve::index<3>)));
+  TTS_EQUAL(shift_4, (eve::slide_right(x, y, eve::index<4>)));
+}
