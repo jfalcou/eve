1b19efb3dd5f7aa24d9e773363088cbbd8d19ef0
Joel Falcou <joel.falcou@lri.fr>
2021-09-10 19:37:25 +0200

    Refurbishing old convert tests

---
+++ test/unit/module/real/core/convert/to_float.cpp
@@ -0,0 +1,89 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#include <eve/function/convert.hpp>
+#include <eve/function/converter.hpp>
+#include <eve/function/saturated/convert.hpp>
+#include <eve/constant/valmin.hpp>
+#include <eve/constant/valmax.hpp>
+#include <type_traits>
+
+//==================================================================================================
+// Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check eve::convert return type", eve::test::simd::all_types)
+<typename T>(eve::as<T>)
+{
+  using t_t = eve::wide<float, eve::cardinal_t<T>>;
+
+  TTS_EXPR_IS(eve::convert(T()  , eve::as<float>())              , t_t);
+  TTS_EXPR_IS(eve::float32(T())                                   , t_t);
+  TTS_EXPR_IS(eve::saturated(eve::convert)(T(), eve::as<float>()), t_t);
+};
+
+//==================================================================================================
+// Value tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check eve::convert arithmetic behavior", eve::test::simd::all_types)
+<typename T>(eve::as<T>)
+{
+  using t_t = eve::wide<float, eve::cardinal_t<T>>;
+  using v_t = eve::element_type_t<T>;
+  constexpr auto tgt = eve::as<float>();
+
+  TTS_EQUAL(eve::convert((T(0))         , tgt), static_cast<t_t>(0) );
+  TTS_EQUAL(eve::convert((T(42.69))     , tgt), static_cast<t_t>(v_t(42.69)) );
+  TTS_EQUAL(eve::convert((T(0))         , tgt), eve::float32(T(0)) );
+  TTS_EQUAL(eve::convert((T(42.69))     , tgt), eve::float32(T(42.69)));
+  TTS_EQUAL(eve::convert(eve::sqr(T(2)) , tgt), eve::float32(eve::sqr)(T(2)));
+
+  if constexpr(sizeof(v_t)<=sizeof(float))
+  {
+    TTS_EQUAL(eve::convert(eve::valmin(eve::as<T>()), tgt), static_cast<t_t>(eve::valmin(eve::as<v_t>())) );
+    TTS_EQUAL(eve::convert(eve::valmax(eve::as<T>()), tgt), static_cast<t_t>(eve::valmax(eve::as<v_t>())) );
+  }
+};
+
+EVE_TEST_TYPES( "Check saturated eve::convert arithmetic behavior", eve::test::simd::all_types)
+<typename T>(eve::as<T>)
+{
+  using t_t = eve::wide<float, eve::cardinal_t<T>>;
+  using v_t = eve::element_type_t<T>;
+  constexpr auto tgt = eve::as<float>();
+
+  if constexpr(std::is_same_v<v_t, double>)
+  {
+    TTS_EQUAL(eve::saturated(eve::convert)(eve::valmin(eve::as<T>()), tgt), (eve::valmin(eve::as<t_t>())) );
+    TTS_EQUAL(eve::saturated(eve::convert)(eve::valmax(eve::as<T>()), tgt), (eve::valmax(eve::as<t_t>())) );
+  }
+  else
+  {
+    if constexpr(sizeof(v_t)<=sizeof(float))
+    {
+      TTS_EQUAL(eve::saturated(eve::convert)(eve::valmin(eve::as<T>()), tgt), static_cast<t_t>(eve::valmin(eve::as<v_t>())) );
+      TTS_EQUAL(eve::saturated(eve::convert)(eve::valmax(eve::as<T>()), tgt), static_cast<t_t>(eve::valmax(eve::as<v_t>())) );
+    }
+  }
+
+  TTS_EQUAL(eve::saturated(eve::convert)((T(0))    , tgt), static_cast<t_t>(0) );
+  TTS_EQUAL(eve::saturated(eve::convert)((T(42.69)), tgt), static_cast<t_t>(v_t(42.69)) );
+};
+
+EVE_TEST_TYPES( "Check eve::convert logical behavior", eve::test::simd::all_types)
+<typename T>(eve::as<T>)
+{
+  using t_t  = eve::logical<eve::wide<float, eve::cardinal_t<T>>>;
+  constexpr auto tgt = eve::as<eve::logical<float>>();
+
+  TTS_EQUAL(eve::convert(eve::logical<T>(true)  , tgt), t_t(true) );
+  TTS_EQUAL(eve::convert(eve::logical<T>(false) , tgt), t_t(false) );
+
+  eve::logical<T> mixed( [](auto i, auto) { return i%2 == 0;});
+  t_t             ref( [](auto i, auto) { return i%2 == 0;});
+
+  TTS_EQUAL(eve::convert(mixed, tgt), ref );
+};
