4fbdd984cd46fc2f3ba2d0dd922b63ecc865f7a4
Joel Falcou <joel.falcou@lri.fr>
2021-09-15 21:03:12 +0200

    Add an examples folder (#952)
    * Add examples/ directory
    
    * Compile examples in CI

--- examples/strlen.cpp
+++ examples/strlen.cpp
@@ -1,15 +1,38 @@
 //==================================================================================================
-/**
+/*
   EVE - Expressive Vector Engine
   Copyright : EVE Contributors & Maintainers
   SPDX-License-Identifier: MIT
-**/
+*/
 //==================================================================================================
 #include "test.hpp"
 
-#include "strlen.hpp"
-
+#include <eve/eve.hpp>
+#include <eve/function/first_true.hpp>
+#include <eve/function/load.hpp>
 #include <eve/memory/aligned_allocator.hpp>
+
+std::size_t strlen_example(const char* s_) {
+  const auto* s = reinterpret_cast<const std::uint8_t*>(s_);
+  using wide = eve::wide<std::uint8_t>;
+
+  const wide zeroes{0};
+
+  eve::aligned_ptr aligned_s = eve::previous_aligned_address(s);
+
+  wide cur = eve::unsafe(eve::load)(aligned_s);
+  eve::ignore_first ignore{s - aligned_s.get()};
+
+  std::optional match = eve::first_true[ignore](cur == zeroes);
+
+  while (!match) {
+    aligned_s += wide::size();
+    cur = eve::unsafe(eve::load)(aligned_s);
+    match = eve::first_true(cur == zeroes);
+  }
+
+  return static_cast<std::size_t>(aligned_s.get() + *match - s);
+}
 
 TTS_CASE( "Check strlen example" )
 {
