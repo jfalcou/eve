cd137ba1b08649f876ae73e062692ce019570d47
Joel Falcou <joel.falcou@lri.fr>
2021-09-17 17:42:46 +0200

    Update toolchain

--- cmake/toolchain/clang.x86.cmake
+++ cmake/toolchain/clang.x86.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Contributors & Maintainers
 ##  SPDX-License-Identifier: MIT
 ##==================================================================================================
-set(CMAKE_C_COMPILER    clang-12   )
-set(CMAKE_CXX_COMPILER  clang++-12 )
+set(CMAKE_C_COMPILER    clang   )
+set(CMAKE_CXX_COMPILER  clang++ )
 
 set(CMAKE_CXX_FLAGS     "-DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
--- cmake/toolchain/gcc.x86.opt.cmake
+++ cmake/toolchain/gcc.x86.opt.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Contributors & Maintainers
 ##  SPDX-License-Identifier: MIT
 ##==================================================================================================
-set(CMAKE_C_COMPILER    gcc-11  )
-set(CMAKE_CXX_COMPILER  g++-11  )
+set(CMAKE_C_COMPILER    gcc  )
+set(CMAKE_CXX_COMPILER  g++  )
 
 set(CMAKE_CXX_FLAGS     "-Wno-psabi -O3 -DNDEBUG" )
