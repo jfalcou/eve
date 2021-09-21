ab66c59c99d734720981a3d2f31a85d51bfd03fd
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-09-21 21:56:49 +0100

    FIX-868: erase remove points

--- test/unit/algo/container/erase_remove.cpp
+++ test/unit/algo/container/erase_remove.cpp
@@ -12,6 +12,7 @@
 
 #include "unit/algo/udt.hpp"
 
+/*
 TTS_CASE("erase/remove idiom in eve")
 {
   eve::algo::soa_vector<udt::line2D> lines {
@@ -22,16 +23,37 @@ TTS_CASE("erase/remove idiom in eve")
   };
 
   udt::point2D bad_start{0, 1};
-  (void) bad_start;
 
   TTS_PASS("FIX-#868, then test");
 
-#if 0  // FIX-#868
   lines.erase(
     eve::algo::remove_if(lines, [&](eve::wide<udt::line2D> l) {
       return get_start(l) == bad_start;
     }),
     lines.end()
   );
-#endif
+}
+*/
+
+TTS_CASE("erase/remove idiom in eve")
+{
+  eve::algo::soa_vector<udt::point2D> points {
+    udt::point2D{0, 1}, udt::point2D{1, 2}, udt::point2D{1, 3}
+  };
+
+  udt::point2D bad{0, 1};
+
+  eve::algo::soa_vector<udt::point2D> expected {
+    points.get(1), points.get(2)
+  };
+
+
+  points.erase(
+    eve::algo::remove_if(points, [&](eve::wide<udt::point2D> p) {
+      return p == bad;
+    }),
+    points.end()
+  );
+
+  TTS_EQUAL(expected, points);
 }
