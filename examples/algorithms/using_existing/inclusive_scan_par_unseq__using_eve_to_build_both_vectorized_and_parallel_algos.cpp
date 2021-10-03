b51b547886a25c0c887c17b66b560dfd6d5e14ff
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-10-03 13:36:21 +0100

    FIX-999: split example

---
+++ examples/algorithms/using_existing/inclusive_scan_par_unseq__using_eve_to_build_both_vectorized_and_parallel_algos.cpp
@@ -0,0 +1,200 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+
+// In this example we are going to have a look at the problem from
+// this https://stackoverflow.com/q/10587598/5021064
+//
+// The solution by @Zbozon was the basis of our inclusive_scan implementation.
+//
+// eve itself does not deal with threading however it's a good building block.
+
+// The main idea:
+//   * split the computation into chunks
+//   * scan each chunk in parallel
+//   * each thread writes the overall sum into a vector
+//   * scan a vector of sums on one thread
+//   * each thread applies a previous sum to each part
+
+// NOTE: please don't do threading like we do, it's very very bad.
+//       Ideally you want some reasonable tasking system.
+//       Maybe: https://stlab.cc/ ?
+
+// NOTE: supporting SOA here is possible but it makes the solution
+//       much more complicated.
+
+#include <eve/algo/inclusive_scan.hpp>
+#include <eve/algo/transform.hpp>
+
+#include <eve/constant/as_value.hpp>
+#include <eve/memory/aligned_ptr.hpp>
+#include <eve/memory/aligned_allocator.hpp>
+#include <eve/function/read.hpp>
+
+#include <future>
+
+// The example was about floats
+using T = float;
+
+// We are going to use alignment of at least 64 bytes so that
+// threads don't touch each other's cache lines.
+
+using cache_line_n = eve::fixed<64 / sizeof(T)>;
+using aptr         = eve::aligned_ptr<T, cache_line_n>;
+
+using uptr_range   = eve::algo::as_range<T*, T*>;
+using aptr_range   = eve::algo::as_range<aptr, aptr>;
+
+// These operators will be the not inlined (probably) reusable wrappers
+// around simd implementation.
+//
+// TODO: think, maybe we should return the sum from `eve::algo::inclusive_scan`
+//       since we don't return the output iterator anyways?
+struct
+{
+  // boundary version
+  float operator()(uptr_range r, T init) const
+  {
+    if (r.begin() == r.end()) return init;
+    eve::algo::inclusive_scan_inplace(r, init);
+    return *(r.end() - 1);
+  }
+
+  // middle version
+  float operator()(aptr_range r) const
+  {
+    if (r.begin() == r.end()) return 0.0f;
+    eve::algo::inclusive_scan_inplace(r, 0.0f);
+    return *(r.end() - 1);
+  }
+
+} inline constexpr inclusive_scan;
+
+// Split range into subranges
+struct subranges_split_t
+{
+  uptr_range              first;
+  std::vector<aptr_range> middle;
+  uptr_range              last;
+
+  subranges_split_t():
+    first(nullptr, nullptr),
+    middle(),
+    last(nullptr, nullptr)
+  {}
+};
+
+subranges_split_t subranges_split(uptr_range r,
+                                  std::ptrdiff_t subrange_count,
+                                  std::ptrdiff_t rough_min_size) {
+  T* f = r.begin();
+  T* l = r.end();
+
+  subranges_split_t res;
+
+  if( l - f <= rough_min_size )
+  {
+    res.first = {f, l};
+    return res;
+  }
+
+  // Select chunk size:
+  // either based on subrange count or rough min size but always divisible by cacheline
+  // We round up.
+  std::ptrdiff_t chunk_size = std::max((l - f) / subrange_count, rough_min_size);
+  chunk_size += cache_line_n{}() - chunk_size % cache_line_n{}();
+
+  aptr f1 = eve::previous_aligned_address(f + chunk_size, cache_line_n{});
+  res.first = {f, f1.get()};
+
+  while (l - f1 > chunk_size) {
+    aptr f2 = f1 + chunk_size;
+    res.middle.emplace_back(f1, f2);
+    f1 = f2;
+  }
+
+  res.last = {f1.get(), l};
+
+  return res;
+}
+
+// -----------------------
+
+#include "test.hpp"
+
+TTS_CASE("subranges_split") {
+  using a_v = std::vector<T, eve::aligned_allocator<T, cache_line_n>>;
+
+  using idx_pair = kumi::tuple<std::ptrdiff_t, std::ptrdiff_t>;
+
+  auto split_to_ints = [](subranges_split_t split) {
+    T* base = split.first.begin();
+
+    std::vector<idx_pair> res;
+
+    auto push_range = [&](auto r) mutable {
+      if (r.begin() == r.end()) return;
+
+      res.push_back(idx_pair{r.begin() - base, r.end() - base});
+    };
+
+    push_range(split.first);
+
+    for (auto r : split.middle) push_range(r);
+
+    push_range(split.last);
+
+    return res;
+  };
+
+  auto test = [&](auto&& r, int group_count, int group_size, std::vector<idx_pair> expected)
+  {
+    auto split = subranges_split(
+      eve::algo::as_range(r.data(), r.data() + r.size()),
+      group_count, group_size);
+    TTS_EQUAL(expected, split_to_ints(split));
+  };
+
+  // empty
+  {
+    a_v v;
+    test(v, 1, 1, {});
+  }
+
+  // align middle
+  {
+    a_v v (100u, 0);
+
+    test(v, 2, 45,
+      { {0, 64}, {64, 100} }
+    );
+  }
+
+  // two chunks middle
+  {
+    a_v v (200u, 0);
+
+    test(v, 4, 50,
+      { {0, 64}, {64, 128}, {128, 192}, {192, 200} }
+    );
+  }
+  // too small of a range
+  {
+    std::vector<T> v(1000u, 0);
+
+    test(v, 3, 10'000,
+      { {0, 1000} }
+    );
+  }
+
+}
+
+TTS_CASE("inclusive_scan") {
+  std::vector<T> v;
+  TTS_EQUAL(inclusive_scan(eve::algo::as_range(v.data(), v.data() + v.size()), T{3}),
+            T{3});
+}
