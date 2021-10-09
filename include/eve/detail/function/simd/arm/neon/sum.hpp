6a045987fc11dea3a26b87a5916c33b7fb9b60c8
Joel Falcou <joel.falcou@lri.fr>
2021-10-09 15:08:02 +0200

    Use A64 vaddv*_*

--- include/eve/detail/function/simd/arm/neon/sum.hpp
+++ include/eve/detail/function/simd/arm/neon/sum.hpp
@@ -13,17 +13,17 @@
 namespace eve::detail
 {
   template<typename T, typename N>
-  EVE_FORCEINLINE wide<T,N> arm_sum_impl(wide<T,N> v, wide<T,N> w) noexcept
+  EVE_FORCEINLINE wide<T,N> arm_sum_impl(wide<T,N> v) noexcept
   {
     constexpr auto c = categorize<wide<T, N>>();
 
-          if constexpr( c== category::float32x2 ) return vpadd_f32(v,w);
-    else  if constexpr( c== category::uint32x2  ) return vpadd_u32(v,w);
-    else  if constexpr( c== category::int32x2   ) return vpadd_s32(v,w);
-    else  if constexpr( c== category::uint16x4  ) return vpadd_u16(v,w);
-    else  if constexpr( c== category::int16x4   ) return vpadd_s16(v,w);
-    else  if constexpr( c== category::uint8x8   ) return vpadd_u8(v,w);
-    else  if constexpr( c== category::int8x8    ) return vpadd_s8(v,w);
+          if constexpr( c== category::float32x2 ) return vpadd_f32(v,v);
+    else  if constexpr( c== category::uint32x2  ) return vpadd_u32(v,v);
+    else  if constexpr( c== category::int32x2   ) return vpadd_s32(v,v);
+    else  if constexpr( c== category::uint16x4  ) return vpadd_u16(v,v);
+    else  if constexpr( c== category::int16x4   ) return vpadd_s16(v,v);
+    else  if constexpr( c== category::uint8x8   ) return vpadd_u8(v,v);
+    else  if constexpr( c== category::int8x8    ) return vpadd_s8(v,v);
   }
 
   template<typename T, typename N>
@@ -32,26 +32,27 @@ namespace eve::detail
   {
     if constexpr( std::same_as<abi_t<T,N>, arm_64_> )
     {
-      if constexpr  (N::value == 1)  return v;
+      if constexpr(N::value == 1)  return v;
       else
       {
-        if constexpr(sizeof(T) <= 4)  v = arm_sum_impl(v,v);
-        if constexpr(sizeof(T) <= 2)  v = arm_sum_impl(v,v);
-        if constexpr(sizeof(T) <= 1)  v = arm_sum_impl(v,v);
+        if constexpr(sizeof(T) <= 4)  v = arm_sum_impl(v);
+        if constexpr(sizeof(T) <= 2)  v = arm_sum_impl(v);
+        if constexpr(sizeof(T) <= 1)  v = arm_sum_impl(v);
         return v;
       }
     }
     else
     {
-      if constexpr(sizeof(T) == 8)
-      {
-        return wide<T,N>{v.get(0)+v.get(1)};
-      }
+      if constexpr(current_api >= asimd) return wide<T,N>(sum(v));
       else
       {
-        auto [l, h] = v.slice();
-        l = splat(sum)(l+h);
-        return wide<T,N>{l, l};
+        if constexpr(sizeof(T) == 8) return wide<T,N>{v.get(0)+v.get(1)};
+        else
+        {
+          auto [l, h] = v.slice();
+          l = splat(sum)(l+h);
+          return wide<T,N>{l, l};
+        }
       }
     }
   }
@@ -60,19 +61,35 @@ namespace eve::detail
   EVE_FORCEINLINE T sum_( EVE_SUPPORTS(neon128_), wide<T,N> v) noexcept
   requires arm_abi<abi_t<T, N>>
   {
-    if constexpr( std::same_as<abi_t<T,N>, arm_64_> ) return splat(sum)(v).get(0);
+    if constexpr(current_api >= asimd)
+    {
+        constexpr auto c = categorize<wide<T, N>>();
+
+              if constexpr( c== category::float64x1 ) return vaddv_f64(v);
+              if constexpr( c== category::float64x2 ) return vaddvq_f64(v);
+        else  if constexpr( c== category::float32x2 ) return vaddv_f32(v);
+        else  if constexpr( c== category::float32x4 ) return vaddvq_f32(v);
+        else  if constexpr( c== category::uint32x2  ) return vaddv_u32(v);
+        else  if constexpr( c== category::uint32x4  ) return vaddvq_u32(v);
+        else  if constexpr( c== category::int32x2   ) return vaddv_s32(v);
+        else  if constexpr( c== category::int32x4   ) return vaddvq_s32(v);
+        else  if constexpr( c== category::uint16x4  ) return vaddv_u16(v);
+        else  if constexpr( c== category::uint16x8  ) return vaddvq_u16(v);
+        else  if constexpr( c== category::int16x4   ) return vaddv_s16(v);
+        else  if constexpr( c== category::int16x8   ) return vaddvq_s16(v);
+        else  if constexpr( c== category::uint8x8   ) return vaddv_u8(v);
+        else  if constexpr( c== category::uint8x16  ) return vaddvq_u8(v);
+        else  if constexpr( c== category::int8x8    ) return vaddv_s8(v);
+        else  if constexpr( c== category::int8x16   ) return vaddvq_s8(v);
+    }
     else
     {
-      if constexpr(sizeof(T) == 8)
-      {
-        return v.get(0)+v.get(1);
-      }
+            if constexpr( std::same_as<abi_t<T,N>, arm_64_> ) return splat(sum)(v).get(0);
+      else  if constexpr(sizeof(T) == 8)                      return v.get(0)+v.get(1);
       else
       {
         auto [l,h] = v.slice();
-        wide<T, typename N::split_type> s = arm_sum_impl(l,h);
-                                        s = splat(eve::detail::sum)(s);
-        return s.get(0);
+        return splat(eve::detail::sum)(l+h).get(0);
       }
     }
   }
