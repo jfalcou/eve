af8e882d71021b1c4663c56e127b3e51324583b8
jtlap <jtlapreste@gmail.com>
2021-10-09 18:00:30 +0200

    correction of poleval for nul pointer

--- include/eve/module/real/core/detail/generic/poleval.hpp
+++ include/eve/module/real/core/detail/generic/poleval.hpp
@@ -17,6 +17,7 @@ namespace eve::detail
   template < typename T, typename A> auto poleval(T x, A const & coefs)
   {
     auto p = coefs.begin();
+    if(coefs.end() == p) return zero(as(x));
     auto r = T(*p++);
 
     while(p!= coefs.end())
@@ -29,8 +30,9 @@ namespace eve::detail
   template < typename T, typename A> auto poleval1(T x, A const & coefs)
   {
     auto p = coefs.begin();
+    if(coefs.end() == p) return one(as(x));
     auto r = x+T(*p++);
-    
+
     while(p!= coefs.end())
     {
       r = fma(r, x,  *p++);
--- test/unit/module/real/polynomial/horner.cpp
+++ test/unit/module/real/polynomial/horner.cpp
@@ -25,29 +25,29 @@
 //==================================================================================================
 //== Types tests
 //==================================================================================================
-// EVE_TEST_TYPES( "Check return types of horner on wide"
-//         , eve::test::simd::all_types
+EVE_TEST_TYPES( "Check return types of horner on wide"
+        , eve::test::simd::all_types
 
-//         )
-// <typename T>(eve::as<T>)
-// {
-//   using v_t = eve::element_type_t<T>;
-//   using i_t = eve::as_integer_t<v_t>;
-//   TTS_EXPR_IS( eve::horner(T(), T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), T(), T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), T(), T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), v_t(), v_t())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), v_t(), int())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), v_t(), i_t())  , T);
-//   TTS_EXPR_IS( eve::horner(v_t(), v_t(), v_t()), v_t);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, T(), T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, T(), T())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), v_t())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), int())  , T);
-//   TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), i_t())  , T);
-//   TTS_EXPR_IS( eve::horner(v_t(), eve::one, v_t(), v_t()), v_t);
-// };
+        )
+<typename T>(eve::as<T>)
+{
+  using v_t = eve::element_type_t<T>;
+  using i_t = eve::as_integer_t<v_t>;
+  TTS_EXPR_IS( eve::horner(T(), T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), T(), T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), T(), T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), v_t(), v_t())  , T);
+  TTS_EXPR_IS( eve::horner(T(), v_t(), int())  , T);
+  TTS_EXPR_IS( eve::horner(T(), v_t(), i_t())  , T);
+  TTS_EXPR_IS( eve::horner(v_t(), v_t(), v_t()), v_t);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, T(), T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, T(), T())  , T);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), v_t())  , T);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), int())  , T);
+  TTS_EXPR_IS( eve::horner(T(), eve::one, v_t(), i_t())  , T);
+  TTS_EXPR_IS( eve::horner(v_t(), eve::one, v_t(), v_t()), v_t);
+};
 
 //==================================================================================================
 //== horner tests
@@ -64,41 +64,41 @@ EVE_TEST( "Check behavior of horner on wide"
   using eve::numeric;
   using eve::one;
   using v_t = eve::element_type_t<T>;
-//   //============================================================================
-//   //== variadic
-//   //============================================================================
+  //============================================================================
+  //== variadic
+  //============================================================================
 
-//   TTS_EQUAL(horner(a0, 0), T(0));
-//   TTS_EQUAL(horner(a0, 1), T(1));
-//   TTS_EQUAL(horner(a0, 1, 2), fma(a0, 1, 2));
-//   TTS_EQUAL(horner(a0, 1, 2, 3), fma(a0, fma(a0, 1, 2), 3));
+  TTS_EQUAL(horner(a0, 0), T(0));
+  TTS_EQUAL(horner(a0, 1), T(1));
+  TTS_EQUAL(horner(a0, 1, 2), fma(a0, 1, 2));
+  TTS_EQUAL(horner(a0, 1, 2, 3), fma(a0, fma(a0, 1, 2), 3));
 
-//   TTS_EQUAL(pedantic(horner)(a0, 0), T(0));
-//   TTS_EQUAL(pedantic(horner)(a0, 1), T(1));
-//   TTS_EQUAL(pedantic(horner)(a0, 1, 2), pedantic(fma)(a0, 1, 2));
-//   TTS_EQUAL(pedantic(horner)(a0, 1, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
+  TTS_EQUAL(pedantic(horner)(a0, 0), T(0));
+  TTS_EQUAL(pedantic(horner)(a0, 1), T(1));
+  TTS_EQUAL(pedantic(horner)(a0, 1, 2), pedantic(fma)(a0, 1, 2));
+  TTS_EQUAL(pedantic(horner)(a0, 1, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
 
-//   TTS_EQUAL(numeric(horner)(a0, 0), T(0));
-//   TTS_EQUAL(numeric(horner)(a0, 1), T(1));
-//   TTS_EQUAL(numeric(horner)(a0, 1, 2), numeric(fma)(a0, 1, 2));
-//   TTS_EQUAL(numeric(horner)(a0, 1, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
+  TTS_EQUAL(numeric(horner)(a0, 0), T(0));
+  TTS_EQUAL(numeric(horner)(a0, 1), T(1));
+  TTS_EQUAL(numeric(horner)(a0, 1, 2), numeric(fma)(a0, 1, 2));
+  TTS_EQUAL(numeric(horner)(a0, 1, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
 
-//   //============================================================================
-//   //== variadic with leading one
-//   //============================================================================
-//   TTS_EQUAL(horner(a0, one), T(1));
-//   TTS_EQUAL(horner(a0, one, 2), fma(a0, 1, 2));
-//   TTS_EQUAL(horner(a0, one, 2, 3), fma(a0, fma(a0, 1, 2), 3));
+  //============================================================================
+  //== variadic with leading one
+  //============================================================================
+  TTS_EQUAL(horner(a0, one), T(1));
+  TTS_EQUAL(horner(a0, one, 2), fma(a0, 1, 2));
+  TTS_EQUAL(horner(a0, one, 2, 3), fma(a0, fma(a0, 1, 2), 3));
 
-//   TTS_EQUAL(pedantic(horner)(a0, 0), T(0));
-//   TTS_EQUAL(pedantic(horner)(a0, one), T(1));
-//   TTS_EQUAL(pedantic(horner)(a0, one, 2), pedantic(fma)(a0, 1, 2));
-//   TTS_EQUAL(pedantic(horner)(a0, one, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
+  TTS_EQUAL(pedantic(horner)(a0, 0), T(0));
+  TTS_EQUAL(pedantic(horner)(a0, one), T(1));
+  TTS_EQUAL(pedantic(horner)(a0, one, 2), pedantic(fma)(a0, 1, 2));
+  TTS_EQUAL(pedantic(horner)(a0, one, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
 
-//   TTS_EQUAL(numeric(horner)(a0, 0), T(0));
-//   TTS_EQUAL(numeric(horner)(a0, one), T(1));
-//   TTS_EQUAL(numeric(horner)(a0, one, 2), numeric(fma)(a0, 1, 2));
-//   TTS_EQUAL(numeric(horner)(a0, one, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
+  TTS_EQUAL(numeric(horner)(a0, 0), T(0));
+  TTS_EQUAL(numeric(horner)(a0, one), T(1));
+  TTS_EQUAL(numeric(horner)(a0, one, 2), numeric(fma)(a0, 1, 2));
+  TTS_EQUAL(numeric(horner)(a0, one, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
 
   {
     //============================================================================
@@ -110,25 +110,25 @@ EVE_TEST( "Check behavior of horner on wide"
     std::array<v_t, 3> tab3 = {1, 2, 3};
 
 
-//     TTS_EQUAL((horner)(a0, tab0), T(0));
-//     TTS_EQUAL((horner)(a0, tab1), T(1));
-//     TTS_EQUAL((horner)(a0, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL((horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL((horner)(a0, tab0), T(0));
+    TTS_EQUAL((horner)(a0, tab1), T(1));
+    TTS_EQUAL((horner)(a0, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL((horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(pedantic(horner)(a0, tab0), T(0));
-//     TTS_EQUAL(pedantic(horner)(a0, tab1), T(1));
-//     TTS_EQUAL(pedantic(horner)(a0, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL(pedantic(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(pedantic(horner)(a0, tab0), T(0));
+    TTS_EQUAL(pedantic(horner)(a0, tab1), T(1));
+    TTS_EQUAL(pedantic(horner)(a0, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL(pedantic(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(numeric(horner)(a0, tab0), T(0));
-//     TTS_EQUAL(numeric(horner)(a0, tab1), T(1));
-//     TTS_EQUAL(numeric(horner)(a0, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL(numeric(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(numeric(horner)(a0, tab0), T(0));
+    TTS_EQUAL(numeric(horner)(a0, tab1), T(1));
+    TTS_EQUAL(numeric(horner)(a0, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL(numeric(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-    TTS_EQUAL((horner)(a0, tab0), eve::detail::poleval(a0, tab0));
-    TTS_EQUAL((horner)(a0, tab1), eve::detail::poleval(a0, tab1));
-    TTS_EQUAL((horner)(a0, tab2), eve::detail::poleval(a0, tab2));
-    TTS_EQUAL((horner)(a0, tab3), eve::detail::poleval(a0, tab3));
+//     TTS_EQUAL((horner)(a0, tab0), eve::detail::poleval(a0, tab0));
+//     TTS_EQUAL((horner)(a0, tab1), eve::detail::poleval(a0, tab1));
+//     TTS_EQUAL((horner)(a0, tab2), eve::detail::poleval(a0, tab2));
+//     TTS_EQUAL((horner)(a0, tab3), eve::detail::poleval(a0, tab3));
 
   }
   {
@@ -139,80 +139,79 @@ EVE_TEST( "Check behavior of horner on wide"
     std::array<v_t, 1> tab2 = {2};
     std::array<v_t, 2> tab3 = {2, 3};
 
-//     TTS_EQUAL((horner)(a0, one, tab1), T(1));
-//     TTS_EQUAL((horner)(a0, one, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL((horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL((horner)(a0, one, tab1), T(1));
+    TTS_EQUAL((horner)(a0, one, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL((horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(numeric(horner)(a0, one, tab1), T(1));
-//     TTS_EQUAL(numeric(horner)(a0, one, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL(numeric(horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(numeric(horner)(a0, one, tab1), T(1));
+    TTS_EQUAL(numeric(horner)(a0, one, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL(numeric(horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(pedantic(horner)(a0, one, tab1), T(1));
-//     TTS_EQUAL(pedantic(horner)(a0, one, tab2), (fma)(a0, 1, 2));
-//     TTS_EQUAL(pedantic(horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(pedantic(horner)(a0, one, tab1), T(1));
+    TTS_EQUAL(pedantic(horner)(a0, one, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL(pedantic(horner)(a0, one, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(horner(a0, one), T(1));
-//     TTS_EQUAL(horner(a0, one, 2), fma(a0, 1, 2));
-//     TTS_EQUAL(horner(a0, one, 2, 3), fma(a0, fma(a0, 1, 2), 3));
+    TTS_EQUAL(horner(a0, one), T(1));
+    TTS_EQUAL(horner(a0, one, 2), fma(a0, 1, 2));
+    TTS_EQUAL(horner(a0, one, 2, 3), fma(a0, fma(a0, 1, 2), 3));
 
-//     TTS_EQUAL(pedantic(horner)(a0, one), T(1));
-//     TTS_EQUAL(pedantic(horner)(a0, one, 2), pedantic(fma)(a0, 1, 2));
-//     TTS_EQUAL(pedantic(horner)(a0, one, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
+    TTS_EQUAL(pedantic(horner)(a0, one), T(1));
+    TTS_EQUAL(pedantic(horner)(a0, one, 2), pedantic(fma)(a0, 1, 2));
+    TTS_EQUAL(pedantic(horner)(a0, one, 2, 3), pedantic(fma)(a0, pedantic(fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(numeric(horner)(a0, one), T(1));
-//     TTS_EQUAL(numeric(horner)(a0, one, 2), numeric(fma)(a0, 1, 2));
-//     TTS_EQUAL(numeric(horner)(a0, one, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
+    TTS_EQUAL(numeric(horner)(a0, one), T(1));
+    TTS_EQUAL(numeric(horner)(a0, one, 2), numeric(fma)(a0, 1, 2));
+    TTS_EQUAL(numeric(horner)(a0, one, 2, 3), numeric(fma)(a0, numeric(fma)(a0, 1, 2), 3));
 
-    TTS_EQUAL((horner)(a0, one, tab1), eve::detail::poleval1(a0, tab1));
+   TTS_EQUAL((horner)(a0, one, tab1), eve::detail::poleval1(a0, tab1));
     TTS_EQUAL((horner)(a0, one, tab2), eve::detail::poleval1(a0, tab2));
     TTS_EQUAL((horner)(a0, one, tab3), eve::detail::poleval1(a0, tab3));
 
   }
-//   {
-//     //============================================================================
-//     //== iterators
-//     //============================================================================
-//     std::vector<v_t> tab0; // std does not want array of size 0
-//     std::array<v_t, 1> tab1 = {1};
-//     std::array<v_t, 2> tab2 = {1, 2};
-//     std::array<v_t, 3> tab3 = {1, 2, 3};
+  {
+    //============================================================================
+    //== iterators
+    //============================================================================
+    std::vector<v_t> tab0; // std does not want array of size 0
+    std::array<v_t, 1> tab1 = {1};
+    std::array<v_t, 2> tab2 = {1, 2};
+    std::array<v_t, 3> tab3 = {1, 2, 3};
 
-//     TTS_EQUAL((horner)(a0, &tab0[0], &tab0[0]), T(0));
-//     TTS_EQUAL((horner)(a0, &tab1[0], &tab1[1]), T(1));
-//     TTS_EQUAL((horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
-//     TTS_EQUAL((horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL((horner)(a0, &tab0[0], &tab0[0]), T(0));
+    TTS_EQUAL((horner)(a0, &tab1[0], &tab1[1]), T(1));
+    TTS_EQUAL((horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
+    TTS_EQUAL((horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(pedantic(horner)(a0, &tab0[0], &tab0[0]), T(0));
-//     TTS_EQUAL(pedantic(horner)(a0, &tab1[0], &tab1[1]), T(1));
-//     TTS_EQUAL(pedantic(horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
-//     TTS_EQUAL(pedantic(horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(pedantic(horner)(a0, &tab0[0], &tab0[0]), T(0));
+    TTS_EQUAL(pedantic(horner)(a0, &tab1[0], &tab1[1]), T(1));
+    TTS_EQUAL(pedantic(horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
+    TTS_EQUAL(pedantic(horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(numeric(horner)(a0, &tab0[0], &tab0[0]), T(0));
-//     TTS_EQUAL(numeric(horner)(a0, &tab1[0], &tab1[1]), T(1));
-//     TTS_EQUAL(numeric(horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
-//     TTS_EQUAL(numeric(horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
-//   }
-//   {
-//     //============================================================================
-//     //== iterators with leading one
-//     //============================================================================
-//     std::vector<v_t> tab1 = {};// std does not want array of size 0
-//     std::array<v_t, 1> tab2 = {2};
-//     std::array<v_t, 2> tab3 = {2, 3};
+    TTS_EQUAL(numeric(horner)(a0, &tab0[0], &tab0[0]), T(0));
+    TTS_EQUAL(numeric(horner)(a0, &tab1[0], &tab1[1]), T(1));
+    TTS_EQUAL(numeric(horner)(a0, &tab2[0], &tab2[2]), (fma)(a0, 1, 2));
+    TTS_EQUAL(numeric(horner)(a0, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
+  }
+  {
+    //============================================================================
+    //== iterators with leading one
+    //============================================================================
+    std::vector<v_t> tab1 = {};// std does not want array of size 0
+    std::array<v_t, 1> tab2 = {2};
+    std::array<v_t, 2> tab3 = {2, 3};
 
+    TTS_EQUAL((horner)(a0, one, &tab1[0], &tab1[0]), T(1));
+    TTS_EQUAL((horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
+    TTS_EQUAL((horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL((horner)(a0, one, &tab1[0], &tab1[0]), T(1));
-//     TTS_EQUAL((horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
-//     TTS_EQUAL((horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(pedantic(horner)(a0, one, &tab1[0], &tab1[0]), T(1));
+    TTS_EQUAL(pedantic(horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
+    TTS_EQUAL(pedantic(horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(pedantic(horner)(a0, one, &tab1[0], &tab1[0]), T(1));
-//     TTS_EQUAL(pedantic(horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
-//     TTS_EQUAL(pedantic(horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(numeric(horner)(a0, one, &tab1[0], &tab1[0]), T(1));
+    TTS_EQUAL(numeric(horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
+    TTS_EQUAL(numeric(horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-//     TTS_EQUAL(numeric(horner)(a0, one, &tab1[0], &tab1[0]), T(1));
-//     TTS_EQUAL(numeric(horner)(a0, one, &tab2[0], &tab2[1]), (fma)(a0, 1, 2));
-//     TTS_EQUAL(numeric(horner)(a0, one, tab3.begin(), tab3.end()), (fma)(a0, (fma)(a0, 1, 2), 3));
-
-//   }
+  }
 
 };
