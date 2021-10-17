325bb6d10e2295ef799d706295366035d0e09f38
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-10-17 08:38:08 +0100

    compress_mask 4 chars

--- include/eve/detail/function/simd/x86/compress_store_swizzle_mask_num.hpp
+++ include/eve/detail/function/simd/x86/compress_store_swizzle_mask_num.hpp
@@ -24,11 +24,26 @@ namespace eve::detail
   template<eve::relative_conditional_expr C, typename T>
   EVE_FORCEINLINE std::pair<int, int>
   compress_store_swizzle_mask_num_(EVE_SUPPORTS(sse2_), C c, logical<wide<T, fixed<4>>> mask)
-    requires (current_api < avx512) && (sizeof(T) == 2)
+    requires (current_api < avx512) && (sizeof(T) <= 2)
   {
-    static_assert(top_bits<logical<wide<T, fixed<4>>>>::bits_per_element == 2);
-    auto to_bytes = eve::convert(mask, eve::as<eve::logical<std::uint8_t>>{});
-    return compress_store_swizzle_mask_num(c, to_bytes);
+    if constexpr (sizeof(T) == 1)
+    {
+      mask = mask && c.mask(eve::as(mask));
+      __m128i sad_mask = _mm_set_epi64x(0, 0x80848281);
+      __m128i sum      = _mm_sad_epu8(_mm_andnot_si128(mask, sad_mask), sad_mask);
+
+      int desc     = _mm_cvtsi128_si32(sum);
+      int num      = desc & 0xf;
+      int popcount = desc >> 7;
+
+      return {num, popcount};
+    }
+    else
+    {
+      static_assert(top_bits<logical<wide<T, fixed<4>>>>::bits_per_element == 2);
+      auto to_bytes = eve::convert(mask, eve::as<eve::logical<std::uint8_t>>{});
+      return compress_store_swizzle_mask_num(c, to_bytes);
+    }
   }
 
   template<typename T>
