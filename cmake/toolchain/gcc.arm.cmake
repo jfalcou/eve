b322f256a373af6163efe6dca30758b10b3c6718
Joel Falcou <joel.falcou@lri.fr>
2021-10-25 19:51:54 +0200

    Fix options for arm v7 testing

--- cmake/toolchain/gcc.arm.cmake
+++ cmake/toolchain/gcc.arm.cmake
@@ -9,6 +9,6 @@ set(CMAKE_SYSTEM_PROCESSOR arm  )
 set(CMAKE_C_COMPILER    arm-linux-gnueabihf-gcc-11  )
 set(CMAKE_CXX_COMPILER  arm-linux-gnueabihf-g++-11  )
 
-set(CMAKE_CXX_FLAGS     "-Wno-psabi -mfpu=neon -march=armv7-a -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}"  )
+set(CMAKE_CXX_FLAGS     "-Wno-psabi -mfpu=neon-vfpv4 -march=armv7-a -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}"  )
 
 set(CMAKE_CROSSCOMPILING_CMD ${PROJECT_SOURCE_DIR}/cmake/toolchain/run_arm.sh     )
--- include/eve/module/real/core/function/regular/simd/arm/neon/fnma.hpp
+++ include/eve/module/real/core/function/regular/simd/arm/neon/fnma.hpp
@@ -23,12 +23,12 @@ namespace eve::detail
   {
     constexpr auto cat = categorize<wide<T, N>>();
 
-         if constexpr(cat == category::float32x2)     return vfms_f32(v2, v1, v0);
+         if constexpr(cat == category::float32x2) return vfms_f32(v2, v1, v0);
+    else if constexpr(cat == category::float32x4) return vfmsq_f32(v2, v1, v0);
     else if constexpr(current_api >= asimd)
     {
             if constexpr(cat == category::float64x1)  return vfms_f64(v2, v1, v0);
       else  if constexpr(cat == category::float64x2)  return vfmsq_f64(v2, v1, v0);
-      else  if constexpr(cat == category::float32x4)  return vfmsq_f32(v2, v1, v0);
       else                                            return fma(-v0, v1, v2);
     }
     else                                              return fma(-v0, v1, v2);
