a5317591456d7eb635e53570aa32e6467250f58f
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-10-28 11:12:14 +0100

    move views tests: iota

--- test/unit/views/iota.cpp
+++ test/unit/views/iota.cpp
@@ -7,10 +7,11 @@
 //==================================================================================================
 
 #include "unit/algo/algo_test.hpp"
+#include "unit/algo/iterator_concept_test.hpp"
 
-#include <eve/algo/views/iota.hpp>
+#include <eve/views/iota.hpp>
 
-#include "iterator_concept_test.hpp"
+#include <eve/views/convert.hpp>
 
 #include <numeric>
 
@@ -22,7 +23,7 @@ EVE_TEST_TYPES("Check iota_scaled_iterator", algo_test::selected_types)
   T values([](int i, int) { return 5 + i * 2; });
   auto replace = [&](auto v, auto ignore) { return eve::replace_ignored(v, ignore, T{0}); };
 
-  auto f = eve::algo::views::iota_scaled(e_t{5}, e_t{2}).cardinal_cast(eve::lane<T::size()>);
+  auto f = eve::views::iota_scaled(e_t{5}, e_t{2}).cardinal_cast(eve::lane<T::size()>);
   auto l = f + T::size();
   TTS_EQUAL((l - f), T::size());
 
@@ -34,13 +35,13 @@ EVE_TEST_TYPES("Check iota_scaled_iterator", algo_test::selected_types)
 EVE_TEST_TYPES("Check iota_scaled_iterator, conversions", eve::test::scalar::all_types)
 <typename T>(eve::as<T>)
 {
-  auto f = eve::algo::views::iota_scaled(T(0), T(1));
+  auto f = eve::views::iota_scaled(T(0), T(1));
 
-  auto actual_bytes   = eve::algo::views::convert(f, eve::as<std::int8_t>{});
-  auto expected_bytes = eve::algo::views::iota_scaled(std::int8_t{0}, std::int8_t{1}).cardinal_cast(eve::lane<eve::expected_cardinal_v<T>>);
+  auto actual_bytes   = eve::views::convert(f, eve::as<std::int8_t>{});
+  auto expected_bytes = eve::views::iota_scaled(std::int8_t{0}, std::int8_t{1}).cardinal_cast(eve::lane<eve::expected_cardinal_v<T>>);
   TTS_EQUAL(expected_bytes, actual_bytes);
 
-  auto actual_back = eve::algo::views::convert(actual_bytes, eve::as<T>{});
+  auto actual_back = eve::views::convert(actual_bytes, eve::as<T>{});
   TTS_EQUAL(f, actual_back);
 };
 
@@ -52,7 +53,7 @@ EVE_TEST_TYPES("Check iota", eve::test::scalar::all_types)
 
   std::array<T, 13> actual;
 
-  auto iota_r = eve::algo::views::iota(T{0}, 13);
+  auto iota_r = eve::views::iota(T{0}, 13);
   for (auto f = iota_r.begin(); f != iota_r.end(); ++f) {
     actual[f - iota_r.begin()] = eve::read(f);
   }
