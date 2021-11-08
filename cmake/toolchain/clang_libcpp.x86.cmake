7e28f84231d836b5da455b5071f10ac231cdcdfd
Joel Falcou <joel.falcou@lri.fr>
2021-11-08 21:12:36 +0100

    Test clang and libc++

--- cmake/toolchain/clang_libcpp.x86.cmake
+++ cmake/toolchain/clang_libcpp.x86.cmake
@@ -3,8 +3,8 @@
 ##  Copyright : EVE Contributors & Maintainers
 ##  SPDX-License-Identifier: MIT
 ##==================================================================================================
-set(CMAKE_C_COMPILER    clang-12   )
-set(CMAKE_CXX_COMPILER  clang++-12 )
+set(CMAKE_C_COMPILER    clang   )
+set(CMAKE_CXX_COMPILER  clang++ )
 
 set(CMAKE_CXX_FLAGS         "-stdlib=libc++ -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
 set(CMAKE_EXE_LINKER_FLAGS  "-stdlib=libc++")
