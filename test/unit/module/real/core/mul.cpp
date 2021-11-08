2e2a16b578e983f389a46b8a05aaeb84153ad4c7
jtlap <jtlapreste@gmail.com>
2021-11-08 21:50:20 +0100

    uncommenting other mul tests

--- test/unit/module/real/core/mul.cpp
+++ test/unit/module/real/core/mul.cpp
@@ -15,143 +15,143 @@
 #include <eve/function/diff/mul.hpp>
 #include <eve/function/is_gtz.hpp>
 
-// //==================================================================================================
-// //==  Types tests
-// //==================================================================================================
-// EVE_TEST_TYPES( "Check return types of mul"
-//           , eve::test::simd::all_types
-//           )
-// <typename T>(eve::as<T>)
-// {
-//   using v_t = eve::element_type_t<T>;
+//==================================================================================================
+//==  Types tests
+//==================================================================================================
+EVE_TEST_TYPES( "Check return types of mul"
+          , eve::test::simd::all_types
+          )
+<typename T>(eve::as<T>)
+{
+  using v_t = eve::element_type_t<T>;
 
-//   //regular
-//   TTS_EXPR_IS( eve::mul(T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::mul(T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), v_t()) , v_t);
+  //regular
+  TTS_EXPR_IS( eve::mul(T(), T()  ) , T);
+  TTS_EXPR_IS( eve::mul(T(), v_t()) , T);
+  TTS_EXPR_IS( eve::mul(v_t(), T()) , T);
+  TTS_EXPR_IS( eve::mul(v_t(), v_t()) , v_t);
 
-//   //saturated
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t()) , v_t);
+  //saturated
+  TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T()  ) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(T(), v_t()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t()) , v_t);
 
-//   //promoted
-//   using pv_t = eve::detail::upgrade_t<v_t>;
-//   using pT = eve::wide<pv_t, eve::cardinal_t<T>>;
-//   TTS_EXPR_IS( eve::promoted(eve::mul)(T(), T()  ) , pT);
-//   TTS_EXPR_IS( eve::promoted(eve::mul)(T(), v_t()) , pT);
-//   TTS_EXPR_IS( eve::promoted(eve::mul)(v_t(), T()) , pT);
-//   TTS_EXPR_IS( eve::promoted(eve::mul)(v_t(), v_t()) , pv_t);
+  //promoted
+  using pv_t = eve::detail::upgrade_t<v_t>;
+  using pT = eve::wide<pv_t, eve::cardinal_t<T>>;
+  TTS_EXPR_IS( eve::promoted(eve::mul)(T(), T()  ) , pT);
+  TTS_EXPR_IS( eve::promoted(eve::mul)(T(), v_t()) , pT);
+  TTS_EXPR_IS( eve::promoted(eve::mul)(v_t(), T()) , pT);
+  TTS_EXPR_IS( eve::promoted(eve::mul)(v_t(), v_t()) , pv_t);
 
-//   //conditionnal
-//   TTS_EXPR_IS( eve::mul[eve::logical<T>()](T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::mul[eve::logical<T>()](T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::mul[eve::logical<T>()](v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(v_t(), T()) , T);
+  //conditionnal
+  TTS_EXPR_IS( eve::mul[eve::logical<T>()](T(), T()  ) , T);
+  TTS_EXPR_IS( eve::mul[eve::logical<T>()](T(), v_t()) , T);
+  TTS_EXPR_IS( eve::mul[eve::logical<T>()](v_t(), T()) , T);
+  TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](T(), T()  ) , T);
+  TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](T(), v_t()) , T);
+  TTS_EXPR_IS( eve::mul[eve::logical<v_t>()](v_t(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(T(), T()  ) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(T(), v_t()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<T>()])(v_t(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(T(), T()  ) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(T(), v_t()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul[eve::logical<v_t>()])(v_t(), T()) , T);
 
 
-//   //multi
-//   TTS_EXPR_IS( eve::mul(T(), T(), T()  )  , T);
-//   TTS_EXPR_IS( eve::mul(T(), v_t(), T())  , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), T(), T())  , T);
-//   TTS_EXPR_IS( eve::mul(T(), T(), v_t() ) , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::mul(v_t(), v_t(), v_t()) , v_t);
+  //multi
+  TTS_EXPR_IS( eve::mul(T(), T(), T()  )  , T);
+  TTS_EXPR_IS( eve::mul(T(), v_t(), T())  , T);
+  TTS_EXPR_IS( eve::mul(v_t(), T(), T())  , T);
+  TTS_EXPR_IS( eve::mul(T(), T(), v_t() ) , T);
+  TTS_EXPR_IS( eve::mul(v_t(), v_t(), T()) , T);
+  TTS_EXPR_IS( eve::mul(v_t(), T(), v_t()) , T);
+  TTS_EXPR_IS( eve::mul(v_t(), v_t(), v_t()) , v_t);
 
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T(), T()  ) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(T(), v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T(), v_t() ) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t(), T()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T(), v_t()) , T);
-//   TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t(), v_t()) , v_t);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T(), T()  ) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(T(), v_t(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(T(), T(), v_t() ) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t(), T()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), T(), v_t()) , T);
+  TTS_EXPR_IS( eve::saturated(eve::mul)(v_t(), v_t(), v_t()) , v_t);
 
-//   //diff
-//   if constexpr(eve::floating_value<T>)
-//   {
-//     using eve::diff_1st;
-//     using eve::diff_2nd;
-//     TTS_EXPR_IS( diff_1st(eve::mul)(T(), T()  ) , T);
-//     TTS_EXPR_IS( diff_1st(eve::mul)(T(), v_t()) , T);
-//     TTS_EXPR_IS( diff_1st(eve::mul)(v_t(), T()) , T);
-//     TTS_EXPR_IS( diff_1st(eve::mul)(v_t(), v_t()) , v_t);
-//     TTS_EXPR_IS( diff_2nd(eve::mul)(T(), T()  ) , T);
-//     TTS_EXPR_IS( diff_2nd(eve::mul)(T(), v_t()) , T);
-//     TTS_EXPR_IS( diff_2nd(eve::mul)(v_t(), T()) , T);
-//     TTS_EXPR_IS( diff_2nd(eve::mul)(v_t(), v_t()) , v_t);
-//   }
-// };
+  //diff
+  if constexpr(eve::floating_value<T>)
+  {
+    using eve::diff_1st;
+    using eve::diff_2nd;
+    TTS_EXPR_IS( diff_1st(eve::mul)(T(), T()  ) , T);
+    TTS_EXPR_IS( diff_1st(eve::mul)(T(), v_t()) , T);
+    TTS_EXPR_IS( diff_1st(eve::mul)(v_t(), T()) , T);
+    TTS_EXPR_IS( diff_1st(eve::mul)(v_t(), v_t()) , v_t);
+    TTS_EXPR_IS( diff_2nd(eve::mul)(T(), T()  ) , T);
+    TTS_EXPR_IS( diff_2nd(eve::mul)(T(), v_t()) , T);
+    TTS_EXPR_IS( diff_2nd(eve::mul)(v_t(), T()) , T);
+    TTS_EXPR_IS( diff_2nd(eve::mul)(v_t(), v_t()) , v_t);
+  }
+};
 
-// //==================================================================================================
-// //==  mul simd tests
-// //==================================================================================================
-// EVE_TEST( "Check behavior of mul on wide"
-//             , eve::test::simd::integers
-//             , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
-//                                   , eve::test::randoms(eve::valmin, eve::valmax)
-//                                   , eve::test::randoms(eve::valmin, eve::valmax)
-//                                   )
-//             )
-// <typename T>(T const& a0, T const& a1, T const& a2)
-// {
-//   using eve::mul;
-//   using eve::saturated;
-//   using eve::detail::map;
-//   TTS_EQUAL( mul(a0, a2), map([](auto e, auto f) { return mul(e, f); }, a0, a2));
-//   TTS_EQUAL( saturated(mul)(a0, a2), map([&](auto e, auto f) { return saturated(mul)(e, f); }, a0, a2));
-//   TTS_EQUAL( mul(a0, a1, a2), map([&](auto e, auto f, auto g) { return mul(mul(e, f), g); }, a0, a1, a2));
-//   TTS_EQUAL( saturated(mul)(a0, a1, a2), map([&](auto e, auto f, auto g) { return saturated(mul)(saturated(mul)(e, f), g); }, a0, a1, a2));
-// };
+//==================================================================================================
+//==  mul simd tests
+//==================================================================================================
+EVE_TEST( "Check behavior of mul on wide"
+            , eve::test::simd::integers
+            , eve::test::generate ( eve::test::randoms(eve::valmin, eve::valmax)
+                                  , eve::test::randoms(eve::valmin, eve::valmax)
+                                  , eve::test::randoms(eve::valmin, eve::valmax)
+                                  )
+            )
+<typename T>(T const& a0, T const& a1, T const& a2)
+{
+  using eve::mul;
+  using eve::saturated;
+  using eve::detail::map;
+  TTS_EQUAL( mul(a0, a2), map([](auto e, auto f) { return mul(e, f); }, a0, a2));
+  TTS_EQUAL( saturated(mul)(a0, a2), map([&](auto e, auto f) { return saturated(mul)(e, f); }, a0, a2));
+  TTS_EQUAL( mul(a0, a1, a2), map([&](auto e, auto f, auto g) { return mul(mul(e, f), g); }, a0, a1, a2));
+  TTS_EQUAL( saturated(mul)(a0, a1, a2), map([&](auto e, auto f, auto g) { return saturated(mul)(saturated(mul)(e, f), g); }, a0, a1, a2));
+};
 
-// EVE_TEST( "Check behavior of mul on wide"
-//             , eve::test::simd::ieee_reals
-//             , eve::test::generate ( eve::test::randoms(-100.0, 100.0)
-//                                   , eve::test::randoms(-100.0, 100.0)
-//                                   , eve::test::randoms(-100.0, 100.0)
-//                                   )
-//             )
-// <typename T>(T const& a0, T const& a1, T const& a2)
-// {
-//   using eve::mul;
-//   using eve::detail::map;
+EVE_TEST( "Check behavior of mul on wide"
+            , eve::test::simd::ieee_reals
+            , eve::test::generate ( eve::test::randoms(-100.0, 100.0)
+                                  , eve::test::randoms(-100.0, 100.0)
+                                  , eve::test::randoms(-100.0, 100.0)
+                                  )
+            )
+<typename T>(T const& a0, T const& a1, T const& a2)
+{
+  using eve::mul;
+  using eve::detail::map;
 
-//   TTS_EQUAL( eve::diff_1st(mul)(a0, a2), a2);
-//   TTS_EQUAL( eve::diff_2nd(mul)(a0, a2), a0);
-//   TTS_EQUAL( eve::diff_3rd(mul)(a0, a1, a2), a1*a0);
+  TTS_EQUAL( eve::diff_1st(mul)(a0, a2), a2);
+  TTS_EQUAL( eve::diff_2nd(mul)(a0, a2), a0);
+  TTS_EQUAL( eve::diff_3rd(mul)(a0, a1, a2), a1*a0);
 
-// };
+};
 
-// //==================================================================================================
-// //==  conditional mul tests on simd
-// //==================================================================================================
-// auto mini = [] < typename T > (eve::as<T> const &){ return std::is_signed_v<eve::element_type_t<T>> ? -128 : 0;};
+//==================================================================================================
+//==  conditional mul tests on simd
+//==================================================================================================
+auto mini = [] < typename T > (eve::as<T> const &){ return std::is_signed_v<eve::element_type_t<T>> ? -128 : 0;};
 
-// EVE_TEST( "Check behavior of mul on signed types"
-//         , eve::test::simd::signed_types
-//         , eve::test::generate ( eve::test::randoms(mini, 127)
-//                               , eve::test::randoms(mini, 127)
-//                               , eve::test::randoms(mini, 127)
-//                               )
-//         )
-// <typename T>( T const& a0, T const& a1, T const& a2)
-// {
-//   using eve::mul;
-//   using eve::saturated;
-//   using eve::detail::map;
-//   TTS_EQUAL( mul[a2 > T(64)](a0, a1), map([](auto e, auto f, auto g) {return g > 64 ? mul(e, f) : e ; }, a0, a1, a2));
-//   TTS_EQUAL( saturated(mul[a2 > T(64)])(a0, a1), map([](auto e, auto f, auto g) { return  g > 64 ? saturated(mul)(e, f): e; }, a0, a1, a2));
-// };
+EVE_TEST( "Check behavior of mul on signed types"
+        , eve::test::simd::signed_types
+        , eve::test::generate ( eve::test::randoms(mini, 127)
+                              , eve::test::randoms(mini, 127)
+                              , eve::test::randoms(mini, 127)
+                              )
+        )
+<typename T>( T const& a0, T const& a1, T const& a2)
+{
+  using eve::mul;
+  using eve::saturated;
+  using eve::detail::map;
+  TTS_EQUAL( mul[a2 > T(64)](a0, a1), map([](auto e, auto f, auto g) {return g > 64 ? mul(e, f) : e ; }, a0, a1, a2));
+  TTS_EQUAL( saturated(mul[a2 > T(64)])(a0, a1), map([](auto e, auto f, auto g) { return  g > 64 ? saturated(mul)(e, f): e; }, a0, a1, a2));
+};
 
 
 //==================================================================================================
@@ -165,27 +165,9 @@ EVE_TEST( "Check behavior of promote mul on all types"
         )
 <typename T>( T const& a0, T const& a1 )
 {
-  if constexpr ((sizeof(eve::element_type_t<T>) < 8) && (sizeof(eve::element_type_t<T>) !=  1) && eve::cardinal_v<T> !=  1)
-  {
-    using eve::mul;
-    using eve::detail::map;
-    using v_t =  eve::element_type_t<T>;
-    using pv_t = eve::detail::upgrade_t<v_t>;
-//     auto a = eve::promoted(mul)(a0, a1);
-//     std::cout << a << std::endl;
-//     TTS_EQUAL(0, 0);
-//    TTS_EQUAL( eve::promoted(mul)(a0, a2), map([](auto e, auto f) { return mul(pv_t(e), pv_t(f)); }, a0, a2));
-    TTS_EQUAL( eve::promoted(mul)(a0, a1), map([](auto e, auto f) { return mul(pv_t(e), pv_t(f)); }, a0, a1));
-//   constexpr int S = 8;
-//   eve::wide < uint32_t, eve::fixed<S>> a(eve::valmax(eve::as<uint32_t>()));
-//   eve::wide < uint32_t, eve::fixed<S>> b{[](auto i, auto){return i; }};
-//   std::cout << std::hex << " a "<< a << std::endl;
-//   std::cout <<" b "<< b << std::endl;
-//   auto z = eve::promoted(eve::mul)(a, b);
-//   std::cout << " pa*pb "<< z << std::endl;
-//   eve::wide < uint64_t, eve::fixed<S>> la(eve::valmax(eve::as<uint32_t>()));
-//   eve::wide < uint64_t, eve::fixed<S>> lb{[](auto i, auto){return i; }};
-//   std::cout << " la*lb "<< la*lb << std::endl;
-  }
-  else TTS_EQUAL(0, 0);
+  using eve::mul;
+  using eve::detail::map;
+  using v_t =  eve::element_type_t<T>;
+  using pv_t = eve::detail::upgrade_t<v_t>;
+  TTS_EQUAL( eve::promoted(mul)(a0, a1), map([](auto e, auto f) { return mul(pv_t(e), pv_t(f)); }, a0, a1));
 };
