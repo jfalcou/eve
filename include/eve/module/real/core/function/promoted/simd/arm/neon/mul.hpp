0af478a29d1850563e957ab9c827b48d1d554f72
jtlap <jtlapreste@gmail.com>
2021-11-11 17:17:00 +0100

    bad return type

--- include/eve/module/real/core/function/promoted/simd/arm/neon/mul.hpp
+++ include/eve/module/real/core/function/promoted/simd/arm/neon/mul.hpp
@@ -11,7 +11,7 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/detail/category.hpp>
 #include <eve/function/combine.hpp>
-
+#include <eve/function/convert.hpp>
 
 namespace eve::detail
 {
@@ -24,17 +24,18 @@ namespace eve::detail
   requires arm_abi<abi_t<T, N>>
   {
    constexpr auto c = categorize<wide<T, N>>();
+   using r_t = wide<upgrade_t<T> , N>>;
           if constexpr(sizeof(T) == 8)            return a*b;
-    else  if constexpr(c == category::int8x8   )  return vmull_s8(a, b);
-    else  if constexpr(c == category::int16x4  )  return vmull_s16(a, b);
-    else  if constexpr(c == category::int32x2  )  return vmull_s32(a, b);
-    else  if constexpr(c == category::uint8x8  )  return vmull_u8(a, b);
-    else  if constexpr(c == category::uint16x4 )  return vmull_u16(a, b);
-    else  if constexpr(c == category::uint32x2 )  return vmull_u32(a, b);
+    else  if constexpr(c == category::int8x8   )  return r_t(vmull_s8(a, b));
+    else  if constexpr(c == category::int16x4  )  return r_t(vmull_s16(a, b));
+    else  if constexpr(c == category::int32x2  )  return r_t(vmull_s32(a, b));
+    else  if constexpr(c == category::uint8x8  )  return r_t(vmull_u8(a, b));
+    else  if constexpr(c == category::uint16x4 )  return r_t(vmull_u16(a, b));
+    else  if constexpr(c == category::uint32x2 )  return r_t(vmull_u32(a, b));
     else
     {
-      auto[la,ha] = a.slice();
-      auto[lb,hb] = b.slice();
+      auto [la, ha] = a.slice();
+      auto [lb, hb] = b.slice();
       return eve::combine( promoted(mul)(la,lb), promoted(mul)(ha,hb) );
     }
   }
