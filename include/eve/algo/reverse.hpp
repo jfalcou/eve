20b37314b2e35be0533e6bc604166be74a1f1395
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-11-16 22:06:02 +0000

    FIX-1060: reverse copy works

--- include/eve/algo/reverse.hpp
+++ include/eve/algo/reverse.hpp
@@ -12,24 +12,45 @@
 
 namespace eve::algo
 {
+  //================================================================================================
+  //! @addtogroup eve.algo
+  //! @{
+  //!  @var reverse_copy
+  //!
+  //!  @brief version of std::reverse_copy
+  //!    * Accepts two things zipping together to range of pair.
+  //!    * Also can accept a `zipped_range_pair`.
+  //!    * returns void.
+  //!    * default unrolling is 1.
+  //!    * will align by default.
+  //!    * will do conversions if necessary.
+  //!
+  //!  NOTE: `reverese_copy(it, range)` works but it's the same as
+  //!        `reverse_copy(views::zip(it, range))`, so `it` will be
+  //!        treated as `first` not `last`.
+  //!
+  //!   **Required header:** `#include <eve/algo/reverse.hpp>`
+  //!
+  //! @}
+  //================================================================================================
+
   template <typename TraitsSupport>
   struct reverse_copy_ : TraitsSupport
   {
-    template <typename R1, typename R2>
-      requires zip_to_range<R1, R2>
-    EVE_FORCEINLINE void operator()(R1&& r1, R2&& r2) const
-    {
-      return eve::algo::copy[TraitsSupport::get_traits()]
-        (views::reverse(EVE_FWD(r1)), EVE_FWD(r2));
-    }
-
     template <zipped_range_pair R>
     EVE_FORCEINLINE void operator()(R r) const
     {
       auto [from, to] = r;
-      return operator()(from, to);
+      return eve::algo::copy[TraitsSupport::get_traits()](views::reverse(from), to);
+    }
+
+    template <typename R1, typename R2>
+      requires zip_to_range<R1, R2>
+    EVE_FORCEINLINE void operator()(R1&& r1, R2&& r2) const
+    {
+      return operator()(views::zip(EVE_FWD(r1), EVE_FWD(r2)));
     }
   };
 
-  inline constexpr auto reverse_copy = function_with_traits<reverse_copy_>[algo::unroll<1>][algo::no_aligning];
+  inline constexpr auto reverse_copy = function_with_traits<reverse_copy_>[algo::unroll<1>];
 }
