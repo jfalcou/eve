471c132429133d64260ccfd56d5b36011df83f01
jtlap <jtlapreste@gmail.com>
2021-12-04 08:33:14 +0100

    range bessel_in

--- test/unit/module/real/special/cyl_bessel_in.cpp
+++ test/unit/module/real/special/cyl_bessel_in.cpp
@@ -154,7 +154,7 @@ EVE_TEST( "Check behavior of cyl_bessel_in on wide with integral order"
 EVE_TEST( "Check behavior of cyl_bessel_in on wide with non integral order"
         , eve::test::simd::ieee_reals
         , eve::test::generate(eve::test::randoms(0.0, 10.0)
-                             , eve::test::randoms(0.0, 60.0))
+                             , eve::test::randoms(0.0, 30.0))
         )
   <typename T>(T n, T a0 )
 {
