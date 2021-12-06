67143fe3e9df9b679db5735ea351fdec037e716d
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-12-06 00:11:55 +0000

    FIX-947: compress switch 8

--- include/eve/detail/function/simd/common/compress_store_impl_switch.hpp
+++ include/eve/detail/function/simd/common/compress_store_impl_switch.hpp
@@ -23,7 +23,7 @@ namespace eve::detail
                                  logical<wide<U, N>> mask,
                                  Ptr ptr) noexcept
   {
-    constexpr bool has_aggregation = has_aggregated_abi_v<wide<T, N>> || has_aggregated_abi_v<wide<U, N>> || N() > 4;
+    constexpr bool has_aggregation = has_aggregated_abi_v<wide<T, N>> || has_aggregated_abi_v<wide<U, N>> || N() > 8;
 
          if constexpr ( C::is_complete  && !C::is_inverted ) return as_raw_pointer(ptr);
     else if constexpr ( has_aggregation && !C::is_complete )
@@ -82,6 +82,48 @@ namespace eve::detail
         count += last_set ? 1 : 0;
         store(v, ptr);
         return as_raw_pointer(ptr) + count;
+      }
+      else if constexpr ( N() == 8 )
+      {
+        const int  num1       = mmask & 7;
+        const bool last_set1  = (bool) (mmask & 8);
+        const int  num2       = (mmask & 0x70);
+        const bool last_set2  = (bool) (mmask & 0x80);
+
+        int count1, count2;
+
+        switch (num1) {
+          case 0b000: { count1 = 0; v = v[pattern<3, 0, 0, 0, 4, 5, 6, 7>]; break; }
+          case 0b001: { count1 = 1; v = v[pattern<0, 3, 0, 0, 4, 5, 6, 7>]; break; }
+          case 0b010: { count1 = 1; v = v[pattern<1, 3, 0, 0, 4, 5, 6, 7>]; break; }
+          case 0b011: { count1 = 2; v = v[pattern<0, 1, 3, 0, 4, 5, 6, 7>]; break; }
+          case 0b100: { count1 = 1; v = v[pattern<2, 3, 0, 0, 4, 5, 6, 7>]; break; }
+          case 0b101: { count1 = 2; v = v[pattern<0, 2, 3, 0, 4, 5, 6, 7>]; break; }
+          case 0b110: { count1 = 2; v = v[pattern<1, 2, 3, 0, 4, 5, 6, 7>]; break; }
+          case 0b111: { count1 = 3;                                         break; }
+          #if defined(SPY_COMPILER_IS_CLANG) or defined(SPY_COMPILER_IS_GCC)
+          default: __builtin_unreachable();
+          #endif
+        }
+        switch (num2) {
+          case 0b000'0000: { count2 = 0; v = v[pattern<0, 1, 2, 3, 7, 7, 7, 7>]; break; }
+          case 0b001'0000: { count2 = 1; v = v[pattern<0, 1, 2, 3, 4, 7, 7, 7>]; break; }
+          case 0b010'0000: { count2 = 1; v = v[pattern<0, 1, 2, 3, 5, 7, 7, 7>]; break; }
+          case 0b011'0000: { count2 = 2; v = v[pattern<0, 1, 2, 3, 4, 5, 7, 7>]; break; }
+          case 0b100'0000: { count2 = 1; v = v[pattern<0, 1, 2, 3, 6, 7, 7, 7>]; break; }
+          case 0b101'0000: { count2 = 2; v = v[pattern<0, 1, 2, 3, 4, 6, 7, 7>]; break; }
+          case 0b110'0000: { count2 = 2; v = v[pattern<0, 1, 2, 3, 5, 6, 7, 7>]; break; }
+          case 0b111'0000: { count2 = 3;                                         break; }
+          #if defined(SPY_COMPILER_IS_CLANG) or defined(SPY_COMPILER_IS_GCC)
+          default: __builtin_unreachable();
+          #endif
+        }
+
+        eve::store(v, ptr);
+        T* after_first = as_raw_pointer(ptr) + count1 + (last_set1 ? 1 : 0);
+
+        eve::store(v.slice(upper_), after_first);
+        return after_first + count2 + (last_set2 ? 1 : 0);
       }
     }
   }
