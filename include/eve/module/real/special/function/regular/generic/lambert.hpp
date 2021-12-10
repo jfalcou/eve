0fa33f282351bb8b6ae8d086156daf00915a1b48
jtlap <jtlapreste@gmail.com>
2021-12-10 13:24:22 +0100

    lambert

--- include/eve/module/real/special/function/regular/generic/lambert.hpp
+++ include/eve/module/real/special/function/regular/generic/lambert.hpp
@@ -53,26 +53,43 @@ namespace eve::detail
 
     if constexpr( has_native_abi_v<T> )
     {
-      auto serie = [](auto r){
-        constexpr std::array < elt_t, 12> P = {
-          -1.0,
-          2.331643981597124203363536062168,
-          -1.812187885639363490240191647568,
-          1.936631114492359755363277457668,
-          -2.353551201881614516821543561516,
-          3.066858901050631912893148922704,
-          -4.175335600258177138854984177460,
-          5.858023729874774148815053846119,
-          -8.401032217523977370984161688514,
-          12.250753501314460424,
-          -18.100697012472442755,
-          27.029044799010561650
-        };
-        return horner(r, P);
+      auto serie = [](auto z, auto r){
+        if constexpr(sizeof(elt_t) == 8)
+        {
+          constexpr std::array < elt_t, 12> P = {
+            -1.0,
+            2.331643981597124203363536062168,
+            -1.812187885639363490240191647568,
+            1.936631114492359755363277457668,
+            -2.353551201881614516821543561516,
+            3.066858901050631912893148922704,
+            -4.175335600258177138854984177460,
+            5.858023729874774148815053846119,
+            -8.401032217523977370984161688514,
+            12.250753501314460424,
+            -18.100697012472442755,
+            27.029044799010561650
+          };
+          return horner(r, P);
+        }
+        else
+        {
+         constexpr std::array < elt_t, 8> P = {
+           -1.0f,
+           1.5f,
+           2.6666666666666666667f,
+           5.2083333333333333333f,
+           10.8f,
+           23.343055555555555556f,
+           52.012698412698412698f,
+           118.62522321428571429f
+         };
+         return if_else(r > 0, horner(z, P), allbits);
+        }
       };
       T q = x+T(0.367879441171442);
-      auto lambert0_small = [&serie](auto q){ //branch 0 q <= 1.0e-3
-        return serie(eve::sqrt(q));
+      auto lambert0_small = [&serie, x](auto q){ //branch 0 q <= 1.0e-3
+        return serie(x, eve::sqrt(q));
       };
       auto lambert0_other = [&halley](auto x, auto q){ // branch 0 q <= 1.0e'3
         auto p = eve::sqrt(T(5.436563656918090)*q);
@@ -83,10 +100,12 @@ namespace eve::detail
         return halley(x, init, 10);
       };
       auto lambert1 = [&halley, &serie](auto x, auto q, auto positivex){ // branch 1 q > 0
+        std::cout << "lambert1 q " << q << std::endl;
         T r = -eve::sqrt(q);
+        std::cout << "lambert1 r " << r << std::endl;
         auto test = (x < T(-1.0e-6));
         T w1(0);
-        if(eve::any(test)) w1 = serie(r);
+        if(eve::any(test)) w1 = serie(x, r);
         if(eve::all(test)&&eve::all(q < T(3.0e-3))) return w1;
         T l1 = log(-x);
         T l2 = log(-l1);
@@ -98,10 +117,16 @@ namespace eve::detail
       r =  if_else(is_eqz(x), zero, r); //zero case treated here
       r =  if_else(x == inf(as(x)), x, r);
       auto notdone =  is_nlez(q)&&(q != inf(as(x)));
-      notdone = next_interval(lambert0_small, notdone, q < T(1.0e-3), r, q);
+      std::cout << "notdone "<< notdone << std::endl;
+      std::cout << "q       "<< q       << std::endl;
+      std::cout << "r       "<< r       << std::endl;
       if(eve::any(notdone))
       {
-        notdone = last_interval(lambert0_other, q >= T(1.0e-3), r, x, q);
+        notdone = next_interval(lambert0_small, notdone, q < T(1.0e-3), r, q);
+        if(eve::any(notdone))
+        {
+          notdone = last_interval(lambert0_other, q >= T(1.0e-3), r, x, q);
+        }
       }
       auto positivex = is_positive(x);
       if (eve::all(positivex)) return kumi::make_tuple(r, r);
