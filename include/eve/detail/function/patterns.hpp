d8e280296f0553e58e88cad5b76ff7cc19b68ed8
Joel Falcou <joel.falcou@lri.fr>
2022-01-02 20:47:02 +0100

    Move broadcast out of detail

--- include/eve/detail/function/patterns.hpp
+++ include/eve/detail/function/patterns.hpp
@@ -8,11 +8,11 @@
 #pragma once
 
 #include <eve/detail/function/swizzle.hpp>
-#include <eve/detail/function/broadcast.hpp>
+#include <eve/function/broadcast_group.hpp>
+#include <eve/function/broadcast.hpp>
 #include <eve/detail/function/swap_adjacent_groups.hpp>
 #include <eve/detail/function/slide_left.hpp>
 #include <eve/detail/function/slide_right.hpp>
-#include <eve/function/broadcast_group.hpp>
 #include <eve/detail/function/reverse.hpp>
 #include <eve/detail/function/simd/common/patterns.hpp>
 
