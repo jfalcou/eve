c3e3e7f72faabb0a6a2fc3a21f8e7902f7986849
jtlap <jtlapreste@gmail.com>
2022-01-07 10:23:15 +0100

    numeric

---
+++ include/eve/module/arithmetic/numeric/negabsmax.hpp
@@ -0,0 +1,11 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/module/arithmetic/regular/negabsmax.hpp>
+#include <eve/module/arithmetic/numeric/impl/negabsmax.hpp>
