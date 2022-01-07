2f73a2f5b9c5eb0d01437b4c6b802bd03d98fcea
jtlap <jtlapreste@gmail.com>
2022-01-07 18:13:08 +0100

    arithmetic/diff

---
+++ include/eve/module/arithmetic/diff/frac.hpp
@@ -0,0 +1,11 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/module/arithmetic/regular/frac.hpp>
+#include <eve/module/arithmetic/diff/impl/frac.hpp>
---
+++ include/eve/module/arithmetic/diff/min.hpp
@@ -0,0 +1,11 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/module/arithmetic/regular/min.hpp>
+#include <eve/module/arithmetic/diff/impl/min.hpp>
