d858ab51d8c91b4dc97b00a8eace13f155726ea7
Joel Falcou <joel.falcou@lri.fr>
2022-01-07 22:43:53 +0100

    Add doc for eve::platform

--- include/eve/platform.hpp
+++ include/eve/platform.hpp
@@ -12,28 +12,43 @@
 
 namespace eve
 {
+  //================================================================================================
+  //! @addtogroup arch
+  //! @{
+  //! @struct platform
+  //! @brief  Platform specific constexpr information
+  //!
+  //! Provides constexpr information about the status (supported or not) of various platform
+  //! computing capability.
+  //! @}
+  //================================================================================================
   struct platform
   {
+    //! @brief Evaluates to `true` if current platform supports NaN values
 #if defined(EVE_NO_NANS)
     static constexpr bool supports_nans = false;
 #else
     static constexpr bool supports_nans = true;
 #endif
 
+    //! @brief Evaluates to `true` if current platform supports infinites values
 #if defined(EVE_NO_INFINITIES)
     static constexpr bool supports_infinites = false;
 #else
     static constexpr bool supports_infinites = true;
 #endif
 
+    //! @brief Evaluates to `true` if current platform supports invalid values
     static constexpr bool supports_invalids = supports_infinites && supports_nans;
 
+    //! @brief Evaluates to `true` if current platform supports negative 0
 #if defined(EVE_NO_MINUSZERO)
     static constexpr bool supports_negative_zero = false;
 #else
     static constexpr bool supports_negative_zero = true;
 #endif
 
+    //! @brief Evaluates to `true` if current platform supports denormal values
 #if defined(EVE_NO_DENORMALS)
     static constexpr bool supports_denormals = false;
 #else
