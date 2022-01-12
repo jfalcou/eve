3f37c7e0985a05e8ffeb29aedf532ba2410c0125
jtlap <jtlapreste@gmail.com>
2022-01-12 13:40:20 +0100

    radindeg and deginrad -> math

--- include/eve/function/deginrad.hpp
+++ include/eve/function/deginrad.hpp
@@ -7,4 +7,4 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/module/arithmetic/regular/deginrad.hpp>
+#include <eve/module/math/regular/deginrad.hpp>
