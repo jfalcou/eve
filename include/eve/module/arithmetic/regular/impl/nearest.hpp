7eb3537a7b0b98879f8037bbb398b516983b2c53
jtlap <jtlapreste@gmail.com>
2022-01-13 17:51:06 +0100

    missing include

--- include/eve/module/arithmetic/regular/impl/nearest.hpp
+++ include/eve/module/arithmetic/regular/impl/nearest.hpp
@@ -11,6 +11,7 @@
 #include <eve/function/raw.hpp>
 #include <eve/function/abs.hpp>
 #include <eve/function/convert.hpp>
+#include <eve/function/converter.hpp>
 #include <eve/function/bitofsign.hpp>
 #include <eve/function/bit_xor.hpp>
 #include <eve/constant/twotonmb.hpp>
