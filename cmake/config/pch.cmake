80f951a68eed6fd817ab4412fbb2d44e75858670
Joel Falcou <joel.falcou@lri.fr>
2022-01-13 09:25:11 +0100

    Fix #1136 - Bench compilation issues (#1152)
    Benchmarks now use their own PCH

--- cmake/config/pch.cmake
+++ cmake/config/pch.cmake
@@ -7,14 +7,23 @@
 ##==================================================================================================
 ## Setup PCH
 ##==================================================================================================
-add_executable(test_pch $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/test_pch.cpp>)
-add_executable(doc_pch  $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/doc_pch.cpp> )
+add_executable(bench_pch  $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/benchmarks/bench_pch.cpp> )
+add_executable(test_pch   $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/test_pch.cpp>)
+add_executable(doc_pch    $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/doc_pch.cpp> )
 
+target_link_libraries(bench_pch PUBLIC eve_bench)
 target_link_libraries(test_pch  PUBLIC eve_test)
 target_link_libraries(doc_pch   PUBLIC eve_test)
 
-set_property( TARGET test_pch PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/unit" )
-set_property( TARGET doc_pch  PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/unit" )
+set_property( TARGET bench_pch  PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bench" )
+set_property( TARGET test_pch   PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/unit" )
+set_property( TARGET doc_pch    PROPERTY RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/unit" )
+
+set_target_properties ( bench_pch PROPERTIES
+                        EXCLUDE_FROM_DEFAULT_BUILD TRUE
+                        EXCLUDE_FROM_ALL TRUE
+                        ${MAKE_UNIT_TARGET_PROPERTIES}
+                      )
 
 set_target_properties ( test_pch PROPERTIES
                         EXCLUDE_FROM_DEFAULT_BUILD TRUE
@@ -28,5 +37,8 @@ set_target_properties ( doc_pch PROPERTIES
                         ${MAKE_UNIT_TARGET_PROPERTIES}
                       )
 
-target_precompile_headers(test_pch PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/test.hpp>)
-target_precompile_headers(doc_pch  PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include/eve/wide.hpp>)
+target_precompile_headers(bench_pch PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/benchmarks/generators.hpp>)
+target_precompile_headers(bench_pch PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/benchmarks/experiment.hpp>)
+target_precompile_headers(bench_pch PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include/eve/wide.hpp>)
+target_precompile_headers(test_pch  PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/test/test.hpp>)
+target_precompile_headers(doc_pch   PRIVATE $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include/eve/wide.hpp>)
