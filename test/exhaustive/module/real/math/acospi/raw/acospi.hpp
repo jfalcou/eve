79582a799d03c721fd5f34c71d0b3a3af32787d1
jtlap <jtlapreste@gmail.com>
2022-01-15 17:02:57 +0100

    random acospi ulps for arm

--- test/exhaustive/module/real/math/acospi/raw/acospi.hpp
+++ test/exhaustive/module/real/math/acospi/raw/acospi.hpp
@@ -16,8 +16,8 @@ TTS_CASE_TPL("wide random check on acospi", EVE_TYPE)
 {
   using v_t = eve::element_type_t<T>;
   auto std_acospi = tts::vectorize<T>( [](auto e) { return eve::invpi(eve::as<v_t>())*std::acos(e); } );
-  double th = std::is_same_v<v_t, double> ? 4096.0 : 512.0;
+  double th = std::is_same_v<v_t, double> ? 4096.0 : 1024.0;
 
-  eve::exhaustive_producer<T> p(-1, 1);
+  eve::exhaustive_producer<T> p(-1, 0.99);
   TTS_ULP_RANGE_CHECK(p, std_acospi, eve::raw(eve::acospi), th);
 }
