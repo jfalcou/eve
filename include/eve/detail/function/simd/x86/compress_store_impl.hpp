65f3b153d67b2660d8fa8678f79aade923ce35ab
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-01-15 00:53:33 +0000

    FIX-1013: compress used for x86 up to 16

--- include/eve/detail/function/simd/x86/compress_store_impl.hpp
+++ include/eve/detail/function/simd/x86/compress_store_impl.hpp
@@ -85,66 +85,6 @@ namespace eve::detail
                           Ptr ptr) noexcept
     requires x86_abi<abi_t<T, N>> && (current_api <= avx2) && ( N() == 16 )
   {
-    using half_wide = wide<T, eve::fixed<8>>;
-
-    if constexpr ( sizeof(T) == 2 )
-    {
-      auto [l, h] = [&] {
-        if constexpr ( current_api == avx2 )
-        {
-          wide<T, N> to_left =  _mm256_shufflehi_epi16(v,       _MM_SHUFFLE(0, 3, 2, 1));
-                     to_left =  _mm256_shufflelo_epi16(to_left, _MM_SHUFFLE(0, 3, 2, 1));
-          v = eve::if_else[mask](v, to_left);
-          return v.slice();
-        }
-        else
-        {
-          auto [l, h] = v.slice();
-          auto [ml, mh] = mask.slice();
-          half_wide l_to_left = _mm_bsrli_si128(l, 2);
-          half_wide h_to_left = _mm_bsrli_si128(h, 2);
-          l = eve::if_else[ml](l, l_to_left);
-          h = eve::if_else[mh](h, h_to_left);
-          return std::pair{l, h};
-        }
-      }();
-
-      using pattern8 = typename wide<T, N>::template rebind<std::uint16_t, eve::fixed<8>>;
-
-      auto [lo_idx, lo_count, hi_idx, hi_count] = compress_store_swizzle_mask_num(mask);
-
-      auto lo_shuffle_ptr = eve::as_aligned(pattern_8_elements_bytes_v<std::uint16_t>[lo_idx].data(), eve::lane<8>);
-      auto hi_shuffle_ptr = eve::as_aligned(pattern_8_elements_bytes_v<std::uint16_t>[hi_idx].data(), eve::lane<8>);
-
-      pattern8 lo_shuffle{lo_shuffle_ptr};
-      pattern8 hi_shuffle{hi_shuffle_ptr};
-
-      l = _mm_shuffle_epi8(l, lo_shuffle);
-      h = _mm_shuffle_epi8(h, hi_shuffle);
-      eve::store(l, ptr);
-
-      T* res = unalign(ptr) + lo_count;
-      eve::store(h, res);
-      return res + hi_count;
-    }
-    else if constexpr ( sizeof(T) == 1 )
-    {
-      using pattern8 = typename wide<T, N>::template rebind<std::uint8_t, eve::fixed<8>>;
-
-      eve::wide<T, N> to_left = _mm_bsrli_si128(v, 1);
-      v = eve::if_else[mask]( v, to_left );
-
-      auto [lo_idx, lo_count, hi_idx, hi_count] = compress_store_swizzle_mask_num(mask);
-
-      pattern8 lo_shuffle{pattern_8_elements_bytes_v<std::uint8_t>[lo_idx].data()};
-      pattern8 hi_shuffle{pattern_8_elements_bytes_v<std::uint8_t>[hi_idx].data()};
-      hi_shuffle |= pattern8{0x08};  // adjust higher idxs +8
-
-      T* res = unalign(ptr);
-      _mm_storel_epi64((__m128i*)res, _mm_shuffle_epi8(v, lo_shuffle));
-      res += lo_count;
-      _mm_storel_epi64((__m128i*)res, _mm_shuffle_epi8(v, hi_shuffle));
-      return res + hi_count;
-    }
+    return compress_store_impl_using_masks(eve::ignore_none, v, mask, ptr);
   }
 }
--- include/eve/detail/function/simd/x86/compress_using_masks.hpp
+++ include/eve/detail/function/simd/x86/compress_using_masks.hpp
@@ -111,4 +111,71 @@ namespace eve::detail
     else compress_using_masks_to_left_(EVE_RETARGET(cpu_), v);
   }
 
+  struct compress_using_masks_shuffle_16
+  {
+    std::ptrdiff_t lo_idx, hi_idx;
+
+    template <typename T, typename N>
+    EVE_FORCEINLINE auto operator()(wide<T, N> v) const
+    {
+      using half_wide = wide<T, eve::fixed<N() / 2>>;
+
+      if constexpr ( kumi::product_type<T> )
+      {
+        auto mapped = kumi::map(*this, v);
+        auto first  = kumi::map([](auto pair) { return get<0>(pair); }, mapped);
+        auto second = kumi::map([](auto pair) { return get<1>(pair); }, mapped);
+        return kumi::tuple{half_wide{first}, half_wide{second}};
+      }
+      else
+      {
+        using u_t = eve::as_integer_t<T, unsigned>;
+        using pattern8 = wide<u_t, eve::fixed<8>>;
+
+        auto lo_shuffle_ptr = eve::as_aligned(pattern_8_elements_bytes_v<u_t>[lo_idx].data(), eve::lane<8>);
+        auto hi_shuffle_ptr = eve::as_aligned(pattern_8_elements_bytes_v<u_t>[hi_idx].data(), eve::lane<8>);
+
+        pattern8 lo_shuffle{lo_shuffle_ptr};
+        pattern8 hi_shuffle{hi_shuffle_ptr};
+
+        if constexpr (sizeof(T) == 1)
+        {
+          hi_shuffle |= pattern8{0x08};  // adjust higher idxs +8
+
+          half_wide l = _mm_shuffle_epi8(v, lo_shuffle);
+          half_wide h = _mm_shuffle_epi8(v, hi_shuffle);
+
+          return kumi::tuple{l, h};
+        }
+        else
+        {
+          auto [l, h] = v.slice();
+
+          l = _mm_shuffle_epi8(l, lo_shuffle);
+          h = _mm_shuffle_epi8(h, hi_shuffle);
+
+          return kumi::tuple{l, h};
+        }
+      }
+    }
+  };
+
+
+  template <typename T, typename N, typename U>
+  EVE_FORCEINLINE
+  auto compress_using_masks_(EVE_SUPPORTS(ssse3_), ignore_none_ c, wide<T, N> v, logical<wide<U, N>> mask)
+    requires ( N() == 16 ) && (max_scalar_size_v<T> <= 2)
+  {
+    if constexpr ( max_scalar_size_v<T> == 2 && current_api < avx ) return compress_using_masks_(EVE_RETARGET(cpu_), c, v, mask);
+    else
+    {
+      v = eve::if_else[mask]( v, compress_using_masks_to_left( v ) );
+      auto [lo_idx, lo_count, hi_idx, hi_count] = compress_store_swizzle_mask_num(mask);
+
+      auto [shuffled_lo, shuffled_hi] = compress_using_masks_shuffle_16{lo_idx, hi_idx}(v);
+
+      return kumi::tuple{ kumi::tuple{shuffled_lo, lo_count}, kumi::tuple{ shuffled_hi, hi_count } };
+    }
+  }
+
 }
