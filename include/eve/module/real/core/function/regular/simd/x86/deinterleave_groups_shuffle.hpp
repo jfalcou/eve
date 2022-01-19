e966e1ff562957543384838f1529feb8474c7ad2
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-01-19 02:28:48 +0000

    FIX-1170: x86 deinterleave 2

--- include/eve/module/real/core/function/regular/simd/x86/deinterleave_groups_shuffle.hpp
+++ include/eve/module/real/core/function/regular/simd/x86/deinterleave_groups_shuffle.hpp
@@ -23,12 +23,19 @@ namespace eve::detail
   deinterleave_groups_shuffle_(EVE_SUPPORTS(sse2_), wide<T, N> v0, wide<T, N> v1, fixed<G>)
     requires (G < N()) && x86_abi<abi_t<T, N>>
   {
-    constexpr auto et_size = sizeof(T) * G;
+    constexpr auto g_sz = sizeof(T) * G;
     constexpr auto n  = N() / G;
+    using w_t = wide<T, N>;
 
-    if constexpr (n == 2 && et_size == 8)
+    // Doing 2 is the most important case since we can deinterleave n / 2 and then deinterleave each half.
+    if constexpr ( n == 2 )
     {
-      if constexpr ( et_size == 8 && std::floating_point<T> )
+      // 16 bytes
+           if constexpr ( g_sz == 1                           ) return _mm_unpacklo_epi8 (v0, v1);
+      else if constexpr ( g_sz == 2                           ) return _mm_unpacklo_epi16(v0, v1);
+      else if constexpr ( g_sz == 4 && std::floating_point<T> ) return _mm_unpacklo_ps   (v0, v1);
+      else if constexpr ( g_sz == 4                           ) return _mm_unpacklo_epi32(v0, v1);
+      else if constexpr ( g_sz == 8 && std::floating_point<T> )
       {
         using doubles  = typename wide<T, N>::template rebind<double,  eve::fixed<2>>;
 
@@ -39,14 +46,17 @@ namespace eve::detail
 
         return {eve::bit_cast(res_l, eve::as(v0)), eve::bit_cast(res_h, eve::as(v0))};
       }
-      else if constexpr ( et_size == 8 )
-      {
-        wide<T, N> res_l = _mm_unpacklo_epi64(v0, v1);
-        wide<T, N> res_h = _mm_unpackhi_epi64(v0, v1);
-
-        return eve::combine(res_l, res_h);
-      }
+      else if constexpr ( g_sz == 8                             ) return {w_t{_mm_unpacklo_epi64(v0, v1)}, w_t{_mm_unpackhi_epi64(v0, v1)}};
+      // avx at least
+      else if constexpr ( g_sz == 16 && std::same_as<T, double> ) return {w_t{_mm256_permute2f128_pd   (v0, v1, 0x20)}, w_t{_mm256_permute2f128_pd   (v0, v1, 0x31)}};
+      else if constexpr ( g_sz == 16 && std::same_as<T, float>  ) return {w_t{_mm256_permute2f128_ps   (v0, v1, 0x20)}, w_t{_mm256_permute2f128_ps   (v0, v1, 0x31)}};
+      else if constexpr ( g_sz == 16                            ) return {w_t{_mm256_permute2f128_si256(v0, v1, 0x20)}, w_t{_mm256_permute2f128_si256(v0, v1, 0x31)}};
+      // avx512 at least
+      else if constexpr ( g_sz == 32 && std::same_as<T, double> ) return {w_t{_mm512_shuffle_f64x2(v0, v1, 0x44)}, w_t{_mm512_shuffle_f64x2(v0, v1, 0xee)}};
+      else if constexpr ( g_sz == 32 && std::same_as<T, float>  ) return {w_t{_mm512_shuffle_f32x4(v0, v1, 0x44)}, w_t{_mm512_shuffle_f32x4(v0, v1, 0xee)}};
+      else if constexpr ( g_sz == 32                            ) return {w_t{_mm512_shuffle_i32x4(v0, v1, 0x44)}, w_t{_mm512_shuffle_i32x4(v0, v1, 0xee)}};
     }
+    // Will aggregate for less than native or deinterleave each and then come back here
     else return deinterleave_groups_shuffle_(EVE_RETARGET(cpu_), v0, v1, eve::lane<G>);
   }
 }
