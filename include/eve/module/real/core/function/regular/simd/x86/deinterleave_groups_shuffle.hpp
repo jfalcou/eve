0e42cf2bb240ecf8139b3e390fcd2fd336f03019
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-01-20 02:48:25 +0000

    FIX-1170: x86 swizzle, up to 2

--- include/eve/module/real/core/function/regular/simd/x86/deinterleave_groups_shuffle.hpp
+++ include/eve/module/real/core/function/regular/simd/x86/deinterleave_groups_shuffle.hpp
@@ -14,7 +14,7 @@ namespace eve::detail
   template<typename T, typename N, std::ptrdiff_t G>
   EVE_FORCEINLINE
   wide<T, N> deinterleave_groups_shuffle_(EVE_SUPPORTS(sse2_), wide<T, N> v, fixed<G>)
-    requires (G * 2 < N()) && x86_abi<abi_t<T, N>>
+    requires (N() / G > 2) && x86_abi<abi_t<T, N>>
   {
     constexpr auto g_sz = sizeof(T) * G;
     constexpr auto n  = N() / G;
@@ -47,6 +47,58 @@ namespace eve::detail
         auto duplicate = _mm_shufflelo_epi16(v, _MM_SHUFFLE(3, 2, 1, 1));
         return _mm_unpacklo_epi8(v, duplicate);
       }
+      else return deinterleave_groups_shuffle_(EVE_RETARGET(cpu_), v, eve::lane<G>);
+    }
+    else if constexpr ( n == 8 )
+    {
+      // g_sz 8 - if we couldn't do 16x4 - no chance
+      // g_sz 4 - 1 light 1 heavy on avx2 instruction as oppose to having to occupy a register
+      //          on avx, splitting will be not ideal as well
+      if constexpr ( g_sz == 4 )
+      {
+        auto floats = eve::bit_cast(v, eve::as<wide<float, fixed<n>>>{});
+        floats = _mm256_permute_ps(v, _MM_SHUFFLE(3, 1, 2, 0));
+        v = eve::bit_cast(floats, as(v));
+        return deinterleave_groups_shuffle(v, lane<N() / 4>);
+      }
+      // pshuvb is crazy cheap, let's use the register when >= ssse3
+      // otherwise - same idea as with floats
+      else if constexpr ( g_sz == 2 && current_api < ssse3 )
+      {
+        v =    _mm_shufflelo_epi16(v, _MM_SHUFFLE(3, 1, 2, 0));
+        v =    _mm_shufflehi_epi16(v, _MM_SHUFFLE(3, 1, 2, 0));
+        return _mm_shuffle_epi32  (v, _MM_SHUFFLE(3, 1, 2, 0));
+      }
+      // should use pshuvb for chars when it's avaliable
+      // otherwise we have to use shorts, no chars shuffles
+      else if constexpr ( g_sz == 1 && current_api < ssse3 )
+      {
+        auto shorts = eve::convert(v, eve::as<std::uint16_t>{});
+        shorts = deinterleave_groups_shuffle(shorts, lane<G>);
+        return eve::convert(shorts, as<T>{});
+      }
+      else return deinterleave_groups_shuffle_(EVE_RETARGET(cpu_), v, eve::lane<G>);
+    }
+    else if constexpr ( n == 16 )
+    {
+      // g_sz 4 - basic will do
+      // g_sz 2 - on avx512 there is a permute
+      // avx2 - pshuvb each halve and deinterleave
+      if constexpr ( g_sz == 2 && current_api == avx2 )
+      {
+        auto half_pattern = as_indexes<wide<std::uint8_t, fixed<n>>>(deinterleave_groups_shuffle_pattern<g_sz, 16>);
+        v = _mm256_shuffle_epi8(v, eve::combine(half_pattern, half_pattern));
+        return deinterleave_groups_shuffle(v, lane<N() / 4>);
+      }
+      // avx - slice and deinterleave
+      else if constexpr ( g_sz == 2 )
+      {
+        auto [lo, hi] = v.slice();
+        lo = deinterleave_groups_shuffle(lo, lane<G>);
+        hi = deinterleave_groups_shuffle(hi, lane<G>);
+        return deinterleave_groups_shuffle(lo, hi, lane<N() / 4>);
+      }
+      // pshuvb
       else return deinterleave_groups_shuffle_(EVE_RETARGET(cpu_), v, eve::lane<G>);
     }
     else return deinterleave_groups_shuffle_(EVE_RETARGET(cpu_), v, eve::lane<G>);
