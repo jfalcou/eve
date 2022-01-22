6f42b9412de5d6d54875782111ad55d86c86f35a
jtlap <jtlapreste@gmail.com>
2022-01-22 20:26:49 +0100

    constant

--- include/eve/constant/mantissamask.hpp
+++ include/eve/constant/mantissamask.hpp
@@ -7,4 +7,4 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/module/ieee/constant/mantissamask.hpp>
+#include <eve/module/arithmetic/constant/mantissamask.hpp>
--- include/eve/constant/nbmantissabits.hpp
+++ include/eve/constant/nbmantissabits.hpp
@@ -7,4 +7,4 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/module/ieee/constant/nbmantissabits.hpp>
+#include <eve/module/arithmetic/constant/nbmantissabits.hpp>
