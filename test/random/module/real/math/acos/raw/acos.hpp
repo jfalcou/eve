d8140ac0c1478bdfa1e47b3cbf2ee56c821afe43
Joel Falcou <joel.falcou@lri.fr>
2022-01-23 15:51:57 +0100

    Fix #1158 -  Rearrange value & bounds on random raw(acos)

--- test/random/module/real/math/acos/raw/acos.hpp
+++ test/random/module/real/math/acos/raw/acos.hpp
@@ -15,7 +15,6 @@ TTS_CASE_TPL("wide random check on acos", EVE_TYPE)
 {
   auto std_acos = [](auto e) { return std::acos(e); };
 
-  double th = std::is_same_v<EVE_VALUE, double> ? 4096.0 : 512.0;
-  eve::uniform_prng<EVE_VALUE> p(-1,1);
-  TTS_RANGE_CHECK_WITH(p, std_acos, eve::raw(eve::acos), th);
+  eve::uniform_prng<EVE_VALUE> p(-1,0.999);
+  TTS_RANGE_CHECK_WITH(p, std_acos, eve::raw(eve::acos), 1024.0);
 };
