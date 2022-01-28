74ed46f344904165553ee4daad967a2ac0157b2a
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-01-28 23:40:12 +0000

    FIX-1013: stack_buffer

--- include/eve/memory/stack_buffer.hpp
+++ include/eve/memory/stack_buffer.hpp
@@ -23,14 +23,14 @@ namespace eve
   //!
   //! @tparam T simd value type
   //!
-  //! Use `get_ptr()` member function to get a pointer compatible with T to operate with.
+  //! Use `ptr()` member function to get a pointer compatible with T to operate with.
   //================================================================================================
 
   template <simd_value T>
   struct alignas( sizeof(element_type_t<T>) * T::size() ) stack_buffer
   {
-    auto get_ptr()       { return eve::as_aligned(buff.data(), lane<T::size()>); }
-    auto get_ptr() const { return eve::as_aligned(buff.data(), lane<T::size()>); }
+    auto ptr()       { return eve::as_aligned(buff.data(), lane<T::size()>); }
+    auto ptr() const { return eve::as_aligned(buff.data(), lane<T::size()>); }
 
     private:
       std::array<element_type_t<T>, T::size()> buff;
@@ -40,8 +40,8 @@ namespace eve
     requires product_type<element_type_t<T>>
   struct stack_buffer<T>
   {
-    auto get_ptr()       { return kumi::apply([](auto& ...buff) { return soa_ptr{buff.get_ptr()... }; }, buffs); }
-    auto get_ptr() const { return kumi::apply([](auto& ...buff) { return soa_ptr{buff.get_ptr()... }; }, buffs); }
+    auto ptr()       { return kumi::apply([](auto& ...buff) { return soa_ptr{buff.ptr()... }; }, buffs); }
+    auto ptr() const { return kumi::apply([](auto& ...buff) { return soa_ptr{buff.ptr()... }; }, buffs); }
 
    private:
     static auto compute_buffs_type()
