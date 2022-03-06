d7ec1126ce15a1093d4a0537e8bb68bb108580f6
jtlap <jtlapreste@gmail.com>
2022-03-06 19:24:28 +0100

    detail print for tests issuing relaxed ranges

---
+++ test/doc/algo/detail/print.hpp
@@ -0,0 +1,22 @@
+//==================================================================================================
+/**
+   EVE - Expressive Vector Engine
+   Copyright : EVE Contributors & Maintainers
+   SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+
+#include <eve/module/core.hpp>
+#include <eve/algo/concepts/relaxed.hpp>
+#include <iostream>
+
+namespace eve::detail
+{
+  template < eve::algo::relaxed_range R>
+  void print(R const & v)
+  {
+    for(auto i = v.begin();  i !=  v.end(); ++i) {std::cout << eve::read(i) << ' ';};
+    std::cout << '\n';
+  }
+}
