3b76e42e1b6157d355e179cb72b38b30ed576ed1
jtlap <jtlapreste@gmail.com>
2022-03-11 11:46:00 +0100

    more changes to all_of and transform

--- test/doc/algo/all_of.cpp
+++ test/doc/algo/all_of.cpp
@@ -11,17 +11,9 @@ int main()
   eve::algo::copy(vr1, r1);
   auto vr2 = eve::views::iota(-3.0, 5);
   eve::algo::copy(vr2, r2);
-  std::cout << " <- std::vector<float> r1(5), r2(5);\n";
-  std::cout << " <- auto vr1 = eve::views::iota_with_step(0.5f, 2.0f, 5);\n";
-  std::cout << " <- eve::algo::copy(vr1, r1);\n";
-  std::cout << " <- auto vr2 = eve::views::iota(-3.0, 5);\n";
-  std::cout << " <- eve::algo::copy(vr2, r2);\n";
-  std::cout << " <- std::vector<float> r2(5);\n";
-  std::cout << " <- eve::algo::copy(vr, r2);;\n";
-  auto my_gez = [](auto i){return (i >= 0); };
-  std::cout << " <- auto my_gez = [](auto i){return (i >= 0); };\n\n";
-  std::cout << " -> eve::algo::all_of(r1, my_gez)       = "<< std::boolalpha << eve::algo::all_of(r1, my_gez)<< '\n';
-  std::cout << " -> eve::algo::all_of(vr1,my_gez)       = "<< std::boolalpha << eve::algo::all_of(vr1, my_gez)<< '\n';
+
+  std::cout << " -> eve::algo::all_of(r1, my_gez)       = "<< std::boolalpha << eve::algo::all_of(r1, [](auto i){return (i >= 0); })<< '\n';
+  std::cout << " -> eve::algo::all_of(vr1,my_gez)       = "<< std::boolalpha << eve::algo::all_of(vr1, [](auto i){return (i >= 0); })<< '\n';
   std::cout << " -> eve::algo::all_of(r2, eve::is_ltz)  = "<< std::boolalpha << eve::algo::all_of(r2, eve::is_ltz)<< '\n';
   std::cout << " -> eve::algo::all_of(vr2, eve::is_ltz) = "<< std::boolalpha << eve::algo::all_of(vr2, eve::is_ltz)<< '\n';
 
--- test/doc/algo/transform.cpp
+++ test/doc/algo/transform.cpp
@@ -1,34 +1,21 @@
 #include <eve/module/core.hpp>
+#include <eve/module/math.hpp>
 #include <eve/algo.hpp>
 #include <iostream>
 #include <vector>
-#include <tuple>
-#include <eve/algo/concepts/relaxed.hpp>
 #include "detail/print.hpp"
-
-// an accurate linspace with transform and lerp
-template < eve::algo::relaxed_range R, eve::scalar_value T,  eve::scalar_value U> R linspace(R& r, T a, U b )
-  requires( eve::floating_scalar_value<decltype(eve::read(r.begin()))>)
-{
-  EVE_ASSERT(eve::is_finite(a), "a is not finite");
-  EVE_ASSERT(eve::is_finite(b), "b is not finite");
-   using elt_t = decltype(eve::read(r.begin()));
-  std::size_t siz = r.end()-r.begin();
-  if (siz == 0u) return r;
-  if (siz == 1u) {*r.begin() = elt_t(b); return r; }
-  if (a == b)    { eve::algo::fill(r, elt_t(a)); return r; }
-  elt_t invnm1 = eve::rec((elt_t(eve::dec(siz))));
-  auto io = eve::views::iota(elt_t{0}, siz);
-  auto f = [a, b, invnm1](auto i){return eve::lerp(elt_t(a), elt_t(b), i*invnm1); };
-  eve::algo::transform_to(eve::views::zip(io, r), f);
-  return r;
-}
 
 
 int main()
 {
-  std::vector<float> r1(10);
-  linspace(r1, 2, 6.3);
-  eve::detail::print(r1);
+  std::vector<float> r1{1.0f, -2.3f, 5.0f, 0.0f, -1.0f};
+  std::vector<float> r2(r1.size());
+  float a = 10.0;
+  float b = 5.0;
+  eve::algo::transform_to(r1, r2, [a, b](auto e){return a*e+b; });
+  std::cout << " -> r1                                  = "; eve::detail::print(r1);
+  std::cout << " -> r2  (a*r1+b)                        = ";eve::detail::print(r2);
+  eve::algo::transform_inplace(r2, eve::log_abs);
+  std::cout << " -> r2  (log(|r2|)                      = ";eve::detail::print(r2);
   return 0;
 }
