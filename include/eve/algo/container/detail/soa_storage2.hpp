bc7e1eff9e514cde3ab69e9488a2d4a325f6f7e2
Joel Falcou <joel.falcou@lri.fr>
2022-03-13 21:09:23 +0100

    Vineyard reimplementation of soa_vector using single allocation

---
+++ include/eve/algo/container/detail/soa_storage2.hpp
@@ -0,0 +1,172 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/module/core.hpp>
+#include <eve/algo/views/zip.hpp>
+#include <eve/detail/kumi.hpp>
+#include <eve/detail/raberu.hpp>
+#include <eve/memory/aligned_allocator.hpp>
+#include <eve/product_type.hpp>
+#include <memory>
+#include <type_traits>
+
+namespace eve::algo::detail
+{
+  template<eve::product_type Type, typename Allocator>
+  struct soa_storage : Allocator
+  {
+    using is_product_type = void;
+
+    soa_storage()                : storage_( sizing(0) ), size_(0) { build(0); }
+    soa_storage( std::size_t n)  : storage_( sizing(n) ), size_(n) { build(n); }
+    soa_storage( std::size_t n, Type const& v)  : storage_( sizing(n) ), size_(n) { build(n,v); }
+
+    soa_storage(soa_storage const& src) : storage_(src.storage), size_(src.size())
+    {
+      reindex(src.size());
+    }
+
+    soa_storage& operator=(soa_storage const& src)
+    {
+      storage_ = src.storage;
+      size_ = src.size();
+      reindex(src.size());
+      return *this;
+    }
+
+    soa_storage(soa_storage&&)            =default;
+    soa_storage& operator=(soa_storage&&) =default;
+
+    ~soa_storage()
+    {
+      using alloc_t = std::allocator_traits<Allocator>;
+      kumi::for_each( [&]<typename T>(T*& s)
+                      {
+                        for(std::size_t i=0;i<size_;++i) alloc_t::destroy(*this, s+i);
+                      }
+                    , indexes_
+                    );
+    }
+
+    EVE_FORCEINLINE auto data_aligned()
+    {
+      auto ptrs = kumi::map([](auto* m) { return as_aligned_pointer(m); }, indexes_);
+      return kumi::apply(eve::algo::views::zip, ptrs);
+    }
+
+    EVE_FORCEINLINE auto data_aligned() const
+    {
+      auto ptrs = kumi::map([](auto const* m) { return as_aligned_pointer(m); }, indexes_);
+      return kumi::apply(eve::algo::views::zip, ptrs);
+    }
+
+    EVE_FORCEINLINE auto data()       { return eve::unalign(data_aligned()); }
+    EVE_FORCEINLINE auto data() const { return eve::unalign(data_aligned()); }
+
+    void swap(soa_storage& other) noexcept
+    {
+      storage_.swap(other.storage_);
+      std::swap(indexes_,other.indexes_);
+      std::swap(size_,other.size_);
+    }
+
+    void build(std::size_t n, Type v = {})
+    {
+      using alloc_t = std::allocator_traits<Allocator>;
+      auto sub_span = eve::align(&storage_[0], eve::over{64});
+
+      kumi::for_each( [&]<typename T>(T*& s, auto m)
+                      {
+                        s = reinterpret_cast<T*>(sub_span);
+                        for(std::size_t i=0;i<n;++i) alloc_t::construct(*this, s+i,m);
+                        sub_span += realign<T>(n);
+                      }
+                    , indexes_, v
+                    );
+
+      capacity_ = capacity_impl(n);
+    }
+
+    void reindex(std::size_t n)
+    {
+      auto sub_span = eve::align(&storage_[0], eve::over{64});
+
+      kumi::for_each( [&]<typename T>(T*& s)
+                      {
+                        s = reinterpret_cast<T*>(sub_span);
+                        sub_span += realign<T>(n);
+                      }
+                    , indexes_
+                    );
+      capacity_ = capacity_impl(n);
+    }
+
+    //==============================================================================================
+    // Static helpers
+    //==============================================================================================
+    template<typename Content>
+    static void piecewise_copy(Content& dst, Content const& src, std::size_t n)
+    {
+      kumi::for_each( [&]<typename T>(T* d, T const* s) { std::memcpy(d,s,sizeof(T)*n); }
+                    , dst, src
+                    );
+    }
+
+    template<typename T> static EVE_FORCEINLINE auto as_aligned_pointer(T* ptr)
+    {
+      return eve::as_aligned(ptr, eve::detail::cache_line_cardinal<T>{});
+    }
+
+    template<typename T>
+    static  auto realign(std::size_t n) { return eve::align(sizeof(T)*n, eve::over{64}); }
+
+    static auto capacity_impl(std::size_t n) noexcept
+    {
+      // Capacity is the smallest gap possible, ie the one in the largest type
+      constexpr std::size_t largest = max_scalar_size_v<Type>;
+      return eve::align(largest*n, eve::over{64})/largest;
+    }
+
+    static auto sizing(std::size_t n) noexcept
+    {
+      return  n ? 64 + kumi::fold_left( [n]<typename S>(auto a, S) { return a + realign<S>(n); }
+                                  , flat_t{}
+                                  , 0ULL
+                                  )
+                : n;
+    }
+
+    //==============================================================================================
+    // Tuple interface
+    //==============================================================================================
+    template<std::size_t I>
+    friend decltype(auto) get(soa_storage& s) noexcept { return get<I>(s.indexes_); }
+
+    template<std::size_t I>
+    friend decltype(auto) get(soa_storage const& s) noexcept { return get<I>(s.indexes_); }
+
+    //==============================================================================================
+    using byte_t      = unsigned char;
+    using flat_t      = kumi::result::flatten_all_t<Type>;
+    using sub_span_t  = kumi::as_tuple_t<flat_t, std::add_pointer>;
+    using alloc_t     = typename std::allocator_traits<Allocator>::template rebind_alloc<byte_t>;
+
+    std::vector<byte_t,alloc_t> storage_;
+    sub_span_t                  indexes_;
+    std::size_t                 size_, capacity_;
+  };
+}
+
+template <eve::product_type Type, typename Allocator>
+struct std::tuple_size<eve::algo::detail::soa_storage<Type,Allocator>> :
+  std::tuple_size<typename eve::algo::detail::soa_storage<Type,Allocator>::sub_span_t> {};
+
+template <std::size_t I, eve::product_type Type, typename Allocator>
+struct std::tuple_element<I, eve::algo::detail::soa_storage<Type,Allocator>> :
+  std::tuple_element<I, typename eve::algo::detail::soa_storage<Type,Allocator>::sub_span_t> {};
---
+++ include/eve/algo/container/soa_vector2.hpp
@@ -0,0 +1,346 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+// #include <eve/algo/equal.hpp>
+#include <eve/algo/views/convert.hpp>
+
+#include <eve/arch/expected_cardinal.hpp>
+#include <eve/algo/container/detail/soa_storage2.hpp>
+#include <eve/memory/aligned_allocator.hpp>
+#include <eve/product_type.hpp>
+#include <eve/traits.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace eve::algo
+{
+  //================================================================================================
+  //! @addtogroup memory
+  //! @{
+  //!   @brief SIMD-aware container for product types
+  //!
+  //!   **Required header:** `#include <eve/memory/soa_vector.hpp>`
+  //!
+  //!   soa_vector is a container similar to `std::vector` but optimized to store
+  //!   [product types](eve::product_type) using a Structure Of Array layout compatible with SIMD
+  //!   processing.
+  //!
+  //!   @tparam Type  Value type stored inside `eve::soa_vector`
+  //! @}
+  //================================================================================================
+  template< eve::product_type Type
+          , typename Allocator = eve::aligned_allocator<unsigned char, eve::fixed<64>>
+          >
+  struct soa_vector
+  {
+    public:
+
+    //! Type of data stored in the container
+    using value_type = Type;
+
+    using storage_type = detail::soa_storage<Type,Allocator>;
+
+    //!=============================================================================================
+    //! @name Pointers and Iterators
+    //! @{
+    //==============================================================================================
+    //!   @brief pointer and iterator types.
+    //!   pointer*  - are eve::algo::views::zip_iterator over fields (no conversion to type, just flat fields)
+    //!   iterator* - are a pointer, converter to the Type.
+    //!   They all satisfy eve::algo::relaxed_iterator but not std::iterator
+
+    using pointer               = decltype(storage_type{}.data());
+    using const_pointer         = decltype(std::declval<storage_type const>().data());
+    using pointer_aligned       = decltype(storage_type{}.data_aligned());
+    using const_pointer_aligned = decltype(std::declval<storage_type const>().data_aligned());
+
+    using iterator               = decltype(views::convert(pointer{}              , as<value_type>{}));
+    using const_iterator         = decltype(views::convert(const_pointer{}        , as<value_type>{}));
+    using iterator_aligned       = decltype(views::convert(pointer_aligned{}      , as<value_type>{}));
+    using const_iterator_aligned = decltype(views::convert(const_pointer_aligned{}, as<value_type>{}));
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    //==============================================================================================
+    //! @name Constructors
+    //! @{
+    //==============================================================================================
+
+    //! Constructs an empty container.
+    soa_vector() : storage(0) {}
+
+    //! Constructs the container with `n` default-inserted instances of `Type`. No copies are made.
+    soa_vector ( std::size_t n) : storage(n) {}
+
+    //! Constructs the container with `n` copies of elements with `value` value.
+    soa_vector(std::size_t n, value_type value)  : storage(n, value) {}
+
+    //! Constructs the container with the contents of the initializer list `l`.
+    soa_vector( std::initializer_list<Type> l ) : storage(l.size())
+    {
+      auto ptr = l.begin();
+      for(std::size_t i = 0;i < size(); ++i)
+      {
+        kumi::for_each( [&](auto m, auto& v) { v[i] = m; }, kumi::flatten_all(*ptr++), storage );
+      }
+    }
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    //==============================================================================================
+    //! @name Capacity
+    //! @{
+    //==============================================================================================
+    //! Returns the number of elements in the container
+    EVE_FORCEINLINE std::size_t size()  const noexcept { return storage.size_; }
+
+    //! Returns the number of elements that the container has currently allocated space for
+    EVE_FORCEINLINE std::size_t capacity()  const noexcept { return storage.capacity_; }
+
+    //! Requests the removal of unused capacity.
+    // EVE_FORCEINLINE void shrink_to_fit() noexcept
+    // {
+    //   kumi::for_each([](auto& m) { m.shrink_to_fit(); }, storage);
+    // }
+
+    //! Checks if the container has no elements
+    EVE_FORCEINLINE bool empty() const noexcept { return size() == 0ULL; }
+
+    //! @brief Increase the capacity of the vector to a value that's greater or equal to `n`.
+    //! If `n` is greater than the current capacity(), new storage is allocated and all iterators,
+    //! including the past-the-end iterator, and all references to the elements are invalidated.
+    //! Otherwise, the method does nothing.
+    //! @warning reserve() does not change the size of the vector.
+    //! @param n New capacity of the `eve::soa_vector`
+    void reserve(std::size_t n)
+    {
+      if(n > capacity())
+      {
+        auto const sz = size();
+        soa_vector that(n);
+        storage_type::piecewise_copy(that.storage, storage, sz);
+        swap(that);
+        storage.size_ = sz;
+     }
+    }
+
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    //==============================================================================================
+    //! @name Modifiers
+    //! @{
+    //==============================================================================================
+    //! @brief Clear the contents of the container.
+    void clear()
+    {
+      storage.capacity_ = storage_type::capacity_impl(storage.size_);
+      storage.size_     = 0;
+    }
+
+    //! @brief Removes an element from the container.
+    //! Has the same invalidation semantics as std::vector.
+    //! end() iterator is not a valid pos.
+    // iterator erase(const_iterator pos)
+    // {
+    //   std::ptrdiff_t distance = pos - cbegin();
+    //   kumi::for_each([&](auto& m) { return m.erase(m.begin() + distance); }, storage);
+    //   return begin() + distance;
+    // }
+
+    //! @brief Removes the elements in the range [first, last)
+    //! Empty range is OK, does nothing
+    // iterator erase(const_iterator f, const_iterator l)
+    // {
+    //   std::ptrdiff_t distance_f = f - cbegin();
+    //   std::ptrdiff_t distance_l = l - cbegin();
+    //   kumi::for_each([&](auto& m) {
+    //     return m.erase(m.begin() + distance_f, m.begin() + distance_l); }, storage);
+    //   return begin() + distance_f;
+    // }
+
+    //! @brief Appends the given element value to the end of the container.
+    //! If the new size() is greater than capacity() then all iterators and references (including
+    //! the past-the-end iterator) are invalidated. Otherwise only the past-the-end iterator is
+    //! invalidated.
+    //! @param value [Value](@eve::vectorizable) to append
+    // EVE_FORCEINLINE void push_back(value_type const& value) noexcept
+    // {
+    //   kumi::for_each( [&](auto& m, auto const& v) { m.push_back(v); }
+    //                 , storage, kumi::flatten_all(value)
+    //                 );
+    // }
+
+    //! @brief Removes the last element of the container.
+    //! Calling pop_back on an empty container results in undefined behavior.
+    //! Iterators and references to the last element, as well as the end() iterator, are invalidated.
+    // EVE_FORCEINLINE void pop_back() noexcept
+    // {
+    //   kumi::for_each([&](auto& m) { m.pop_back(); }, storage);
+    // }
+
+    //! @brief Resizes the container to contain count elements.
+    //!
+    //! If the current size is greater than `n`, the container is reduced to its first `n` elements.
+    //! If the current size is less than `n`, additional elements are appended. Those elements are
+    //! either default-inserted or copied if `value` is provided.
+    //! @param n      new size of the container
+    //! @param value  the value to initialize the new elements with
+    void resize(std::size_t n, value_type value = {})
+    {
+      if(n <= capacity())
+      {
+        using alloc_t = std::allocator_traits<Allocator>;
+        kumi::for_each( [&]<typename T>(T* s, auto m)
+                        {
+                          for(std::size_t i=size();i<n;++i) alloc_t::construct(storage, s+i,m);
+                        }
+                      , storage, value
+                      );
+        storage.size_ = n;
+      }
+      else
+      {
+        soa_vector that(n, value);
+        storage_type::piecewise_copy(that.storage, storage, size());
+        swap(that);
+     }
+    }
+
+    //! @brief Exchanges the contents of the container with those of `other`.
+    //! Does not invoke any move, copy, or swap operations on individual elements.
+    //! All iterators and references remain valid. The past-the-end iterator is invalidated.
+    void swap(soa_vector& other) noexcept
+    {
+      storage.swap(other.storage);
+    }
+
+    //! @brief Swaps the contents of `lhs` and `rhs` by calling `lhs.swap(rhs)`.
+    friend EVE_FORCEINLINE void swap(soa_vector &lhs, soa_vector &rhs) noexcept
+    {
+      lhs.swap(rhs);
+    }
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    public:
+    //==============================================================================================
+    //! @name Element access
+    //! @{
+    //==============================================================================================
+    //! Returns a zipped aligned pointer to the beginning
+    EVE_FORCEINLINE auto data_aligned()         { return storage.data_aligned(); }
+
+    //! Returns a zipped aligned pointer to the beginning
+    EVE_FORCEINLINE auto data_aligned()  const  { return storage.data_aligned(); }
+
+    //! Returns a zipped pointer to the beginning
+    EVE_FORCEINLINE auto data()       { return  storage.data(); }
+
+    //! Returns a constant zipped pointer to the beginning
+    EVE_FORCEINLINE auto data() const { return  storage.data(); }
+
+    //! @brief Returns the value of the `i`th element of the container
+    //! @param i Index of the value to retrieve
+    EVE_FORCEINLINE auto get(std::size_t i) const { return eve::read(begin() + i); }
+
+    //! @brief Modify the value of the `i`th element of the container
+    //! @param i Index of the value to write
+    //! @param v Value to write
+    EVE_FORCEINLINE void set(std::size_t i, value_type const& v)
+    {
+      return eve::write(v, begin() + i);
+    }
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    //==============================================================================================
+    //! @name Iterators
+    //! @{
+    //==============================================================================================
+    //! Returns an aligned iterator to the beginning
+    EVE_FORCEINLINE auto begin_aligned() -> iterator_aligned { return views::convert(data_aligned(), eve::as<Type>{}); }
+
+    //! Returns an aligned iterator to the beginning
+    EVE_FORCEINLINE auto begin_aligned()  const -> const_iterator_aligned { return views::convert(data_aligned(), eve::as<Type>{}); }
+
+    //! Returns a constant aligned iterator to the beginning
+    EVE_FORCEINLINE auto cbegin_aligned() const -> const_iterator_aligned { return begin_aligned(); }
+
+    //! Returns an iterator to the beginning
+    EVE_FORCEINLINE auto begin() -> iterator { return views::convert(data(), eve::as<Type>{}); }
+
+    //! Returns an iterator to the beginning
+    EVE_FORCEINLINE auto begin()  const -> const_iterator { return views::convert(data(), eve::as<Type>{}); }
+
+    //! Returns a constant iterator to the beginning
+    EVE_FORCEINLINE auto cbegin() const -> const_iterator { return begin(); }
+
+    //! Returns an iterator to the end
+    EVE_FORCEINLINE auto end()  -> iterator  { return begin() + size(); }
+
+    //! Returns an iterator to the end
+    EVE_FORCEINLINE auto end()  const -> const_iterator { return begin() + size(); }
+
+    //! Returns a constant iterator to the end
+    EVE_FORCEINLINE auto cend() const -> const_iterator { return end(); }
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    //==============================================================================================
+    //! @name Comparisons and ordering
+    //! @{
+    //==============================================================================================
+    // friend bool operator==(soa_vector const& lhs, soa_vector const& rhs)
+    // {
+    //   if( lhs.size() != rhs.size() ) return false;
+    //   return eve::algo::equal( lhs, rhs.begin_aligned());
+    // }
+
+    // friend bool operator!=(soa_vector const& lhs, soa_vector const& rhs)
+    // {
+    //   return !(lhs == rhs);
+    // }
+
+    //==============================================================================================
+    //! @}
+    //==============================================================================================
+
+    friend std::ostream& operator<<(std::ostream& os, soa_vector const& v)
+    {
+      os << "{ ";
+      for(std::size_t i=0;i<v.size();++i) os << v.get(i) << " ";
+      os << '}';
+      return os;
+    }
+
+    template <typename Traits, typename Self>
+    requires std::same_as<std::remove_reference_t<Self>, soa_vector>
+    EVE_FORCEINLINE friend auto tagged_dispatch(algo::preprocess_range_, Traits tr, Self& self)
+    {
+      return algo::preprocess_range(tr, algo::as_range(self.begin_aligned(), self.end()));
+    }
+
+    private:
+    storage_type storage;
+  };
+}
