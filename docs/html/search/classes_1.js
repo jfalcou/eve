6e7c4af94ff67940ff2c9525f6559805438cf29d
Joel Falcou <joel.falcou@lri.fr>
2022-03-14 19:31:07 +0100

    Updated documentation

--- docs/html/search/classes_1.js
+++ docs/html/search/classes_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['backward_5fiterator_0',['backward_iterator',['../structbackward__iterator.html',1,'backward_iterator'],['../structeve_1_1algo_1_1views_1_1backward__iterator.html',1,'eve::algo::views::backward_iterator&lt; I &gt;']]],
-  ['backward_5frange_1',['backward_range',['../structbackward__range.html',1,'backward_range'],['../structeve_1_1algo_1_1views_1_1backward__range.html',1,'eve::algo::views::backward_range&lt; R &gt;']]],
-  ['base_5fplus_5foffset_5fiterator_2',['base_plus_offset_iterator',['../structbase__plus__offset__iterator.html',1,'base_plus_offset_iterator'],['../structeve_1_1algo_1_1views_1_1base__plus__offset__iterator.html',1,'eve::algo::views::base_plus_offset_iterator&lt; T, N &gt;']]]
+  ['backward_5frange_1',['backward_range',['../structbackward__range.html',1,'backward_range'],['../structeve_1_1algo_1_1views_1_1backward__range.html',1,'eve::algo::views::backward_range&lt; R &gt;']]]
 ];
