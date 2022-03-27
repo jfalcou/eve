fa6aac6390cbb8fe9e04f82645e5a757e92e2351
jtlap <jtlapreste@gmail.com>
2022-03-27 08:39:14 +0200

    chebytchev_u.hpp

--- include/eve/module/polynomial/regular/impl/tchebytchev_u.hpp
+++ include/eve/module/polynomial/regular/impl/tchebytchev_u.hpp
@@ -56,83 +56,91 @@ namespace eve::detail
       default :
         auto nn = inc(convert(n, as<element_type_t<T>>()));
         auto z =  eve::abs(x);
-        auto acx = acos(x);
-
-        auto r0 = sin(nn*acx)/sin(acx);
-        auto r = if_else((1-z < eps(as(x))
-                         , if_else(is_gez(x), nn, nn*alternate_sign(n))
-                         , r0)
-                        );
-    auto test = z <= one(as(z));
-    if (all(test))
-    {
-      return r;
+        auto test = z <= one(as(z));
+        if (test)
+        {
+          auto acx = acos(x);
+          auto r0 = sin(nn*acx)/sin(acx);
+          return (1-z < eps(as(x))) ? (is_gez(x) ? nn : nn*alternate_sign(n)) : r0;
+        }
+        else
+        {
+          auto achx = acosh(x);
+          auto t0 = sinh(n*achx)/sinh(achx);
+          return (z-1 < eps(as(x))) ? (is_gez(x) ? nn : nn*alternate_sign(n)) : t0;
+        }
+      }
+      else return apply_over(tchebytchev_u, n, x);
     }
-    else
-    {
-      auto achx = acosh(x);
-      auto t0 = sinh(n*achx)/sinh(achx);
-      auto t = if_else((z-1 < eps(as(x))
-                       , if_else(is_gez(x), nn, nn*alternate_sign(n))
-                       , t0)
-                      );
-      return if_else(test,  r, if_else(is_gez(x), t, t*sign_alternate(nn)));
-    }
-    }
-    else return apply_over(tchebytchev_u, n, x);
   }
 
-//   template<simd_value I, floating_real_value T>
-//   EVE_FORCEINLINE auto tchebytchev_u_(EVE_SUPPORTS(cpu_), I n, T x) noexcept
-//   //  requires(index_compatible < I, T>
-//   {
-//     if constexpr(has_native_abi_v<T> && has_native_abi_v<I>)
-//     {
-//       EVE_ASSERT(eve::all(is_gez(n)), "some elements of n are not positive");
-//       EVE_ASSERT(eve::all(is_flint(n)), "some elements of n are not flint");
-//       auto nn = convert(n, as<eve::element_type_t<T>>());
-//       auto z =  eve::abs(x);
-//       auto r = cos(nn*acos(x));
-//       auto test = z <= one(as(z));
-//       if (all(test)) return r;
-//       else
-//       {
-//         auto t = cosh(nn*acosh(z));
-//         return if_else(test,  r, if_else(is_gez(x), t, t*sign_alternate(nn)));
-//       }
-//     }
-//     else return apply_over(tchebytchev_u, n, x);
-//   }
+  template<scalar_value I, floating_simd_value T>
+  EVE_FORCEINLINE T tchebytchev_u_(EVE_SUPPORTS(cpu_), I n, T x) noexcept
+  {
+    EVE_ASSERT(is_gez(n), "n is not positive");
+    EVE_ASSERT(is_flint(n), "n is not flint");
+    if constexpr(has_native_abi_v<T>)
+    {
+      switch (int(n)){
+      case 0  :return one(as(x));
+      case 1  :return 2*x;
+      case 2  :return fms(4*x, x, 1);
+      case 3  :return 4*x*fms(2*x, x, 1);
+      case 4  :return inc(4*sqr(x)*(fms(2*x, 2*x, 3)));
+      default :
+        auto nn = inc(convert(n, as<element_type_t<T>>()));
+        auto z =  eve::abs(x);
+        auto test = z <= one(as(z));
+        auto r = nan(as(x));
+        if (any(test))
+        {
+          auto acx = acos(x);
+          auto r0 = sin(nn*acx)/sin(acx);
+          auto r0 = if_else(1-z < eps(as(x)), if_else(is_gez(x), nn,  nn*alternate_sign(n)), r0);
+          r = if_else(test, r0, r)
+        }
+        if(eve::all(test))
+          return r;
+        else
+        {
+          auto achx = acosh(x);
+          auto t0 = sinh(n*achx)/sinh(achx);
+          auto r1 = if_else(z-1 < eps(as(x)), if_else(is_gez(x), nn, nn*alternate_sign(n)), t0);
+          return if_else(test, r, r1);
+        }
+      }
+      else return apply_over(tchebytchev, n, x);
+    }
+  }
 
-
-//   template<integral_simd_value I, floating_real_value T>
-//   EVE_FORCEINLINE auto tchebytchev_u_(EVE_SUPPORTS(cpu_), kind_2_type const &, I n, T x) noexcept
-//   {
-//     EVE_ASSERT(eve::all(is_gez(n)), "some elements of n are not positive");
-//     auto nn = inc(convert(n, as<element_type_t<T>>()));
-//     auto z =  eve::abs(x);
-//     auto acx = acos(x);
-
-//     auto r0 = sin(nn*acx)/sin(acx);
-//     auto r = if_else((1-z < eps(as(x))
-//                      , if_else(is_gez(x), nn, nn*alternate_sign(n))
-//                      , r0)
-//                     );
-//     auto test = z <= one(as(z));
-//     if (all(test))
-//     {
-//       return r;
-//     }
-//     else
-//     {
-//       auto achx = acosh(x);
-//       auto t0 = sinh(n*achx)/sinh(achx);
-//       auto t = if_else((z-1 < eps(as(x))
-//                        , if_else(is_gez(x), nn, nn*alternate_sign(n))
-//                        , t0)
-//                       );
-//       return if_else(test,  r, if_else(is_gez(x), t, t*sign_alternate(nn)));
-//     }
-//   }
-
+  template<simd_value I, floating_real_value T>
+  EVE_FORCEINLINE auto tchebytchev_(EVE_SUPPORTS(cpu_), I n, T x) noexcept
+  {
+    if constexpr(has_native_abi_v<T> && has_native_abi_v<I>)
+    {
+      EVE_ASSERT(eve::all(is_gez(n)), "some elements of n are not positive");
+      EVE_ASSERT(eve::all(is_flint(n)), "some elements of n are not flint");
+      auto nn = inc(convert(n, as<element_type_t<T>>()));
+      auto z =  eve::abs(x);
+      auto test = z <= one(as(z));
+      auto r = nan(as(x));
+      if (any(test))
+      {
+        auto acx = acos(x);
+        auto r0 = sin(nn*acx)/sin(acx);
+        auto r0 = if_else(1-z < eps(as(x)), if_else(is_gez(x), nn,  nn*alternate_sign(n)), r0);
+        r = if_else(test, r0, r)
+          }
+      if(eve::all(test))
+        return r;
+      else
+      {
+        auto achx = acosh(x);
+        auto t0 = sinh(n*achx)/sinh(achx);
+        auto r1 = if_else(z-1 < eps(as(x)), if_else(is_gez(x), nn, nn*alternate_sign(n)), t0);
+        return if_else(test, r, r1);
+      }
+    }
+    else return apply_over(tchebytchev, n, x);
+  }
 }
