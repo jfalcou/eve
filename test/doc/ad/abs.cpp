4970dc8878df20ff36b57dda86bbdcfec70972ae
jtlap <jtlapreste@gmail.com>
2022-03-27 09:07:33 +0200

    doc cpp for ad beginning

---
+++ test/doc/ad/abs.cpp
@@ -0,0 +1,26 @@
+#include <eve/module/math.hpp>
+#include <eve/module/ad/regular/ad.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+using wide_ft = eve::wide<float, eve::fixed<4>>;
+using wide_vdt = eve::wide<eve::valder<float>, eve::fixed<4>>;
+using vd_t     = eve::valder<double>;
+
+int main()
+{
+  wide_ft x   = {-1.0f, 2.0f, -3.0f,  0.0f};
+  wide_vdt vx(eve::var(x));
+
+  std::cout << "---- simd" << '\n'
+            << "<- x            = " << x << '\n'
+            << "<- vx           = " << vx << '\n'
+            << "-> abs(vx)      = " << eve::abs(vx) << '\n';
+
+  vd_t sx(eve::var(-2.4));
+
+  std::cout << "---- scalar" << '\n'
+            << "<- sx        = " << sx << '\n'
+            << "-> abs(sx)   = " << eve::abs(sx) << '\n';
+  return 0;
+}
