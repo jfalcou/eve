fab1609ecd9135460321d8a484271d766867e7b9
jtlap <jtlapreste@gmail.com>
2022-03-31 20:09:04 +0200

    maxmag

--- include/eve/module/core/diff/impl/maxmag.hpp
+++ include/eve/module/core/diff/impl/maxmag.hpp
@@ -21,4 +21,11 @@ namespace eve::detail
     auto o = [](auto m){return eve::one(as(m)); };
     return minmax_kernel<N>(eve::maxmag, o, arg0, arg1, args...);
   }
+
+  template<int N, conditional_expr C, typename T0, typename T1, typename... Ts>
+  auto maxmag_(EVE_SUPPORTS(cpu_), C const & cond, diff_type<N>
+           , T0 arg0, T1 arg1, Ts... args) noexcept
+  {
+    return mask_op(  cond, diff_nth<N>(eve::maxmag), arg0, arg1, args...);
+  }  
 }
