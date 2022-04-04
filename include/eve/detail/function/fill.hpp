80410ec8298a350d49958e9e5744cefdf256aaba
Joel Falcou <joel.falcou@lri.fr>
2022-04-04 21:12:30 +0200

    Fix issue with top_bits

--- include/eve/detail/function/fill.hpp
+++ include/eve/detail/function/fill.hpp
@@ -17,32 +17,18 @@ namespace eve::detail
   template<typename Generator, typename Pack>
   EVE_FORCEINLINE auto fill(eve::as<Pack> const &, Generator g) noexcept
   {
-    if constexpr( has_aggregated_abi_v<Pack> )
-    {
-      Pack that;
-
-      that.storage().apply
-      (
-        [&]<typename... Sub>(Sub&... v)
-        {
-          std::ptrdiff_t k = 0;
-
-          ( ( v = Sub([&](auto i, auto) { return g(i + k, Pack::size()); })
-            , k += Sub::size()
-            )
-          , ...
-          );
-        }
-      );
-
-      return that;
-    }
-    else
+    // MSVC emits a spurious error trying to interpret the Pack construction as an
+    // initializer_list when it should not. We then manually split the two cases.
+    if constexpr(cardinal_v<Pack> > 1)
     {
       return  [&g]<std::ptrdiff_t... N>(std::integer_sequence<std::ptrdiff_t,N...>)
               {
                 return Pack( g(N,cardinal_v<Pack>)... );
               }( std::make_integer_sequence<std::ptrdiff_t,cardinal_v<Pack>>{});
+    }
+    else
+    {
+      return Pack( g(0,1) );
     }
   }
 }
