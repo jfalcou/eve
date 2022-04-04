313d46e05153491446518f31ef438313e8f570ba
jtlap <jtlapreste@gmail.com>
2022-04-04 14:35:46 +0200

    deginrad radindeg radinpi

--- include/eve/module/math/diff/impl/radinpi.hpp
+++ include/eve/module/math/diff/impl/radinpi.hpp
@@ -12,16 +12,14 @@
 namespace eve::detail
 {
   template<floating_real_value T>
-  EVE_FORCEINLINE constexpr auto radindeg_(EVE_SUPPORTS(cpu_)
+  EVE_FORCEINLINE constexpr T radinpi_(EVE_SUPPORTS(cpu_)
+                                       , diff_type<1> const &
                                        , T const &a) noexcept
- {
-   if constexpr(has_native_abi_v<T>)
-   {
-     auto radradindeg  = Ieee_constant<T, 0X42652EE1U, 0X404CA5DC1A63C1F8ULL>();
-     auto radradindegr = Ieee_constant<T, 0X353387C0U, 0X3CE1E7AB456405F8ULL>();
-     return fma(a, radradindegr, a*radradindeg);
-   }
-   else return apply_over(radindeg, a);
- }
-
+  {
+    if constexpr(has_native_abi_v<T>)
+    {
+      return invpi(eve::as(a));
+    }
+    else return apply_over(radinpi, a);
+  }
 }
