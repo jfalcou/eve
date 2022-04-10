6b4ec72ffce418a82c7a2f9dc98a3d7f877383b8
jtlap <jtlapreste@gmail.com>
2022-04-10 18:38:31 +0200

    reduce.cpp

--- test/doc/algo/reduce.cpp
+++ test/doc/algo/reduce.cpp
@@ -9,8 +9,8 @@ int main()
   std::vector<float> v(10);
   auto vv = eve::views::iota(1.0f, 10);
   eve::algo::copy(vv, v);
-  auto r1 = eve::algo::reduce(v, std::make_pair(eve::add, eve::zero), 0.0f);
-  auto r2 = eve::algo::reduce(vv, std::make_pair(eve::add, eve::zero), 5.0f);
+  auto r1 = eve::algo::reduce(v, 0.0f);
+  auto r2 = eve::algo::reduce(vv, 5.0f);
 
   std::cout << " <- std::vector<float> r1(10);\n";
   std::cout << " <- eve::views::iota(1.0f, 10);;\n";
