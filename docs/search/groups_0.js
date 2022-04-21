7078fd535fd5b1d3b197364c03b63bc06fba34dd
Joel Falcou <joel.falcou@lri.fr>
2022-04-21 15:28:55 +0200

    Update doc to latest status

--- docs/search/groups_0.js
+++ docs/search/groups_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['algorithms_0',['Algorithms',['../group__algo.html',1,'']]],
-  ['algorithms_20and_20views_1',['Algorithms and Views',['../group__algorithm.html',1,'']]],
-  ['architecture_20related_20types_20and_20helpers_2',['Architecture related Types and Helpers',['../group__arch.html',1,'']]]
+  ['algorithms_1',['algorithms',['../group__algorithm.html',1,'']]],
+  ['algorithms_2',['Algorithms',['../group__algorithms.html',1,'']]],
+  ['architecture_20related_20types_20and_20helpers_3',['Architecture related Types and Helpers',['../group__arch.html',1,'']]]
 ];
