95b8e5ff54ed521fdd4800be73f5f5eb8156818c
Joel Falcou <joel.falcou@lri.fr>
2022-04-22 21:27:11 +0200

    sum/diff_of_prod helpers

---
+++ include/eve/module/core/regular/impl/sum_of_prod.hpp
@@ -0,0 +1,41 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/apply_over.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+  template<floating_real_value T, floating_real_value U, floating_real_value V, floating_real_value W >
+  EVE_FORCEINLINE auto sum_of_prod_(EVE_SUPPORTS(cpu_)
+                                   , const T& a
+                                   , const U& b
+                                   , const V& c
+                                   , const W& d
+                                   ) noexcept
+  {
+    return arithmetic_call(sum_of_prod, a, b, c, d);
+  }
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE auto sum_of_prod_(EVE_SUPPORTS(cpu_)
+                                   , const T& a
+                                   , const T& b
+                                   , const T& c
+                                   , const T& d
+                                   ) noexcept
+  requires(has_native_abi_v<T>)
+  {
+    T mcd = -c * d;
+    T err = fma(c, d, mcd);
+    T dop = fms(a, b, mcd);
+    return dop + err;
+  }
+}
