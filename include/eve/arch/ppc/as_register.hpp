adec11713cacb5d391a72c4e5419de1aff24bab0
Joel Falcou <joel.falcou@lri.fr>
2022-04-28 23:09:54 +0200

    Upgrade to latest SPY
    Prologue work to add support for WASM and fixed size SVE

--- include/eve/arch/ppc/as_register.hpp
+++ include/eve/arch/ppc/as_register.hpp
@@ -39,7 +39,7 @@ namespace eve
       if constexpr( std::is_same_v<T,float> && Size::value <= 4) { return wrap<__vector float>{}; }
       else if constexpr( std::is_same_v<T,double> && Size::value <= 2 )
       {
-        if constexpr(spy::simd_instruction_set == spy::vsx_)  { return wrap<__vector double>{}; }
+        if constexpr(spy::simd_instruction_set >= spy::vsx_)  { return wrap<__vector double>{}; }
         else                                                  { return emulated_{};         }
       }
       else if constexpr( std::is_integral_v<T> )
@@ -53,7 +53,7 @@ namespace eve
         if constexpr(!signed_v && size_check(2,8 )) return wrap<__vector unsigned short>{};
         if constexpr(!signed_v && size_check(4,4 )) return wrap<__vector unsigned int>{};
 
-        if constexpr(spy::simd_instruction_set == spy::vsx_)
+        if constexpr(spy::simd_instruction_set >= spy::vsx_)
         {
           if constexpr( signed_v && size_check(8,2)) return wrap<__vector signed long>{};
           if constexpr(!signed_v && size_check(8,2)) return wrap<__vector unsigned long>{};
@@ -83,7 +83,7 @@ namespace eve
       if constexpr( std::is_same_v<T,float> && Size::value <= 4) { return wrap<__vector __bool int>{}; }
       else if constexpr( std::is_same_v<T,double> && Size::value <= 2 )
       {
-        if constexpr(spy::simd_instruction_set == spy::vsx_)  { return wrap<__vector __bool long>{}; }
+        if constexpr(spy::simd_instruction_set >= spy::vsx_)  { return wrap<__vector __bool long>{}; }
         else                                                  { return emulated_{}; }
       }
       else if constexpr( std::is_integral_v<T> )
@@ -97,7 +97,7 @@ namespace eve
         if constexpr(!signed_v && size_check(2,8 ) ) return wrap<__vector __bool short>{};
         if constexpr(!signed_v && size_check(4,4 ) ) return wrap<__vector __bool int>{};
 
-        if constexpr(spy::simd_instruction_set == spy::vsx_)
+        if constexpr(spy::simd_instruction_set >= spy::vsx_)
         {
           if constexpr( signed_v && size_check(8,2) ) return wrap<__vector __bool long>{};
           if constexpr(!signed_v && size_check(8,2) ) return wrap<__vector __bool long>{};
