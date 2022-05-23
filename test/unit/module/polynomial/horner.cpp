661cd34f0a0bcd5da83c0907491403f510296a9f
jtlap <jtlapreste@gmail.com>
2022-05-23 14:59:06 +0200

    comp -> compensated

--- test/unit/module/polynomial/horner.cpp
+++ test/unit/module/polynomial/horner.cpp
@@ -93,10 +93,10 @@ EVE_TEST( "Check behavior of horner on wide"
     TTS_EQUAL(numeric(horner)(a0, tab2), (fma)(a0, 1, 2));
     TTS_EQUAL(numeric(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
-    TTS_EQUAL(comp(horner)(a0, tab0), T(0));
-    TTS_EQUAL(comp(horner)(a0, tab1), T(1));
-    TTS_EQUAL(comp(horner)(a0, tab2), (fma)(a0, 1, 2));
-    TTS_EQUAL(comp(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
+    TTS_EQUAL(compensated(horner)(a0, tab0), T(0));
+    TTS_EQUAL(compensated(horner)(a0, tab1), T(1));
+    TTS_EQUAL(compensated(horner)(a0, tab2), (fma)(a0, 1, 2));
+    TTS_EQUAL(compensated(horner)(a0, tab3), (fma)(a0, (fma)(a0, 1, 2), 3));
 
     TTS_EQUAL((horner)(a0, tab0), eve::detail::poleval(a0, tab0));
     TTS_EQUAL((horner)(a0, tab1), eve::detail::poleval(a0, tab1));
