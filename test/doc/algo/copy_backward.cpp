cadd1cb7ebbab64f222d89616731faa5b8adc485
Joel Falcou <joel.falcou@lri.fr>
2022-05-26 19:00:49 +0200

    Fix copy_backward test for doc

--- test/doc/algo/copy_backward.cpp
+++ test/doc/algo/copy_backward.cpp
@@ -1,4 +1,5 @@
 #include <eve/algo/copy.hpp>
+#include <eve/algo/as_range.hpp>
 #include <iostream>
 #include <vector>
 
@@ -7,7 +8,7 @@ int main()
   std::vector<int> from_vector = {1,2,3,4,5,6,7,8,9,10};
   std::vector<int> to_vector(15);
 
-  eve::algo::copy_backward(from_vector, to_vector);
+  eve::algo::copy_backward(from_vector, eve::algo::as_range(to_vector.end()-10, to_vector.end()) );
 
   std::cout << "from_vector = ";
   for (auto i: from_vector) std::cout << i << " ";
