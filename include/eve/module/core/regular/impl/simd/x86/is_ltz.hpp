70973b7921ce632ae73b4629559ac615e19b890c
Joel Falcou <joel.falcou@lri.fr>
2022-05-31 11:30:25 +0200

    Don't code on a monday without coffee

--- include/eve/module/core/regular/impl/simd/x86/is_ltz.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_ltz.hpp
@@ -17,10 +17,10 @@ namespace eve::detail
   // Generate better code on x86 than just comparing to 0
   template<signed_integral_value T, typename N>
   EVE_FORCEINLINE constexpr auto is_ltz_(EVE_SUPPORTS(sse2_), wide<T,N> const& v) noexcept
-  requires x86_abi<abi_t<T, N>>
+  requires(sizeof(T) < 8 && x86_abi<abi_t<T, N>> && abi_t<T, N>::is_wide_logical)
   {
     using l_t = as_logical_t<wide<T,N>>;
-    if constexpr( abi_t<T, N>::is_wide_logical )  return bit_cast(v >> 31, as<l_t>{});
-    else                                          return  is_ltz_(EVE_RETARGET(cpu_), v);
+    constexpr auto shift = 8*sizeof(T) - 1;
+    return bit_cast(v >> shift, as<l_t>{});
   }
 }
