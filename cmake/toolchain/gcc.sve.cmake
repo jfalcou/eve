dc1b8b84104d63d2dc60f1d9e279f3a9ab8a0b7c
Joel Falcou <joel.falcou@lri.fr>
2022-06-01 17:59:21 +0200

    Add SVE target for CI

---
+++ cmake/toolchain/gcc.sve.cmake
@@ -0,0 +1,13 @@
+##==================================================================================================
+##  EVE - Expressive Vector Engine
+##  Copyright : EVE Contributors & Maintainers
+##  SPDX-License-Identifier: MIT
+##==================================================================================================
+set(CMAKE_SYSTEM_NAME Linux     )
+set(CMAKE_SYSTEM_PROCESSOR arm  )
+
+set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-11  )
+set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-11  )
+set(CMAKE_CXX_FLAGS     "-Wno-psabi -DEVE_NO_FORCEINLINE ${EVE_OPTIONS} -march=armv8-a+sve -msve-vector-bits=512" )
+
+set(CMAKE_CROSSCOMPILING_CMD qemu-aarch64)
