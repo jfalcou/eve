c8759bb3d1f0fe1d40354b763c3f8b31bcce71e5
Joel Falcou <joel.falcou@lri.fr>
2022-06-02 19:09:44 +0200

    Better plain_scalar

--- include/eve/concept/scalar.hpp
+++ include/eve/concept/scalar.hpp
@@ -8,18 +8,22 @@
 #pragma once
 
 #include <eve/detail/kumi.hpp>
+#include <eve/detail/meta.hpp>
 #include <type_traits>
 #include <cstdint>
 #include <cstddef>
 
 namespace eve
 {
+  template<typename T, typename... Ts>
+  concept one_of = (std::same_as<T,Ts> || ... || false);
+
   template<typename T>
-  concept plain_scalar  = std::is_same_v<T,float>         ||  std::is_same_v<T,double>
-                      ||  std::is_same_v<T,std::int8_t>   ||  std::is_same_v<T,std::int16_t>
-                      ||  std::is_same_v<T,std::int32_t>  ||  std::is_same_v<T,std::int64_t>
-                      ||  std::is_same_v<T,std::uint8_t>  ||  std::is_same_v<T,std::uint16_t>
-                      ||  std::is_same_v<T,std::uint32_t> ||  std::is_same_v<T,std::uint64_t>;
+  concept plain_scalar =  one_of< T
+                                , float, double
+                                , std::int8_t , std::int16_t , std::int32_t , std::int64_t
+                                , std::uint8_t, std::uint16_t, std::uint32_t, std::uint64_t
+                                >;
 
   namespace detail
   {
