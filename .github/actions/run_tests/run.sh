dfc6a86611ff29767b710dfb4112ebd49948b6d7
Joel Falcou <joel.falcou@lri.fr>
2022-06-19 18:26:31 +0200

    Aggregate tests to save more Compile Time

--- .github/actions/run_tests/run.sh
+++ .github/actions/run_tests/run.sh
@@ -2,52 +2,22 @@
 
 compile_target()
 {
-  echo "::group::Compiling $1" ;
-  ninja -v $1 -j 8;
+  echo "::group::Compiling Unit Tests" ;
+  ninja -v unit -j 8;
   compile=$?;
   echo "::endgroup::" ;
 
   return $compile;
 }
 
-compile_targets()
-{
-  for i in `../cmake/toolchain/filter.sh $1 keys`;
-  do
-  compile_target $i;
-  if [ "$?" -ne "0" ]
-  then
-    echo "::error $i can not be compiled!" ;
-    return 1;
-  fi
-  done;
-
-  return 0;
-}
-
 test_target()
 {
-  echo "::group::Running $1 tests" ;
-  ctest --output-on-failure -R $1 -j 8;
+  echo "::group::Running Unit Tests" ;
+  ctest --output-on-failure -R ^unit.*.exe -j 8;
   tested=$?;
   echo "::endgroup::" ;
 
   return $tested;
-}
-
-test_targets()
-{
-  for i in `../cmake/toolchain/filter.sh $1 values`;
-  do
-    test_target $i;
-    if [ "$?" -ne "0" ]
-    then
-      echo "::error $i tests failed!" ;
-      return 1;
-    fi
-  done;
-
-  return 0;
 }
 
 echo "::group::Running: 'cmake .. -G Ninja -DEVE_OPTIONS="$1" $2'"
@@ -56,56 +26,16 @@ cd build
 cmake .. -G Ninja -DEVE_OPTIONS="$1" $2
 echo "::endgroup::"
 
-compile_targets ../cmake/toolchain/arch.targets.json
+compile_target
 if [ "$?" -eq "1" ]
 then
   exit 1;
 fi
 
-test_targets    ../cmake/toolchain/arch.targets.json
+test_target
 if [ "$?" -eq "1" ]
 then
   exit 1;
-fi
-
-compile_targets ../cmake/toolchain/api.targets.json
-if [ "$?" -eq "1" ]
-then
-  exit 1;
-fi
-
-test_targets    ../cmake/toolchain/api.targets.json
-if [ "$?" -eq "1" ]
-then
-  exit 1;
-fi
-
-if [ "$3" -eq "1" ]
-then
-
-  compile_targets ../cmake/toolchain/doc.targets.json
-  if [ "$?" -eq "1" ]
-  then
-    exit 1;
-  fi
-
-  test_targets    ../cmake/toolchain/doc.targets.json
-  if [ "$?" -eq "1" ]
-  then
-    exit 1;
-  fi
-
-  compile_targets ../cmake/toolchain/real.targets.json
-  if [ "$?" -eq "1" ]
-  then
-    exit 1;
-  fi
-
-  test_targets    ../cmake/toolchain/real.targets.json
-  if [ "$?" -eq "1" ]
-  then
-    exit 1;
-  fi
 fi
 
 exit 0
