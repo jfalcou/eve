c50e4f59403d982c9267a9d60e3f2971c3e4dd3b
Joel Falcou <joel.falcou@lri.fr>
2022-06-30 19:15:04 +0200

    Factorize sum style cleanup into remove_garbage

--- include/eve/detail/remove_garbage.hpp
+++ include/eve/detail/remove_garbage.hpp
@@ -9,6 +9,7 @@
 
 #include <eve/traits.hpp>
 #include <eve/detail/function/bit_cast.hpp>
+#include <eve/module/core/regular/slide_right.hpp>
 #include <utility>
 
 namespace eve::detail
@@ -27,6 +28,24 @@ namespace eve::detail
       }(std::make_index_sequence<ec_t::value>{});
 
       v = bit_cast(bit_cast(v,as<as_wide_t<v_t,ec_t>>()) & exact_mask, as(v));
+    }
+
+    return v;
+  }
+
+ template<typename W> auto slide_garbage(W v) noexcept
+  {
+    using v_t   = element_type_t<W>;
+    using c_t   = cardinal_t<W>;
+    using ec_t  = expected_cardinal_t<v_t, typename W::abi_type>;
+
+    if constexpr(c_t::value < ec_t::value)
+    {
+      v = bit_cast( slide_right ( bit_cast(v,as<wide<v_t,ec_t>>())
+                                , index<ec_t::value - c_t::value>
+                                )
+                  , as(v)
+                  );
     }
 
     return v;
