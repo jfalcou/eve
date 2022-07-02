5eaa3304678f9c1112c74ce50437d2665e9d96aa
Joel Falcou <joel.falcou@lri.fr>
2022-07-02 14:28:40 +0200

    Test FetchContent support

--- .github/actions/integration/entrypoint.sh
+++ .github/actions/integration/entrypoint.sh
@@ -1,21 +1,28 @@
 #!/bin/sh -l
 set -e
 
-echo "::group::Configure EVE"
+echo "::group::Prepare EVE repository for branch " $1
 mkdir build && cd build
 cmake .. -G Ninja
-echo "::endgroup::"
-
-echo "::group::Install EVE"
 ninja install | grep cmake
+CURRENT_SHA1=`git rev-parse HEAD`
 cd ..
 echo "::endgroup::"
 
-echo "::group::Compile EVE-dependent project"
+echo "::group::Test EVE via the install target"
 mkdir install-test && cd install-test
 cmake ../integration/install-test -G Ninja
 ninja test_eve
 ./test_eve
+cd ..
+echo "::endgroup::"
+
+echo "::group::Test EVE via FetchContent"
+mkdir fetch-test && cd fetch-test
+cmake ../integration/fetch-test -G Ninja -DEVE_SHA1=$1
+ninja test_eve
+./test_eve
+cd ..
 echo "::endgroup::"
 
 return 0;
