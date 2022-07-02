097752cb2e178aab9f52a4b1193e13310e6e97ea
Joel Falcou <joel.falcou@lri.fr>
2022-07-02 15:38:53 +0200

    Parallel install tests

--- .github/actions/integration/entrypoint.sh
+++ .github/actions/integration/entrypoint.sh
@@ -1,36 +1,42 @@
 #!/bin/sh -l
 set -e
 
-echo "::group::Prepare EVE repository for branch " $1
-mkdir build && cd build
-cmake .. -G Ninja
-ninja install | grep cmake
-CURRENT_SHA1=`git rev-parse HEAD`
-cd ..
-echo "::endgroup::"
+if [ $2 -eq 0 ]
+then
+  echo "::group::Prepare EVE repository for branch " $1
+  mkdir build && cd build
+  cmake .. -G Ninja
+  ninja install | grep cmake
+  CURRENT_SHA1=`git rev-parse HEAD`
+  cd ..
+  echo "::endgroup::"
 
-echo "::group::Test EVE via the install target"
-mkdir install-test && cd install-test
-cmake ../test/integration/install-test -G Ninja
-ninja test_eve
-./test_eve
-cd ..
-echo "::endgroup::"
+  echo "::group::Test EVE via the install target"
+  mkdir install-test && cd install-test
+  cmake ../test/integration/install-test -G Ninja
+  ninja test_eve
+  ./test_eve
+  echo "::endgroup::"
+fi
 
-echo "::group::Test EVE via FetchContent"
-mkdir fetch-test && cd fetch-test
-cmake ../test/integration/fetch-test -G Ninja -DEVE_SHA1=$1
-ninja test_eve
-./test_eve
-cd ..
-echo "::endgroup::"
+if [ $2 -eq 1 ]
+then
+  echo "::group::Test EVE via FetchContent"
+  mkdir fetch-test && cd fetch-test
+  cmake ../test/integration/fetch-test -G Ninja -DEVE_SHA1=$1
+  ninja test_eve
+  ./test_eve
+  echo "::endgroup::"
+fi
 
-echo "::group::Test EVE via CPM"
-mkdir cpm-test && cd cpm-test
-cmake ../test/integration/cpm-test -G Ninja -DEVE_SHA1=$1
-ninja test_eve
-./test_eve
-cd ..
-echo "::endgroup::"
+if [ $2 -eq 2 ]
+then
+  echo "::group::Test EVE via CPM"
+  mkdir cpm-test && cd cpm-test
+  cmake ../test/integration/cpm-test -G Ninja -DEVE_SHA1=$1
+  ninja test_eve
+  ./test_eve
+  echo "::endgroup::"
+fi
 
 return 0;
