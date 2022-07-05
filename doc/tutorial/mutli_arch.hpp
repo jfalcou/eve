d40688294848d62883067e8a8e14c001148475c0
Joel Falcou <joel.falcou@lri.fr>
2022-07-05 19:52:25 +0200

    Grammar & typos fixes

--- doc/tutorial/mutli_arch.hpp
+++ doc/tutorial/mutli_arch.hpp
@@ -5,21 +5,20 @@
 
 # The Issue
 The **EVE** library uses compile-time code generation process to ensure a high-level of performance.
-However, there can be many reasons why your application or library maybe run on unknown systems.
-Thus, you may want to be able to write your SIMD kernel once but be able to adapt to your end-user's
+However, there can be many reasons why your application or library might run on unknown systems.
+Thus, you may want to be able to write your SIMD kernel once and yet be able to adapt to your end-user's
 hardware.
 
 A first solution could be to just recompile a given kernel with different architecture settings and
 link everything. Then, at runtime, use eve::is_supported to choose the correct implementation
-between the available ones. If the premise of this idea is correct, you'll face issues as the
+between the available ones. However, in attempting to solve the problem in this way, you'll face issues as the
 core of **EVE** is based on templated callable object, you may end up violating the One Definition
-Rule and ends up with a binary containing the incorrect symbols and implementations.
+Rule and end up with a binary containing the incorrect symbols and implementations.
 
 # From static to dynamic dispatch
-The correct process is based on the same premisses but avoids those pitfalls. The idea will be to
-isolate the various version of a given kernel into separate binaries. In this case, no multiple
-definition can happen. This means using **dynamic libraries** to store your code and use
-**dynamic loading**.
+A more successful approach is isolate the various version of a given kernel into separate dynamic
+libraries. In this case, no multiple definition can happen. This means using **dynamic libraries**
+to store your code and use **dynamic loading**.
 
 Dynamic loading in itself is a large topic mostly due to its OS specific components so
 [a good read on the subject is maybe in order](https://en.wikipedia.org/wiki/Dynamic_loading).
@@ -33,26 +32,24 @@ vectorise this process.
 @snippet test/integration/multi-arch/kernel.cpp kernel
 
 The code breaks down this way:
-  + We start by including all the necessary include files for the algorithm, functions and types we
+  + We start by including all the necessary include files for the algorithms, functions and types we
     will use.
   + We define the `compute_kernel` function. This function takes a pointer and a size as parameters
-    and is declared `extern "C"` to prevent unwanted C++ mangling of the symbol. This is a classical
-    process for dynamic library. Remark we use the `"compute_kernel"` symbol directly. This is possible
-    because the `compute_kernel` function has been compiled with `extern "C"`, thus preventing
-    C++ mangling to occur.
+    and is declared `extern "C"` to prevent unwanted C++ mangling of the symbol. This is a typical
+    process for dynamic library.
   + Inside `compute_kernel`, we display some informations about the SIMD API we will be using and
     proceed to the computation. As a quick way to handle the data, we pass a `std::span` to our
     algorithm.
 
 Nothing special is required except for the `extern "C"` attribute. If we want to get around this
-limitation and have arbitrary function taking arbitrary C++ types as parameters, there is different
-strategies discussed in various place from arbitrarily use the mangled name to just export a function
-returning an array or a structure containing all the pre-computed function pointer from the .so.
+limitation and have an arbitrary function taking arbitrary C++ types as parameters, there are different
+strategies.  One such strategy is to use the mangled name to export a function returning an array or
+structure containing all the pre-computed functions pointers from the `.so` library.
 
 ## Writing the dynamic function hub
 We now need to write an actual `compute` function that our users will call. This is the place where
-most of the OS dependent code will be written. The example we give is made to work on Linux. For a
-more OS independent way to handle dynamic loading, you can give a shot at
+most of the OS-dependent code will be written. The example we give is made to work on Linux. For a
+more OS-independent way to handle dynamic loading, you can have a look at
 [libltdl](https://www.gnu.org/software/libtool/manual/html_node/Using-libltdl.html) or use your
 preferred OS API.
 
@@ -70,12 +67,12 @@ What is going on inside `compute` ?
     results in static variables.
   + Once everything is loaded and setup, we call the function pointer with the appropriate parameters.
 
-Obviously, in realistic settings you would actually care about runtime issues, check that every pointers
+Obviously, in realistic settings you would actually care about runtime issues, check that every pointer
 are non-null, and use functions like `dlerror` to find out what would have caused an issue. This is
 left as an exercice for the reader.
 
 ## Compiling and Using Dynamic Kernels
-THe next part is much more down-to-earth. We need to compile the kernel function inside multiple
+The next part is much more down-to-earth. We need to compile the kernel function inside multiple
 dynamic libraries with different set of options. Then, we need to compile the main binary.
 To simplify this process, **EVE** provides a CMake function named `eve_build_variants` that you can
 access directly when using `find_package` or that can be included manually in your CMake after
@@ -121,17 +118,17 @@ target_link_libraries(multi-arch PUBLIC ${EVE_LIBRARIES})
 ```
 
 Let's get into the details:
-  + After the classical CMake basics, we use `find_package` to grab **EVE**. Depending on where your
+  + After the classical CMake basics, we use `find_package` to grab **EVE**. Depending on your
     **EVE** installation path, you may have to specify `CMAKE_PREFIX_PATH` for CMake to find it.
   + We call `eve_build_variants` to build a set of targets. The base name will be `compute` and there
-    is three different target suffix: `sse2`, `sse4`, `haswell`. For each of those, the following
+    are three different target suffixes: `sse2`, `sse4`, `haswell`. For each of those, the following
     options will be used:  `-msse2`, `-msse4.1`, `-mavx2`. Each target is compiled with its corresponding
     options. In the end, we expect three libraries to be compiled: `libcompute_sse2.so`, `libcompute_sse4.so`,
     and `libcompute_haswell.so`. Notice how the suffixes are arbitrary. They just need to correspond
     to any naming scheme you see fit and those names will have to be used in the dynamic loading function.
   + To perform this compilation, we use the `EVE_LIBRARIES` interface that contains all the proper
     setup for compiling **EVE** code.
-  + We add out executable target that just compiles a main file and the compute function. To be sure
+  + We add our executable target that just compiles a main file and the compute function. To be sure
     we don't forget to compile the libraries when we compile the main executable, we use the exported
     `eve_compute_variants` macro that contains the list of targets created by `eve_build_variants`.
 
@@ -148,8 +145,8 @@ After:
 2 2.82843 3.4641 4 4.47214 4.89898 5.2915 5.65685 6.32456 8.94427 10.9545 12.6491 14.1421 15.4919 16.7332 17.8885 18.9737 20 63.2456 200
 ```
 
-The complete project is available as a test in `test\integration\multi-arch`. As an exercice,
-try to modify the code to handle AVX512 and checks everything still works.
+The complete project is available as a test in `test\integration\multi-arch`. As an exercise,
+try to modify the code to handle AVX512 and check everything still works.
 
 # Conclusion
 Handling multiple architecture within a single application is not trivial. It requires some scaffolding,
