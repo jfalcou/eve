18cde3e2615f41387b2e23c8e477ebf8afd1b26b
jtlap <jtlapreste@gmail.com>
2022-07-11 13:50:39 +0200

    ellint_1

--- include/eve/module/elliptic/regular/ellint_1.hpp
+++ include/eve/module/elliptic/regular/ellint_1.hpp
@@ -14,80 +14,59 @@ namespace eve
   //================================================================================================
   //! @addtogroup elliptic
   //! @{
-  //! @var ellint_1
+  //!   @var ellint_1
+  //!   @brief Computes the  elliptic integrals of the first kind :
+  //!   \f$\int_0^{\phi} \frac{\mathrm{d}t}{\sqrt{1-k^2\sin^2 t}}\f$.
   //!
-  //! @brief Callable object computing the elliptic integrals of the first kind :
-  //!        \f$\int_0^{\phi} \frac{\mathrm{d}t}{\sqrt{1-k^2\sin^2 t}}\f$.
+  //!   $details$
   //!
-  //! **Required header:** `#include <eve/module/elliptic.hpp>`
+  //!   **Defined in Header**
   //!
-  //! #### Members Functions
+  //!   @code
+  //!   #include <eve/module/elliptic.hpp>
+  //!   @endcode
   //!
-  //! | Member       | Effect                                                     |
-  //! |:-------------|:-----------------------------------------------------------|
-  //! | `operator()` | the ellint_1 operation   |
-  //! | `operator[]` | Construct a conditional version of current function object |
+  //!   @groupheader{Callable Signatures}
   //!
-  //! ---
+  //!   @code
+  //!   namespace eve
+  //!   {
+  //!      template< eve::floating_real_value T >
+  //!      T ellint_1(T phi) noexcept;                                                //1
   //!
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-  //!  auto operator()(floating_real_value auto  k) const noexcept;
-  //!  auto operator()(floating_real_value auto phi, floating_real_value auto k) const noexcept;
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!      template< eve::floating_real_value T  eve::loating_real_value U >
+  //!      eve::common_compatible_value<T, U> ellint_1(T phi; U k) noexcept;          //2
+  //!   }
   //!
-  //! **Parameters**
+  //!   1. complete elliptic integrals of the first kind ( corresponds to \f$\phi = \pi/2\f$ ).
+  //!   2. incomplete elliptic integrals of the first kind.
   //!
-  //!`phi`, `k`:   [floating real values](@ref eve::floating_real_value).
+  //!   @endcode
   //!
-  //! computes the complete and incomplete elliptic integrals of the first kind :
+  //!   **Parameters**
   //!
-  //!\f[ F(\phi,k) = \int_0^{\phi} \frac{\mbox{d}t}{\sqrt{1-k^2\sin^2 t}}\f]
+  //!   `phi`, `k`:   [floating real arguments](@ref eve::floating_real_value).
   //!
-  //!The complete case corresponds to \f$\phi = \pi/2\f$.
+  //!   **Return value**
   //!
-  //!  - `k` must verify \f$k^2\sin^2\phi \le 1\f$ or the result is nan.
+  //!     the complete and incomplete elliptic integrals of the first kind are returned:
   //!
-  //!  - In the complete case this means \f$|k| \le 1\f$.
+  //!   **Notes**
   //!
-  //!  In any other case the result is nan.
+  //!     - `k` must verify \f$k^2\sin^2\phi \le 1\f$ or the result is nan.
   //!
-  //!@warning
-  //!  Be aware that as \f$\pi/2\f$ is not exactly represented by floating point values the result of the incomplete
-  //!  function with a \f$\phi\f$ value near \f$\pi/2\f$ can differ a lot with the result of the complete call.
+  //!     - In the complete case this means \f$|k| \le 1\f$.
   //!
-  //! **Return value**
+  //!  @warning
+  //!    Be aware that as \f$\pi/2\f$ is not exactly represented by floating point
+  //!    values the result of the incomplete  function with a \f$\phi\f$ value
+  //!    "representing" \f$\pi/2\f$ can differ a lot with the result of the complete call.
   //!
-  //!Returns  [elementwise](@ref glossary_elementwise) the elliptic integral of the first kind.
   //!
-  //! The result type is of the compatibility type of the three parameters.
+  //!  @groupheader{Example}
   //!
-  //! ---
-  //!
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-  //!  auto operator[]( conditional_expression auto cond ) const noexcept;
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  //!
-  //!  Higher-order function generating a masked version of eve::ellint_1
-  //!
-  //!  **Parameters**
-  //!
-  //!  `cond` : conditional expression
-  //!
-  //!  **Return value**
-  //!
-  //!  A Callable object so that the expression `ellint_1[cond](x, ...)` is equivalent to `if_else(cond,ellint_1(x, ...),x)`
-  //!
-  //! ---
-  //!
-  //! #### Supported decorators
-  //!
-  //!  no decorators are supported
-  //!
-  //! #### Example
-  //!
-  //! @godbolt{doc/elliptic/ellint_1.cpp}
-  //!
-  //!  @}
+  //!  @godbolt{doc/elliptic/regular/ellint_1.cpp}
+  //! @}
   //================================================================================================
   EVE_MAKE_CALLABLE(ellint_1_, ellint_1);
 }
