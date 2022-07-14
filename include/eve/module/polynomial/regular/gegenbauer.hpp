8c7ea790326a99eb3e845a94b2b933c6db3fc4b0
jtlap <jtlapreste@gmail.com>
2022-07-14 10:07:17 +0200

    gegenbauer

--- include/eve/module/polynomial/regular/gegenbauer.hpp
+++ include/eve/module/polynomial/regular/gegenbauer.hpp
@@ -14,74 +14,51 @@ namespace eve
   //================================================================================================
   //! @addtogroup polynomial
   //! @{
-  //! @var gegenbauer
+  //!   @var gegenbauer
+  //!   @brief Computes the value of a  gegenbauer polynomial \f$ \mathbf{C}_n^\lambda(x)\f$.
   //!
-  //! @brief Callable object computing the gegenbauer operation.
+  //!   The Gegenbauer polynomials are a sequence of orthogonal polynomials relative to
+  //!   \f$(1-x^2)^{\lambda-1/2\f$ on the \f$[-1, +1]\f$ interval satisfying the following
+  //!   recurrence relation:
+  //!     * \f$ \mathbf{C}_0^\lambda(x) = 1\f$.
+  //!     * \f$ \mathbf{C}_1^\lambda(x) = 2\lambda x\f$.
+  //!     * \f$ \mathbf{C}_n^\lambda(x) = \left[(2x+\lambda-1)\mathbf{C}_{n-1}^\lambda(x) -
+  //!           (n+2\lambda-2)\mathbf{C}_{n-2}^\lambda(x)\right]/n\f$.
   //!
-  //! **Required header:** `#include <eve/module/polynomial.hpp>`
+  //!   **Defined in header**
   //!
-  //! #### Members Functions
+  //!   @code
+  //!   #include <eve/module/polynomial.hpp>
+  //!   @endcode
   //!
-  //! | Member       | Effect                                                     |
-  //! |:-------------|:-----------------------------------------------------------|
-  //! | `operator()` | the gegenbauer operation   |
-  //! | `operator[]` | Construct a conditional version of current function object |
+  //!   @groupheader{Callable Signatures}
   //!
-  //! ---
+  //!   @code
+  //!   namespace eve
+  //!   {
+  //!     template< eve::integral_value T eve::floating_real_value T, eve::floating_real_value U >
+  //!     auto  gegenbauer(N n, T lambda, U x) noexcept;
+  //!   }
+  //!   @endcode
   //!
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-  //!  auto operator()( integral_value auto n
-  //!                 , floating_value auto l
-  //!                 , floating_value auto x) const noexcept;
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //!   **Parameters**
   //!
-  //! ---
+  //!     * `n` :  [integral argument](@ref eve::integrall_value).
   //!
-  //! **Parameters**
+  //!     * `lambda` :  [real floating argument](@ref eve::floating_real_value).
+  //!                   Must be greater than \f$-\frac12\f$.
   //!
-  //!`n`:   [integral values](@ref eve::value).
+  //!     * `x` :  [real floating argument](@ref eve::floating_real_value) .
   //!
-  //!`l`:   [floating values](@ref eve::value), elements must be greater than \f$-\frac12\f$.
+  //!    **Return value**
   //!
-  //!`x`:   [floating values](@ref eve::value).
+  //!    the value of \f$ \mathbf{C}_n^\lambda(x)f$
+  //!    is returned.
   //!
-  //! **Return value**
+  //!  @groupheader{Example}
   //!
-  //! Returns [elementwise](@ref glossary_elementwise) the value of the gegenbauer polynomial of order `n` at `l`, `x`:
-  //!
-  //! The result type is of the [common compatible type](@ref common_compatible) of the two last parameters,
-  //! if necessary with the [cardinal](@ref eve::cardinal) of the first.
-  //!
-  //! ---
-  //!
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-  //!  auto operator[]( conditional_expression auto cond ) const noexcept;
-  //!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  //!
-  //!  Higher-order function generating a masked version of eve::gegenbauer
-  //!
-  //!  **Parameters**
-  //!
-  //!  `cond` : conditional expression
-  //!
-  //!  **Return value**
-  //!
-  //!  A Callable object so that the expression `gegenbauer[cond](x, ...)` is equivalent to `if_else(cond,gegenbauer(x, ...),x)`
-  //!
-  //! ---
-  //!
-  //! #### Supported decorators
-  //!
-  //!  * eve::diff, eve::diff_1st, eve::diff_nth
-  //!
-  //!
-  //!     The expression `diff(gegenbauer)(n,l,x)` computes the derivative of the function relative to `x`.
-  //!
-  //! #### Example
-  //!
-  //! @godbolt{doc/polynomial/gegenbauer.cpp}
-  //!
-  //!  @}
+  //!  @godbolt{doc/special/regular/gegenbauer.cpp}
+  //! @}
   //================================================================================================
   EVE_MAKE_CALLABLE(gegenbauer_, gegenbauer);
 }
