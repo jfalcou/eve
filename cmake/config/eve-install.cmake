3f96d241a5e8ec81a74513dec7b76d933f3c1f71
Justen Di Ruscio <justend29@gmail.com>
2022-07-17 15:39:47 -0300

    export namespaced library name

--- cmake/config/eve-install.cmake
+++ cmake/config/eve-install.cmake
@@ -15,13 +15,14 @@ target_include_directories( eve_lib INTERFACE
                             $<INSTALL_INTERFACE:${INSTALL_DEST}>
                           )
 target_compile_features(eve_lib INTERFACE cxx_std_20)
+set_target_properties(eve_lib PROPERTIES EXPORT_NAME eve)
 add_library(eve::eve ALIAS eve_lib)
 
 ## =================================================================================================
-## Install target with versionned folder
+## Install target with versioned folder
 ## =================================================================================================
 install(TARGETS   eve_lib EXPORT eve_lib  DESTINATION "${MAIN_DEST}")
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/eve               DESTINATION "${INSTALL_DEST}" )
 install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-config.cmake    DESTINATION "${MAIN_DEST}"    )
 install(FILES     ${PROJECT_SOURCE_DIR}/cmake/eve-multiarch.cmake DESTINATION "${MAIN_DEST}"    )
-install(EXPORT    eve_lib DESTINATION "${MAIN_DEST}")
+install(EXPORT    eve_lib NAMESPACE "eve::" DESTINATION "${MAIN_DEST}")
