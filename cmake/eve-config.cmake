9c815d253d9e929b95c752611ce1bebae5542086
Justen Di Ruscio <justend29@gmail.com>
2022-07-17 16:50:16 -0300

    remove include() on non-existant file

--- cmake/eve-config.cmake
+++ cmake/eve-config.cmake
@@ -8,6 +8,5 @@
 ## Reuse install.cmake to preapre package properly
 ##==================================================================================================
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/eve_lib.cmake)
 include(${SELF_DIR}/eve-multiarch.cmake)
 set(EVE_LIBRARIES eve_lib)
