bcbe3fb4dc75fc72d6df8ca648ba18633cd46f9a
Joel Falcou <joel.falcou@lri.fr>
2022-07-21 14:58:07 +0200

    Fix long double usage in test

--- test/unit/module/core/oneosqrteps.cpp
+++ test/unit/module/core/oneosqrteps.cpp
@@ -37,7 +37,7 @@ TTS_CASE_TPL( "Check behavior of oneosqrteps on wide"
   using eve::upward;
   using elt_t = eve::element_type_t<T>;
 
-  TTS_EQUAL(eve::oneosqrteps(as<T>()), T(1.0l/std::sqrt(eve::eps(as<eve::element_type_t<T>>()))));
+  TTS_EQUAL(eve::oneosqrteps(as<T>()), T(elt_t(1.0l/std::sqrt(eve::eps(as<elt_t>())))));
   TTS_EXPECT(eve::all(downward(eve::oneosqrteps)(as<elt_t>()) <= std::sqrt(1.0l/(long double)(eve::eps(as<elt_t>())))));
   TTS_EXPECT(eve::all(  upward(eve::oneosqrteps)(as<elt_t>()) >= std::sqrt(1.0l/(long double)(eve::eps(as<elt_t>())))));
   TTS_EXPECT(eve::all(downward(eve::oneosqrteps)(as<T>()) <= eve::oneosqrteps(as<T>())));
