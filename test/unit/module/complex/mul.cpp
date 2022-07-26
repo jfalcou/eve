81c60ac563b6421707fabb97776e396f55eecfbe
jtlap <jtlapreste@gmail.com>
2022-07-26 21:50:11 +0200

    bad test remaning

--- test/unit/module/complex/mul.cpp
+++ test/unit/module/complex/mul.cpp
@@ -16,47 +16,45 @@ TTS_CASE_WITH( "Check behavior of average on wide"
                            , tts::randoms(-10, 10)
                            , tts::randoms(-10, 10))
              )
-  <typename T>(T const& a0, T const& a1, T const& , T const&  )
+  <typename T>(T const& a0, T const& a1, T const& a2 , T const& a3 )
 {
   using z_t = eve::as_complex_t<T>;
   auto a = z_t(a0, a1);
-//   auto b = z_t(a2, a3);
-//   TTS_ULP_EQUAL( eve::mul(a, b), a*b, 0.5);
-//   TTS_ULP_EQUAL( eve::pedantic(eve::mul)(a, b), a*b, 0.5);
-  if (eve::cardinal_v<T> == 1)
+  auto b = z_t(a2, a3);
+  TTS_ULP_EQUAL( eve::mul(a, b), a*b, 0.5);
+  TTS_ULP_EQUAL( eve::pedantic(eve::mul)(a, b), a*b, 0.5);
   {
-    std::cout << "a " << a << std::endl;
-    {
-      z_t c(a0, eve::inf(eve::as(a0)));
-      z_t rr(eve::minf(eve::as(a0)), eve::inf(eve::as(a0)));
-      z_t ac = a*c;
-      z_t  d = eve::pedantic(eve::mul)(a, c);
-      TTS_ULP_EQUAL( d, rr, 0.5);
-      TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
-      TTS_ULP_EQUAL( ac, rr, 0.5);
-    }
-    {
-      z_t c(eve::inf(eve::as(a0)), a0);
-      z_t rr(eve::inf(eve::as(a0)), eve::nan(eve::as(a0)));
-      z_t a(1, 0);
-      z_t  d = eve::pedantic(eve::mul)(a, c);
-      TTS_ULP_EQUAL( d, c, 0.5);
-      TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
-    }
-    {
-      z_t c(eve::inf(eve::as(a0)), a0);
-      z_t rr(eve::inf(eve::as(a0)), eve::inf(eve::as(a0)));
-      z_t  d = eve::pedantic(eve::mul)(a, c);
-      TTS_ULP_EQUAL( d, rr, 0.5);
-      TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
-    }
-    {
-      z_t a(eve::inf(eve::as(a0)), 1.5);
-      z_t c(1, 0);
-      z_t rr(eve::inf(eve::as(a0)), eve::nan(eve::as(a0)));
-      z_t  d = eve::pedantic(eve::mul)(a, c);
-      TTS_ULP_EQUAL( d, a, 0.5);
-      TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
-    }
+    z_t c(1, eve::inf(eve::as(a0)));
+    z_t a(2.0, 3.0);
+    z_t rr(eve::minf(eve::as(a0)), eve::inf(eve::as(a0)));
+    z_t ac = a*c;
+    z_t  d = eve::pedantic(eve::mul)(a, c);
+    TTS_ULP_EQUAL( d, rr, 0.5);
+    TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
+    TTS_ULP_EQUAL( ac, rr, 0.5);
+  }
+  {
+    z_t c(eve::inf(eve::as(a0)), a0);
+    z_t rr(eve::inf(eve::as(a0)), eve::nan(eve::as(a0)));
+    z_t a(1, 0);
+    z_t  d = eve::pedantic(eve::mul)(a, c);
+    TTS_ULP_EQUAL( d, c, 0.5);
+    TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
+  }
+  {
+    z_t c(eve::inf(eve::as(a0)), 1.5);
+    z_t rr(eve::inf(eve::as(a0)), eve::inf(eve::as(a0)));
+    z_t a(eve::inf(eve::as(a0)), 1.5);
+    z_t  d = eve::pedantic(eve::mul)(a, c);
+    TTS_ULP_EQUAL( d, rr, 0.5);
+    TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
+  }
+  {
+    z_t a(eve::inf(eve::as(a0)), 1.5);
+    z_t c(1, 0);
+    z_t rr(eve::inf(eve::as(a0)), eve::nan(eve::as(a0)));
+    z_t  d = eve::pedantic(eve::mul)(a, c);
+    TTS_ULP_EQUAL( d, a, 0.5);
+    TTS_ULP_EQUAL( eve::mul(a, c), rr, 0.5);
   }
 };
