d8adb490b9cd50dd245ba0a675abfd8afa75f364
Joel Falcou <joel.falcou@lri.fr>
2022-08-01 22:11:35 +0200

    Upgrade to latest Docker
    Including g++-12 support

--- cmake/toolchain/gcc.sve256.cmake
+++ cmake/toolchain/gcc.sve256.cmake
@@ -6,8 +6,8 @@
 set(CMAKE_SYSTEM_NAME Linux     )
 set(CMAKE_SYSTEM_PROCESSOR arm  )
 
-set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-11  )
-set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-11  )
+set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-12  )
+set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-12  )
 set(CMAKE_CXX_FLAGS     "-Wno-psabi -DEVE_NO_FORCEINLINE ${EVE_OPTIONS} -march=armv8-a+sve -msve-vector-bits=256" )
 
 set(CMAKE_CROSSCOMPILING_CMD ${PROJECT_SOURCE_DIR}/cmake/toolchain/run_sve256.sh     )
