8b31fd2c2f3e8c7caeb340dc789bece94358e0ab
jtlap <jtlapreste@gmail.com>
2022-08-06 13:39:06 +0200

    cafouillis with logical_or

--- include/eve/module/core/regular/logical_or.hpp
+++ include/eve/module/core/regular/logical_or.hpp
@@ -8,7 +8,7 @@
 #pragma once
 #include <eve/concept/value.hpp>
 #include <eve/detail/implementation.hpp>
-#include <eve/module/core/constant/false.hpp>
+#include <eve/module/core/constant/true.hpp>
 #include <eve/traits/as_logical.hpp>
 
 namespace eve
@@ -68,27 +68,30 @@ template<> struct supports_conditional<tag::logical_or_> : std::false_type
 
 EVE_MAKE_CALLABLE(logical_or_, logical_or);
 
-namespace detail
-{
-  template<logical_value T, logical_value U>
-  EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), T a, U b) noexcept
+  namespace detail
   {
-    if constexpr( scalar_value<T> || scalar_value<U> ) return as_logical_t<T>(a || b);
-    else return a || b;
-  }
+    template<logical_value T, logical_value U>
+    EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), T a, U b) noexcept
+    {
+      if constexpr( scalar_value<T> && scalar_value<U> )  return as_logical_t<T>(a || b);
+      else                                                return a || b;
+    }
 
-  template<logical_value T>
-  EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), T a, bool b) noexcept
-  {
-    return b ? T {a} : false_(as<T>());
-  }
+    template<logical_value T>
+    EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), T a, bool b) noexcept
+    {
+      return b ? true_(as<T>()) : T{a};
+    }
 
-  template<logical_value U>
-  EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), bool a, U b) noexcept
-  {
-    return a ? U {b} : false_(as<U>());
-  }
+    template<logical_value U>
+    EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), bool a, U b) noexcept
+    {
+      return a ? true_(as<U>()) : U{b};
+    }
 
-  EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), bool a, bool b) noexcept { return a || b; }
-}
+    EVE_FORCEINLINE auto logical_or_(EVE_SUPPORTS(cpu_), bool a, bool b) noexcept
+    {
+      return a || b;
+    }
+  }
 }
