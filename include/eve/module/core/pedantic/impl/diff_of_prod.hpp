959d264425f5b572d176a035797d2e88fd69128f
jtlap <jtlapreste@gmail.com>
2022-08-06 19:47:26 +0200

    Implements raw diff/sum_of_prod

--- include/eve/module/core/pedantic/impl/diff_of_prod.hpp
+++ include/eve/module/core/pedantic/impl/diff_of_prod.hpp
@@ -16,14 +16,31 @@ namespace eve::detail
   template<floating_real_value T
            , floating_real_value U, floating_real_value V, floating_real_value W >
   EVE_FORCEINLINE auto diff_of_prod_(EVE_SUPPORTS(cpu_)
-                                  , pedantic_type const &
+                                   , pedantic_type const &
                                    , const T& a
                                    , const U& b
                                    , const V& c
                                    , const W& d
                                    ) noexcept
+{
+  return arithmetic_call(pedantic(diff_of_prod), a, b, c, d);
+}
+
+  template<floating_real_value T>
+  EVE_FORCEINLINE auto diff_of_prod_(EVE_SUPPORTS(cpu_)
+                                   , pedantic_type const &
+                                   , const T& a
+                                   , const T& b
+                                   , const T& c
+                                   , const T& d
+                                   ) noexcept
+  requires(has_native_abi_v<T>)
   {
-    return diff_of_prod(a, b, c, d);
+    T mcd = c * d;
+    T err = pedantic(fma)(-c, d, mcd);
+    T dop = pedantic(fms)(a, b, mcd);
+    return if_else(is_finite(err), dop + err, dop);
   }
+
 
 }
