0793ad7543dd3832cba5c7b6a3ff053157191239
Joel Falcou <joel.falcou@lri.fr>
2022-08-09 00:16:52 +0200

    Benchmarks

--- benchmarks/module/core/is_ordered/regular/is_ordered.hpp
+++ benchmarks/module/core/is_ordered/regular/is_ordered.hpp
@@ -2,7 +2,7 @@
 /*
   EVE - Expressive Vector Engine
   Copyright : EVE Contributors & Maintainers
-  SPDX-License-Identifier: MIT
+  SPDX-License-Identifier: BSL-1.0
 */
 //==================================================================================================
 #include <eve/module/core.hpp>
@@ -12,7 +12,7 @@ int main()
 {
   auto lmin = eve::valmin(eve::as<EVE_VALUE>());
   auto lmax = eve::valmax(eve::as<EVE_VALUE>());
-  using L_VALUE = eve::logical<EVE_VALUE>; 
+  using L_VALUE = eve::logical<EVE_VALUE>;
   auto arg0 = eve::bench::random_<EVE_VALUE>(lmin,lmax);
   auto arg1 = eve::bench::random_<EVE_VALUE>(lmin,lmax);
   auto std__is_ordered =  [](EVE_VALUE x,  EVE_VALUE y) -> eve::logical<EVE_VALUE> {return L_VALUE(!std::isunordered(x, y)); };
