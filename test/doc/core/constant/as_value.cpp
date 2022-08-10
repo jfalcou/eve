6b540333bb9c50cc5897c836d5a8ed2e2ebe5ccd
jtlap <jtlapreste@gmail.com>
2022-08-10 15:48:57 +0200

    unused value in test

--- test/doc/core/constant/as_value.cpp
+++ test/doc/core/constant/as_value.cpp
@@ -8,8 +8,7 @@ using wide_it = eve::wide<int16_t, eve::fixed<4>>;
 int main()
 {
   std::cout << "---- simd" << '\n'
-            << "<- pf                               = " << pf << '\n'
-/           << "<- as_value(pf, as<wide_ft>())      = " << eve::as_value(32.5, eve::as<wide_ft>()) << '\n'
+            << "<- as_value(pf, as<wide_ft>())      = " << eve::as_value(32.5, eve::as<wide_ft>()) << '\n'
             << "<- as_value(valmax, as<wide_it>())  = " << eve::as_value(eve::valmax, eve::as<wide_it>()) << '\n';
 
   double        xf = -62768.0f;
