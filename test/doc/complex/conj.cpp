fa5e80426144d2b8edd15c3015d614572a533580
jtlap <jtlapreste@gmail.com>
2022-08-18 10:32:36 +0200

    arithmetic complex doc tests

---
+++ test/doc/complex/conj.cpp
@@ -0,0 +1,18 @@
+#include <eve/module/complex.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+using wide_ft = eve::wide <float, eve::fixed<4>>;
+
+int main()
+{
+  wide_ft ref = { 0.0f, 1.0f, -1.0f, 0.5f};
+  wide_ft imf = { 2.0f , -1.0,  -5.0, 0.0};
+  auto z = eve::as_complex_t<wide_ft>(ref, imf);
+
+  std::cout
+    << "---- simd" << std::endl
+    << "<- z       = " << z << std::endl
+    << "-> conj(z) = " << eve::conj(z) << std::endl;
+  return 0;
+}
