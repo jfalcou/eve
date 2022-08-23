5a8e1335b6504b99310f4036e992487b7af11869
jtlap <jtlapreste@gmail.com>
2022-08-23 15:40:31 +0200

    exp log

--- include/eve/module/math/regular/log.hpp
+++ include/eve/module/math/regular/log.hpp
@@ -31,27 +31,57 @@ namespace eve
 //!   namespace eve
 //!   {
 //!      template< eve::floating_value T >
-//!      T log(T x) noexcept;
+//!      T log(T x) noexcept;                                    //1
+//!
+//!      template< eve::floating_value T >
+//!      eve::complex<T> exp(eve::complex<T> z) noexcept;        //2
 //!   }
 //!   @endcode
 //!
 //! **Parameters**
 //!
-//!`x`:   [floating real value](@ref eve::floating_real_value).
+//!   *  `x`:   [floating value](@ref eve::floating_value).
+//!   *  `z`:   [complex ](@ref eve::complex) value.
 //!
 //! **Return value**
 //!
-//! Returns the [elementwise](@ref glossary_elementwise) the natural logarithm  of `x`: \f$\log
-//! x\f$.
+//!   1. Returns the [elementwise](@ref glossary_elementwise) the natural
+//!      logarithm  of `x`: \f$\log x\f$.
 //!
-//!   * If the element is \f$\pm0\f$, \f$-\infty\f$ is returned.
-//!   * If the element is \f$1\f$, \f$+0\f$ is returned.
-//!   * If the element is \f$\infty\f$, \f$\infty\f$ is returned.
-//!   * If the element is less than 0, `NaN` is returned.
+//!      * If the element is \f$\pm0\f$, \f$-\infty\f$ is returned.
+//!      * If the element is \f$1\f$, \f$+0\f$ is returned.
+//!      * If the element is \f$\infty\f$, \f$\infty\f$ is returned.
+//!      * If the element is less than 0, `NaN` is returned.
+//!
+//!   2. Returns [elementwise](@ref glossary_elementwise) the exponential of the input
+//!      in the range of a strip in the interval \f$i\times[-i\pi, +i\pi] along the imaginary axis
+//!      and mathematically unbounded along the real axis. .
+//!
+//!      * The function is continuous onto the branch cut  along the negative real axis,
+//!        taking into account the sign of imaginary part
+//!      * for every z: `eve::log(eve::conj(z)) == eve::conj(eve::log(z))`
+//!      * If z is (-0), the result is (-\infty+i \pi )
+//!      * If z is (+0), the result is (-\infty)
+//!      * If z is (x+i \infty) (for any finite x), the result is (+\infty+i \pi/2)
+//!      * If z is (x+i NaN) (for any finite x), the result is (NaN+i NaN)
+//!      * If z is (-\infty+i y) (for any finite positive y), the result is (+\infty+i \pi )
+//!      * If z is (+\infty+i y) (for any finite positive y), the result is (+\infty)
+//!      * If z is (-\infty+i \infty), the result is (+\infty+i 3\pi/4)
+//!      * If z is (+\infty+i \infty), the result is (+\infty+i \pi/4)
+//!      * If z is (±\infty+i NaN), the result is (+\infty+i NaN)
+//!      * If z is (NaN+i y) (for any finite y), the result is (NaN+i NaN)
+//!      * If z is (NaN+i \infty), the result is (+\infty+i NaN)
+//!      * If z is (NaN+i NaN), the result is (NaN+i NaN)
 //!
 //!  @groupheader{Example}
 //!
+//!   **Real version**
+//!
 //!  @godbolt{doc/math/log.cpp}
+//!
+//!   **Complex version**
+//!
+//!  @godbolt{doc/complex/log.cpp}
 //!
 //!  @groupheader{Semantic Modifiers}
 //!
