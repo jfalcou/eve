f837e8d8df9f316b654d6eb8df909e9753014a2f
jtlap <jtlapreste@gmail.com>
2022-09-11 15:46:31 +0200

    sph_bessel_j1

--- include/eve/module/bessel/regular/impl/sph_bessel_j1.hpp
+++ include/eve/module/bessel/regular/impl/sph_bessel_j1.hpp
@@ -9,6 +9,7 @@
 
 #include <eve/module/core.hpp>
 #include <eve/module/math.hpp>
+#include <eve/module/bessel/regular/sph_bessel_jn.hpp>
 
 namespace eve::detail
 {
@@ -16,7 +17,7 @@ template<floating_value T>
 EVE_FORCEINLINE auto
 sph_bessel_j1_(EVE_SUPPORTS(cpu_), T x) noexcept
 {
-  return if_else((abs(x) <= eps(as(x))) || (x == inf(as(x))), zero, (sinc(x) - cos(x)) / x);
+  return sph_bessel_jn(1, x);
 }
 
 // -----------------------------------------------------------------------------------------------
