b5b88a9ef043c220c14db92546e7fb0a81f45479
jtlap <jtlapreste@gmail.com>
2022-09-13 19:17:55 +0200

    j1

--- test/doc/bessel/regular/cyl_bessel_j1.cpp
+++ test/doc/bessel/regular/cyl_bessel_j1.cpp
@@ -8,13 +8,13 @@ int main()
 {
 #include <eve/module/core.hpp>
 
-  wide_ft x = {0.5, -1.5, 0.1, -1.0, 19.0, 25.0, 21.5, 10000.0};
+  wide_ft x = {0.5, 1.5, 0.1, 1.0, 19.0, 25.0, 21.5, 10000.0};
   std::cout << "---- simd" << '\n'
             << "<- x                       = " << x << '\n'
             << "-> cyl_bessel_j1(x)        = " << eve::cyl_bessel_j1(x) << '\n'
             ;
 
-  double xd = -1.0;
+  double xd = 1.0;
 
   std::cout << "---- scalar" << '\n'
             << "<- xd                = " << xd << '\n'
