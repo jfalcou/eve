d8c204de24831b88aad26f91e4c40483197ad7fd
Joel Falcou <joel.falcou@lri.fr>
2022-10-12 21:57:31 +0200

    Optimal bit_cast

--- include/eve/detail/function/bit_cast.hpp
+++ include/eve/detail/function/bit_cast.hpp
@@ -7,41 +7,16 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/implementation.hpp>
-#include <eve/detail/spy.hpp>
-#include <eve/as.hpp>
-#include <cstring>
-
-#if defined(SPY_COMPILER_IS_GCC)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#endif
+#include <eve/arch.hpp>
+#include <eve/detail/overload.hpp>
 
 namespace eve
 {
   EVE_MAKE_CALLABLE(bit_cast_, bit_cast);
 }
 
-namespace eve::detail
-{
-  template<typename T, typename Target>
-  EVE_FORCEINLINE auto bit_cast_(EVE_SUPPORTS(cpu_), T const &a, as<Target> const &) noexcept
-  requires (sizeof(T) == sizeof(Target))
-  {
-    if constexpr(std::is_same_v<T, Target>)
-    {
-      return a;
-    }
-    else
-    {
-      [[maybe_unused]] Target that;
-      std::memcpy((char*)&that, (char*)&a, sizeof(a));
-      return that;
-    }
-  }
-}
+#include <eve/detail/function/simd/common/bit_cast.hpp>
 
-#if defined(SPY_COMPILER_IS_GCC)
-#pragma GCC diagnostic pop
+#if defined(EVE_INCLUDE_SVE_HEADER)
+#  include <eve/detail/function/simd/arm/sve/bit_cast.hpp>
 #endif
