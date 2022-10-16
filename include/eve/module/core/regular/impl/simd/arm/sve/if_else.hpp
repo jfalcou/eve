5cee99353899a365a37fcd1e052533f201ad8140
Joel Falcou <joel.falcou@lri.fr>
2022-10-16 15:43:31 +0200

    Basic if_else

---
+++ include/eve/module/core/regular/impl/simd/arm/sve/if_else.hpp
@@ -0,0 +1,36 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+template<real_scalar_value T, typename N>
+EVE_FORCEINLINE wide<T, N>
+if_else_(EVE_SUPPORTS(sve_),
+         logical<wide<T, N>> const& c,
+         wide<T, N> const& vt,
+         wide<T, N> const& vf) noexcept
+requires sve_abi<abi_t<T, N>>
+{
+  return svsel(c, vt, vf);
+}
+
+template<real_scalar_value T, typename N>
+EVE_FORCEINLINE logical<wide<T, N>>
+if_else_(EVE_SUPPORTS(sve_),
+         logical<wide<T, N>> const& c,
+         logical<wide<T, N>> const& vt,
+         logical<wide<T, N>> const& vf) noexcept
+requires sve_abi<abi_t<T, N>>
+{
+  return svsel(c, vt, vf);
+}
+}
