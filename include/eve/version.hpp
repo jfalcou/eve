1e03f9e19ba2a1c5caf6ff3f7ece9e8674d320bf
Joel Falcou <joel.falcou@lri.fr>
2022-10-18 21:40:48 +0200

    Upgrade versions

--- include/eve/version.hpp
+++ include/eve/version.hpp
@@ -7,11 +7,11 @@
 //==================================================================================================
 #pragma once
 
-#define EVE_VERSION 20220900
+#define EVE_VERSION 20220901
 
 #define EVE_VERSION_YEAR      (EVE_VERSION / 10000)
 #define EVE_VERSION_MONTH     ((EVE_VERSION % 10000) / 100)
 #define EVE_VERSION_PATCH     (EVE_VERSION % 100)
 
-#define EVE_LIB_VERSION "2022.09.00"
+#define EVE_LIB_VERSION "2022.09.01"
 #define EVE_LIB_CODENAME "Rosalind Serendipitous"
