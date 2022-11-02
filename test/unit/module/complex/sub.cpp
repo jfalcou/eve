f4953c0facdda9d2c33a7af0547b742e78de6a3f
jtlap <jtlapreste@gmail.com>
2022-11-02 20:14:24 +0100

    eve::add and eve::sub did not have any complex unit test

---
+++ test/unit/module/complex/sub.cpp
@@ -0,0 +1,52 @@
+//==================================================================================================
+/**
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+**/
+//==================================================================================================
+#include "test.hpp"
+#include "measures.hpp"
+#include <eve/module/complex.hpp>
+
+TTS_CASE_WITH( "Check behavior of average on wide"
+             , eve::test::simd::ieee_reals
+             ,tts::generate(tts::randoms(-10, 10)
+                           , tts::randoms(-10, 10)
+                           , tts::randoms(-10, 10)
+                           , tts::randoms(-10, 10))
+             )
+  <typename T>(T const& a0, T const& a1, T const& a2 , T const& a3 )
+{
+  using z_t = eve::as_complex_t<T>;
+  auto a = z_t(a0, a1);
+  auto b = z_t(a2, a3);
+  TTS_ULP_EQUAL( eve::sub(a, b), a-b, 0.5);
+  TTS_ULP_EQUAL( eve::pedantic(eve::sub)(a, b), a-b, 0.5);
+  {
+    z_t c(1, eve::inf(eve::as(a0)));
+    z_t a(2.0, 3.0);
+    z_t rr(1.0, eve::minf(eve::as(a0)));
+    z_t ac = a-c;
+    z_t  d = eve::pedantic(eve::sub)(a, c);
+    TTS_ULP_EQUAL( d, rr, 0.5);
+    TTS_ULP_EQUAL( eve::sub(a, c), rr, 0.5);
+    TTS_ULP_EQUAL( ac, rr, 0.5);
+  }
+  {
+    z_t c(eve::inf(eve::as(a0)), a0);
+    z_t rr(eve::minf(eve::as(a0)), -a0);
+    z_t a(1, 0);
+    z_t  d = eve::pedantic(eve::sub)(a, c);
+    TTS_ULP_EQUAL( d, rr, 0.5);
+    TTS_ULP_EQUAL( eve::sub(a, c), rr, 0.5);
+  }
+  {
+    z_t c(eve::inf(eve::as(a0)), 1.5);
+    z_t rr(eve::nan(eve::as(a0)), 0.0);
+    z_t a(eve::inf(eve::as(a0)), 1.5);
+    z_t  d = eve::pedantic(eve::sub)(a, c);
+    TTS_ULP_EQUAL( d, rr, 0.5);
+    TTS_ULP_EQUAL( eve::sub(a, c), rr, 0.5);
+  }
+};
