000e77dfd8f84761fe7f0ee294967f63bac3d62b
jtlap <jtlapreste@gmail.com>
2022-11-05 16:21:39 +0100

    more tests for operators

--- test/unit/module/complex/div.cpp
+++ test/unit/module/complex/div.cpp
@@ -14,14 +14,18 @@ TTS_CASE_WITH( "Check behavior of average on wide"
              ,tts::generate(tts::randoms(-10, 10)
                            , tts::randoms(-10, 10)
                            , tts::randoms(-10, 10)
+                           , tts::randoms(-10, 10)
+                           , tts::randoms(-10, 10)
                            , tts::randoms(-10, 10))
              )
-  <typename T>(T const& a0, T const& a1, T const& a2, T const& a3 )
+  <typename T>(T const& a0, T const& a1, T const& a2, T const& a3, T const& a4 , T const& a5  )
 {
   using z_t = eve::as_complex_t<T>;
   auto a = z_t(a0, a1);
   auto b = z_t(a2, a3);
+  auto c = z_t(a4, a5);
   TTS_ULP_EQUAL( eve::div(a, b), a/b, 0.5);
+  TTS_ULP_EQUAL( eve::div(a, b, c), a/(b*c), 0.5);
   TTS_ULP_EQUAL( eve::pedantic(eve::div)(a, b), a/b, 10);
   {
     z_t c(1, eve::inf(eve::as(a0)));
