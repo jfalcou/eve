758a1f63133880443e86f0f6a6b76e90884fe830
Joel Falcou <joel.falcou@lri.fr>
2022-11-09 00:02:27 +0100

    Add more passing tests to SVE subset

--- test/unit/internals/aggregation.cpp
+++ test/unit/internals/aggregation.cpp
@@ -10,7 +10,7 @@
 
 TTS_CASE("Check that aggregated_ ABI differs with architecture specificities" )
 {
-# if defined(SPY_SIMD_IS_X86_AVX512)
+# if defined(SPY_SIMD_IS_X86_AVX512) || defined(SPY_SIMD_IS_ARM_FIXED_SVE)
   TTS_EXPECT_NOT( eve::aggregated_::is_wide_logical );
 # else
   TTS_EXPECT( eve::aggregated_::is_wide_logical );
@@ -22,7 +22,7 @@ int aggregation_kind( eve::internal_bit_logical::aggregated_  ) { return -1; }
 
 TTS_CASE("Check that aggregated_ ABI participates to function mangling" )
 {
-# if defined(SPY_SIMD_IS_X86_AVX512)
+# if defined(SPY_SIMD_IS_X86_AVX512) || defined(SPY_SIMD_IS_ARM_FIXED_SVE)
   TTS_EQUAL( aggregation_kind(eve::aggregated_{}), -1);
 # else
   TTS_EQUAL( aggregation_kind(eve::aggregated_{}), +1);
