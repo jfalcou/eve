50323d4f347a52335c5a895bcbd1c60518394ddc
Joel Falcou <joel.falcou@lri.fr>
2022-11-09 21:26:55 +0100

    Min/Max

---
+++ include/eve/module/core/regular/impl/simd/arm/sve/min.hpp
@@ -0,0 +1,24 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/arch/arm/sve/sve_true.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+template<scalar_value T, typename N>
+EVE_FORCEINLINE auto
+min_(EVE_SUPPORTS(sve_), wide<T, N> const& a, wide<T, N> const& b) noexcept -> wide<T, N>
+requires sve_abi<abi_t<T, N>>
+{
+  return svmin_z(sve_true<T>(),a,b);
+}
+}
