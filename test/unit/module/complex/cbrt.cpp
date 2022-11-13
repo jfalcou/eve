3fa57b2d4edc1ce2b9d2b79ed7932f09182b6c9c
jtlap <jtlapreste@gmail.com>
2022-11-13 23:23:29 +0100

    is(_not)_denormal

--- test/unit/module/complex/cbrt.cpp
+++ test/unit/module/complex/cbrt.cpp
@@ -11,11 +11,6 @@
 #include <eve/module/complex.hpp>
 #include <complex>
 
-template < typename T >
-auto cv(std::complex < T > sc)
-{
-  return eve::complex<T>(sc.real(), sc.imag());
-}
 
 TTS_CASE_WITH( "Check behavior of cbrt on scalar"
         , tts::bunch<eve::test::scalar::ieee_reals>
@@ -25,13 +20,13 @@ TTS_CASE_WITH( "Check behavior of cbrt on scalar"
   <typename T>(T const& a0, T const& a1 )
 {
   using e_t = typename T::value_type;
-  using c_t = std::complex<e_t>;
+  using c_t = eve::complex<e_t>;
   for(auto e : a0)
   {
     for(auto f : a1)
     {
       auto z = c_t(e, f);
-      TTS_ULP_EQUAL(eve::cbrt(z),  eve::pow(z, eve::third(as(e))), 2);
+      TTS_ULP_EQUAL(eve::cbrt(z),  eve::pow(z, eve::third(eve::as(e))), 20);
     }
   }
 };
@@ -44,9 +39,7 @@ TTS_CASE_WITH( "Check behavior of cbrt on wide"
   <typename T>(T const& a0, T const& a1 )
 {
   using e_t = typename T::value_type;
-  using ce_t = eve::complex<e_t>;
   using z_t = eve::as_complex_t<T>;
-  using c_t = std::complex<e_t>;
   z_t z{a0, a1};
-  TTS_ULP_EQUAL(eve::cbrt(z), eve::pow(z, eve::third(as<e_t>())), 2);
+  TTS_ULP_EQUAL(eve::cbrt(z), eve::pow(z, eve::third(eve::as<e_t>())), 20);
 };
