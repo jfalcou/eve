082a6ad78397fdf2a1c811a4148d61594a619a12
Joel Falcou <joel.falcou@lri.fr>
2022-11-14 21:30:00 +0100

    Enfore no logical<->wide conversion rules

--- test/unit/module/core/convert/to_tuple.cpp
+++ test/unit/module/core/convert/to_tuple.cpp
@@ -45,15 +45,15 @@ TTS_CASE("Check convert return type for UDT/tuple")
 TTS_CASE("Conversion from/to UDTs &nd tuples")
 {
   TTS_EQUAL((eve::convert(udt::grid2d {-9, 70}, eve::as<udt::label_position>())),
-            (udt::label_position {-9.f, 'F'}));
+            (udt::label_position {-9.f, std::uint8_t{'F'}}));
 
-  TTS_EQUAL((eve::convert(udt::label_position {-9.f, 'F'}, eve::as<udt::grid2d>())),
+  TTS_EQUAL((eve::convert(udt::label_position {-9.f, std::uint8_t{'F'}}, eve::as<udt::grid2d>())),
             (udt::grid2d {-9, 70}));
 
   TTS_EQUAL((eve::convert(udt::grid2d {66, -9}, eve::as<kumi::tuple<std::int8_t, float>>())),
-            (kumi::tuple {'B', -9.f}));
+            (kumi::tuple {std::uint8_t{'B'}, -9.f}));
 
-  TTS_EQUAL((eve::convert(kumi::tuple {'B', -9.f}, eve::as<udt::grid2d>())),
+  TTS_EQUAL((eve::convert(kumi::tuple {std::uint8_t{'B'}, -9.f}, eve::as<udt::grid2d>())),
             (udt::grid2d {66, -9}));
 
   TTS_EQUAL((eve::convert(kumi::tuple {udt::grid2d {-9, 7}, udt::grid2d {3, 5}},
