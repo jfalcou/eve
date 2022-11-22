a350b9b4b22909f16a4d5c22c4e03e43b4b26217
Joel Falcou <joel.falcou@lri.fr>
2022-11-22 19:43:34 +0100

    SVE store and conditional store

---
+++ include/eve/module/core/regular/impl/simd/arm/sve/store.hpp
@@ -0,0 +1,47 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
+#include <eve/detail/implementation.hpp>
+#include <eve/module/core/regular/unalign.hpp>
+
+namespace eve::detail
+{
+// Regular store
+template<real_scalar_value T, typename N, simd_compatible_ptr<wide<T, N>> Ptr>
+EVE_FORCEINLINE void
+store_(EVE_SUPPORTS(sve_), wide<T, N> v, Ptr p)
+requires(sve_abi<abi_t<T, N>> && !has_store_equivalent<wide<T, N>, Ptr>)
+{
+  auto ptr = unalign(p);
+  if constexpr( N() != expected_cardinal_v<T> ) memcpy(ptr, (T const*)(&v), N::value * sizeof(T));
+  else svst1(sve_true<T>(), ptr, v);
+}
+
+// Conditional store
+template<scalar_value T,
+         typename N,
+         relative_conditional_expr       C,
+         simd_compatible_ptr<wide<T, N>> Ptr>
+EVE_FORCEINLINE void
+store_(EVE_SUPPORTS(sve_), C const& cond, wide<T, N> const& v, Ptr ptr) noexcept
+requires sve_abi<abi_t<T, N>> && (!has_store_equivalent<wide<T, N>, Ptr>)
+{
+  if constexpr( C::is_complete || C::has_alternative ) store_(EVE_RETARGET(cpu_), cond, v, ptr);
+  else if constexpr( N() != expected_cardinal_v<T> )
+  {
+    store_(EVE_RETARGET(cpu_), cond, v, ptr);
+  }
+  else
+  {
+    svst1(cond.mask(as<wide<T, N>>{}), unalign(ptr), v);
+  }
+}
+}
