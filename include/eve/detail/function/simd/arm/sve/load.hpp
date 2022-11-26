12acef8681cdf536829e2a8c4dca2ffceb75bcb3
Joel Falcou <joel.falcou@lri.fr>
2022-11-26 22:46:03 +0100

    Implements and tests more SVE memory functions (#1453)
    * Enable some memory tests
    
    * SVE store and conditional store
    
    * Enable memory tests
    
    * SVE has no need for compress_mask tests
    
    * Use store[keep_first] to optimize partial store
    
    * Proper masked load
    
    * Remove some test sneeding a bit more work
    
    * Formatting
    
    * use map_alternative in load

--- include/eve/detail/function/simd/arm/sve/load.hpp
+++ include/eve/detail/function/simd/arm/sve/load.hpp
@@ -15,28 +15,73 @@
 
 namespace eve::detail
 {
-template<real_scalar_value T, typename N, simd_compatible_ptr<wide<T, N>> Ptr>
+template<relative_conditional_expr C,
+         real_scalar_value         T,
+         typename N,
+         simd_compatible_ptr<wide<T, N>> Ptr>
 EVE_FORCEINLINE wide<T, N>
-load_(EVE_SUPPORTS(sve_), ignore_none_ const&, safe_type const&, eve::as<wide<T, N>> const&, Ptr p)
+load_(EVE_SUPPORTS(sve_), C const& cond, safe_type const&, eve::as<wide<T, N>> const&, Ptr p)
 requires(sve_abi<abi_t<T, N>>)
 {
   auto ptr = unalign(p);
 
-  if constexpr( N() == expected_cardinal_v<T> ) return svld1(sve_true<T>(), ptr);
-  else return svldff1(sve_true<T>(), ptr);
+  if constexpr(C::is_complete && C::is_inverted)
+  {
+    if constexpr( N() == expected_cardinal_v<T> ) return svld1(sve_true<T>(), ptr);
+    else return svldff1(sve_true<T>(), ptr);
+  }
+  else
+  {
+    wide<T, N> loaded;
+
+    if constexpr(!C::is_complete)
+    {
+      auto mask = cond.mask(as<wide<T,N>>{});
+      if constexpr( N() == expected_cardinal_v<T> ) loaded = svld1(mask, ptr);
+      else                                          loaded = svldff1(mask, ptr);
+      if constexpr( C::has_alternative )
+      {
+        loaded = svsel(mask, loaded, wide<T,N>(cond.alternative));
+      }
+    }
+    else
+    {
+      if constexpr( C::has_alternative ) loaded = wide<T,N>(cond.alternative);
+    }
+
+    return loaded;
+  }
 }
 
 //================================================================================================
 // Logical support
 //================================================================================================
-template<typename T, typename N, data_source Pointer>
+template<relative_conditional_expr C, typename T, typename N, data_source Pointer>
 EVE_FORCEINLINE logical<wide<T, N>>
-load_ ( EVE_SUPPORTS(sve_), ignore_none_ const&, safe_type const&
-      , eve::as<logical<wide<T, N>>> const&, Pointer ptr
-      ) noexcept
+                load_(EVE_SUPPORTS(sve_),
+                      C const& cond,
+                      safe_type const&,
+                      eve::as<logical<wide<T, N>>> const&,
+                      Pointer ptr) noexcept
 requires(dereference_as<logical<T>, Pointer>::value && sve_abi<abi_t<T, N>>)
 {
-  auto block = load(ignore_none, safe, eve::as<wide<T, N>>{}, ptr_cast<T const>(ptr));
+  auto const c1     = map_alternative(cond, [](auto alt) { return alt.mask(); });
+  auto const block  = load(c1, safe, eve::as<wide<T, N>> {}, ptr_cast<T const>(ptr));
+  return to_logical(block);
+}
+
+template<relative_conditional_expr C,typename Iterator, typename T, typename N>
+EVE_FORCEINLINE logical<wide<T, N>>
+                load_(EVE_SUPPORTS(sve_),
+                      C const& cond,
+                      safe_type const&,
+                      eve::as<logical<wide<T, N>>> const&,
+                      Iterator b,
+                      Iterator e) noexcept
+requires sve_abi<abi_t<T, N>>
+{
+  auto const c1     = map_alternative(cond, [](auto alt) { return alt.mask(); });
+  auto const block  = load(c1, safe, eve::as<wide<T, N>> {}, b, e);
   return to_logical(block);
 }
 }
