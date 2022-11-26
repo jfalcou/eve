6b1393614f82348c36fcc4d292745d168318f889
Joel Falcou <joel.falcou@lri.fr>
2022-11-26 16:32:03 +0100

    Fix over-eager value_type fix

--- include/eve/traits/value_type.hpp
+++ include/eve/traits/value_type.hpp
@@ -17,34 +17,38 @@ namespace eve
 {
   namespace detail
   {
-    template<range T> constexpr auto value_type_impl()
-    {
-      using ref = std::add_lvalue_reference_t<T>;
-      return value_type_impl<decltype(begin(std::declval<ref>()))>();
-    }
-
-    template<std::input_or_output_iterator T> constexpr auto value_type_impl()
+    template<std::input_or_output_iterator T>
+    constexpr auto value_type_impl()
     {
       return std::type_identity<typename std::iterator_traits<T>::value_type>{};
     }
 
-    // Maybe this should be deleted
-    template<kumi::product_type T> constexpr auto value_type_impl()
-    {
-      auto mapper = []<typename U>(U)
-      {
-        return typename decltype(value_type_impl<U>())::type{};
-      };
-
-      return std::type_identity<kumi::result::map_t<decltype(mapper), T>>{};
-    }
-
-    template<typename T>
-    requires requires { typename T::value_type; }
+    template <typename T>
     constexpr auto value_type_impl()
     {
-      return std::type_identity<typename T::value_type>{};
+      if constexpr ( requires { typename T::value_type; } )
+      {
+        return std::type_identity<typename T::value_type>{};
+      }
+      // Maybe this should be deleted
+      else if constexpr ( kumi::product_type<T> )
+      {
+        auto mapper = []<typename U>(U)
+        {
+          return typename decltype( value_type_impl<U>() )::type{};
+        };
+
+        return std::type_identity<kumi::result::map_t<decltype(mapper), T>>{};
+      }
     }
+
+    template<range T>
+    constexpr auto value_type_impl()
+    {
+      using ref = std::add_lvalue_reference_t<T>;
+      return value_type_impl<decltype(std::begin(std::declval<ref>()))>();
+    }
+
   }
 
   //================================================================================================
@@ -70,7 +74,7 @@ namespace eve
   //! @}
   //================================================================================================
 
-  template<typename T>
-  requires (!value<T>)
+  template <typename T>
+    requires (!value<T>)
   using value_type_t = typename decltype(detail::value_type_impl<T>())::type;
 }
