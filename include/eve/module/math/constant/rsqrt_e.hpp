543d017d3b4708a774ebc5009e1f745699bdf4b3
jtlap <jtlapreste@gmail.com>
2022-12-04 15:35:03 +0100

    math constant

--- include/eve/module/math/constant/rsqrt_e.hpp
+++ include/eve/module/math/constant/rsqrt_e.hpp
@@ -51,7 +51,7 @@ EVE_MAKE_CALLABLE(rsqrt_e_, rsqrt_e);
 
 namespace detail
 {
-  template<flating_ordered_value T>
+  template<floating_ordered_value T>
   EVE_FORCEINLINE auto rsqrt_e_(EVE_SUPPORTS(cpu_), eve::as<T> const&) noexcept
   {
     using t_t = element_type_t<T>;
@@ -59,7 +59,7 @@ namespace detail
     else if constexpr( std::is_same_v<t_t, double> ) return T(0x1.368b2fc6f960ap-1);
   }
 
-  template<flating_ordered_value T, typename D>
+  template<floating_ordered_value T, typename D>
   EVE_FORCEINLINE constexpr auto rsqrt_e_(EVE_SUPPORTS(cpu_), D const&, as<T> const&) noexcept
       requires(is_one_of<D>(types<upward_type, downward_type> {}))
   {
