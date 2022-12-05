c4c32ecbf23634ffd3a1cf9841da5ba3b56edeb7
Joel Falcou <joel.falcou@lri.fr>
2022-12-05 19:06:09 +0100

    Add support for fma-like intrinsics

---
+++ include/eve/module/core/regular/impl/simd/arm/sve/fma.hpp
@@ -0,0 +1,33 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/arch/arm/sve/sve_true.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+template<arithmetic_scalar_value T, typename N>
+EVE_FORCEINLINE auto
+fma_(EVE_SUPPORTS(sve_), wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
+requires sve_abi<abi_t<T, N>>
+{
+  return fma[ignore_none](v0, v1, v2);
+}
+
+template<conditional_expr C, arithmetic_scalar_value T, typename N>
+EVE_FORCEINLINE auto
+fma_(EVE_SUPPORTS(sve_), C cond, wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
+requires sve_abi<abi_t<T, N>>
+{
+  auto const alt = alternative(cond, v0, as(v0));
+  if constexpr( C::is_complete && !C::is_inverted ) return alt;
+  return svmad_m(cond.mask(as(v0)), alt, v1, v2);
+}
+}
---
+++ include/eve/module/core/regular/impl/simd/arm/sve/fsm.hpp
@@ -0,0 +1,22 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+template<arithmetic_scalar_value T, typename N>
+EVE_FORCEINLINE auto
+fsm_(EVE_SUPPORTS(sve_), wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
+requires sve_abi<abi_t<T, N>>
+{
+  return -fanm(v0, v1, v2);
+}
+}
