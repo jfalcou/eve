72379ef336a11667dfc3c8bf19c767fe5f6072e2
Joel Falcou <joel.falcou@lri.fr>
2022-12-09 23:58:31 +0100

    Extend plain_scalar_value support
    A lot of internal code use non csdtint type and it would have been to
    complicated to change all of them. Also, the added types are commonly
    used by users they warrant to be taken into account.

--- include/eve/concept/scalar.hpp
+++ include/eve/concept/scalar.hpp
@@ -18,17 +18,15 @@
 namespace eve
 {
 template<typename T>
-concept plain_scalar_value = detail::one_of<T,
-                                            float,
-                                            double,
-                                            std::int8_t,
-                                            std::int16_t,
-                                            std::int32_t,
-                                            std::int64_t,
-                                            std::uint8_t,
-                                            std::uint16_t,
-                                            std::uint32_t,
-                                            std::uint64_t>;
+concept   plain_scalar_value
+        = detail::one_of<T
+                        , float, double
+                        , std::int8_t   , std::int16_t , std::int32_t , std::int64_t
+                        , std::uint8_t  , std::uint16_t, std::uint32_t, std::uint64_t
+                        , decltype(0ULL), decltype(0UL), decltype(0U)
+                        , decltype(0LL) , decltype(0L) , decltype(0)
+                        , std::ptrdiff_t, std::size_t
+                        >;
 
 namespace detail
 {
