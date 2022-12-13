2fe15ca9f4b7ebccb555eaffe8ca923e18c46b0a
Joel Falcou <joel.falcou@lri.fr>
2022-12-13 18:22:33 +0100

    Simplify plain_scalar_value checks

--- include/eve/concept/scalar.hpp
+++ include/eve/concept/scalar.hpp
@@ -17,25 +17,30 @@
 
 namespace eve
 {
-template<typename T>
-concept   plain_scalar_value
-        =  !(std::is_same_v<T, bool> || std::is_same_v<T, long double>)
-        &&  (std::is_floating_point_v<T> || std::is_integral_v<T>);
-
 namespace detail
 {
   template<typename T>
-  constexpr bool scalar_tuple()
+  constexpr bool is_plain() noexcept
+  {
+    return !(std::is_same_v<T, bool> || std::is_same_v<T, long double>)
+        &&  (std::is_floating_point_v<T> || std::is_integral_v<T>);
+  }
+
+  template<typename T>
+  constexpr bool scalar_tuple() noexcept
   {
     if constexpr(!kumi::product_type<T>) return false;
     else
     {
-      constexpr auto f = []<typename M>(M) { return std::bool_constant<plain_scalar_value<M>>{}; };
+      constexpr auto f = []<typename M>(M) { return std::bool_constant<is_plain<M>()>{}; };
       using flt_t = kumi::result::flatten_all_t<kumi::as_tuple_t<T>, decltype(f)>;
       return  kumi::all_of( flt_t{}, [](bool b) { return b; } );
     }
   }
 }
+
+template<typename T>
+concept plain_scalar_value = detail::is_plain<T>();
 
 template<typename T>
 concept product_scalar_value = detail::scalar_tuple<T>();
