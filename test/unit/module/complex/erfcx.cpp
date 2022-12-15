b7b9d8a4a0702c6085d8df31741dbac30224648f
jtlap <jtlapreste@gmail.com>
2022-12-15 11:02:06 +0100

    testing erfcx

--- test/unit/module/complex/erfcx.cpp
+++ test/unit/module/complex/erfcx.cpp
@@ -11,12 +11,12 @@
 #include <complex>
 
 
-TTS_CASE_TPL( "Check corner cases of faddeeva", eve::test::scalar::ieee_reals)
+TTS_CASE_TPL( "Check corner cases of erfcx", eve::test::scalar::ieee_reals)
 <typename T>(tts::type<T>)
 {
   using c_t = eve::complex<T>;
   using eve::as;
-  const int N = 10;
+  const int N = 9;
   auto zer = eve::zero(as<T>());
   auto inf = eve::inf(as<T>());
   auto nan = eve::nan(as<T>());
@@ -33,27 +33,26 @@ TTS_CASE_TPL( "Check corner cases of faddeeva", eve::test::scalar::ieee_reals)
       c_t(1.0e-4, 2), //5
       c_t(0.785398163397448, zer), //6
       c_t(inf,zer),   //7
-      c_t(inf,nan),   //8
-      c_t(nan,nan)    //9
+      c_t(nan,nan)    //8
     };
 
   std::array<c_t, N> results =
     {
-      c_t(3.04744205256913e-01, 2.08218938202832e-01 ),//0
-      c_t(3.91234021452136e-01, 1.27202410884648e-01 ),//1
-      c_t(2.82794674542325e-02, 2.81384332763369e-02 ),//2
-      c_t(0.00000000000000e+00, 8.68086731176095e-03 ),//3
-      c_t(4.27568139375386e-01, 2.73204092253626e-03 ),//4
-      c_t(2.55395675892478e-01, 1.06796461698883e-05 ),//5
-      c_t(5.39641485816297e-01, 5.97805988669632e-01 ),//6
-      c_t(0.00000000000000e+00, 0.00000000000000e+00 ),//7
-      c_t(0.00000000000000e+00, 0.00000000000000e+00 ),//8
-      c_t(nan, nan )                                   //9
+      c_t(3.04744205256913e-01, -2.08218938202832e-01 ), //0
+      c_t(3.54900332867578e-01, -3.42871719131101e-01 ), //1
+      c_t(2.82794674542325e-02, -2.81384332763369e-02 ), //2
+      c_t(eve::erfcx(T(65)), zer), //c_t(8.67881291138928e-03, +0.00000000000000e+00 ), //3
+      c_t(3.68702417397766e-01, -5.99851994495788e-01 ), //4
+      c_t(1.83388101767463e-02, -3.40018889576381e-01 ), //5
+      c_t(4.94195834537586e-01, +0.00000000000000e+00 ), //6
+      c_t(eve::erfcx(inf), zer), //c_t(0.00000000000000e+00, +0.00000000000000e+00 ), //7
+      c_t(nan                ,  nan                   )  //8
     };
   using eve::conj;
-  using eve::faddeeva;
+  using eve::erfcx;
   for(int i=0; i < N; ++i)
   {
-    TTS_ULP_EQUAL(faddeeva(inputs[i]), results[i], 100)  << "i " << i << " -> " << inputs[i] <<  " -> " <<faddeeva(inputs[i])<<"\n";
+    TTS_ULP_EQUAL(erfcx(inputs[i]), results[i], 100)  << "i " << i << " -> " << inputs[i] <<  " -> " <<erfcx(inputs[i])<<"\n";
   }
+  std::cout << erfcx(eve::inf(as(T()))) << std::endl;
 };
