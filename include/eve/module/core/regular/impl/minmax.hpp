4a5053a70c0af749da351223d292eaffc5e47dd6
Joel Falcou <joel.falcou@lri.fr>
2022-12-30 21:37:54 +0100

    Clearer selection process

--- include/eve/module/core/regular/impl/minmax.hpp
+++ include/eve/module/core/regular/impl/minmax.hpp
@@ -9,6 +9,7 @@
 
 #include <eve/arch/platform.hpp>
 #include <eve/concept/value.hpp>
+#include <eve/detail/category.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/module/core/regular/max.hpp>
 #include <eve/module/core/regular/min.hpp>
@@ -16,21 +17,19 @@
 
 namespace eve::detail
 {
-template<typename T>
+template<typename W>
 constexpr bool has_native_minmax() noexcept
 {
-  using e_t = element_type_t<T>;
-  constexpr bool small_int = !(std::integral<e_t> && sizeof(e_t) == 8);
+  if constexpr( scalar_value<W> ) return true;
+  else
+  {
+    using enum category;
+    constexpr bool is_ints64 = match(categorize<W>(), int64x4, uint64x4, int64x2, uint64x2);
 
-  if constexpr( x86_tag<current_api_type> )
-  {
-    return ((current_api <= avx2) && small_int) || current_api == avx512;
+    if constexpr( x86_tag<current_api_type> )       return current_api == avx512 || !is_ints64;
+    else if constexpr( arm_tag<current_api_type> )  return !is_ints64;
+    else return true;
   }
-  else if constexpr( arm_tag<current_api_type> )
-  {
-    if constexpr( current_api >= asimd ) return small_int; else return sizeof(e_t) != 8;
-  }
-  else return true;
 };
 
 // -----  Regular
@@ -48,7 +47,7 @@ minmax_(EVE_SUPPORTS(cpu_), T0 v0, T1 v1, Ts... vs) noexcept
     // If there is no native min/max, we compute the check once
     // We use > cause it is more often optimized than <
     auto check = v0 > v1;
-    return kumi::tuple {if_else(check, v1, v0), if_else(check, v0, v1)};
+    return kumi::tuple {if_else(check, v1, v0, vs...), if_else(check, v0, v1, vs...)};
   }
 }
 
@@ -77,7 +76,7 @@ minmax_(EVE_SUPPORTS(cpu_), Callable f)
   if constexpr( std::same_as<Callable, callable_is_less_> ) return eve::minmax;
   else if constexpr( std::same_as<Callable, callable_is_greater_> )
   {
-    return [](auto x, auto y) { return kumi::tuple {max(x, y), min(x, y)}; };
+    return [](auto x, auto y) { return kumi::reorder<1,0>(minmax(x,y)); };
   }
   else
   {
