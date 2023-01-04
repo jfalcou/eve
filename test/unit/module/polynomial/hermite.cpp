9bd79a17a07d4006fefc96fd5e977e9bdee9cf00
jtlap <jtlapreste@gmail.com>
2023-01-04 16:04:26 +0100

    hermite laguerre std tested

--- test/unit/module/polynomial/hermite.cpp
+++ test/unit/module/polynomial/hermite.cpp
@@ -8,8 +8,7 @@
 #include "test.hpp"
 
 #include <eve/module/polynomial.hpp>
-
-#include <boost/math/special_functions/hermite.hpp>
+#include <cmath>
 
 //==================================================================================================
 //== Types tests
@@ -40,22 +39,22 @@ TTS_CASE_WITH("Check behavior of hermite on wide",
   auto eve__hermitev = [](auto n, auto x) { return eve::hermite(n, x); };
   for( unsigned int n = 0; n < 5; ++n )
   {
-    auto boost_hermite = [&](auto i, auto) { return boost::math::hermite(n, a0.get(i)); };
-    TTS_ULP_EQUAL(eve__hermitev(n, a0), T(boost_hermite), 16);
+    auto std_hermite = [&](auto i, auto) { return std::hermite(n, a0.get(i)); };
+    TTS_ULP_EQUAL(eve__hermitev(n, a0), T(std_hermite), 16);
   }
-  auto boost_hermitev = [&](auto i, auto) { return boost::math::hermite(i0.get(i), a0.get(i)); };
-  TTS_ULP_EQUAL(eve__hermitev(i0, a0), T(boost_hermitev), 16);
+  auto std_hermitev = [&](auto i, auto) { return std::hermite(i0.get(i), a0.get(i)); };
+  TTS_ULP_EQUAL(eve__hermitev(i0, a0), T(std_hermitev), 16);
   for( unsigned int j = 0; j < eve::cardinal_v<T>; ++j )
   {
-    auto boost_hermite2 = [&](auto i, auto) { return boost::math::hermite(i0.get(i), a0.get(j)); };
-    TTS_ULP_EQUAL(eve__hermitev(i0, a0.get(j)), T(boost_hermite2), 32);
+    auto std_hermite2 = [&](auto i, auto) { return std::hermite(i0.get(i), a0.get(j)); };
+    TTS_ULP_EQUAL(eve__hermitev(i0, a0.get(j)), T(std_hermite2), 32);
   }
   for( unsigned int j = 0; j < eve::cardinal_v<T>; ++j )
   {
     for( unsigned int n = 0; n < eve::cardinal_v<T>; ++n )
     {
       TTS_ULP_EQUAL(
-          eve__hermitev(i0.get(j), a0.get(n)), v_t(boost::math::hermite(i0.get(j), a0.get(n))), 32);
+          eve__hermitev(i0.get(j), a0.get(n)), v_t(std::hermite(i0.get(j), a0.get(n))), 32);
     }
   }
 };
--- test/unit/module/polynomial/laguerre.cpp
+++ test/unit/module/polynomial/laguerre.cpp
@@ -8,8 +8,7 @@
 #include "test.hpp"
 
 #include <eve/module/polynomial.hpp>
-
-#include <boost/math/special_functions/laguerre.hpp>
+#include <cmath>
 
 //==================================================================================================
 //== Types tests
@@ -33,30 +32,30 @@ TTS_CASE_TPL("Check return types of laguerre on wide", eve::test::simd::ieee_rea
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of laguerre on wide",
               eve::test::simd::ieee_reals,
-              tts::generate(tts::between(-1, 1), tts::as_integer(tts::ramp(0))))
+              tts::generate(tts::between(0, 1), tts::as_integer(tts::ramp(0))))
 <typename T, typename I>(T const& a0, I const& i0)
 {
   using v_t           = eve::element_type_t<T>;
   auto eve__laguerrev = [](auto n, auto x) { return eve::laguerre(n, x); };
   for( unsigned int n = 0; n < 5; ++n )
   {
-    auto boost_laguerre = [&](auto i, auto) { return boost::math::laguerre(n, a0.get(i)); };
-    TTS_ULP_EQUAL(eve__laguerrev(n, a0), T(boost_laguerre), 1024);
+    auto std_laguerre = [&](auto i, auto) { return std::laguerre(n, a0.get(i)); };
+    TTS_ULP_EQUAL(eve__laguerrev(n, a0), T(std_laguerre), 1024);
   }
-  auto boost_laguerrev = [&](auto i, auto) { return boost::math::laguerre(i0.get(i), a0.get(i)); };
-  TTS_RELATIVE_EQUAL(eve__laguerrev(i0, a0), T(boost_laguerrev), 0.01);
+  auto std_laguerrev = [&](auto i, auto) { return std::laguerre(i0.get(i), a0.get(i)); };
+  TTS_RELATIVE_EQUAL(eve__laguerrev(i0, a0), T(std_laguerrev), 0.01);
   for( unsigned int j = 0; j < eve::cardinal_v<T>; ++j )
   {
-    auto boost_laguerre2 = [&](auto i, auto)
-    { return boost::math::laguerre(i0.get(i), a0.get(j)); };
-    TTS_RELATIVE_EQUAL(eve__laguerrev(i0, a0.get(j)), T(boost_laguerre2), 0.01);
+    auto std_laguerre2 = [&](auto i, auto)
+    { return std::laguerre(i0.get(i), a0.get(j)); };
+    TTS_RELATIVE_EQUAL(eve__laguerrev(i0, a0.get(j)), T(std_laguerre2), 0.01);
   }
   for( unsigned int j = 0; j < eve::cardinal_v<T>; ++j )
   {
     for( unsigned int n = 0; n < eve::cardinal_v<T>; ++n )
     {
       TTS_RELATIVE_EQUAL(eve__laguerrev(i0.get(j), a0.get(n)),
-                         v_t(boost::math::laguerre(i0.get(j), a0.get(n))),
+                         v_t(std::laguerre(i0.get(j), a0.get(n))),
                          0.01);
     }
   }
