c48f848db012964bf727336d15eb79fae6496f9a
jtlap <jtlapreste@gmail.com>
2023-01-05 12:11:37 +0100

    sph j0 and y1

--- test/unit/module/bessel/cyl_bessel_y1.cpp
+++ test/unit/module/bessel/cyl_bessel_y1.cpp
@@ -8,9 +8,7 @@
 #include "test.hpp"
 
 #include <eve/module/bessel.hpp>
-
-#include <boost/math/special_functions/bessel.hpp>
-#include <boost/math/special_functions/bessel_prime.hpp>
+#include <cmath>
 
 TTS_CASE_TPL("Check return types of cyl_bessel_y1", eve::test::simd::ieee_reals)
 <typename T>(tts::type<T>)
@@ -28,7 +26,7 @@ TTS_CASE_WITH("Check behavior of cyl_bessel_y1 on wide",
   using v_t = eve::element_type_t<T>;
 
   auto eve__cyl_bessel_y1 = [](auto x) { return eve::cyl_bessel_y1(x); };
-  auto std__cyl_bessel_y1 = [](auto x) -> v_t { return boost::math::cyl_neumann(v_t(1), x); };
+  auto std__cyl_bessel_y1 = [](auto x) -> v_t { return std::cyl_neumann(v_t(1), x); };
   if constexpr( eve::platform::supports_invalids )
   {
     TTS_ULP_EQUAL(eve__cyl_bessel_y1(eve::minf(eve::as<v_t>())), eve::nan(eve::as<v_t>()), 0);
@@ -38,27 +36,27 @@ TTS_CASE_WITH("Check behavior of cyl_bessel_y1 on wide",
     TTS_ULP_EQUAL(eve__cyl_bessel_y1(eve::inf(eve::as<T>())), T(0), 0);
     TTS_ULP_EQUAL(eve__cyl_bessel_y1(eve::nan(eve::as<T>())), eve::nan(eve::as<T>()), 0);
   }
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1500)), std__cyl_bessel_y1(v_t(1500)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(500)), std__cyl_bessel_y1(v_t(500)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(10)), std__cyl_bessel_y1(v_t(10)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(5)), std__cyl_bessel_y1(v_t(5)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(2)), std__cyl_bessel_y1(v_t(2)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1.5)), std__cyl_bessel_y1(v_t(1.5)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(0.5)), std__cyl_bessel_y1(v_t(0.5)), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1)), std__cyl_bessel_y1(v_t(1)), 2.5);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1500)), std__cyl_bessel_y1(v_t(1500)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(500)), std__cyl_bessel_y1(v_t(500)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(10)), std__cyl_bessel_y1(v_t(10)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(5)), std__cyl_bessel_y1(v_t(5)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(2)), std__cyl_bessel_y1(v_t(2)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1.5)), std__cyl_bessel_y1(v_t(1.5)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(0.5)), std__cyl_bessel_y1(v_t(0.5)), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(1)), std__cyl_bessel_y1(v_t(1)), 5000.0);
   TTS_ULP_EQUAL(eve__cyl_bessel_y1(v_t(0)), eve::minf(eve::as<v_t>()), 0.0);
 
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1500)), T(std__cyl_bessel_y1(v_t(1500))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(500)), T(std__cyl_bessel_y1(v_t(500))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(10)), T(std__cyl_bessel_y1(v_t(10))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(5)), T(std__cyl_bessel_y1(v_t(5))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(2)), T(std__cyl_bessel_y1(v_t(2))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1.5)), T(std__cyl_bessel_y1(v_t(1.5))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(0.5)), T(std__cyl_bessel_y1(v_t(0.5))), 2.0);
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1)), T(std__cyl_bessel_y1(v_t(1))), 2.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1500)), T(std__cyl_bessel_y1(v_t(1500))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(500)), T(std__cyl_bessel_y1(v_t(500))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(10)), T(std__cyl_bessel_y1(v_t(10))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(5)), T(std__cyl_bessel_y1(v_t(5))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(2)), T(std__cyl_bessel_y1(v_t(2))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1.5)), T(std__cyl_bessel_y1(v_t(1.5))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(0.5)), T(std__cyl_bessel_y1(v_t(0.5))), 5000.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(1)), T(std__cyl_bessel_y1(v_t(1))), 5000.0);
   TTS_ULP_EQUAL(eve__cyl_bessel_y1(T(0)), eve::minf(eve::as<T>()), 0.0);
 
-  TTS_ULP_EQUAL(eve__cyl_bessel_y1(a0), map(std__cyl_bessel_y1, a0), 512.0);
+  TTS_ULP_EQUAL(eve__cyl_bessel_y1(a0), map(std__cyl_bessel_y1, a0), 2048.0);
 };
 
 
