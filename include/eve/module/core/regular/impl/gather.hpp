ac0a8826f936e00a2045a0e2165f637d2df62b1d
Joel Falcou <joel.falcou@lri.fr>
2023-01-28 10:29:12 +0100

    Fixed common gather mask implementation

--- include/eve/module/core/regular/impl/gather.hpp
+++ include/eve/module/core/regular/impl/gather.hpp
@@ -7,6 +7,7 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/concept/conditional.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/memory/aligned_ptr.hpp>
@@ -25,11 +26,13 @@ gather_(EVE_SUPPORTS(cpu_), U const *ptr, wide<T, N> const& v) noexcept
   return that;
 }
 
-template<typename U, value X, integral_scalar_value T, typename N>
+template<typename U, conditional_expr C, integral_scalar_value T, typename N>
 EVE_FORCEINLINE auto
-gather_(EVE_SUPPORTS(cpu_), logical<X> const& cond, U const *ptr, wide<T, N> const& v) noexcept
+gather_(EVE_SUPPORTS(cpu_), C cx, U const *ptr, wide<T, N> const& v) noexcept
 {
-  return if_else(cond, gather(ptr, v), zero_);
+  auto src = alternative(cx, wide<U, N> {}, as<wide<U, N>> {});
+  auto m   = expand_mask(cx, as<wide<U, N>> {});
+  return if_else(m, gather(ptr, v), src);
 }
 
 //================================================================================================
@@ -41,14 +44,11 @@ gather_(EVE_SUPPORTS(cpu_), aligned_ptr<U, S> ptr, wide<T, N> const& v) noexcept
   return gather(ptr.get(), v);
 }
 
-template<typename U, typename S, value X, integral_scalar_value T, typename N>
+template<typename U, typename S, conditional_expr C, integral_scalar_value T, typename N>
 EVE_FORCEINLINE auto
-gather_(EVE_SUPPORTS(cpu_),
-        logical<X> const& cond,
-        aligned_ptr<U, S> ptr,
-        wide<T, N> const& v) noexcept
+gather_(EVE_SUPPORTS(cpu_), C cx, aligned_ptr<U, S> ptr, wide<T, N> const& v) noexcept
 {
-  return if_else(cond, gather(ptr.get(), v), zero_);
+  return gather[cx](ptr.get(), v);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
