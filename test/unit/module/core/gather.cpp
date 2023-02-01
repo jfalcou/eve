2ab9f8c148148326e9f3bc0c6b72dc389b3ec12b
Joel Falcou <joel.falcou@lri.fr>
2023-02-01 19:25:04 +0100

    x86 AVX2/AVX512 gather + masked gather (#1526)
    * x86 AVX2/AVX512 gather
    
    * Fixed common gather mask implementation
    
    * Genric implementation for masked should not fail
    
    * Add masking tests
    
    * Proper AVX512/AVX2 mask handling
    
    * More refactoring over gather/gather mask
    
    * Remove useless inversion in expand_mask
    
    * Fix impromptu error on AVX512 aggregate
    
    * Shortcut for ignore_all

--- test/unit/module/core/gather.cpp
+++ test/unit/module/core/gather.cpp
@@ -25,6 +25,22 @@ TTS_CASE_TPL("Check eve::gather behavior with 32 bits indexes", eve::test::simd:
   const v_t *cdata = &data[0];
   TTS_EQUAL(ref, eve::gather(eve::as_aligned(cdata, eve::cardinal_t<T> {}), maps));
   TTS_EQUAL(ref, eve::gather(eve::as_aligned(&data[0], eve::cardinal_t<T> {}), maps));
+
+  eve::as_logical_t<T> mask = [](auto i, auto) { return i % 2 == 0; };
+  T mref([&](auto i, auto) { return mask.get(i) ? data[maps.get(i)] : v_t{0}; });
+
+  auto cgathered  = eve::if_else( mask
+                                , eve::gather[mask](eve::as_aligned(cdata, eve::cardinal_t<T> {}), maps)
+                                , eve::zero
+                                );
+
+  auto gathered = eve::if_else( mask
+                              , eve::gather[mask](eve::as_aligned(&data[0], eve::cardinal_t<T> {}), maps)
+                              , eve::zero
+                              );
+
+  TTS_EQUAL(mref, gathered);
+  TTS_EQUAL(mref, cgathered);
 };
 
 TTS_CASE_TPL("Check eve::gather behavior with 64 bits indexes", eve::test::simd::all_types)
@@ -43,6 +59,23 @@ TTS_CASE_TPL("Check eve::gather behavior with 64 bits indexes", eve::test::simd:
   const v_t *cdata = &data[0];
   TTS_EQUAL(ref, eve::gather(eve::as_aligned(cdata, eve::cardinal_t<T> {}), maps));
   TTS_EQUAL(ref, eve::gather(eve::as_aligned(&data[0], eve::cardinal_t<T> {}), maps));
+
+  eve::as_logical_t<T> mask = [](auto i, auto) { return i % 2 == 0; };
+
+  T mref([&](auto i, auto) { return mask.get(i) ? data[maps.get(i)] : v_t{0}; });
+
+  auto cgathered  = eve::if_else( mask
+                                , eve::gather[mask](eve::as_aligned(cdata, eve::cardinal_t<T> {}), maps)
+                                , eve::zero
+                                );
+
+  auto gathered = eve::if_else( mask
+                              , eve::gather[mask](eve::as_aligned(&data[0], eve::cardinal_t<T> {}), maps)
+                              , eve::zero
+                              );
+
+  TTS_EQUAL(mref, gathered);
+  TTS_EQUAL(mref, cgathered);
 };
 
 TTS_CASE_TPL("Check unaligned eve::gather behavior with 32 bits indexes",
@@ -61,6 +94,22 @@ TTS_CASE_TPL("Check unaligned eve::gather behavior with 32 bits indexes",
   const v_t *cdata = &data[0];
   TTS_EQUAL(ref, eve::gather(cdata, maps));
   TTS_EQUAL(ref, eve::gather(&data[0], maps));
+
+  eve::as_logical_t<T> mask = [](auto i, auto) { return i % 2 == 0; };
+  T mref([&](auto i, auto) { return mask.get(i) ? data[maps.get(i)] : v_t{0}; });
+
+  auto cgathered  = eve::if_else( mask
+                                , eve::gather[mask](cdata, maps)
+                                , eve::zero
+                                );
+
+  auto gathered = eve::if_else( mask
+                              , eve::gather[mask](&data[0], maps)
+                              , eve::zero
+                              );
+
+  TTS_EQUAL(mref, gathered);
+  TTS_EQUAL(mref, cgathered);
 };
 
 TTS_CASE_TPL("Check unaligned eve::gather behavior with 64 bits indexes",
@@ -79,4 +128,20 @@ TTS_CASE_TPL("Check unaligned eve::gather behavior with 64 bits indexes",
   const v_t *cdata = &data[0];
   TTS_EQUAL(ref, eve::gather(cdata, maps));
   TTS_EQUAL(ref, eve::gather(&data[0], maps));
+
+  eve::as_logical_t<T> mask = [](auto i, auto) { return i % 2 == 0; };
+  T mref([&](auto i, auto) { return mask.get(i) ? data[maps.get(i)] : v_t{0}; });
+
+  auto cgathered  = eve::if_else( mask
+                                , eve::gather[mask](cdata, maps)
+                                , eve::zero
+                                );
+
+  auto gathered = eve::if_else( mask
+                              , eve::gather[mask](&data[0], maps)
+                              , eve::zero
+                              );
+
+  TTS_EQUAL(mref, gathered);
+  TTS_EQUAL(mref, cgathered);
 };
