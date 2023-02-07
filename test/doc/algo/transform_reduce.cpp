234b9802a9abceb328a2808a6fdaa74a8b34ea6a
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2023-02-07 13:54:32 +0000

    FIX-1541: fuse_operations for transform_reduce (#1543)

--- test/doc/algo/transform_reduce.cpp
+++ test/doc/algo/transform_reduce.cpp
@@ -9,19 +9,22 @@ int main()
 {
   std::vector<float>  v = {1.0f, 2.0f, 3.0f, 4.0f};
 
-  std::cout << " -> v                                                                                               = "
+  std::cout << " -> v                                                                                                                        = "
             << tts::as_string(v)
             << "\n";
 
-  std::cout << " -> eve::algo::transform_reduce(v, [](auto x) { return x + x }, 0.)                                 = "
+  std::cout << " -> eve::algo::transform_reduce(v, [](auto x) { return x + x }, 0.f)                                                         = "
             << eve::algo::transform_reduce(v, [](auto x) { return x + x; }, 0.) << "\n";
 
-  std::cout << " -> std::transform_reduce(v.begin(), v.end(), std::plus<>{}, 0., [](auto x) { return x + x })       = "
+  std::cout << " -> std::transform_reduce(v.begin(), v.end(), std::plus<>{}, 0.f, [](auto x) { return x + x })                               = "
             << std::transform_reduce(v.begin(), v.end(), 0., std::plus<>{}, [](auto x) { return x + x; }) << "\n";
 
-  std::cout << " -> eve::algo::reduce(eve::views::map(v, [](auto x) { return x + x }), 0.)                          = "
-            << eve::algo::reduce(eve::views::map(v, [](auto x) { return x + x; }), 0.) << "\n";
+  std::cout << " -> eve::algo::reduce(eve::views::map(v, [](auto x) { return x + x }), 0.f)                                                  = "
+            << eve::algo::reduce(eve::views::map(v, [](auto x) { return x + x; }), 0.f) << "\n";
 
-  std::cout << " -> eve::algo::transform_reduce(v, [](auto x) { return x + x }, std::pair{eve::mul, eve::one}, 0.)  = "
-            << eve::algo::transform_reduce(v, [](auto x) { return x + x; }, std::pair{eve::mul, eve::one}, 1.) << "\n";
+  std::cout << " -> eve::algo::transform_reduce(v, [](auto x) { return x + x }, std::pair{eve::mul, eve::one}, 1.f)  = "
+            << eve::algo::transform_reduce(v, [](auto x) { return x + x; }, std::pair{eve::mul, eve::one}, 1.f) << "\n";
+
+  std::cout << " -> eve::algo::transform_reduce[eve::algo::fuse_operations](v, [](auto x, auto sum) { return eve::fma(x, 2.f, sum); }, 0.f)  = "
+            << eve::algo::transform_reduce[eve::algo::fuse_operations](v, [](auto x, auto sum) { return eve::fma(x, .5f, sum); }, 0.f) << "\n";
 }
--- test/unit/module/algo/algorithm/transform_reduce_generic.cpp
+++ test/unit/module/algo/algorithm/transform_reduce_generic.cpp
@@ -23,8 +23,23 @@ template<typename TraitsSupport> struct transfrom_reduce_2_reduce_ : TraitsSuppo
 inline constexpr auto transform_reduce_2_reduce = eve::algo::function_with_traits<
     transfrom_reduce_2_reduce_>[eve::algo::transform_reduce.get_traits()];
 
+template<typename TraitsSupport> struct transfrom_reduce_2_reduce_fuse_ : TraitsSupport
+{
+  EVE_FORCEINLINE auto operator()(auto&& rng, auto init) const
+  {
+    return eve::algo::transform_reduce[TraitsSupport::get_traits()][eve::algo::fuse_operations](
+        EVE_FWD(rng), []<typename Sum, typename N>(auto x, eve::wide<Sum, N> sum) {
+          return eve::convert(x, eve::as<Sum>{}) + sum; }, init);
+  }
+};
+
+inline constexpr auto transfrom_reduce_2_reduce_fuse = eve::algo::function_with_traits<
+    transfrom_reduce_2_reduce_fuse_>[eve::algo::transform_reduce.get_traits()];
+
+
 TTS_CASE_TPL("Check transform reduce generic", algo_test::selected_types)
 <typename T>(tts::type<T>)
 {
   reduce_generic_all_test_cases(eve::as<T> {}, transform_reduce_2_reduce);
+  reduce_generic_all_test_cases(eve::as<T> {}, transfrom_reduce_2_reduce_fuse);
 };
