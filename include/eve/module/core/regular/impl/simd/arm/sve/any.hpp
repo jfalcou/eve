3ed2e2701b55cf93aabac9354633d96f1a83cb97
Joel Falcou <joel.falcou@lri.fr>
2023-02-23 18:13:53 +0100

    More optimised SVE garbage removal in any

--- include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
@@ -18,11 +18,13 @@ EVE_FORCEINLINE bool
 any_(EVE_SUPPORTS(sve_), C const& cond, logical<wide<T,N>> v) noexcept
 requires sve_abi<abi_t<T, N>>
 {
-  v = remove_garbage(v);
-
   if constexpr( C::is_complete )
   {
-    if constexpr( C::is_inverted )  return svptest_any(sve_true<T>(), v);
+    if constexpr( C::is_inverted )
+    {
+      using ec_t  = expected_cardinal_t<T, typename logical<wide<T,N>>::abi_type>;
+      return svptest_any(keep_first(N::value).mask(as<wide<T,ec_t>>{}), v);
+    }
     else                            return false;
   }
   else return svptest_any(cond.mask(as<wide<T,N>>{}),v);
