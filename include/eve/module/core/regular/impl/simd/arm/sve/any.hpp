9b2390a5d3d3008b2f2e01f88875324b49ba9f04
Joel Falcou <joel.falcou@lri.fr>
2023-02-23 15:33:42 +0100

    Fix any behavior in SVE with garbage values

--- include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
@@ -9,14 +9,17 @@
 
 #include <eve/detail/has_abi.hpp>
 #include <eve/detail/implementation.hpp>
+#include <eve/detail/remove_garbage.hpp>
 
 namespace eve::detail
 {
 template<scalar_value T, typename N, relative_conditional_expr C>
 EVE_FORCEINLINE bool
-any_(EVE_SUPPORTS(sve_), C const& cond, logical<wide<T,N>> const& v) noexcept
+any_(EVE_SUPPORTS(sve_), C const& cond, logical<wide<T,N>> v) noexcept
 requires sve_abi<abi_t<T, N>>
 {
+  v = remove_garbage(v);
+
   if constexpr( C::is_complete )
   {
     if constexpr( C::is_inverted )  return svptest_any(sve_true<T>(), v);
