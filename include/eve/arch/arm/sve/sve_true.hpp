2da8b78d56b92c81ab67dbb4a99b05efe6879c0f
Joel Falcou <joel.falcou@lri.fr>
2023-02-24 09:33:29 +0100

    Refactor garbage masking into sve_true

--- include/eve/arch/arm/sve/sve_true.hpp
+++ include/eve/arch/arm/sve/sve_true.hpp
@@ -8,10 +8,10 @@
 #pragma once
 
 #include <eve/arch.hpp>
+#include <eve/conditional.hpp>
 
 namespace eve::detail
 {
-
 // Returns clear sve_true for type
 // Calling svptrue_b8 is OK for most cases, where you OK with
 // 1s in insignificant bits but sometimes you are not.
@@ -23,5 +23,16 @@ sve_true()
   else if constexpr( sizeof(T) == 2 ) return svptrue_b16();
   else if constexpr( sizeof(T) == 4 ) return svptrue_b32();
   else if constexpr( sizeof(T) == 8 ) return svptrue_b64();
+}
+
+// Returns clear sve_true for type of a given cardinal
+// while masking potential garbage value
+template<typename T>
+EVE_FORCEINLINE svbool_t
+sve_true(T const&)
+{
+  using v_t   = element_type_t<T>;
+  using ec_t  = expected_cardinal_t<v_t, typename T::abi_type>;
+  return keep_first(T::size()).mask(as<as_wide_t<v_t,ec_t>>{});
 }
 } // namespace eve::detail
--- include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/any.hpp
@@ -10,6 +10,7 @@
 #include <eve/detail/has_abi.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/detail/remove_garbage.hpp>
+#include <eve/arch/arm/sve/sve_true.hpp>
 
 namespace eve::detail
 {
@@ -20,11 +21,7 @@ requires sve_abi<abi_t<T, N>>
 {
   if constexpr( C::is_complete )
   {
-    if constexpr( C::is_inverted )
-    {
-      using ec_t  = expected_cardinal_t<T, typename logical<wide<T,N>>::abi_type>;
-      return svptest_any(keep_first(N::value).mask(as<wide<T,ec_t>>{}), v);
-    }
+    if constexpr( C::is_inverted )  return svptest_any(sve_true(v) , v);
     else                            return false;
   }
   else return svptest_any(cond.mask(as<wide<T,N>>{}),v);
