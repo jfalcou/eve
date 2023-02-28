25adcf9d0c2d17e7802648f2ecbee9b2a998e6bf
Joel Falcou <joel.falcou@lri.fr>
2023-02-28 21:02:04 +0100

    More small fixes on SVE

--- include/eve/module/special/regular/impl/lfactorial.hpp
+++ include/eve/module/special/regular/impl/lfactorial.hpp
@@ -13,8 +13,7 @@
 
 namespace eve::detail
 {
-
-template<integral_value T>
+template<ordered_value T>
 EVE_FORCEINLINE auto
 lfactorial_(EVE_SUPPORTS(cpu_), T n) noexcept
 {
@@ -22,26 +21,21 @@ lfactorial_(EVE_SUPPORTS(cpu_), T n) noexcept
   EVE_ASSERT(eve::all(is_gez(n)), "lfactorial : some entry elements are not positive");
   if constexpr( has_native_abi_v<T> )
   {
-    auto r = eve::log(factorial(n));
-    if( eve::all(n < 171) ) return r;
-    else { return if_else(n < 171, r, eve::log_abs_gamma(float64(inc(n)))); }
-  }
-  else return apply_over(lfactorial, n);
-}
-
-template<floating_ordered_value T>
-EVE_FORCEINLINE auto
-lfactorial_(EVE_SUPPORTS(cpu_), T n) noexcept
-{
-  EVE_ASSERT(eve::all(is_flint(n)), "lfactorial : some entry elements are not flint");
-  EVE_ASSERT(eve::all(is_gez(n)), "lfactorial : some entry elements are not positive");
-  if constexpr( has_native_abi_v<T> )
-  {
-    using elt_t        = element_type_t<T>;
-    constexpr auto max = std::same_as<elt_t, double> ? 171 : 35;
+    constexpr auto max = std::same_as<element_type_t<T>, double> ? 171 : 35;
     auto           r   = eve::log(factorial(n));
+
     if( eve::all(n < max) ) return r;
-    else return if_else(n < max, r, log_abs_gamma(inc(n)));
+    else
+    {
+      auto np = [](auto x)
+      {
+        if constexpr(integral_value<T>) return float64(inc(x));
+        else                            return inc(x);
+      }(n);
+
+      return if_else(n < max, r, log_abs_gamma(np));
+
+    }
   }
   else return apply_over(lfactorial, n);
 }
