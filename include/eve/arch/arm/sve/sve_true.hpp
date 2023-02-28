6f96ba4a89b5f20d69cd2e07f2107000e446e394
Joel Falcou <joel.falcou@lri.fr>
2023-02-28 08:50:10 +0100

    Fix eve::any behavior in SVE for smaller registers

--- include/eve/arch/arm/sve/sve_true.hpp
+++ include/eve/arch/arm/sve/sve_true.hpp
@@ -8,10 +8,11 @@
 #pragma once
 
 #include <eve/arch.hpp>
+#include <eve/conditional.hpp>
+#include <eve/arch/fundamental_cardinal.hpp>
 
 namespace eve::detail
 {
-
 // Returns clear sve_true for type
 // Calling svptrue_b8 is OK for most cases, where you OK with
 // 1s in insignificant bits but sometimes you are not.
@@ -24,4 +25,24 @@ sve_true()
   else if constexpr( sizeof(T) == 4 ) return svptrue_b32();
   else if constexpr( sizeof(T) == 8 ) return svptrue_b64();
 }
-} // namespace eve::detail
+
+// Returns clear sve_true for type of a given cardinal
+// while masking potential garbage value
+template<relative_conditional_expr C, typename T>
+EVE_FORCEINLINE svbool_t
+sve_true(C cond, as<T> tgt)
+{
+  if constexpr(C::is_complete && C::is_inverted)
+  {
+    using v_t   = element_type_t<T>;
+    using fc_t  = fundamental_cardinal_t<v_t>;
+
+    if constexpr(T::size() >= fc_t::value )      return sve_true<v_t>();
+    else  return keep_first(T::size()).mask(as<as_wide_t<v_t,fc_t>>{});
+  }
+  else
+  {
+    return cond.mask(tgt);
+  }
+}
+}
