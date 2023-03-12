984b357fb48060f1ab2c9fbe3268301dc87e8f60
jtlap <jtlapreste@gmail.com>
2023-03-12 10:21:40 +0100

    using std::max

--- include/eve/module/core/promote/impl/fma.hpp
+++ include/eve/module/core/promote/impl/fma.hpp
@@ -13,6 +13,7 @@
 #include <eve/detail/skeleton_calls.hpp>
 
 #include <type_traits>
+#include <algorithm>
 
 namespace eve::detail
 {
@@ -20,23 +21,15 @@ namespace eve::detail
   EVE_FORCEINLINE auto
   fma_(EVE_SUPPORTS(cpu_), promote_type const&, T const& a, U const& b, V const& c) noexcept
   {
-   using et_t =  element_type_t<T>;
-   using eu_t =  element_type_t<U>;
-   using ev_t =  element_type_t<V>;
-   using er_t = common_type_t<et_t, eu_t, ev_t>;
-   constexpr int ct = cardinal_v<T>;
-   constexpr int cu = cardinal_v<U>;
-   constexpr int cv = cardinal_v<V>;
-   constexpr int Ntu = ct < cu ? cu : cv;
-   constexpr int N   = cv < Ntu ? Ntu : cv;
-   using r_t = wide<er_t, fixed<N>>;
-   auto cv_t = as(eve::as<er_t>());
-   if constexpr(has_native_abi_v<r_t>)
-   {
-     return fma(convert(a, cv_t), convert(b, cv_t), convert(c,cv_t));
-   }
-   else
-     return apply_over(fma, convert(a, cv_t), convert(b, cv_t), convert(c, cv_t));
+    using er_t = common_type_t<element_type_t<T>, element_type_t<U>, element_type_t<V>>;
+    constexpr auto N = std::max({cardinal_v<T>,cardinal_v<U>,cardinal_v<V>});
+    using r_t = wide<er_t, fixed<N>>;
+    auto cv_t = as(eve::as<er_t>());
+    if constexpr(has_native_abi_v<r_t>)
+    {
+      return fma(convert(a, cv_t), convert(b, cv_t), convert(c,cv_t));
+    }
+    else
+      return apply_over(fma, convert(a, cv_t), convert(b, cv_t), convert(c, cv_t));
   }
-
 }
