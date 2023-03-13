23d528c239871b0512c516083e7c4a554fe6fd79
Joel Falcou <joel.falcou@lri.fr>
2023-03-13 21:58:26 +0100

    Apple M1 runtime detection

--- include/eve/arch/arm/detection.hpp
+++ include/eve/arch/arm/detection.hpp
@@ -11,7 +11,10 @@
 #include <eve/arch/arm/neon/tags.hpp>
 
 #if defined( SPY_SIMD_IS_ARM )
-  #if defined( SPY_OS_IS_LINUX )
+  #if defined(SPY_OS_IS_MACOS)
+    #include <sys/types.h>
+    #include <sys/sysctl.h>
+  #elif defined( SPY_OS_IS_LINUX )
     #include <asm/hwcap.h>
     #include <sys/auxv.h>
   #endif
@@ -24,22 +27,34 @@ namespace eve
   //================================================================================================
   inline bool is_supported(neon128_ const &) noexcept
   {
-    #if defined( SPY_SIMD_IS_ARM )
+#if defined( SPY_SIMD_IS_ARM )
+# if defined(SPY_OS_IS_MACOS)
+    int         val = 0;
+    std::size_t len = sizeof(val);
+    return sysctlbyname("hw.optional.AdvSIMD", &val, &len, nullptr, 0) == 0 && val != 0;
+# else
     auto hwcaps = getauxval(AT_HWCAP);
     return (hwcaps & (1 << 12)) != 0;
-    #else
-      return false;
-    #endif
+# endif
+#else
+    return false;
+# endif
   }
 
   inline bool is_supported(asimd_ const &) noexcept
   {
-    #if defined( SPY_SIMD_IS_ARM_ASIMD )
+#if defined( SPY_SIMD_IS_ARM_ASIMD )
+# if defined(SPY_OS_IS_MACOS)
+    int         val = 0;
+    std::size_t len = sizeof(val);
+    return sysctlbyname("hw.optional.AdvSIMD", &val, &len, nullptr, 0) == 0 && val != 0;
+# else
     auto hwcaps = getauxval(AT_HWCAP);
     return (hwcaps & HWCAP_ASIMD) != 0;
-    #else
-      return false;
-    #endif
+# endif
+#else
+    return false;
+ #endif
   }
 
 }
