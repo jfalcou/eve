d24e6cc3905ad00ffe7b5f7c7a8a1fb08bca04df
Joel Falcou <joel.falcou@lri.fr>
2023-04-11 12:23:21 +0200

    Update CI configuration to latest MAC OS X image changes on Github Actions

--- cmake/toolchain/clang.x86_osx.cmake
+++ cmake/toolchain/clang.x86_osx.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
-set(CMAKE_C_COMPILER    /usr/local/opt/llvm/bin/clang   )
-set(CMAKE_CXX_COMPILER  /usr/local/opt/llvm/bin/clang++ )
+set(CMAKE_C_COMPILER    /usr/local/opt/llvm@15/bin/clang   )
+set(CMAKE_CXX_COMPILER  /usr/local/opt/llvm@15/bin/clang++ )
 
 set(CMAKE_CXX_FLAGS     "-DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
