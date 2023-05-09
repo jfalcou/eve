414207f286bc3b69e339185fa0c26efbeec9fcca
jtlap <jtlapreste@gmail.com>
2023-05-09 16:11:43 +0200

    dt_fht

--- include/eve/module/transforms/fht/detail/fht_dt_kernel.hpp
+++ include/eve/module/transforms/fht/detail/fht_dt_kernel.hpp
@@ -14,6 +14,7 @@
 
 namespace eve::detail
 {
+
   template < bool initial_radix_16 = true >
   EVE_FORCEINLINE constexpr void
   fht_dt_kernel(auto f, auto log2_n, const bool simd = true) noexcept
@@ -50,67 +51,67 @@ namespace eve::detail
     {
       if constexpr(initial_radix_16)
       {
-        for (e_t *fi=f; fi<fn; fi+=16)  // radix-16 step
+        std::cout << "icitte" << std::endl;
+        for (auto fi=f; fi<fn; fi+=16)  // radix-16 step
         {
-            e_t f0, f1, f2, f3;
-            sd(fi[0], fi[1], f0, f1);
-            sd(fi[2], fi[3], f2, f3);
-            sd(f0, f2, fi[0], fi[2]);
-            sd(f1, f3, fi[1], fi[3]);
+          e_t f0, f1, f2, f3;
+          sd(fi[0], fi[1], f0, f1);
+          sd(fi[2], fi[3], f2, f3);
+          sd(f0, f2, fi[0], fi[2]);
+          sd(f1, f3, fi[1], fi[3]);
 
-            sd(fi[4], fi[5], f0, f1);
-            sd(fi[6], fi[7], f2, f3);
-            sd(f0, f2, fi[4], fi[6]);
-            sd(f1, f3, fi[5], fi[7]);
+          sd(fi[4], fi[5], f0, f1);
+          sd(fi[6], fi[7], f2, f3);
+          sd(f0, f2, fi[4], fi[6]);
+          sd(f1, f3, fi[5], fi[7]);
 
-            sd(fi[8], fi[9], f0, f1);
-            sd(fi[10], fi[11], f2, f3);
-            sd(f0, f2, fi[8], fi[10]);
-            sd(f1, f3, fi[9], fi[11]);
+          sd(fi[8], fi[9], f0, f1);
+          sd(fi[10], fi[11], f2, f3);
+          sd(f0, f2, fi[8], fi[10]);
+          sd(f1, f3, fi[9], fi[11]);
 
-            sd(fi[12], fi[13], f0, f1);
-            sd(fi[14], fi[15], f2, f3);
-            sd(f0, f2, fi[12], fi[14]);
-            sd(f1, f3, fi[13], fi[15]);
+          sd(fi[12], fi[13], f0, f1);
+          sd(fi[14], fi[15], f2, f3);
+          sd(f0, f2, fi[12], fi[14]);
+          sd(f1, f3, fi[13], fi[15]);
 
-            sd(fi[0], fi[4], f0, f1);
-            sd(fi[8], fi[12], f2, f3);
-            sd(f0, f2, fi[0], fi[8]);
-            sd(f1, f3, fi[4], fi[12]);
+          sd(fi[0], fi[4], f0, f1);
+          sd(fi[8], fi[12], f2, f3);
+          sd(f0, f2, fi[0], fi[8]);
+          sd(f1, f3, fi[4], fi[12]);
 
-            sd(fi[2], fi[6], f0, f1);
-            f3 = sqrt2 * fi[14];
-            f2 = sqrt2 * fi[10];
-            sd(f0, f2, fi[2], fi[10]);
-            sd(f1, f3, fi[6], fi[14]);
+          sd(fi[2], fi[6], f0, f1);
+          f3 = sqrt2 * fi[14];
+          f2 = sqrt2 * fi[10];
+          sd(f0, f2, fi[2], fi[10]);
+          sd(f1, f3, fi[6], fi[14]);
 
-            e_t a, b, g0, g1, g2, g3;
-            sd(fi[5], fi[7], a, b);
-            a *= invsqrt2;
-            b *= invsqrt2;
-            sd(fi[1], a, f0, f1);
-            sd(fi[3], b, g0, g1);
-            sd(fi[13], fi[15], a, b);
-            a *= invsqrt2;
-            b *= invsqrt2;
-            sd(fi[9], a, f2, f3);
-            sd(fi[11], b, g2, g3);
-            e_t c1 = e_t( 0.923879532511286756128183189397); //COS_1_PI_8;  // jjkeep
-            e_t s1 =  e_t( 0.382683432365089771728459984029); //SIN_1_PI_8;  // jjkeep
-            kumi::tie(b, a) = c_t(s1, c1)*c_t(f2, g3);
-             //  cmult(s1, c1, f2, g3, b, a);
-            sd(f0, a, fi[1], fi[9]);
-            sd(g1, b, fi[7], fi[15]);
-            kumi::tie(b, a) = c_t(s1, c1)*c_t(f2, g3);
-//            cmult(c1, s1, g2, f3, b, a);
-            sd(g0, a, fi[3], fi[11]);
-            sd(f1, b, fi[5], fi[13]);
+          e_t a, b, g0, g1, g2, g3;
+          sd(fi[5], fi[7], a, b);
+          a *= invsqrt2;
+          b *= invsqrt2;
+          sd(fi[1], a, f0, f1);
+          sd(fi[3], b, g0, g1);
+          sd(fi[13], fi[15], a, b);
+          a *= invsqrt2;
+          b *= invsqrt2;
+          sd(fi[9], a, f2, f3);
+          sd(fi[11], b, g2, g3);
+          e_t c1 = e_t( 0.923879532511286756128183189397);
+          e_t s1 =  e_t( 0.382683432365089771728459984029);
+          kumi::tie(b, a) = c_t(s1, c1)*c_t(f2, g3);
+          sd(f0, a, fi[1], fi[9]);
+          sd(g1, b, fi[7], fi[15]);
+          kumi::tie(b, a) = c_t(c1, s1)*c_t(g2, f3);
+          sd(g0, a, fi[3], fi[11]);
+          sd(f1, b, fi[5], fi[13]);
         }
         ldk = 4;
       }
       else
       {
-        for (e_t *fi=f; fi<fn; fi+=4)  // radix-4 step
+        std::cout << "icitte 2 " << simd << std::endl;
+        for (auto fi=f; fi<fn; fi+=4)  // radix-4 step
         {
           e_t f0, f1, f2, f3;
           sd(fi[0], fi[1], f0, f1);
@@ -119,12 +120,13 @@ namespace eve::detail
           sd(f1, f3, fi[1], fi[3]);
         }
         ldk = 2;
-      }  // INITIAL_RADIX_16
+      }
     }
     else          // ldk==1,  n is no power of 4
     {
-      for (double *fi=f; fi<fn; fi+=8)  // radix-8 step
+      for (auto fi=f; fi<fn; fi+=8)  // radix-8 step
       {
+        std::cout << "icitte 3 " << simd << std::endl;
         e_t g0, f0, f1, g1;
         sd(fi[0], fi[1], f0, g0);
         sd(fi[2], fi[3], f1, g1);
@@ -151,75 +153,109 @@ namespace eve::detail
     }
     for (  ; ldk<log2_n;  ldk+=2)
     {
-        i_t k   = 1UL<<ldk;
-        i_t kh  = k >> 1;
-        i_t k2  = k << 1;
-        i_t k3  = k2 + k;
-        i_t k4  = k2 << 1;
+      i_t k   = 1 <<ldk;
+      i_t kh  = k >> 1;
+      i_t k2  = k << 1;
+      i_t k3  = k2 + k;
+      i_t k4  = k2 << 1;
 
-        for (e_t *fi=f, *gi=f+kh;  fi<fn;  fi+=k4, gi+=k4)
+      for (auto fi=f, gi=f+kh;  fi<fn;  fi+=k4, gi+=k4)
+      {
+        e_t f0, f1, f2, f3;
+        sd(fi[0], fi[k], f0, f1);
+        sd(fi[k2], fi[k3], f2, f3);
+        sd(f0, f2, fi[0], fi[k2]);
+        sd(f1, f3, fi[k], fi[k3]);
+
+        sd(gi[0], gi[k], f0, f1);
+        f3 = sqrt2 * gi[k3];
+        f2 = sqrt2 * gi[k2];
+        sd(f0, f2, gi[0], gi[k2]);
+        sd(f1, f3, gi[k], gi[k3]);
+      }
+
+      e_t tt = rec(e_t(kh*4));
+      auto scalar = [&](auto i)
+      {
+        auto [c1, s1] = exp_ipi(tt*i);
+        auto [c2, s2] = sqr(c_t(c1, s1));
+        for (e_t *fi=f+i, *gi=f+k-i;  fi<fn;  fi+=k4, gi+=k4)
         {
-            e_t f0, f1, f2, f3;
-            sd(fi[0], fi[k], f0, f1);
-            sd(fi[k2], fi[k3], f2, f3);
-            sd(f0, f2, fi[0], fi[k2]);
-            sd(f1, f3, fi[k], fi[k3]);
+          e_t a, b, g0, f0, f1, g1, f2, g2, f3, g3;
+          kumi::tie(b, a) = c_t(s2, c2)*c_t(fi[k], gi[k]);
+          sd(fi[0], a, f0, f1);
+          sd(gi[0], b, g0, g1);
 
-            sd(gi[0], gi[k], f0, f1);
-            f3 = sqrt2 * gi[k3];
-            f2 = sqrt2 * gi[k2];
-            sd(f0, f2, gi[0], gi[k2]);
-            sd(f1, f3, gi[k], gi[k3]);
+          kumi::tie(b, a) = c_t(s2, c2)*c_t(fi[k3], gi[k3]);
+          sd(fi[k2], a, f2, f3);
+          sd(gi[k2], b, g2, g3);
+
+          kumi::tie(b, a) = c_t(s1, c1)*c_t(f2, g3);
+          sd(f0, a, fi[0], fi[k2]);
+          sd(g1, b, gi[k], gi[k3]);
+
+          kumi::tie(b, a) = c_t(c1, s1)*c_t(g2, f3);
+          sd(g0, a, gi[0], gi[k2]);
+          sd(f1, b, fi[k], fi[k3]);
         }
+      };
+      i_t cardinal = eve::expected_cardinal_v<e_t>;
+      auto sm = min(cardinal, kh);
+      for (i_t i=1; i < sm; i++) scalar(i);
+      if (simd && (kh > cardinal))
+      {
+        auto phi0 = tt*cardinal;
+        auto is = eve::views::iota(e_t(cardinal), kh-cardinal);
+        auto phs= eve::views::iota_with_step(e_t{phi0}, tt, kh-cardinal);
+        auto view = eve::views::zip(is, phs);
+        auto doit = [k, k2, k3, k4, tt, f, cardinal, fn](auto zz, auto ignore){
+          auto [i, ph] = load[ignore](zz);
+          auto cs1 = exp_ipi(tt*i);
+          auto [c1, s1] = cs1;
+          using c_t =  decltype(cs1);
+          auto [c2, s2] =  sqr(cs1);
 
-        e_t tt = rec(e_t(kh*4));
-#if defined USE_TRIG_REC
-        e_t s1 = 0.0,  c1 = 1.0;  // jjkeep
-        e_t al = sinpi(0.5*tt);  // jjkeep
-        al *= (2.0*al);
-        e_t be = sinpi(tt);  // jjkeep
-#endif  // USE_TRIG_REC
-
-        for (i_t i=1; i<kh; i++)
-        {
-#if defined USE_TRIG_REC
-            {
-                e_t t1 = c1;  // jjkeep
-                c1 -= (al*t1+be*s1);
-                s1 -= (al*s1-be*t1);
-            }
-#else
-            // e_t s1, c1;  // jjkeep
-            //SinCos(tt*(double)i, &s1, &c1);  // jjkeep
-            auto [s1, c1] = exp_ipi(tt*i);
-#endif  // USE_TRIG_REC
-
-            //e_t c2, s2;  // jjkeep
-            auto [c2, s2] = sqr(c_t(c1, s1));
-            for (e_t *fi=f+i, *gi=f+k-i;  fi<fn;  fi+=k4, gi+=k4)
-            {
-                e_t a, b, g0, f0, f1, g1, f2, g2, f3, g3;
-                //               cmult(s2, c2, fi[k], gi[k], b, a);
-                kumi::tie(b, a) = c_t(s2, c2)*c_t(fi[k], gi[k]);
-                sd(fi[0], a, f0, f1);
-                sd(gi[0], b, g0, g1);
-
-                kumi::tie(b, a) = c_t(s2, c2)*c_t(fi[k3], gi[k3]);
-//                cmult(s2, c2, fi[k3], gi[k3], b, a);
-                sd(fi[k2], a, f2, f3);
-                sd(gi[k2], b, g2, g3);
-
-                kumi::tie(b, a) = c_t(s1, c1)*c_t(f2, g3);
-//                cmult(s1, c1, f2, g3, b, a);
-                sd(f0, a, fi[0], fi[k2]);
-                sd(g1, b, gi[k], gi[k3]);
-
-//                cmult(c1, s1, g2, f3, b, a);
-                kumi::tie(b, a) = c_t(c1, s1)*c_t(g2, f3);
-                sd(g0, a, gi[0], gi[k2]);
-                sd(f1, b, fi[k], fi[k3]);
-            }
-        }
+          auto i0 = size_t(i.get(0));
+          for (auto fi=f+i0, gi=f+k-i0-cardinal+1;  fi<fn;  fi+=k4, gi+=k4)
+          {
+            auto dgi0  =reverse(load(gi+0 ));
+            auto dgik  =reverse(load(gi+k ));
+            auto dgik2 =reverse(load(gi+k2));
+            auto dgik3 =reverse(load(gi+k3));
+            auto dfi0  =load(fi+0 );
+            auto dfik  =load(fi+k );
+            auto dfik2 =load(fi+k2);
+            auto dfik3 =load(fi+k3);
+            using we_t = wide<e_t>;
+            we_t a, b, g0, f0, f1, g1, f2, g2, f3, g3;
+            kumi::tie(b, a) = kumi::to_tuple(c_t(s2, c2)*c_t(dfik, dgik));
+            sd(dfi0, a, f0, f1);
+            sd(dgi0, b, g0, g1);
+            kumi::tie(b, a) = kumi::to_tuple(c_t(s2, c2)*c_t(dfik3, dgik3));
+            sd(dfik2, a, f2, f3);
+            sd(dgik2, b, g2, g3);
+            kumi::tie(b, a) = kumi::to_tuple(c_t(s1, c1)*c_t(f2, g3));
+            sd(f0, a, dfi0, dfik2);
+            sd(g1, b, dgik, dgik3);
+            kumi::tie(b, a) = kumi::to_tuple(c_t(c1, s1)*c_t(g2, f3));
+            sd(g0, a, dgi0, dgik2);
+            sd(f1, b, dfik, dfik3);
+            store(reverse(dgi0) , gi+0 );
+            store(reverse(dgik) , gi+k );
+            store(reverse(dgik2), gi+k2);
+            store(reverse(dgik3), gi+k3);
+            store(dfi0 , fi+0 );
+            store(dfik , fi+k );
+            store(dfik2, fi+k2);
+            store(dfik3, fi+k3);
+          }
+        };
+        eve::algo::for_each[eve::algo::allow_frequency_scaling](view, doit);
+      }
+      else
+      {
+        for (i_t i=sm; i<kh; i++) scalar(i);
+      }
     }
   }
 }
--- test/unit/module/transforms/fht/small_dt_fht.cpp
+++ test/unit/module/transforms/fht/small_dt_fht.cpp
@@ -14,12 +14,292 @@
 #include <eve/module/transforms.hpp>
 #include <vector>
 
+
+#include <cmath>  // M_PI, M_SQRT1_2
+#define COS_1_PI_8  0.923879532511286756128183189397
+#define SIN_1_PI_8  0.382683432365089771728459984029
+
+// tuning parameter:
+// define to use trig recurrence:
+// (and possibly lose some precision, see below)
+//#define USE_TRIG_REC
+// with type 'long double' slight speed loss on my machine,
+// with type 'double' little speed gain.
+
+#if defined USE_TRIG_REC
+//#warning 'FYI: fht(double *, ulong) uses trig recursion'
+#endif
+
+// // tuning parameter:
+// #define  INITIAL_RADIX_16  1  // 0 or 1 (default)
+// //
+// #if  ( INITIAL_RADIX_16==1 )
+// //#warning 'FYI: INITIAL_RADIX_16 set in fht_dit(double *, ulong)'
+// #else
+// #warning 'INITIAL_RADIX_16 is NOT SET in fht_dit(double *, ulong)'
+// #endif
+
+// namespace pipo
+// {
+//   template <typename Type>
+//   static inline void sumdiff(Type a, Type b, Type &s, Type &d)
+//   // {s, d}  <--| {a+b, a-b}
+//   { s=a+b; d=a-b; }
+
+//   static inline void csqr(double a, double b,
+//                           double &u, double &v)
+//   // {u,v} <--| {a*a-b*b, 2*a*b}
+//   {
+//     u = a * a - b * b;
+//     v = a * b;
+//     v += v;
+//   }
+
+//   static inline void cmult(double c, double s,
+//                            double &u, double &v)
+//   // {u,v} <--| u = u*c-v*s;  v = u*s+v*c
+//   {
+//     double t = u * s + v * c;
+//     u *= c;
+//     u -= v * s;
+//     v = t;
+//   }
+
+//   void
+//   fht_dit_core(double *f, ulong ldn)
+//   // Decimation in time (DIT) FHT.
+//   // Input data must be in revbin_permuted order.
+//   // ldn := base-2 logarithm of the array length.
+//   {
+//     if ( ldn<=2 )
+//     {
+//       if ( ldn==1 )  // two point fht
+//       {
+//         sd(f[0], f[1]);
+//       }
+//       else if ( ldn==2 )  // four point fht
+//       {
+//         double f0, f1, f2, f3;
+//         sumdiff(f[0], f[1], f0, f1);
+//         sumdiff(f[2], f[3], f2, f3);
+//         sumdiff(f0, f2, f[0], f[2]);
+//         sumdiff(f1, f3, f[1], f[3]);
+//       }
+//       return;
+//     }
+
+//     const ulong n = (1UL<<ldn);
+//     const double *fn = f + n;
+//     ulong ldk = ldn & 1;
+
+//     if ( ldk==0 )    // ldn is multiple of 2  => n is a power of 4
+//     {
+// #if  ( INITIAL_RADIX_16==1 )
+//       for (double *fi=f; fi<fn; fi+=16)  // radix-16 step
+//       {
+//         double f0, f1, f2, f3;
+//         sumdiff(fi[0], fi[1], f0, f1);
+//         sumdiff(fi[2], fi[3], f2, f3);
+//         sumdiff(f0, f2, fi[0], fi[2]);
+//         sumdiff(f1, f3, fi[1], fi[3]);
+
+//         sumdiff(fi[4], fi[5], f0, f1);
+//         sumdiff(fi[6], fi[7], f2, f3);
+//         sumdiff(f0, f2, fi[4], fi[6]);
+//         sumdiff(f1, f3, fi[5], fi[7]);
+
+//         sumdiff(fi[8], fi[9], f0, f1);
+//         sumdiff(fi[10], fi[11], f2, f3);
+//         sumdiff(f0, f2, fi[8], fi[10]);
+//         sumdiff(f1, f3, fi[9], fi[11]);
+
+//         sumdiff(fi[12], fi[13], f0, f1);
+//         sumdiff(fi[14], fi[15], f2, f3);
+//         sumdiff(f0, f2, fi[12], fi[14]);
+//         sumdiff(f1, f3, fi[13], fi[15]);
+
+//         sumdiff(fi[0], fi[4], f0, f1);
+//         sumdiff(fi[8], fi[12], f2, f3);
+//         sumdiff(f0, f2, fi[0], fi[8]);
+//         sumdiff(f1, f3, fi[4], fi[12]);
+
+//         sumdiff(fi[2], fi[6], f0, f1);
+//         f3 = M_SQRT2 * fi[14];
+//         f2 = M_SQRT2 * fi[10];
+//         sumdiff(f0, f2, fi[2], fi[10]);
+//         sumdiff(f1, f3, fi[6], fi[14]);
+
+//         double a, b, g0, g1, g2, g3;
+//         sumdiff(fi[5], fi[7], a, b);
+//         a *= M_SQRT1_2;
+//         b *= M_SQRT1_2;
+//         sumdiff(fi[1], a, f0, f1);
+//         sumdiff(fi[3], b, g0, g1);
+//         sumdiff(fi[13], fi[15], a, b);
+//         a *= M_SQRT1_2;
+//         b *= M_SQRT1_2;
+//         sumdiff(fi[9], a, f2, f3);
+//         sumdiff(fi[11], b, g2, g3);
+//         double c1 = COS_1_PI_8;  // jjkeep
+//         double s1 = SIN_1_PI_8;  // jjkeep
+//         cmult(s1, c1, f2, g3, b, a);
+//         sumdiff(f0, a, fi[1], fi[9]);
+//         sumdiff(g1, b, fi[7], fi[15]);
+//         cmult(c1, s1, g2, f3, b, a);
+//         sumdiff(g0, a, fi[3], fi[11]);
+//         sumdiff(f1, b, fi[5], fi[13]);
+//       }
+//       ldk = 4;
+// #else  // INITIAL_RADIX_16
+//       for (double *fi=f; fi<fn; fi+=4)  // radix-4 step
+//       {
+//         double f0, f1, f2, f3;
+//         sumdiff(fi[0], fi[1], f0, f1);
+//         sumdiff(fi[2], fi[3], f2, f3);
+//         sumdiff(f0, f2, fi[0], fi[2]);
+//         sumdiff(f1, f3, fi[1], fi[3]);
+//       }
+//       ldk = 2;
+// #endif  // INITIAL_RADIX_16
+//     }
+//     else          // ldk==1,  n is no power of 4
+//     {
+//       for (double *fi=f; fi<fn; fi+=8)  // radix-8 step
+//       {
+//         double g0, f0, f1, g1;
+//         sumdiff(fi[0], fi[1], f0, g0);
+//         sumdiff(fi[2], fi[3], f1, g1);
+
+//         sumdiff(f0, f1);
+//         sumdiff(g0, g1);
+
+//         double s1, c1, s2, c2;
+//         sumdiff(fi[4], fi[5], s1, c1);
+//         sumdiff(fi[6], fi[7], s2, c2);
+
+//         sumdiff(s1, s2);
+
+//         sumdiff(f0, s1, fi[0], fi[4]);
+//         sumdiff(f1, s2, fi[2], fi[6]);
+
+//         c1 *= M_SQRT2;
+//         c2 *= M_SQRT2;
+
+//         sumdiff(g0, c1, fi[1], fi[5]);
+//         sumdiff(g1, c2, fi[3], fi[7]);
+//       }
+//       ldk = 3;
+//     }
+
+
+//     for (  ; ldk<ldn;  ldk+=2)
+//     {
+//       ulong k   = 1UL<<ldk;
+//       ulong kh  = k >> 1;
+//       ulong k2  = k << 1;
+//       ulong k3  = k2 + k;
+//       ulong k4  = k2 << 1;
+
+//       for (double *fi=f, *gi=f+kh;  fi<fn;  fi+=k4, gi+=k4)
+//       {
+//         double f0, f1, f2, f3;
+//         sumdiff(fi[0], fi[k], f0, f1);
+//         sumdiff(fi[k2], fi[k3], f2, f3);
+//         sumdiff(f0, f2, fi[0], fi[k2]);
+//         sumdiff(f1, f3, fi[k], fi[k3]);
+
+//         sumdiff(gi[0], gi[k], f0, f1);
+//         f3 = M_SQRT2 * gi[k3];
+//         f2 = M_SQRT2 * gi[k2];
+//         sumdiff(f0, f2, gi[0], gi[k2]);
+//         sumdiff(f1, f3, gi[k], gi[k3]);
+//       }
+
+//       double tt = M_PI_4/(double)kh;  // jjkeep
+// #if defined USE_TRIG_REC
+//       double s1 = 0.0,  c1 = 1.0;  // jjkeep
+//       double al = sin(0.5*tt);  // jjkeep
+//       al *= (2.0*al);
+//       double be = sin(tt);  // jjkeep
+// #endif  // USE_TRIG_REC
+
+//       for (ulong i=1; i<kh; i++)
+//       {
+// #if defined USE_TRIG_REC
+//         {
+//           double t1 = c1;  // jjkeep
+//           c1 -= (al*t1+be*s1);
+//           s1 -= (al*s1-be*t1);
+//         }
+// #else
+//         double s1, c1;  // jjkeep
+//         eve::sincos(tt*(double)i, &s1, &c1);  // jjkeep
+// #endif  // USE_TRIG_REC
+
+//         double c2, s2;  // jjkeep
+//         csqr(c1, s1, c2, s2);
+
+//         for (double *fi=f+i, *gi=f+k-i;  fi<fn;  fi+=k4, gi+=k4)
+//         {
+//           double a, b, g0, f0, f1, g1, f2, g2, f3, g3;
+//           cmult(s2, c2, fi[k], gi[k], b, a);
+//           sumdiff(fi[0], a, f0, f1);
+//           sumdiff(gi[0], b, g0, g1);
+
+//           cmult(s2, c2, fi[k3], gi[k3], b, a);
+//           sumdiff(fi[k2], a, f2, f3);
+//           sumdiff(gi[k2], b, g2, g3);
+
+//           cmult(s1, c1, f2, g3, b, a);
+//           sumdiff(f0, a, fi[0], fi[k2]);
+//           sumdiff(g1, b, gi[k], gi[k3]);
+
+//           cmult(c1, s1, g2, f3, b, a);
+//           sumdiff(g0, a, gi[0], gi[k2]);
+//           sumdiff(f1, b, fi[k], fi[k3]);
+//         }
+//       }
+//     }
+//   }
+//   // -------------------------
+
+//   void
+//   fht_dit(double *f, ulong ldn)
+//   // Fast Hartley Transform.
+//   // Split-radix decimation in time (DIT) algorithm.
+//   // ldn := base-2 logarithm of the array length.
+//   {
+//     if ( ldn<=2 )
+//     {
+//       if ( ldn==1 )  // two point fht
+//       {
+//         sumdiff(f[0], f[1]);
+//       }
+//       else if ( ldn==2 )  // four point fht
+//       {
+//         double f0, f1, f2, f3;
+//         sumdiff(f[0], f[2], f0, f1);
+//         sumdiff(f[1], f[3], f2, f3);
+//         sumdiff(f0, f2, f[0], f[2]);
+//         sumdiff(f1, f3, f[1], f[3]);
+//       }
+
+//       return;
+//     }
+
+//     revbin_permute(f, 1UL<<ldn);
+//     fht_dit_core(f, ldn);
+//   }
+//   // -------------------------
+// }
+
+
 TTS_CASE_TPL("Check naive_ht on aos", eve::test::simd::ieee_reals)
 <typename T>(tts::type<T>)
 {
   if constexpr(std::same_as<eve::element_type_t<T>, double> && eve::cardinal_v<T> == 1)
   {
-    size_t N = 32;
+    size_t N = 64;
     std::cout << tts::typename_<T> << std::endl; ;
     using e_t = typename T::value_type;
     std::vector<e_t> a(N), orig(N), naiv(N);
@@ -29,9 +309,9 @@ TTS_CASE_TPL("Check naive_ht on aos", eve::test::simd::ieee_reals)
     for(size_t i=0; i <N ; ++i){
       TTS_ABSOLUTE_EQUAL(a[i],naiv[i], 100*eve::eps(eve::as<e_t>()));
     }
-    eve::aos(eve::naive_ht)(a, e_t(2)/(N)); //inverse ht
-    for(size_t i=0; i <N ; ++i){
-      TTS_ABSOLUTE_EQUAL(a[i],orig[i], 100*eve::eps(eve::as<e_t>()));
-    }
+//     eve::aos(eve::naive_ht)(a, e_t(2)/(N)); //inverse ht
+//     for(size_t i=0; i <N ; ++i){
+//       TTS_ABSOLUTE_EQUAL(a[i],orig[i], 100*eve::eps(eve::as<e_t>()));
+//     }
   }
 };
