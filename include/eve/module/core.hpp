e7bc910c29bd999e4faf6f613d4119ff6e5391b5
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2023-08-21 17:26:13 +0100

    Fix the doxygen typo (#1648)
    This will address the doxygen issue

--- include/eve/module/core.hpp
+++ include/eve/module/core.hpp
@@ -93,7 +93,7 @@
 //! @ingroup core
 //! Functions that are just shuffles with a different api.
 //!
-//! @defgroup core_comporess Compress functions
+//! @defgroup core_compress Compress functions
 //! @ingroup core
 //! Functions that in different way expose `compressing` selected elements
 //! together to beginning. This is at the core of `remove_if`, `copy_if` etc.
