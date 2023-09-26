432a2b0b137e9669aaeea1bdeb9eb1da4f590e67
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2023-09-26 09:41:52 -0400

    another source (#1682)

--- include/eve/module/core/regular/sort.hpp
+++ include/eve/module/core/regular/sort.hpp
@@ -42,6 +42,8 @@ namespace eve
 //!    * Google's: Vectorized and performance-portable Quicksort
 //!      Mark Blacher, Joachim Giesen, Peter Sanders, Jan Wassenberg
 //!      https://opensource.googleblog.com/2022/06/Vectorized%20and%?20performance%20portable%20Quicksort.html
+//!    * A Question of Sorts
+//!      http://seven-degrees-of-freedom.blogspot.com/2010/07/question-of-sorts.html
 //!    * Kyle Hegeman (@khegeman)
 //!      https://github.com/khegeman/floki
 //!
