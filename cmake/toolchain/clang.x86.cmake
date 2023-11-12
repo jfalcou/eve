14a7d0a67105d77587b42ac4bce342d46415909d
Joel Falcou <joel.falcou@lri.fr>
2023-11-12 15:05:08 +0100

    Use proper clang++ path in CI

--- cmake/toolchain/clang.x86.cmake
+++ cmake/toolchain/clang.x86.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
-set(CMAKE_C_COMPILER    clang-15   )
-set(CMAKE_CXX_COMPILER  clang++-15 )
+set(CMAKE_C_COMPILER    clang   )
+set(CMAKE_CXX_COMPILER  clang++ )
 
 set(CMAKE_CXX_FLAGS     "-DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
