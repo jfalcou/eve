69a2f6cd36460973076cbdefe9a19c59c29473dd
jtlap <jtlapreste@gmail.com>
2023-11-19 21:53:53 +0100

    fibonacci

--- include/eve/module/combinatorial/regular/impl/fibonacci.hpp
+++ include/eve/module/combinatorial/regular/impl/fibonacci.hpp
@@ -26,15 +26,16 @@ fibonacci_(EVE_SUPPORTS(cpu_), N n, T a, T b) noexcept
 {
   if constexpr( has_native_abi_v<T> &&  has_native_abi_v<N>)
   {
+    using w_t                 = as_wide_as_t<T, N>;
     using elt_t               = element_type_t<T>;
     using eli_t               = as_integer_t<elt_t, unsigned>;
-    constexpr elt_t gold      = 1.61803398874989484820458683436563811772030917980575;
-    constexpr elt_t goldbar   = -0.61803398874989484820458683436563811772030917980575;
+    auto gold                 = w_t(1.61803398874989484820458683436563811772030917980575);
+    auto goldbar              = w_t(-0.61803398874989484820458683436563811772030917980575);
     constexpr elt_t oneosqrt5 = 0.4472135954999579392818347337462552470881236719223;
     auto            nm1       = to_<eli_t>((n));
-    auto            c2        = fms(T(gold), a, b) * oneosqrt5;
+    auto            c2        = fms(gold, a, b) * oneosqrt5;
     auto            c1        = a - c2;
-    auto            f         = fma(c1, pow(T(gold), nm1), c2 * pow(T(goldbar), nm1));
+    auto            f         = fma(c1, pow(gold, nm1), c2 * pow(goldbar, nm1));
     return if_else(is_flint(a) && is_flint(b), nearest(f), f);
   }
   else return apply_over(fibonacci, n, a, b);
