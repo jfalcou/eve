ae5e71c26acb93a57dbf3eee850e13d88ef07d8a
jtlap <jtlapreste@gmail.com>
2023-12-17 18:06:46 +0100

    stupid blind modif

--- test/doc/core/masked/bit_reverse.cpp
+++ test/doc/core/masked/bit_reverse.cpp
@@ -10,11 +10,11 @@ int main()
   wide_it pi = {-14, 1, 3, 0};
 
   std::cout << "---- simd" << '\n'
-            << "<- pi                         = " << pi << '\n'
+            << "<- pi                               = " << pi << '\n'
             << "-> bit_reverse[pi>0](pi)            = " << eve::bit_reverse[pi>0](pi) << '\n'
             << "-> bit_reverse[pi>0](pi, 4)         = " << eve::bit_reverse[pi>0](pi, 4)<< '\n'
-            << "-> bit_reverse[pi>0][pi>2](pi)      = " << eve::bit_reverse[pi>0][pi > 2](pi)<< '\n'
-            << "-> bit_reverse[pi>0][pi > 2](pi, 4) = " << eve::bit_reverse[pi>0][pi > 2](pi, 4)<< '\n'
+            << "-> bit_reverse[pi>0](pi)            = " << eve::bit_reverse[pi>0](pi)<< '\n'
+            << "-> bit_reverse[pi>0][pi > 2](pi, 4) = " << eve::bit_reverse[pi>0](pi, 4)<< '\n'
     ;
 
   return 0;
