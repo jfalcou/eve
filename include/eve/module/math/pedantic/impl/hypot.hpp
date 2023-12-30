0faf9e12ff01917ea0479bf0382da9514ed838a7
jtlap <jtlapreste@gmail.com>
2023-12-30 10:44:53 +0100

    pedantic hypot correction

--- include/eve/module/math/pedantic/impl/hypot.hpp
+++ include/eve/module/math/pedantic/impl/hypot.hpp
@@ -11,22 +11,46 @@
 
 namespace eve::detail
 {
-template<value T0, value... Ts>
-auto
-hypot_(EVE_SUPPORTS(cpu_), pedantic_type const&, T0 a0, Ts... args)
-  -> decltype(hypot(a0, args...))
-{
-  using r_t = decltype(hypot(a0, args...));
-  if constexpr( has_native_abi_v<r_t> )
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  //  This implementation is inspired by
+  //  AN IMPROVED ALGORITHM FOR HYPOT(A,B) arXiv:1904.09481v6 [math.NA] 14 Jun 2019, CARLOS F. BORGES
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  template<value T0, value T1>
+  auto  hypot_(EVE_SUPPORTS(cpu_), pedantic_type const&, T0 a, T1 b)
+    -> decltype(hypot(a, b))
   {
-    auto e = -maxmag(exponent(r_t(a0)), exponent(r_t(args))...);
-    auto f = [&](auto a){return sqr_abs(pedantic(ldexp)(r_t(a), e)); };
-    r_t that = add(f(a0), f(args)...);
-    auto r = pedantic(ldexp)(sqrt(that), -e);
-    if(none(is_nan(r))) return r;
-    auto rinf = numeric(maxabs)(a0, args...);
-    return if_else(is_infinite(rinf), rinf, r);
+    using r_t = decltype(hypot(a, b));
+    if constexpr( has_native_abi_v<r_t> )
+    {
+      r_t aa(abs(a));
+      r_t ab(abs(b));
+      auto test = aa > ab;
+      eve::swap_if(test, aa, ab); // now aa < bb
+      auto doit =  [](auto b){    // compute sqrt(1+b*b)
+        auto h = sqrt(inc(sqr(b)));
+        auto h2 = sqr(h);
+        auto x = fma(-b,b,dec(h2)) + fms(h,h,h2);
+        return if_else(is_eqz(h), h, h-x/(2*h)); // Maclaurin correction
+      };
+      auto h = if_else(is_eqz(ab), zero, ab*doit(aa/ab));
+      return if_else(is_infinite(a) || is_infinite(b), inf(as<r_t>()), h);
+    }
+    else { return apply_over(pedantic(hypot), r_t(a), r_t(b)); }
   }
-  else { return apply_over(pedantic(hypot), r_t(a0), r_t(args)...); }
-}
+
+  template<value T0, value T1, value... Ts>
+  auto hypot_(EVE_SUPPORTS(cpu_), pedantic_type const&, T0 a0, T1 a1, Ts... args)
+    -> decltype(hypot(a0, a1, args...))
+  {
+    using r_t = decltype(hypot(a0, a1, args...));
+    if constexpr( has_native_abi_v<r_t> )
+    {
+      r_t that(pedantic(hypot)(r_t(a0), r_t(a1)));
+      ((that = pedantic(hypot)(that, r_t(args))), ...);
+      return that;
+    }
+    else { return apply_over(pedantic(hypot), r_t(a0), r_t(a1), r_t(args)...); }
+  }
+
 }
