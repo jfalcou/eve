4651e420ce7e1546efacf67f4a5d0a44238f657f
jtlap <jtlapreste@gmail.com>
2024-01-07 19:21:45 +0100

    exp and log family

--- include/eve/module/math/regular/impl/exp10.hpp
+++ include/eve/module/math/regular/impl/exp10.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <eve/module/core.hpp>
+#include <eve/module/math/regular/horner.hpp>
 #include <eve/module/core/detail/generic/horn.hpp>
 #include <eve/module/core/detail/generic/horn1.hpp>
 #include <eve/module/core/regular/sqr.hpp> // THIS IS A PROBLEM SUPPRESS
@@ -18,78 +19,81 @@
 
 namespace eve::detail
 {
-template<floating_ordered_value T, decorator D>
-EVE_FORCEINLINE constexpr T
-exp10_(EVE_SUPPORTS(cpu_), D const&, T x) noexcept
-    requires(is_one_of<D>(types<regular_type, pedantic_type> {}))
-{
-  if constexpr( has_native_abi_v<T> )
+  template<floating_ordered_value T, decorator D>
+  EVE_FORCEINLINE constexpr T
+  exp10_(EVE_SUPPORTS(cpu_), D const&, T x) noexcept
+  requires(is_one_of<D>(types<regular_type, pedantic_type> {}))
   {
-    using elt_t    = element_type_t<T>;
-    auto minlogval = []()
+    if constexpr( has_native_abi_v<T> )
     {
-      if constexpr( (!eve::platform::supports_denormals) || std::is_same_v<D, regular_type> )
+      using elt_t    = element_type_t<T>;
+      auto minlogval = []()
+        {
+          if constexpr( (!eve::platform::supports_denormals) || std::is_same_v<D, regular_type> )
+          {
+            return minlog10(eve::as<T>());
+          }
+          else { return minlog10denormal(eve::as<T>()); }
+        };
+      const T Log10_2hi   = Ieee_constant<T, 0x3e9a0000U, 0x3fd3440000000000ULL>();
+      const T Log10_2lo   = Ieee_constant<T, 0x39826a14U, 0x3ed3509f79fef312ULL>();
+      auto    xltminlog10 = x <= minlogval();
+      auto    xgemaxlog10 = x >= maxlog10(eve::as(x));
+      if constexpr( scalar_value<T> )
       {
-        return minlog10(eve::as<T>());
+        if( xgemaxlog10 ) return inf(eve::as(x));
+        if( xltminlog10 ) return zero(eve::as(x));
       }
-      else { return minlog10denormal(eve::as<T>()); }
-    };
-    const T Log10_2hi   = Ieee_constant<T, 0x3e9a0000U, 0x3fd3440000000000ULL>();
-    const T Log10_2lo   = Ieee_constant<T, 0x39826a14U, 0x3ed3509f79fef312ULL>();
-    auto    xltminlog10 = x <= minlogval();
-    auto    xgemaxlog10 = x >= maxlog10(eve::as(x));
-    if constexpr( scalar_value<T> )
-    {
-      if( xgemaxlog10 ) return inf(eve::as(x));
-      if( xltminlog10 ) return zero(eve::as(x));
-    }
-    auto c = nearest(invlog10_2(eve::as<T>()) * x);
-    auto k = c;
-    x      = fnma(c, Log10_2hi, x); // x-c*L
-    x      = fnma(c, Log10_2lo, x);
-    if constexpr( std::is_same_v<elt_t, float> )
-    {
-      c = inc(horn<T, 0x40135d8e, 0x4029a926, 0x400237da, 0x3f95eb4c, 0x3f0aacef, 0x3e54dff1>(x)
-              * x);
-    }
-    else if constexpr( std::is_same_v<elt_t, double> )
-    {
-      T xx = sqr(x);
-      T px = x
-             * horn<T,
-                    0x40a2b4798e134a01ull,
-                    0x40796b7a050349e4ull,
-                    0x40277d9474c55934ull,
-                    0x3fa4fd75f3062dd4ull>(xx);
-      T x2 = px
-             / (horn1<T, 0x40a03f37650df6e2ull, 0x4093e05eefd67782ull, 0x405545fdce51ca08ull>(xx)
-                - px);
-      c = inc(x2 + x2);
-    }
-    auto z = pedantic(ldexp)(c, k);
-    if constexpr( simd_value<T> )
-    {
-      z = if_else(xltminlog10, eve::zero, z);
-      z = if_else(xgemaxlog10, inf(eve::as(x)), z);
-    }
-    return z;
-  }
-  else return apply_over(D()(exp10), x);
-}
+      auto c = nearest(invlog10_2(eve::as<T>()) * x);
+      auto k = c;
+      x      = fnma(c, Log10_2hi, x); // x-c*L
+      x      = fnma(c, Log10_2lo, x);
+      if constexpr( std::is_same_v<elt_t, float> )
+      {
+        c = inc(
+          eve::reverse_horner(x, T(0x1.26bb1cp+1f), T(0x1.53524cp+1f), T(0x1.046fb4p+1f)
+                             , T(0x1.2bd698p+0f), T(0x1.1559dep-1f), T(0x1.a9bfe2p-3f)) * x);
+      }
+      else if constexpr( std::is_same_v<elt_t, double> )
+      {
+        T xx = sqr(x);
+        T px = x
+          *
+          eve::reverse_horner(xx, T(0x1.2b4798e134a01p+11), T(0x1.96b7a050349e4p+8)
+                             , T(0x1.77d9474c55934p+3), T(0x1.4fd75f3062dd4p-5))
+          ;
+        T x2 = px
+          / (
+             eve::reverse_horner(xx, T(0x1.03f37650df6e2p+11), T(0x1.3e05eefd67782p+10)
+                                , T(0x1.545fdce51ca08p+6), T(0x1.0p0))
 
-template<floating_ordered_value T>
-EVE_FORCEINLINE constexpr T
-exp10_(EVE_SUPPORTS(cpu_), T const& x) noexcept
-{
-  return exp10(regular_type(), x);
-}
+             - px);
+        c = inc(x2 + x2);
+      }
+      auto z = pedantic(ldexp)(c, k);
+      if constexpr( simd_value<T> )
+      {
+        z = if_else(xltminlog10, eve::zero, z);
+        z = if_else(xgemaxlog10, inf(eve::as(x)), z);
+      }
+      return z;
+    }
+    else return apply_over(D()(exp10), x);
+  }
+
+  template<floating_ordered_value T>
+  EVE_FORCEINLINE constexpr T
+  exp10_(EVE_SUPPORTS(cpu_), T const& x) noexcept
+  {
+    return exp10(regular_type(), x);
+  }
 
 // -----------------------------------------------------------------------------------------------
 // Masked case
-template<conditional_expr C, value U>
-EVE_FORCEINLINE auto
-exp10_(EVE_SUPPORTS(cpu_), C const& cond, U const& t) noexcept
-{
-  return mask_op(cond, eve::exp10, t);
-}
+  template<conditional_expr C, value U>
+  EVE_FORCEINLINE auto
+  exp10_(EVE_SUPPORTS(cpu_), C const& cond, U const& t) noexcept
+  {
+    return mask_op(cond, eve::exp10, t);
+  }
 }
--- include/eve/module/math/regular/impl/exp2.hpp
+++ include/eve/module/math/regular/impl/exp2.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <eve/module/core.hpp>
+#include <eve/module/math/regular/horner.hpp>
 #include <eve/module/core/detail/generic/horn.hpp>
 #include <eve/module/math/constant/log_2.hpp>
 #include <eve/module/math/constant/maxlog2.hpp>
@@ -16,93 +17,92 @@
 
 namespace eve::detail
 {
-template<floating_ordered_value T, decorator D>
-EVE_FORCEINLINE constexpr T
-exp2_(EVE_SUPPORTS(cpu_), D const&, T x) noexcept
-    requires(is_one_of<D>(types<regular_type, pedantic_type> {}))
-{
-  if constexpr( has_native_abi_v<T> )
+  template<floating_ordered_value T, decorator D>
+  EVE_FORCEINLINE constexpr T
+  exp2_(EVE_SUPPORTS(cpu_), D const&, T x) noexcept
+  requires(is_one_of<D>(types<regular_type, pedantic_type> {}))
   {
-    using elt_t    = element_type_t<T>;
-    auto minlogval = []()
+    if constexpr( has_native_abi_v<T> )
     {
-      if constexpr( !eve::platform::supports_denormals || std::is_same_v<D, regular_type> )
-        return minlog2(eve::as<T>());
-      else return minlog2denormal(eve::as<T>());
-    };
-    auto xltminlog2 = x <= minlogval();
-    auto xgemaxlog2 = x >= maxlog2(eve::as(x));
-    if constexpr( scalar_value<T> )
-    {
-      if( xgemaxlog2 ) return inf(eve::as(x));
-      if( xltminlog2 ) return zero(eve::as(x));
+      using elt_t    = element_type_t<T>;
+      auto minlogval = []()
+        {
+          if constexpr( !eve::platform::supports_denormals || std::is_same_v<D, regular_type> )
+          return minlog2(eve::as<T>());
+          else return minlog2denormal(eve::as<T>());
+        };
+      auto xltminlog2 = x <= minlogval();
+      auto xgemaxlog2 = x >= maxlog2(eve::as(x));
+      if constexpr( scalar_value<T> )
+      {
+        if( xgemaxlog2 ) return inf(eve::as(x));
+        if( xltminlog2 ) return zero(eve::as(x));
+      }
+      auto k = nearest(x);
+      x      = x - k;
+      if constexpr( std::is_same_v<elt_t, float> )
+      {
+        T y =
+          eve::reverse_horner(x, T(0x1.ebfbe2p-3f), T(0x1.c6add6p-5f)
+                             , T(0x1.3b2844p-7f), T(0x1.602430p-10f), T(0x1.459188p-13f));
+        x   = inc(fma(y, sqr(x), x * log_2(eve::as<T>())));
+      }
+      else if constexpr( std::is_same_v<elt_t, double> )
+      {
+        x *= log_2(eve::as<T>());
+        T    t = sqr(x);
+        auto h =
+          eve::reverse_horner(t, T(0x1.555555555553ep-3), T(-0x1.6c16c16bebd93p-9)
+                             , T(0x1.1566aaf25de2cp-14), T(-0x1.bbd41c5d26bf1p-20), T(0x1.6376972bea4d0p-25));
+        T    c = fnma(t, h, x); // x-h*t
+        x      = oneminus(((-(x * c) / (T(2) - c)) - x));
+      }
+      auto z = pedantic(ldexp)(x, k);
+      if constexpr( simd_value<T> )
+      {
+        z = if_else(xltminlog2, eve::zero, z);
+        z = if_else(xgemaxlog2, inf(eve::as(x)), z);
+      }
+      return z;
     }
-    auto k = nearest(x);
-    x      = x - k;
-    if constexpr( std::is_same_v<elt_t, float> )
-    {
-      T y = horn<T, 0x3e75fdf1, 0x3d6356eb, 0x3c1d9422, 0x3ab01218, 0x3922c8c4>(x);
-      x   = inc(fma(y, sqr(x), x * log_2(eve::as<T>())));
-    }
-    else if constexpr( std::is_same_v<elt_t, double> )
-    {
-      x *= log_2(eve::as<T>());
-      T    t = sqr(x);
-      auto h = horn<T,
-                    0x3fc555555555553eull,
-                    0xbf66c16c16bebd93ull,
-                    0x3f11566aaf25de2cull,
-                    0xbebbbd41c5d26bf1ull,
-                    0x3e66376972bea4d0ull>(t);
-      T    c = fnma(t, h, x); // x-h*t
-      x      = oneminus(((-(x * c) / (T(2) - c)) - x));
-    }
-    auto z = pedantic(ldexp)(x, k);
-    if constexpr( simd_value<T> )
-    {
-      z = if_else(xltminlog2, eve::zero, z);
-      z = if_else(xgemaxlog2, inf(eve::as(x)), z);
-    }
-    return z;
+    else { return apply_over(D()(exp2), x); }
   }
-  else { return apply_over(D()(exp2), x); }
-}
 
-template<floating_ordered_value T>
-EVE_FORCEINLINE constexpr T
-exp2_(EVE_SUPPORTS(cpu_), T const& x) noexcept
-{
-  return exp2(regular_type(), x);
-}
+  template<floating_ordered_value T>
+  EVE_FORCEINLINE constexpr T
+  exp2_(EVE_SUPPORTS(cpu_), T const& x) noexcept
+  {
+    return exp2(regular_type(), x);
+  }
 
 ////////////////////////////////////////////////////////////////////////////////////
 // integral types
 
-template<integral_value T, typename D>
-EVE_FORCEINLINE constexpr auto
-exp2_(EVE_SUPPORTS(cpu_), D const&, T xx) noexcept
-    requires(is_one_of<D>(types<converter_type<float>,
-                                converter_type<double>,
-                                floating_converter,
-                                pedantic_type,
-                                raw_type,
-                                regular_type> {}))
+  template<integral_value T, typename D>
+  EVE_FORCEINLINE constexpr auto
+  exp2_(EVE_SUPPORTS(cpu_), D const&, T xx) noexcept
+  requires(is_one_of<D>(types<converter_type<float>,
+                        converter_type<double>,
+                        floating_converter,
+                        pedantic_type,
+                        raw_type,
+                        regular_type> {}))
 {
   if constexpr( has_native_abi_v<T> )
   {
     if constexpr( is_one_of<D>(
-                      types<converter_type<float>, converter_type<double>, floating_converter> {}) )
+                    types<converter_type<float>, converter_type<double>, floating_converter> {}) )
     {
       using b_t  = std::conditional_t<std::is_same_v<D, floating_converter>,
-                                     eve::as_floating_point_t<T>,
-                                     typename D::base_type>;
+        eve::as_floating_point_t<T>,
+        typename D::base_type>;
       using vd_t = typename b_t::value_type;
       using i_t  = as_integer_t<vd_t>;
 
       auto x  = to_<i_t>(xx);
       auto z  = is_nez(x);
       auto zz = eve::min(x + maxexponent(eve::as<vd_t>()), 2 * maxexponent(eve::as<vd_t>()) + 1)
-                & z.mask();
+        & z.mask();
       zz        = zz << nbmantissabits(eve::as<vd_t>());
       using r_t = std::conditional_t<scalar_value<T>, vd_t, wide<vd_t, cardinal_t<T>>>;
       return if_else(z, bit_cast(zz, as<r_t>()), one);
