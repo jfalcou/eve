311387e5746f6302f80bc9c0e0fe1c7a417c9631
jtlap <jtlapreste@gmail.com>
2024-01-08 17:31:18 +0100

    ieee_constant unit test

--- test/doc/core/constant/ieee_constant.cpp
+++ test/doc/core/constant/ieee_constant.cpp
@@ -9,12 +9,12 @@ using wide_dt = eve::wide<double>;
 int main()
 {
   std::cout << "---- simd" << std::setprecision(15) << std::endl
-            << "-> Ieee_constant<wide_ft, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>() " << eve::Ieee_constant<wide_ft, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()<< std::endl
-            << "-> Ieee_constant<wide_dt, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>() " << eve::Ieee_constant<wide_dt, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()<< std::endl;
+            << "-> ieee_constant<wide_ft>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1) " << eve::ieee_constant<wide_ft>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)<< std::endl
+            << "-> ieee_constant<wide_dt>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1) " << eve::ieee_constant<wide_dt>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)<< std::endl;
 
   std::cout << "---- scalar" << std::endl
-            << "-> Ieee_constant<float, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()   " << eve::Ieee_constant<float, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()<< std::endl
-            << "-> Ieee_constant<double, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()  " << eve::Ieee_constant<double, 0X3F1DE9E7U, 0x3FE3BD3CC9BE45DEULL>()<< std::endl;
+            << "-> ieee_constant<float>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)   " << eve::ieee_constant<float>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)<< std::endl
+            << "-> ieee_constant<double>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)  " << eve::ieee_constant<double>(0x1.3bd3ce0p-1f, 0x1.3bd3cc9be45dep-1)<< std::endl;
 
   return 0;
 }
