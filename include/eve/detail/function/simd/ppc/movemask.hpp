6d174fe9176299ed65ff293da47ca08e07d8bf8c
Joel Falcou <joel.falcou@lri.fr>
2024-01-08 11:48:16 +0100

    Fix the movemask typing for PPC

--- include/eve/detail/function/simd/ppc/movemask.hpp
+++ include/eve/detail/function/simd/ppc/movemask.hpp
@@ -16,8 +16,8 @@ namespace eve::detail
   // Adapted from this completely unsuspecting thread:
   // https://stackoverflow.com/questions/33938584/on-powerpc-is-there-any-equivalent-of-intels-movemask-intrinsics
   template<arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE auto movemask(logical<wide<T, N>> const &v) noexcept
-  requires ppc_abi<abi_t<T, N>>
+  EVE_FORCEINLINE std::pair<std::uint64_t,fixed<1>>
+  movemask(logical<wide<T, N>> const &v) noexcept requires ppc_abi<abi_t<T, N>>
   {
     auto mask = []()
     {
@@ -31,6 +31,6 @@ namespace eve::detail
     }();
 
     auto result = vec_vbpermq(bit_cast(v.bits(),as(mask)).storage(), mask.storage());
-    return std::pair{result[0], eve::lane<1>};
+    return {result[0], eve::lane<1>};
   }
 }
