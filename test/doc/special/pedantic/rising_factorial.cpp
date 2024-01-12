5a2e9963167786ceabd65c47c08b9b613b469dc3
jtlap <jtlapreste@gmail.com>
2024-01-12 10:36:07 +0100

    forgotten decorated doc test

--- test/doc/special/pedantic/rising_factorial.cpp
+++ test/doc/special/pedantic/rising_factorial.cpp
@@ -13,6 +13,6 @@ int main()
   std::cout << "---- simd" << std::setprecision(17) << '\n'
             << " <- n                                  = " << n << '\n'
             << " <- x                                  = " << x << '\n'
-            << " -> pedantic(rising_factorial)(n, x)   = " << eve::pedantic(eve::rising_factorial)(n, x) << '\n';
+            << " -> pedantic(rising_factorial)(n, x)   = " << eve::rising_factorial[eve::pedantic](n, x) << '\n';
   return 0;
 }
