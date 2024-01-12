02054c42fb4fd35caf0cc1c56bec34b032ebad33
Joel Falcou <joel.falcou@lri.fr>
2024-01-12 22:09:45 +0100

    Add fien grained support for PPC sub-API

--- include/eve/arch/ppc/spec.hpp
+++ include/eve/arch/ppc/spec.hpp
@@ -53,12 +53,32 @@ namespace eve
 
 #  if !defined(EVE_ABI_DETECTED) && defined(SPY_SIMD_IS_PPC_VMX)
 #   define EVE_CURRENT_ABI ::eve::ppc_
-#   define EVE_CURRENT_API ::eve::vmx_
+#   if defined(SPY_SIMD_IS_PPC_VMX_3_01)
+#     define EVE_CURRENT_API ::eve::vmx_3_01_
+#   elif defined(SPY_SIMD_IS_PPC_VMX_3_00)
+#     define EVE_CURRENT_API ::eve::vmx_3_00_
+#   elif defined(SPY_SIMD_IS_PPC_VMX_2_07)
+#     define EVE_CURRENT_API ::eve::vmx_2_07_
+#   elif defined(SPY_SIMD_IS_PPC_VMX_2_06)
+#     define EVE_CURRENT_API ::eve::vmx_2_06_
+#   elif defined(SPY_SIMD_IS_PPC_VMX_2_05)
+#     define EVE_CURRENT_API ::eve::vmx_2_05_
+#   elif defined(SPY_SIMD_IS_PPC_VMX_2_03)
+#     define EVE_CURRENT_API ::eve::vmx_2_03_
+#   endif
 #   define EVE_ABI_NAMESPACE ppc_abi_v0
 #   define EVE_ABI_DETECTED
 #  elif !defined(EVE_ABI_DETECTED) && defined(SPY_SIMD_IS_PPC_VSX)
 #   define EVE_CURRENT_ABI ::eve::ppc_
-#   define EVE_CURRENT_API ::eve::vsx_
+#   if defined(SPY_SIMD_IS_PPC_VSX_3_01)
+#     define EVE_CURRENT_API ::eve::vsx_3_01_
+#   elif defined(SPY_SIMD_IS_PPC_VSX_3_00)
+#     define EVE_CURRENT_API ::eve::vsx_3_00_
+#   elif defined(SPY_SIMD_IS_PPC_VSX_2_07)
+#     define EVE_CURRENT_API ::eve::vsx_2_07_
+#   elif defined(SPY_SIMD_IS_PPC_VSX_2_06)
+#     define EVE_CURRENT_API ::eve::vsx_2_06_
+#   endif
 #   define EVE_ABI_NAMESPACE ppc_abi_v0
 #   define EVE_ABI_DETECTED
 #  endif
--- include/eve/arch/ppc/tags.hpp
+++ include/eve/arch/ppc/tags.hpp
@@ -7,6 +7,7 @@
 //==================================================================================================
 #pragma once
 
+#include "eve/detail/spy.hpp"
 #include <eve/arch/cpu/tags.hpp>
 #include <eve/arch/ppc/predef.hpp>
 #include <eve/detail/meta.hpp>
@@ -36,14 +37,34 @@ namespace eve
   //================================================================================================
   // Dispatching tag for V*X SIMD implementation
   //================================================================================================
-  struct vmx_    : simd_api<simd_, spy::vmx_> { using is_ppc = void; };
-  struct vsx_    : simd_api<vmx_ , spy::vsx_> { using is_ppc = void; };
+  struct  vmx_      : simd_api<simd_    , spy::vmx_     > { using is_ppc = void; };
+  struct  vmx_2_03_ : simd_api<vmx_     , spy::vmx_2_03_> { using is_ppc = void; };
+  struct  vmx_2_05_ : simd_api<vmx_2_03_, spy::vmx_2_05_> { using is_ppc = void; };
+  struct  vmx_2_06_ : simd_api<vmx_2_05_, spy::vmx_2_06_> { using is_ppc = void; };
+  struct  vmx_2_07_ : simd_api<vmx_2_06_, spy::vmx_2_07_> { using is_ppc = void; };
+  struct  vmx_3_00_ : simd_api<vmx_2_07_, spy::vmx_3_00_> { using is_ppc = void; };
+  struct  vmx_3_01_ : simd_api<vmx_3_00_, spy::vmx_3_01_> { using is_ppc = void; };
+  struct  vsx_      : simd_api<vmx_3_01_, spy::vsx_     > { using is_ppc = void; };
+  struct  vsx_2_06_ : simd_api<vsx_     , spy::vsx_2_06_> { using is_ppc = void; };
+  struct  vsx_2_07_ : simd_api<vsx_2_06_, spy::vsx_2_07_> { using is_ppc = void; };
+  struct  vsx_3_00_ : simd_api<vsx_2_07_, spy::vsx_3_00_> { using is_ppc = void; };
+  struct  vsx_3_01_ : simd_api<vsx_3_00_, spy::vsx_3_01_> { using is_ppc = void; };
 
 //================================================================================================
   //  V*X extension tag objects
   //================================================================================================
-  inline constexpr vmx_ vmx = {};
-  inline constexpr vsx_ vsx = {};
+  inline constexpr vmx_       vmx       = {};
+  inline constexpr vmx_2_03_  vmx_2_03  = {};
+  inline constexpr vmx_2_05_  vmx_2_05  = {};
+  inline constexpr vmx_2_06_  vmx_2_06  = {};
+  inline constexpr vmx_2_07_  vmx_2_07  = {};
+  inline constexpr vmx_3_00_  vmx_3_00  = {};
+  inline constexpr vmx_3_01_  vmx_3_01  = {};
+  inline constexpr vsx_       vsx       = {};
+  inline constexpr vsx_2_06_  vsx_2_06  = {};
+  inline constexpr vsx_2_07_  vsx_2_07  = {};
+  inline constexpr vsx_3_00_  vsx_3_00  = {};
+  inline constexpr vsx_3_01_  vsx_3_01  = {};
 
   // clang-format on
   //================================================================================================
