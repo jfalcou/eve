95e74d5797ec7e79db7a9975928825d90e205178
jtlap <jtlapreste@gmail.com>
2024-01-13 11:05:20 +0100

    i0 i1 in  jn cyl

--- include/eve/module/bessel/regular/impl/cyl_bessel_in.hpp
+++ include/eve/module/bessel/regular/impl/cyl_bessel_in.hpp
@@ -13,46 +13,37 @@
 
 namespace eve::detail
 {
-//////////////////////////////////////////////////////////////////
-// floating scalar,  floating simd -> floating simd,  floating simd
-template<floating_ordered_value T>
-EVE_FORCEINLINE auto
-cyl_bessel_in_(EVE_SUPPORTS(cpu_), T nu, T x) noexcept requires has_native_abi_v<T>
-{
-  return kernel_bessel_i(nu, x);
-}
-
-template<value I, floating_ordered_value T>
-EVE_FORCEINLINE auto
-cyl_bessel_in_(EVE_SUPPORTS(cpu_), I nu, T x) noexcept
-{
-  if constexpr( has_native_abi_v<T> && has_native_abi_v<I> )
+  template<typename I, typename T>
+  EVE_FORCEINLINE auto
+  cyl_bessel_in_(EVE_REQUIRES(cpu_), I nu, T x) noexcept
   {
-    if constexpr( integral_value<I> )
+    if constexpr( has_native_abi_v<T> && has_native_abi_v<I> )
     {
-      if constexpr( simd_value<I> && scalar_value<T> )
+      if constexpr( integral_value<I> )
       {
-        using c_t = wide<T, cardinal_t<I>>;
-        return cyl_bessel_in(convert(nu, as(x)), c_t(x));
+        if constexpr( simd_value<I> && scalar_value<T> )
+        {
+          using c_t = wide<T, cardinal_t<I>>;
+          return cyl_bessel_in(convert(nu, as(x)), c_t(x));
+        }
+        else if constexpr( simd_value<I> && simd_value<T> )
+        {
+          using elt_t = element_type_t<T>;
+          auto tnu    = convert(nu, as(elt_t()));
+          return cyl_bessel_in(tnu, x);
+        }
+        else if constexpr( integral_scalar_value<I> ) { return cyl_bessel_in(T(nu), x); }
       }
-      else if constexpr( simd_value<I> && simd_value<T> )
+      else
       {
-        using elt_t = element_type_t<T>;
-        auto tnu    = convert(nu, as(elt_t()));
-        return cyl_bessel_in(tnu, x);
+        if constexpr( simd_value<I> && scalar_value<T> )
+        {
+          using c_t = wide<T, cardinal_t<I>>;
+          return cyl_bessel_in(nu, c_t(x));
+        }
+        else if constexpr( scalar_value<I> && simd_value<T> ) { return cyl_bessel_in(T(nu), x); }
       }
-      else if constexpr( integral_scalar_value<I> ) { return cyl_bessel_in(T(nu), x); }
     }
-    else
-    {
-      if constexpr( simd_value<I> && scalar_value<T> )
-      {
-        using c_t = wide<T, cardinal_t<I>>;
-        return cyl_bessel_in(nu, c_t(x));
-      }
-      else if constexpr( scalar_value<I> && simd_value<T> ) { return cyl_bessel_in(T(nu), x); }
-    }
+    else return apply_over(cyl_bessel_in, nu, x);
   }
-  else return apply_over(cyl_bessel_in, nu, x);
-}
 }
