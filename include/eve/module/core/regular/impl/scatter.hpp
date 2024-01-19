0fd16bd9c0e70ddcc69ea8849d3c4793f326c51b
Joel Falcou <joel.falcou@lri.fr>
2024-01-19 21:20:29 +0100

    Properly scatter aggregates

--- include/eve/module/core/regular/impl/scatter.hpp
+++ include/eve/module/core/regular/impl/scatter.hpp
@@ -18,25 +18,37 @@ namespace eve::detail
   template<typename T, typename Idx, typename Ptr, callable_options O>
   EVE_FORCEINLINE void scatter_(EVE_REQUIRES(cpu_), O const& o, T const& v, Ptr p, Idx const& idx)
   {
-    // Retrieve element to scatter
-    auto se   = store_equivalent(o[condition_key],v,p);
-
-    // Extract the pointer from a potential aligned_ptr
-    auto base = unalign(get<2>(se));
-
-    // Single-value scatter
-    auto sc = [&](auto n, auto c, auto v)
+    if constexpr(has_aggregated_abi_v<T> && requires{ p + 1; })
     {
-      // We only write if mask is set
-      if constexpr(match_option<condition_key,O,ignore_none_>) write(v.get(n),base+idx.get(n));
-      else
-      {
-        auto m = c.mask( as<as_logical_t<T>>{} );
-        if(m.get(n)) write(v.get(n),base+idx.get(n));
-      }
-    };
+      // slice and scatter both parts;
+      auto[lv,hv] = v.slice();
+      auto[li,hi] = idx.slice();
 
-    // Scatter all (clang doesn't like capturing structured bindings)
-    eve::detail::for_<0, 1, T::size()>([&](auto... I) { ( sc(I,get<0>(se),get<1>(se)), ...); });
+      scatter[o](lv,p,li);
+      scatter[o](hv,p + lv.size(),hi);
+    }
+    else
+    {
+      // Retrieve element to scatter
+      auto se   = store_equivalent(o[condition_key],v,p);
+
+      // Extract the pointer from a potential aligned_ptr
+      auto base = unalign(get<2>(se));
+
+      // Single-value scatter
+      auto sc = [&](auto n, auto c, auto v)
+      {
+        // We only write if mask is set
+        if constexpr(match_option<condition_key,O,ignore_none_>) write(v.get(n),base+idx.get(n));
+        else
+        {
+          auto m = c.mask( as<as_logical_t<T>>{} );
+          if(m.get(n)) write(v.get(n),base+idx.get(n));
+        }
+      };
+
+      // Scatter all (clang doesn't like capturing structured bindings)
+      eve::detail::for_<0, 1, T::size()>([&](auto... I) { ( sc(I,get<0>(se),get<1>(se)), ...); });
+    }
   }
 }
