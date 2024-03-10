781b9b68879f240511f8d76f84c408b7a82a5138
jtlap <jtlapreste@gmail.com>
2024-03-10 11:23:59 +0100

    expx2

--- include/eve/module/math/regular/expx2.hpp
+++ include/eve/module/math/regular/expx2.hpp
@@ -12,6 +12,15 @@
 
 namespace eve
 {
+  template<typename Options>
+  struct expx2_t : elementwise_callable<expx2_t, Options>
+  {
+    template<eve::floating_ordered_value T>
+    EVE_FORCEINLINE T operator()(T v) const  { return EVE_DISPATCH_CALL(v); }
+
+    EVE_CALLABLE_OBJECT(expx2_t, expx2_);
+  };
+
 //================================================================================================
 //! @addtogroup math_exp
 //! @{
@@ -32,7 +41,7 @@ namespace eve
 //!   {
 //!      template< floating_value T>
 //!      T expx2(T x) noexcept;
-///!   }
+//!   }
 //!   @endcode
 //!
 //! **Parameters**
@@ -60,7 +69,50 @@ namespace eve
 //!        @godbolt{doc/math/masked/expx2.cpp}
 //!  @}
 //================================================================================================
-EVE_MAKE_CALLABLE(expx2_, expx2);
-}
+  inline constexpr auto expx2 = functor<expx2_t>;
 
-#include <eve/module/math/regular/impl/expx2.hpp>
+  namespace detail
+  {
+    template<ordered_value T, callable_options O>
+    EVE_FORCEINLINE constexpr T
+    expx2_(EVE_REQUIRES(cpu_), O const&, T a0) noexcept
+    {
+      if constexpr( has_native_abi_v<T> )
+      {
+        if constexpr( eve::platform::supports_invalids && scalar_value<T> )
+        {
+          if( is_nan(a0) ) return a0;
+        }
+        if constexpr( scalar_value<T> && eve::platform::supports_infinites )
+          if( is_infinite(a0) ) return inf(as<T>());
+        T       x       = eve::abs(a0);
+        using u_t = underlying_type_t<T>;
+        const u_t Expx2c1 = ieee_constant<0x1.0p+5f, 0x1.0000000000000p+7>(eve::as<u_t>{});
+        const u_t Expx2c2 = ieee_constant<0x1.0p-5f, 0x1.0000000000000p-7>(eve::as<u_t>{});
+        /* Represent x as an exact multiple of 1/32 plus a residual.  */
+        T m = Expx2c1 * eve::floor(fma(Expx2c2, x, half(as<T>())));
+        x -= m;
+        /* x**2 = m**2 + 2mf + f**2 */
+        T u  = sqr(m);
+        T u1 = fma(T(2) * m, x, sqr(x));
+        /* u is exact, u1 is small.  */
+        auto gtmxlg = is_not_less_equal(u + u1, maxlog(as<T>()));
+        if constexpr( scalar_value<T> )
+        {
+          if( gtmxlg ) return inf(as<T>());
+          return eve::exp(u) * eve::exp(u1);
+        }
+        else if constexpr( simd_value<T> )
+        {
+          T r = eve::if_else(gtmxlg, eve::inf(as<T>()), eve::exp(u) * eve::exp(u1));
+          if constexpr( eve::platform::supports_infinites ) r = eve::if_else((x == inf(as<T>())), x, r);
+          if constexpr( eve::platform::supports_invalids )
+            r = eve::if_else(is_nan(a0), eve::allbits, r);
+          return r;
+        }
+        return u + u1;
+      }
+      else return apply_over(expx2, a0);
+    }
+  }
+}
