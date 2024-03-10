5c959df27db12bb578a80a0676f1f1b5f78eb48a
jtlap <jtlapreste@gmail.com>
2024-03-10 11:14:32 +0100

    exp2 without converters

--- include/eve/module/math/pedantic/math.hpp
+++ include/eve/module/math/pedantic/math.hpp
@@ -10,8 +10,6 @@
 #include <eve/module/math/pedantic/atan2.hpp>
 #include <eve/module/math/pedantic/atan2d.hpp>
 #include <eve/module/math/pedantic/atan2pi.hpp>
-#include <eve/module/math/pedantic/exp.hpp>
-#include <eve/module/math/pedantic/exp2.hpp>
 #include <eve/module/math/pedantic/expm1.hpp>
 #include <eve/module/math/pedantic/horner.hpp>
 #include <eve/module/math/pedantic/hypot.hpp>
