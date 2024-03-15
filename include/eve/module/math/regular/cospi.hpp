0371355ad8f912b61534b4514bcb14fdee1ab782
jtlap <jtlapreste@gmail.com>
2024-03-15 17:31:03 +0100

    cospi

--- include/eve/module/math/regular/cospi.hpp
+++ include/eve/module/math/regular/cospi.hpp
@@ -7,10 +7,26 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/overload.hpp>
+#include <eve/arch.hpp>
+#include <eve/traits/overload.hpp>
+#include <eve/module/core/decorator/core.hpp>
+#include <eve/module/core.hpp>
+#include <eve/module/math/constant/pi.hpp>
+#include <eve/module/math/decorator/trigo_tags.hpp>
+#include <eve/module/math/regular/cos.hpp>
 
 namespace eve
 {
+  template<typename Options>
+  struct cospi_t : elementwise_callable<cospi_t, Options, quarter_circle_option, half_circle_option,
+                                        full_circle_option, medium_option, big_option>
+  {
+    template<eve::value T>
+    constexpr EVE_FORCEINLINE T operator()(T v) const  { return EVE_DISPATCH_CALL(v); }
+
+    EVE_CALLABLE_OBJECT(cospi_t, cospi_);
+  };
+
 //================================================================================================
 //! @addtogroup math_trig
 //! @{
@@ -63,8 +79,50 @@ namespace eve
 //!
 //!  @}
 //================================================================================================
+ inline constexpr auto cospi = functor<cospi_t>;
 
-EVE_MAKE_CALLABLE(cospi_, cospi);
+  namespace detail
+  {
+    template<typename T, callable_options O>
+    constexpr EVE_FORCEINLINE T cospi_(EVE_REQUIRES(cpu_), O const&, T const& a0)
+    {
+      if constexpr(floating_value<T>)
+      {
+        if constexpr(O::contains(half_circle2) || O::contains(full_circle2)
+                     ||  O::contains(medium2) || O::contains(big2))
+        {
+          if constexpr( scalar_value<T> )
+            if( is_not_finite(a0) ) return nan(eve::as<T>());
+          auto x = eve::abs(a0);
+          if constexpr( scalar_value<T> )
+          {
+            if( x > maxflint(eve::as<T>()) ) return T(1);
+          }
+          else
+          {
+            x = if_else(is_not_finite(x), eve::allbits, x); // nan or Inf input
+            x = if_else(is_greater(x, maxflint(eve::as(x))), eve::zero, x);
+          }
+          auto [fn, xr, dxr] = rem2(x);
+          return cos_finalize(fn, xr, dxr);
+        }
+        else if constexpr(O::contains(quarter_circle2))
+        {
+          return eve::cos[quarter_circle2](a0*pi(eve::as<T>()));
+        }
+        else
+        {
+          auto x = abs(a0);
+          if( eve::all(eve::abs(x) <= T(0.25)) )
+            return cospi[quarter_circle2](x);
+          else
+            return cospi[big2](x);
+        }
+      }
+      else
+      {
+        return if_else(is_odd(a0), eve::mone, one(eve::as(a0))); //(-1)^n
+      }
+    }
+  }
 }
-
-#include <eve/module/math/regular/impl/cospi.hpp>
