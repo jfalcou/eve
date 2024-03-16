fc932934bf42dab2b94dd391fdb3949b33298f30
Joel Falcou <joel.falcou@lri.fr>
2024-03-16 08:33:15 +0100

    #1520 - avx512 scatter support

--- include/eve/module/core/regular/scatter.hpp
+++ include/eve/module/core/regular/scatter.hpp
@@ -19,13 +19,14 @@ template<typename Options>
 struct scatter_t : callable<scatter_t, Options, relative_conditional_no_alternative_option>
 {
   template<simd_value T, integral_simd_value Idx, simd_compatible_ptr<T> Ptr>
+  requires(T::size() == Idx::size())
   EVE_FORCEINLINE void operator()(T const& v, Ptr ptr, Idx const& idx) const noexcept { EVE_DISPATCH_CALL(v,ptr,idx); }
 
   EVE_CALLABLE_OBJECT(scatter_t, scatter_);
 };
 
 //======================================================================================================================
-//! @addtogroup core_arithmetic
+//! @addtogroup core_simd
 //! @{
 //!   @var scatter
 //!   @brief Store a SIMD register to memory using scattered indexes
@@ -52,6 +53,7 @@ struct scatter_t : callable<scatter_t, Options, relative_conditional_no_alternat
 //!   namespace eve
 //!   {
 //!     template<simd_value T, integral_simd_value Idx, simd_compatible_ptr<T> Ptr>
+//!     requires(T::size() == Idx::size())
 //!     void scatter(T const& v, Ptr ptr, Idx const& idx) noexcept;
 //!   }
 //!   @endcode
@@ -61,6 +63,23 @@ struct scatter_t : callable<scatter_t, Options, relative_conditional_no_alternat
 //!     * `v` :  [SIMD value](@ref eve::simd_value) to scatter
 //!     * `ptr` : Base pointer to scatter to.
 //!     * `idx` :  [Integral SIMD value](@ref eve::integral_simd_value) containing the index to scatter to.
+//!
+//!   @groupheader{Semantic Modifiers}
+//!
+//!   * Masked Call
+//!
+//!   eve::scatter can be masked using [Relative conditionals](@ref relative_conditional_expr) to skip scattering
+//!   of certain elements.
+//!
+//!   The call `eve::scatter[cond](v,p,i)` is semantically equivalent to:
+//!
+//!   @code
+//!   auto m = cond.mask( as<as_logical_t<T>>{} );
+//!   for(std::size_t n=0;n<v.size();++n)
+//!   {
+//!     if(m.get(n)) write(v.get(n),p+idx.get(n));
+//!   }
+//!   @endcode
 //!
 //!   @groupheader{Example}
 //!
@@ -72,3 +91,7 @@ inline constexpr auto scatter = functor<scatter_t>;
 }
 
 #include <eve/module/core/regular/impl/scatter.hpp>
+
+#if defined(EVE_INCLUDE_X86_HEADER)
+#  include <eve/module/core/regular/impl/simd/x86/scatter.hpp>
+#endif
--- test/unit/module/core/scatter.cpp
+++ test/unit/module/core/scatter.cpp
@@ -26,6 +26,28 @@ void test_scatter(T const& values, auto ptr)
   TTS_EXPECT(std::equal(&ref[0],&ref[0]+2*T::size(),base));
 }
 
+template<typename Index, typename T, typename M>
+void test_scatter(T const& values, M const& cond, auto ptr)
+{
+  using e_t = eve::element_type_t<T>;
+  eve::wide<Index, eve::fixed<T::size()>> indexes = [](auto i, auto) { return 2*i; };
+
+  e_t ref [2*T::size()];
+  auto base = eve::unalign(ptr);
+  std::copy(base,base+2*T::size(),&ref[0]);
+
+  auto m = cond.mask( eve::as<eve::as_logical_t<T>>{} );
+
+  for( std::size_t n = 0; n < T::size(); ++n )
+  {
+    if(m.get(n)) ref[indexes.get(n)] = values.get(n);
+  }
+
+  eve::scatter[cond](values, ptr, indexes);
+
+  TTS_EXPECT(std::equal(&ref[0],&ref[0]+2*T::size(),base));
+}
+
 TTS_CASE_TPL("Check eve::scatter behavior with pointer", eve::test::simd::all_types)
 <typename T>(tts::type<T>)
 {
@@ -40,6 +62,20 @@ TTS_CASE_TPL("Check eve::scatter behavior with pointer", eve::test::simd::all_ty
   test_scatter<std::uint64_t>(values,&data[0]);
 };
 
+TTS_CASE_TPL("Check eve::scatter behavior with pointer and mask", eve::test::simd::all_types)
+<typename T>(tts::type<T>)
+{
+  using e_t = eve::element_type_t<T>;
+  e_t data[2*T::size()];
+  for( std::size_t i = 0; i < 2*T::size(); ++i ) data[i] = e_t(99);
+  T values = [](auto i, auto) { return 2*i+1; };
+
+  test_scatter<std::int32_t >(values, eve::ignore_first(1), &data[0]);
+  test_scatter<std::uint32_t>(values, eve::ignore_first(1), &data[0]);
+  test_scatter<std::int64_t >(values, eve::ignore_first(1), &data[0]);
+  test_scatter<std::uint64_t>(values, eve::ignore_first(1), &data[0]);
+};
+
 TTS_CASE_TPL("Check eve::scatter behavior with aligned_ptr", eve::test::simd::all_types)
 <typename T>(tts::type<T>)
 {
@@ -52,4 +88,18 @@ TTS_CASE_TPL("Check eve::scatter behavior with aligned_ptr", eve::test::simd::al
   test_scatter<std::uint32_t>(values,eve::as_aligned(&data[0],typename T::cardinal_type{}));
   test_scatter<std::int64_t >(values,eve::as_aligned(&data[0],typename T::cardinal_type{}));
   test_scatter<std::uint64_t>(values,eve::as_aligned(&data[0],typename T::cardinal_type{}));
+};
+
+TTS_CASE_TPL("Check eve::scatter behavior with aligned_ptr and mask", eve::test::simd::all_types)
+<typename T>(tts::type<T>)
+{
+  using e_t = eve::element_type_t<T>;
+  alignas(T::alignment()) e_t data[2*T::size()];
+  for( std::size_t i = 0; i < 2*T::size(); ++i ) data[i] = e_t(99);
+  T values = [](auto i, auto) { return 2*i+1; };
+
+  test_scatter<std::int32_t >(values,eve::ignore_first(1),eve::as_aligned(&data[0],typename T::cardinal_type{}));
+  test_scatter<std::uint32_t>(values,eve::ignore_first(1),eve::as_aligned(&data[0],typename T::cardinal_type{}));
+  test_scatter<std::int64_t >(values,eve::ignore_first(1),eve::as_aligned(&data[0],typename T::cardinal_type{}));
+  test_scatter<std::uint64_t>(values,eve::ignore_first(1),eve::as_aligned(&data[0],typename T::cardinal_type{}));
 };
