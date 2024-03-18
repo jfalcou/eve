57ff7c76eae8fe5fd787bb5e543543960664b012
Joel Falcou <joel.falcou@lri.fr>
2024-03-18 21:28:20 +0100

    Missing rempio2 elements

--- include/eve/module/math/regular/rempio2.hpp
+++ include/eve/module/math/regular/rempio2.hpp
@@ -10,7 +10,6 @@
 #include <eve/traits/overload.hpp>
 #include <eve/module/core.hpp>
 #include <eve/module/core/decorator/core.hpp>
-#include <eve/module/core.hpp>
 #include <eve/module/math/decorator/trigo_tags.hpp>
 #include <eve/module/math/detail/constant/rempio2_limits.hpp>
 #include <eve/module/math/detail/generic/rempio2_kernel.hpp>
@@ -18,13 +17,14 @@
 namespace eve
 {
   template<typename Options>
-  struct rempio2_t : elementwise_callable<rempio2_t, Options, quarter_circle_option, half_circle_option
-                                          , full_circle_option, medium_option, big_option>
+  struct rempio2_t : elementwise_callable < rempio2_t, Options
+                                          , quarter_circle_option, half_circle_option
+                                          , full_circle_option, medium_option, big_option
+                                          >
   {
-    template<eve::floating_ordered_value T>
-    constexpr EVE_FORCEINLINE auto
-    operator()(T v) const  noexcept
-    { return EVE_DISPATCH_CALL(v); }
+    template<eve::floating_value T>
+    constexpr EVE_FORCEINLINE
+    eve::result_t<zip,T,T,T> operator()(T v) const  noexcept { return EVE_DISPATCH_CALL(v); }
 
     EVE_CALLABLE_OBJECT(rempio2_t, rempio2_);
   };
@@ -73,7 +73,7 @@ namespace eve
   {
     template<typename T, callable_options O>
     constexpr EVE_FORCEINLINE auto
-    rempio2_(EVE_REQUIRES(cpu_), O const& o, T const& x) noexcept
+    rempio2_(EVE_REQUIRES(cpu_), O const&, T const& x) noexcept
     {
       if      constexpr( O::contains(quarter_circle2))  return rempio2_half_circle(x);
       else if constexpr( O::contains(half_circle2))     return rempio2_half_circle(x);
@@ -82,7 +82,7 @@ namespace eve
       else if constexpr( O::contains(big2))             return rempio2_big(x);
       else
       {
-        if( eve::all(x <= Rempio2_limit[quarter_circle2](as(x))))   return  kumi::make_tuple(T(0), x, T(0));
+        if( eve::all(x <= Rempio2_limit[quarter_circle2](as(x))))   return eve::zip(T(0), x, T(0));
         else if( eve::all(x <= Rempio2_limit[half_circle2](as(x)))) return rempio2_half_circle(x);
         else if( eve::all(x <= Rempio2_limit[full_circle2](as(x)))) return rempio2_full_circle(x);
         else if( eve::all(x <= Rempio2_limit[medium2](as(x))))      return rempio2_medium(x);
