46c9f61931f74018041cd7aa315453fa3c0397fb
jtlap <jtlapreste@gmail.com>
2024-03-22 11:23:27 +0100

    bit_ ceil/floor/flip

--- include/eve/module/core/regular/bit_ceil.hpp
+++ include/eve/module/core/regular/bit_ceil.hpp
@@ -7,10 +7,30 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/overload.hpp>
+#include <eve/arch.hpp>
+#include <eve/traits/overload.hpp>
+#include <eve/module/core/decorator/core.hpp>
+#include <eve/module/core/constant/one.hpp>
+#include <eve/module/core/regular/bit_floor.hpp>
+#include <eve/module/core/regular/dec.hpp>
+#include <eve/module/core/regular/if_else.hpp>
+#include <eve/module/core/regular/ifrexp.hpp>
+#include <eve/module/core/regular/is_less.hpp>
+#include <eve/module/core/regular/is_less_equal.hpp>
+#include <eve/module/core/regular/ldexp.hpp>
 
 namespace eve
 {
+  template<typename Options>
+  struct bit_ceil_t : elementwise_callable<bit_ceil_t, Options>
+  {
+    template<eve::value T>
+    constexpr EVE_FORCEINLINE T operator()(T v) const
+    { return EVE_DISPATCH_CALL(v); }
+
+    EVE_CALLABLE_OBJECT(bit_ceil_t, bit_ceil_);
+  };
+
 //================================================================================================
 //! @addtogroup core_bitops
 //! @{
@@ -61,7 +81,35 @@ namespace eve
 //!        @godbolt{doc/core/masked/bit_ceil.cpp}
 //! @}
 //================================================================================================
-EVE_MAKE_CALLABLE(bit_ceil_, bit_ceil);
-}
+  inline constexpr auto bit_ceil = functor<bit_ceil_t>;
 
-#include <eve/module/core/regular/impl/bit_ceil.hpp>
+  namespace detail
+  {
+    template<typename T, callable_options O>
+    EVE_FORCEINLINE constexpr T
+    bit_ceil_(EVE_REQUIRES(cpu_), O const&, T const& v) noexcept
+    {
+      auto vle1 = v <= one(eve::as(v));
+      if constexpr( scalar_value<T> )
+      {
+        if( vle1 ) return one(eve::as(v));
+      }
+      if constexpr( floating_value<T> )
+      {
+        auto [m, e] = ifrexp(v);
+        e           = dec(e);
+        auto tmp    = ldexp(one(eve::as(v)), e);
+        auto tmpltv = tmp < v;
+        if constexpr( scalar_value<T> ) { return tmpltv ? tmp + tmp : tmp; }
+        else { return if_else(vle1, one(eve::as(v)), if_else(tmpltv, tmp + tmp, tmp)); }
+      }
+      else
+      {
+        auto tmp    = bit_floor(v);
+        auto tmpltv = tmp < v;
+        if constexpr( scalar_value<T> ) return T(tmpltv ? tmp + tmp : tmp);
+        else return if_else(vle1, one, if_else(tmpltv, tmp + tmp, tmp));
+      }
+    }
+  }
+}
