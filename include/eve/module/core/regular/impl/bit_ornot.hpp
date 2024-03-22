3913efaa929a9a9fe2af7c2b9729d9d36a158d47
jtlap <jtlapreste@gmail.com>
2024-03-22 14:26:49 +0100

    bit_ornot

--- include/eve/module/core/regular/impl/bit_ornot.hpp
+++ include/eve/module/core/regular/impl/bit_ornot.hpp
@@ -7,7 +7,6 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/concept/compatible.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/detail/apply_over.hpp>
 #include <eve/detail/function/conditional.hpp>
@@ -21,86 +20,31 @@
 
 namespace eve::detail
 {
-template<ordered_value T, ordered_value U>
-EVE_FORCEINLINE auto
-bit_ornot_(EVE_SUPPORTS(cpu_), T const& a, U const& b) noexcept requires bit_compatible_values<T, U>
-{
-  return bit_call(bit_ornot, a, b);
-}
-
-template<ordered_value T>
-EVE_FORCEINLINE auto
-bit_ornot_(EVE_SUPPORTS(cpu_), T const& a, T const& b) noexcept
-{
-  if constexpr(scalar_value<T>)
+  template<typename T0, typename T1, typename... Ts, callable_options O>
+  EVE_FORCEINLINE constexpr bit_value_t<T0, T1, Ts...>
+  bit_ornot_(EVE_REQUIRES(cpu_), O const & o, T0 a, T1 b, Ts... args) noexcept
   {
-    if constexpr( floating_value<T> )
+    using r_t = bit_value_t<T0, T1, Ts...>;
+    if constexpr(sizeof...(Ts) == 0)
     {
-      using b_t = as_integer_t<T, unsigned>;
-      return bit_cast(b_t(bit_cast(a, as<b_t>()) | ~bit_cast(b, as<b_t>())), as(a));
+      if constexpr(scalar_value<r_t>)
+      {
+        if constexpr( floating_value<r_t> )
+        {
+          using b_t = as_integer_t<r_t, unsigned>;
+          return bit_cast(b_t(bit_cast(a, as<b_t>()) | ~bit_cast(b, as<b_t>())), as(a));
+        }
+        else return r_t(a | ~b);
+      }
+      else //simd case
+      {
+        return bit_or(a, bit_not(b));
+      }
     }
-    else return T(a | ~b);
+    else
+    {
+      auto that = bit_and(r_t(b), r_t(args)...);
+      return eve::bit_ornot(r_t(a), that);
+    }
   }
-  else //simd case
-  {
-    return bit_or(a, bit_not(b)); // fallback never taken if proper intrinsics are at hand
-  }
-}
-
-// -----------------------------------------------------------------------------------------------
-// Masked case
-template<conditional_expr C, ordered_value U, ordered_value V>
-EVE_FORCEINLINE auto
-bit_ornot_(EVE_SUPPORTS(cpu_),
-           C const& cond,
-           U const& t,
-           V const& f) noexcept requires bit_compatible_values<U, V>
-{
-  return mask_op(cond, eve::bit_ornot, t, f);
-}
-
-//================================================================================================
-// N parameters
-//================================================================================================
-template<ordered_value T0, ordered_value T1, ordered_value... Ts>
-auto
-bit_ornot_(EVE_SUPPORTS(cpu_),
-           T0 a0,
-           T1 a1,
-           Ts... args) requires bit_compatible_values<T0, T1> &&(bit_compatible_values<T1, Ts>&&...)
-{
-  auto that = bit_and(a1, args...);
-  return bit_ornot(a0, that);
-}
-
-template<conditional_expr C, ordered_value T0, ordered_value T1, ordered_value... Ts>
-auto
-bit_ornot_(EVE_SUPPORTS(cpu_), C const& cond, T0 a0, T1 a1, Ts... args) requires
-    bit_compatible_values<T0, T1> &&(bit_compatible_values<T1, Ts>&&...)
-{
-  auto that = bit_and(a1, args...);
-  return mask_op(cond, eve::bit_ornot, a0, that);
-}
-
-
-//================================================================================================
-// tuples
-//================================================================================================
-template<kumi::non_empty_product_type Ts>
-auto
-bit_ornot_(EVE_SUPPORTS(cpu_), Ts const & args)
-{
-  if constexpr(kumi::size_v<Ts> == 1) return get<0>(args);
-  else if constexpr(kumi::size_v<Ts> == 2) return bit_ornot(get<0>(args), get<1>(args));
-  else return bit_ornot(get<0>(args), kumi::apply(bit_and, kumi::pop_front(args)));
-}
-
-template<conditional_expr C, kumi::non_empty_product_type Ts>
-auto
-bit_ornot_(EVE_SUPPORTS(cpu_), C const& cond, Ts const & args)
-{
-  if constexpr(kumi::size_v<Ts> == 1) return get<0>(args);
-  else return kumi::apply( [&](auto... m) { return mask_op(cond, eve::bit_ornot, m...); }, args);
-}
-
 }
