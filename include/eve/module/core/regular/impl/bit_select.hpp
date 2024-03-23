1cabe6a84b76ba74d115ee1774ea9e8487224841
jtlap <jtlapreste@gmail.com>
2024-03-23 23:26:30 +0100

    unused params

--- include/eve/module/core/regular/impl/bit_select.hpp
+++ include/eve/module/core/regular/impl/bit_select.hpp
@@ -22,11 +22,14 @@ namespace eve::detail
   EVE_FORCEINLINE constexpr  bit_value_t<common_value_t<T, U>, M>
   bit_select_(EVE_REQUIRES(cpu_), O const&, M m, T u, U v) noexcept
   {
-    using c_t = common_value_t<T, U>;
     if constexpr(!std::same_as<T, U>)
+    {
+      using c_t = common_value_t<T, U>;
       return bit_select(m, c_t(u), c_t(v));
+    }
     else
     {
+      using c_t = common_value_t<T, U>;
       using r_t = bit_value<c_t, M>;
       if constexpr(scalar_value<r_t>)
         return bit_or(bit_and(u, m), bit_andnot(v, m));
