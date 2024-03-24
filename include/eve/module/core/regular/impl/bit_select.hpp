6150a11f3f26c98f4b4f58097ff6ae8e365505f6
jtlap <jtlapreste@gmail.com>
2024-03-24 13:30:27 +0100

    bit_select

--- include/eve/module/core/regular/impl/bit_select.hpp
+++ include/eve/module/core/regular/impl/bit_select.hpp
@@ -19,22 +19,9 @@ namespace eve::detail
 {
 
   template<typename T, typename U, typename M, callable_options O>
-  EVE_FORCEINLINE constexpr  bit_value_t<common_value_t<T, U>, M>
+  EVE_FORCEINLINE constexpr auto
   bit_select_(EVE_REQUIRES(cpu_), O const&, M m, T u, U v) noexcept
   {
-    if constexpr(!std::same_as<T, U>)
-    {
-      using c_t = common_value_t<T, U>;
-      return bit_select(m, c_t(u), c_t(v));
-    }
-    else
-    {
-      using c_t = common_value_t<T, U>;
-      using r_t = bit_value<c_t, M>;
-      if constexpr(scalar_value<r_t>)
-        return bit_or(bit_and(u, m), bit_andnot(v, m));
-      else
-        return (u & m) | bit_andnot(v, m);
-    }
+    return bit_or(bit_and(u, m), bit_andnot(v, m));
   }
 }
