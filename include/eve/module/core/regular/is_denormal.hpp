4d96245340e382d606c3ea3f4a81588e003785ee
jtlap <jtlapreste@gmail.com>
2024-03-28 13:10:13 +0100

    is_denormal

--- include/eve/module/core/regular/is_denormal.hpp
+++ include/eve/module/core/regular/is_denormal.hpp
@@ -93,7 +93,7 @@ namespace eve
   {
     template<typename T, callable_options O>
     EVE_FORCEINLINE constexpr as_logical_t<T>
-    is_denormal_(EVE_REQUIRES(cpu_), T const& a) noexcept
+    is_denormal_(EVE_REQUIRES(cpu_), O const &, T const& a) noexcept
     {
       if constexpr( !floating_value<T> || !eve::platform::supports_denormals )
         return false_(eve::as<T>());
