49aff3a2880ba7e33142cbb3bd7b5f8f7723b9d9
jtlap <jtlapreste@gmail.com>
2024-03-30 20:30:45 +0100

    nearest sve

--- include/eve/module/core/regular/impl/simd/arm/sve/nearest.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/nearest.hpp
@@ -10,16 +10,17 @@
 #include <eve/concept/value.hpp>
 #include <eve/arch/arm/sve/sve_true.hpp>
 #include <eve/detail/implementation.hpp>
+#include <iostream>
 
 namespace eve::detail
 {
   template<floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE  wide<T, N> nearest_(EVE_REQUIRES(sve_),
-                                       O           const&,
+                                       O           const& opts,
                                        wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
-    return nearest[ignore_none](v);
+    return nearest_(adl_helper_t{}, sve_{}, ignore_none, opts, v);
   }
 
   template<conditional_expr C,floating_scalar_value T, typename N, callable_options O>
@@ -29,9 +30,9 @@ namespace eve::detail
                                       wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
-    if constexpr( C::is_complete)
-      return alternative(cond, v,as(v));
-    else
-      return svrintn_m(alternative(cond, v, as(v)), cond.mask(as(v)), v);
+    auto alt = alternative(cond, v, as(v));
+
+    if      constexpr(C::is_complete && !C::is_inverted) return alt;
+    else   return svrintn_m(alt, cond.mask(as(v)), v);
   }
 }
