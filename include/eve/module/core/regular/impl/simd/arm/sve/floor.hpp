d0a0b71792db789ab396715ffa19897750752a85
jtlap <jtlapreste@gmail.com>
2024-04-01 17:10:02 +0200

    some cleaning

--- include/eve/module/core/regular/impl/simd/arm/sve/floor.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/floor.hpp
@@ -14,9 +14,9 @@
 namespace eve::detail
 {
   template<floating_scalar_value T, typename N, callable_options O>
-  EVE_FORCEINLINE auto floor_(EVE_REQUIRES(sve_),
-                              O const& opts,
-                              wide<T, N> const& v) noexcept -> wide<T, N>
+  EVE_FORCEINLINE wide<T, N> floor_(EVE_REQUIRES(sve_),
+                              O          const& opts,
+                              wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
     return floor_(adl_helper_t{}, sve_{}, ignore_none, opts, v);
@@ -24,8 +24,8 @@ namespace eve::detail
 
   template<conditional_expr C,floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N> floor_(EVE_REQUIRES(sve_),
-                              O const& opts,
-                              C const& cond,
+                              O          const& opts,
+                              C          const& cond,
                               wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
@@ -33,6 +33,6 @@ namespace eve::detail
 
     if      constexpr(C::is_complete && !C::is_inverted) return alt;
     else if constexpr(!O::contains(tolerance))           return svrintm_m(alt, cond.mask(as(v)), v);
-    else                                                 return trunc.behavior(cpu_{}, opts, v);
+    else                                                 return floor.behavior(cpu_{}, opts, v);
   }
 }
