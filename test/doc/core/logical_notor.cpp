53f8125f3ef954995f4c9407b4a71a6f0b936f17
jtlap <jtlapreste@gmail.com>
2024-04-02 18:09:44 +0200

    l*.cpp +manhattan + mantissa

---
+++ test/doc/core/logical_notor.cpp
@@ -0,0 +1,26 @@
+#include <eve/module/core.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+using wide_ft = eve::wide<float, eve::fixed<4>>;
+
+int main()
+{
+  using eve::is_even;
+  using eve::is_odd;
+  wide_ft pf = {3.0f, -2.0f, -3.0f, 51.04f};
+  wide_ft qf = {4.0f, -1.0f, -3.0f, 0.0f};
+  std::cout << "---- simd" << '\n'
+            << " <- pf                                          = " << pf << '\n'
+            << " <- qf                                          = " << qf << '\n'
+            << " -> eve::logical_notor(is_odd(pf), is_even(qf)) = " << eve::logical_notor(is_odd(pf), is_even(qf)) << '\n';
+
+  float xf = 3.0f, yf = 4.5f;
+
+  std::cout << "---- scalar" << '\n'
+            << " xf                                             = " << xf << '\n'
+            << " yf                                             = " << yf << '\n'
+            << " -> eve::logical_notor(is_odd(xf), is_even(yf)) = " << eve::logical_notor(is_odd(xf), is_even(yf)) << '\n'
+            << " -> eve::logical_notor(xf == 3,    is_even(yf)) = " << eve::logical_notor(xf == 3   , is_even(yf)) << '\n' ;
+  return 0;
+}
