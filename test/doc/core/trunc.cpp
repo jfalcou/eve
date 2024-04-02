8832793d553062c68c6de723e4d2145f883f90ac
jtlap <jtlapreste@gmail.com>
2024-04-02 22:24:48 +0200

    [tz]*

---
+++ test/doc/core/trunc.cpp
@@ -0,0 +1,26 @@
+#include <eve/module/core.hpp>
+#include <eve/wide.hpp>
+#include <iostream>
+
+using wide_ft = eve::wide<float, eve::fixed<8>>;
+
+int main()
+{
+  float decr = -eve::eps(eve::as<float>());
+  wide_ft pf = {-1.0f, -1.3f, -1.5f, -1.7f, 2.0f, 2.3f, 2.5f, 2.7f};
+
+  std::cout << "---- simd" << '\n'
+            << "<- pf                            = " << pf << '\n'
+            << "-> trunc(pf)                     = " << eve::trunc(pf) << '\n'
+            << "-> trunc[tolerant](pf)           = " << eve::trunc[eve::tolerant](pf)           << '\n'
+            << "-> trunc[tolerance = 2*decr](pf) = " << eve::trunc[eve::tolerance = 2*decr](pf) << '\n'
+            << "-> trunc[tolerance = 4](pf)      = " << eve::trunc[eve::tolerance = 4](pf)      << '\n';
+  return 0;
+  float xf = -32.768f;
+
+  std::cout << "---- scalar" << '\n'
+            << "<- xf        = " << xf << '\n'
+            << "-> trunc(xf) = " << eve::trunc(xf) << '\n';
+
+  return 0;
+}
