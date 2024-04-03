543364ed1dee3ad9f7950d78ccc28169c4c6ce27
jtlap <jtlapreste@gmail.com>
2024-04-03 16:29:49 +0200

    average

--- test/doc/core/average.cpp
+++ test/doc/core/average.cpp
@@ -8,13 +8,12 @@ int main()
   using w_t = eve::wide<std::uint32_t, eve::fixed<4>>;
   w_t pi = {3, 2, 3, 3};
   w_t qi = {4, 1, 1, eve::valmax(eve::as<float>())};
-  w_t ri = {4, 1, 1, eve::valmax(eve::as<float>())};
 
   std::cout << "---- simd" << '\n'
             << " <- pi                       = " << pi << '\n'
             << " <- qi                       = " << qi << '\n'
             << " -> average(pi, qi)          = " << eve::average(pi, qi) << '\n';
-//            << " -> average(pi, qi, ri)      = " << eve::average(pi, qi, ri) << '\n';
+
 
   std::uint32_t xi = 3, yi = 4;
 
@@ -26,10 +25,10 @@ int main()
   using w_ft = eve::wide<float, eve::fixed<4>>;
   w_ft pf = {3, 4, 3, 10}, qf = {4, 1, 1, 15};;
   std::cout << "---- multi" << '\n'
-            << " <- pf                               = " << pf << '\n'
-            << " <- qf                               = " << qf << '\n'
-            << " -> average(pf, 0.0f, qf, pf, 11.0f) = " << eve::average(pf, 0.0f, qf, pf, 11.0f) <<  '\n'
-            << " -> raw(average)(pi, qi, ri)         = " << eve::average[eve::raw](pf, 0.0f, pf, qf, 11.0f) << '\n';
+            << " <- pf                                    = " << pf << '\n'
+            << " <- qf                                    = " << qf << '\n'
+            << " -> average(pf, 0.0f, qf, pf, 11.0f)      = " << eve::average(pf, 0.0f, qf, pf, 11.0f) <<  '\n'
+            << " -> average[raw](pf, 0.0f, pf, qf, 11.0f) = " << eve::average[eve::raw](pf, 0.0f, pf, qf, 11.0f) << '\n';
 
   std::cout << "---- multi parameters" << '\n';
   return 0;
