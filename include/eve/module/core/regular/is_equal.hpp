e976a850ee977949e2fb50c6dc4f60ea9221dda7
jtlap <jtlapreste@gmail.com>
2024-04-08 20:15:30 +0200

    is_equal

--- include/eve/module/core/regular/is_equal.hpp
+++ include/eve/module/core/regular/is_equal.hpp
@@ -7,15 +7,39 @@
 //==================================================================================================
 #pragma once
 
+
 #include <eve/arch.hpp>
+#include <eve/traits/overload.hpp>
+#include <eve/module/core/decorator/core.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/detail/function/friends.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/detail/overload.hpp>
 #include <eve/traits/as_logical.hpp>
+#include <eve/module/core/constant/eps.hpp>
+#include <eve/module/core/regular/abs.hpp>
+#include <eve/module/core/regular/convert.hpp>
+#include <eve/module/core/regular/dist.hpp>
+#include <eve/module/core/regular/is_less_equal.hpp>
+#include <eve/module/core/regular/max.hpp>
+#include <eve/module/core/regular/nb_values.hpp>
 
 namespace eve
 {
+  template<typename Options>
+  struct is_equal_t : elementwise_callable<is_equal_t, Options, numeric_option, tolerant_option>
+  {
+    template<value T,  value U>
+    constexpr EVE_FORCEINLINE as_logical_t<common_value_t<T, U>> operator()(logical<T> a, logical<U> b) const
+    { return EVE_DISPATCH_CALL(a, b); }
+
+    template<value T,  value U>
+    constexpr EVE_FORCEINLINE as_logical_t<common_value_t<T, U>> operator()(T a, U b) const
+    { return EVE_DISPATCH_CALL(a, b); }
+
+    EVE_CALLABLE_OBJECT(is_equal_t, is_equal_);
+  };
+
 //================================================================================================
 //! @addtogroup core_predicates
 //! @{
@@ -82,25 +106,64 @@ namespace eve
 //!
 //! @}
 //================================================================================================
-EVE_IMPLEMENT_CALLABLE(is_equal_, is_equal);
+  inline constexpr auto is_equal = functor<is_equal_t>;
 
-namespace detail
-{
-  template<value T, value U>
-  EVE_FORCEINLINE auto is_equal_(EVE_SUPPORTS(cpu_), T const& a, U const& b) noexcept
+  namespace detail
   {
-    if constexpr( scalar_value<T> && scalar_value<U> ) return as_logical_t<T>(a == b);
-    else return a == b;
+    template<value T, value U, callable_options O>
+    EVE_FORCEINLINE constexpr as_logical_t<common_value_t<T, U>>
+    is_equal_(EVE_REQUIRES(cpu_),
+              O const & o,
+              T const& a, U const& b) noexcept
+    {
+      using w_t =  common_value_t<T, U>;
+      if constexpr((is_logical_v<T> && is_logical_v<U>))
+      {
+        if constexpr( scalar_value<U> &&  scalar_value<T>)
+        {
+          using r_t =  common_value_t<T, U>;
+          return as_logical_t<r_t>(a == b);
+        }
+        else return a == b;
+      }
+      else if constexpr(O::contains(tolerance))
+      {
+        using w_t =  common_value_t<T, U>;
+        using e_t =  element_type_t<w_t>;
+        using r_t = as_logical_t<w_t>;
+        auto tol = [&]<typename V>(V const& t){
+          if constexpr(std::same_as<V,default_tolerance>) return 3 * eps(as<e_t>());
+          else if constexpr(integral_value<V>)            return t;
+          else                                            return convert(t,as<e_t>());
+        }(o[tolerance]);
+        if constexpr(integral_value<decltype(tol)>)
+          return if_else(nb_values(a, b) <= tol, true_(as<r_t>()), false_(as<r_t>())) ;
+        else
+        {
+          return is_less_equal(dist[pedantic](a, b), tol * max(eve::abs(a), eve::abs(b)));
+        }
+      }
+      else if constexpr(O::contains(numeric2))
+      {
+        auto tmp = is_equal(a, b);
+        using r_t =  common_value_t<T, U>;
+        if constexpr( floating_value<r_t> )
+          return tmp || (is_nan(a) && is_nan(b));
+        else
+          return tmp;
+      }
+      else
+      {
+        if constexpr( scalar_value<U> &&  scalar_value<T>)
+        {
+          using r_t =  common_value_t<T, U>;
+          return as_logical_t<r_t>(a == b);
+        }
+        else
+          return a == b;
+      }
+    }
   }
-
-  // -----------------------------------------------------------------------------------------------
-  // logical masked case
-  template<conditional_expr C, value U, value V>
-  EVE_FORCEINLINE auto is_equal_(EVE_SUPPORTS(cpu_), C const& cond, U const& u, V const& v) noexcept
-  {
-    return logical_mask_op(cond, is_equal, u, v);
-  }
-}
 }
 
 #if defined(EVE_INCLUDE_X86_HEADER)
--- test/unit/module/core/is_equal.cpp
+++ test/unit/module/core/is_equal.cpp
@@ -26,15 +26,14 @@ TTS_CASE_TPL("Check return types of eve::is_equal(simd)", eve::test::simd::all_t
 
   if constexpr( eve::floating_value<T> )
   {
-    TTS_EXPR_IS(eve::numeric(eve::is_equal)(T(), T()), logical<T>);
-    TTS_EXPR_IS(eve::numeric(eve::is_equal)(v_t(), v_t()), logical<v_t>);
-    TTS_EXPR_IS(eve::numeric(eve::is_equal)(T(), v_t()), logical<T>);
-    TTS_EXPR_IS(eve::numeric(eve::is_equal)(v_t(), T()), logical<T>);
-
-    TTS_EXPR_IS(eve::almost(eve::is_equal)(T(), T()), logical<T>);
-    TTS_EXPR_IS(eve::almost(eve::is_equal)(v_t(), v_t()), logical<v_t>);
-    TTS_EXPR_IS(eve::almost(eve::is_equal)(T(), v_t()), logical<T>);
-    TTS_EXPR_IS(eve::almost(eve::is_equal)(v_t(), T()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::numeric](T(), T()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::numeric](v_t(), v_t()), logical<v_t>);
+    TTS_EXPR_IS(eve::is_equal[eve::numeric](T(), v_t()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::numeric](v_t(), T()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::tolerant](T(), T()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::tolerant](v_t(), v_t()), logical<v_t>);
+    TTS_EXPR_IS(eve::is_equal[eve::tolerant](T(), v_t()), logical<T>);
+    TTS_EXPR_IS(eve::is_equal[eve::tolerant](v_t(), T()), logical<T>);
   }
 };
 
@@ -45,7 +44,7 @@ TTS_CASE_WITH(
     "Check behavior of eve::is_equal(simd)",
     eve::test::simd::all_types,
     tts::generate(tts::ramp(0), tts::reverse_ramp(4, 2), tts::logicals(0, 3), tts::logicals(1, 2)))
-<typename T, typename M>(T const& a0, T const& a1, M const& l0, M const& l1)
+<typename T, typename M>(T const& a0, T const& a1, M const& l0, M const& )
 {
   using eve::detail::map;
   using v_t = eve::element_type_t<T>;
@@ -58,8 +57,8 @@ TTS_CASE_WITH(
             map([](auto e) -> eve::logical<v_t> { return e == v_t(1); }, a0));
   TTS_EQUAL(eve::is_equal(v_t(14), a1),
             map([](auto e) -> eve::logical<v_t> { return e == v_t(14); }, a1));
-  TTS_EQUAL(eve::is_equal(l0, l1),
-            map([](auto e, auto f) -> eve::logical<v_t> { return e == f; }, l0, l1));
+//   TTS_EQUAL(eve::is_equal(l0, l1),
+//             map([](auto e, auto f) -> eve::logical<v_t> { return e == f; }, l0, l1));
   TTS_EQUAL(eve::is_equal[l0](a0, a1),
             eve::if_else(l0, eve::is_equal(a0, a1), eve::false_(eve::as(a0))));
 };
@@ -73,29 +72,24 @@ TTS_CASE_TPL("Check behavior of eve::is_equal(simd)", eve::test::simd::ieee_real
   using eve::as;
   using eve::detail::map;
   using v_t  = eve::element_type_t<T>;
-  using ui_t = eve::as_integer_t<T, unsigned>;
+  using ui_t = eve::as_integer_t<v_t, unsigned>;
 
   auto cases = tts::limits(tgt);
   TTS_EQUAL(eve::is_equal(cases.nan, cases.nan), eve::false_(as<T>()));
-  TTS_EQUAL(eve::numeric(eve::is_equal)(cases.nan, cases.nan), eve::true_(as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(1 + eve::eps(as<T>())), T(1), eve::eps(as<T>())),
-            eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(1 + 2 * eve::eps(as<T>())), T(1), eve::eps(as<T>())),
-            eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(1 + eve::eps(as<T>())), v_t(1), eve::eps(as<T>())),
-            eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(3), T(1), eve::eps(as<T>())), eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(3), v_t(1), eve::eps(as<T>())), eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(1), T(1), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(1), v_t(1), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(3), T(1), ui_t(3)), eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(T(3), v_t(1), ui_t(3)), eve::false_(eve::as<T>()));
-  auto md = eve::mindenormal(eve::as<v_t>());
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(md), T(0), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(2 * md), T(0), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(3 * md), T(0), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(4 * md), T(0), ui_t(3)), eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(1), v_t(1), ui_t(3)), eve::true_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(3), T(1), ui_t(3)), eve::false_(eve::as<T>()));
-  TTS_EQUAL(eve::almost(eve::is_equal)(v_t(3), v_t(1), ui_t(3)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::numeric](cases.nan, cases.nan), eve::true_(as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance =  eve::eps(as<T>())](T(1 + eve::eps(as<T>())), T(1)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance =  eve::eps(as<T>())](T(1 + 2 * eve::eps(as<T>())), T(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance =  eve::eps(as<T>())](T(1 + eve::eps(as<T>())), v_t(1)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance =  eve::eps(as<T>())](T(3), T(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance =  eve::eps(as<T>())](T(3), v_t(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](T(1), T(1)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](T(1), v_t(1)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](T(3), T(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](T(3), v_t(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](eve::next(T(0)), T(0)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](eve::next(T(0), 3), T(0)), eve::true_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](eve::next(T(0), 4), T(0)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](v_t(1), v_t(1)), eve::true_(eve::as<v_t>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](v_t(3), T(1)), eve::false_(eve::as<T>()));
+  TTS_EQUAL(eve::is_equal[eve::tolerance = ui_t(3)](v_t(3), v_t(1)), eve::false_(eve::as<v_t>()));
 };
