70b056a57be50d405c9939c3e1195e06278ef555
jtlap <jtlapreste@gmail.com>
2024-04-13 15:44:21 +0200

    fam

--- include/eve/module/core/regular/impl/simd/x86/fam.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fam.hpp
@@ -17,39 +17,40 @@ namespace eve::detail
 {
 // -----------------------------------------------------------------------------------------------
 // Masked case
-template<conditional_expr C, arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE wide<T, N>
-                fam_(EVE_SUPPORTS(sse2_),
-                     C const                         &cx,
-                     wide<T, N> const                &v,
-                     wide<T, N> const                &w,
-                     wide<T, N> const                &x) noexcept requires x86_abi<abi_t<T, N>>
-{
-  constexpr auto c = categorize<wide<T, N>>();
-
-  if constexpr( C::is_complete || abi_t<T, N>::is_wide_logical )
+  template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE wide<T, N> fam_(EVE_SUPPORTS(sse2_),
+                                  C          const &cx,
+                                  O          const &opts,
+                                  wide<T, N> const &v,
+                                  wide<T, N> const &w,
+                                  wide<T, N> const &x) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    return fam_(EVE_RETARGET(cpu_), cx, v, w, x);
-  }
-  else
-  {
-    auto m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
+    constexpr auto c = categorize<wide<T, N>>();
 
-    if constexpr( !C::has_alternative )
+    if constexpr( C::is_complete || abi_t<T, N>::is_wide_logical )
     {
-      if constexpr( c == category::float32x16 ) return _mm512_mask3_fmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x8 ) return _mm512_mask3_fmadd_pd(w, x, v, m);
-      else if constexpr( c == category::float32x8 ) return _mm256_mask3_fmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x4 ) return _mm256_mask3_fmadd_pd(w, x, v, m);
-      else if constexpr( c == category::float32x8 ) return _mm128_mask3_fmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x4 ) return _mm128_mask3_fmadd_pd(w, x, v, m);
-      else return fam_(EVE_RETARGET(cpu_), cx, v, w, x);
+      return fam.behavior(cpu_{}, opts, v, w, x);
     }
     else
     {
-      auto src = alternative(cx, v, as<wide<T, N>> {});
-      return fam_(EVE_RETARGET(cpu_), cx, v, w, x);
+      auto m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
+
+      if constexpr( !C::has_alternative )
+      {
+        if constexpr( c == category::float32x16 ) return _mm512_mask3_fmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x8 ) return _mm512_mask3_fmadd_pd(w, x, v, m);
+        else if constexpr( c == category::float32x8 ) return _mm256_mask3_fmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x4 ) return _mm256_mask3_fmadd_pd(w, x, v, m);
+        else if constexpr( c == category::float32x8 ) return _mm128_mask3_fmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x4 ) return _mm128_mask3_fmadd_pd(w, x, v, m);
+        else return fam.behavior(cpu_{}, opts, v, w, x);
+      }
+      else
+      {
+        auto src = alternative(cx, v, as<wide<T, N>> {});
+        return fam.behavior(cpu_{}, opts, v, w, x);
+      }
     }
   }
-}
 }
