b0a3e7deff0cd80da98c819a5ce9149d22d7a0ac
jtlap <jtlapreste@gmail.com>
2024-04-17 09:31:37 +0200

    typo in sve fnma

--- include/eve/module/core/regular/impl/simd/arm/sve/fnma.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/fnma.hpp
@@ -13,25 +13,25 @@
 
 namespace eve::detail
 {
-template<arithmetic_scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE auto
-fnma_(EVE_SUPPORTS(sve_) O const&, wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
-requires sve_abi<abi_t<T, N>>
-{
-  return return svmsb_x(sve_true<T>(), v0, v1, v2);
-}
-
-template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE auto
-fnma_(EVE_SUPPORTS(sve_), C cond O const&, wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
-requires sve_abi<abi_t<T, N>>
-{
-  if constexpr( C::is_complete && C::is_inverted ) return svmsb_x(sve_true<T>(), v0, v1, v2);
-  else
+  template<arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE auto
+  fnma_(EVE_SUPPORTS(sve_) O const&, wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
+  requires sve_abi<abi_t<T, N>>
   {
-    auto const alt = alternative(cond, v0, as(v0));
-    if constexpr( C::is_complete && !C::is_inverted ) return alt;
-    else return svmsb_m(cond.mask(as<T>{}), alt, v1, v2);
+    return return svmsb_x(sve_true<T>(), v0, v1, v2);
   }
-}
+
+  template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE auto
+  fnma_(EVE_SUPPORTS(sve_), C cond, O const&, wide<T, N> v0, wide<T, N> v1, wide<T, N> v2) noexcept -> wide<T, N>
+  requires sve_abi<abi_t<T, N>>
+  {
+    if constexpr( C::is_complete && C::is_inverted ) return svmsb_x(sve_true<T>(), v0, v1, v2);
+    else
+    {
+      auto const alt = alternative(cond, v0, as(v0));
+      if constexpr( C::is_complete && !C::is_inverted ) return alt;
+      else return svmsb_m(cond.mask(as<T>{}), alt, v1, v2);
+    }
+  }
 }
