be95bcfb27e6a5cbf626b2097e0212551a17094f
Joel Falcou <joel.falcou@lri.fr>
2024-04-21 18:09:59 +0200

    Fix promote

--- include/eve/module/core/decorator/core.hpp
+++ include/eve/module/core/decorator/core.hpp
@@ -35,16 +35,16 @@ namespace eve
   struct kind_1_mode          {};
   struct kind_2_mode          {};
   struct p_kind_mode          {};
+  struct promote_mode         {};
   struct q_kind_mode          {};
+  struct spherical_mode       {};
 
   struct downward_mode    {};
   struct numeric_mode     {};
   struct pedantic_mode    {};
-  struct promote_mode     {};
   struct raw_mode         {};
   struct regular_mode     {};
   struct saturated_mode   {};
-  struct spherical_mode   {};
   struct successor_mode   {};
   struct to_nearest_mode  {};
   struct toward_zero_mode {};
@@ -57,15 +57,15 @@ namespace eve
   [[maybe_unused]] inline constexpr auto kind_2           = ::rbr::flag( kind_2_mode{}          );
   [[maybe_unused]] inline constexpr auto p_kind           = ::rbr::flag( p_kind_mode{}          );
   [[maybe_unused]] inline constexpr auto q_kind           = ::rbr::flag( q_kind_mode{}          );
+  [[maybe_unused]] inline constexpr auto spherical        = ::rbr::flag( spherical_mode{}       );
+  [[maybe_unused]] inline constexpr auto promote          = ::rbr::flag( promote_mode{}         );
 
   [[maybe_unused]] inline constexpr auto downward2    = ::rbr::flag( downward_mode{}    );
   [[maybe_unused]] inline constexpr auto numeric2     = ::rbr::flag( numeric_mode{}     );
   [[maybe_unused]] inline constexpr auto pedantic2    = ::rbr::flag( pedantic_mode{}    );
-  [[maybe_unused]] inline constexpr auto promote2     = ::rbr::flag( promote_mode{}     );
   [[maybe_unused]] inline constexpr auto raw2         = ::rbr::flag( raw_mode{}         );
   [[maybe_unused]] inline constexpr auto regular2     = ::rbr::flag( regular_mode{}     );
   [[maybe_unused]] inline constexpr auto saturated2   = ::rbr::flag( saturated_mode{}   );
-  [[maybe_unused]] inline constexpr auto spherical2   = ::rbr::flag( spherical_mode{}   );
   [[maybe_unused]] inline constexpr auto successor2   = ::rbr::flag( successor_mode{}   );
   [[maybe_unused]] inline constexpr auto to_nearest2  = ::rbr::flag( to_nearest_mode{}  );
   [[maybe_unused]] inline constexpr auto toward_zero2 = ::rbr::flag( toward_zero_mode{} );
@@ -77,16 +77,16 @@ namespace eve
   struct kind_1_option          : detail::exact_option<kind_1>          {};
   struct kind_2_option          : detail::exact_option<kind_2>          {};
   struct p_kind_option          : detail::exact_option<p_kind>          {};
+  struct promote_option         : detail::exact_option<promote>         {};
   struct q_kind_option          : detail::exact_option<q_kind>          {};
+  struct spherical_option       : detail::exact_option<spherical>       {};
 
   struct downward_option     : detail::exact_option<downward2>    {};
   struct numeric_option      : detail::exact_option<numeric2>     {};
   struct pedantic_option     : detail::exact_option<pedantic2>    {};
-  struct promote_option      : detail::exact_option<promote2>     {};
   struct raw_option          : detail::exact_option<raw2>         {};
   struct regular_option      : detail::exact_option<regular2>     {};
   struct saturated_option    : detail::exact_option<saturated2>   {};
-  struct spherical_option    : detail::exact_option<spherical2>   {};
   struct successor_option    : detail::exact_option<successor2>   {};
   struct to_nearest_option   : detail::exact_option<to_nearest2>  {};
   struct toward_zero_option  : detail::exact_option<toward_zero2> {};
@@ -98,11 +98,9 @@ namespace eve
   inline constexpr auto as_option(downward_type     const&) { return downward2;     }
   inline constexpr auto as_option(numeric_type      const&) { return numeric2;      }
   inline constexpr auto as_option(pedantic_type     const&) { return pedantic2;     }
-  inline constexpr auto as_option(promote_type      const&) { return promote2;      }
   inline constexpr auto as_option(raw_type          const&) { return raw2;          }
   inline constexpr auto as_option(regular_type      const&) { return regular2;      }
   inline constexpr auto as_option(saturated_type    const&) { return saturated2;    }
-  inline constexpr auto as_option(spherical_type    const&) { return spherical2;    }
   inline constexpr auto as_option(successor_type    const&) { return successor2;    }
   inline constexpr auto as_option(to_nearest_type   const&) { return to_nearest2;   }
   inline constexpr auto as_option(toward_zero_type  const&) { return toward_zero2;  }
--- include/eve/module/core/regular/fsnm.hpp
+++ include/eve/module/core/regular/fsnm.hpp
@@ -18,12 +18,12 @@ namespace eve
   struct fsnm_t : strict_elementwise_callable<fsnm_t, Options, pedantic_option, promote_option>
   {
     template<value T,  value U,  value V>
-    requires(Options::contains(promote2))
+    requires(Options::contains(promote))
       constexpr EVE_FORCEINLINE auto operator()(T a, U b, V c) const
     { return EVE_DISPATCH_CALL(a, b, c); }
 
     template<eve::value T,eve::value U,eve::value V>
-    requires(!Options::contains(promote2))
+    requires(!Options::contains(promote))
     constexpr EVE_FORCEINLINE
     common_value_t<T,U,V> operator()(T a, U b, V c) const noexcept { return EVE_DISPATCH_CALL(a,b,c); }
 
