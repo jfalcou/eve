19eb2b6f2acd3161b9b502371bf489eb34ce0054
jtlap <jtlapreste@gmail.com>
2024-04-22 11:50:44 +0200

    (un)ordered

--- include/eve/module/core/regular/impl/simd/x86/is_ordered.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_ordered.hpp
@@ -24,12 +24,12 @@ namespace eve::detail
     using l_t        = logical<wide<T, N>>;
     constexpr auto c = categorize<wide<T, N>>();
     constexpr auto m = _CMP_ORD_Q;
-    
+
     if constexpr( match(c, category::integer_) ) return true_(eve::as<l_t>());
     else if constexpr( current_api >= eve::avx512 )
     {
       using s_t = typename l_t::storage_type;
-      
+
       if constexpr( c == category::float64x8 ) return s_t {_mm512_cmp_pd_mask(a, b, m)};
       else if constexpr( c == category::float64x4 ) return s_t {_mm256_cmp_pd_mask(a, b, m)};
       else if constexpr( c == category::float64x2 ) return s_t {_mm_cmp_pd_mask(a, b, m)};
@@ -42,38 +42,29 @@ namespace eve::detail
     else if constexpr( c == category::float32x8 ) return l_t(_mm256_cmp_ps(a, b, m));
     else if constexpr( c == category::float32x4 ) return l_t(_mm_cmpord_ps(a, b));
   }
-  
+
 // -----------------------------------------------------------------------------------------------
 // masked  implementation
   template<conditional_expr C, floating_scalar_value T, typename N, callable_options O>
-  EVE_FORCEINLINE auto is_ordered_(EVE_REQUIRES(avx512_),
-                                   C          const& cx,
-                                   O          const& o,
-                                   wide<T, N> const& v,
-                                   wide<T, N> const& w) noexcept
-  -> decltype(is_ordered(v, w))
-    requires x86_abi<abi_t<T, N>>
+  EVE_FORCEINLINE  as_logical_t<wide<T, N>> is_ordered_(EVE_REQUIRES(avx512_),
+                                                        C          const& mask,
+                                                        O          const&,
+                                                        wide<T, N> const& v,
+                                                        wide<T, N> const& w) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    constexpr auto c = categorize<wide<T, N>>();
-    
-    if constexpr( C::has_alternative || C::is_complete || abi_t<T, N>::is_wide_logical )
-    {
-      return is_not_greater.behavior(cpu_{}, o, v, w);
-    }
-    else
-    {
-      auto           m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
-      constexpr auto f = to_integer(cmp_flt::ord_q);
-      
-      if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x8 )
-        return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x8 )
-        return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x4 )
-        return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x4 ) return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x2 ) return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
-    }
+    auto const            s = alternative(mask, v, as(to_logical(v)));
+    [[maybe_unused]] auto m = expand_mask(mask, as(v)).storage().value;
+    constexpr auto        c = categorize<wide<T, N>>();
+    constexpr auto        f = to_integer(cmp_flt::ord_q);
+
+    if      constexpr( C::is_complete )            return s;
+    else if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x8 )  return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x8 )  return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x4 )  return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x4 )  return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x2 )  return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
+    else return false(as<wide<T, N>>());
   }
 }
--- include/eve/module/core/regular/impl/simd/x86/is_unordered.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_unordered.hpp
@@ -47,35 +47,25 @@ namespace eve::detail
 // -----------------------------------------------------------------------------------------------
 // masked  implementation
   template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
-  EVE_FORCEINLINE auto is_unordered_(EVE_REQUIRES(avx512_),
-                                     C const& cx,
-                                     O const& o,
-                                     wide<T, N> const& v,
-                                     wide<T, N> const& w) noexcept
-  -> decltype(is_not_less_equal(v, w))
-    requires x86_abi<abi_t<T, N>>
+  EVE_FORCEINLINE as_logical_t<wide<T, N>> is_unordered_(EVE_REQUIRES(avx512_),
+                                                         C const& mask,
+                                                         O const& o,
+                                                         wide<T, N> const& v,
+                                                         wide<T, N> const& w) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    constexpr auto c = categorize<wide<T, N>>();
+    auto const            s = alternative(mask, v, as(to_logical(v)));
+    [[maybe_unused]] auto m = expand_mask(mask, as(v)).storage().value;
+    constexpr auto        c = categorize<wide<T, N>>();
+    constexpr auto f = to_integer(cmp_flt::unord_q);
 
-    if constexpr( C::has_alternative || C::is_complete || abi_t<T, N>::is_wide_logical )
-    {
-      return is_unordered.behavior(cpu_{}, o, v, w);
-    }
-    else
-    {
-      auto           m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
-      constexpr auto f = to_integer(cmp_flt::unord_q);
-
-      if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x8 )
-        return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x8 )
-        return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x4 )
-        return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x4 ) return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x2 ) return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
-      else return is_less[cx](w, v);
-    }
+    if      constexpr( C::is_complete )            return s;
+    else if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x8 )  return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x8 )  return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x4 )  return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x4 )  return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x2 )  return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
+    else return true_(as<wide<T, N>>());
   }
 }
