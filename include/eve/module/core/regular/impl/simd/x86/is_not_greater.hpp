12b626575eabe9b101b0b5882fe1432584b5b3fa
jtlap <jtlapreste@gmail.com>
2024-04-22 11:14:45 +0200

    is_not_greater

--- include/eve/module/core/regular/impl/simd/x86/is_not_greater.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_not_greater.hpp
@@ -29,7 +29,7 @@ namespace eve::detail
     using l_t        = logical<wide<T, N>>;
     constexpr auto c = categorize<wide<T, N>>();
     constexpr auto m = _CMP_NGT_UQ;
-    
+
     if constexpr( O::contains(definitely2))
     {
       return is_not_greater.behavior(cpu_{}, opts, a, b);
@@ -39,7 +39,7 @@ namespace eve::detail
       if constexpr( current_api >= eve::avx512 )
       {
         using s_t = typename l_t::storage_type;
-        
+
         if constexpr( c == category::float64x8 ) return s_t {_mm512_cmp_pd_mask(a, b, m)};
         else if constexpr( c == category::float64x4 ) return s_t {_mm256_cmp_pd_mask(a, b, m)};
         else if constexpr( c == category::float64x2 ) return s_t {_mm_cmp_pd_mask(a, b, m)};
@@ -54,38 +54,29 @@ namespace eve::detail
       else return is_less_equal(b, a);
     }
   }
+
 // -----------------------------------------------------------------------------------------------
 // masked  implementation
   template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
-  EVE_FORCEINLINE auto
-  is_not_greater_(EVE_SUPPORTS(avx512_),
-                  C          const& cx,
-                  O          const& opts,
-                  wide<T, N> const& v,
-                  wide<T, N> const& w) noexcept
-  -> decltype(is_not_greater(v, w)) requires x86_abi<abi_t<T, N>>
+  EVE_FORCEINLINE as_logical_t<wide<T, N>> is_not_greater_(EVE_SUPPORTS(avx512_),
+                                                           C          const& mask,
+                                                           O          const& o,
+                                                           wide<T, N> const& v,
+                                                           wide<T, N> const& w) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    constexpr auto c = categorize<wide<T, N>>();
-    
-    if constexpr( C::has_alternative || C::is_complete || abi_t<T, N>::is_wide_logical  || O::contains(definitely2))
-    {
-      return is_not_greater.behavior(cpu_{}, opts, v, w);
-    }
-    else
-    {
-      auto           m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
-      constexpr auto f = to_integer(cmp_flt::ngt_uq);
-      
-      if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x8 )
-        return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x8 )
-        return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x4 )
-        return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
-      else if constexpr( c == category::float32x4 ) return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
-      else if constexpr( c == category::float64x2 ) return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
-      else return is_less_equal[cx](v, w);
-    }
+    auto const            s = alternative(mask, v, as(to_logical(v)));
+    [[maybe_unused]] auto m = expand_mask(mask, as(v)).storage().value;
+    constexpr auto        c = categorize<wide<T, N>>();
+    constexpr auto        f = to_integer(cmp_flt::ngt_uq);
+
+    if      constexpr( C::is_complete )            return s;
+    else if constexpr( c == category::float32x16 ) return mask16 {_mm512_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x8 )  return mask8 {_mm512_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x8 )  return mask8 {_mm256_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x4 )  return mask8 {_mm256_mask_cmp_pd_mask(m, v, w, f)};
+    else if constexpr( c == category::float32x4 )  return mask8 {_mm_mask_cmp_ps_mask(m, v, w, f)};
+    else if constexpr( c == category::float64x2 )  return mask8 {_mm_mask_cmp_pd_mask(m, v, w, f)};
+    else return is_less_equal[o](v, w);
   }
 }
