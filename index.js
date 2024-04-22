f4ade9a07c8d593e1e6441779e9925e9cb924329
jfalcou <jfalcou@users.noreply.github.com>
2024-04-22 19:38:19 +0000

    deploy: 99f533de94c9591dd5eb61a2a41a68e99d75889b

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var index =
 [
-    [ "Video materials", "index.html#autotoc_md101", null ],
-    [ "Bibliographic References", "index.html#autotoc_md102", null ]
+    [ "Video materials", "index.html#autotoc_md85", null ],
+    [ "Bibliographic References", "index.html#autotoc_md86", null ]
 ];
--- search/variables_d.js
+++ search/variables_d.js
@@ -20,6 +20,5 @@ var searchData=
   ['none_17',['none',['../group__core__reduction_ga6bac192090779b76bc7ffe5425fcabc8.html#ga6bac192090779b76bc7ffe5425fcabc8',1,'eve']]],
   ['none_5fof_18',['none_of',['../group__algos_ga0385dc0d44b416d165ee39b191d00924.html#ga0385dc0d44b416d165ee39b191d00924',1,'eve::algo']]],
   ['nth_5fprime_19',['nth_prime',['../group__combinatorial_gab810f120aa46b31a1b6e437d337e460c.html#gab810f120aa46b31a1b6e437d337e460c',1,'eve']]],
-  ['nthroot_20',['nthroot',['../group__math__exp_gaa0b5a64467474bf1d37bf7130958946a.html#gaa0b5a64467474bf1d37bf7130958946a',1,'eve']]],
-  ['numeric_21',['numeric',['../group__core__decorators_ga9e4172d6b835dc7d61072c41ea61a634.html#ga9e4172d6b835dc7d61072c41ea61a634',1,'eve']]]
+  ['nthroot_20',['nthroot',['../group__math__exp_gaa0b5a64467474bf1d37bf7130958946a.html#gaa0b5a64467474bf1d37bf7130958946a',1,'eve']]]
 ];
