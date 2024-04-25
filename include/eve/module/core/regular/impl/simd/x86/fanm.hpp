8af225ac62fb23c21571bb50fed94226e068f7c3
jtlap <jtlapreste@gmail.com>
2024-04-25 15:45:10 +0200

    Fix missing X86 FMA related optimisations

--- include/eve/module/core/regular/impl/simd/x86/fanm.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fanm.hpp
@@ -8,48 +8,47 @@
 #pragma once
 
 #include <eve/concept/value.hpp>
+#include <eve/detail/abi.hpp>
 #include <eve/detail/category.hpp>
-#include <eve/detail/implementation.hpp>
-
-#include <type_traits>
+#include <eve/forward.hpp>
 
 namespace eve::detail
 {
 // -----------------------------------------------------------------------------------------------
 // Masked case
-template<conditional_expr C, arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE wide<T, N>
-                fanm_(EVE_SUPPORTS(sse2_),
-                      C const                         &cx,
-                      wide<T, N> const                &v,
-                      wide<T, N> const                &w,
-                      wide<T, N> const                &x) noexcept requires x86_abi<abi_t<T, N>>
-{
-  constexpr auto c = categorize<wide<T, N>>();
-
-  if constexpr( C::is_complete || abi_t<T, N>::is_wide_logical )
+  template<conditional_expr C, arithmetic_scalar_value T, typename N>
+  EVE_FORCEINLINE wide<T, N> fanm_(EVE_SUPPORTS(avx512_),
+                                   C const            &mask,
+                                   wide<T, N> const   &v,
+                                   wide<T, N> const   &w,
+                                   wide<T, N> const   &x) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    return fanm_(EVE_RETARGET(cpu_), cx, v, w, x);
-  }
-  else
-  {
-    auto m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
+    // NOTE: As those masked version are at the AVX512 level, they will always uses a variant of
+    // hardware VMADD, thus ensuring the pedantic behavior by default, hence why we don't care about
+    // PEDANTIC. As usual, we don't care about PROMOTE as we only accept similar types.
 
-    if constexpr( !C::has_alternative )
+    if      constexpr( C::is_complete )
+      return alternative(mask, v, as(v));
+    else if constexpr( !C::has_alternative )
     {
-      if constexpr( c == category::float32x16 ) return _mm512_mask3_fnmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x8 ) return _mm512_mask3_fnmadd_pd(w, x, v, m);
-      else if constexpr( c == category::float32x8 ) return _mm256_mask3_fnmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x4 ) return _mm256_mask3_fnmadd_pd(w, x, v, m);
-      else if constexpr( c == category::float32x8 ) return _mm128_mask3_fnmadd_ps(w, x, v, m);
-      else if constexpr( c == category::float64x4 ) return _mm128_mask3_fnmadd_pd(w, x, v, m);
-      else return fanm_(EVE_RETARGET(cpu_), cx, v, w, x);
-    }
-    else
-    {
-      auto src = alternative(cx, v, as<wide<T, N>> {});
-      return fanm_(EVE_RETARGET(cpu_), cx, v, w, x);
+      constexpr auto              c = categorize<wide<T, N>>();
+      [[maybe_unused]] auto const m = expand_mask(mask, as(v)).storage().value;
+
+      if constexpr( !C::has_alternative )
+      {
+        if      constexpr( c == category::float32x16) return _mm512_mask3_fnmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x8 ) return _mm512_mask3_fnmadd_pd(w, x, v, m);
+        else if constexpr( c == category::float32x8 ) return _mm256_mask3_fnmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x4 ) return _mm256_mask3_fnmadd_pd(w, x, v, m);
+        else if constexpr( c == category::float32x8 ) return _mm128_mask3_fnmadd_ps(w, x, v, m);
+        else if constexpr( c == category::float64x4 ) return _mm128_mask3_fnmadd_pd(w, x, v, m);
+        // No rounding issue with integers, so we just mask over regular FMA
+        else
+          return if_else(mask, eve::fanm(v, w, x), v);
+      }
+      else
+        return if_else(mask, eve::fanm(v, w, x), alternative(mask, v, as(v)));
     }
   }
-}
 }
