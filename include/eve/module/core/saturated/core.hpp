6c9599a85b8bda6b592fcfe1213c5382f99faf49
jtlap <jtlapreste@gmail.com>
2024-04-28 09:34:27 +0200

    minus oneminus + cleaning

--- include/eve/module/core/saturated/core.hpp
+++ include/eve/module/core/saturated/core.hpp
@@ -11,9 +11,7 @@
 #include <eve/module/core/saturated/add.hpp>
 #include <eve/module/core/saturated/convert.hpp>
 #include <eve/module/core/saturated/div.hpp>
-#include <eve/module/core/saturated/minus.hpp>
 #include <eve/module/core/saturated/mul.hpp>
-#include <eve/module/core/saturated/oneminus.hpp>
 #include <eve/module/core/saturated/plus.hpp>
 #include <eve/module/core/saturated/sqr.hpp>
 #include <eve/module/core/saturated/sub.hpp>
