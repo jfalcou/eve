71732af47e67b062a1bc2eab68fd83bc2d51f101
jtlap <jtlapreste@gmail.com>
2024-04-28 09:57:14 +0200

    cleaning

--- include/eve/module/core/saturated/core.hpp
+++ include/eve/module/core/saturated/core.hpp
@@ -13,5 +13,4 @@
 #include <eve/module/core/saturated/div.hpp>
 #include <eve/module/core/saturated/mul.hpp>
 #include <eve/module/core/saturated/plus.hpp>
-#include <eve/module/core/saturated/sqr.hpp>
 #include <eve/module/core/saturated/sub.hpp>
