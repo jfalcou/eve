86d7b6ed87314a78d76b70a7dd42e607b3cbac36
jtlap <jtlapreste@gmail.com>
2024-04-28 08:30:22 +0200

    cleaning

--- include/eve/module/core/pedantic/core.hpp
+++ include/eve/module/core/pedantic/core.hpp
@@ -8,8 +8,6 @@
 #pragma once
 
 #include <eve/module/core/pedantic/diff_of_prod.hpp>
-#include <eve/module/core/pedantic/negatenz.hpp>
 #include <eve/module/core/pedantic/rem.hpp>
 #include <eve/module/core/pedantic/rsqrt.hpp>
-#include <eve/module/core/pedantic/signnz.hpp>
 #include <eve/module/core/pedantic/sum_of_prod.hpp>
