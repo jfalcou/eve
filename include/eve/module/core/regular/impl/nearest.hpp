dc94fbfc008995a5d238ecea02e64dbd866f9756
jtlap <jtlapreste@gmail.com>
2024-05-02 21:33:59 +0200

    nearest pow_abs

--- include/eve/module/core/regular/impl/nearest.hpp
+++ include/eve/module/core/regular/impl/nearest.hpp
@@ -47,9 +47,9 @@ namespace eve::detail
   {
     auto z = nearest(a0);
     if constexpr(unsigned_value<U>)
-      return uint_(z);
+      return convert(z, uint_from<T>());
     else if constexpr(signed_integral_value<U>)
-      return int_(z);
+      return convert(z, int_from<T>());
     else
       return z;
   }
