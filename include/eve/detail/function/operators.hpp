f949784b011a991564f9677b7b06a84659540fe6
jtlap <jtlapreste@gmail.com>
2024-05-09 08:42:21 +0200

    mul suppressed from operators

--- include/eve/detail/function/operators.hpp
+++ include/eve/detail/function/operators.hpp
@@ -33,19 +33,6 @@ namespace eve::detail
   }
 
   template<scalar_value T>
-  EVE_FORCEINLINE  auto  mul_(EVE_SUPPORTS(cpu_), T a, T b) noexcept
-  {
-    return static_cast<T>(a * b);
-  }
-
-  template<value T, value U>
-  EVE_FORCEINLINE auto
-  mul_(EVE_SUPPORTS(cpu_), T const &a, U const &b) noexcept requires compatible_values<T, U>
-  {
-    return a * b;
-  }
-
-  template<scalar_value T>
   EVE_FORCEINLINE  auto  div_(EVE_SUPPORTS(cpu_), T a, T b) noexcept
   {
     return static_cast<T>(a / b);
