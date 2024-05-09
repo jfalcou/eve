0af8c902dd2643dcbd4ffe035f76a72d88bb593e
jtlap <jtlapreste@gmail.com>
2024-05-09 11:02:56 +0200

    mul in multi_div

--- include/eve/module/core/detail/multi_div.hpp
+++ include/eve/module/core/detail/multi_div.hpp
@@ -18,13 +18,13 @@ namespace eve::detail
 {
   template<decorator D, value T0, value T1, value... Ts>
   auto
-  div_(EVE_SUPPORTS(cpu_), D const& d, T0 a0, T1 a1, Ts... args) noexcept
+  div_(EVE_SUPPORTS(cpu_), O const& d, T0 a0, T1 a1, Ts... args) noexcept
   -> common_value_t<T0, T1, Ts...>
   {
     using r_t = common_value_t<T0, T1, Ts...>;
     r_t that(a1);
-    that = d(mul)(that, r_t(args)...);
-    EVE_ASSERT(eve::all(is_nez(that)), "[eve] D()(div) - 0/0 is undefined");
+    that = mul[d](that, r_t(args)...);
+    EVE_ASSERT(eve::all(is_nez(that)), "[eve] div[O] - 0/0 is undefined");
     return (D()(div))(r_t(a0), that);
   }
 }
