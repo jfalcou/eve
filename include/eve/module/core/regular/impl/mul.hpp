749ec4549dd23f84b6f4f922118eb8fd1069e2e1
jtlap <jtlapreste@gmail.com>
2024-05-09 08:20:28 +0200

    mul

--- include/eve/module/core/regular/impl/mul.hpp
+++ include/eve/module/core/regular/impl/mul.hpp
@@ -7,64 +7,142 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/function/conditional.hpp>
-#include <eve/traits/common_value.hpp>
+
+
 #include <eve/concept/value.hpp>
-#include <type_traits>
+#include <eve/module/core/constant/valmax.hpp>
+#include <eve/module/core/constant/valmin.hpp>
+#include <eve/module/core/constant/zero.hpp>
+#include <eve/module/core/decorator/saturated.hpp>
+#include <eve/module/core/regular/abs.hpp>
+#include <eve/module/core/regular/all.hpp>
+#include <eve/module/core/regular/bit_xor.hpp>
+#include <eve/module/core/regular/bitofsign.hpp>
+#include <eve/module/core/regular/max.hpp>
+#include <eve/module/core/regular/min.hpp>
+#include <eve/module/core/regular/saturate.hpp>
+#include <eve/module/core/regular/sign.hpp>
+#include <eve/module/core/regular/min.hpp>
 
 namespace eve::detail
 {
-//================================================================================================
-//== Masked case
-//================================================================================================
-template<conditional_expr C, value U, value... V>
-EVE_FORCEINLINE auto
-mul_(EVE_SUPPORTS(cpu_),
-     C const& cond,
-     U    t,
-     V... f) noexcept
-{
-  return mask_op(cond, eve::mul, t, f...);
-}
+  template<typename T, callable_options O>
+  EVE_FORCEINLINE constexpr T mul_(EVE_REQUIRES(cpu_), O const&, T a, T b) noexcept
+  {
+    if constexpr(O::contains(saturated2) && integral_value<T>)
+    {
+      if constexpr( signed_integral_value<T> )
+      {
+        if constexpr(scalar_value<T>)
+        {
+          if constexpr( sizeof(T) <= 2 )
+          {
+            using up_t = upgrade_t<T>;
+            return static_cast<T>(saturate(static_cast<up_t>(a) * static_cast<up_t>(b), as<T>()));
+          }
+          else if constexpr( sizeof(T) == 4 )
+          {
+            using un_t = std::make_unsigned_t<T>;
+            using up_t = int64_t;
+            enum Sizee { value = sizeof(T) * 8 - 1 };
 
-//================================================================================================
-//==  regular N parameters
-//================================================================================================
-template<decorator D, value T0, value... Ts>
-auto
-mul_(EVE_SUPPORTS(cpu_), D const& d, T0 a0, Ts... args) noexcept
-{
-  common_value_t<T0, Ts...> that(a0);
-  ((that = d(mul)(that, args)), ...);
-  return that;
-}
+            up_t res  = up_t(a) * up_t(b);
+            un_t res2 = (un_t(a ^ b) >> Sizee::value) + valmax(eve::as<T>());
+            T    hi   = (res >> (Sizee::value + 1));
+            T    lo   = res;
+            if( hi != (lo >> Sizee::value) ) res = res2;
+            return static_cast<T>(res);
+          }
+          else if constexpr( sizeof(T) == 8 )
+          {
+            using un_t = std::make_unsigned_t<T>;
+            if( b == 0 || a == 0 ) return zero(eve::as<T>());
+            T sgn = bit_xor(bitofsign(a), bitofsign(b));
+            if( b == valmin(eve::as<T>()) || a == valmin(eve::as<T>()) )
+              return sgn ? valmin(eve::as<T>()) : valmax(eve::as<T>());
+            un_t aa  = eve::abs(a);
+            un_t bb  = eve::abs(b);
+            auto aux = [sgn](const T& mini, const T& maxi, const un_t& amini, const un_t& amaxi)
+              {
+                un_t z = valmax(eve::as<T>()) / amaxi;
+                return (z < amini) ? (sgn ? valmin(eve::as<T>()) : valmax(eve::as<T>())) : mini * maxi;
+              };
+            if( bb >= aa ) return aux(a, b, aa, bb);
+            else return aux(b, a, bb, aa);
+          }
+        }
+        else // simd
+        {
+          using elt_t = element_type_t<T>;
+          if constexpr( floating_value<T> ) { return a * b; }
+          else if constexpr( integral_value<T> )
+          {
+            if constexpr( sizeof(elt_t) <= 4 )
+            {
+              using sup_t = upgrade_t<elt_t>;
+              auto z      = mul(convert(a, as<sup_t>()), convert(b, as<sup_t>()));
+              auto s      = saturate(z, as<elt_t>());
+              return convert(s, as<elt_t>());
+            }
+            else
+            {
+              auto that = map(eve::mul[saturated], a, b);
+              return that;
+            }
+          }
+        }
+      }
+      else //unsigned
+      {
+        if constexpr(scalar_value<T>)
+        {
+          if constexpr( sizeof(T) <= 4 )
+          {
+            using up_t = upgrade_t<T>;
+            up_t res   = up_t(a) * up_t(b);
+            return (res > valmax(eve::as<T>())) ? valmax(eve::as<T>()) : static_cast<T>(res);
+          }
+          else
+          {
+            auto aux = [](const T& mini, const T& maxi)
+              {
+                T z = valmax(eve::as<T>()) / maxi;
+                return (z < mini) ? valmax(eve::as<T>()) : mini * maxi;
+              };
+            if( b == 0 || a == 0 ) return zero(eve::as<T>());
+            if( b >= a ) return aux(a, b);
+            else return aux(b, a);
+          }
+        }
+        else //simd
+        {
+          using elt_t = element_type_t<T>;
+          if constexpr( sizeof(elt_t) <= 4 )
+          {
+            using sup_t = upgrade_t<elt_t>;
+            auto z      = mul(convert(a, as<sup_t>()), convert(b, as<sup_t>()));
+            auto s      = saturate(z, as<elt_t>());
+            return convert(s, as<elt_t>());
+          }
+          else
+          {
+            auto that = map(saturated(eve::mul), a, b);
+            return that;
+          }
+        }
+      }
+    }
+    else
+    {
+      return a *= b;
+    }
+  }
 
-template<value T0, value... Ts>
-auto
-mul_(EVE_SUPPORTS(cpu_), T0 a0, Ts... args) noexcept
--> common_value_t<T0, Ts...>
-{
-  auto that((a0 * ... * args));
-  return that;
-}
-
-//================================================================================================
-// tuples
-//================================================================================================
-template<kumi::non_empty_product_type Ts>
-auto
-mul_(EVE_SUPPORTS(cpu_), Ts tup) noexcept
-{
-  if constexpr( kumi::size_v<Ts> == 1) return get<0>(tup);
-  else return kumi::apply( [&](auto... m) { return mul(m...); }, tup);
-}
-
-template<decorator D, kumi::non_empty_product_type Ts>
-auto
-mul_(EVE_SUPPORTS(cpu_), D const & d, Ts tup) noexcept
-{
-  if constexpr( kumi::size_v<Ts> == 1) return get<0>(tup);
-  else return kumi::apply( [&](auto... m) { return d(mul)(m...); }, tup);
-}
-
+  template<typename T, std::same_as<T>... Ts, callable_options O>
+  EVE_FORCEINLINE constexpr T mul_(EVE_REQUIRES(cpu_), O const & o, T r0, T r1, Ts... rs) noexcept
+  {
+    r0   = mul[o](r0,r1);
+    ((r0 = mul[o](r0,rs)),...);
+    return r0;
+  }
 }
--- include/eve/module/core/regular/impl/simd/x86/mul.hpp
+++ include/eve/module/core/regular/impl/simd/x86/mul.hpp
@@ -9,29 +9,33 @@
 
 namespace eve::detail
 {
-// -----------------------------------------------------------------------------------------------
-// Masked case
-template<conditional_expr C, arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE wide<T, N>
-                mul_(EVE_SUPPORTS(sse2_),
-                     C const                         &cx,
-                     wide<T, N> const                &v,
-                     wide<T, N> const                &w) noexcept requires x86_abi<abi_t<T, N>>
-{
-  constexpr auto c = categorize<wide<T, N>>();
 
-  if constexpr( C::is_complete || abi_t<T, N>::is_wide_logical )
+  template<conditional_expr C, typename T, typename N, callable_options O>
+  EVE_FORCEINLINE
+  wide<T, N> mul_(EVE_REQUIRES(avx512_), C cx, O const& opts, wide<T, N> v, wide<T, N> w) noexcept
+  requires x86_abi<abi_t<T, N>>
   {
-    return mul_(EVE_RETARGET(cpu_), cx, v, w);
-  }
-  else
-  {
+    constexpr auto c = categorize<wide<T, N>>();
+
     auto src = alternative(cx, v, as<wide<T, N>> {});
     auto m   = expand_mask(cx, as<wide<T, N>> {}).storage().value;
 
-    if constexpr( c == category::float32x16 ) return _mm512_mask_mul_ps(src, m, v, w);
-    else if constexpr( c == category::float64x8 ) return _mm512_mask_mul_pd(src, m, v, w);
-    else return mul_(EVE_RETARGET(cpu_), cx, v, w);
+    if constexpr(O::contains(saturated2) && integral_value<T>)
+    {
+      return mul.behavior(cpu_{}, opts, v, w);
+    }
+    else
+    {
+      if      constexpr( c == category::float32x16) return _mm512_mask_mul_ps   (src, m, v, w);
+      else if constexpr( c == category::float32x8 ) return _mm256_mask_mul_ps   (src, m, v, w);
+      else if constexpr( c == category::float32x4 ) return _mm_mask_mul_ps      (src, m, v, w);
+      else if constexpr( c == category::float64x8 ) return _mm512_mask_mul_pd   (src, m, v, w);
+      else if constexpr( c == category::float64x4 ) return _mm256_mask_mul_pd   (src, m, v, w);
+      else if constexpr( c == category::float64x2 ) return _mm_mask_mul_pd      (src, m, v, w);
+      else if constexpr( match(c,category::int32x16, category::uint32x16) ) return _mm512_mask_mul_epi32(src, m, v, w);
+      else if constexpr( match(c,category::int32x8 , category::uint32x8 ) ) return _mm256_mask_mul_epi32(src, m, v, w);
+      else if constexpr( match(c,category::int32x4 , category::uint32x4 ) ) return _mm_mask_mul_epi32   (src, m, v, w);
+      else                                                                  return mul.behavior(cpu_{}, opts, v, w);
+    }
   }
-}
 }
