bd43b8cfd9a1d3f315461fe9578817dda0a43613
jtlap <jtlapreste@gmail.com>
2024-05-11 19:30:08 +0200

    rem

--- test/unit/module/core/rem.cpp
+++ test/unit/module/core/rem.cpp
@@ -101,26 +101,14 @@ TTS_CASE_WITH("Check behavior of rem on signed types",
 <typename T>(T a0, T a1, T a2)
 {
   using eve::is_nez;
-  using eve::pedantic;
   using eve::rem;
   using eve::detail::map;
-  TTS_RELATIVE_EQUAL((rem[is_nez(a2)])(a0, a2),
-                     map([](auto e, auto f) { return is_nez(f) ? rem(e, f) : e; }, a0, a2),
-                     0.001);
-
+  a1 = eve::if_else(a1 >= 0, eve::one, a1);
   a2 = eve::if_else(a2 >= 0, eve::one, a2);
-  TTS_RELATIVE_EQUAL(
-    rem[is_nez(a2)](a0, a2), map([](auto e, auto f) { return rem(e, f); }, a0, a2), 0.001);
-
-  TTS_RELATIVE_EQUAL(
-    rem[a2 > T(64)](a0, a1),
-    map([](auto e, auto f, auto g) { return g > 64 ? rem(e, f) : e; }, a0, a1, a2),
-    0.001);
-  a1 = eve::if_else(eve::is_eqz(a1), eve::one, a1);
-  TTS_RELATIVE_EQUAL(
-    rem[a2 > T(64)](a0, a1),
-    map([](auto e, auto f, auto g) { return g > 64 ? rem(e, f) : e; }, a0, a1, a2),
-    0.001);
+  TTS_EQUAL(rem[is_nez(a2)](a0, a2), map([](auto e, auto f) { return rem(e, f); }, a0, a2));
+  TTS_EQUAL(rem[is_nez(a2)](a0, a2), map([](auto e, auto f) { return rem(e, f); }, a0, a2));
+  TTS_EQUAL(rem[a2 > T(64)](a0, a1), map([](auto e, auto f, auto g) { return g > 64 ? rem(e, f) : e; }, a0, a1, a2));
+  TTS_EQUAL(rem[a2 > T(64)](a0, a1), map([](auto e, auto f, auto g) { return g > 64 ? rem(e, f) : e; }, a0, a1, a2));
 };
 
 
@@ -133,9 +121,10 @@ TTS_CASE_WITH("Check behavior of eve::masked(eve::rem)(eve::wide)",
                             tts::randoms(eve::valmin, eve::valmax),
                             tts::logicals(0, 3)))
 <typename T, typename M>(T const& a0,
-                         T const& a1,
+                         T  a1,
                          M const& mask)
 {
-  TTS_IEEE_EQUAL(eve::rem[mask](a0, a1),
+  a1 = eve::if_else(eve::is_eqz(a1), eve::one, a1);
+  TTS_EQUAL(eve::rem[mask](a0, a1),
             eve::if_else(mask, eve::rem(a0, a1), a0));
 };
