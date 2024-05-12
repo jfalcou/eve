4b416407a5d3ae15e04eb04005d36cac85f1498e
jtlap <jtlapreste@gmail.com>
2024-05-12 12:05:31 +0200

    sve mul div

--- include/eve/module/core/regular/impl/simd/arm/sve/mul.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/mul.hpp
@@ -14,7 +14,7 @@ namespace eve::detail
 {
   template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T,N> mul_((EVE_SUPPORTS(sve_),
-                                  C          const& cx,
+                                  C          const& mask,
                                   O          const& opts,
                                   wide<T, N> const& v,
                                   wide<T, N> const& w) noexcept
