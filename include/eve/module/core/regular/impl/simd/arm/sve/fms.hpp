fdd09720f652f358cffccbdf4e93a6b2c46388b6
jtlap <jtlapreste@gmail.com>
2024-05-13 11:28:02 +0200

    fms sve

--- include/eve/module/core/regular/impl/simd/arm/sve/fms.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/fms.hpp
@@ -14,25 +14,25 @@
 
 namespace eve::detail
 {
-template<scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE auto
-fma_(EVE_REQUIRES(sve_), O const&, wide<T, N> const& a, wide<T, N> const& b, wide<T, N> const& c) noexcept -> wide<T, N>
-requires sve_abi<abi_t<T, N>>
-{
-  // We don't care about PEDANTIC as this is a proper FMA.
-  // We don't care about PROMOTE as we only accept similar types.
-  return svmsb_x(sve_true<T>(), a, b, c);
-}
+  template<scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE auto
+  fma_(EVE_REQUIRES(sve_), O const&, wide<T, N> const& a, wide<T, N> const& b, wide<T, N> const& c) noexcept -> wide<T, N>
+  requires sve_abi<abi_t<T, N>>
+  {
+    // We don't care about PEDANTIC as this is a proper FMA.
+    // We don't care about PROMOTE as we only accept similar types.
+    return svmsb_x(sve_true<T>(), a, b, c);
+  }
 
   template<conditional_expr C, scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE auto
-fma_(EVE_REQUIRES(sve_), C cond, O const&, wide<T, N> a, wide<T, N> b, wide<T, N> c) noexcept
-requires sve_abi<abi_t<T, N>>
-{
-  // We don't care about PEDANTIC as this is a proper FMA.
-  // We don't care about PROMOTE as we only accept similar types.
-  auto const alt = alternative(cond, a, as(a));
-  if constexpr( C::is_complete && !C::is_inverted ) return alt;
-  else                                              return svmsb_m(cond.mask(as<T>{}), alt, b, c);
-}
+  EVE_FORCEINLINE auto
+  fma_(EVE_REQUIRES(sve_), C cond, O const&, wide<T, N> a, wide<T, N> b, wide<T, N> c) noexcept
+  requires sve_abi<abi_t<T, N>>
+  {
+    // We don't care about PEDANTIC as this is a proper FMA.
+    // We don't care about PROMOTE as we only accept similar types.
+    auto const alt = alternative(cond, a, as(a));
+    if constexpr( C::is_complete && !C::is_inverted ) return alt;
+    else                                              return svmsb_m(cond.mask(as<T>{}), alt, b, c);
+  }
 }
