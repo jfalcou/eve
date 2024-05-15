5a27268688b7d85ff1be445b6d19be578b27eea7
jtlap <jtlapreste@gmail.com>
2024-05-15 08:45:01 +0200

    refurbishing all fma sve family

--- include/eve/module/core/regular/impl/simd/arm/sve/fsm.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/fsm.hpp
@@ -26,14 +26,13 @@ namespace eve::detail
 
   template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N> fsm_(EVE_REQUIRES(sve_),
-                                   C cond,
-                                   O const& o,
-                                   wide<T, N> a,
-                                   wide<T, N> b,
-                                   wide<T, N> c) noexcept
+                                  C cond,
+                                  O const& o,
+                                  wide<T, N> a,
+                                  wide<T, N> b,
+                                  wide<T, N> c) noexcept
   requires sve_abi<abi_t<T, N>>
   {
     -fanm[o](a, b, c);
   }
-}
 }
