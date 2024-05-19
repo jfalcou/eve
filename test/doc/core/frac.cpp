c7f418d5c2f8cdda5260e1acab1870ebbe972d8d
jtlap <jtlapreste@gmail.com>
2024-05-19 18:44:21 +0200

    frac fuzzy raw and pedantic

--- test/doc/core/frac.cpp
+++ test/doc/core/frac.cpp
@@ -8,16 +8,21 @@ using wide_ft = eve::wide<float, eve::fixed<8>>;
 int main()
 {
   wide_ft pf = {-1.0f, -1.3f, -0.0f, 0.0f,
-                 2.0f,  2.3f, eve::inf(eve::as<float>()), eve::nan(eve::as<float>())};
+                2.0f,  eve::prev(2.0f), eve::inf(eve::as<float>()), eve::nan(eve::as<float>())};
 
-  std::cout << "---- simd" << '\n'
-            << "<- pf                 = " << pf << '\n'
-            << "-> frac(pf)           = " << eve::frac(pf) << '\n'
-            << "-> frac[pf!=2.3f](pf) = " << eve::frac[pf !=  2.3f](pf) << '\n';
+  std::cout << "---- simd" << std::setprecision(8) << '\n'
+            << "<- pf                         = " << pf << '\n'
+            << "-> frac(pf)                   = " << eve::frac(pf) << '\n'
+            << "-> frac[pf!=2.3f](pf)         = " << eve::frac[pf !=  2.3f](pf) << '\n'
+            << "-> frac[pedantic](pf)         = " << eve::frac[eve::pedantic](pf)<< '\n'
+            << "-> frac[raw](pf)              = " << eve::frac[eve::raw](pf)<< '\n'
+            << "-> frac[almost](pf)           = " << eve::frac[eve::almost](pf)<< '\n'
+            << "-> frac[almost][pedantic](pf) = " << eve::frac[eve::almost][eve::pedantic](pf)<< '\n'
+    ;
 
   float xf = -327.68f;
 
-  std::cout << "---- scalar" << std::setprecision(10) << '\n'
+  std::cout << "---- scalar" << std::setprecision(8) << '\n'
             << "<- xf             = " << xf << '\n'
             << "-> frac(xf)       = " << eve::frac(xf) << '\n';
   return 0;
