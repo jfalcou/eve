d2971d5c027f4823e3cfc13bd1b98983f0fa2308
jtlap <jtlapreste@gmail.com>
2024-05-19 22:08:37 +0200

    countr_zero

--- include/eve/module/core/regular/impl/simd/arm/sve/countr_zero.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/countr_zero.hpp
@@ -16,10 +16,10 @@ namespace eve::detail
   template<unsigned_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N> countr_zero_(EVE_REQUIRES(sve_),
                                           O          const&,
-                                          wide<T, N> const& v) noexcept -> wide<T, N>
+                                          wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
-    return svrbit_x(sve_true<T>(),v))
+    return countl_zero(svrbit_x(sve_true<T>(),v)));
   }
 
   template<conditional_expr C, unsigned_scalar_value T, typename N, callable_options O>
@@ -29,11 +29,8 @@ namespace eve::detail
   requires sve_abi<abi_t<T, N>>
   {
     if constexpr( C::is_complete)
-      return svrbit_x(detail::sve_true<T>(),v));
+      return countl_zero(wide<T,N>(svrbit_x(detail::sve_true<T>(),v)));
     else
-      return svclz_m( alternative(cond, v, as(v))
-                    , expand_mask(cond, as(v))
-                    , svrbit_x(sve_true<T>(),v)
-                    );
+      return svclz_m( alternative(cond,v,as(v)), expand_mask(cond,as(v)), svrbit_x(sve_true<T>(),v));
   }
 }
