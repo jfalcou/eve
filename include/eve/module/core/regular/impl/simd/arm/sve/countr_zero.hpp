20acfd3b06addf247e02176b7230f0840e9d4eaa
jtlap <jtlapreste@gmail.com>
2024-05-20 13:46:01 +0200

    sve/countr_zero

--- include/eve/module/core/regular/impl/simd/arm/sve/countr_zero.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/countr_zero.hpp
@@ -24,14 +24,14 @@ namespace eve::detail
 
   template<conditional_expr C, unsigned_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N> countr_zero_(EVE_REQUIRES(sve_),
-                                          C          const& cond,
+                                          C          const& mask,
                                           wide<T, N> const& v) noexcept
   requires sve_abi<abi_t<T, N>>
   {
     auto const alt = alternative(mask, v, as(v));
     if constexpr( C::is_complete)
-      return alt; 
+      return alt;
     else
-      return svclz_m( alt, expand_mask(cond,as(v)), svrbit_x(sve_true<T>(),v));
+      return svclz_m( alt, expand_mask(mask,as(v)), svrbit_x(sve_true<T>(),v));
   }
 }
