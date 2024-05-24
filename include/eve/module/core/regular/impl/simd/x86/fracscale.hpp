ba630b0c2ffa6a99efb533930f8a323cf5669777
jtlap <jtlapreste@gmail.com>
2024-05-24 10:41:05 +0200

    decorator/core.hpp +frac/roundscale

--- include/eve/module/core/regular/impl/simd/x86/fracscale.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fracscale.hpp
@@ -12,8 +12,6 @@
 #include <eve/module/core/constant/true.hpp>
 #include <eve/module/core/decorator/roundings.hpp>
 
-#include <type_traits>
-
 namespace eve::detail
 {
 
@@ -23,7 +21,7 @@ namespace eve::detail
   fracscale_(EVE_REQUIRES(avx512_),
              wide<T, N> const & a0,
              O          const & o,
-             std::integral_constant<int, S> ) noexcept requires x86_abi<abi_t<T, N>>
+             eve::index_t<S> const & ) noexcept requires x86_abi<abi_t<T, N>>
   {
     return fracscale[true_(as(a0))][o](a0, S);
   }
@@ -36,33 +34,25 @@ namespace eve::detail
                                         C          const &mask,
                                         O          const &,
                                         wide<T, N> const &a0,
-                                        std::integral_constant<int, S> const &) noexcept
+                                        eve::index_t<S> const &) noexcept
   requires x86_abi<abi_t<T, N>>
   {
     auto const alt = alternative(mask, a0, as(a0));
-
     if constexpr( C::is_complete)  return alt;
     else
     {
       auto          src = alternative(mask, a0, as<wide<T, N>> {});
       auto          m   = expand_mask(mask, as<wide<T, N>> {}).storage().value;
       constexpr int s   = ((S) << 4);
+      constexpr auto c = categorize<wide<T, N>>();
+      int constexpr V = eve::rounding_mode<O>(eve::to_nearest2) & 3;
 
-      auto call = [] <int V >(auto src,  auto m, auto a0){
-        constexpr auto c = categorize<wide<T, N>>();
-
-        if      constexpr( c == category::float32x16) return _mm512_mask_reduce_ps(src, m, a0, s+V);
-        else if constexpr( c == category::float64x8 ) return _mm512_mask_reduce_pd(src, m, a0, s+V);
-        else if constexpr( c == category::float32x8 ) return _mm256_mask_reduce_ps(src, m, a0, s+V);
-        else if constexpr( c == category::float64x4 ) return _mm256_mask_reduce_pd(src, m, a0, s+V);
-        else if constexpr( c == category::float32x4 ) return _mm_mask_reduce_ps(src, m, a0, s+V);
-        else if constexpr( c == category::float64x2 ) return _mm_mask_reduce_pd(src, m, a0, s+V);
-      };
-
-      if       constexpr(O::contains(to_nearest2))  return call<0>(src, m, a0);
-      else if  constexpr(O::contains(downward2))    return call<1>(src, m, a0);
-      else if  constexpr(O::contains(upward2))      return call<2>(src, m, a0);
-      else if  constexpr(O::contains(toward_zero2)) return call<3>(src, m, a0);
+      if      constexpr( c == category::float32x16) return _mm512_mask_reduce_ps(src, m, a0, s+V);
+      else if constexpr( c == category::float64x8 ) return _mm512_mask_reduce_pd(src, m, a0, s+V);
+      else if constexpr( c == category::float32x8 ) return _mm256_mask_reduce_ps(src, m, a0, s+V);
+      else if constexpr( c == category::float64x4 ) return _mm256_mask_reduce_pd(src, m, a0, s+V);
+      else if constexpr( c == category::float32x4 ) return _mm_mask_reduce_ps(src, m, a0, s+V);
+      else if constexpr( c == category::float64x2 ) return _mm_mask_reduce_pd(src, m, a0, s+V);
     }
   }
 }
--- test/doc/core/roundscale.cpp
+++ test/doc/core/roundscale.cpp
@@ -10,12 +10,22 @@ int main()
   wide_ft pf( [](auto i, auto) { return 1.2345678+i; } );
 
   std::cout << "---- simd" << '\n'
-            << " <- pf                            = " << pf << '\n'
-            << " -> roundscale(pf, 4)              = " << eve::roundscale(pf, 4) << '\n'
-            << " -> roundscale[downward2  ](pf, 4) = " << eve::roundscale[eve::downward2  ](pf, 4) << '\n'
-            << " -> roundscale[upward2    ](pf, 4) = " << eve::roundscale[eve::upward2    ](pf, 4) << '\n'
-            << " -> roundscale[to_nearest2](pf, 4) = " << eve::roundscale[eve::to_nearest2](pf, 4) << '\n';
+            << " <- pf                              = " << pf << '\n'
+            << " -> roundscale(pf, 4)               = " << eve::roundscale(pf, 4) << '\n'
+            << " -> roundscale[downward2  ](pf, 4)  = " << eve::roundscale[eve::downward2  ](pf, 4) << '\n'
+            << " -> roundscale[upward2    ](pf, 4)  = " << eve::roundscale[eve::upward2    ](pf, 4) << '\n'
+            << " -> roundscale[to_nearest2](pf, 4)  = " << eve::roundscale[eve::to_nearest2](pf, 4) << '\n'
+            << " -> roundscale[toward_zero2](pf, 4) = " << eve::roundscale[eve::toward_zero2](pf, 4) << '\n'   ;
 
+ constexpr eve::index_t<4> four;
+
+  std::cout << "---- simd" << '\n'
+            << " <- pf                                 = " << pf << '\n'
+            << " -> roundscale(pf, four)               = " << eve::roundscale(pf, four) << '\n'
+            << " -> roundscale[downward2  ](pf, four)  = " << eve::roundscale[eve::downward2  ](pf, four) << '\n'
+            << " -> roundscale[upward2    ](pf, four)  = " << eve::roundscale[eve::upward2    ](pf, four) << '\n'
+            << " -> roundscale[to_nearest2](pf, four)  = " << eve::roundscale[eve::to_nearest2](pf, four) << '\n'
+            << " -> roundscale[toward_zero2](pf, four) = " << eve::roundscale[eve::toward_zero2](pf, four) << '\n'   ;
 
   float xf = 0x1.fffffep0f;
 
