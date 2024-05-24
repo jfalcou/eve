f30f245e66837edbfc1cfee61052e809c360bc20
jtlap <jtlapreste@gmail.com>
2024-05-24 19:52:48 +0200

    Cleanup popcount

--- include/eve/module/core/regular/impl/popcount.hpp
+++ include/eve/module/core/regular/impl/popcount.hpp
@@ -10,14 +10,9 @@
 #include <eve/as.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/detail/apply_over.hpp>
-#include <eve/detail/has_abi.hpp>
 #include <eve/detail/spy.hpp>
-#include <eve/module/core/regular/bit_cast.hpp>
 #include <eve/traits.hpp>
-
 #include <bit>
-
-#include <type_traits>
 
 #if defined(SPY_COMPILER_IS_MSVC)
 #  include <intrin.h>
@@ -25,17 +20,13 @@
 
 namespace eve::detail
 {
-template<unsigned_value T>
-EVE_FORCEINLINE auto
-popcount_(EVE_SUPPORTS(cpu_), T x) noexcept
-{
-  using r_t = as_integer_t<T, unsigned>;
-  if constexpr( has_native_abi_v<T> )
+  template<typename T, callable_options O>
+  EVE_FORCEINLINE constexpr auto popcount_(EVE_REQUIRES(cpu_), O const&, T x) noexcept
   {
-    if constexpr( scalar_value<T> ) { return r_t(std::popcount(x)); }
+    if constexpr( scalar_value<T> )
+      return T(std::popcount(x));
     else
     {
-      //        return map(eve::popcount, v);
       constexpr auto siz = sizeof(eve::element_type_t<T>) * 8;
       if constexpr( siz == 8 )
       {
@@ -63,15 +54,4 @@ popcount_(EVE_SUPPORTS(cpu_), T x) noexcept
       }
     }
   }
-  else { return apply_over(popcount, x); }
-}
-
-// -----------------------------------------------------------------------------------------------
-// Masked case
-template<conditional_expr C, unsigned_value U>
-EVE_FORCEINLINE auto
-popcount_(EVE_SUPPORTS(cpu_), C const& cond, U const& t) noexcept
-{
-  return mask_op(cond, eve::popcount, t);
-}
 }
