260d1a14d011e0bb64fe9895e5ef27877f0ab5a8
jtlap <jtlapreste@gmail.com>
2024-05-25 18:23:12 +0200

    fracscale

--- include/eve/module/core/regular/impl/simd/x86/fracscale.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fracscale.hpp
@@ -24,7 +24,7 @@ namespace eve::detail
   requires x86_abi<abi_t<T, N>>
   {
     constexpr int spv = ((S) << 4) + (eve::rounding_mode<O>(eve::to_nearest2) & 3);
-    
+
     constexpr auto c = categorize<wide<T, N>>();
     if constexpr(S > 15)                          return fracscale.behavior(o, a0, S);
     else if constexpr( c == category::float32x16) return _mm512_reduce_ps(a0, spv);
@@ -34,10 +34,10 @@ namespace eve::detail
     else if constexpr( c == category::float32x4 ) return _mm_reduce_ps(a0, spv);
     else if constexpr( c == category::float64x2 ) return _mm_reduce_pd(a0, spv);
   }
-  
+
   // -----------------------------------------------------------------------------------------------
   // Masked case
-  
+
   template<auto S, conditional_expr C, floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N> fracscale_(EVE_REQUIRES(avx512_),
                                         C          const &mask,
@@ -53,16 +53,15 @@ namespace eve::detail
     {
       auto          src = alternative(mask, a0, as<wide<T, N>> {});
       auto          m   = expand_mask(mask, as<wide<T, N>> {}).storage().value;
-      constexpr int s   = ((S) << 4);
+      constexpr int spv = ((S) << 4) + (eve::rounding_mode<O>(eve::to_nearest2) & 3);
       constexpr auto c = categorize<wide<T, N>>();
-      int constexpr V = eve::rounding_mode<O>(eve::to_nearest2) & 3;
 
-      if      constexpr( c == category::float32x16) return _mm512_mask_reduce_ps(src, m, a0, s+V);
-      else if constexpr( c == category::float64x8 ) return _mm512_mask_reduce_pd(src, m, a0, s+V);
-      else if constexpr( c == category::float32x8 ) return _mm256_mask_reduce_ps(src, m, a0, s+V);
-      else if constexpr( c == category::float64x4 ) return _mm256_mask_reduce_pd(src, m, a0, s+V);
-      else if constexpr( c == category::float32x4 ) return _mm_mask_reduce_ps(src, m, a0, s+V);
-      else if constexpr( c == category::float64x2 ) return _mm_mask_reduce_pd(src, m, a0, s+V);
+      if      constexpr( c == category::float32x16) return _mm512_mask_reduce_ps(src, m, a0, spv);
+      else if constexpr( c == category::float64x8 ) return _mm512_mask_reduce_pd(src, m, a0, spv);
+      else if constexpr( c == category::float32x8 ) return _mm256_mask_reduce_ps(src, m, a0, spv);
+      else if constexpr( c == category::float64x4 ) return _mm256_mask_reduce_pd(src, m, a0, spv);
+      else if constexpr( c == category::float32x4 ) return _mm_mask_reduce_ps(src, m, a0, spv);
+      else if constexpr( c == category::float64x2 ) return _mm_mask_reduce_pd(src, m, a0, spv);
     }
   }
 }
