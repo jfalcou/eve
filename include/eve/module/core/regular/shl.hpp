9bade28484822ed877d58c1235e9aade5baf9d33
SadiinsoSnowfall <sadiinso@gmail.com>
2024-05-29 15:35:31 +0200

    take 3

--- include/eve/module/core/regular/shl.hpp
+++ include/eve/module/core/regular/shl.hpp
@@ -120,3 +120,7 @@ namespace eve
     }
   }
 }
+
+#if defined(EVE_INCLUDE_X86_HEADER)
+#  include <eve/module/core/regular/impl/simd/x86/shl.hpp>
+#endif
--- include/eve/module/core/regular/shr.hpp
+++ include/eve/module/core/regular/shr.hpp
@@ -121,3 +121,7 @@ namespace eve
     }
   }
 }
+
+#if defined(EVE_INCLUDE_X86_HEADER)
+#  include <eve/module/core/regular/impl/simd/x86/shr.hpp>
+#endif
