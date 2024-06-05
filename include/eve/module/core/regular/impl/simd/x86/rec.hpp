8b8d95ae1aaf2f8a4db971fdb59a698e39e74cce
jtlap <jtlapreste@gmail.com>
2024-06-05 19:30:29 +0200

    rec avx512 pedantic

--- include/eve/module/core/regular/impl/simd/x86/rec.hpp
+++ include/eve/module/core/regular/impl/simd/x86/rec.hpp
@@ -51,34 +51,22 @@ namespace eve::detail
     }
     else if constexpr(O::contains(pedantic2))
     {
-      if (current_api < avx512 )
+      if (current_api >= avx512)
       {
- //        if     constexpr( c == category::float32x16 ) return _mm512_div_ps(one(eve::as(v)), v);
-//         else if constexpr( c == category::float64x8 ) return _mm512_div_pd(one(eve::as(v)), v);
-        if (current_api >= avx)
-        {
-          if      constexpr( c == category::float32x8 ) return _mm256_div_ps(one(eve::as(v)), v);
-          else if constexpr( c == category::float64x4 ) return _mm256_div_pd(one(eve::as(v)), v);
-        }
-        if (current_api >= sse2)
-        {
-          if      constexpr( c == category::float32x4 ) return _mm_div_ps(one(eve::as(v)), v);
-          else if constexpr( c == category::float64x2 ) return _mm_div_pd(one(eve::as(v)), v);
-        }
-        return rec.behavior(cpu_{}, o, v);
+        if      constexpr( c == category::float32x16) return _mm512_div_ps(one(eve::as(v)), v);
+        else if constexpr( c == category::float64x8 ) return _mm512_div_pd(one(eve::as(v)), v);
       }
-      else
+      if (current_api >= avx)
       {
-        auto x = rec[raw2](v);
-        x = fma(fnma(x, v, one(eve::as(v))), x, x);
-        x = fma(fnma(x, v, one(eve::as(v))), x, x);
-        return if_else(is_eqz(v),
-                       v | inf(eve::as(v)),
-                       if_else(is_infinite(v),
-                               v & mzero(eve::as(v)),
-                               x)
-                      );
+        if      constexpr( c == category::float32x8 ) return _mm256_div_ps(one(eve::as(v)), v);
+        else if constexpr( c == category::float64x4 ) return _mm256_div_pd(one(eve::as(v)), v);
       }
+      if (current_api >= sse2)
+      {
+        if      constexpr( c == category::float32x4 ) return _mm_div_ps(one(eve::as(v)), v);
+        else if constexpr( c == category::float64x2 ) return _mm_div_pd(one(eve::as(v)), v);
+      }
+      return rec.behavior(cpu_{}, o, v);
     }
     else
     {
