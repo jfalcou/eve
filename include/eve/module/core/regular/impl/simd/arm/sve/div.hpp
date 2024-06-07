8d2fe0092f2ccaa0b8af050cf17dc29f303f8f5d
jtlap <jtlapreste@gmail.com>
2024-06-07 22:14:32 +0200

    sve ppc

--- include/eve/module/core/regular/impl/simd/arm/sve/div.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/div.hpp
@@ -12,23 +12,34 @@
 
 namespace eve::detail
 {
-template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE wide<T,N> div_(EVE_SUPPORTS(sve_),
-                               C          const& cx,
-                               O          const& o,
-                               wide<T, N> const& v,
-                               wide<T, N> const& w) noexcept
-requires sve_abi<abi_t<T, N>>
-{
-  auto alt = alternative(cx, v, as<wide<T, N>> {});
-  if constexpr( C::is_complete )  return alt;
-  else
+  template<conditional_expr C, arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE wide<T,N> div_(EVE_REQUIRES(sve_),
+                                 C          const& cx,
+                                 O          const& o,
+                                 wide<T, N> const& v,
+                                 wide<T, N> const& w) noexcept
+  requires sve_abi<abi_t<T, N>>
   {
-    if constexpr( !C::has_alternative)
+    if constexpr(O::contains(toward_zero2) || O::contains(upward2) || O::contains(downward2) || O::contains(to_nearest2))
     {
-      auto m   = expand_mask(cx, as<wide<T, N>> {});
-      return svdiv_m(m,v,w);
+      if (floating_scalar_value<T>)
+        return round[o](div[cx](v, w));
+      else
+        return div.behavior(cpu_{}, o, v, w);
     }
-    else  return div.behavior(cpu_{},o,v,w);
-}
+    else
+    {
+      auto alt = alternative(cx, v, as<wide<T, N>> {});
+      if constexpr( C::is_complete )  return alt;
+      else
+      {
+        if constexpr( !C::has_alternative)
+        {
+          auto m   = expand_mask(cx, as<wide<T, N>> {});
+          return svdiv_m(m,v,w);
+        }
+        else  return div.behavior(cpu_{},o,v,w);
+      }
+    }
+  }
 }
--- include/eve/module/core/regular/impl/simd/ppc/div.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/div.hpp
@@ -12,13 +12,13 @@
 
 namespace eve::detail
 {
-template<arithmetic_scalar_value T, typename N, callable_options O>
-EVE_FORCEINLINE wide<T, N> div_(EVE_REQUIRES(vsx_),
-                                O const&
-                                wide<T, N> v0,
-                                wide<T, N> const& v1) noexcept
-requires ppc_abi<abi_t<T, N>>
-{
-  return v0 /= v1;
-}
+  template<arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE wide<T, N> div_(EVE_REQUIRES(vsx_),
+                                  O const&,
+                                  wide<T, N> v0,
+                                  wide<T, N> const& v1) noexcept
+  requires ppc_abi<abi_t<T, N>>
+  {
+    return v0 /= v1;
+  }
 }
