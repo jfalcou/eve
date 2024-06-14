b9b69ef81968d5b22a4b50ee580b5a983b3a9edc
SadiinsoSnowfall <sadiinso@gmail.com>
2024-06-14 14:44:49 +0200

    speedup compress tests

--- test/unit/api/compress/compress_copy_scalar_tuple.small.cpp
+++ test/unit/api/compress/compress_copy_scalar_tuple.small.cpp
@@ -9,8 +9,10 @@
 
 #include <eve/module/core.hpp>
 
-TTS_CASE_TPL("Check compress copy behavior", eve::test::simd::all_types)
+using test_types = eve::test::wides<tts::types<std::int16_t, std::uint16_t, std::int8_t, std::uint8_t>>::type;
+
+TTS_CASE_TPL("Check compress copy behavior", test_types)
 <typename T>(tts::type<T>) {
   using e_t = kumi::tuple<std::int8_t, eve::element_type_t<T>, double>;
-  compress_copy_tst(eve::as<eve::wide<e_t, typename T::cardinal_type>>{}, eve::compress_copy);
+  compress_copy_tst(eve::as<eve::wide<e_t, typename T::cardinal_type>>{}, eve::compress_copy_scalar);
 };
