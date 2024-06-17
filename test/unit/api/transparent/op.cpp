7e2cfb3527b22702b59748978ad1060fc84831f9
SadiinsoSnowfall <sadiinso@gmail.com>
2024-06-17 21:49:27 +0200

    uncomment test

--- test/unit/api/transparent/op.cpp
+++ test/unit/api/transparent/op.cpp
@@ -62,65 +62,65 @@ TTS_CASE_TPL( "Check return types of arithmetic operators on wide<transparent en
   TTS_EXPR_IS( ~T()           , T);
 
   TTS_EXPR_IS((T() == T())                  , logical<T>);
-  // TTS_EXPR_IS((T() != T())                  , logical<T>);
-  // TTS_EXPR_IS((T() <  T())                  , logical<T>);
-  // TTS_EXPR_IS((T() <= T())                  , logical<T>);
-  // TTS_EXPR_IS((T() >  T())                  , logical<T>);
-  // TTS_EXPR_IS((T() >= T())                  , logical<T>);
-  // TTS_EXPR_IS((logical<T>() != logical<T>()), logical<T>);
-  // TTS_EXPR_IS((logical<T>() != logical<T>()), logical<T>);
+  TTS_EXPR_IS((T() != T())                  , logical<T>);
+  TTS_EXPR_IS((T() <  T())                  , logical<T>);
+  TTS_EXPR_IS((T() <= T())                  , logical<T>);
+  TTS_EXPR_IS((T() >  T())                  , logical<T>);
+  TTS_EXPR_IS((T() >= T())                  , logical<T>);
+  TTS_EXPR_IS((logical<T>() != logical<T>()), logical<T>);
+  TTS_EXPR_IS((logical<T>() != logical<T>()), logical<T>);
 
-  // TTS_EXPR_IS((T() == v)                  , logical<T>);
-  // TTS_EXPR_IS((T() != v)                  , logical<T>);
-  // TTS_EXPR_IS((T() <  v)                  , logical<T>);
-  // TTS_EXPR_IS((T() <= v)                  , logical<T>);
-  // TTS_EXPR_IS((T() >  v)                  , logical<T>);
-  // TTS_EXPR_IS((T() >= v)                  , logical<T>);
-  // TTS_EXPR_IS((logical<T>() != logical<E>()), logical<T>);
-  // TTS_EXPR_IS((logical<T>() != logical<E>()), logical<T>);
+  TTS_EXPR_IS((T() == v)                  , logical<T>);
+  TTS_EXPR_IS((T() != v)                  , logical<T>);
+  TTS_EXPR_IS((T() <  v)                  , logical<T>);
+  TTS_EXPR_IS((T() <= v)                  , logical<T>);
+  TTS_EXPR_IS((T() >  v)                  , logical<T>);
+  TTS_EXPR_IS((T() >= v)                  , logical<T>);
+  TTS_EXPR_IS((logical<T>() != logical<E>()), logical<T>);
+  TTS_EXPR_IS((logical<T>() != logical<E>()), logical<T>);
 
-  // TTS_EXPR_IS((v == T())                  , logical<T>);
-  // TTS_EXPR_IS((v != T())                  , logical<T>);
-  // TTS_EXPR_IS((v <  T())                  , logical<T>);
-  // TTS_EXPR_IS((v <= T())                  , logical<T>);
-  // TTS_EXPR_IS((v >  T())                  , logical<T>);
-  // TTS_EXPR_IS((v >= T())                  , logical<T>);
-  // TTS_EXPR_IS((logical<E>() != logical<T>()), logical<T>);
-  // TTS_EXPR_IS((logical<E>() != logical<T>()), logical<T>);
+  TTS_EXPR_IS((v == T())                  , logical<T>);
+  TTS_EXPR_IS((v != T())                  , logical<T>);
+  TTS_EXPR_IS((v <  T())                  , logical<T>);
+  TTS_EXPR_IS((v <= T())                  , logical<T>);
+  TTS_EXPR_IS((v >  T())                  , logical<T>);
+  TTS_EXPR_IS((v >= T())                  , logical<T>);
+  TTS_EXPR_IS((logical<E>() != logical<T>()), logical<T>);
+  TTS_EXPR_IS((logical<E>() != logical<T>()), logical<T>);
 
-  // if constexpr( eve::integral_element_value<T> )
-  // {
-  //   TTS_EXPR_IS( T() % T(), T);
-  //   TTS_EXPR_IS( v % T(), T);
-  //   TTS_EXPR_IS( T() % v, T);
-  // }
+  if constexpr( eve::integral_element_value<T> )
+  {
+    TTS_EXPR_IS( T() % T(), T);
+    TTS_EXPR_IS( v % T(), T);
+    TTS_EXPR_IS( T() % v, T);
+  }
 };
 
-// TTS_CASE_TPL("Wide<transparent enum> eq/neq", eve::test::simd::integers)
-// <typename Wb>(tts::type<Wb>)
-// {
-//   enum class E: eve::element_type_t<Wb> { };
-//   using W = typename Wb::template retype<E>;
+TTS_CASE_TPL("Wide<transparent enum> eq/neq", eve::test::simd::integers)
+<typename Wb>(tts::type<Wb>)
+{
+  enum class E: eve::element_type_t<Wb> { };
+  using W = typename Wb::template retype<E>;
 
-//   W x{ static_cast<E>(3) };
-//   W y{ static_cast<E>(2) };
+  W x{ static_cast<E>(3) };
+  W y{ static_cast<E>(2) };
 
-//   TTS_EQUAL(x, x);
-//   TTS_NOT_EQUAL(x, y);
-//   TTS_EQUAL((x != static_cast<E>(0)), (y != static_cast<E>(0)));
-// };
+  TTS_EQUAL(x, x);
+  TTS_NOT_EQUAL(x, y);
+  TTS_EQUAL((x != static_cast<E>(0)), (y != static_cast<E>(0)));
+};
 
-// TTS_CASE_TPL("Wide<transparent struct> eq/neq", eve::test::simd::all_types)
-// <typename Wb>(tts::type<Wb>)
-// {
-//   using E = eve::element_type_t<Wb>;
-//   using S = BS<E>;
-//   using W = typename Wb::template retype<S>;
+TTS_CASE_TPL("Wide<transparent struct> eq/neq", eve::test::simd::all_types)
+<typename Wb>(tts::type<Wb>)
+{
+  using E = eve::element_type_t<Wb>;
+  using S = BS<E>;
+  using W = typename Wb::template retype<S>;
 
-//   W x{ S::of(3) };
-//   W y{ S::of(2) };
+  W x{ S::of(3) };
+  W y{ S::of(2) };
 
-//   TTS_EQUAL(x, x);
-//   TTS_NOT_EQUAL(x, y);
-//   TTS_EQUAL((x != S::of(0)), (y != S::of(0)));
-// };
+  TTS_EQUAL(x, x);
+  TTS_NOT_EQUAL(x, y);
+  TTS_EQUAL((x != S::of(0)), (y != S::of(0)));
+};
