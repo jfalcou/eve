9b9098a16b1724e3935050c2cc1cdf3c20fb2903
jtlap <jtlapreste@gmail.com>
2024-06-26 15:55:10 +0200

    count...+cos

--- include/eve/module/core/regular/countl_zero.hpp
+++ include/eve/module/core/regular/countl_zero.hpp
@@ -29,9 +29,9 @@ namespace eve
 //! @addtogroup core_bitops
 //! @{
 //!   @var countl_zero
-//!   @brief Computes the number of consecutive 0 in a value starting from left
+//!   @brief elementwise_callable` object computing the number of consecutive bits unset in a value starting from left
 //!
-//!   **Defined in Header**
+//!   @groupheader{Header file}
 //!
 //!   @code
 //!   #include <eve/module/core.hpp>
@@ -42,23 +42,31 @@ namespace eve
 //!   @code
 //!   namespace eve
 //!   {
-//!      template< eve::value T >
-//!      T countl_zero(T x) noexcept;
+//!      // Regular overload
+//!      constexpr auto countl_zero(value auto x)                          noexcept; // 1
+//!
+//!      // Lanes masking 
+//!      constexpr auto countl_zero[conditional_expr auto c](value auto x) noexcept; // 2
+//!      constexpr auto countl_zero[logical_value auto m](value auto x)    noexcept; // 2
 //!   }
 //!   @endcode
 //!
 //!   **Parameters**
 //!
 //!     * `x` :  [argument](@ref eve::value).
+//!     * `c`: [Conditional expression](@ref conditional_expr) masking the operation.
+//!     * `m`: [Logical value](@ref logical) masking the operation.
 //!
 //!    **Return value**
 //!
-//!      The value of the number of consecutive 0 ("zero") bits in the value of `x`, starting
-//!      from the most significant bit ("left"), with same type as `x`,
-//!      is returned.
+//!       1. The value of the number of consecutive 0 ("zero") bits in the value of `x`, starting
+//!        from the most significant bit ("left"), with same type as `x` is returned.
+//!       2. [The operation is performed conditionnaly](@ref conditional).
+//!
+//!  @groupheader{External references}
+//!   *  [C++ standard reference](https://en.cppreference.com/w/cpp/numeric/countl_zero)
 //!
 //!  @groupheader{Example}
-//!
 //!  @godbolt{doc/core/countl_zero.cpp}
 //! @}
 //================================================================================================
--- include/eve/module/core/regular/countr_one.hpp
+++ include/eve/module/core/regular/countr_one.hpp
@@ -28,9 +28,9 @@ namespace eve
 //! @addtogroup core_bitops
 //! @{
 //!   @var countr_one
-//!   @brief Computes the number of consecutive 1 in a value starting from right.
+//!   @brief `elementwise_callable` object computing the number of consecutive bits set in a value starting from right.
 //!
-//!   **Defined in Header**
+//!   @groupheader{Header file}
 //!
 //!   @code
 //!   #include <eve/module/core.hpp>
@@ -41,23 +41,31 @@ namespace eve
 //!   @code
 //!   namespace eve
 //!   {
-//!      template< eve::value T >
-//!      T countr_one(T x) noexcept;
+//!      // Regular overload
+//!      constexpr auto countr_one(value auto x)                          noexcept; // 1
+//!
+//!      // Lanes masking
+//!      constexpr auto countr_one[conditional_expr auto c](value auto x) noexcept; // 2
+//!      constexpr auto countr_one[logical_value auto m](value auto x)    noexcept; // 2
 //!   }
 //!   @endcode
 //!
 //!   **Parameters**
 //!
 //!     * `x` :  [argument](@ref eve::value).
+//!     * `c`: [Conditional expression](@ref conditional_expr) masking the operation.
+//!     * `m`: [Logical value](@ref logical) masking the operation.
 //!
 //!    **Return value**
 //!
-//!      The value of the number of consecutive 1 ("one") bits in the value of `x`, starting
-//!      from the least significant bit ("right"), with same type as `x`,
-//!      is returned.
+//!       1.The value of the number of consecutive 1 ("one") bits in the value of `x`, starting
+//!         from the least significant bit ("right"), with same type as `x`, is returned.
+//!       2. [The operation is performed conditionnaly](@ref conditional).
+//!
+//!  @groupheader{External references}
+//!   *  [C++ standard reference](https://en.cppreference.com/w/cpp/numeric/countr_one)
 //!
 //!  @groupheader{Example}
-//!
 //!  @godbolt{doc/core/countr_one.cpp}
 //! @}
 //================================================================================================
