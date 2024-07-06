c20c925bf574a4b9111c97494fc74314dd796f64
jfalcou <jfalcou@users.noreply.github.com>
2024-07-06 13:27:41 +0000

    deploy: 19ca0d97c4da42ada27700ab666911a6a81288d8

--- search/all_a.js
+++ search/all_a.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['keep_5fbetween_0',['keep_between',['../structeve_1_1keep__between.html',1,'eve']]],
   ['keep_5ffirst_1',['keep_first',['../structeve_1_1keep__first.html',1,'eve::keep_first'],['../structeve_1_1keep__first.html#a3980975e38c2675ec2f812a4ff3eb7c4',1,'eve::keep_first::keep_first()']]],
-  ['keep_5flast_2',['keep_last',['../structeve_1_1keep__last.html',1,'eve']]],
-  ['khinchin_3',['khinchin',['../group__math__constants_gad32bbc77b245754ed40e0d855da6b08e.html#gad32bbc77b245754ed40e0d855da6b08e',1,'eve']]]
+  ['keep_5fif_2',['keep_if',['../group__algos_ga392231d168d36c8efdaa564de2c8c498.html#ga392231d168d36c8efdaa564de2c8c498',1,'eve::algo']]],
+  ['keep_5flast_3',['keep_last',['../structeve_1_1keep__last.html',1,'eve']]],
+  ['khinchin_4',['khinchin',['../group__math__constants_gad32bbc77b245754ed40e0d855da6b08e.html#gad32bbc77b245754ed40e0d855da6b08e',1,'eve']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['khinchin_0',['khinchin',['../group__math__constants_gad32bbc77b245754ed40e0d855da6b08e.html#gad32bbc77b245754ed40e0d855da6b08e',1,'eve']]]
+  ['keep_5fif_0',['keep_if',['../group__algos_ga392231d168d36c8efdaa564de2c8c498.html#ga392231d168d36c8efdaa564de2c8c498',1,'eve::algo']]],
+  ['khinchin_1',['khinchin',['../group__math__constants_gad32bbc77b245754ed40e0d855da6b08e.html#gad32bbc77b245754ed40e0d855da6b08e',1,'eve']]]
 ];
