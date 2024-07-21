04d4501f539bf3dac62e488afca8caa75824cc29
jtlap <jtlapreste@gmail.com>
2024-07-21 19:15:34 +0200

    fibonacci.cpp again

--- test/doc/combinatorial/fibonacci.cpp
+++ test/doc/combinatorial/fibonacci.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 eve::wide<std::uint32_t> n([](auto i, auto )->std::uint32_t{ return i+2;});
-eve::wide m{13, 25, 32, 180, 1, 2, 3, 4};
+eve::wide m{13u, 25u, 32u, 80u, 1u, 2u, 3u, 4u};
 eve::wide a{1.0f, 2.0f, 3.0f, 4.0f, 0.5f, 0.33f, -4.5f, 0.0f};
 eve::wide b{2.0f, 3.0f, 4.0f, 0.5f, 0.33f, -4.5f, 0.0f, 1.0f};
 
@@ -14,5 +14,5 @@ int main(){
    std::cout << "<- b   = " << b   << "\n";
 
    std::cout << "-> fibonacci(n, 1.0f, 1.0f)= " << eve::fibonacci(n, 1.0f, 1.0f) << "\n";
-   std::cout << "-> fibonacci(m, a, b)      = " << eve::fibonacci(n, a, b) << "\n";
+   std::cout << "-> fibonacci(m, a, b)      = " << eve::fibonacci(m, a, b) << "\n";
 }
