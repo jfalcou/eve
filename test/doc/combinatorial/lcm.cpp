15c660d2481cfcba6e0965cca6ca34f04cc49248
jtlap <jtlapreste@gmail.com>
2024-07-21 18:45:07 +0200

    cpp ajustments

--- test/doc/combinatorial/lcm.cpp
+++ test/doc/combinatorial/lcm.cpp
@@ -4,13 +4,17 @@
 
 eve::wide<float> wf0([](auto i, auto c)->float{ return (i-c/2);});
 eve::wide<float> wf1([](auto i, auto )->float{ return 10*i; });
+eve::wide n{93, 25, 32, 368,  216,  43, 18, 25};
+eve::wide p{42, 30, 27, 1024, 36, 12, 51, 44};
 
 int main(){
    std::cout << "<- wf0 = " << wf0 << "\n";
    std::cout << "<- wf1 = " << wf1 << "\n";
+   std::cout << "<- n   = " << n   << "\n";
+   std::cout << "<- p   = " << p   << "\n";
 
    std::cout << "-> lcm(wf0, wf1)                = " << eve::lcm(wf0, wf1) << "\n";
    std::cout << "-> lcm[ignore_last(2)](wf0, wf1)= " << eve::lcm[eve::ignore_last(2)](wf0, wf1) << "\n";
    std::cout << "-> lcm[wf0 != -2.0f](wf0, wf1)  = " << eve::lcm[wf0 != -2.0f](wf0, wf1) << "\n";
-
+   std::cout << "-> lcm(p, n)                    = " << eve::lcm(p, n) << "\n";
 }
--- test/unit/module/combinatorial/fibonacci.cpp
+++ test/unit/module/combinatorial/fibonacci.cpp
@@ -24,15 +24,3 @@ TTS_CASE_TPL("Check return types of eve::fibonacci", eve::test::simd::ieee_reals
   TTS_EXPR_IS(eve::fibonacci(i_t(), elt_t(), T()), T);
   TTS_EXPR_IS(eve::fibonacci(std::uint8_t(), elt_t(), elt_t()), elt_t);
 };
-
-//==================================================================================================
-// Test for corner-cases values
-//==================================================================================================
-TTS_CASE_TPL("Check corner-cases behavior of eve::fibonacci on wide", eve::test::simd::ieee_reals)
-<typename T>(tts::type<T>)
-{
-  using eve::as;
-  using i_t = eve::as_integer_t<T, unsigned>;
-  TTS_EQUAL(eve::fibonacci(9u, T(1), T(1)), T(55));
-  TTS_EQUAL(eve::fibonacci(i_t(9), T(1), T(1)), T(55));
-};
