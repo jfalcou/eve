ff4aae11fc1633a4dac15162698b7aa8506f2854
jtlap <jtlapreste@gmail.com>
2024-07-21 19:02:44 +0200

    fibonacci.cpp was flawed

--- test/doc/combinatorial/fibonacci.cpp
+++ test/doc/combinatorial/fibonacci.cpp
@@ -2,13 +2,17 @@
 #include <eve/module/combinatorial.hpp>
 #include <iostream>
 
-eve::wide<float> wf0([](auto i, auto c)->float{ return 2*(i-c/2);});
-eve::wide<float> wf1([](auto i, auto c)->float{ return 2*(i+c/2);});
+eve::wide<std::uint32_t> n([](auto i, auto )->std::uint32_t{ return i+2;});
+eve::wide m{13, 25, 32, 180, 1, 2, 3, 4};
+eve::wide a{1.0f, 2.0f, 3.0f, 4.0f, 0.5f, 0.33f, -4.5f, 0.0f};
+eve::wide b{2.0f, 3.0f, 4.0f, 0.5f, 0.33f, -4.5f, 0.0f, 1.0f};
 
 int main(){
-   std::cout << "<- wf0 = " << wf0 << "\n";
-   std::cout << "<- wf1 = " << wf1 << "\n";
+   std::cout << "<- n   = " << n   << "\n";
+   std::cout << "<- m   = " << m   << "\n";
+   std::cout << "<- a   = " << a   << "\n";
+   std::cout << "<- b   = " << b   << "\n";
 
-   std::cout << "-> fibonacci(n, wf0, wf1)= " << eve::fibonacci(n, wf0, wf1) << "\n";
-   std::cout << "-> fibonacci
+   std::cout << "-> fibonacci(n, 1.0f, 1.0f)= " << eve::fibonacci(n, 1.0f, 1.0f) << "\n";
+   std::cout << "-> fibonacci(m, a, b)      = " << eve::fibonacci(n, a, b) << "\n";
 }
