68c806b9a367bc8c48fcbc6dac67ae175c807d90
jtlap <jtlapreste@gmail.com>
2024-07-24 07:15:56 +0200

    rising_factorial.cpp

--- test/doc/special/rising_factorial.cpp
+++ test/doc/special/rising_factorial.cpp
@@ -2,8 +2,6 @@
 #include <eve/module/special.hpp>
 #include <iostream>
 
-eve::wide<float> wf([](auto i, auto c)->float{ return 2*(i-c/2);});
-
 int main()
 {
   eve::wide n = {1, 2, -3, 7};
@@ -14,7 +12,7 @@ int main()
 
   std::cout << "-> rising_factorial(n, p)                = " << eve::rising_factorial(n, p) << "\n";
   std::cout << "-> rising_factorial[ignore_last(2)](n, p)= " << eve::rising_factorial[eve::ignore_last(2)](n, p) << "\n";
-  std::cout << "-> rising_factorial[wf != -2.0f](n, p)   = " << eve::rising_factorial[wf != -2.0f](n, p) << "\n";
+  std::cout << "-> rising_factorial[n != 2.0f](n, p)     = " << eve::rising_factorial[n != 2.0f](n, p) << "\n";
   std::cout << "-> rising_factorial[raw](n, p)           = " << eve::rising_factorial[eve::raw](n, p) << "\n";
   std::cout << "-> rising_factorial[pedantic](n, p)      = " << eve::rising_factorial[eve::pedantic](n, p) << "\n";
 }
