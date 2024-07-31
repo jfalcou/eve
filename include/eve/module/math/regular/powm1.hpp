b7c6b5fe0e40c01bf852ac9632ce50ca64cabb7b
jtlap <jtlapreste@gmail.com>
2024-07-31 20:10:48 +0200

    p*.hpp

--- include/eve/module/math/regular/powm1.hpp
+++ include/eve/module/math/regular/powm1.hpp
@@ -70,8 +70,8 @@ namespace eve
 //!
 //!    1. Returns the [elementwise](@ref glossary_elementwise) power minus one, with good accuracy,
 //!       even when `y` is very small, or when `x` is close to 1.
-//!     2. [The operation is performed conditionnaly](@ref conditional)
-//!     3. faster but less accurate call.
+//!    2. [The operation is performed conditionnaly](@ref conditional)
+//!    3. faster but less accurate call.
 //!
 //!  @groupheader{Example}
 //!  @godbolt{doc/math/powm1.cpp}
