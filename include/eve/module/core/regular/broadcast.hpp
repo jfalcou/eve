c4d7ff9f62705f5d4718e1d2ee00ae098fe372b0
jtlap <jtlapreste@gmail.com>
2024-08-01 13:18:36 +0200

    b*.hpp

--- include/eve/module/core/regular/broadcast.hpp
+++ include/eve/module/core/regular/broadcast.hpp
@@ -36,48 +36,50 @@ namespace eve
     EVE_CALLABLE_OBJECT(broadcast_t, broadcast_);
   };
 
-  //================================================================================================
-  //! @addtogroup core_simd
-  //! @{
-  //!   @var broadcast
-  //!   @brief Computes the
-  //!
-  //!   **Defined in Header**
-  //!
-  //!   @code
-  //!   #include <eve/module/core.hpp>
-  //!   @endcode
-  //!
-  //!   @groupheader{Callable Signatures}
-  //!
-  //!   @code
-  //!   namespace eve
-  //!   {
-  //!     template<eve::scalar_value T> eve::as_wide_t<T> broadcast(T v) noexcept;                            // 1
-  //!     template<eve::scalar_value T, std::ptrdiff_t N> as_wide_t<T> broadcast(T v, fixed<N> sz) noexcept;  // 2
-  //!     template<eve::simd_value T, std::size_t I> T broadcast(T v, index_t<I> i) noexcept;                 // 3
-  //!     template<eve::simd_value T, std::size_t I, std::ptrdiff_t N>
-  //!     eve::as_wide_t<T,fixed<N>> broadcast(T v, index_t<I> i, fixed<N> sz) noexcept;                      // 4
-  //!   }
-  //!   @endcode
-  //!
-  //!   **Parameters**
-  //!     * `v`: A [value](@ref eve::simd_value).
-  //!     * `i`: An eve::index instance indicating which lane of `x` to broadcast.
-  //!     * `sz`: An eve::lane instance indicating how many lanes to broadcast to.
-  //!
-  //!   **Return value**
-  //!     1. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T>{v}`.
-  //!     2. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T,eve::fixed<N>>{v}`.
-  //!     3. A [SIMD value](@ref eve::simd_value) equals to `T{v.get(I)}`.
-  //!     4. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T,eve::fixed<N>>{v.get(I)}`.
-  //!
-  //!   @groupheader{Example}
-  //!
-  //!   @godbolt{doc/core/broadcast.cpp}
-  //! @}
-  //================================================================================================
+//================================================================================================
+//! @addtogroup core_simd
+//! @{
+//!   @var broadcast
+//!   @brief Computes the
+//!
+//!   **Defined in Header**
+//!
+//!   @code
+//!   #include <eve/module/core.hpp>
+//!   @endcode
+//!
+//!   @groupheader{Callable Signatures}
+//!
+//!   @code
+//!   namespace eve
+//!   {
+//!     template<eve::scalar_value T> eve::as_wide_t<T> broadcast(T v) noexcept;                            // 1
+//!     template<eve::scalar_value T, std::ptrdiff_t N> as_wide_t<T> broadcast(T v, fixed<N> sz) noexcept;  // 2
+//!     template<eve::simd_value T, std::size_t I> T broadcast(T v, index_t<I> i) noexcept;                 // 3
+//!     template<eve::simd_value T, std::size_t I, std::ptrdiff_t N>
+//!     eve::as_wide_t<T,fixed<N>> broadcast(T v, index_t<I> i, fixed<N> sz) noexcept;                      // 4
+//!   }
+//!   @endcode
+//!
+//!   **Parameters**
+//!     * `v`: A [value](@ref eve::simd_value).
+//!     * `i`: An eve::index instance indicating which lane of `x` to broadcast.
+//!     * `sz`: An eve::lane instance indicating how many lanes to broadcast to.
+//!
+//!   **Return value**
+//!     1. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T>{v}`.
+//!     2. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T,eve::fixed<N>>{v}`.
+//!     3. A [SIMD value](@ref eve::simd_value) equals to `T{v.get(I)}`.
+//!     4. A [SIMD value](@ref eve::simd_value) equals to `eve::as_wide_t<T,eve::fixed<N>>{v.get(I)}`.
+//!
+//!   @groupheader{Example}
+//!
+//!   @godbolt{doc/core/broadcast.cpp}
+//================================================================================================
   inline constexpr auto broadcast = functor<broadcast_t>;
+//================================================================================================
+//! @}
+//================================================================================================
 }
 
 #include <eve/module/core/regular/impl/broadcast.hpp>
