7580b1c948a041a4d3e81f53541f066f96cd8783
jfalcou <jfalcou@users.noreply.github.com>
2024-08-06 07:16:05 +0000

    deploy: 00d6bfb062b9ce5ab5563e76f6c8356687e5b942

--- search/groups_9.js
+++ search/groups_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['mathematical_20functions_0',['Mathematical functions',['../group__math.html',1,'']]],
-  ['memory_20management_1',['Memory Management',['../group__memory.html',1,'']]]
+  ['math_5fsigmoid_0',['Math_sigmoid',['../group__math__sigmoid.html',1,'']]],
+  ['mathematical_20functions_1',['Mathematical functions',['../group__math.html',1,'']]],
+  ['memory_20management_2',['Memory Management',['../group__memory.html',1,'']]]
 ];
