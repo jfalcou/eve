ec487cf019d4a1a47b34fd303a966964334ce52f
jtlap <jtlapreste@gmail.com>
2024-08-18 15:28:00 +0200

    Improve countl_zero, bit_ceil and bit_floor performances

--- benchmarks/module/core/bit_ceil/regular/bit_ceil.hpp
+++ benchmarks/module/core/bit_ceil/regular/bit_ceil.hpp
@@ -13,12 +13,20 @@ int main()
   auto lmin = eve::valmin(eve::as<EVE_VALUE>());
   auto lmax = eve::valmax(eve::as<EVE_VALUE>());
 
-  auto const std__bit_ceil = [](EVE_VALUE x) { return x > 0 ? x : EVE_VALUE(-x); };
-
   auto arg0 = eve::bench::random_<EVE_VALUE>(lmin,lmax);
+  auto const std__bit_ceil = [](auto x) {
+    if constexpr(std::is_integral_v<decltype(x)>)
+       return std::bit_ceil(x);
+    else return x;
+  };
 
   eve::bench::experiment xp;
-  run<EVE_VALUE> (EVE_NAME(std__bit_ceil) , xp, std__bit_ceil, arg0);
+  using v_t = EVE_VALUE;
+  if constexpr(std::is_integral_v<v_t>)
+  {
+    run<EVE_VALUE> (EVE_NAME(std__bit_floor) , xp, std__bit_ceil, arg0);
+  }
+
   run<EVE_VALUE> (EVE_NAME(bit_ceil) , xp, eve::bit_ceil, arg0);
   run<EVE_TYPE>  (EVE_NAME(bit_ceil) , xp, eve::bit_ceil, arg0);
 }
--- include/eve/module/core/regular/impl/simd/x86/countl_zero.hpp
+++ include/eve/module/core/regular/impl/simd/x86/countl_zero.hpp
@@ -9,7 +9,12 @@
 
 #include <eve/concept/value.hpp>
 #include <eve/detail/implementation.hpp>
-
+#include <eve/module/core/regular/bit_shr.hpp>
+#include <eve/module/core/regular/bit_notand.hpp>
+#include <eve/module/core/regular/convert.hpp>
+#include <eve/module/core/regular/sub.hpp>
+#include <eve/module/core/regular/min.hpp>
+#include <eve/module/core/regular/bit_cast.hpp>
 #include <type_traits>
 
 namespace eve::detail
@@ -22,14 +27,41 @@ namespace eve::detail
   {
     using r_t        = wide<T, N>;
     constexpr auto c = categorize<r_t>();
-
-    if constexpr( current_api < avx512 || sizeof(T) < 4 ) return countl_zero.behavior(cpu_{}, opts, a0);
-    else if constexpr( c == category::uint64x8 ) return r_t(_mm512_lzcnt_epi64(a0));
-    else if constexpr( c == category::uint32x16) return r_t(_mm512_lzcnt_epi32(a0));
-    else if constexpr( c == category::uint64x4 ) return r_t(_mm256_lzcnt_epi64(a0));
-    else if constexpr( c == category::uint32x8 ) return r_t(_mm256_lzcnt_epi32(a0));
-    else if constexpr( c == category::uint64x2 ) return r_t(_mm_lzcnt_epi64(a0));
-    else if constexpr( c == category::uint32x4 ) return r_t(_mm_lzcnt_epi32(a0));
+    if constexpr(sizeof(T) >=  4)
+    {
+      if constexpr( current_api == avx512)
+      {
+        if constexpr( c == category::uint64x8 ) return r_t(_mm512_lzcnt_epi64(a0));
+        else if constexpr( c == category::uint32x16) return r_t(_mm512_lzcnt_epi32(a0));
+        else if constexpr( c == category::uint64x4 ) return r_t(_mm256_lzcnt_epi64(a0));
+        else if constexpr( c == category::uint32x8 ) return r_t(_mm256_lzcnt_epi32(a0));
+        else if constexpr( c == category::uint64x2 ) return r_t(_mm_lzcnt_epi64(a0));
+        else if constexpr( c == category::uint32x4 ) return r_t(_mm_lzcnt_epi32(a0));
+      }
+      else if constexpr( current_api >= sse2 )
+      {
+        //Inspired from: https://stackoverflow.com/questions/58823140/count-leading-zero-bits-for-each-element-in-avx2-vector-emulate-mm256-lzcnt-ep
+        using ri_t       = wide<std::int32_t,N>;
+        using ru_t2      = downgrade_t<r_t>;
+        if constexpr( match(c, category::uint32x8, category::uint32x4 ))
+        {
+          auto    v   = bit_notand(bit_shr(a0, 8), a0);
+          v   = bit_cast(convert(bit_cast(v, as<ri_t>{}), as<float>{}), as<r_t>{});
+          v   = bit_shr(v, 23);
+          auto w   = bit_cast(v, as<ru_t2>{});
+          w   = sub[saturated2](158, w);                    // undo bias
+          auto v32 = bit_cast(r_t(32u), as<ru_t2>{});
+          return bit_cast(eve::min(w,v32), as<r_t>());
+        }
+        else return countl_zero.behavior(cpu_{}, opts, a0);
+      }
+      else
+        return countl_zero.behavior(cpu_{}, opts, a0);
+    }
+    else
+    {
+      return countl_zero.behavior(cpu_{}, opts, a0);
+    }
   }
 
 // -----------------------------------------------------------------------------------------------
