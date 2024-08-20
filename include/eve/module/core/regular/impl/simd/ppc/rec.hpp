658ab9e3fc52fca595f0bdefcec11f3a825f1922
SadiinsoSnowfall <sadiinso@gmail.com>
2024-08-20 15:07:21 +0200

    fix typo

--- include/eve/module/core/regular/impl/simd/ppc/rec.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/rec.hpp
@@ -25,7 +25,7 @@ namespace eve::detail
 
   template<floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N>
-  rec_(EVE_REQUIRES(neon128_), O const&, wide<T, N> v0) noexcept
+  rec_(EVE_REQUIRES(vmx_), O const&, wide<T, N> v0) noexcept
   requires ppc_abi<abi_t<T, N>>
   {
     if constexpr(O::contains(raw))
