76f66fa40b5e113034fd9f4ca410d7409f85a6a0
SadiinsoSnowfall <sadiinso@gmail.com>
2024-08-26 10:45:00 +0200

    go

--- include/eve/detail/function/simd/arm/sve/subscript.hpp
+++ include/eve/detail/function/simd/arm/sve/subscript.hpp
@@ -8,24 +8,32 @@
 #pragma once
 
 #include <eve/detail/abi.hpp>
+#include <eve/detail/alias.hpp>
+#include <eve/detail/has_abi.hpp>
+#include <eve/traits/element_type.hpp>
+#include <eve/as.hpp>
 #include <eve/arch/arm/sve/sve_true.hpp>
 
 namespace eve::detail
 {
+
 template<callable_options O, typename T, typename N>
-EVE_FORCEINLINE T extract_(EVE_REQUIRES(sve_), O const&, wide<T, N> v, std::size_t i) noexcept requires sve_abi<abi_t<T, N>>
+EVE_FORCEINLINE T extract_(EVE_REQUIRES(sve_), O const&, wide<T, N> const& v, std::size_t i) noexcept
+  requires sve_abi<abi_t<T, N>>
 {
   return v.storage()[i];
 }
 
 template<callable_options O, typename T, typename N>
-EVE_FORCEINLINE T insert_(EVE_REQUIRES(sve_), O const&, wide<T, N>& v, std::size_t i, T x) noexcept requires sve_abi<abi_t<T, N>>
+EVE_FORCEINLINE T insert_(EVE_REQUIRES(sve_), O const&, wide<T, N>& v, std::size_t i, T x) noexcept
+  requires sve_abi<abi_t<T, N>>
 {
   return v.storage()[i] = x;
 }
 
 template<callable_options O, typename T, typename N>
-EVE_FORCEINLINE logical<T> extract_(EVE_REQUIRES(sve_), O const&, logical<wide<T, N>> v, std::size_t i) noexcept requires sve_abi<abi_t<T, N>>
+EVE_FORCEINLINE logical<T> extract_(EVE_REQUIRES(sve_), O const&, logical<wide<T, N>> const& v, std::size_t i) noexcept
+  requires sve_abi<abi_t<T, N>>
 {
   auto bits = [&]() -> typename logical<wide<T, N>>::bits_type
   {
@@ -41,7 +49,8 @@ EVE_FORCEINLINE logical<T> extract_(EVE_REQUIRES(sve_), O const&, logical<wide<T
 }
 
 template<callable_options O, typename T, typename N>
-EVE_FORCEINLINE void insert_(EVE_REQUIRES(sve_), O const&, logical<wide<T, N>>& v, std::size_t i, auto x) noexcept requires sve_abi<abi_t<T, N>>
+EVE_FORCEINLINE void insert_(EVE_REQUIRES(sve_), O const&, logical<wide<T, N>>& v, std::size_t i, auto x) noexcept
+  requires sve_abi<abi_t<T, N>>
 {
   using bits_type = typename logical<wide<T, N>>::bits_type;
   using e_t       = element_type_t<bits_type>;
