da67fc34fe92227293a299c654cf58a9c363629f
Joel Falcou <joel.falcou@lri.fr>
2024-08-28 16:36:01 +0200

    No integral NaNs

--- test/doc/core/constant/nan.cpp
+++ test/doc/core/constant/nan.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 using wide_ft = eve::wide<float>;
-using wide_it = eve::wide<std::int16_t>;
 
 template<typename T>
 consteval auto constexpr_nan() { return eve::nan(eve::as<T>{}); }
@@ -11,22 +10,16 @@ consteval auto constexpr_nan() { return eve::nan(eve::as<T>{}); }
 int main()
 {
   wide_ft wxf;
-  wide_it wxi;
 
   std::cout << "---- simd"  << std::endl
             << "-> nan(as<wide_ft>())  = " << eve::nan(eve::as<wide_ft>()) << std::endl
-            << "-> nan(as<wide_it>())  = " << eve::nan(eve::as<wide_it>()) << std::endl
-            << "-> nan(as(wxf))        = " << eve::nan(eve::as(wxf))       << std::endl
-            << "-> nan(as(wxi))        = " << eve::nan(eve::as(wxi))       << std::endl;
+            << "-> nan(as(wxf))        = " << eve::nan(eve::as(wxf))       << std::endl;
 
   double       xf;
-  std::int16_t xi;
 
   std::cout << "---- scalar" << std::endl
             << "-> nan(as<float>())         = " << eve::nan(eve::as(float())) << std::endl
-            << "-> nan(as<std::int16_t>())  = " << eve::nan(eve::as(std::int16_t())) << std::endl
-            << "-> nan(as<xf))              = " << eve::nan(eve::as(xf)) << std::endl
-            << "-> nan(as<xi))              = " << eve::nan(eve::as(xi)) << std::endl;
+            << "-> nan(as<xf))              = " << eve::nan(eve::as(xf)) << std::endl;
 
   std::cout << "-> constexpr nan            = " << constexpr_nan<float>() << std::endl;
 
