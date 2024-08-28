26c3a16cf43d13c8271298116e3fa8a994bc2c43
SadiinsoSnowfall <sadiinso@gmail.com>
2024-08-28 12:40:42 +0200

    add missing saturated fallback

--- include/eve/module/core/regular/impl/simd/x86/mul.hpp
+++ include/eve/module/core/regular/impl/simd/x86/mul.hpp
@@ -12,77 +12,84 @@ namespace eve::detail
   template<callable_options O, typename T, typename N>
   EVE_FORCEINLINE wide<T, N> mul_(EVE_REQUIRES(sse2_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
   {
-    constexpr auto c = categorize<wide<T, N>>();
-
-          if constexpr( c == category::float64x8  ) return _mm512_mul_pd(a, b);
-    else  if constexpr( c == category::float64x4  ) return _mm256_mul_pd(a, b);
-    else  if constexpr( c == category::float64x2  ) return _mm_mul_pd(a, b);
-    else  if constexpr( c == category::float32x16 ) return _mm512_mul_ps(a, b);
-    else  if constexpr( c == category::float32x8  ) return _mm256_mul_ps(a, b);
-    else  if constexpr( c == category::float32x4  ) return _mm_mul_ps(a, b);
-    else  if constexpr( c == category::int64x8    ) return _mm512_mullo_epi64(a, b);
-    else  if constexpr( c == category::uint64x8   ) return _mm512_mullo_epi64(a, b);
-    else  if constexpr( c == category::int32x16   ) return _mm512_mullo_epi32(a, b);
-    else  if constexpr( c == category::uint32x16  ) return _mm512_mullo_epi32(a, b);
-    else  if constexpr(((c == category::int32x8) || (c == category::uint32x8)) && (current_api >= avx2))
+    if constexpr (O::contains(saturated2) && std::integral<T>)
     {
-      return _mm256_mullo_epi32(a, b);
-    }
-    else  if constexpr ((c == category::int32x4) || (c == category::uint32x4))
-    {
-      if constexpr (current_api >= sse4_1)
-      {
-        return _mm_mullo_epi32(a, b);
-      }
-      else
-      {
-        static constexpr auto half_size = ((N::value / 2) > 0) ? N::value / 2 : 1;
-        using htype  = wide<std::int64_t, fixed<half_size>>;
-
-        htype mhi    = _mm_setr_epi32(-1, 0, -1, 0);
-        htype mlo    = mhi;
-        auto  la     = _mm_srli_si128(a, 4);
-        auto  lb     = _mm_srli_si128(b, 4);
-
-        mhi &= htype {_mm_mul_epu32(a, b)};
-        mlo &= htype {_mm_mul_epu32(la, lb)};
-        mhi |= htype {_mm_slli_si128(mlo, 4)};
-
-        return mhi.storage();
-      }
-    }
-    else  if constexpr((c == category::int16x16 || c == category::uint16x16) && current_api >= avx2)
-    {
-      return _mm256_mullo_epi16(a, b);
-    }
-    else  if constexpr( c == category::int16x32   ) return _mm512_mullo_epi16(a, b);
-    else  if constexpr( c == category::uint16x32  ) return _mm512_mullo_epi16(a, b);
-    else  if constexpr( c == category::int16x8    ) return _mm_mullo_epi16(a, b);
-    else  if constexpr( c == category::uint16x8   ) return _mm_mullo_epi16(a, b);
-    else  if constexpr ((c == category::int8x16) || (c == category::uint8x16))
-    {
-      static constexpr auto half_size = ((N::value / 2) > 0) ? N::value / 2 : 1;
-
-      using htype  = wide<std::int16_t, fixed<half_size>>;
-
-      htype mhi    = _mm_set1_epi16(0x00FF);
-      htype mlo    = mhi;
-      auto  la  = _mm_srli_epi16(a, 8);
-      auto  lb = _mm_srli_epi16(b, 8);
-
-      mhi &= htype {_mm_mullo_epi16(a, b)};
-      mlo &= htype {_mm_mullo_epi16(la, lb)};
-      mhi |= htype {_mm_slli_epi16(mlo, 8)};
-
-      return mhi.storage();
+      return mul.behavior(cpu_{}, opts, a, b);
     }
     else
     {
-      auto s = a;
-      constexpr auto smul = [](auto a, auto b) { return a * b; };
+      constexpr auto c = categorize<wide<T, N>>();
 
-      if constexpr (N::value >= 2) return aggregate(smul, s, b);
-      else                         return map(smul, s, b);
+            if constexpr( c == category::float64x8  ) return _mm512_mul_pd(a, b);
+      else  if constexpr( c == category::float64x4  ) return _mm256_mul_pd(a, b);
+      else  if constexpr( c == category::float64x2  ) return _mm_mul_pd(a, b);
+      else  if constexpr( c == category::float32x16 ) return _mm512_mul_ps(a, b);
+      else  if constexpr( c == category::float32x8  ) return _mm256_mul_ps(a, b);
+      else  if constexpr( c == category::float32x4  ) return _mm_mul_ps(a, b);
+      else  if constexpr( c == category::int64x8    ) return _mm512_mullo_epi64(a, b);
+      else  if constexpr( c == category::uint64x8   ) return _mm512_mullo_epi64(a, b);
+      else  if constexpr( c == category::int32x16   ) return _mm512_mullo_epi32(a, b);
+      else  if constexpr( c == category::uint32x16  ) return _mm512_mullo_epi32(a, b);
+      else  if constexpr(((c == category::int32x8) || (c == category::uint32x8)) && (current_api >= avx2))
+      {
+        return _mm256_mullo_epi32(a, b);
+      }
+      else  if constexpr ((c == category::int32x4) || (c == category::uint32x4))
+      {
+        if constexpr (current_api >= sse4_1)
+        {
+          return _mm_mullo_epi32(a, b);
+        }
+        else
+        {
+          static constexpr auto half_size = ((N::value / 2) > 0) ? N::value / 2 : 1;
+          using htype  = wide<std::int64_t, fixed<half_size>>;
+
+          htype mhi    = _mm_setr_epi32(-1, 0, -1, 0);
+          htype mlo    = mhi;
+          auto  la     = _mm_srli_si128(a, 4);
+          auto  lb     = _mm_srli_si128(b, 4);
+
+          mhi &= htype {_mm_mul_epu32(a, b)};
+          mlo &= htype {_mm_mul_epu32(la, lb)};
+          mhi |= htype {_mm_slli_si128(mlo, 4)};
+
+          return mhi.storage();
+        }
+      }
+      else  if constexpr((c == category::int16x16 || c == category::uint16x16) && current_api >= avx2)
+      {
+        return _mm256_mullo_epi16(a, b);
+      }
+      else  if constexpr( c == category::int16x32   ) return _mm512_mullo_epi16(a, b);
+      else  if constexpr( c == category::uint16x32  ) return _mm512_mullo_epi16(a, b);
+      else  if constexpr( c == category::int16x8    ) return _mm_mullo_epi16(a, b);
+      else  if constexpr( c == category::uint16x8   ) return _mm_mullo_epi16(a, b);
+      else  if constexpr ((c == category::int8x16) || (c == category::uint8x16))
+      {
+        static constexpr auto half_size = ((N::value / 2) > 0) ? N::value / 2 : 1;
+
+        using htype  = wide<std::int16_t, fixed<half_size>>;
+
+        htype mhi    = _mm_set1_epi16(0x00FF);
+        htype mlo    = mhi;
+        auto  la  = _mm_srli_epi16(a, 8);
+        auto  lb = _mm_srli_epi16(b, 8);
+
+        mhi &= htype {_mm_mullo_epi16(a, b)};
+        mlo &= htype {_mm_mullo_epi16(la, lb)};
+        mhi |= htype {_mm_slli_epi16(mlo, 8)};
+
+        return mhi.storage();
+      }
+      else
+      {
+        auto s = a;
+        constexpr auto smul = [](auto a, auto b) { return a * b; };
+
+        if constexpr (N::value >= 2) return aggregate(smul, s, b);
+        else                         return map(smul, s, b);
+      }
     }
   }
 
