2115b1fe018f3b344f905319d8fd9415fd4cb420
SadiinsoSnowfall <sadiinso@gmail.com>
2024-08-29 11:37:40 +0200

    updated common_value

--- include/eve/traits/common_value.hpp
+++ include/eve/traits/common_value.hpp
@@ -15,34 +15,41 @@
 namespace eve::detail
 {
   template<typename T>
-  struct find_type_impl {
+  struct find_common_value_reducer;
+
+  template<>
+  struct find_common_value_reducer<void> {};
+
+  template<typename T>
+  struct find_common_value_reducer {
     using type = T;
 
-    template<value U>
-    friend consteval find_type_impl<std::conditional_t<
-        scalar_value<T> && scalar_value<U>,
-          decltype(std::declval<T>() + std::declval<U>()),
-          std::conditional_t<
-              (scalar_value<T> && arithmetic_simd_value<U>) ||
-              (arithmetic_simd_value<T> && scalar_value<U>) ||
-              (arithmetic_simd_value<T> && std::same_as<T, U>),
-                std::conditional_t<arithmetic_simd_value<T>, T, U>,
-                void
-          >
-    >> operator%(find_type_impl, find_type_impl<U>) noexcept { return {}; }
+    template <typename U>
+    friend auto operator%(
+      find_common_value_reducer x,
+      find_common_value_reducer<U> y
+    ) {
+      if constexpr (plain_scalar_value<T> && plain_scalar_value<U>) {
+        return find_common_value_reducer<decltype(std::declval<T>() + std::declval<U>())>{};
+      } else if constexpr (plain_scalar_value<T> && arithmetic_simd_value<U>) {
+        return y;
+      } else if constexpr (arithmetic_simd_value<T> && (plain_scalar_value<U> || std::same_as<T, U>)) {
+        return x;
+      } else {
+        return find_common_value_reducer<void>{};
+      }
+    }
   };
+  
+  template<typename... Ts>
+  using find_common_value_wide = typename decltype((find_common_value_reducer<Ts>{} % ...))::type;
 
   template<typename... Ts>
-  using find_type = typename decltype((find_type_impl<Ts>{} % ...))::type;
-
-  template<typename... Ts>
-  requires(!scalar_value<Ts> || ... )
-  auto find_common_value() -> find_type<Ts... >;
+  requires (!plain_scalar_value<Ts> || ...)
+  auto find_common_value() -> find_common_value_wide<Ts... >;
 
   template<typename T0, typename... Ts>
-  requires(   (scalar_value<T0> && ... && scalar_value<Ts>)
-          &&  (std::same_as<T0,Ts> && ...)
-          )
+  requires((plain_scalar_value<T0> && ... && plain_scalar_value<Ts>) && (std::same_as<T0,Ts> && ...))
   T0 find_common_value();
 
   template<typename, typename... Ts>
