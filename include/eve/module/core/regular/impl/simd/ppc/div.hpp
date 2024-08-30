3c03314e14a08f6afc8034dd923fde09dc11513f
SadiinsoSnowfall <sadiinso@gmail.com>
2024-08-30 11:31:55 +0200

    small ppc fix

--- include/eve/module/core/regular/impl/simd/ppc/div.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/div.hpp
@@ -13,7 +13,7 @@
 namespace eve::detail
 {
   template<callable_options O, floating_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N> div_(EVE_REQUIRES(vsx_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
+  EVE_FORCEINLINE wide<T, N> div_(EVE_REQUIRES(vmx_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
     requires ppc_abi<abi_t<T, N>>
   {
     if constexpr (O::contains(saturated2))
