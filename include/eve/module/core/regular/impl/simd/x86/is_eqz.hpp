01fd4e8f40864b73c3eea094e17922228c62c170
jtlap <jtlapreste@gmail.com>
2024-09-01 15:24:07 +0200

    is_eqz

---
+++ include/eve/module/core/regular/impl/simd/x86/is_eqz.hpp
@@ -0,0 +1,76 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/detail/implementation.hpp>
+#include <eve/module/core/regular/bit_cast.hpp>
+#include <eve/traits/as_logical.hpp>
+
+namespace eve::detail
+{
+  // Generate better code on x86 than just comparing to 0
+  template<integral_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE constexpr auto
+  is_ltz_(EVE_REQUIRES(sse2_), O const &, wide<T, N> const& v) noexcept
+  requires(sizeof(T) < 8 && x86_abi<abi_t<T, N>> && abi_t<T, N>::is_wide_logical)
+  {
+    using l_t            = as_logical_t<wide<T, N>>;
+    constexpr auto shift = 8 * sizeof(T) - 1;
+    return bit_cast(v >> shift, as<l_t> {});
+  }
+
+  template<floating_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE logical<wide<T, N>> is_ltz_(EVE_REQUIRES(avx512_),
+                                                   O          const &,
+                                                   wide<T, N> const &a) noexcept
+  requires x86_abi<abi_t<T, N>>
+  {
+    using l_t        = logical<wide<T, N>>;
+    constexpr auto c = categorize<wide<T, N>>();
+    constexpr auto f = fpclass::poszero | fpclass::negzero;
+
+    using s_t = typename l_t::storage_type;
+
+         if constexpr( c == category::float64x8 ) return s_t {_mm512_fpclass_pd_mask(a, f)};
+    else if constexpr( c == category::float64x4 ) return s_t {_mm256_fpclass_pd_mask(a, f)};
+    else if constexpr( c == category::float64x2 ) return s_t {_mm_fpclass_pd_mask(a, f)};
+    else if constexpr( c == category::float32x16) return s_t {_mm512_fpclass_ps_mask(a, f)};
+    else if constexpr( c == category::float32x8 ) return s_t {_mm256_fpclass_ps_mask(a, f)};
+    else if constexpr( c == category::float32x4 ) return s_t {_mm_fpclass_ps_mask(a, f)};
+  }
+
+
+// -----------------------------------------------------------------------------------------------
+// masked  implementation
+  template<conditional_expr C, floating_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE auto is_ltz_(EVE_REQUIRES(avx512_),
+                                     C const& cx,
+                                     O const& o,
+                                     wide<T, N> const& v) noexcept
+    requires x86_abi<abi_t<T, N>>
+  {
+    constexpr auto c = categorize<wide<T, N>>();
+
+    if constexpr( C::has_alternative || C::is_complete || abi_t<T, N>::is_wide_logical )
+    {
+      return is_eqz.behavior(cpu_{}, o, v, v);
+    }
+    else
+    {
+      auto           m = expand_mask(cx, as<wide<T, N>> {}).storage().value;
+      constexpr auto f = fpclass::poszero | fpclass::negzero;
+
+      if constexpr( c == category::float32x16 )     return mask16 {_mm512_mask_fpclass_ps_mask(m, v, f)};
+      else if constexpr( c == category::float64x8 ) return mask8 {_mm512_mask_fpclass_pd_mask(m, v, f)};
+      else if constexpr( c == category::float32x8 ) return mask8 {_mm256_mask_fpclass_ps_mask(m, v, f)};
+      else if constexpr( c == category::float64x4 ) return mask8 {_mm256_mask_fpclass_pd_mask(m, v, f)};
+      else if constexpr( c == category::float32x4 ) return mask8 {_mm_mask_fpclass_ps_mask(m, v, f)};
+      else if constexpr( c == category::float64x2 ) return mask8 {_mm_mask_fpclass_pd_mask(m, v, f)};
+    }
+  }
+}
--- test/doc/core/is_eqz.cpp
+++ test/doc/core/is_eqz.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-  eve::wide wf0{0.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
+  eve::wide wf0{0.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -0.0};
   eve::wide wi0{0, 1, 2, 3, -1, -2, -3, -4};
   eve::wide wu0{0u, 1u, 2u, 3u, 4u, 5u, 6u, 7u};
 
   std::cout << "<- wf0                         = " << wf0 << "\n";
   std::cout << "<- wi0                         = " << wi0 << "\n";
   std::cout << "<- wu0                         = " << wu0 << "\n";
-                                               
+
   std::cout << "-> is_eqz(wf0)                 = " << eve::is_eqz(wf0) << "\n";
   std::cout << "-> is_eqz[ignore_last(2)](wf0) = " << eve::is_eqz[eve::ignore_last(2)](wf0) << "\n";
   std::cout << "-> is_eqz[wf0 != 0](wf0)       = " << eve::is_eqz[wf0 != 0](wf0) << "\n";
