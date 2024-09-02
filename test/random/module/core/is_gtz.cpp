795e71b70eb789e9440acdb95db463aeed6a568d
jtlap <jtlapreste@gmail.com>
2024-09-02 11:30:40 +0200

    is_gtz

--- test/random/module/core/is_gtz.cpp
+++ test/random/module/core/is_gtz.cpp
@@ -10,12 +10,12 @@
 
 #include <cmath>
 
-TTS_CASE_TPL("Random check for eve::is_ltz", eve::test::simd::all_types)
+TTS_CASE_TPL("Random check for eve::is_gtz", eve::test::simd::all_types)
 <typename T>(tts::type<T>)
 {
   using e_t = eve::element_type_t<T>;
   auto vmin = eve::valmin(eve::as<e_t>());
   auto vmax = eve::valmax(eve::as<e_t>());
-  auto std_is_ltz = [](auto e) -> eve::logical<e_t>{ return e < 0; };
-  EVE_ULP_RANGE_CHECK( T, eve::uniform_prng<e_t>(vmin, vmax),  std_is_ltz, eve::is_ltz );
+  auto std_is_gtz = [](auto e) -> eve::logical<e_t>{ return e > 0; };
+  EVE_ULP_RANGE_CHECK( T, eve::uniform_prng<e_t>(vmin, vmax),  std_is_gtz, eve::is_gtz );
  };
