9e4415d00abfdf4b94aec313ce671dc90a92171a
jtlap <jtlapreste@gmail.com>
2024-09-03 14:30:56 +0200

    is_nemz typo

--- test/unit/module/core/is_nemz.cpp
+++ test/unit/module/core/is_nemz.cpp
@@ -33,6 +33,6 @@ TTS_CASE_WITH("Check behavior of eve::is_nemz(simd)",
   using eve::detail::map;
   using v_t = eve::element_type_t<T>;
 
-  TTS_EQUAL(eve::is_nemz(a0), map([](auto e) -> eve::logical<v_t> { return eve::isltz(e) || eve::is_positive(e); }, a0));
+  TTS_EQUAL(eve::is_nemz(a0), map([](auto e) -> eve::logical<v_t> { return eve::is_ltz(e) || eve::is_positive(e); }, a0));
   TTS_EQUAL(eve::is_nemz[t](a0), eve::if_else(t, eve::is_nemz(a0), eve::false_(eve::as(a0))));
 };
