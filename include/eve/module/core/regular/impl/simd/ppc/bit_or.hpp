c463b4ff7e3662e0f0cc551d23a51fc575eae7a5
SadiinsoSnowfall <sadiinso@gmail.com>
2024-09-10 01:56:20 +0200

    fix

--- include/eve/module/core/regular/impl/simd/ppc/bit_or.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/bit_or.hpp
@@ -13,7 +13,7 @@
 namespace eve::detail
 {
   template<callable_options O, typename T, typename N>
-  EVE_FORCEINLINE bool bit_or_(EVE_SUPPORTS(vmx_), O const&, wide<T, N> a, wide<T, N> b) noexcept
+  EVE_FORCEINLINE wide<T, N> bit_or_(EVE_SUPPORTS(vmx_), O const&, wide<T, N> a, wide<T, N> b) noexcept
     requires ppc_abi<abi_t<T, N>>
   {
     return vec_or(a.storage(), b.storage());
