9dfe4bb47681a01de8c6384f0daa3441e96bfcad
SadiinsoSnowfall <sadiinso@gmail.com>
2024-09-09 18:22:36 +0200

    removed unused parameter

--- include/eve/module/core/regular/impl/simd/arm/sve/bit_xor.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/bit_xor.hpp
@@ -13,7 +13,7 @@
 namespace eve::detail
 {
   template<callable_options O, typename T, typename N>
-  EVE_FORCEINLINE wide<T, N> bit_xor_(EVE_REQUIRES(sve_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
+  EVE_FORCEINLINE wide<T, N> bit_xor_(EVE_REQUIRES(sve_), O const&, wide<T, N> a, wide<T, N> b) noexcept
     requires sve_abi<abi_t<T, N>>
   {
     // no sveor_x for floating point types 
