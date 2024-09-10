9855b4062ee8c9e4ca3f64282f77db2648e272d1
SadiinsoSnowfall <sadiinso@gmail.com>
2024-09-10 15:41:37 +0200

    fixed some more unit tests

--- test/doc/core/bit_shr.cpp
+++ test/doc/core/bit_shr.cpp
@@ -5,12 +5,13 @@
 int main()
 {
   eve::wide wi0{0, 1, 2, 3, -1, -2, -3, -4};
-  eve::wide wi1{0, -4, 1, -1, 2, -2, 3, -3};
+  eve::wide wi1{0, 4, 1, 1, 2, 2, 3, 3};
+  eve::wide wi2{0, -4, 1, -1, 2, -2, 3, -3};
 
   std::cout << "<- wi0                               = " << wi0 << "\n";
   std::cout << "<- wi1                               = " << wi1 << "\n";
                                                      
   std::cout << "-> bit_shr(wi0, wi1)                 = " << eve::bit_shr(wi0, wi1) << "\n";
   std::cout << "-> bit_shr[ignore_last(2)](wi0, wi1) = " << eve::bit_shr[eve::ignore_last(2)](wi0, wi1) << "\n";
-  std::cout << "-> bit_shr[wi0 != 0](wi0, wi1)       = " << eve::bit_shr[wi0 != 0](wi0, wi1) << "\n";
+  std::cout << "-> bit_shr[wi2 > 0](wi0, wi2)       = " << eve::bit_shr[wi2 > 0](wi0, wi2) << "\n";
 }
