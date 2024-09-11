c165f5a6525c368fe237151ab9006fac198d8dec
SadiinsoSnowfall <sadiinso@gmail.com>
2024-09-11 17:14:15 +0200

    added backends

--- include/eve/detail/function/simd/arm/sve/friends.hpp
+++ include/eve/detail/function/simd/arm/sve/friends.hpp
@@ -42,26 +42,6 @@ EVE_FORCEINLINE auto
 self_geq(wide<T, N> v, wide<T, N> w) noexcept -> as_logical_t<wide<T, N>>
 requires sve_abi<abi_t<T, N>> { return svcmpge(sve_true<T>(), v, w); }
 
-
-// ---------------------------------------------------------
-
-template<typename T, typename U, typename N>
-EVE_FORCEINLINE auto
-self_logand(sve_ const&, logical<wide<T,N>> v, logical<wide<U,N>> w) noexcept -> logical<wide<T, N>>
-requires(sve_abi<abi_t<T, N>> || sve_abi<abi_t<U, N>>)
-{
-  if constexpr(!is_aggregated_v<abi_t<T, N>>)
-  {
-    return svmov_z(v, convert(w, as<logical<T>>{}));
-  }
-  else
-  {
-    auto[lv,hv] = v.slice();
-    auto[lw,hw] = w.slice();
-    return logical<wide<T, N>>{ lv && lw, hv && hw};
-  }
-}
-
 template<typename T, typename U, typename N>
 EVE_FORCEINLINE auto
 self_logor(sve_ const&, logical<wide<T,N>> v, logical<wide<U,N>> w) noexcept -> logical<wide<T, N>>
