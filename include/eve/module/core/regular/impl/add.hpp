31ac31379441972922422370ec63c24558d70b8b
jtlap <jtlapreste@gmail.com>
2024-09-12 11:17:21 +0200

    add downard attempts

--- include/eve/module/core/regular/impl/add.hpp
+++ include/eve/module/core/regular/impl/add.hpp
@@ -18,13 +18,28 @@
 #include <eve/module/core/regular/bit_mask.hpp>
 #include <eve/module/core/constant/valmax.hpp>
 #include <eve/module/core/constant/valmin.hpp>
+#include <eve/module/core/detail/roundings.hpp>
 
 namespace eve::detail
 {
   template<callable_options O, typename T>
   EVE_FORCEINLINE constexpr T add_(EVE_REQUIRES(cpu_), O const&, T a, T b) noexcept
   {
-    if constexpr(O::contains(saturated2) && integral_value<T>)
+    if constexpr(O::contains(downward))
+    {
+      std::fesetround(round_down);
+      auto r = add(a, b);
+      std::fesetround(round_to_nearest);
+      return r;
+    }
+    else if constexpr(O::contains(downward))
+    {
+      std::fesetround(round_up);
+      auto r = add(a, b);
+      std::fesetround(round_to_nearest);
+      return r;
+    }
+    else if constexpr(O::contains(saturated2) && integral_value<T>)
     {
       if constexpr( signed_integral_value<T> )
       {
@@ -55,8 +70,29 @@ namespace eve::detail
   {
     //TODO: both GCC and Clang can fail to properly reorder the op chain to reduce dependencies
     //      we might want to do this manually
-    r0   = add[o](r0,r1);
-    ((r0 = add[o](r0,rs)),...);
-    return r0;
+    if constexpr(O::contains(downward))
+    {
+      std::fesetround(round_down);
+      auto tmp1 =  rbr::drop(downward, o);
+      auto oo = options<decltype(tmp1)>{tmp1};
+      auto r = add[oo](r0, rs...);
+      std::fesetround(round_to_nearest);
+      return r;
+    }
+    else if constexpr(O::contains(upward))
+    {
+      std::fesetround(round_up);
+      auto tmp1 =  rbr::drop(upward, o);
+      auto oo = options<decltype(tmp1)>{tmp1};
+      auto r = add[oo](r0, rs...);
+      std::fesetround(round_to_nearest);
+      return r;
+    }
+    else
+    {
+      r0   = add[o](r0,r1);
+      ((r0 = add[o](r0,rs)),...);
+      return r0;
+    }
   }
 }
