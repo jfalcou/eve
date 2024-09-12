916a1bcae01ceb5345593e1c20159e477cca9655
jtlap <jtlapreste@gmail.com>
2024-09-12 19:35:39 +0200

    pipo add

--- test/doc/core/pipo.cpp
+++ test/doc/core/pipo.cpp
@@ -6,23 +6,45 @@
 
 int main()
 {
-  auto sml = eve::smallestposval(eve::as(1.0f));
+  auto sml = eve::smallestposval(eve::as(1.0));
   {
-    eve::wide wf0{1.0f, 1.0f, 2.0f, 3.0f, -1.0f, -2.0f, -3.0f, -4.0f};
-    eve::wide wf1{sml, -sml, 1.0f, -1.0f, 2.0f, -2.0f, 3.0f, -3.0f};
+    eve::wide wf0{1.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
+    eve::wide wf1{sml, -sml, 1.0, -1.0, 2.0, -2.0, 3.0, -3.0};
 
-    std::cout << std::setprecision(15)<< "-> add          (wf0, wf1)       = " << eve::add               (wf0, wf1) << "\n";
-    std::cout << std::setprecision(15)<< "-> add[downward](wf0, wf1)       = " << eve::add[eve::downward](wf0, wf1) << "\n";
-    std::cout << std::setprecision(15)<< "-> add[upward](wf0, wf1)         = " << eve::add[eve::upward](wf0, wf1) << "\n";
-//    std::cout << std::setprecision(15)<< "-  diff                            = " << eve::add[eve::upward](wf0, wf1) - eve::add[eve::downward](wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "<- wf0                           = " <<wf0 << "\n";
+    std::cout << std::setprecision(20)<< "<- wf1                           = " <<wf1 << "\n";
+    std::cout << std::setprecision(20)<< "-> add          (wf0, wf1)       = " << eve::add               (wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "-> add[downward](wf0, wf1)       = " << eve::add[eve::downward](wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "-> add[upward](wf0, wf1)         = " << eve::add[eve::upward](wf0, wf1) << "\n";
+//    std::cout << std::setprecision(20)<< "-  diff                            = " << eve::add[eve::upward](wf0, wf1) - eve::add[eve::downward](wf0, wf1) << "\n";
   }
   {
-    static eve::wide wf0{1.0f, 1.0f, 2.0f, 3.0f, -1.0f, -2.0f, -3.0f, -4.0f};
-    static eve::wide wf1{eve::smallestposval(eve::as(1.0f)), -eve::smallestposval(eve::as(1.0f)), 1.0f, -1.0f, 2.0f, -2.0f, 3.0f, -3.0f};
+    static eve::wide wf0{1.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
+    static eve::wide wf1{eve::smallestposval(eve::as(1.0)), -eve::smallestposval(eve::as(1.0)), 1.0, -1.0, 2.0, -2.0, 3.0, -3.0};
 
-    std::cout << std::setprecision(15)<< "-> add          (wf0, wf1)       = " << eve::add               (wf0, wf1) << "\n";
-    std::cout << std::setprecision(15)<< "-> add[downward](wf0, wf1)       = " << eve::add[eve::downward](wf0, wf1) << "\n";
-    std::cout << std::setprecision(15)<< "-> add[upward](wf0, wf1)         = " << eve::add[eve::upward](wf0, wf1) << "\n";
-//    std::cout << std::setprecision(15)<< "-  diff                            = " << eve::add[eve::upward](wf0, wf1) - eve::add[eve::downward](wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "-> add          (wf0, wf1)       = " << eve::add               (wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "-> add[downward](wf0, wf1)       = " << eve::add[eve::downward](wf0, wf1) << "\n";
+    std::cout << std::setprecision(20)<< "-> add[upward](wf0, wf1)         = " << eve::add[eve::upward](wf0, wf1) << "\n";
+//    std::cout << std::setprecision(20)<< "-  diff                            = " << eve::add[eve::upward](wf0, wf1) - eve::add[eve::downward](wf0, wf1) << "\n";
+  }
+
+  {
+    static eve::wide wf0{1.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
+    static eve::wide wf1{eve::smallestposval(eve::as(1.0)), -eve::smallestposval(eve::as(1.0)), 1.0, -1.0, 2.0, -2.0, 3.0, -3.0};
+    auto [r, e] = eve::two_add(wf0, wf1);
+    std::cout << std::setprecision(20)<< "r = " << r << std::endl;
+    std::cout << std::setprecision(20)<< "e = " << e << std::endl;
+    r = eve::if_else(eve::is_gtz(e), eve::next(r), r);
+    std::cout << std::setprecision(20)<< "-  r = " << r << std::endl;
+  }
+
+  {
+    static eve::wide wf0{1.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
+    static eve::wide wf1{eve::smallestposval(eve::as(1.0)), -eve::smallestposval(eve::as(1.0)), 1.0, -1.0, 2.0, -2.0, 3.0, -3.0};
+    auto [r, e] = eve::two_add(wf0, wf1);
+    std::cout << std::setprecision(20)<< "r = " << r << std::endl;
+    std::cout << std::setprecision(20)<< "e = " << e << std::endl;
+    r = eve::if_else(eve::is_ltz(e), eve::prev(r), r);
+    std::cout << std::setprecision(20)<< "-  r = " << r << std::endl;
   }
 }
