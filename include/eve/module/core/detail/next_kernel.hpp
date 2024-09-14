d83209642932125cb4a38cdd8368ce6ea3435bdb
jtlap <jtlapreste@gmail.com>
2024-09-14 18:09:40 +0200

    addsub

--- include/eve/module/core/detail/next_kernel.hpp
+++ include/eve/module/core/detail/next_kernel.hpp
@@ -13,26 +13,26 @@
 #include <eve/module/core/constant/signmask.hpp>
 #include <eve/module/core/regular/bit_cast.hpp>
 #include <eve/module/core/regular/if_else.hpp>
-#include <eve/module/core/regular/is_gez.hpp>
+#include <eve/module/core/regular/is_ltz.hpp>
+#include <eve/module/core/regular/sub.hpp>
 
 namespace eve::detail
 {
-template<typename T>
-EVE_FORCEINLINE constexpr as_integer_t<T>
-bitinteger(T const& a) noexcept
-{
-  using r_t = as_integer_t<T>;
-  r_t a0    = bit_cast(a, as<r_t>());
-  return if_else(is_gez(a0), a0, signmask(eve::as<r_t>()) - a0);
-}
+  template<typename T>
+  EVE_FORCEINLINE constexpr as_integer_t<T>
+  bitinteger(T const& a) noexcept
+  {
+    using r_t = as_integer_t<T>;
+    r_t a0    = bit_cast(a, as<r_t>());
+    return sub[if_(is_ltz(a0)).else_(a0)](signmask(eve::as<r_t>()),a0);
+  }
 
-template<typename T>
-EVE_FORCEINLINE constexpr as_floating_point_t<T>
-bitfloating(T const& a) noexcept
-{
-  using r_t = as_floating_point_t<T>;
-  T s       = bit_cast(signmask(eve::as<r_t>()), as<T>());
-  return bit_cast(if_else(is_gez(a), a, s - a), as<r_t>());
-}
-
+  template<typename T>
+  EVE_FORCEINLINE constexpr as_floating_point_t<T>
+  bitfloating(T const& a) noexcept
+  {
+    using r_t = as_floating_point_t<T>;
+    T s       = bit_cast(signmask(eve::as<r_t>()), as<T>());
+    return bit_cast(sub[if_(is_ltz(a)).else_(a)](s,a), as<r_t>());
+  }
 }
--- include/eve/module/core/regular/impl/sub.hpp
+++ include/eve/module/core/regular/impl/sub.hpp
@@ -22,6 +22,7 @@
 #include <eve/module/core/regular/max.hpp>
 #include <eve/module/core/regular/min.hpp>
 #include <eve/module/core/regular/saturate.hpp>
+#include <eve/module/core/regular/add.hpp>
 
 namespace eve::detail
 {
