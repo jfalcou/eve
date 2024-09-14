52689645161df39fa8ccbf398bcdfb1046cc369d
jtlap <jtlapreste@gmail.com>
2024-09-14 14:02:08 +0200

    next_kernel

--- include/eve/module/core/detail/next_kernel.hpp
+++ include/eve/module/core/detail/next_kernel.hpp
@@ -14,29 +14,25 @@
 #include <eve/module/core/regular/bit_cast.hpp>
 #include <eve/module/core/regular/if_else.hpp>
 #include <eve/module/core/regular/is_gez.hpp>
+#include <eve/module/core/regular/sub.hpp>
 
 namespace eve::detail
 {
-template<typename T>
-EVE_FORCEINLINE constexpr as_integer_t<T>
-bitinteger(T const& a) noexcept
-{
-  using r_t = as_integer_t<T>;
-  r_t a0    = bit_cast(a, as<r_t>());
-  auto test = is_gez(a0);
-  r_t a0m   = if_else(test, zero, a0);
-  return if_else(test, a0, signmask(eve::as<r_t>()) - a0m);
-}
+  template<typename T>
+  EVE_FORCEINLINE constexpr as_integer_t<T>
+  bitinteger(T const& a) noexcept
+  {
+    using r_t = as_integer_t<T>;
+    r_t a0    = bit_cast(a, as<r_t>());
+    return sub[if_(is_ltz(a0)).else_(a0)](signmask(eve::as<r_t>()),a0);
+  }
 
-template<typename T>
-EVE_FORCEINLINE constexpr as_floating_point_t<T>
-bitfloating(T const& a) noexcept
-{
-  using r_t = as_floating_point_t<T>;
-  auto test = is_gez(a);
-  auto am   =  if_else(test, zero, a);
-  T s       = bit_cast(signmask(eve::as<r_t>()), as<T>());
-  return bit_cast(if_else(test, a, s - am), as<r_t>());
-}
-
+  template<typename T>
+  EVE_FORCEINLINE constexpr as_floating_point_t<T>
+  bitfloating(T const& a) noexcept
+  {
+    using r_t = as_floating_point_t<T>;
+    T s       = bit_cast(signmask(eve::as<r_t>()), as<T>());
+    return bit_cast(sub[if_(is_ltz(a)).else_(a)](s,a), as<r_t>());
+  }
 }
