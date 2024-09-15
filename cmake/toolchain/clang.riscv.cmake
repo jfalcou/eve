0966a4e07a0f7178a931cc5b8bc7c7ddb73c4315
Tetyushkin Ivan <ivan.tetyushkin@syntacore.com>
2024-09-15 15:07:37 +0300

    [riscv] Add tests unit.api.regular.wide.exe & unit.core.simd_cast.exe to CI

--- cmake/toolchain/clang.riscv.cmake
+++ cmake/toolchain/clang.riscv.cmake
@@ -9,5 +9,5 @@ set(CMAKE_SYSTEM_PROCESSOR  riscv64)
 set(CMAKE_C_COMPILER    /opt/llvm/bin/clang)
 set(CMAKE_CXX_COMPILER  /opt/llvm/bin/clang++)
 
-set(CMAKE_CXX_FLAGS "-march=rv64gcv --static --target=riscv64-unknown-linux-gnu ${EVE_OPTIONS}" )
+set(CMAKE_CXX_FLAGS "-march=rv64gcv --static -fbracket-depth=512 --target=riscv64-unknown-linux-gnu ${EVE_OPTIONS}" )
 set(CMAKE_CROSSCOMPILING_CMD ${PROJECT_SOURCE_DIR}/cmake/toolchain/run_rvv128.sh )
