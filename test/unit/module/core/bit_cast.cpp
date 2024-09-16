abebaa40f7da8430c2303dca96a74c7336455165
ita-sc <109672931+ita-sc@users.noreply.github.com>
2024-09-16 10:36:36 +0300

    Improve bit_cast tests for RISC-V support
    For RISC-V we can not always bit_cast between wides, even if `cardinal*sizeof(scalar_type)` is equal.
    So this patch adds a check that size of underlying types is the same. 
    
    Also this patch adds checks for result types for missed combinations.

--- test/unit/module/core/bit_cast.cpp
+++ test/unit/module/core/bit_cast.cpp
@@ -10,18 +10,39 @@
 //==================================================================================================
 // Types tests
 //==================================================================================================
+
+// We use sizeof(Wide) to get real occupied size, so if we divide it to the sizeof(Scalar),
+// we get the number of Scalar elements that we need to occupy the same size.
+template<eve::arithmetic_simd_value Wide, typename Scalar>
+using same_size_of =
+    eve::wide<Scalar, eve::fixed<std::max<size_t>(1, sizeof(Wide) / sizeof(Scalar))>>;
+
 TTS_CASE_TPL("Check return types of bit_cast", eve::test::simd::all_types)
 <typename T>(tts::type<T>)
 {
   using ut_t = eve::as_integer_t<T, unsigned>;
   using it_t = eve::as_integer_t<T, signed>;
-  using v_t  = eve::element_type_t<T>;
-  using u8_t = eve::as_wide_t<std::int8_t, eve::fixed<eve::cardinal_v<T> * sizeof(v_t)>>;
+  using u8_t  = same_size_of<T, std::int8_t>;
+  using u16_t = same_size_of<T, std::int16_t>;
+  using u32_t = same_size_of<T, std::int32_t>;
+  using u64_t = same_size_of<T, std::int64_t>;
+  using f32_t = same_size_of<T, float>;
+  using f64_t = same_size_of<T, double>;
   // regular
   TTS_EXPR_IS(eve::bit_cast(T(), eve::as<it_t>()), it_t);
   TTS_EXPR_IS(eve::bit_cast(T(), eve::as<ut_t>()), ut_t);
   TTS_EXPR_IS(eve::bit_cast(T(), eve::as<T>()), T);
   TTS_EXPR_IS(eve::bit_cast(T(), eve::as<u8_t>()), u8_t);
+  if constexpr( sizeof(T) == sizeof(u16_t) )
+    TTS_EXPR_IS(eve::bit_cast(T(), eve::as<u16_t>()), u16_t);
+  if constexpr( sizeof(T) == sizeof(u32_t) )
+    TTS_EXPR_IS(eve::bit_cast(T(), eve::as<u32_t>()), u32_t);
+  if constexpr( sizeof(T) == sizeof(f32_t) )
+    TTS_EXPR_IS(eve::bit_cast(T(), eve::as<f32_t>()), f32_t);
+  if constexpr( sizeof(T) == sizeof(u64_t) )
+    TTS_EXPR_IS(eve::bit_cast(T(), eve::as<u64_t>()), u64_t);
+  if constexpr( sizeof(T) == sizeof(f64_t) )
+    TTS_EXPR_IS(eve::bit_cast(T(), eve::as<f64_t>()), f64_t);
 };
 
 //==================================================================================================
