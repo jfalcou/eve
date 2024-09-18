8cbdfbeabd258ddcd8063e6525e08950f4321979
jtlap <jtlapreste@gmail.com>
2024-09-18 15:08:21 +0200

    sub

--- include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
@@ -51,9 +51,9 @@ namespace eve::detail
     requires sve_abi<abi_t<T, N>>
   {
     // We call the saturated sub if required or we just go to the common case of doing a+b
-   if constexpr(((O::contains(downward) || O::contains(upward)) && floating_value<T>))
+    if constexpr(((O::contains(downward) || O::contains(upward)) && floating_value<T>))
     {
-      return add.behavior(cpu_{}, opts, a+b);
+      return sub.behavior(cpu_{}, opts, a, b);
     }
     else if constexpr(O::contains(saturated2) && std::integral<T>) return svqsub(a, b);
     else                                                           return svsub_x(sve_true<T>(), a, b);
