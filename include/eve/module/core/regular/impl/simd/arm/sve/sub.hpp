4fe2c3f20ae10315ba9df8a0f003f636b5456173
jtlap <jtlapreste@gmail.com>
2024-09-19 09:37:54 +0200

    sub sve

--- include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
@@ -22,36 +22,32 @@ namespace eve::detail
     // ignore all just return alternative
     if constexpr( C::is_complete ) return alt;
 
+    //  if saturated on integer or oriented on floats, we don't have masked op so we delegate
     if constexpr(((O::contains(downward) || O::contains(upward)) && floating_value<T>) ||
                  (O::contains(saturated2) && std::integral<T>))
     {
-      return add.behavior(cpu_{}, opts, a, b);
+      return sub.behavior(cpu_{}, opts, a, b);
     }
+    //  If not, we can mask if there is no alterative value
+    else  if  constexpr( !C::has_alternative )
+    {
+      auto m = expand_mask(mask, as{a});
+      return svsub_m(m, a, b);
+    }
+    // If not, we delegate to the automasking
     else
     {
-      //  if saturated on integer, we don't have masked op so we delegate
-      if        constexpr(O::contains(saturated) && std::integral<T>) return sub.behavior(cpu_{}, opts, a, b);
-
-      //  If not, we can mask if there is no alterative value
-      else  if  constexpr( !C::has_alternative )
-      {
-        auto m = expand_mask(mask, as{a});
-        return svsub_m(m, a, b);
-      }
-      // If not, we delegate to the automasking
-      else
-      {
-        return sub.behavior(cpu_{}, opts, a, b);
-      }
+      return sub.behavior(cpu_{}, opts, a, b);
     }
   }
+
 
   template<callable_options O, arithmetic_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N> sub_(EVE_REQUIRES(sve_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
     requires sve_abi<abi_t<T, N>>
   {
-    // We call the saturated sub if required or we just go to the common case of doing a+b
-    if constexpr(((O::contains(downward) || O::contains(upward)) && floating_value<T>))
+    // We call the saturated sub if required or we just go to the common case of doing a-b
+    if constexpr((O::contains(downward) || O::contains(upward)) && floating_value<T>)
     {
       return sub.behavior(cpu_{}, opts, a, b);
     }
