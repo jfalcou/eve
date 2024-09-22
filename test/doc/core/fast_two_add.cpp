914ffa2557b1c40306ae3e712207a9d090cb654c
jtlap <jtlapreste@gmail.com>
2024-09-22 11:55:43 +0200

    fast_two_add test

--- test/doc/core/fast_two_add.cpp
+++ test/doc/core/fast_two_add.cpp
@@ -5,7 +5,7 @@
 int main()
 {
   eve::wide wf0 = {30.0, 2.5, -32.7, 1.0};
-  eve::wide wf1 = {4.2, 1.5, -100.834, eve::eps(eve::as<double>())/2};
+  eve::wide wf1 = {4.2, 1.5, -10.834, eve::eps(eve::as<double>())/2};
 
   std::cout << "<- wf0               = " << wf0 << "\n";
   std::cout << "<- wf1               = " << wf1 << "\n";
