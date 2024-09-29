73d89da3856dfdb9406f6bcb6a6d45fb0ac3e952
jtlap <jtlapreste@gmail.com>
2024-09-29 19:10:46 +0200

    fnma neon

--- include/eve/module/core/regular/impl/simd/arm/neon/fnma.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/fnma.hpp
@@ -24,7 +24,9 @@ EVE_FORCEINLINE wide<T, N>
 {
   constexpr auto cat = categorize<wide<T, N>>();
 
-  if constexpr( cat == category::float32x2 ) return vfms_f32(v2, v1, v0);
+  if constexpr(O::contains(lower) || O::contains(upper))
+    return fnma.behavior(cpu_{}, o, v0, v1, v2);
+  else  if constexpr( cat == category::float32x2 ) return vfms_f32(v2, v1, v0);
   else if constexpr( cat == category::float32x4 ) return vfmsq_f32(v2, v1, v0);
   else if constexpr( current_api >= asimd )
   {
