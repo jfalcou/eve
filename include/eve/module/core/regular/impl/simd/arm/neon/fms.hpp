93a7dae4b453abf26b9f2bfb8c6653e497109af5
jtlap <jtlapreste@gmail.com>
2024-09-30 11:25:47 +0200

    fms neon typo

--- include/eve/module/core/regular/impl/simd/arm/neon/fms.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/fms.hpp
@@ -15,7 +15,7 @@ namespace eve::detail
 {
   template<typename T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N>
-  fms_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> const& a, wide<T, N> const& b, wide<T, N> const& c) noexcept
+  fms_(EVE_REQUIRES(neon128_), O const& o, wide<T, N> const& a, wide<T, N> const& b, wide<T, N> const& c) noexcept
   requires arm_abi<abi_t<T, N>>
   {
     return fma[o](a, b, -c);
