03da7b7a6614ad293466f18c67e451cc783e39b7
jtlap <jtlapreste@gmail.com>
2024-10-02 15:29:30 +0200

    fnms x86

--- include/eve/module/core/regular/impl/simd/x86/fnms.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fnms.hpp
@@ -127,7 +127,7 @@ namespace eve::detail
         }
         else                                                 return fnms.behavior(cpu_{}, opts, a, b, c);
       }
-      if ((O::contains(lower) || O::contains(upper))&& floating_value<T>) return if_else(mask, eve::fnms[opts.drop(condition_key)](v, w, x), v);
+      if ((O::contains(lower) || O::contains(upper))&& floating_value<T>) return if_else(mask, eve::fnms[opts.drop(condition_key)](a, b, c), a);
       else if constexpr( cx == category::float32x16 ) return _mm512_mask_fnmsub_ps(a, m, b, c);
       else if constexpr( cx == category::float64x8  ) return _mm512_mask_fnmsub_pd(a, m, b, c);
       else if constexpr( cx == category::float32x8  ) return _mm256_mask_fnmsub_ps(a, m, b, c);
