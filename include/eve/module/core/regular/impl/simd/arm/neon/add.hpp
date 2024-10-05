fc448e31469cfebde38fcc6438264bd05885b845
jtlap <jtlapreste@gmail.com>
2024-10-05 11:16:14 +0200

    add neon

--- include/eve/module/core/regular/impl/simd/arm/neon/add.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/add.hpp
@@ -15,20 +15,20 @@
 namespace eve::detail
 {
 
- template<callable_options O, typename T, typename N>
-  EVE_FORCEINLINE  wide<up_t<T>, N> add_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> v, wide<T, N> w) noexcept
-  requires (arm_abi<abi_t<T, N>> && O::contains(widen))
-  {
-    constexpr auto c = categorize<wide<T, N>>();
+//  template<callable_options O, typename T, typename N>
+//   EVE_FORCEINLINE  wide<up_t<T>, N> add_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> v, wide<T, N> w) noexcept
+//   requires (arm_abi<abi_t<T, N>> && O::contains(widen))
+//   {
+//     constexpr auto c = categorize<wide<T, N>>();
 
-    if      constexpr( c == category::int32x2    ) return vaddl_s32 (v, w);
-    else if constexpr( c == category::uint32x2   ) return vaddl_u32 (v, w);
-    else if constexpr( c == category::int16x4    ) return vaddl_s16 (v, w);
-    else if constexpr( c == category::uint16x4   ) return vaddl_u16 (v, w);
-    else if constexpr( c == category::int8x8     ) return vaddl_s8  (v, w);
-    else if constexpr( c == category::uint8x8    ) return vaddl_u8  (v, w);
-    else return add.behavior(cpu_{}, opts, v, w);
-  }
+//     if      constexpr( c == category::int32x2    ) return vaddl_s32 (v, w);
+//     else if constexpr( c == category::uint32x2   ) return vaddl_u32 (v, w);
+//     else if constexpr( c == category::int16x4    ) return vaddl_s16 (v, w);
+//     else if constexpr( c == category::uint16x4   ) return vaddl_u16 (v, w);
+//     else if constexpr( c == category::int8x8     ) return vaddl_s8  (v, w);
+//     else if constexpr( c == category::uint8x8    ) return vaddl_u8  (v, w);
+//     else return add.behavior(cpu_{}, opts, v, w);
+//   }
 
   template<callable_options O, arithmetic_scalar_value T, typename N>
   EVE_FORCEINLINE auto add_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> v, wide<T, N> w) noexcept
@@ -36,7 +36,7 @@ namespace eve::detail
   {
     constexpr auto c = categorize<wide<T, N>>();
 
-    if constexpr(O::contains(narrow))
+    if constexpr(O::contains_any(narrow, widen))
     {
       return add.behavior(cpu_{}, opts, v, w);
     }
