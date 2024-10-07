ea180c98b9377f25191c7651877634b330fd577a
jfalcou <jfalcou@users.noreply.github.com>
2024-10-07 15:20:52 +0000

    deploy: 2cb4fa4960abd72e692fecc85c6c8a6a6ede47af

--- search/all_7.js
+++ search/all_7.js
@@ -5,9 +5,10 @@ var searchData=
   ['handling_20multiple_20architecture_20targets_2',['Handling Multiple Architecture Targets',['../multiarch.html',1,'']]],
   ['has_5fequal_5fin_3',['has_equal_in',['../group__core__simd_ga64cba22849ce7362968aa0c2661a5ab0.html#ga64cba22849ce7362968aa0c2661a5ab0',1,'eve']]],
   ['has_5fzeros_4',['has_zeros',['../structeve_1_1pattern__t.html#adaf1e7da40b7b86a9e00d120044039df',1,'eve::pattern_t']]],
-  ['hermite_5',['hermite',['../group__polynomial_ga7bb3ce7bbd71384fadab1bcdd1905a25.html#ga7bb3ce7bbd71384fadab1bcdd1905a25',1,'eve']]],
-  ['hi_6',['hi',['../group__core__bitops_ga46f5764b631e4afe92f87ff3ea1e8524.html#ga46f5764b631e4afe92f87ff3ea1e8524',1,'eve']]],
-  ['horner_7',['horner',['../group__math_ga90f1fb37df377d7fd9f95282ec3c70cb.html#ga90f1fb37df377d7fd9f95282ec3c70cb',1,'eve']]],
-  ['hyperbolic_8',['Hyperbolic',['../group__math__hyper.html',1,'']]],
-  ['hypot_9',['hypot',['../group__math__exp_ga1f3f9f62c04251ff83111a058c2e64b9.html#ga1f3f9f62c04251ff83111a058c2e64b9',1,'eve']]]
+  ['heaviside_5',['heaviside',['../group__core__arithmetic_gabba0e6a809a48695c1111a40ac5d8f40.html#gabba0e6a809a48695c1111a40ac5d8f40',1,'eve']]],
+  ['hermite_6',['hermite',['../group__polynomial_ga7bb3ce7bbd71384fadab1bcdd1905a25.html#ga7bb3ce7bbd71384fadab1bcdd1905a25',1,'eve']]],
+  ['hi_7',['hi',['../group__core__bitops_ga46f5764b631e4afe92f87ff3ea1e8524.html#ga46f5764b631e4afe92f87ff3ea1e8524',1,'eve']]],
+  ['horner_8',['horner',['../group__math_ga90f1fb37df377d7fd9f95282ec3c70cb.html#ga90f1fb37df377d7fd9f95282ec3c70cb',1,'eve']]],
+  ['hyperbolic_9',['Hyperbolic',['../group__math__hyper.html',1,'']]],
+  ['hypot_10',['hypot',['../group__math__exp_ga1f3f9f62c04251ff83111a058c2e64b9.html#ga1f3f9f62c04251ff83111a058c2e64b9',1,'eve']]]
 ];
--- search/variables_7.js
+++ search/variables_7.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['half_0',['half',['../group__core__constants_ga8d9cee3337e0a95b596cef102e04e1a5.html#ga8d9cee3337e0a95b596cef102e04e1a5',1,'eve']]],
   ['has_5fequal_5fin_1',['has_equal_in',['../group__core__simd_ga64cba22849ce7362968aa0c2661a5ab0.html#ga64cba22849ce7362968aa0c2661a5ab0',1,'eve']]],
-  ['hermite_2',['hermite',['../group__polynomial_ga7bb3ce7bbd71384fadab1bcdd1905a25.html#ga7bb3ce7bbd71384fadab1bcdd1905a25',1,'eve']]],
-  ['hi_3',['hi',['../group__core__bitops_ga46f5764b631e4afe92f87ff3ea1e8524.html#ga46f5764b631e4afe92f87ff3ea1e8524',1,'eve']]],
-  ['horner_4',['horner',['../group__math_ga90f1fb37df377d7fd9f95282ec3c70cb.html#ga90f1fb37df377d7fd9f95282ec3c70cb',1,'eve']]],
-  ['hypot_5',['hypot',['../group__math__exp_ga1f3f9f62c04251ff83111a058c2e64b9.html#ga1f3f9f62c04251ff83111a058c2e64b9',1,'eve']]]
+  ['heaviside_2',['heaviside',['../group__core__arithmetic_gabba0e6a809a48695c1111a40ac5d8f40.html#gabba0e6a809a48695c1111a40ac5d8f40',1,'eve']]],
+  ['hermite_3',['hermite',['../group__polynomial_ga7bb3ce7bbd71384fadab1bcdd1905a25.html#ga7bb3ce7bbd71384fadab1bcdd1905a25',1,'eve']]],
+  ['hi_4',['hi',['../group__core__bitops_ga46f5764b631e4afe92f87ff3ea1e8524.html#ga46f5764b631e4afe92f87ff3ea1e8524',1,'eve']]],
+  ['horner_5',['horner',['../group__math_ga90f1fb37df377d7fd9f95282ec3c70cb.html#ga90f1fb37df377d7fd9f95282ec3c70cb',1,'eve']]],
+  ['hypot_6',['hypot',['../group__math__exp_ga1f3f9f62c04251ff83111a058c2e64b9.html#ga1f3f9f62c04251ff83111a058c2e64b9',1,'eve']]]
 ];
