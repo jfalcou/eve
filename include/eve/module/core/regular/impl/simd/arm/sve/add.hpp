c072010595931f557e1d7d87b8fdd72a2eba57e4
jtlap <jtlapreste@gmail.com>
2024-10-08 21:59:53 +0200

    sve add

--- include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
@@ -24,7 +24,7 @@ namespace eve::detail
   template<callable_options O, arithmetic_scalar_value T, typename N, conditional_expr C>
   EVE_FORCEINLINE auto add_(EVE_REQUIRES(sve_), C const& mask, O const& opts,
                             wide<T, N> v, wide<T, N> w) noexcept
-  requires (sve_abi<abi_t<T, N> && O:contains(widen))
+  requires (sve_abi<abi_t<T, N>> && O::contains(widen))
   {
     return add.behavior(cpu_{}, opts, v, w);
   }
