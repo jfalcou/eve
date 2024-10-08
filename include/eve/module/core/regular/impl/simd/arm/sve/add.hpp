64cef64769761a2024302496f891d4142a83f5c2
jtlap <jtlapreste@gmail.com>
2024-10-08 22:24:13 +0200

    sve add

--- include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
@@ -32,7 +32,7 @@ namespace eve::detail
   template<callable_options O, arithmetic_scalar_value T, typename N, conditional_expr C>
   EVE_FORCEINLINE wide<T, N> add_(EVE_REQUIRES(sve_), C const& mask, O const& opts,
                                   wide<T, N> v, wide<T, N> w) noexcept
-  requires (sve_abi<abi_t<T, N>> && !O:contains(widen))
+  requires (sve_abi<abi_t<T, N>> && !O::contains(widen))
   {
     auto const alt = alternative(mask, v, as(v));
 
