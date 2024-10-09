ae151d6c00720a8201379c8b0ed88fc5445bc664
jtlap <jtlapreste@gmail.com>
2024-10-09 17:36:15 +0200

    neon sub

--- include/eve/module/core/regular/impl/simd/arm/neon/sub.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/sub.hpp
@@ -40,7 +40,7 @@ namespace eve::detail
 
   template<callable_options O, arithmetic_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N> sub_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
-  requires (arm_abi<abi_t<T, N>> && !O::contains(widen))
+  requires (arm_abi<abi_t<T, N>>> && !O::contains(widen))
   {
     if constexpr(O::contains_any(lower, upper))
     {
