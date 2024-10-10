0fa69cede271baf92cb26c2df13c6e1bb565ad1f
SadiinsoSnowfall <sadiinso@gmail.com>
2024-10-10 10:39:57 +0200

    stash

--- include/eve/module/core/regular/sub.hpp
+++ include/eve/module/core/regular/sub.hpp
@@ -33,73 +33,72 @@ namespace eve
     EVE_CALLABLE_OBJECT(sub_t, sub_);
   };
 
-//================================================================================================
-//! @addtogroup core_arithmetic
-//! @{
-//!   @var sub
-//!   @brief `tuple_callable` computing the difference of its first argument with the sum of the others.
-//!
-//!   @groupheader{Header file}
-//!
-//!   @code
-//!   #include <eve/module/core.hpp>
-//!   @endcode
-//!
-//!   @groupheader{Callable Signatures}
-//!
-//!   @code
-//!   namespace eve
-//!   {
-//!      // Regular overloads
-//!      constexpr auto sub(value auto x, value auto ... xs)                          noexcept; // 1
-//!      constexpr auto sub(kumi::non_empty_product_type auto const& tup)             noexcept; // 2
-//!
-//!      // Lanes masking
-//!      constexpr auto sub[conditional_expr auto c](/*any of the above overloads*/)  noexcept; // 3
-//!      constexpr auto sub[logical_value auto m](/*any of the above overloads*/)     noexcept; // 3
-//!
-//!      // Semantic options
-//!      constexpr auto sub[saturated](/*any of the above overloads*/)                noexcept; // 4
-//!      constexpr auto sub[lower](/*any of the above overloads*/)                    noexcept; // 5
-//!      constexpr auto sub[upper](/*any of the above overloads*/)                    noexcept; // 6
-//!   }
-//!   @endcode
-//!
-//!   **Parameters**
-//!
-//!     * `... xs` :  [real](@ref eve::value) arguments.
-//!     * `tup`: [non empty tuple](@ref kumi::non_empty_product_type) of arguments.
-//!     * `c`: [Conditional expression](@ref conditional_expr) masking the operation.
-//!     * `m`: [Logical value](@ref logical) masking the operation.
-//!
-//!    **Return value**
-//!
-//!    The value of the difference of its first argument with the sum of the others
-//!    1. Take care that for floating entries, the addition is not perfectly associative due to rounding errors.
-//!       This call performs additions in reverse incoming order.
-//!    2. equivalent to the call on the elements of the tuple.
-//!    3. [The operation is performed conditionnaly](@ref conditional)
-//!    4. The call `sub[saturated](...)` computes a saturated version of `add`.
-//!       Take care that for signed integral entries this kind of a is highly order dependant.
-//!       This call perform saturated additions in reverse incoming order.
-//!        We do not advise to use it for more than 2 parameters.
-//!    5. The substraction is computed in a 'round toward \f$-\infty\f$ mode. The result is guaranted
-//!       to be less or equal to the exact one (except for Nans).
-//!    6. The substraction is computed in a 'round toward \f$\infty\f$ mode. The result is guaranted
-//!       to be greater or equal to the exact one (except for Nans).
-//!
-//!   @note
-//!      Although the infix notation with `-` is supported for two parameters, the `-` operator on
-//!      standard scalar types is the original one and so can lead to automatic promotion.
-//!
-//!  @groupheader{Example}
-//!  @godbolt{doc/core/sub.cpp}
-//================================================================================================
+  //================================================================================================
+  //! @addtogroup core_arithmetic
+  //! @{
+  //!   @var sub
+  //!   @brief `tuple_callable` computing the difference of its first argument with the sum of the others.
+  //!
+  //!   @groupheader{Header file}
+  //!
+  //!   @code
+  //!   #include <eve/module/core.hpp>
+  //!   @endcode
+  //!
+  //!   @groupheader{Callable Signatures}
+  //!
+  //!   @code
+  //!   namespace eve
+  //!   {
+  //!      // Regular overloads
+  //!      constexpr auto sub(value auto x, value auto ... xs)                          noexcept; // 1
+  //!      constexpr auto sub(kumi::non_empty_product_type auto const& tup)             noexcept; // 2
+  //!
+  //!      // Lanes masking
+  //!      constexpr auto sub[conditional_expr auto c](/*any of the above overloads*/)  noexcept; // 3
+  //!      constexpr auto sub[logical_value auto m](/*any of the above overloads*/)     noexcept; // 3
+  //!
+  //!      // Semantic options
+  //!      constexpr auto sub[saturated](/*any of the above overloads*/)                noexcept; // 4
+  //!      constexpr auto sub[lower](/*any of the above overloads*/)                    noexcept; // 5
+  //!      constexpr auto sub[upper](/*any of the above overloads*/)                    noexcept; // 6
+  //!   }
+  //!   @endcode
+  //!
+  //!   **Parameters**
+  //!
+  //!     * `... xs` :  [real](@ref eve::value) arguments.
+  //!     * `tup`: [non empty tuple](@ref kumi::non_empty_product_type) of arguments.
+  //!     * `c`: [Conditional expression](@ref conditional_expr) masking the operation.
+  //!     * `m`: [Logical value](@ref logical) masking the operation.
+  //!
+  //!    **Return value**
+  //!
+  //!    The value of the difference of its first argument with the sum of the others
+  //!    1. Take care that for floating entries, the addition is not perfectly associative due to rounding errors.
+  //!       This call performs additions in reverse incoming order.
+  //!    2. equivalent to the call on the elements of the tuple.
+  //!    3. [The operation is performed conditionnaly](@ref conditional)
+  //!    4. The call `sub[saturated](...)` computes a saturated version of `add`.
+  //!       Take care that for signed integral entries this kind of a is highly order dependant.
+  //!       This call perform saturated additions in reverse incoming order.
+  //!        We do not advise to use it for more than 2 parameters.
+  //!    5. The substraction is computed in a 'round toward \f$-\infty\f$ mode. The result is guaranted
+  //!       to be less or equal to the exact one (except for Nans).
+  //!    6. The substraction is computed in a 'round toward \f$\infty\f$ mode. The result is guaranted
+  //!       to be greater or equal to the exact one (except for Nans).
+  //!
+  //!   @note
+  //!      Although the infix notation with `-` is supported for two parameters, the `-` operator on
+  //!      standard scalar types is the original one and so can lead to automatic promotion.
+  //!
+  //!  @groupheader{Example}
+  //!  @godbolt{doc/core/sub.cpp}
+  //================================================================================================
   inline constexpr auto sub = functor<sub_t>;
-//================================================================================================
-//! @}
-//================================================================================================
-
+  //================================================================================================
+  //! @}
+  //================================================================================================
 }
 
 #include <eve/module/core/regular/impl/sub.hpp>
