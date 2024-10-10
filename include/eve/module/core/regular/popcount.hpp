f2bc7111f38c5bed314a2247b0d550f2a7f9db68
jtlap <jtlapreste@gmail.com>
2024-10-10 19:27:10 +0200

    neon popcount not included

--- include/eve/module/core/regular/popcount.hpp
+++ include/eve/module/core/regular/popcount.hpp
@@ -7,31 +7,17 @@
 #pragma once
 
 #include <eve/arch.hpp>
-#include <eve/traits/overload.hpp>
-#include <eve/module/core/decorator/core.hpp>
-#include <eve/detail/assert_utils.hpp>
+#include <eve/detail/overload.hpp>
 
 namespace eve
 {
-  template<typename Options>
-  struct popcount_t : elementwise_callable<popcount_t, Options>
-  {
-    template<unsigned_value T>
-    EVE_FORCEINLINE constexpr T operator()(T t) const noexcept
-    {
-      return EVE_DISPATCH_CALL(t);
-    }
-
-    EVE_CALLABLE_OBJECT(popcount_t, popcount_);
-  };
-
 //================================================================================================
 //! @addtogroup core_bitops
 //! @{
 //!   @var popcount
-//!   @brief `elementwise_callable` object computing elementwise the number of bits set in the parameter.
+//!   @brief Computes elementwise the number of bits set in the parameter.
 //!
-//!   @groupheader{Header file}
+//!   **Defined in Header**
 //!
 //!   @code
 //!   #include <eve/module/core.hpp>
@@ -42,26 +28,26 @@ namespace eve
 //!   @code
 //!   namespace eve
 //!   {
-//!      constexpr auto popcount(unsigned_value auto x) noexcept;
+//!      template< eve::unsigned_value T >
+//!      T popcount(T x) noexcept;
 //!   }
 //!   @endcode
 //!
 //!   **Parameters**
 //!
-//!     * `x`: [unsigned argument](@ref eve::unsigned_value).
+//!     * `x` :  [unsigned argument](@ref eve::unsigned_value).
 //!
 //!    **Return value**
 //!
-//!    The value of  number of bits set in the value `x` is returned.
+//!    The value of  number of bits set in the value `x`
+//!    is returned.
 //!
 //!  @groupheader{Example}
+//!
 //!  @godbolt{doc/core/popcount.cpp}
-//================================================================================================
-  inline constexpr auto popcount = functor<popcount_t>;
-//================================================================================================
 //! @}
 //================================================================================================
-
+EVE_MAKE_CALLABLE(popcount_, popcount);
 }
 
 #include <eve/module/core/regular/impl/popcount.hpp>
@@ -72,4 +58,8 @@ namespace eve
 
 #if defined(EVE_INCLUDE_ARM_SVE_HEADER)
 #  include <eve/module/core/regular/impl/simd/arm/sve/popcount.hpp>
+#endif
+
+#if defined(EVE_INCLUDE_ARM_HEADER)
+#  include <eve/module/core/regular/impl/simd/arm/neon/popcount.hpp>
 #endif
