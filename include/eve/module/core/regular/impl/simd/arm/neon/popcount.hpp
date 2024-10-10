82c4915f80a6738bcd1c0d893a37fb57e10198d1
jtlap <jtlapreste@gmail.com>
2024-10-10 15:32:12 +0200

    pocount neon

--- include/eve/module/core/regular/impl/simd/arm/neon/popcount.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/popcount.hpp
@@ -21,21 +21,18 @@ namespace eve::detail
   {
     constexpr auto cat = categorize<wide<T, N>>();
 
+    auto split_it = [](auto v){
+      auto z =  bit_cast(v, as<wide<std::uint8, fixed < fixed < N>::value*2>>>>());
+      return  popcount(z);
+    };
 
-    else if constexpr( cat == category::uint8x8         ) return vcnt_u8(v);
-    else if constexpr( cat == category::uint8x16        ) return vcntq_u8(v);
-    else if constexpr( cat == category::uint16x4        )
-    {
-      auto z =  bit_cast(v, as<wide<std::uint8, fixed < fixed < N>::value*2>>>>());
-      auto r = popcount(z);
-      return vpaddl_u8(r);
-    }
-    else if constexpr( cat == category::uint16x8       )
-    {
-      auto z =  bit_cast(v, as<wide<std::uint8, fixed < fixed < N>::value*2>>>>());
-      auto r = popcount(z);
-      return vpaddlq_u8(r);
-    }
+    if      constexpr( cat == category::uint8x8 ) return vcnt_u8(v);
+    else if constexpr( cat == category::uint8x16) return vcntq_u8(v);
+    else if constexpr( cat == category::uint16x4) return vpaddl_u8(split_it(r));
+    else if constexpr( cat == category::uint16x8) return vpaddlq_u8(split_it(r));
+    else if constexpr( cat == category::uint32x2) return vpaddl_u16(split_it(r));
+    else if constexpr( cat == category::uint32x4) return vpaddlq_u16(split_it(r));
+    else if constexpr( cat == category::uint64x2) return vpaddl_u32(split_it(r));
     else return popcount.behavior(popcount, o, v);
 
   }
