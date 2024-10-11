8cad321aea6b91ff44e0bd547812cb3b99ca040b
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2024-10-11 18:13:02 +0200

    Better UB handling in convert & document bit_cast codegen fix

--- include/eve/detail/function/simd/common/bit_cast.hpp
+++ include/eve/detail/function/simd/common/bit_cast.hpp
@@ -15,6 +15,7 @@ namespace eve::detail
   template<callable_options O, typename T, typename Target>
   EVE_FORCEINLINE constexpr auto bit_cast_(EVE_REQUIRES(cpu_), O const&, T const &a, as<Target> const &) noexcept
   {
+    // Fixes bad codegen on some compilers
     if constexpr(std::is_same_v<T, Target>) return a;
     else                                    return std::bit_cast<Target>(a);
   }
