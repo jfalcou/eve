87c0072c72003eb8374baca6cd7d4ea7fb2fca18
jtlap <jtlapreste@gmail.com>
2024-10-12 18:11:58 +0200

    bit_reverse

--- include/eve/module/core/regular/impl/simd/arm/neon/bit_reverse.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/bit_reverse.hpp
@@ -17,11 +17,15 @@ namespace eve::detail
   template<unsigned_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE wide<T, N>
   bit_reverse_(EVE_REQUIRES(neon128_), O const& opts, wide<T, N> const& v)
-    noexcept requires (arm_abi<abi_t<T, N>> &&  current_api >= asimd)
+    noexcept requires (arm_abi<abi_t<T, N>>)
   {
-    constexpr auto cat = categorize<wide<T, N>>();
-    if      constexpr( cat == category::uint8x16    ) return vrbitq_u8(v);
-    else if constexpr( cat == category::uint8x8     ) return vrbit_u8(v);
+    if constexpr( current_api >= asimd)
+    {
+      constexpr auto cat = categorize<wide<T, N>>();
+      if      constexpr( cat == category::uint8x16    ) return vrbitq_u8(v);
+      else if constexpr( cat == category::uint8x8     ) return vrbit_u8(v);
+      else return bit_reverse.behavior(cpu_{}, opts, v);
+    }
     else return bit_reverse.behavior(cpu_{}, opts, v);
   }
 }
