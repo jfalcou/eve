ca2c400cfc1991216a7363997887fa447bc4ba56
jtlap <jtlapreste@gmail.com>
2024-10-18 19:40:53 +0200

    sve

--- include/eve/module/core/regular/impl/simd/arm/sve/compare_absolute.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/compare_absolute.hpp
@@ -20,12 +20,13 @@
 
 namespace eve::detail
 {
+
   template<callable_options O, floating_scalar_value T, typename N,  typename F>
   EVE_FORCEINLINE logical<wide<T, N>> compare_absolute_(EVE_REQUIRES(sve_), O const& opts,
                                                         wide<T, N> v, wide<T, N> w,  F f) noexcept
   requires (arm_abi<abi_t<T, N>>)
   {
-    return compare_absolute[true_(as(v)](v, w, f);
+    return compare_absolute[true_(as(v))](v, w, f);
   }
 
   template<callable_options O, floating_scalar_value T, typename N, conditional_expr C,  typename F>
