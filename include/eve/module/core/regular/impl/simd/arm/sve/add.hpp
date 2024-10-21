43dc7c8a207b4e429558d0eb046afdc303844992
jtlap <jtlapreste@gmail.com>
2024-10-21 11:13:37 +0200

    sve case

--- include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/add.hpp
@@ -14,7 +14,7 @@
 namespace eve::detail
 {
   template<callable_options O, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE upgrade_t<wide<T, N>> add_(EVE_REQUIRES(neon128_), O const& opts,
+  EVE_FORCEINLINE auto add_(EVE_REQUIRES(sve_), O const& opts,
                                               wide<T, N> v, wide<T, N> w) noexcept
   requires (sve_abi<abi_t<T, N>> && O::contains(widen))
   {
