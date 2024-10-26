b1b5d6fd7262bd95eece54f473b1e22c07c90fe2
jtlap <jtlapreste@gmail.com>
2024-10-26 11:52:24 +0200

    sve

--- include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/sub.hpp
@@ -12,61 +12,58 @@
 
 namespace eve::detail
 {
-  template<callable_options O, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE auto sub_(EVE_REQUIRES(sve_), O const& opts,
-                            wide<T, N> v, wide<T, N> w) noexcept
-  requires (sve_abi<abi_t<T, N>> && O::contains(widen))
-  {
-    return sub.behavior(cpu_{}, opts, v, w);
-  }
-
   template<callable_options O, arithmetic_scalar_value T, typename N, conditional_expr C>
-  EVE_FORCEINLINE auto sub_(EVE_REQUIRES(sve_), C const& mask, O const& opts,
-                            wide<T, N> v, wide<T, N> w) noexcept
-  requires (sve_abi<abi_t<T, N>> && O::contains(widen))
-  {
-    return sub.behavior(cpu_{}, opts, v, w);
-  }
-
-  template<callable_options O, arithmetic_scalar_value T, typename N, conditional_expr C>
-  EVE_FORCEINLINE wide<T, N> sub_(EVE_REQUIRES(sve_), C const& mask, O const& opts, wide<T, N> a, wide<T, N> b) noexcept
+  EVE_FORCEINLINE auto sub_(EVE_REQUIRES(sve_), C const& mask, O const& opts, wide<T, N> a, wide<T, N> b) noexcept
     requires sve_abi<abi_t<T, N>>
   {
-    auto const alt = alternative(mask, a, as(a));
-
-    // ignore all just return alternative
-    if constexpr( C::is_complete ) return alt;
-
-    //  if saturated on integer or oriented on floats, we don't have masked op so we delegate
-    if constexpr(((O::contains(lower) || O::contains(upper)) && floating_value<T>) ||
-                 (O::contains(saturated) && std::integral<T>))
+    if constexpr(O::contains(widen))
     {
-      return sub.behavior(cpu_{}, opts, a, b);
+      return sub.behavior(cpu_{}, opts, v, w);
     }
-    //  If not, we can mask if there is no alterative value
-    else  if  constexpr( !C::has_alternative )
-    {
-      auto m = expand_mask(mask, as{a});
-      return svsub_m(m, a, b);
-    }
-    // If not, we delegate to the automasking
     else
     {
-      return sub.behavior(cpu_{}, opts, a, b);
+      auto const alt = alternative(mask, a, as(a));
+
+      // ignore all just return alternative
+      if constexpr( C::is_complete ) return alt;
+
+      //  if saturated on integer or oriented on floats, we don't have masked op so we delegate
+      if constexpr(((O::contains(lower) || O::contains(upper)) && floating_value<T>) ||
+                   (O::contains(saturated) && std::integral<T>))
+      {
+        return sub.behavior(cpu_{}, opts, a, b);
+      }
+      //  If not, we can mask if there is no alterative value
+      else  if  constexpr( !C::has_alternative )
+      {
+        auto m = expand_mask(mask, as{a});
+        return wide<T, N>(svsub_m(m, a, b));
+      }
+      // If not, we delegate to the automasking
+      else
+      {
+        return sub.behavior(cpu_{}, opts, a, b);
+      }
     }
   }
 
-
   template<callable_options O, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N> sub_(EVE_REQUIRES(sve_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
+  EVE_FORCEINLINE auto sub_(EVE_REQUIRES(sve_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
     requires sve_abi<abi_t<T, N>>
   {
-    // We call the saturated sub if required or we just go to the common case of doing a-b
-    if constexpr((O::contains(lower) || O::contains(upper)) && floating_value<T>)
+    if constexpr(O::contains(widen))
     {
-      return sub.behavior(cpu_{}, opts, a, b);
+      return sub.behavior(cpu_{}, opts, v, w);
     }
-    else if constexpr(O::contains(saturated) && std::integral<T>) return svqsub(a, b);
-    else                                                           return svsub_x(sve_true<T>(), a, b);
+    else
+    {
+      // We call the saturated sub if required or we just go to the common case of doing a-b
+      if constexpr((O::contains(lower) || O::contains(upper)) && floating_value<T>)
+      {
+        return sub.behavior(cpu_{}, opts, a, b);
+      }
+      else if constexpr(O::contains(saturated) && std::integral<T>) return wide<T, N>(svqsub(a, b));
+      else                                                          return wide<T, N>(svsub_x(sve_true<T>(), a, b));
+    }
   }
 }
