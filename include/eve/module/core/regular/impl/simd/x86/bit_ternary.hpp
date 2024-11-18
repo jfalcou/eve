a3527a2de201038147ad1c059b958e359327ca97
SadiinsoSnowfall <sadiinso@gmail.com>
2024-11-18 14:04:35 +0100

    fixed bit_ternary doc and removed dead code

--- include/eve/module/core/regular/impl/simd/x86/bit_ternary.hpp
+++ include/eve/module/core/regular/impl/simd/x86/bit_ternary.hpp
@@ -75,33 +75,4 @@ namespace eve::detail
       return bit_cast(doit(opts, va, vb, vc), as<w_t>());
     }
   }
-
-  template<std::uint8_t K, scalar_value T, typename N, conditional_expr C, callable_options O>
-  EVE_FORCEINLINE wide<T, N>
-  abs_(EVE_REQUIRES(avx512_), C const& mask, O const& opts, std::integral_constant<std::uint8_t, K> const &,
-       wide<T, N> const& x, wide<T, N> const& y, wide<T, N> const& z) noexcept requires x86_abi<abi_t<T, N>>
-  {
-    auto const            s = alternative(mask, x, as(x));
-    [[maybe_unused]] auto m = expand_mask(mask, as(x)).storage().value;
-    constexpr auto        ca = categorize<wide<T, N>>();
-
-    using w_t = wide<T, N>;
-    using i_t = as_integer_t<w_t, unsigned>;
-    auto va = bit_cast(x, as<i_t>());
-    auto vb = bit_cast(y, as<i_t>());
-    auto vc = bit_cast(z, as<i_t>());
-    auto doit = [s, m](auto const& o, auto a, auto b, auto c)
-    {
-      using c_t = std::integral_constant<std::uint8_t, K>;
-      if      constexpr( C::is_complete )               return s;
-      else if constexpr( ca == category::uint64x8       ) return _mm512_mask_ternary_logic_epi64(s, m, a, b, c, K);
-      else if constexpr( ca == category::uint64x4       ) return _mm256_mask_ternary_logic_epi64(s, m, a, b, c, K);
-      else if constexpr( ca == category::uint64x2       ) return _mm_mask_ternary_logic_epi64   (s, m, a, b, c, K);
-      else if constexpr( ca == category::uint32x16      ) return _mm512_mask_ternary_logic_epi32(s, m, a, b, c, K);
-      else if constexpr( ca == category::uint32x8       ) return _mm256_mask_ternary_logic_epi32(s, m, a, b, c, K);
-      else if constexpr( ca == category::uint32x4       ) return _mm_mask_ternary_logic_epi32   (s, m, a, b, c, K);
-      else return bit_ternary.behavior(cpu_{}, o, c_t{}, a, b, c);
-    };
-      return bit_cast(doit(opts, va, vb, vc), as<w_t>());
-  }
 }
