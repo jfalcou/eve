289b179a1d5252a4a6dc1a46cf4eee32a1b6c683
SadiinsoSnowfall <sadiinso@gmail.com>
2024-11-20 11:09:25 +0100

    added bit_swap_pair tests for signed integers & return type

--- test/unit/module/core/bit_swap_pairs.cpp
+++ test/unit/module/core/bit_swap_pairs.cpp
@@ -9,18 +9,23 @@
 #include <eve/module/core.hpp>
 #include <bit>
 
-//==================================================================================================
-//  bit_swap_pairs simd tests
-//==================================================================================================
+TTS_CASE_TPL("Check eve::bit_swap_pairs return type", eve::test::simd::integers)
+<typename T>(tts::type<T>)
+{
+  using v_t = eve::element_type_t<T>;
+  TTS_EXPR_IS(eve::bit_swap_pairs(T{}, T{}, T{}), T);
+  TTS_EXPR_IS(eve::bit_swap_pairs(T{}, v_t{}, v_t{}), T);
+  TTS_EXPR_IS(eve::bit_swap_pairs(v_t{}, v_t{}, v_t{}), v_t);
+};
+
 TTS_CASE_WITH("Check behavior of bit_swap_pairs(simd) on integral types",
-              eve::test::simd::unsigned_integers,
+              eve::test::simd::integers,
               tts::generate(tts::randoms(eve::valmin, eve::valmax), tts::logicals(0, 3)))
 <typename T, typename U>(T const& a0, U const& t)
 {
   using v_t = eve::element_type_t<T>;
-  using eve::bit_swap_pairs;
 
-  TTS_EQUAL(bit_swap_pairs(a0, 0u, 7u), tts::map([](auto e) -> v_t { return eve::bit_swap_pairs(e, 0u, 7u); }, a0)) << a0 << '\n';
+  TTS_EQUAL(eve::bit_swap_pairs(a0, 0u, 7u), tts::map([](auto e) -> v_t { return eve::bit_swap_pairs(e, 0u, 7u); }, a0)) << a0 << '\n';
   TTS_EQUAL(eve::bit_swap_pairs[t](a0, 0u, 7u), eve::if_else(t, eve::bit_swap_pairs(a0, 0u, 7u), a0));
 
   eve::wide<int, typename T::cardinal_type> wn{[](auto i, auto) { return -i; }};
