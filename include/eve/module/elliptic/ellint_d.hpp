0b7d986b4a8613374f34100710c038e13f6ae637
SadiinsoSnowfall <sadiinso@gmail.com>
2024-11-25 19:28:36 +0100

    elliptic callables

--- include/eve/module/elliptic/ellint_d.hpp
+++ include/eve/module/elliptic/ellint_d.hpp
@@ -20,15 +20,18 @@ namespace eve
   template<typename Options>
   struct ellint_d_t : elementwise_callable<ellint_d_t, Options>
   {
-    template<eve::floating_value T>
-    constexpr EVE_FORCEINLINE
-    T operator()(T a) const noexcept { return EVE_DISPATCH_CALL(a); }
+    template<floating_value T>
+    constexpr EVE_FORCEINLINE T operator()(T a) const noexcept
+    {
+      return this->behavior(as<T>{}, eve::current_api, this->options(), a);
+    }
 
-    template<eve::floating_value T0, eve::floating_value T1>
-    requires (same_lanes_or_scalar<T0, T1>)
-    constexpr EVE_FORCEINLINE
-    eve::common_value_t<T0, T1> operator()(T0 a, T1 b) const noexcept
-    { return EVE_DISPATCH_CALL(a, b); }
+    template<floating_value T0, floating_value T1>
+    constexpr EVE_FORCEINLINE common_value_t<T0, T1> operator()(T0 a, T1 b) const noexcept
+      requires (same_lanes_or_scalar<T0, T1>)
+    {
+      return this->behavior(as<common_value_t<T0, T1>>{}, eve::current_api, this->options(), a, b);
+    }
 
     EVE_CALLABLE_OBJECT(ellint_d_t, ellint_d_);
   };
