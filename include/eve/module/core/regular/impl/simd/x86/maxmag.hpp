bf957ec11ca8203ad39ac20275836d0efe44c1cf
jtlap <jtlapreste@gmail.com>
2024-11-30 10:21:18 +0100

    missing pragma and license

--- include/eve/module/core/regular/impl/simd/x86/maxmag.hpp
+++ include/eve/module/core/regular/impl/simd/x86/maxmag.hpp
@@ -1,3 +1,11 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
 
 #include <eve/detail/category.hpp>
 #include <eve/detail/function/simd/x86/flags.hpp>
