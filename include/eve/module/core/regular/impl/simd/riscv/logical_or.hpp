98a2a82f0a698c0d0d79e31b729eea2e5ea74078
SadiinsoSnowfall <sadiinso@gmail.com>
2024-12-10 16:57:04 +0100

    fix

---
+++ include/eve/module/core/regular/impl/simd/riscv/logical_or.hpp
@@ -0,0 +1,22 @@
+//==================================================================================================
+/*
+  EVE - Expressive Vector Engine
+  Copyright : EVE Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <eve/concept/compatible.hpp>
+#include <eve/concept/value.hpp>
+#include <eve/detail/implementation.hpp>
+
+namespace eve::detail
+{
+  template<callable_options O, typename T, typename U, typename N>
+  EVE_FORCEINLINE logical<wide<T, N>> logical_or_(EVE_REQUIRES(rvv_), O const&, logical<wide<T, N>> a, logical<wide<U, N>> b) noexcept
+    requires(rvv_abi<abi_t<T, N>> || rvv_abi<abi_t<U, N>>)
+  {
+    return __riscv_vmor(a, bit_cast(b, as<logical<wide<T, N>>>{}), N::value);
+  }
+}
