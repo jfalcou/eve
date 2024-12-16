742d79d78de83bdc093932523904b85a32b5bd15
SadiinsoSnowfall <sadiinso@gmail.com>
2024-12-16 22:41:26 +0100

    more comments

--- include/eve/module/core/regular/logical_or.hpp
+++ include/eve/module/core/regular/logical_or.hpp
@@ -80,8 +80,11 @@ namespace eve
     EVE_FORCEINLINE constexpr common_logical_t<T, U> logical_or_(EVE_REQUIRES(cpu_), O const&, T a, U b) noexcept
       requires (relaxed_logical_scalar_value<T> && relaxed_logical_scalar_value<U>) || (std::same_as<T, U>)
     {
+      // case 1: both are scalar/bool, possibly different.
       if  constexpr (relaxed_logical_scalar_value<T>) return a || b;
-      else                              return bit_cast(a.bits() | b.bits(), as<T>{});
+      // case 2: both are logical<wide> of the same type. This is required for most archs as they can only perform
+      // bitswise operations and convert.
+      else                                            return bit_cast(a.bits() | b.bits(), as<T>{});
     }
   }
 }
