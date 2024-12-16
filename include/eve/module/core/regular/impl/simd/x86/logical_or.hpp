7a79aaea9cd697aa9b281a7c982c752187dd563d
SadiinsoSnowfall <sadiinso@gmail.com>
2024-12-16 22:27:35 +0100

    refactor AVX512 code

--- include/eve/module/core/regular/impl/simd/x86/logical_or.hpp
+++ include/eve/module/core/regular/impl/simd/x86/logical_or.hpp
@@ -13,19 +13,10 @@
 
 namespace eve::detail
 {
-  template<callable_options O, typename T, typename U, typename N>
-  EVE_FORCEINLINE auto logical_or_(EVE_REQUIRES(sse2_), O const& opts, logical<wide<T, N>> a, logical<wide<U, N>> b) noexcept
-    requires (x86_abi<abi_t<T, N>> || x86_abi<abi_t<U, N>>)
+  template<arithmetic_scalar_value T, typename N, callable_options O>
+  EVE_FORCEINLINE auto logical_or_(EVE_REQUIRES(avx512_), O const&, logical<wide<T, N>> a, logical<wide<T, N>> b) noexcept
+    requires (x86_abi<abi_t<T, N>>)
   {
-    if constexpr (use_is_wide_logical<abi_t<T, N>>::value)
-    {
-      return logical_or.behavior(cpu_{}, opts, a, b);
-    }
-    else
-    {
-      typename common_logical_t<wide<T, N>, wide<U, N>>::storage_type dst;
-      dst.value = a.storage().value | b.storage().value;
-      return dst;
-    }
+    return logical<wide<T, N>>{a.storage() | b.storage()};
   }
 }
