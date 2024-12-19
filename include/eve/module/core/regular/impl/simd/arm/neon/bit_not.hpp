a7b92b0c2e5a6f69eb152450cf2e4a5fd175dbc4
SadiinsoSnowfall <sadiinso@gmail.com>
2024-12-19 13:39:48 +0100

    neon fix

--- include/eve/module/core/regular/impl/simd/arm/neon/bit_not.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/bit_not.hpp
@@ -20,22 +20,22 @@ namespace eve::detail
     constexpr auto c = categorize<wide<T, N>>();
     using i_t = wide<as_integer_t<T, unsigned>, N>;
 
-          if constexpr(c == category::int64x1 ) return vreinterpret_s64_s8(vmvn_s8(vreinterpret_s8_s64(v)));
-    else  if constexpr(c == category::uint64x1) return vreinterpret_u64_u8(vmvn_u8(vreinterpret_u8_u64(v)));
-    else  if constexpr(c == category::int64x2 ) return vreinterpretq_s64_s8(vmvnq_s8(vreinterpretq_s8_s64(v)));
-    else  if constexpr(c == category::uint64x2) return vreinterpretq_u64_u8(vmvnq_u8(vreinterpretq_u8_u64(v)));
-    else  if constexpr(c == category::int32x2 ) return vmvn_s32(v);
-    else  if constexpr(c == category::uint32x2) return vmvn_u32(v);
-    else  if constexpr(c == category::int32x4 ) return vmvnq_s32(v);
-    else  if constexpr(c == category::uint32x4) return vmvnq_u32(v);
-    else  if constexpr(c == category::int16x4 ) return vmvn_s16(v);
-    else  if constexpr(c == category::uint16x4) return vmvn_u16(v);
-    else  if constexpr(c == category::int16x8 ) return vmvnq_s16(v);
-    else  if constexpr(c == category::uint16x8) return vmvnq_u16(v);
-    else  if constexpr(c == category::int8x8  ) return vmvn_s8(v);
-    else  if constexpr(c == category::uint8x8 ) return vmvn_u8(v);
-    else  if constexpr(c == category::int8x16 ) return vmvnq_s8(v);
-    else  if constexpr(c == category::uint8x16) return vmvnq_u8(v);
-    else                                        return bit_cast(~bit_cast(v,as<i_t>{}), as(v));
+          if constexpr(c == category::int64x1 ) return vreinterpret_s64_s8(vmvn_s8(vreinterpret_s8_s64(w)));
+    else  if constexpr(c == category::uint64x1) return vreinterpret_u64_u8(vmvn_u8(vreinterpret_u8_u64(w)));
+    else  if constexpr(c == category::int64x2 ) return vreinterpretq_s64_s8(vmvnq_s8(vreinterpretq_s8_s64(w)));
+    else  if constexpr(c == category::uint64x2) return vreinterpretq_u64_u8(vmvnq_u8(vreinterpretq_u8_u64(w)));
+    else  if constexpr(c == category::int32x2 ) return vmvn_s32(w);
+    else  if constexpr(c == category::uint32x2) return vmvn_u32(w);
+    else  if constexpr(c == category::int32x4 ) return vmvnq_s32(w);
+    else  if constexpr(c == category::uint32x4) return vmvnq_u32(w);
+    else  if constexpr(c == category::int16x4 ) return vmvn_s16(w);
+    else  if constexpr(c == category::uint16x4) return vmvn_u16(w);
+    else  if constexpr(c == category::int16x8 ) return vmvnq_s16(w);
+    else  if constexpr(c == category::uint16x8) return vmvnq_u16(w);
+    else  if constexpr(c == category::int8x8  ) return vmvn_s8(w);
+    else  if constexpr(c == category::uint8x8 ) return vmvn_u8(w);
+    else  if constexpr(c == category::int8x16 ) return vmvnq_s8(w);
+    else  if constexpr(c == category::uint8x16) return vmvnq_u8(w);
+    else                                        return bit_cast(~bit_cast(w,as<i_t>{}), as(w));
   }
 }
