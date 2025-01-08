3281a0885ae5f62332b30e1af25e52ecf591a197
SadiinsoSnowfall <sadiinso@gmail.com>
2025-01-08 17:35:42 +0100

    riscv test

--- include/eve/module/core/regular/impl/simd/riscv/logical_not.hpp
+++ include/eve/module/core/regular/impl/simd/riscv/logical_not.hpp
@@ -13,6 +13,7 @@
 
 namespace eve::detail
 {
+  #warning test
   template<callable_options O, arithmetic_scalar_value T, typename N>
   EVE_FORCEINLINE logical<wide<T, N>> logical_not_(EVE_REQUIRES(rvv_), O const&, logical<wide<T, N>> v) noexcept
     requires rvv_abi<abi_t<T, N>>
