0e2b3de700a203fd622d4ec7e1a1709abadaba91
SadiinsoSnowfall <sadiinso@gmail.com>
2025-01-16 15:55:37 +0100

    handle product types

--- include/eve/module/core/regular/impl/is_less.hpp
+++ include/eve/module/core/regular/impl/is_less.hpp
@@ -12,7 +12,11 @@ namespace eve::detail
   template<typename T, typename U, callable_options O>
   EVE_FORCEINLINE constexpr common_logical_t<T, U> is_less_(EVE_REQUIRES(cpu_), O const& o, T a, U b) noexcept
   {
-    if constexpr (O::contains(definitely))
+    if constexpr (product_type<T> && product_type<U>)
+    {
+      return convert(kumi::to_tuple(a) < kumi::to_tuple(b), as_element<common_logical_t<T, U>>());
+    }
+    else if constexpr (O::contains(definitely))
     {
       using w_t = common_value_t<T, U>;
       auto aa = w_t{a};
