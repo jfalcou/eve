00f1bd3389c8d096feb8bbd086e12196c0fbfb28
SadiinsoSnowfall <sadiinso@gmail.com>
2025-01-23 19:21:17 +0100

    simplified generic backend

--- include/eve/module/core/regular/impl/is_less.hpp
+++ include/eve/module/core/regular/impl/is_less.hpp
@@ -9,40 +9,24 @@
 
 namespace eve::detail
 {
-  template<typename T, typename U, callable_options O>
-  EVE_FORCEINLINE constexpr common_logical_t<T, U> is_less_(EVE_REQUIRES(cpu_), O const& o, T a, U b) noexcept
+  template<callable_options O, typename T>
+  EVE_FORCEINLINE constexpr common_logical_t<T> is_less_(EVE_REQUIRES(cpu_), O const& o, T a, T b) noexcept
   {
-    if constexpr (product_type<T> && product_type<U>)
+    if constexpr (product_type<T>)
     {
       return kumi::to_tuple(a) < kumi::to_tuple(b);
     }
     else if constexpr (O::contains(definitely))
     {
-      using w_t = common_value_t<T, U>;
-      auto aa = w_t{a};
-      auto bb = w_t{b};
+      auto tol = o[definitely].value(T{});
 
-      auto tol = o[definitely].value(w_t{});
-
-      if constexpr (integral_value<decltype(tol)>) return aa < eve::prev(bb, tol);
-      else                                         return aa < fam(bb, -tol, eve::max(eve::abs(aa), eve::abs(bb)));
+      if constexpr (integral_value<decltype(tol)>) return a < eve::prev(b, tol);
+      else                                         return a < fam(b, -tol, eve::max(eve::abs(a), eve::abs(b)));
     }
     else
     {
-      if constexpr (scalar_value<T>)
-      {
-        if      constexpr (scalar_value<U>) return common_logical_t<T, U>(a < b);
-        // because of the auto-conversion rules in elementwise_callable, we can assume that T will have the type
-        // element_type<U> at that point and that the appropriate wide<S> x S backend doesn't exist.
-        else                                return is_less(U{a}, b);
-      }
-      else
-      {
-        if      constexpr (simd_value<U>) return map(is_less, a, b);
-        // because of the auto-conversion rules in elementwise_callable, we can assume that U will have the type
-        // element_type<T> at that point and that the appropriate wide<S> x S backend doesn't exist.
-        else                              return is_less(a, T{b});
-      }
+      if constexpr (scalar_value<T>) return as_logical_t<T>(a < b);
+      else                           return map([](auto e, auto f) { return e < f; }, a, b);
     }
   }
 }
