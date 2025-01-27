66351d838526fe6b76cfdad5c4a14530f3262499
SadiinsoSnowfall <sadiinso@gmail.com>
2025-01-27 17:25:07 +0100

    removed unused apply_over variant

--- include/eve/detail/apply_over.hpp
+++ include/eve/detail/apply_over.hpp
@@ -16,27 +16,14 @@
 
 namespace eve::detail
 {
-  // -----------------------------------------------------------------------------------------------
-  // N parameters apply_over
   template<typename Obj, value T0, value ... T>
-  EVE_FORCEINLINE  auto apply_over(Obj f
-                                  , T0 const & arg0
-                                  , T const &... args ) noexcept
+  EVE_FORCEINLINE  auto apply_over(Obj f, T0 const & arg0, T const &... args) noexcept
   requires simd_value<T0> || (simd_value<T> ||  ...)
   {
     constexpr bool any_aggregated = has_aggregated_abi_v<T0> || (has_aggregated_abi_v<T> || ...);
     constexpr bool any_emulated   = has_emulated_abi_v<T0>   || (has_emulated_abi_v<T>   || ...);
-    if constexpr(any_aggregated) return aggregate(f, arg0, args...);
+    if      constexpr(any_aggregated) return aggregate(f, arg0, args...);
     else if constexpr(any_emulated)   return map(f, arg0, args...);
     else                              return f(arg0, args...);
-
-  }
-
-  template<typename Obj, simd_value T>
-  EVE_FORCEINLINE auto apply_over(Obj f, T const & v)
-  {
-    if constexpr(has_aggregated_abi_v<T>)     return aggregate(f, v);
-    else if constexpr(has_emulated_abi_v<T>)  return map(f, v);
-    else                                      return f(v);
   }
 }
