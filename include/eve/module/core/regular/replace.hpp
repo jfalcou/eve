07622399979371cfdf27ce15b2d75a6766af8143
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2025-02-10 18:38:37 +0100

    Made replace_ignored a new format callable (#2061)

--- include/eve/module/core/regular/replace.hpp
+++ include/eve/module/core/regular/replace.hpp
@@ -15,56 +15,61 @@
 
 namespace eve
 {
-// TODO DOC is it to be in user interface ?
-//================================================================================================
-//! @addtogroup core_logical
-//! @{
-//!    @var replace_ignored
-//!    @brief A small helper to replace ignored values
-//!
-//!   @groupheader{Header file}
-//!   @code
-//!   #include <eve/module/core.hpp>
-//!   @endcode
-//!
-//!   A convinience wrapper around if_else.
-//!
-//!   @groupheader{Callable Signatures}
-//!
-//!   @code
-//!   namespace eve
-//!   {
-//!      template< eve::value T, eve::conditional_expr Ignore, eve::value Other >
-//!      auto replace_ignored(T x, Ignore ignore, Other with )
-//!        -> decltype(eve::if_else(ignore, x, with))
-//!   }
-//!   @endcode
-//!
-//!   **Parameters**
-//!
-//!   * `x` - main value
-//!   * `ignore` - selection
-//!   * `with` - value to replace with
-//!
-//!   **Return value**
-//!
-//!   Same as if_else.
-//!
-//================================================================================================
-
-EVE_MAKE_CALLABLE(replace_ignored_, replace_ignored);
-
-namespace detail
-{
-  //================================================================================================
-  // Helper function - Replace ignored value with an alternative
-  //================================================================================================
-  template<simd_value Wide, conditional_expr Ignore, typename Other>
-  EVE_FORCEINLINE auto
-  replace_ignored_(EVE_SUPPORTS(cpu_), Wide x, Ignore ignore, Other with) noexcept
-      -> decltype(eve::if_else(ignore, x, with))
+  template<typename Options>
+  struct replace_ignored_t : callable<replace_ignored_t, Options>
   {
-    return eve::if_else(ignore, x, with);
-  }
-}
+    template<simd_value T, relative_conditional_expr Ignore, value Other>
+    constexpr EVE_FORCEINLINE T operator()(T x, Ignore ignore, Other with) const noexcept
+    {
+      return eve::if_else(ignore, x, with);
+    }
+
+    template<simd_value T, relative_conditional_expr Ignore, generator Other>
+    constexpr EVE_FORCEINLINE T operator()(T x, Ignore ignore, Other with) const noexcept
+    {
+      return eve::if_else(ignore, x, with);
+    }
+  };
+
+  //================================================================================================
+  //! @addtogroup core_logical
+  //! @{
+  //!    @var replace_ignored
+  //!    @brief A small helper to replace ignored values
+  //!
+  //!   @groupheader{Header file}
+  //!   @code
+  //!   #include <eve/module/core.hpp>
+  //!   @endcode
+  //!
+  //!   A convinience wrapper around if_else.
+  //!
+  //!   @groupheader{Callable Signatures}
+  //!
+  //!   @code
+  //!   namespace eve
+  //!   {
+  //!      template<simd_value T, relative_conditional_expr Ignore, value Other>
+  //!      T replace_ignored(T x, Ignore ignore, Other with);
+  //!
+  //!      template<simd_value T, relative_conditional_expr Ignore, generator Other>
+  //!      T replace_ignored(T x, Ignore ignore, Other with);
+  //!   }
+  //!   @endcode
+  //!
+  //!   **Parameters**
+  //!
+  //!   * `x` - main value
+  //!   * `ignore` - selection
+  //!   * `with` - value to replace with
+  //!
+  //!   **Return value**
+  //!
+  //!   Same as if_else.
+  //!
+  //================================================================================================
+  inline constexpr auto replace_ignored = functor<replace_ignored_t>;
+  //================================================================================================
+  //! @}
+  //================================================================================================
 }
