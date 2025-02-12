4c6d0a0b8866de2500c9897474db1cf5ca588fe6
jfalcou <jfalcou@users.noreply.github.com>
2025-02-12 14:34:31 +0000

    deploy: e85f518b5ebb98a7329a01c5c0026623edc4c996

--- search/variables_6.js
+++ search/variables_6.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['gamma_5fp_0',['gamma_p',['../group__special_gaca2a6730649297012022897eb2b4b549.html#gaca2a6730649297012022897eb2b4b549',1,'eve']]],
   ['gamma_5fp_5finv_1',['gamma_p_inv',['../group__special_ga84e6f5cc6ba688751341a9a2a314363e.html#ga84e6f5cc6ba688751341a9a2a314363e',1,'eve']]],
-  ['gather_2',['gather',['../group__core__simd_gacd8956ad1daf07ac479efc00b3a92bbf.html#gacd8956ad1daf07ac479efc00b3a92bbf',1,'eve']]],
+  ['gather_2',['gather',['../group__core__simd_ga3cbe6596b8e7ca4d4d0611ac642fcf39.html#ga3cbe6596b8e7ca4d4d0611ac642fcf39',1,'eve']]],
   ['gcd_3',['gcd',['../group__combinatorial_gadcf37697731fa4c94ebe972425b2dbc3.html#gadcf37697731fa4c94ebe972425b2dbc3',1,'eve']]],
   ['gd_4',['gd',['../group__math__hyper_gac2f0fbac24a6b4ca2f12dcdd07c054bc.html#gac2f0fbac24a6b4ca2f12dcdd07c054bc',1,'eve']]],
   ['gegenbauer_5',['gegenbauer',['../group__polynomial_ga654af420c26859a9b088461f2b49d336.html#ga654af420c26859a9b088461f2b49d336',1,'eve']]],
