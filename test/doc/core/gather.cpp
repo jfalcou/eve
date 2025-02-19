786bcbc4dafb096cad9e4be4a1aae456617a05af
SadiinsoSnowfall <sadiinso@gmail.com>
2025-02-19 16:38:23 +0100

    fix SVE doc tests

--- test/doc/core/gather.cpp
+++ test/doc/core/gather.cpp
@@ -5,7 +5,7 @@ int main()
 {
   std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-  eve::wide<int, eve::fixed<4>> res = eve::gather(v.data(), eve::wide<char, eve::fixed<4>>{0, 2, 2, 11});
+  eve::wide<int, eve::fixed<4>> res = eve::gather(v.data(), eve::wide<unsigned char, eve::fixed<4>>{0, 2, 2, 11});
 
   std::cout << "res -> " << res << std::endl;
 }
