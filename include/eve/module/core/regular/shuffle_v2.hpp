c88d4d6931774c53ecf6fc7031dffe78285bd722
Joel Falcou <joel.falcou@lri.fr>
2025-02-28 17:22:40 +0100

    Explicit fix for shadow - temporary

--- include/eve/module/core/regular/shuffle_v2.hpp
+++ include/eve/module/core/regular/shuffle_v2.hpp
@@ -7,17 +7,7 @@
 //==================================================================================================
 #pragma once
 
-#include <eve/detail/spy.hpp>
-#if defined(SPY_COMPILER_IS_GCC) || defined(SPY_COMPILER_IS_CLANG)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
-#endif
-
 //TODO DOC here
 #include <eve/detail/shuffle_v2/shuffle_v2_fwd.hpp>
 #include <eve/detail/shuffle_v2/native_shuffle_lookup.hpp>
 #include <eve/detail/shuffle_v2/shuffle_v2_driver.hpp>
-
-#if defined(SPY_COMPILER_IS_GCC) || defined(SPY_COMPILER_IS_CLANG)
-#pragma GCC diagnostic pop
-#endif
