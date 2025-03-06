97bf82df123b82d2c89beab3ab4c28a54036ea76
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-06 13:11:59 +0100

    more cleanup

--- include/eve/module/core/regular/impl/simd/arm/sve/has_equal_in.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/has_equal_in.hpp
@@ -20,11 +20,11 @@ namespace eve::detail
 {
   template<callable_options O, typename T, typename N, typename Pred>
   constexpr EVE_FORCEINLINE logical<wide<T, N>> has_equal_in_(EVE_REQUIRES(sve2_), O const& opts, wide<T, N> x, wide<T, N> match_against, Pred op) noexcept
-    requires (sve_abi<abi_t<T, N>> && (sizeof(T) <= 2))
+    requires (sve_abi<abi_t<T, N>> && (sizeof(T) <= 2) && integral_scalar_value<T>)
   {
     constexpr auto c = categorize<wide<T, N>>();
     
-    if constexpr (!same_callable<Pred, eve::is_equal> || !match(c, category::int8, category::uint8, category::int16, category::uint16))
+    if constexpr (!same_callable<Pred, eve::is_equal>)
     {
       return has_equal_in.behavior(cpu_{}, opts, x, match_against, op);
     }
