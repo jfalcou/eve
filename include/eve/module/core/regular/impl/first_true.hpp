9e6dcd8400aefae25866c954aed82ebb17f94caa
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-07 17:26:44 +0100

    go

--- include/eve/module/core/regular/impl/first_true.hpp
+++ include/eve/module/core/regular/impl/first_true.hpp
@@ -58,17 +58,33 @@ namespace eve::detail
   }
 
   /*
+    top bits
+  */
+ template<callable_options O, logical_simd_value Logical>
+ EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), O const&, top_bits<Logical> mmask) noexcept
+ {
+   return any(mmask) ? std::make_optional(first_true_guaranteed(mmask)) : std::nullopt;
+ }
+
+ template<callable_options O, conditional_expr C, logical_simd_value Logical>
+ EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), C const& cx, O const& opts, top_bits<Logical> mmask) noexcept
+ {
+    mmask &= top_bits{cx.mask(as<Logical>{})};
+    return any(mmask) ? std::make_optional(first_true_guaranteed(mmask)) : std::nullopt;
+ }
+
+  /*
     logicals
   */
-  template<callable_options O, relative_conditional_expr C, logical_value T>
+  template<callable_options O, conditional_expr C, logical_value T>
   EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), C const& cx, O const&, T const& v) noexcept
   {
     if constexpr (scalar_value<T>)                   return count_true[cx](v.value());
     if constexpr (C::is_complete && !C::is_inverted) return std::nullopt;
     else if constexpr (has_emulated_abi_v<T>)
     {
-      std::ptrdiff_t first = cond.offset(eve::as<T> {});
-      std::ptrdiff_t last  = first + cond.count(eve::as<T> {});
+      std::ptrdiff_t first = cx.offset(eve::as<T> {});
+      std::ptrdiff_t last  = first + cx.count(eve::as<T> {});
 
       while (first != last)
       {
@@ -84,13 +100,14 @@ namespace eve::detail
       // No ignore, we might luck out even if some elements should not be counted.
       if (!eve::any(v)) return std::nullopt;
 
-      top_bits mmask {v, cx};
-      if constexpr (C::is_complete) return first_true_guaranteed(mmask);
-      else                          return first_true(mmask);
+      if      constexpr (C::is_complete)               return first_true_guaranteed(top_bits{v, cx});
+      else if constexpr (relative_conditional_expr<C>) return first_true(top_bits{v, cx});
+      else                                             return first_true[cx](top_bits{v});
     }
     else
     {
-      return first_true(top_bits{v, cx});
+      if constexpr (relative_conditional_expr<C>) return first_true(top_bits{v, cx});
+      else                                        return first_true[cx](top_bits{v});
     }
   }
 
@@ -99,20 +116,5 @@ namespace eve::detail
   {
     if constexpr (scalar_value<T>) return first_true(v.value());
     else                           return first_true_(EVE_TARGETS(cpu_), ignore_none, opts, v);
-  }
-
-  /*
-    top bits
-  */
-  template<callable_options O, logical_simd_value Logical>
-  EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), O const&, top_bits<Logical> mmask) noexcept
-  {
-    return any(nmask) ? std::make_optional(first_true_guaranteed(mmask)) : std::nullopt;
-  }
-
-  template<callable_options O, conditional_expr C, logical_simd_value Logical>
-  EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), C const& cx, O const&, top_bits<Logical> mmask) noexcept
-  {
-    //TODO
   }
 }
