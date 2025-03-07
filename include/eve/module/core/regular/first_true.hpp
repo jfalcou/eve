589c4030b10b91c45aa8d08d42836abcea49873d
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-07 17:26:44 +0100

    go

--- include/eve/module/core/regular/first_true.hpp
+++ include/eve/module/core/regular/first_true.hpp
@@ -15,13 +15,13 @@ namespace eve
   struct first_true_t : conditional_callable<first_true_t, Options>
   {
     template<relaxed_logical_value T>
-    constexpr EVE_FORCEINLINE std::ptrdiff_t operator()(T v) const noexcept
+    constexpr EVE_FORCEINLINE std::optional<std::ptrdiff_t> operator()(T v) const noexcept
     {
       return EVE_DISPATCH_CALL(v);
     }
 
     template<logical_simd_value T>
-    constexpr EVE_FORCEINLINE std::ptrdiff_t operator()(top_bits<T> v) const noexcept
+    constexpr EVE_FORCEINLINE std::optional<std::ptrdiff_t> operator()(top_bits<T> v) const noexcept
     {
       return EVE_DISPATCH_CALL(v);
     }
@@ -56,28 +56,27 @@ namespace eve
   //!   std::optional<std::ptrdiff_t> first_true(L m);                                                                  // 1
   //!
   //!   template <logical_simd_value L>
-  //!   std::optional<std::ptrdiff_t> first_true(top_bits<L> m);                                                        // 2
+  //!   std::optional<std::ptrdiff_t> first_true(top_bits<L> m);                                                        // 1
   //!
   //!   // lane masking
-  //!   constexpr std::optional<std::ptrdiff_t> first_true[conditional_expr auto c](/* any of the above */) noexcept;   // 3
-  //!   constexpr std::optional<std::ptrdiff_t> first_true[logical_value auto c](/* any of the above */) noexcept;      // 3
+  //!   constexpr std::optional<std::ptrdiff_t> first_true[conditional_expr auto c](/* any of the above */) noexcept;   // 1
+  //!   constexpr std::optional<std::ptrdiff_t> first_true[logical_value auto c](/* any of the above */) noexcept;      // 1
   //!
   //!   @endcode
   //!
   //!   **Parameters**
   //!
   //!     * m - logical_value or top_bits where to find first true value
-  //!     * ignore - ignored elements are considered false. Only supported for
-  //!                `logical_simd_value`
   //!
   //!   **Return value**
   //!
-  //!    Returns `std::nullopt` if eve::none(m).
-  //!    Otherwise returns 0 based index.
+  //!    1. An `std::optional` containing the index of the first non-zero element, `std::nullopt` if there are none.
+  //!    2. Same as 1. but lanes masked by the condition are ignored during the search.
   //!
   //!  @groupheader{Example}
   //!
   //!  @godbolt{doc/core/first_true.cpp}
+  //!
   //================================================================================================
   inline constexpr auto first_true = functor<first_true_t>;
   //================================================================================================
@@ -87,10 +86,10 @@ namespace eve
 
 #include <eve/module/core/regular/impl/first_true.hpp>
 
-// #if defined(EVE_INCLUDE_ARM_NEON_HEADER)
-// #  include <eve/module/core/regular/impl/simd/arm/neon/first_true.hpp>
-// #endif
+#if defined(EVE_INCLUDE_ARM_NEON_HEADER)
+#  include <eve/module/core/regular/impl/simd/arm/neon/first_true.hpp>
+#endif
 
-// #if defined(EVE_INCLUDE_ARM_SVE_HEADER)
-// #  include <eve/module/core/regular/impl/simd/arm/sve/first_true.hpp>
-// #endif
+#if defined(EVE_INCLUDE_ARM_SVE_HEADER)
+#  include <eve/module/core/regular/impl/simd/arm/sve/first_true.hpp>
+#endif
