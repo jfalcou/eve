93e2e4dc109d4cc49dd884fe6899e554e306a162
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-19 16:36:03 +0100

    try fix

--- tools/clang_coverage_gather.sh
+++ tools/clang_coverage_gather.sh
@@ -12,9 +12,9 @@ else
 fi
 
 # merge all .profraw files into a single .profdata file
-llvm-profdata merge -o $wdir/merged.profdata $wdir/unit/profraw.d/*.profraw
+llvm-profdata merge --sparse -o $wdir/merged.profdata $wdir/unit/profraw.d/*.profraw
 
-object_files=$(find $wdir -type f -name "*.o" -path "*unit.*")
+object_files=$(find $wdir -type f -name "*.o")
 object_files_arg=""
 for obj_file in $object_files; do
   object_files_arg="$object_files_arg --object $obj_file"
@@ -22,3 +22,5 @@ done
 
 # convert the .profraw file into a .lcov file
 llvm-cov export -format=lcov -instr-profile $wdir/merged.profdata $object_files_arg > $wdir/merged.lcov
+
+# llvm-cov show -dump --instr-profile $wdir/merged.profdata $object_files_arg
