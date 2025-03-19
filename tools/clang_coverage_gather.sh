7df11435ea1d5f3e2d0e87bfe0a1ed2d656fbf22
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-19 11:48:55 +0100

    added conversion to lcov format step

--- tools/clang_coverage_gather.sh
+++ tools/clang_coverage_gather.sh
@@ -6,8 +6,19 @@
 ##==================================================================================================
 
 if [ -z "$1" ]; then
-    base_dir=$(git rev-parse --show-toplevel)
-    llvm-profdata merge -o $base_dir/build/merged.profdata $base_dir/build/unit/profraw.d/*.profraw
+  base_dir="build"
 else
-    llvm-profdata merge -o $1/merged.profdata $1/unit/profraw.d/*.profraw
+  wdir=$1
 fi
+
+# merge all .profraw files into a single .profdata file
+llvm-profdata merge -o $wdir/merged.profdata $wdir/unit/profraw.d/*.profraw
+
+object_files=$(find $wdir -type f -name "*.o" -path "*unit.*")
+object_files_arg=""
+for obj_file in $object_files; do
+  object_files_arg="$object_files_arg --object $obj_file"
+done
+
+# convert the .profraw file into a .lcov file
+llvm-cov export -format=lcov -instr-profile $wdir/merged.profdata $object_files_arg > $wdir/merged.lcov
