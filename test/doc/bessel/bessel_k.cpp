3a589905ba42a11c49f5c236a8b6f9bb3320658a
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-27 11:14:14 +0100

    prevent gcc 12 regression trigger on bessel doctests

--- test/doc/bessel/bessel_k.cpp
+++ test/doc/bessel/bessel_k.cpp
@@ -1,14 +1,14 @@
-// revision 1
+#include <eve/eve.hpp>
 #include <eve/module/bessel.hpp>
 #include <iostream>
 
-eve::wide<double> wdf([](auto i, auto c)->double{ return 2*(i-c/2);});
-eve::wide<std::uint64_t> wu([](auto i, auto )->std::uint64_t{ return 2*i;});
-eve::wide wd{0.5, 1.5, 0.1, 1.0, 19.0, 0.0, 0.0, 10000.0};
-eve::wide m{0.5, -1.0, 1.5, -2.0, 2.5, -2.6, 3.2, -12.0};
-
 int main()
 {
+   eve::wide<double> wdf([](auto i, auto c)->double{ return 2*(i-c/2);});
+   eve::wide<std::uint64_t> wu([](auto i, auto )->std::uint64_t{ return 2*i;});
+   eve::wide wd{0.5, 1.5, 0.1, 1.0, 19.0, 0.0, 0.0, 10000.0};
+   eve::wide m{0.5, -1.0, 1.5, -2.0, 2.5, -2.6, 3.2, -12.0};
+
    std::cout << "<- wdf = " << wdf << "\n";
    std::cout << "<- wu = " << wu << "\n";
    std::cout << "<- m  = " << m << "\n";
