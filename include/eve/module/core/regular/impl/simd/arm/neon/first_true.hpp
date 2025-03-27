c3f4c88a1696e9d559dff31516779cd1cd1f4749
SadiinsoSnowfall <sadiinso@gmail.com>
2025-03-27 09:53:03 +0100

    [no ci] refactor

--- include/eve/module/core/regular/impl/simd/arm/neon/first_true.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/first_true.hpp
@@ -13,10 +13,13 @@
 
 namespace eve::detail
 {
-  template<callable_options O, relative_conditional_expr C, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(neon128_), C const& cx, O const& opts, logical<wide<T, N>> v) noexcept
+  template<callable_options O, arithmetic_scalar_value T, typename N>
+  EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(neon128_), O const& opts, logical<wide<T, N>> v) noexcept
     requires std::same_as<abi_t<T, N>, arm_128_>
   {
+    using C = rbr::result::fetch_t<condition_key, O>;
+    auto cx = opts[condition_key];
+
     if constexpr (C::is_complete && !C::is_inverted) return std::nullopt;
     // we still have to convert down here, so we can do it before ignore.
     else if constexpr ((eve::current_api < eve::asimd) && (sizeof(T) >= 2))
@@ -26,12 +29,5 @@ namespace eve::detail
       return first_true[cx](halved);
     }
     else return first_true.behavior(cpu_{}, opts, v);
-  }
-
-  template<callable_options O, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(neon128_), O const& opts, logical<wide<T, N>> v) noexcept
-    requires std::same_as<abi_t<T, N>, arm_128_>
-  {
-    return first_true_(EVE_TARGETS(current_api_type), ignore_none, opts, v);
   }
 }
