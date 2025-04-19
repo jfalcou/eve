36569ade0c34a3e178be604e03fe46a3f6952b13
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2025-04-19 14:28:09 +0200

    Made first_true a new format callable (#2076)

--- include/eve/module/core/regular/impl/first_true.hpp
+++ include/eve/module/core/regular/impl/first_true.hpp
@@ -44,66 +44,87 @@ EVE_FORCEINLINE std::ptrdiff_t
   }
 }
 
-template<logical_simd_value T, relative_conditional_expr C>
+template<callable_options O, logical_value T>
 EVE_FORCEINLINE std::optional<std::ptrdiff_t>
-                first_true_(EVE_SUPPORTS(cpu_), C const                &cond, T const                &v) noexcept
+                first_true_(EVE_REQUIRES(cpu_), O const& opts, T const& v) noexcept
 {
+  using C = rbr::result::fetch_t<condition_key, O>;
+  auto cond = opts[condition_key];
+
+  if constexpr (scalar_value<T>)                         return first_true[cond](v.value());
+  else
   if constexpr( C::is_complete && !C::is_inverted ) return {};
   else if constexpr( has_emulated_abi_v<T> )
   {
-    std::ptrdiff_t first = cond.offset(eve::as<T> {});
-    std::ptrdiff_t last  = first + cond.count(eve::as<T> {});
-
-    while( first != last )
+    if constexpr (relative_conditional_expr<C>)
     {
-      if( v.get(first) ) return first;
-      ++first;
+      std::ptrdiff_t first = cond.offset(eve::as<T> {});
+      std::ptrdiff_t last  = first + cond.count(eve::as<T> {});
+      constexpr std::ptrdiff_t size = T::size();
+      EVE_ASSUME((first >= 0) && (first <= last) && (last <= size));
+
+      while( first != last )
+      {
+        if( v.get(first) ) return first;
+        ++first;
+      }
+
+      return {};
+    }
+    else
+    {
+      auto mask = expand_mask(cond, as(v));
+
+      for (std::ptrdiff_t i = 0; i < v.size(); ++i)
+        if (v.get(i) && mask.get(i))
+          return i;
+
+      return std::nullopt;
+    }
+  }
+  else
+  {
+    // This is pretty good for aggreagted as well.
+    if constexpr( !top_bits<T>::is_cheap )
+    {
+      // No ignore, we might luck out even if some elements should not be counted.
+      if( !eve::any(v) ) return {};
+
+      // any + ignore_none
+      if constexpr (C::is_complete)
+      {
+        return first_true_guaranteed(top_bits{v, cond});
+      }
     }
 
-    return {};
-  }
-  // This is pretty good for aggreagted as well.
-  else if constexpr( !top_bits<T>::is_cheap )
-  {
-    // No ignore, we might luck out even if some elements should not be counted.
-    if( !eve::any(v) ) return {};
-
-    top_bits mmask {v, cond};
-    if constexpr( C::is_complete ) return first_true_guaranteed(mmask);
-    else return first_true(mmask);
-  }
-  else
-  {
-    top_bits mmask {v, cond};
-    return first_true(mmask);
+    if constexpr (relative_conditional_expr<C>) return first_true(top_bits{v, cond});
+    else                                        return first_true[cond](top_bits{v});
   }
 }
 
+template<callable_options O>
 EVE_FORCEINLINE std::optional<std::ptrdiff_t>
-                first_true_(EVE_SUPPORTS(cpu_), bool v) noexcept
+                first_true_(EVE_REQUIRES(cpu_), O const& opts, bool v) noexcept
 {
-  if( !v ) return std::nullopt;
-  return 0;
-}
-
-template<value T>
-EVE_FORCEINLINE std::optional<std::ptrdiff_t>
-                first_true_(EVE_SUPPORTS(cpu_), T const                &v) noexcept
-{
-  if constexpr( !scalar_value<T> ) return eve::first_true[ignore_none](v);
-  else
+  if constexpr (match_option<condition_key, O, ignore_none_>)
   {
     if( !v ) return std::nullopt;
     return 0;
   }
+  else
+  {
+    return opts[condition_key].mask(as(v)) && v ? std::make_optional(0) : std::nullopt;
+  }
 }
 
-template<logical_simd_value Logical>
-EVE_FORCEINLINE std::optional<std::ptrdiff_t>
-                first_true_(EVE_SUPPORTS(cpu_), top_bits<Logical> mmask) noexcept
+template<callable_options O, logical_simd_value Logical>
+EVE_FORCEINLINE std::optional<std::ptrdiff_t> first_true_(EVE_REQUIRES(cpu_), O const& opts, top_bits<Logical> mmask) noexcept
 {
+  if constexpr (!match_option<condition_key, O, ignore_none_>)
+  {
+    mmask &= top_bits{expand_mask(opts[condition_key], as<Logical>{})};
+  }
   if( !any(mmask) ) return {};
   return first_true_guaranteed(mmask);
 }
-
-} // namespace eve
+}
