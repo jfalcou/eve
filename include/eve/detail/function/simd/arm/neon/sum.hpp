f887d3e58cd862a5bcdca851ae7eb51c8ead90a0
SadiinsoSnowfall <sadiinso@gmail.com>
2025-04-29 14:49:05 +0200

    Made sum a new format callable

--- include/eve/detail/function/simd/arm/neon/sum.hpp
+++ include/eve/detail/function/simd/arm/neon/sum.hpp
@@ -14,7 +14,7 @@
 namespace eve::detail
 {
   template<typename T, typename N>
-  EVE_FORCEINLINE wide<T,N> arm_sum_impl(wide<T,N> v) noexcept
+  EVE_FORCEINLINE wide<T, N> arm_sum_impl(wide<T,N> v) noexcept
   {
     constexpr auto c = categorize<wide<T, N>>();
 
@@ -27,46 +27,46 @@ namespace eve::detail
     else  if constexpr( c== category::int8x8    ) return vpadd_s8(v,v);
   }
 
-  template<typename T, typename N>
-  EVE_FORCEINLINE wide<T,N> sum_( EVE_SUPPORTS(neon128_), splat_type const&, wide<T,N> v) noexcept
-  requires arm_abi<abi_t<T, N>>
+  template<callable_options O, typename T, typename N>
+  EVE_FORCEINLINE wide<T,N> sum_(EVE_REQUIRES(neon128_), O const&, wide<T, N> v) noexcept
+    requires (arm_abi<abi_t<T, N>> && O::contains(splat2) && match_option<condition_key, O, ignore_none_>)
   {
-          if constexpr(N::value == 1)  return v;
-    else  if constexpr(current_api >= asimd)
+    if      constexpr (N::value == 1) return v;
+    else if constexpr (current_api >= asimd)
     {
-      return wide<T,N>(sum(v));
+      return wide<T, N>{sum(v)};
     }
     else
     {
-      if constexpr( std::same_as<abi_t<T,N>, arm_64_> )
+      if constexpr (std::same_as<abi_t<T, N>, arm_64_>)
       {
         v = slide_garbage(v);
-        if constexpr(sizeof(T) <= 4)  v = arm_sum_impl(v);
-        if constexpr(sizeof(T) <= 2)  v = arm_sum_impl(v);
-        if constexpr(sizeof(T) <= 1)  v = arm_sum_impl(v);
+        if constexpr (sizeof(T) <= 4) v = arm_sum_impl(v);
+        if constexpr (sizeof(T) <= 2) v = arm_sum_impl(v);
+        if constexpr (sizeof(T) <= 1) v = arm_sum_impl(v);
         return v;
       }
       else
       {
-        if constexpr(sizeof(T) == 8) return wide<T,N>{v.get(0)+v.get(1)};
+        if constexpr(sizeof(T) == 8) return wide<T, N>{ v.get(0) + v.get(1) };
         else
         {
-          auto [l, h] = v.slice();
-          l = splat(sum)(l+h);
-          return wide<T,N>{l, l};
+          const auto [l, h] = v.slice();
+          const auto r = sum[splat2](l + h);
+          return wide<T, N>{ r, r };
         }
       }
     }
   }
 
-  template<typename T, typename N>
-  EVE_FORCEINLINE T sum_( EVE_SUPPORTS(neon128_), wide<T,N> v) noexcept
-  requires arm_abi<abi_t<T, N>>
+  template<callable_options O, typename T, typename N>
+  EVE_FORCEINLINE T sum_(EVE_REQUIRES(neon128_), O const&, wide<T, N> v) noexcept
+    requires (arm_abi<abi_t<T, N>> && !O::contains(splat2) && match_option<condition_key, O, ignore_none_>)
   {
-    if constexpr(N::value == 1)  return v.get(0);
+    if constexpr (N::value == 1) return v.get(0);
     else
     {
-      if constexpr(current_api >= asimd)
+      if constexpr (current_api >= asimd)
       {
         v = slide_garbage(v);
         constexpr auto c = categorize<wide<T, N>>();
@@ -91,12 +91,12 @@ namespace eve::detail
       }
       else
       {
-              if constexpr( std::same_as<abi_t<T,N>, arm_64_> ) return splat(sum)(v).get(0);
-        else  if constexpr(sizeof(T) == 8)                      return v.get(0)+v.get(1);
+        if      constexpr (std::same_as<abi_t<T,N>, arm_64_>) return sum[splat2](v).get(0);
+        else if constexpr (sizeof(T) == 8)                    return v.get(0) + v.get(1);
         else
         {
-          auto [l,h] = v.slice();
-          return splat(eve::detail::sum)(l+h).get(0);
+          const auto [ l, h ] = v.slice();
+          return sum[splat2](l + h).get(0);
         }
       }
     }
