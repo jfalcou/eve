b0f4a41a5280fda1fcea965c817936b1980f86fc
SadiinsoSnowfall <sadiinso@gmail.com>
2025-05-05 14:00:57 +0200

    minimized total lines changed

--- include/eve/module/core/regular/impl/simd/arm/sve/all.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/all.hpp
@@ -13,24 +13,25 @@
 
 namespace eve::detail
 {
-  template<callable_options O, scalar_value T, typename N>
-  EVE_FORCEINLINE bool all_(EVE_REQUIRES(sve_), O const& opts, logical<wide<T, N>> v) noexcept
-    requires sve_abi<abi_t<T, N>>
+template<callable_options O, scalar_value T, typename N>
+EVE_FORCEINLINE bool
+all_(EVE_REQUIRES(sve_), O const& opts, logical<wide<T, N>> v) noexcept
+requires sve_abi<abi_t<T, N>>
+{
+  using C = rbr::result::fetch_t<condition_key, O>;
+  auto cx = opts[condition_key];
+
+  if constexpr( C::is_complete )
   {
-    using C = rbr::result::fetch_t<condition_key, O>;
-    auto cx = opts[condition_key];
-
-    if constexpr (C::is_complete)
-    {
-      if constexpr (C::is_inverted) return count_true(v) == N::value;
-      else                          return true;
-    }
-    else
-    {
-      const auto masked_count = count_true[cx](v);
-
-      if constexpr (relative_conditional_expr<C>) return masked_count == cx.count(as<wide<T, N>>{});
-      else                                        return masked_count == count_true(expand_mask(cx, as<wide<T, N>>{}));
-    }
+    if constexpr( C::is_inverted )  return count_true(v) == N::value;
+    else                            return true;
   }
+  else
+  {
+    const auto masked_count = count_true[cx](v);
+
+    if constexpr (relative_conditional_expr<C>) return masked_count == cx.count(as<wide<T, N>>{});
+    else                                        return masked_count == count_true(expand_mask(cx, as<wide<T, N>>{}));
+  }
+}
 }
