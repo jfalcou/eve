6eeeccd02c0bfc4ce6c46301e48bf32eb1cf6bdf
SadiinsoSnowfall <sadiinso@gmail.com>
2025-05-12 15:51:04 +0200

    minimized changes count

--- include/eve/detail/function/simd/common/sum.hpp
+++ include/eve/detail/function/simd/common/sum.hpp
@@ -18,6 +18,31 @@
 
 namespace eve::detail
 {
+  template<callable_options O, arithmetic_scalar_value T, typename N>
+  EVE_FORCEINLINE wide<T, N> sum_(EVE_REQUIRES(cpu_), O const& opts, wide<T, N> v
+                            ) noexcept
+    requires (O::contains(splat2))
+  {
+    using C = rbr::result::fetch_t<condition_key, O>;
+
+    if constexpr (std::same_as<C, ignore_none_>)
+    {
+            if constexpr( N::value == 1 )               return v;
+      else  if constexpr( is_emulated_v<abi_t<T, N>>  ) return wide<T,N>( eve::detail::sum(v) );
+      else  if constexpr( is_aggregated_v<abi_t<T, N>>)
+      {
+        auto[l,h] = v.slice();
+        const auto r = sum[splat2](l + h);
+        return eve::combine(r,r);
+      }
+      else return butterfly_reduction(v, eve::add);
+    }
+    else
+    {
+      return sum[splat2](if_else(opts[condition_key], v, zero(as(v))));
+    }
+  }
+
   template<callable_options O, arithmetic_value T>
   EVE_FORCEINLINE element_type_t<T> sum_(EVE_REQUIRES(cpu_), O const& opts, T v) noexcept
     requires (!O::contains(splat2))
@@ -32,8 +57,8 @@ namespace eve::detail
         using N   = typename T::cardinal_type;
         using r_t = element_type_t<T>;
 
-        if      constexpr (N::value == 1) return v.get(0);
-        else if constexpr (is_emulated_v<abi_t<r_t, N>>)
+          if constexpr( N::value == 1 )         return v.get(0);
+        else  if constexpr( is_emulated_v<abi_t<r_t, N>> )
         {
           r_t r = v.get(0);
 
@@ -43,10 +68,10 @@ namespace eve::detail
 
           return r;        
         }
-        else if constexpr (is_aggregated_v<abi_t<r_t, N>>)
+        else  if constexpr( is_aggregated_v<abi_t<r_t, N>> )
         {
-          const auto [ l, h ] = v.slice();
-          return sum(l + h);
+          auto[l,h] = v.slice();
+          return  sum( l+h );
         }
         else return butterfly_reduction(v, eve::add).get(0);
       }
@@ -57,30 +82,6 @@ namespace eve::detail
 
       if constexpr (scalar_value<T>) return expand_mask(cx, as(v)) ? v : zero(as(v));
       else                           return sum(if_else(cx, v, zero(as(v))));
-    }
-  }
-
-  template<callable_options O, arithmetic_scalar_value T, typename N>
-  EVE_FORCEINLINE wide<T, N> sum_(EVE_REQUIRES(cpu_), O const& opts, wide<T, N> v) noexcept
-    requires (O::contains(splat2))
-  {
-    using C = rbr::result::fetch_t<condition_key, O>;
-
-    if constexpr (std::same_as<C, ignore_none_>)
-    {
-            if constexpr (N::value == 1)                return v;
-      else  if constexpr (is_emulated_v<abi_t<T, N>>)   return wide<T, N>{eve::detail::sum(v)};
-      else  if constexpr (is_aggregated_v<abi_t<T, N>>)
-      {
-        const auto [ l, h ] = v.slice();
-        const auto r = sum[splat2](l + h);
-        return eve::combine(r, r);
-      }
-      else return butterfly_reduction(v, eve::add);
-    }
-    else
-    {
-      return sum[splat2](if_else(opts[condition_key], v, zero(as(v))));
     }
   }
 }
