c1edb6560ac6812a54d9d070624170c41b44e9c4
SadiinsoSnowfall <sadiinso@gmail.com>
2025-05-12 15:19:28 +0200

    minimized changes

--- include/eve/module/core/regular/impl/simd/arm/neon/all.hpp
+++ include/eve/module/core/regular/impl/simd/arm/neon/all.hpp
@@ -7,28 +7,31 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/arch/top_bits.hpp>
 #include <eve/concept/value.hpp>
 #include <eve/conditional.hpp>
 #include <eve/module/core/regular/bit_cast.hpp>
 #include <eve/module/core/regular/convert.hpp>
-#include <eve/module/core/constant/allbits.hpp>
 
 namespace eve::detail
 {
 
 template<callable_options O, arithmetic_scalar_value T, typename N>
 EVE_FORCEINLINE bool
-all_(EVE_REQUIRES(neon128_), O const& opts,
+all_(EVE_REQUIRES(neon128_),
+    O const           & opts,
     logical<wide<T, N>> v0) noexcept requires std::same_as<abi_t<T, N>, arm_64_>
 {
-  if constexpr (!match_option<condition_key, O, ignore_none_> || (current_api >= asimd) || ((sizeof(T) * N()) <= 4u))
-  {
-    return all.behavior(cpu_{}, opts, v0);
-  }
+  using C = rbr::result::fetch_t<condition_key, O>;
+  using u32_2 = typename wide<T, N>::template rebind<std::uint32_t, eve::fixed<2>>;
+
+  if constexpr( C::is_complete && !C::is_inverted ) return true;
+  else if constexpr( !C::is_complete ) return all.behavior(cpu_{}, opts, v0);
+  else if constexpr( eve::current_api >= eve::asimd ) return all.behavior(cpu_{}, opts, v0);
   else
   {
-    using u32_2 = typename wide<T, N>::template rebind<uint32_t, fixed<2>>;
-
+    if constexpr( sizeof(T) * N() <= 4u ) return all.behavior(cpu_{}, opts, v0);
+    auto cond = opts[condition_key];
     auto dwords = eve::bit_cast(v0.bits(), eve::as<u32_2> {});
     dwords      = vpmin_u32(dwords, dwords);
 
@@ -42,7 +45,7 @@ all_(EVE_REQUIRES(neon128_), O const& opts,
 template<callable_options O, arithmetic_scalar_value T, typename N>
 EVE_FORCEINLINE bool
 all_(EVE_REQUIRES(neon128_),
-    O const& opts,
+    O const            & opts,
     logical<wide<T, N>> v0) noexcept requires std::same_as<abi_t<T, N>, arm_128_>
 {
   using u32_4 = typename wide<T, N>::template rebind<std::uint32_t, eve::fixed<4>>;
