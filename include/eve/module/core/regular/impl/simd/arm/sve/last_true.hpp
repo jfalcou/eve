a078b2fae5b3d343a02a57f9f1983031eefdde69
SadiinsoSnowfall <sadiinso@gmail.com>
2025-05-16 09:24:42 +0200

    cleanup

--- include/eve/module/core/regular/impl/simd/arm/sve/last_true.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/last_true.hpp
@@ -19,49 +19,46 @@ namespace eve::detail
     auto cx = opts[condition_key];
 
     if constexpr (C::is_complete && !C::is_inverted) return std::nullopt;
+    else if constexpr (has_aggregated_abi_v<L>)
+    {
+      if constexpr (!C::is_complete)
+      {
+        if constexpr (relative_conditional_expr<C>) m = m && sve_true(cx, as(m));
+        else                                        m = m && expand_mask(cx, as<L>{});
+      }
+
+      auto [lo, hi] = m.slice();
+      auto hi_res   = last_true(hi);
+      auto lo_res   = last_true(lo);
+
+      if (hi_res) return *hi_res + lo.size();
+      else        return lo_res;
+    }
     else
     {
-      if constexpr (has_aggregated_abi_v<L>)
+      L c_m = m;
+
+      constexpr auto has_inactive_lanes = N::value < fundamental_cardinal_v<T>;
+
+      // Compute the condition mask only if necessary, this gives slightly better codegen.
+      // This also masks the inactive lanes of the input.
+      if constexpr (!C::is_complete || has_inactive_lanes)
       {
-        if constexpr (!C::is_complete)
-        {
-          if constexpr (relative_conditional_expr<C>) m = m && sve_true(cx, as(m));
-          else                                        m = m && expand_mask(cx, as<L>{});
-        }
-
-        auto [lo, hi] = m.slice();
-        auto hi_res   = last_true(hi);
-        auto lo_res   = last_true(lo);
-
-        if (hi_res) return *hi_res + lo.size();
-        else        return lo_res;
+        if      constexpr (relative_conditional_expr<C>) c_m = sve_true(cx, as(m));
+        // merge everything right away in this case to eliminate one and instruction
+        else if constexpr (has_inactive_lanes)           c_m = m && remove_garbage(expand_mask(cx, as<L>{}));
+        else                                             c_m = expand_mask(cx, as<L>{});
       }
-      else
-      {
-        L c_m = m;
 
-        const auto is_ec = std::same_as<N, expected_cardinal_t<T>>;
+      // Merging the two masks after the ptest makes this branch appear earlier in the resulting assembly.
+      if (!svptest_any(c_m, m)) return std::nullopt;
 
-        // Compute the condition mask only if necessary, this gives slightly better codegen.
-        // This also masks the inactive lanes of the input.
-        if constexpr (!C::is_complete || !is_ec)
-        {
-          if      constexpr (relative_conditional_expr<C>) c_m = sve_true(cx, as(m));
-          else if constexpr (is_ec)                        c_m = expand_mask(cx, as<L>{});
-          // merge everything right away in this case to eliminate one and instruction
-          else                                             c_m = m && remove_garbage(expand_mask(cx, as<L>{}));
-        }
+      // unconditional mask merge because we need to mask the inactive lanes of there are any.
+      // no-op in the has_inactive_lanes case.
+      m = m && c_m;
 
-        // Merging the two masks after the ptest makes this branch appear earlier in the resulting assembly.
-        if (!svptest_any(c_m, m)) return std::nullopt;
-
-        // unconditional mask merge because we need to mask the inactive lanes of there are any.
-        // no-op in the !is_ec case.
-        m = m && c_m;
-
-        using it = make_integer_t<sizeof(T), unsigned>;
-        return svmaxv(m, eve::iota(as<wide<it, N>>{}));
-      }
+      using it = make_integer_t<sizeof(T), unsigned>;
+      return svmaxv(m, eve::iota(as<wide<it, N>>{}));
     }
   }
 
