9da4b75ec6fdb7b01b0710ae9ef41fa4531da5b8
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2025-05-22 19:45:46 +0200

    Fixed possible UB in SVE count_true and first_true (#2101)

--- include/eve/module/core/regular/impl/simd/arm/sve/first_true.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/first_true.hpp
@@ -20,44 +20,53 @@ namespace eve::detail
     auto cx = opts[condition_key];
 
     if constexpr (C::is_complete && !C::is_inverted) return std::nullopt;
+    else if constexpr (has_aggregated_abi_v<L>)
+    {
+      if constexpr (!C::is_complete)
+      {
+        m = m && expand_mask(cx, as<L>{});
+      }
+
+      auto [lo, hi] = m.slice();
+      auto lo_res   = first_true(lo);
+      auto hi_res   = first_true(hi);
+
+      if (lo_res) return lo_res;
+      if (hi_res) *hi_res += lo.size();
+
+      return hi_res;
+    }
     else
     {
       L c_m = m;
 
+      constexpr auto has_inactive_lanes = N::value < fundamental_cardinal_v<T>;
+
       // Compute the condition mask only if necessary, this gives slightly better codegen.
       // This also masks the inactive lanes of the input.
-      if constexpr (!C::is_complete || !std::same_as<N, expected_cardinal_t<T>>)
+      if constexpr (!C::is_complete || has_inactive_lanes)
       {
-        if constexpr (relative_conditional_expr<C>) c_m = sve_true(cx, as(m));
-        else                                        c_m = expand_mask(cx, as<L>{});
+        if      constexpr (relative_conditional_expr<C>) c_m = sve_true(cx, as(m));
+        // merging everything right away is faster in this case
+        else if constexpr (has_inactive_lanes)           c_m = m && remove_garbage(expand_mask(cx, as<L>{}));
+        else                                             c_m = expand_mask(cx, as<L>{});
       }
 
-      if constexpr (has_aggregated_abi_v<L>)
+      if (!svptest_any(c_m, m)) return std::nullopt;
+      
+      // svbrkb_ need the first lanes to be active for it to work with count_true. In the case of keep_first / ignore_last
+      // we can use c_m as the predicate directly but in the other cases we need to provide a fully-active predicate.
+      if constexpr (!std::same_as<C, keep_first> && !std::same_as<C, ignore_last>)
       {
-        if constexpr (!C::is_complete) m = m && c_m;
-
-        auto [lo, hi] = m.slice();
-        auto lo_res   = first_true(lo);
-        auto hi_res   = first_true(hi);
-        
-        if (lo_res) return lo_res;
-        if (hi_res) *hi_res += lo.size();
-
-        return hi_res;
-      }
-      else
-      {
-        // Merging the two masks after the ptest makes this branch appear earlier in the resulting assembly.
-        if (!svptest_any(c_m, m)) return std::nullopt;
-
         // Unconditional mask merge because we need to mask the inactive lanes of there are any.
+        // no-op in the has_inactive_lanes case.
         m = m && c_m;
-
-        // Implicit cast to the underlying vector size required.
-        as_wide_t<element_type_t<L>> first_true_mask = svbrkb_z(sve_true<element_type_t<L>>(), m);
-
-        return count_true(first_true_mask);
+        c_m = sve_true<element_type_t<L>>();
       }
+      
+      L first_true_mask = svbrkb_z(c_m, m);
+
+      return count_true(first_true_mask);
     }
   }
 
@@ -67,4 +76,3 @@ namespace eve::detail
     return first_true.behavior(current_api, opts, to_logical(m));
   }
 }
-
