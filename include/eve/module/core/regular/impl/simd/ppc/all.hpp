62e72b2d570e9868acc72df46b385c939c9f0ea4
SadiinsoSnowfall <sadiinso@gmail.com>
2025-05-23 16:48:43 +0200

    better neon & ppc impl

--- include/eve/module/core/regular/impl/simd/ppc/all.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/all.hpp
@@ -11,39 +11,39 @@
 #include <eve/detail/implementation.hpp>
 #include <eve/module/core/constant/true.hpp>
 #include <eve/module/core/regular/simd_cast.hpp>
+#include <eve/detail/remove_garbage.hpp>
 
 namespace eve::detail
 {
-template<callable_options O, arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE bool
-all_(EVE_REQUIRES(vmx_), O const& opts, logical<wide<T, N>> const& v0) noexcept requires ppc_abi<abi_t<T, N>>
-{
-  if constexpr (N::value == 1)
+  template<callable_options O, arithmetic_scalar_value T, typename N>
+  EVE_FORCEINLINE bool all_(EVE_REQUIRES(vmx_), O const& opts, logical<wide<T, N>> v) noexcept
+    requires ppc_abi<abi_t<T, N>>
   {
-    const auto m = v0.bits();
+    const auto m = v.bits();
 
-    if constexpr (match_option<condition_key, O, ignore_none_>)
+    if constexpr (N::value == 1)
     {
-      return static_cast<bool>(m.get(0));
+      if constexpr (match_option<condition_key, O, ignore_none_>)
+      {
+        return static_cast<bool>(m.get(0));
+      }
+      else
+      {
+        const auto cm = expand_mask(opts[condition_key], as<wide<T, N>>{});
+        return static_cast<bool>(v.get(0)) || !static_cast<bool>(cm.get(0));
+      }
     }
     else
     {
-      const auto cm = expand_mask(opts[condition_key], as<wide<T, N>>{});
-      return static_cast<bool>(v0.get(0)) || !static_cast<bool>(cm.get(0));
+      if constexpr (match_option<condition_key, O, ignore_none_>)
+      {
+        return vec_all_eq(remove_garbage(m).storage(), true_(as(m)).storage());
+      }
+      else
+      {
+        const auto mask = expand_mask_no_garbage(opts[condition_key], as(v)).bits();
+        return vec_all_eq((m & mask).storage(), mask.storage());
+      }
     }
   }
-  else
-  {
-    const auto m = simd_cast(v0, as<logical<wide<T>>>{});
-    auto mask = simd_cast(expand_mask(opts[condition_key], as<wide<T, N>>{}), as<logical<wide<T>>>{});
-
-    if constexpr (N::value != expected_cardinal_v<T, ppc_>)
-    {
-      logical<wide<T>> mm = [](auto i, auto) { return i < N::value; };
-      mask = mask && mm;
-    }
-
-    return vec_all_eq((m && mask).bits().storage(), mask.bits().storage());
-  }
-}
 }
