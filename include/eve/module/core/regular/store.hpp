23cb573ad2bc7bab14729db6c0d3bdc64ce86043
SadiinsoSnowfall <sadiinso@gmail.com>
2025-06-06 08:56:14 +0200

    stash

--- include/eve/module/core/regular/store.hpp
+++ include/eve/module/core/regular/store.hpp
@@ -12,65 +12,49 @@
 
 namespace eve
 {
-// DOC TODO
-//================================================================================================
-//! @addtogroup memory
-//! @{
-//! @var store
-//!
-//! @brief Callable object computing   //!  description NOT FOUND.
-//!
-//! **Required header:** `#include <eve/module/core.hpp>`
-//!
-//! #### Members Functions
-//!
-//! | Member       | Effect                                                     |
-//! |:-------------|:-----------------------------------------------------------|
-//! | `operator()` |   //!  description NOT FOUND   |
-//! | `operator[]` | Construct a conditional version of current function object |
-//!
-//! ---
-//!
-//!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-//!  template<value T, typename Dest>
-//!  void operator()(T const& value, Dest* ptr) const noexcept
-//!                                             requires( std::is_same_v<Dest, element_type_t<T>);
-//!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//!
-//! **Parameters**
-//!
-//!  parameters NOT FOUND
-//!
-//! **Return value**
-//!
-//!  return values NOT FOUND
-//!
-//! ---
-//!
-//!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
-//!  auto operator[]( conditional_expression auto cond ) const noexcept;
-//!  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//!
-//!  Higher-order function generating a masked version of eve::store
-//!
-//!  **Parameters**
-//!
-//!  `cond` : conditional expression
-//!
-//!  **Return value**
-//!
-//!  A Callable object so that the expression `store[cond](x, ...)` is equivalent to
-//!  `if_else(cond,store(x, ...),x)`
-//!
-//! ---
-//!
-//! #### Supported decorators
-//!
-//!  no decorators are supported
-//!
-//!  @}
-//================================================================================================
-EVE_MAKE_CALLABLE(store_, store);
+  template<typename Options>
+  struct store_t : relative_conditional_callable<store_t, Options>
+  {
+    template<simd_value T, simd_compatible_ptr<T> Ptr>
+    EVE_FORCEINLINE void operator()(T value, Ptr ptr) const noexcept
+    {
+      return EVE_DISPATCH_CALL(value, ptr);
+    }
+
+    EVE_CALLABLE_OBJECT(store_t, store_);
+  };
+
+  //================================================================================================
+  //! @addtogroup memory
+  //! @{
+  //!   @var store
+  //!   @brief Store the elements of a [SIMD value](@ref eve::simd_value) into the given memory
+  //!          location.
+  //!
+  //!   @groupheader{Header file}
+  //!
+  //!   @code
+  //!   #include <eve/module/core.hpp>
+  //!   @endcode
+  //!
+  //!   @groupheader{Callable Signatures}
+  //!
+  //!   @code
+  //!   namespace eve
+  //!   {
+  //!
+  //!   }
+  //!   @endcode
+  //!
+  //!   **Parameters**
+  //!
+  //!   **Return value**
+  //!
+  //================================================================================================
+  inline constexpr auto store = functor<store_t>;
+  //================================================================================================
+  //! @}
+  //================================================================================================
 }
 
 #include <eve/module/core/regular/impl/store.hpp>
@@ -79,18 +63,18 @@ EVE_MAKE_CALLABLE(store_, store);
 #  include <eve/module/core/regular/impl/simd/x86/store.hpp>
 #endif
 
-#if defined(EVE_INCLUDE_POWERPC_HEADER)
-#  include <eve/module/core/regular/impl/simd/ppc/store.hpp>
-#endif
+// #if defined(EVE_INCLUDE_POWERPC_HEADER)
+// #  include <eve/module/core/regular/impl/simd/ppc/store.hpp>
+// #endif
 
-#if defined(EVE_INCLUDE_ARM_NEON_HEADER)
-#  include <eve/module/core/regular/impl/simd/arm/neon/store.hpp>
-#endif
+// #if defined(EVE_INCLUDE_ARM_NEON_HEADER)
+// #  include <eve/module/core/regular/impl/simd/arm/neon/store.hpp>
+// #endif
 
-#if defined(EVE_INCLUDE_ARM_SVE_HEADER)
-#  include <eve/module/core/regular/impl/simd/arm/sve/store.hpp>
-#endif
+// #if defined(EVE_INCLUDE_ARM_SVE_HEADER)
+// #  include <eve/module/core/regular/impl/simd/arm/sve/store.hpp>
+// #endif
 
-#if defined(EVE_INCLUDE_RISCV_HEADER)
-#  include <eve/module/core/regular/impl/simd/riscv/store.hpp>
-#endif
+// #if defined(EVE_INCLUDE_RISCV_HEADER)
+// #  include <eve/module/core/regular/impl/simd/riscv/store.hpp>
+// #endif
