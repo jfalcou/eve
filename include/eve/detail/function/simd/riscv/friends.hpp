011cde7edb06a213fc6fb3d8b1fd6c29e80f912d
Joel Falcou <joel.falcou@lri.fr>
2025-06-08 09:45:19 +0200

    Make is_equal a callable

--- include/eve/detail/function/simd/riscv/friends.hpp
+++ include/eve/detail/function/simd/riscv/friends.hpp
@@ -9,40 +9,6 @@
 
 namespace eve::detail
 {
-
-// *_impl in separate functions, as otherwise compiler can not
-// choose overload between riscv-specific and common one
-
-template<plain_scalar_value T, typename N, value U>
-EVE_FORCEINLINE auto
-self_eq_impl(wide<T, N> lhs, U rhs) noexcept -> logical<wide<T, N>>
-requires rvv_abi<abi_t<T, N>> && (std::same_as<wide<T, N>, U> || scalar_value<U>)
-{
-  if constexpr( scalar_value<U> && !std::same_as<T, U> ) return self_eq(lhs, static_cast<T>(rhs));
-  else
-  {
-    constexpr auto c = categorize<wide<T, N>>();
-    if constexpr( match(c, category::integer_) ) return __riscv_vmseq(lhs, rhs, N::value);
-    else if constexpr( match(c, category::float_) ) return __riscv_vmfeq(lhs, rhs, N::value);
-  }
-}
-
-template<plain_scalar_value T, typename N>
-EVE_FORCEINLINE auto
-self_eq(wide<T, N> lhs, wide<T, N> rhs) noexcept -> logical<wide<T, N>>
-requires rvv_abi<abi_t<T, N>>
-{
-  return self_eq_impl(lhs, rhs);
-}
-
-template<plain_scalar_value T, typename N>
-EVE_FORCEINLINE auto
-self_eq(wide<T, N> lhs, std::convertible_to<T> auto rhs) noexcept -> logical<wide<T, N>>
-requires rvv_abi<abi_t<T, N>>
-{
-  return self_eq_impl(lhs, rhs);
-}
-
 template<plain_scalar_value T, typename N, value U>
 EVE_FORCEINLINE auto
 self_neq_impl(wide<T, N> lhs, U rhs) noexcept -> logical<wide<T, N>>
