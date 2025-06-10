b6e8978b41e429f111304ee1f3ec49c7c2e56378
Joel Falcou <joel.falcou@lri.fr>
2025-06-10 17:04:56 +0200

    Upgrade to g++ 14 to remove senseless warnings

--- cmake/toolchain/gcc.x86.cmake
+++ cmake/toolchain/gcc.x86.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
-set(CMAKE_C_COMPILER    gcc  )
-set(CMAKE_CXX_COMPILER  g++  )
+set(CMAKE_C_COMPILER    gcc-14  )
+set(CMAKE_CXX_COMPILER  g++-14  )
 
 set(CMAKE_CXX_FLAGS     "-Wno-psabi -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
