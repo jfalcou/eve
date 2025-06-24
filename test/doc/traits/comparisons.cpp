365dc619e9a390d65a6f73b190a6eed125d31509
Joel Falcou <joel.falcou@lri.fr>
2025-06-24 17:25:17 +0200

    More adaptations

--- test/doc/traits/comparisons.cpp
+++ test/doc/traits/comparisons.cpp
@@ -3,30 +3,26 @@
 
 namespace udt
 {
-  struct point : kumi::tuple<int, float> {};
+  struct point : eve::struct_support<point, int, float> {};
 }
-
-template<>  struct std::tuple_size<udt::point>       : std::integral_constant<std::size_t, 2> {};
-template<>  struct std::tuple_element<0,udt::point>  { using type = int; };
-template<>  struct std::tuple_element<1,udt::point>  { using type = float; };
 
 // Specialize eve::comparisons so that only position is used for equality
 template<> struct eve::comparisons<udt::point>
 {
-  static constexpr auto equal(auto const& o, auto const& a, auto const& b) noexcept
+  static constexpr auto equal(auto const& a, auto const& b) noexcept
   {
-    return eve::is_equal[o](get<0>(a),get<0>(b));
+    return eve::is_equal(get<0>(a),get<0>(b));
   }
 
-  static constexpr auto not_equal(auto const& o, auto const& a, auto const& b) noexcept
+  static constexpr auto not_equal(auto const& a, auto const& b) noexcept
   {
-    return !equal(o, a, b);
+    return !equal(a, b);
   }
 };
 
 int main()
 {
-  eve::wide<float> a{1,2,3,4,5,6,7,8}, b{2,2,4,4,6,6,7,7};
+  eve::wide<float> a{1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9}, b{0.2,0.2,0.4,0.4,0.6,0.6,0.7,0.7};
   eve::wide<int>   i{1,2,3,4,5,6,7,8};
 
   eve::wide<udt::point> p1{i,a}, p2{i,b};
