124774809d4c6795635bb4a9533df9adc8ba6361
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2025-06-27 15:06:34 +0200

    Allow masked iterator load
    * masked load for iterators + aligned_ptr fix
    * fixed unspecified behaviour related to function parameter evaluation order

--- include/eve/detail/function/simd/common/load.hpp
+++ include/eve/detail/function/simd/common/load.hpp
@@ -35,11 +35,48 @@ namespace eve::detail
   {
     auto impl = [&](auto... I)
     {
-      auto deref = [&](auto p, auto const &i) { std::advance(p, i); return *p; };
-      return Wide(deref(ptr, static_cast<std::ptrdiff_t>(I))...);
+      std::array<element_type_t<Wide>, Wide::size()> values;
+
+      values[0] = *ptr;
+      for(std::size_t i = 1; i < Wide::size(); ++i)
+      {
+        values[i] = *(ptr = std::next(ptr));
+      }
+
+      return Wide(values[I]...);
     };
 
     return apply<Wide::size()>(impl);
+  }
+
+  template<relative_conditional_expr C, typename Wide, typename Iterator>
+  EVE_FORCEINLINE auto piecewise_load_cx(C const& cx, Iterator ptr, as<Wide> tgt) noexcept
+  {
+    if constexpr (C::is_complete)
+    {
+      if constexpr (C::is_inverted) return piecewise_load(ptr, tgt);
+      else                          return detail::alternative(cx, Wide {}, tgt);
+    }
+    else
+    {
+      std::ptrdiff_t begin = cx.offset(tgt);
+      std::ptrdiff_t end = begin + cx.count(tgt);
+
+      Wide res = detail::alternative(cx, Wide { }, tgt);
+
+      if (begin != end)
+      {
+        std::advance(ptr, begin);
+        res.set(begin, *ptr);
+        for (std::ptrdiff_t i = begin + 1; i < end; ++i)
+        {
+          ++ptr;
+          res.set(i, *ptr);
+        }
+      }
+
+      return res;
+    }
   }
 
   template<typename Wide, typename Pointer>
@@ -92,7 +129,13 @@ namespace eve::detail
     using e_t = typename pointer_traits<Wide>::value_type;
     using c_t = cardinal_t<Wide>;
 
-    if constexpr (!std::is_pointer_v<DS>)
+    // If the ignore/keep is complete we can jump over if_else
+    if constexpr (C::is_complete)
+    {
+      if constexpr (C::has_alternative) return Wide {cx.alternative};
+      else                              return Wide {};
+    }
+    else if constexpr (instance_of<DS, aligned_ptr>)
     {
       constexpr bool is_aligned_enough = c_t() * sizeof(e_t) >= DS::alignment();
 
@@ -108,40 +151,31 @@ namespace eve::detail
         return eve::load[cx](src.get(), tgt);
       }
     }
+    else if constexpr (logical_simd_value<Wide> && !Wide::abi_type::is_wide_logical)
+    {
+      using W = as_arithmetic_t<Wide>;
+      using T = element_type_t<W>;
+
+      auto const mcx = map_alternative(cx, [](auto alt) { return alt.mask(); });
+      return to_logical(load[mcx](ptr_cast<T const>(src), as<W>{}));
+    }
     else
     {
-      // If the ignore/keep is complete we can jump over if_else
-      if constexpr (C::is_complete)
-      {
-        if constexpr (C::has_alternative) return Wide {cx.alternative};
-        else                              return Wide {};
-      }
-      else if constexpr (logical_simd_value<Wide> && !Wide::abi_type::is_wide_logical)
-      {
-        using W = as_arithmetic_t<Wide>;
-        using T = element_type_t<W>;
+      auto offset = cx.offset(tgt);
 
-        auto const mcx = map_alternative(cx, [](auto alt) { return alt.mask(); });
-        return to_logical(load[mcx](ptr_cast<T const>(src), as<W>{}));
+      if constexpr (C::has_alternative)
+      {
+        [[maybe_unused]] Wide that(cx.alternative);
+        auto                *dst = (e_t *)(&that.storage());
+        std::memcpy((void *)(dst + offset), src + offset, sizeof(e_t) * cx.count(tgt));
+        return that;
       }
       else
       {
-        auto offset = cx.offset(tgt);
-
-        if constexpr (C::has_alternative)
-        {
-          [[maybe_unused]] Wide that(cx.alternative);
-          auto                *dst = (e_t *)(&that.storage());
-          std::memcpy((void *)(dst + offset), src + offset, sizeof(e_t) * cx.count(tgt));
-          return that;
-        }
-        else
-        {
-          [[maybe_unused]] Wide that = {};
-          auto                *dst = (e_t *)(&that.storage());
-          std::memcpy((void *)(dst + offset), src + offset, sizeof(e_t) * cx.count(tgt));
-          return that;
-        }
+        [[maybe_unused]] Wide that = {};
+        auto                *dst = (e_t *)(&that.storage());
+        std::memcpy((void *)(dst + offset), src + offset, sizeof(e_t) * cx.count(tgt));
+        return that;
       }
     }
   }
@@ -149,24 +183,17 @@ namespace eve::detail
   template<callable_options O, std::input_iterator It, typename Wide>
   EVE_FORCEINLINE Wide load_iterator_(O const& opts, It src, as<Wide> tgt) noexcept
   {
-    using C = rbr::result::fetch_t<condition_key, O>;
-    [[maybe_unused]] auto cx = opts[condition_key];
-
     if constexpr (std::contiguous_iterator<It>)
     {
       return load.behavior(cpu_{}, opts, std::to_address(src), tgt);
     }
-    else if constexpr (!std::same_as<C, ignore_none_>)
-    {
-      return load_cx_(cx, src, tgt);
-    }
     else if constexpr (logical_simd_value<Wide> && !Wide::abi_type::is_wide_logical)
     {
-      return to_logical(piecewise_load(src, as<as_arithmetic_t<Wide>>{}));
+      return to_logical(piecewise_load_cx(opts[condition_key], src, as<as_arithmetic_t<Wide>>{}));
     }
     else
     {
-      return piecewise_load(src, tgt);
+      return piecewise_load_cx(opts[condition_key], src, tgt);
     }
   }
 
@@ -211,6 +238,29 @@ namespace eve::detail
   }
 
   template<callable_options O, detail::data_source DS, typename Wide>
+  SPY_DISABLE_SANITIZERS Wide load_unsafe_(O const& opts, DS src, as<Wide> tgt) noexcept
+  {
+    if constexpr (spy::supports::sanitizers_status)
+    {
+      auto cx = opts[condition_key];
+
+      auto res = detail::alternative(cx, Wide {}, tgt);
+
+      auto offset = cx.offset(tgt);
+      auto count  = cx.count(tgt);
+
+      for (std::ptrdiff_t i = offset; i != count + offset; ++i)
+        res.set(i, src[i]);
+
+      return res;
+    }
+    else
+    {
+      return eve::load[opts.drop(unsafe2)](src, tgt);
+    }
+  }
+
+  template<callable_options O, detail::data_source DS, typename Wide>
   EVE_FORCEINLINE Wide load_(EVE_REQUIRES(cpu_), O const& opts, DS src, as<Wide> tgt) noexcept
   {
     using C = rbr::result::fetch_t<condition_key, O>;
@@ -220,28 +270,13 @@ namespace eve::detail
     {
       return src.load(opts, tgt);
     }
-    else if constexpr (std::input_iterator<DS> && !std::is_pointer_v<DS>)
+    else if constexpr (std::input_iterator<DS> && !detail::scalar_pointer<DS>)
     {
       return load_iterator_(opts, src, tgt);
     }
     else if constexpr (O::contains(unsafe2))
     {
-      if constexpr (spy::supports::sanitizers_status)
-      {
-        Wide res;
-
-        auto offset = cx.offset(tgt);
-        auto count  = cx.count(tgt);
-
-        for (std::ptrdiff_t i = offset; i != count + offset; ++i)
-          res.set(i, src[i]);
-
-        return res;
-      }
-      else
-      {
-        return eve::load[opts.drop(unsafe2)](src, tgt);
-      }
+      return load_unsafe_(opts, src, tgt);
     }
     else if constexpr (has_bundle_abi_v<Wide>)
     {
