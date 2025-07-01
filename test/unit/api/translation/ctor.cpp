7897033ebe122e44cd8343044c9a45cac67d6dc4
SadiinsoSnowfall <sadiinso@gmail.com>
2025-07-01 09:45:53 +0200

    added translated load tests

--- test/unit/api/translation/ctor.cpp
+++ test/unit/api/translation/ctor.cpp
@@ -37,6 +37,43 @@ TTS_CASE_TPL("Trans wide ctor - splat", eve::test::simd::all_types)
   }
 };
 
+TTS_CASE_TPL("Trans wide ctor - from pointer", eve::test::simd::all_types)
+<typename W>(tts::type<W>)
+{
+  using T = eve::element_type_t<W>;
+  using N = typename W::cardinal_type;
+  using trans_t = BaseStruct<T>;
+
+  if constexpr (std::integral<T>)
+  {
+    enum class E: T { };
+
+    std::array<E, N::value> scratch;
+    for (std::ptrdiff_t i = 0; i < N::value; ++i)
+    {
+      scratch[i] = static_cast<E>(i);
+    }
+
+    eve::wide<E, N> wt(&scratch[0]);
+    for (std::ptrdiff_t i = 0; i < N::value; ++i)
+    {
+      TTS_EQUAL(wt.get(i), static_cast<E>(i));
+    }
+  }
+
+  std::array<trans_t, N::value> scratch;
+  for (std::ptrdiff_t i = 0; i < N::value; ++i)
+  {
+    scratch[i] = static_cast<trans_t>(i);
+  }
+
+  eve::wide<trans_t, N> wt(&scratch[0]);
+  for (std::ptrdiff_t i = 0; i < N::value; ++i)
+  {
+    TTS_EQUAL(wt.get(i), static_cast<trans_t>(i));
+  }
+};
+
 TTS_CASE_TPL("Trans wide ctor - generator", eve::test::simd::all_types)
 <typename W>(tts::type<W>)
 {
@@ -105,5 +142,54 @@ TTS_CASE_TPL("Trans wide ctor - enumerating", eve::test::simd::all_types)
   for (std::ptrdiff_t i = 1; i < W::size(); ++i)
   {
     TTS_EQUAL(wt.get(i), static_cast<trans_t>(1 + i));
+  }
+};
+
+TTS_CASE_TPL("Trans wide - load", eve::test::simd::all_types)
+<typename W>(tts::type<W>)
+{
+  using T = eve::element_type_t<W>;
+  using N = typename W::cardinal_type;
+  using trans_t = BaseStruct<T>;
+  
+  if constexpr (std::integral<T>)
+  {
+    enum class E: T { };
+
+    std::array<E, N::value> scratch;
+    for (std::ptrdiff_t i = 0; i < N::value; ++i)
+    {
+      scratch[i] = static_cast<E>(i);
+    }
+
+    eve::wide<E, N> wt = eve::load(scratch.data(), N{});
+    for (std::ptrdiff_t i = 0; i < N::value; ++i)
+    {
+      TTS_EQUAL(wt.get(i), static_cast<E>(i));
+    }
+
+    wt = eve::load(scratch.data(), eve::as(wt));
+    for (std::ptrdiff_t i = 0; i < N::value; ++i)
+    {
+      TTS_EQUAL(wt.get(i), static_cast<E>(i));
+    }
+  }
+
+  std::array<trans_t, N::value> scratch;
+  for (std::ptrdiff_t i = 0; i < N::value; ++i)
+  {
+    scratch[i] = static_cast<trans_t>(i);
+  }
+
+  eve::wide<trans_t, N> wt = eve::load(scratch.data(), N{});
+  for (std::ptrdiff_t i = 0; i < N::value; ++i)
+  {
+    TTS_EQUAL(wt.get(i), static_cast<trans_t>(i));
+  }
+
+  wt = eve::load(scratch.data(), eve::as(wt));
+  for (std::ptrdiff_t i = 0; i < N::value; ++i)
+  {
+    TTS_EQUAL(wt.get(i), static_cast<trans_t>(i));
   }
 };
