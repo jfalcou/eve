6b18c758b9f314c66fc3e6e3bd07354978173efd
SadiinsoSnowfall <sadiinso@gmail.com>
2025-07-15 16:27:43 +0200

    Made maximum a new format callable

--- test/unit/module/core/maximum.cpp
+++ test/unit/module/core/maximum.cpp
@@ -18,9 +18,7 @@ TTS_CASE_TPL("Check return types of eve::maximum(wide)", eve::test::simd::all_ty
   using v_t = eve::element_type_t<T>;
 
   TTS_EXPR_IS(eve::maximum(T()), v_t);
-  TTS_EXPR_IS(eve::maximum(eve::logical<T>()), bool);
-  TTS_EXPR_IS(eve::splat(eve::maximum)(T()), T);
-  TTS_EXPR_IS(eve::splat(eve::maximum)(eve::logical<T>()), eve::logical<T>);
+  TTS_EXPR_IS(eve::maximum[eve::splat](T()), T);
 };
 
 //==================================================================================================
@@ -28,8 +26,8 @@ TTS_CASE_TPL("Check return types of eve::maximum(wide)", eve::test::simd::all_ty
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of eve::maximum(eve::wide)",
               eve::test::simd::all_types,
-              tts::generate(tts::randoms(eve::valmin, eve::valmax), tts::logicals(0, 3)))
-<typename T, typename L>(T const& a0, L const& l0)
+              tts::generate(tts::randoms(eve::valmin, eve::valmax)))
+<typename T>(T const& a0)
 {
   using v_t = eve::element_type_t<T>;
 
@@ -37,12 +35,5 @@ TTS_CASE_WITH("Check behavior of eve::maximum(eve::wide)",
   for( int i = 1; i != T::size(); ++i ) { max_value = std::max(max_value, a0.get(i)); }
 
   TTS_EQUAL(eve::maximum(a0), max_value);
-  TTS_EQUAL(eve::splat(eve::maximum)(a0), T(max_value));
-
-  TTS_EQUAL(eve::maximum(l0), true);
-  TTS_EQUAL(eve::splat(eve::maximum)(l0), L(true));
-
-  TTS_EQUAL(eve::maximum[l0](a0), eve::maximum(eve::if_else(l0, a0, eve::valmin(eve::as(a0)))));
-  TTS_EQUAL(eve::splat(eve::maximum[l0])(a0),
-            T(eve::maximum(eve::if_else(l0, a0, eve::valmin(eve::as(a0))))));
+  TTS_EQUAL(eve::maximum[eve::splat](a0), T(max_value));
 };
