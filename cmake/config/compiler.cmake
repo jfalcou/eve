4409cacdafbd5a0dc4b7f0050d82b8e84b4f8da9
SadiinsoSnowfall <sadiinso@gmail.com>
2025-07-17 11:28:12 +0200

    clang fix

--- cmake/config/compiler.cmake
+++ cmake/config/compiler.cmake
@@ -13,7 +13,7 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
   target_compile_options( eve_test INTERFACE /bigobj /W3 /EHsc /std:c++20 /wd4267 /wd4244 /wd4146)
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   target_compile_options( eve_test INTERFACE -std=c++20 -Werror -Wshadow -Wall -Wpedantic -Wextra -fcolor-diagnostics
-                          -ftemplate-backtrace-limit=0 -fconcepts-diagnostics-depth=8
+                          -ftemplate-backtrace-limit=0
                         )
 else()
   target_compile_options( eve_test INTERFACE -std=c++20 -Werror -Wshadow -Wall -Wpedantic -Wextra -fdiagnostics-color=always
