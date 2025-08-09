6facb881f0d6f8dc87b11ccf47bedb9838d3d919
jtlap <jtlapreste@gmail.com>
2025-08-09 09:27:12 +0200

    epsilon

--- include/eve/module/core/regular/epsilon.hpp
+++ include/eve/module/core/regular/epsilon.hpp
@@ -15,11 +15,13 @@
 #include <eve/module/core/regular/if_else.hpp>
 #include <eve/module/core/regular/is_nan.hpp>
 #include <eve/module/core/regular/next.hpp>
+#include <eve/module/core/regular/sulp.hpp>
 
 namespace eve
 {
   template<typename Options>
-  struct epsilon_t : elementwise_callable<epsilon_t, Options, upward_option, downward_option>
+  struct epsilon_t : elementwise_callable<epsilon_t, Options, upward_option, downward_option
+                                          , kahan_option, harrisson_option>
   {
     template<eve::value T>
     constexpr EVE_FORCEINLINE T operator()(T a) const noexcept
@@ -54,8 +56,8 @@ namespace eve
 //!      constexpr auto epsilon[logical_value auto m](value auto x)    noexcept; // 2
 //!
 //!      // Semantic options
-//!      constexpr auto abs[downward](value auto x)                    noexcept; // 1
-//!      constexpr auto abs[upward](value auto x)                      noexcept; // 3
+//!      constexpr auto abs[downward](value auto x)                    noexcept; // 3
+//!      constexpr auto abs[upward](value auto x)                      noexcept; // 1
 //!   }
 //!   @endcode
 //!
@@ -67,9 +69,12 @@ namespace eve
 //!
 //!   **Return value**
 //!
-//!      1. The distance of abs(x) to the next representable element in the type of `x`. (Kahan-Harrisson definition)
+//!      1. The distance of abs(x) to the next representable element in the type of `x`. (Kahan definition)
 //!      2. [The operation is performed conditionnaly](@ref conditional).
 //!      3. The distance of abs(x) to the previous representable element in the type of `x`. (Goldberg definition)
+//!
+//!    @note somewhat faster algorithms are at hand using [sulp](@ref sulp),
+//!      if you are insterested by by the signed ulp value.
 //!
 //!  @groupheader{External references}
 //!   *  [wikipedia](//!https://en.wikipedia.org/wiki/Unit_in_the_last_place)
@@ -86,17 +91,22 @@ namespace eve
   namespace detail
   {
     template<typename T, callable_options O>
-    EVE_FORCEINLINE constexpr auto epsilon_(EVE_REQUIRES(cpu_), O const&, T const& a0)
+    EVE_FORCEINLINE constexpr auto epsilon_(EVE_REQUIRES(cpu_), O const& o, T const& a0)
     {
       if constexpr(integral_value<T>)
         return T(1);
       else
       {
         auto aa = eve::abs(a0);
-        if constexpr(O::contains(downward))
-          return dist[is_finite(aa)](aa, prev(aa));
+        auto finite = is_finite(aa);
+        if constexpr(O::contains(kahan) || O::contains(harrisson))
+        {
+          return eve::sulp[finite][o](aa);
+        }
+        else if constexpr(O::contains(downward))
+          return dist[finite](aa, prev(aa));
         else // upward is the default
-          return dist[is_finite(aa)](aa, next(aa));
+          return dist[finite](aa, next(aa));
       }
     }
   }
