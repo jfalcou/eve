377a48c207075b8e7b385ed7cbf6879252295a77
jtlap <jtlapreste@gmail.com>
2025-08-10 18:03:32 +0200

    dekker suppression

--- include/eve/module/core/regular/core.hpp
+++ include/eve/module/core/regular/core.hpp
@@ -58,7 +58,6 @@
 #include <eve/module/core/regular/dec.hpp>
 #include <eve/module/core/regular/deinterleave_groups.hpp>
 #include <eve/module/core/regular/deinterleave_groups_shuffle.hpp>
-#include <eve/module/core/regular/dekker_prod.hpp>
 #include <eve/module/core/regular/diff_of_prod.hpp>
 #include <eve/module/core/regular/dist.hpp>
 #include <eve/module/core/regular/div.hpp>
