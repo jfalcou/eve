91fc2602cd079e635cd70bd882c8fbe60ebb7d4b
Sadiinso <32017313+SadiinsoSnowfall@users.noreply.github.com>
2025-08-18 22:11:26 +0200

    Made maximum a new format callable

--- include/eve/module/core/regular/impl/maximum.hpp
+++ include/eve/module/core/regular/impl/maximum.hpp
@@ -8,79 +8,62 @@
 #pragma once
 
 #include <eve/concept/value.hpp>
-#include <eve/detail/function/reduce.hpp>
 #include <eve/detail/implementation.hpp>
 #include <eve/module/core/constant/valmin.hpp>
-#include <eve/module/core/regular/any.hpp>
 #include <eve/module/core/regular/if_else.hpp>
 #include <eve/module/core/regular/max.hpp>
-#include <eve/module/core/regular/splat.hpp>
 
 namespace eve::detail
 {
-template<arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), splat_type const&, wide<T, N> const& v) noexcept
-{
-  if constexpr( N::value == 1 ) return v;
-  else if constexpr( !is_aggregated_v<abi_t<T, N>> ) return butterfly_reduction(v, eve::max);
-  else
+  template<callable_options O, arithmetic_scalar_value T, typename N>
+  EVE_FORCEINLINE wide<T, N> maximum_(EVE_REQUIRES(cpu_), O const& opts, wide<T, N> v) noexcept
+    requires (O::contains(splat2))
   {
-    auto [l, h] = v.slice();
-    auto r      = splat(maximum)(eve::max(l, h));
-    return eve::combine(r, r);
+    if constexpr (match_option<condition_key, O, ignore_none_>)
+    {
+      if constexpr( N::value == 1 ) return v;
+      else if constexpr( !is_aggregated_v<abi_t<T, N>> ) return butterfly_reduction(v, eve::max);
+      else
+      {
+        auto [l, h] = v.slice();
+        auto r      = maximum[splat](eve::max(l, h));
+        return eve::combine(r, r);
+      }
+    }
+    else
+    {
+      return maximum[splat](if_else(opts[condition_key], v, eve::minorant));
+    }
   }
-}
 
-template<simd_value T>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), splat_type const&, logical<T> const& v) noexcept
-{
-  return logical<T>(eve::any(v));
-}
-
-template<value T>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), T const& v) noexcept
-requires(scalar_value<T>)
-{
-  return v;
-}
-
-template<arithmetic_scalar_value T, typename N>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), wide<T, N> const& v) noexcept
-{
-  if constexpr( N::value == 1 ) return v.get(0);
-  else if constexpr( !is_aggregated_v<abi_t<T, N>> ) return butterfly_reduction(v, eve::max).get(0);
-  else
+  template<callable_options O, arithmetic_value T>
+  EVE_FORCEINLINE element_type_t<T> maximum_(EVE_REQUIRES(cpu_), O const& opts, T v) noexcept
+    requires (!O::contains(splat2))
   {
-    auto [l, h] = v.slice();
-    return maximum(eve::max(l, h));
+    if constexpr (match_option<condition_key, O, ignore_none_>)
+    {
+      if constexpr (scalar_value<T>) return v;
+      else
+      {
+        using N   = typename T::cardinal_type;
+        using r_t = element_type_t<T>;
+
+        if constexpr( N::value == 1 ) return v.get(0);
+        else if constexpr( !is_aggregated_v<abi_t<r_t, N>> ) return butterfly_reduction(v, eve::max).get(0);
+        else
+        {
+          auto [l, h] = v.slice();
+          return maximum(eve::max(l, h));
+        }
+      }
+    }
+    else if constexpr (scalar_value<T>)
+    {
+      return if_else(opts[condition_key], v, eve::minorant);
+    }
+    else
+    {
+      return maximum(if_else(opts[condition_key], v, eve::minorant));
+    }
   }
-}
-
-template<simd_value T>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), logical<T> const& v) noexcept
-{
-  return eve::any(v);
-}
-
-// -----------------------------------------------------------------------------------------------
-// Masked case
-template<conditional_expr C, value U>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), C const& cond, U const& t) noexcept
-{
-  return maximum(if_else(cond, t, eve::valmin));
-}
-
-template<conditional_expr C, value U>
-EVE_FORCEINLINE auto
-maximum_(EVE_SUPPORTS(cpu_), C const& cond, splat_type const&, U const& t) noexcept
-{
-  return splat(maximum)(if_else(cond, t, eve::valmin));
-}
-
 }
