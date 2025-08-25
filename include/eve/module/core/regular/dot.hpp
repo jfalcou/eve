370711f68892dba746ebd426435c289610a3c5d9
jtlap <jtlapreste@gmail.com>
2025-08-25 17:53:29 +0200

    dot

--- include/eve/module/core/regular/dot.hpp
+++ include/eve/module/core/regular/dot.hpp
@@ -13,12 +13,33 @@
 namespace eve
 {
   template<typename Options>
-  struct dot_t : elementwise_callable<dot_t, Options>
+  struct dot_t : tuple_callable<dot_t, Options>
   {
     template<eve::value T,  value U>
     requires(eve::same_lanes_or_scalar<T, U>)
     constexpr EVE_FORCEINLINE common_value_t<T, U> operator()(T a, U b) const
     { return EVE_DISPATCH_CALL(a, b); }
+
+     template<value T0, eve::value T1, value... Ts>
+    requires(eve::same_lanes_or_scalar<T0, T1, Ts...> && !Options::contains(widen))
+    EVE_FORCEINLINE constexpr common_value_t<T0,T1, Ts...> operator()(T0 t0, T1 t1, Ts...ts) const noexcept
+    {
+      return EVE_DISPATCH_CALL(t0, t1, ts...);
+    }
+
+    template<value T0, eve::value T1, value... Ts>
+    requires(eve::same_lanes_or_scalar<T0, T1, Ts...> && Options::contains(widen))
+    EVE_FORCEINLINE common_value_t<upgrade_t<T0>, upgrade_t<T1>, upgrade_t<Ts>... >
+    constexpr operator()(T0 t0, T1 t1, Ts...ts) const noexcept
+    {
+      return EVE_DISPATCH_CALL(t0, t1, ts...);
+    }
+
+    template<kumi::non_empty_product_type Tup>
+    requires(eve::same_lanes_or_scalar_tuple<Tup>)
+    EVE_FORCEINLINE constexpr
+    kumi::apply_traits_t<eve::common_value,Tup>
+    operator()(Tup const& t) const noexcept { return EVE_DISPATCH_CALL(t); }
 
     EVE_CALLABLE_OBJECT(dot_t, dot_);
   };
@@ -73,4 +94,35 @@ namespace eve
       return mul(a, b);
     }
   }
+
+    template<typename... Ts, callable_options O>
+    EVE_FORCEINLINE constexpr auto
+    sum_of_squares_(EVE_REQUIRES(cpu_), O const & o , Ts... args) noexcept
+    requires(sizeof...(Ts) != 0  && sizeof...(Ts)%2 == 0)
+    {
+      using r_t =  eve::common_value_t<Ts...>;
+      auto coeffs = kumi::tuple{xs...};
+      auto[f,s]   = kumi::split(coeffs, kumi::index<sizeof...(T)/2>);
+      if constexpr(O::contains(kahan))
+      {
+        auto[f0,fs] = kumi::split(f, kumi::index<1>);
+        auto[s0,ss] = kumi::split(s, kumi::index<1>);
+
+        auto [sum,error] = eve::two_prod(get<0>(f0), get<0>(s0));
+
+        kumi::for_each( [&](auto a, auto b) {
+                          auto[s1, e1] = eve::two_fma_approx(a, b, sum);
+                          error += e1;
+                          sum    = s1;
+                        }, fs, ss
+                      );
+
+        return sum + error;
+      }
+      else
+      {
+        return kumi::sum( kumi::map([](auto a, auto b) { return a*b; }, f, s));
+     }
+    }
+
 }
