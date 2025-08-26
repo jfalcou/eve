b87a2dd3911527410b8376c75651ed805d0c642d
jtlap <jtlapreste@gmail.com>
2025-08-26 14:08:00 +0200

    dot for ranges + suppress some iostream

--- include/eve/module/core/regular/dot.hpp
+++ include/eve/module/core/regular/dot.hpp
@@ -13,33 +13,43 @@
 namespace eve
 {
   template<typename Options>
-  struct dot_t : tuple_callable<dot_t, Options, kahan_option>
+  struct dot_t : callable<dot_t, Options, kahan_option>
   {
-    template<eve::value T,  value U>
+    template<eve::value T,  eve::value U>
     requires(eve::same_lanes_or_scalar<T, U>)
-    constexpr EVE_FORCEINLINE common_value_t<T, U> operator()(T a, U b) const
+      constexpr EVE_FORCEINLINE common_value_t<T, U> operator()(T a, U b) const
     { return EVE_DISPATCH_CALL(a, b); }
 
-     template<value T0, eve::value T1, value... Ts>
-    requires(eve::same_lanes_or_scalar<T0, T1, Ts...> && !Options::contains(widen))
-    EVE_FORCEINLINE constexpr common_value_t<T0,T1, Ts...> operator()(T0 t0, T1 t1, Ts...ts) const noexcept
+    template<eve::value... Ts>
+    requires(sizeof...(Ts) >= 2 && eve::same_lanes_or_scalar<Ts...> && !Options::contains(widen))
+      EVE_FORCEINLINE constexpr common_value_t<Ts...> operator()(Ts...ts) const noexcept
     {
-      return EVE_DISPATCH_CALL(t0, t1, ts...);
+      return EVE_DISPATCH_CALL(ts...);
     }
 
-    template<value T0, eve::value T1, value... Ts>
-    requires(eve::same_lanes_or_scalar<T0, T1, Ts...> && Options::contains(widen))
-    EVE_FORCEINLINE common_value_t<upgrade_t<T0>, upgrade_t<T1>, upgrade_t<Ts>... >
-    constexpr operator()(T0 t0, T1 t1, Ts...ts) const noexcept
-    {
-      return EVE_DISPATCH_CALL(t0, t1, ts...);
-    }
+//     template<eve::value... Ts>
+//     requires(sizeof...(Ts) >= 2 && eve::same_lanes_or_scalar<Ts...> && Options::contains(widen))
+//       EVE_FORCEINLINE common_value_t<upgrade_t<Ts>... >
+//     constexpr operator()(Ts...ts) const noexcept
+//     {
+//       return EVE_DISPATCH_CALL(ts...);
+//     }
 
-    template<kumi::non_empty_product_type Tup>
-    requires(eve::same_lanes_or_scalar_tuple<Tup>)
+
+    template<eve::detail::range R1, eve::detail::range R2>
     EVE_FORCEINLINE constexpr
-    kumi::apply_traits_t<eve::common_value,Tup>
-    operator()(Tup const& t) const noexcept { return EVE_DISPATCH_CALL(t); }
+    eve::common_value_t<typename R1::value_type, typename R2::value_type>
+    operator()(R1 const& t1,  R2 const & t2) const noexcept
+    {
+      EVE_ASSERT( std::distance(begin(t1), end(t1)) == std::distance(begin(t2), end(t2)),
+                  "[eve::dot]:ranges sizes are not equal");
+      return EVE_DISPATCH_CALL(t1, t2);
+    }
+//     template<kumi::non_empty_product_type Tup>
+//     requires(eve::same_lanes_or_scalar_tuple<Tup>)
+//     EVE_FORCEINLINE constexpr
+//     kumi::apply_traits_t<eve::common_value,Tup>
+//     operator()(Tup const& t) const noexcept { return EVE_DISPATCH_CALL(t); }
 
     EVE_CALLABLE_OBJECT(dot_t, dot_);
   };
@@ -48,7 +58,8 @@ namespace eve
 //! @addtogroup core_arithmetic
 //! @{
 //!   @var dot
-//!   @brief `elementwise_callable` object computing elementwise the dot product of the two parameters.
+//!   @brief `elementwise_callable` object computing elementwise the dot product
+//!     of the vector of the first half parameter by thevector of the last half.
 //!
 //!   @warning This is not a reduction ! For reals the dot product is the product
 //!
@@ -65,9 +76,10 @@ namespace eve
 //!   {
 //!      // Regular overloads
 //!      constexpr auto dot(auto value... xs, auto value... ys)        noexcept; // 1
+//!      constexpr auto dot(Range r1. xs, auto value... ys, Range r2)  noexcept; // 2
 //!
 //!      // Semantic options
-//!      constexpr auto dot[kahan](/*any of the above overloads*/)     noexcept; // 2
+//!      constexpr auto dot[kahan](/*any of the above overloads*/)     noexcept; // 3
 //!   }
 //!   @endcode
 //!
@@ -79,8 +91,9 @@ namespace eve
 //!
 //!    **Return value**
 //!
-//!    dot product. \f$\sum_s x_s*y_s\f$.
-//!    With kahan option the result is more accurately computed using a compensated algorithm.
+//!    1. dot product. \f$\sum_s x_s*y_s\f$.
+//!    2. use teh content of the ranges.
+//!    3. With kahan option the result is more accurately computed using a compensated algorithm.
 //!
 //!  @groupheader{Example}
 //!  @godbolt{doc/core/dot.cpp}
@@ -92,7 +105,7 @@ namespace eve
 
   namespace detail
   {
-    template<typename T, typename U, callable_options O>
+    template<value T, value U, callable_options O>
     EVE_FORCEINLINE constexpr common_value_t<T, U>
     dot_(EVE_REQUIRES(cpu_), O const &, T a,  U b) noexcept
     {
@@ -128,5 +141,40 @@ namespace eve
         return kumi::sum( kumi::map([](auto a, auto b) { return a*b; }, f, s));
       }
     }
+
+    template<eve::detail::range R1, eve::detail::range R2, callable_options O>
+    EVE_FORCEINLINE constexpr auto
+    dot_(EVE_REQUIRES(cpu_), O const & o, R1 r1, R2 r2) noexcept
+    {
+      using r_t   = common_value_t<typename R1::value_type, typename R2::value_type>;
+      auto fr1 = begin(r1);
+      auto lr1  = end(r1);
+      if( fr1 == lr1 ) return r_t(0);
+      using std::advance;
+      auto fr2 = begin(r2);
+      auto cr2 = fr2;
+      auto cr1 = fr1;
+
+      advance(cr1, 1);
+      advance(cr2, 1);
+      if constexpr(O::contains(kahan))
+      {
+        auto [sum,err] = eve::two_prod(*fr1, *fr2);
+        auto step = [&](auto a, auto b) {
+          auto[s1, e1] = eve::two_fma_approx(a, b, sum);
+          err += e1;
+          sum = s1;
+        };
+        for(; cr1 != lr1; advance(cr1, 1), advance(cr2, 1) ) step(*cr1, *cr2);
+        return sum+err;
+      }
+      else
+      {
+        auto sum = mul[o](*fr1, *fr2);
+        for(; cr1 != lr1; advance(cr1, 1), advance(cr2, 1) ) sum+= mul[o](*cr1, *cr2);
+        return sum;
+      }
+    }
   }
+
 }
