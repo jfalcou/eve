8a4f2ac8b65296553d661108f61b596d0826dbc8
jtlap <jtlapreste@gmail.com>
2025-08-26 08:53:12 +0200

    dot average

--- test/doc/core/average.cpp
+++ test/doc/core/average.cpp
@@ -10,20 +10,28 @@ int main()
   eve::wide wi0{0, 1, 2, 3, -1, -2, -3, -4};
   eve::wide wi1{0, -4, 1, -1, 2, -2, 3, -3};
 
-  std::cout << "<- wf0                               = " << wf0 << "\n";
-  std::cout << "<- wf1                               = " << wf1 << "\n";
-  std::cout << "<- wi0                               = " << wi0 << "\n";
-  std::cout << "<- wi1                               = " << wi1 << "\n";
+  std::cout << "<- wf0                                        = " << wf0 << "\n";
+  std::cout << "<- wf1                                        = " << wf1 << "\n";
+  std::cout << "<- wi0                                        = " << wi0 << "\n";
+  std::cout << "<- wi1                                        = " << wi1 << "\n";
+  std::cout << std::setprecision(15);
 
-  std::cout << "-> average(wf0, wf1)                 = " << eve::average(wf0, wf1) << "\n";
-  std::cout << "-> average(wi0, wi1)                 = " << eve::average(wi0, wi1) << "\n";
-  std::cout << "-> average[ignore_last(2)](wi0, wi1) = " << eve::average[eve::ignore_last(2)](wi0, wi1) << "\n";
-  std::cout << "-> average[wi0 != 0](wi0, wi1)       = " << eve::average[wi0 != 0](wi0, wi1) << "\n";
-  std::cout << "-> average[raw](wi0, wi1)            = " << eve::average[eve::raw](wi0, wi1) << "\n";
-  std::cout << "-> average[upper](wi0, wi1)          = " << eve::average[eve::upper](wi0, wi1) << "\n";
-  std::cout << "-> average[lower](wi0, wi1)          = " << eve::average[eve::lower](wi0, wi1) << "\n";
-  std::cout << std::setprecision(20) << "-> average[upper](wf0, wf1)          = " << eve::average[eve::upper](wf0, wf1) << "\n";
-  std::cout << "-> average[lower](wf0, wf1)          = " << eve::average[eve::lower](wf0, wf1) << "\n";
-  std::cout << std::setprecision(20) << "-> average[lower][strict](wf0, wf1)   = " << eve::average[eve::lower][eve::strict](wf0, wf1) << "\n";
-  std::cout << std::setprecision(20) << "-> average[upper][strict](wf0, wf1)   = " << eve::average[eve::upper][eve::strict](wf0, wf1) << "\n";
+  std::cout << "-> average(wf0, wf1)                          = " << eve::average(wf0, wf1) << "\n";
+  std::cout << "-> average(wi0, wi1)                          = " << eve::average(wi0, wi1) << "\n";
+  std::cout << "-> average[ignore_last(2)](wi0, wi1)          = " << eve::average[eve::ignore_last(2)](wi0, wi1) << "\n";
+  std::cout << "-> average[wi0 != 0](wi0, wi1)                = " << eve::average[wi0 != 0](wi0, wi1) << "\n";
+  std::cout << "-> average[raw](wi0, wi1)                     = " << eve::average[eve::raw](wi0, wi1) << "\n";
+  std::cout << "-> average[upper](wi0, wi1)                   = " << eve::average[eve::upper](wi0, wi1) << "\n";
+  std::cout << "-> average[lower](wi0, wi1)                   = " << eve::average[eve::lower](wi0, wi1) << "\n";
+  std::cout << "-> average[upper](wf0, wf1)                   = " << eve::average[eve::upper](wf0, wf1) << "\n";
+  std::cout << "-> average[lower](wf0, wf1)                   = " << eve::average[eve::lower](wf0, wf1) << "\n";
+  std::cout << "-> average[lower][strict](wf0, wf1)           = " << eve::average[eve::lower][eve::strict](wf0, wf1) << "\n";
+  std::cout << "-> average[upper][strict](wf0, wf1)           = " << eve::average[eve::upper][eve::strict](wf0, wf1) << "\n";
+  auto eps_2 = eve::eps(eve::as<float>())/2;
+  std::cout << "-> average(1.0f, eps_2, eps_2, eps_2)         = " << eve::average(1.0f, eps_2, eps_2, eps_2) << "\n";
+  std::cout << "-> average[kahan](1.0f, eps_2, eps_2, eps_2)  = " << eve::average[eve::kahan](1.0f, eps_2, eps_2, eps_2) << " // float computation\n";
+  std::cout << "-> average[raw](1.0f, eps_2, eps_2, eps_2)    = " << eve::average[eve::raw](1.0f, eps_2, eps_2, eps_2) << "\n";
+  auto deps_2 = double(eps_2);
+  std::cout << "-> average(1.0, deps_2, deps_2, eps_2)        = " << float(eve::average[eve::kahan](1.0, deps_2, deps_2, deps_2)) << " // double computation converted to float\n";
+
 }
