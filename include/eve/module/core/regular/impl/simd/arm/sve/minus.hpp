b41b3809c8a6e0fb2327fe0c756f44b4e2128e17
jtlap <jtlapreste@gmail.com>
2025-08-31 20:02:12 +0200

    minus for arm

--- include/eve/module/core/regular/impl/simd/arm/sve/minus.hpp
+++ include/eve/module/core/regular/impl/simd/arm/sve/minus.hpp
@@ -15,7 +15,7 @@ namespace eve::detail
 {
   template<callable_options O, signed_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N> minus_(EVE_REQUIRES(sve_), O const& o, wide<T, N> v) noexcept
-    requires sve_abi<abi_t<T, N>>
+  requires(sve_abi<abi_t<T, N>> && !O::contains(mod))
   {
     // saturated integer has no intrinsic and floating is better by default
     if constexpr((O::contains(saturated) && std::integral<T>) || floating_value<T>) return minus.behavior(cpu_{},o,v);
@@ -24,7 +24,7 @@ namespace eve::detail
 
   template<callable_options O, conditional_expr C, signed_scalar_value T, typename N>
   EVE_FORCEINLINE wide<T, N> minus_(EVE_REQUIRES(sve_), C const& mask, O const& o, wide<T, N> v) noexcept
-    requires sve_abi<abi_t<T, N>>
+  requires (sve_abi<abi_t<T, N>> && !O::contains(mod))
   {
     auto const alt = alternative(mask, v, as(v));
 
