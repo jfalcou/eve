ead83cd6ad756ed0006768a2be332c28acf2e4ed
SadiinsoSnowfall <SadiinsoSnowfall@users.noreply.github.com>
2025-09-01 11:24:07 +0000

    deploy: b777741e9e9aa2902f91a2022d643db57b7d2de6

--- search/functions_11.js
+++ search/functions_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['translate_0',['translate',['../namespaceeve_a2ca18720b2c0736de236fbbd8b003ab8.html#a2ca18720b2c0736de236fbbd8b003ab8',1,'eve::translate(V const &amp;val)'],['../namespaceeve_a72f8af3ec7e96896cb46aa0f520897f7.html#a72f8af3ec7e96896cb46aa0f520897f7',1,'eve::translate(V &amp;val)']]]
+  ['translate_0',['translate',['../namespaceeve_a2ca18720b2c0736de236fbbd8b003ab8.html#a2ca18720b2c0736de236fbbd8b003ab8',1,'eve::translate(V const &amp;val)'],['../namespaceeve_ae1475d9cc84bb8240682ba568181e438.html#ae1475d9cc84bb8240682ba568181e438',1,'eve::translate(std::array&lt; T, N &gt; val)'],['../namespaceeve_a72f8af3ec7e96896cb46aa0f520897f7.html#a72f8af3ec7e96896cb46aa0f520897f7',1,'eve::translate(V &amp;val)']]]
 ];
