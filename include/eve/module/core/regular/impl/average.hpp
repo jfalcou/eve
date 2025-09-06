c32e63aea30f5ca85f06601081f4c5b63b3ecf6f
jtlap <jtlapreste@gmail.com>
2025-09-06 09:58:40 +0200

    average

--- include/eve/module/core/regular/impl/average.hpp
+++ include/eve/module/core/regular/impl/average.hpp
@@ -82,6 +82,19 @@ namespace eve::detail
         auto [r, e] = p0;
         return r+e;
       }
+      else if constexpr(O::contains(welford))
+      {
+        e_t count(1);
+        auto welfordstep = [&count](auto mprev, auto a)
+        {
+          count = inc(count);
+          auto mcur = mprev+(a-mprev)/count;
+          return mcur;
+        };
+        auto p0 = r_t(a0);
+        ((p0 = welfordstep(p0,args)),...);
+        return p0;
+      }
       else
       {
         r_t that(a0 * invn);
@@ -91,5 +104,35 @@ namespace eve::detail
         return that;
       }
     }
+  }
+
+  template<typename T0, typename ... Ts, callable_options O>
+  EVE_FORCEINLINE constexpr auto
+  average_(EVE_REQUIRES(cpu_), O const & , std::size_t n, T0 prev_mean, Ts const &... args) noexcept
+  requires(O::contains(welford))
+  {
+    using r_t =  eve::common_value_t<T0, Ts...>;
+    using e_t =  eve::element_type_t<r_t>;
+    e_t count(n);
+    auto welfordstep = [&count](auto mprev, auto a)
+      {
+        count = inc(count);
+        auto mcur = mprev+(a-mprev)/count;
+        return mcur;
+      };
+    auto p0 = r_t(prev_mean);
+    ((p0 = welfordstep(p0,args)),...);
+    return p0;
+  }
+
+  template<conditional_expr C, typename T0, typename ... Ts, callable_options O>
+
+  EVE_FORCEINLINE constexpr auto average_(EVE_REQUIRES(cpu_), C c, T0 t0, Ts... ts) noexcept
+  {
+    auto z = average(t0, ts...);
+    if constexpr(O::contains(eve::condition_key))
+      return  mask_op(c[eve::condition_key], return_2nd, t0, z);
+    else
+      return z;
   }
 }
