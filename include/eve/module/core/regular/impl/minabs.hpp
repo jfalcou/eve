4ff55f5fd4b3febda1e3c8c599f993c1f5efa1f0
jtlap <jtlapreste@gmail.com>
2025-09-12 14:33:53 +0200

    min/maxabs simplified and extended with widen

--- include/eve/module/core/regular/impl/minabs.hpp
+++ include/eve/module/core/regular/impl/minabs.hpp
@@ -12,11 +12,30 @@
 
 namespace eve::detail
 {
-  template<typename T0, typename T1, typename... Ts, callable_options O>
-  EVE_FORCEINLINE constexpr common_value_t<T0, T1, Ts...>
-  minabs_(EVE_REQUIRES(cpu_), O const & o, T0 a0, T1 a1, Ts... as) noexcept
+  template<typename T0, typename... Ts, callable_options O>
+  EVE_FORCEINLINE constexpr common_value_t<T0, Ts...>
+  minabs_(EVE_REQUIRES(cpu_), O const & o, T0 t0, Ts... as) noexcept
   {
-    auto abso = abs[o.drop(pedantic,numeric)];
-    return eve::min[o.drop(saturated)](abso(a0), abso(a1), abso(as)...);
+    using r_t = eve::common_value_t<T0, Ts...>;
+    if constexpr(O::contains(widen))
+    {
+      if constexpr(signed_integral_value<r_t> && O::contains(saturated))
+        return minabs[o.drop(widen)](upgrade(t0), upgrade(as)...);
+      else
+        return upgrade(minabs[o.drop(widen)](t0, as...));
+    }
+    else if constexpr(sizeof...(Ts) == 0)
+    {
+      return abs[o.drop(pedantic,numeric)](r_t(t0));
+    }
+    else
+    {
+      auto abso = abs[o.drop(pedantic,numeric)];
+      auto r = eve::min[o.drop(saturated)](abso(t0), abso(as)...);
+      if constexpr(integral_value<r_t> || !O::contains(pedantic))
+        return r;
+      else
+        return force_if_any(o, r, eve::is_eqz, eve::zero(eve::as(r)), t0, as...);
+    }
   }
 }
