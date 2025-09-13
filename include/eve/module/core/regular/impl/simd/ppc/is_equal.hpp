2bbbb78647799c9ce7df27e9a3ddc6c2a43006d8
SadiinsoSnowfall <sadiinso@gmail.com>
2025-09-13 19:22:39 +0200

    fix

--- include/eve/module/core/regular/impl/simd/ppc/is_equal.hpp
+++ include/eve/module/core/regular/impl/simd/ppc/is_equal.hpp
@@ -14,7 +14,7 @@ namespace eve::detail
 {
   template<callable_options O, arithmetic_scalar_value T, typename N>
   EVE_FORCEINLINE logical<wide<T, N>> is_equal_(EVE_REQUIRES(vmx_), O const& opts, wide<T, N> a, wide<T, N> b) noexcept
-  requires ppc_abi<abi_t<T, N>>
+    requires ppc_abi<abi_t<T, N>>
   {
     if constexpr (O::contains(almost) || O::contains(numeric)) return is_equal.behavior(cpu_{}, opts, a, b);
     else return logical<wide<T,N>>(vec_cmpeq(a.storage(), b.storage()));
