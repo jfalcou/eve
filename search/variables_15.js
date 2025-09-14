bca8862e478f3576b34328282c776cb773aa327a
jfalcou <jfalcou@users.noreply.github.com>
2025-09-14 08:28:00 +0000

    deploy: 88a917d400dc34fb83bbd22a883eac0700ad6d0e

--- search/variables_15.js
+++ search/variables_15.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['we_5f_0',['we_',['../group__simd__types.html#gab6c3c30138d82ef45b1bbcf9e65145d4',1,'eve']]],
-  ['write_1',['write',['../group__memory_ga3914bc9a9604e1c20021fdb28557141d.html#ga3914bc9a9604e1c20021fdb28557141d',1,'eve']]]
+  ['welford_5faverage_1',['welford_average',['../group__core__arithmetic_gabe69385da9a598348aedde44c20028c6.html#gabe69385da9a598348aedde44c20028c6',1,'eve']]],
+  ['write_2',['write',['../group__memory_ga3914bc9a9604e1c20021fdb28557141d.html#ga3914bc9a9604e1c20021fdb28557141d',1,'eve']]]
 ];
