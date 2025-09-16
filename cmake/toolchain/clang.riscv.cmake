d2ce03912bf8229f18e295e74a10672c9dd7b6dd
SadiinsoSnowfall <sadiinso@gmail.com>
2025-09-16 20:08:09 +0200

    test

--- cmake/toolchain/clang.riscv.cmake
+++ cmake/toolchain/clang.riscv.cmake
@@ -6,8 +6,8 @@
 set(CMAKE_SYSTEM_NAME       Generic)
 set(CMAKE_SYSTEM_PROCESSOR  riscv64)
 
-set(CMAKE_C_COMPILER    clang)
-set(CMAKE_CXX_COMPILER  clang++)
+set(CMAKE_C_COMPILER    clang-20)
+set(CMAKE_CXX_COMPILER  clang++-20)
 
 set(CMAKE_CXX_FLAGS "-march=rv64gcv --static -fbracket-depth=512 --target=riscv64-unknown-linux-gnu ${EVE_OPTIONS}" )
 set(CMAKE_CROSSCOMPILING_CMD ${PROJECT_SOURCE_DIR}/cmake/toolchain/run_rvv128.sh )
