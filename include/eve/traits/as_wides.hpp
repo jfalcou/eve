90af192330c3d5bc8b1c96d71f97232ec8a05448
jtlap <jtlapreste@gmail.com>
2025-09-19 16:04:04 +0200

    zero lol shadow

--- include/eve/traits/as_wides.hpp
+++ include/eve/traits/as_wides.hpp
@@ -12,15 +12,15 @@
 namespace eve
 {
   template<typename T0, typename... T>
-  auto as_wides(T0 z, T... vs)
+  auto as_wides(T0 zz, T... vs)
   {
     constexpr auto rem = (sizeof...(T)) % eve::wide<T0>::size();
     if constexpr(rem != 0)
     {
       return [&]<std::size_t... N>(std::index_sequence<N...>)
       {
-        auto z = [&](auto) { return zero; };
-        return as_wides(z,vs..., z(N)...);
+        auto z = [&](auto) { return zz; };
+        return as_wides(zero,vs..., z(N)...);
       }(std::make_index_sequence<eve::wide<T0>::size() - rem>());
     }
     else
