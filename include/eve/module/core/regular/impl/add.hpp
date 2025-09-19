e75493e95a6822d48d63dfa4e1520681ff89774f
jtlap <jtlapreste@gmail.com>
2025-09-19 09:17:14 +0200

    add but using butterfly not sum

--- include/eve/module/core/regular/impl/add.hpp
+++ include/eve/module/core/regular/impl/add.hpp
@@ -26,14 +26,6 @@
 
 namespace eve::detail
 {
-  template<callable_options O, typename T0>
-  EVE_FORCEINLINE constexpr auto add_(EVE_REQUIRES(cpu_), O const&, T0 a) noexcept
-  {
-    if constexpr(O::contains(widen))
-      return upgrade(a);
-    else
-      return a;
-  }
 
   template<callable_options O, typename T0, typename T1>
   EVE_FORCEINLINE constexpr auto add_(EVE_REQUIRES(cpu_), O const& o, T0 a0, T1 b0) noexcept
@@ -112,39 +104,60 @@ namespace eve::detail
     }
   }
 
+  template<callable_options O, typename T0>
+  EVE_FORCEINLINE constexpr auto add_(EVE_REQUIRES(cpu_), O const&, T0 a) noexcept
+  {
+    if constexpr(O::contains(widen))
+      return upgrade(a);
+    else
+      return a;
+  }
+
   template<typename T0, typename T1, typename ... Ts, callable_options O>
   EVE_FORCEINLINE constexpr auto add_(EVE_REQUIRES(cpu_), O const & o, T0 r0, T1 r1, Ts... rs) noexcept
   requires(sizeof...(Ts) != 0)
   {
-    using r_t = eve::common_value_t<Ts...>;
+    using r_t = eve::common_value_t<T0, T1, Ts...>;
     //TODO: both GCC and Clang can fail to properly reorder the op chain to reduce dependencies
     //      we might want to do this manually
     if constexpr(O::contains(widen))
       return add[o.drop(widen)](upgrade(r0), upgrade(r1), upgrade(rs)...);
-    else if constexpr(O::contains(kahan))
-    {
-      // kahan being precursor, but this is S. M. Rump, T. Ogita, and S. Oishi algorithm
-      // Accurate floating-point summation part I: Faithful rounding.
-      // SIAM Journal on Scientific Computing, 31(1):189-224, 2008.
-      auto get_fn= [](){
-        if constexpr(O::contains(raw)) return two_add[raw];
-        else return two_add;
-      };
-      auto pair_add = [fn = get_fn()](auto pair0, auto ri){
-        auto [a0, e0] = pair0;
-        auto [s, e1] = fn(a0, ri);
-        return zip(s, e0+e1);
-      };
-      auto p0   = two_add(r_t(r0),r_t(r1));
-      ((p0 = pair_add(p0,r_t(rs))),...);
-      auto [r, e] = p0;
-      return r+e;
-    }
     else
     {
-      r0   = add[o](r_t(r0),r_t(r1));
-      ((r0 = add[o](r_t(r0),r_t(rs))),...);
-      return r0;
+      if constexpr(scalar_value<r_t> && (sizeof...(Ts)+2 >= eve::expected_cardinal_v<r_t>) && !O::contains(saturated))
+      {
+        auto head = eve::as_wides(eve::zero(eve::as<r_t>()), r0, r1, rs...);
+        auto s = eve::add[o](head);
+//         if constexpr(O::size() == 1 && O::contains(ignore_none))
+//           return sum(s);
+//         else
+          return butterfly_reduction(s, eve::add[o]).get(0);
+      }
+      else if constexpr(O::contains(kahan))
+      {
+        // kahan being precursor, but this is S. M. Rump, T. Ogita, and S. Oishi algorithm
+        // Accurate floating-point summation part I: Faithful rounding.
+        // SIAM Journal on Scientific Computing, 31(1):189-224, 2008.
+        auto get_fn= [](){
+          if constexpr(O::contains(raw)) return two_add[raw];
+          else return two_add;
+        };
+        auto pair_add = [fn = get_fn()](auto pair0, auto ri){
+          auto [a0, e0] = pair0;
+          auto [s, e1] = fn(a0, ri);
+          return zip(s, e0+e1);
+        };
+        auto p0   = two_add(r_t(r0),r_t(r1));
+        ((p0 = pair_add(p0,r_t(rs))),...);
+        auto [r, e] = p0;
+        return r+e;
+      }
+      else
+      {
+        auto p0   = add[o](r_t(r0),r_t(r1));
+        ((p0 = add[o](p0,r_t(rs))),...);
+        return p0;
+      }
     }
   }
 }
