648d9a8b8a2e0bc17c3c53df21e0a8abcd1afedd
jtlap <jtlapreste@gmail.com>
2025-10-13 10:42:07 +0200

    [no ci] dummy

--- benchmarks/module/core/abs/regular/abs.hpp
+++ benchmarks/module/core/abs/regular/abs.hpp
@@ -15,7 +15,7 @@ int main()
 
   auto const std__abs = [](EVE_VALUE x) { return x > 0 ? x : EVE_VALUE(-x); };
 
-  auto arg0 = eve::bench::random_<EVE_VALUE>(lmin,lmax);
+  auto arg0 = eve::bench::random_<EVE_VALUE>(lmin, lmax);
 
   eve::bench::experiment xp;
   run<EVE_VALUE> (EVE_NAME(std__abs) , xp, std__abs, arg0);
