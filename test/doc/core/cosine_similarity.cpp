7b664305c5a619b208e06e54bb95be2649ec5de5
jtlap <jtlapreste@gmail.com>
2025-10-22 18:46:07 +0200

    test/doc/core/cosine_similarity.cpp

--- test/doc/core/cosine_similarity.cpp
+++ test/doc/core/cosine_similarity.cpp
@@ -1,34 +1,41 @@
 // revision 0
 #include <eve/module/core.hpp>
 #include <iostream>
+#include "../../vec3.hpp"
 
 int main()
 {
   kumi::tuple wf0{0.0, 1.0, 2.0, 3.0, -1.0, -2.0, -3.0, -4.0};
-  kumi::tuple wf1{0.0, -4.0, 1.0, -1.0, 2.0, -2.0, 3.0, -3.0};
+  kumi::tuple wf1{0.0, -4.0, 1.0, -1.0, 2.0, -2.0, 3.0, -4.0};
 
   std::cout << "<- wf0           = " << wf0 << "\n";
   std::cout << "<- wf1           = " << wf1 << "\n";
 
 
   std::cout << "-> cosine_similarity(wf0, wf1) = " << eve::cosine_similarity(wf0, wf1) << "\n";
-//  std::cout << "-> cosine_similarity(wf0, wf1, wf0, wf1) = "<< eve::cosine_similarity(wf0, wf1, wf0, wf1) << "\n";
-//   std::cout << "-> cosine_similarity[widen](wf0, wf1, wf0, wf1) = "<< eve::cosine_similarity[eve::widen](wf0, wf1, wf0, wf1) << "\n";
+  std::cout << "-> cosine_similarity(wf0, wf0) = " << eve::cosine_similarity(wf0, wf0) << "\n";
+  vec3<float> a(1.0, 2.0, 3.0);
+  vec3<float> b(-3.0, -4.0, -6.0);
+  std::cout <<  "a " << a << std::endl;
+  std::cout <<  "b " << b << std::endl;
+  std::cout << "eve::cosine_similarity(a, b) = "<< eve::cosine_similarity(a, b) << std::endl;
+  std::cout << "eve::cosine_similarity(a, a) = " << eve::cosine_similarity(a, a) << std::endl;
+  std::cout << "eve::cosine_similarity(b, a) = " << eve::cosine_similarity(b, a) << std::endl;
+  std::cout << "eve::cosine_similarity(b, b) = " << eve::cosine_similarity(b, b) << std::endl;
 
-//    auto vwf0= kumi::tuple{wf0, 2*wf0, 3*wf0};
-//    auto vwf1= kumi::tuple{wf1, 2*wf1, 3*wf1};
-//    std::cout << "-> cosine_similarity(vwf0, vwf1)        = " << eve::cosine_similarity(vwf0, vwf1) << "\n";
-//    std::cout << "-> cosine_similarity[widen](vwf0, vwf1) = " << eve::cosine_similarity[eve::widen](vwf0, vwf1) << "\n";
+  // simd cosine_similarity eve::wide<vec3<float>, eve::fixed<4>>;
+  using wv3_t = eve::wide<vec3<float>, eve::fixed<4>>;
+  auto wa = wv3_t(a, a, b, b);
+  auto wb = wv3_t(b, a, a, b);
+   std::cout << "wa " << wa << std::endl;
+   std::cout << "wb " << wb << std::endl;
+   std::cout << "eve::cosine_similarity[eve::widen](wa, wb) = "<< eve::cosine_similarity[eve::widen](wa, wb) << std::endl;
 
-//    auto a(kumi::generate<11>([](auto p){return float(p); }));
-//    auto b(kumi::generate<11>([](auto p){return float(p)+5; }));;
-//    std::cout << "<- a           = " << a << "\n";
-//    std::cout << "<- b           = " << b << "\n";
-//    std::cout << "-> cosine_similarity(a, b)        = " << eve::cosine_similarity(a, b) << "\n";
-//    std::cout << "-> cosine_similarity(a, a)        = " << eve::cosine_similarity(a, a) << "\n";
-//    auto c(kumi::generate<11>([](auto p){return p > 5  ? float(p) : 0.f; }));
-//    auto d(kumi::generate<11>([](auto p){return p > 5 ? 0.f :float(p)+5; }));;
-//    std::cout << "<- c           = " << c << "\n";
-//    std::cout << "<- d           = " << d << "\n";
-//    std::cout << "-> cosine_similarity(c, d)        = " << eve::cosine_similarity(c, d) << "\n";
+
+  kumi::tuple x{4.0f, 3.0f, 2.0f, 1.0f};
+  kumi::tuple y{1.0f, 2.0f, 3.0f, 4.0f};
+  std::cout << eve::cosine_similarity(x, x) <<  std::endl;
+  std::cout << eve::cosine_similarity(x, y) <<  std::endl;
+  std::cout << eve::cosine_similarity(y, x) <<  std::endl;
+
 }
