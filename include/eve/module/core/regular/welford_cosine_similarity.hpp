7f4573b83d46622999033b8eac213110da5c47b7
jtlap <jtlapreste@gmail.com>
2025-10-25 21:01:30 +0200

    cosine_similarity

--- include/eve/module/core/regular/welford_cosine_similarity.hpp
+++ include/eve/module/core/regular/welford_cosine_similarity.hpp
@@ -32,10 +32,12 @@ namespace eve
 
     template < typename T>
     std::ostream& operator << (std::ostream& os,welford_cosine_similarity_result<T> const & v){
-      os << '[' << ".sumx2 = " << v.sumx2
+      os << '['
+         << ".sumx2 = " << v.sumx2
          << ", .sumy2 = " << v.sumy2
-         << ", .sumxy = "<<  v.sumxy
-         << ", .cosine_similarity" << v.cosine_similarity << "]";
+         << ", .sumxy = " << v.sumxy
+         << ", .cosine_similarity = "<< v.cosine_similarity
+         << ']';
       return os;
     }
 
@@ -54,65 +56,7 @@ namespace eve
   template<typename Options>
   struct welford_cosine_similarity_t : callable<welford_cosine_similarity_t, Options, widen_option, unbiased_option>
   {
-
-//     template<floating_value T1, floating_value T2>
-//     requires(!Options::contains(widen))
-//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<T1>,
-//                                                                                         detail::internal_welford_cosine_similarity_t<T2>>>
-//     operator()(T1 t1, T2 t2) const noexcept
-//     {
-//       return EVE_DISPATCH_CALL(t1, t2);
-//     }
-
-//     template<floating_value T1, floating_value T2>
-//     requires(Options::contains(widen))
-//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<T1>,
-//                                                                                         detail::internal_welford_cosine_similarity_t<T2>>>
-//     operator()(T1 t1, T2 t2) const noexcept
-//     {
-//       return EVE_DISPATCH_CALL(t1, t2);
-//     }
-
-//     template<typename... Ts>
-//     requires(simd_value<common_value_t<Ts...>> && (sizeof...(Ts) !=  0) && Options::contains(widen))
-//        EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<upgrade_t<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>>
-//     operator()(Ts...ts) const noexcept
-//     {
-//       return EVE_DISPATCH_CALL(ts...);
-//     }
-
-//     template<typename... Ts>
-//     requires(simd_value<common_value_t<Ts...>> && (sizeof...(Ts) !=  0) && !Options::contains(widen))
-//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>
-//     operator()(Ts...ts) const noexcept
-//     {
-//       return EVE_DISPATCH_CALL(ts...);
-//     }
-
-    template<typename... Ts>
-    requires(simd_value<common_value_t<Ts...>> && (sizeof...(Ts) !=  0) && Options::contains(widen))
-       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<upgrade_t<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>>
-    operator()(Ts...ts) const noexcept
-    {
-      return EVE_DISPATCH_CALL(ts...);
-    }
-
-    template<typename... Ts>
-    requires(scalar_value<common_value_t<Ts...>> && (sizeof...(Ts) !=  0) && !Options::contains(widen))
-      EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>
-    operator()(Ts...ts) const noexcept
-    {
-      return EVE_DISPATCH_CALL(ts...);
-    }
-
-    template<typename... Ts>
-    requires(scalar_value<common_value_t<Ts...>> && (sizeof...(Ts) !=  0) && Options::contains(widen))
-       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<upgrade_t<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>>
-    operator()(Ts...ts) const noexcept
-    {
-      return EVE_DISPATCH_CALL(ts...);
-    }
-
+    // sequences of welford_cosine_similarity_result_t
     template<typename... Ts>
     requires((detail::is_welford_cosine_similarity_result_v<Ts > && ...) && !Options::contains(widen))
       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>
@@ -121,45 +65,28 @@ namespace eve
       return EVE_DISPATCH_CALL(ts...);
     }
 
-    template<typename... Ts>
-    requires((detail::is_welford_cosine_similarity_result_v<Ts> && ...) && Options::contains(widen))
-      EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>
-    operator()(Ts...ts) const noexcept
-     {
-      return EVE_DISPATCH_CALL(ts...);
-    }
-
-//     template<typename T>
-//     requires(detail::is_welford_cosine_similarity_result_v<T> && !Options::contains(widen))
-//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<element_type_t<detail::internal_welford_cosine_similarity_t<T>>>
-//     operator()(T t) const noexcept
+ //    template<typename... Ts>
+//     requires((detail::is_welford_cosine_similarity_result_v<Ts> && ...) && Options::contains(widen))
+//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>
+//     operator()(Ts...ts) const noexcept
 //     {
-//       return EVE_DISPATCH_CALL(t);
+//       return EVE_DISPATCH_CALL(ts...);
 //     }
 
-//     template<typename T>
-//     requires(detail::is_welford_cosine_similarity_result_v<T> && Options::contains(widen))
-//       EVE_FORCEINLINE constexpr detail::welford_cosine_similarity_result<detail::internal_welford_cosine_similarity_t<element_type_t<upgrade_t<T>>>>
-//     operator()(T t) const noexcept
-//     {
-//       return EVE_DISPATCH_CALL(t);
-//     }
-
+    // tuples of floating_value
     template<kumi::non_empty_product_type Tup1, kumi::non_empty_product_type Tup2>
     requires(eve::same_lanes_or_scalar_tuple<Tup1> && eve::same_lanes_or_scalar_tuple<Tup2> && !Options::contains(widen))
       EVE_FORCEINLINE constexpr
-    detail::welford_cosine_similarity_result<kumi::apply_traits_t<eve::common_value, Tup1>>
+    detail::welford_cosine_similarity_result<kumi::apply_traits_t<eve::common_value, kumi::result::cat_t<Tup1, Tup2>>>
     operator()(Tup1 const& t1, Tup2 const& t2) const noexcept
     { return EVE_DISPATCH_CALL(t1, t2); }
 
     template<kumi::non_empty_product_type Tup1, kumi::non_empty_product_type Tup2>
     requires(eve::same_lanes_or_scalar_tuple<Tup1> && eve::same_lanes_or_scalar_tuple<Tup2> && Options::contains(widen))
       EVE_FORCEINLINE constexpr
-    detail::welford_cosine_similarity_result<upgrade_t<kumi::apply_traits_t<eve::common_value, Tup1>>>
+    detail::welford_cosine_similarity_result<upgrade_t<kumi::apply_traits_t<eve::common_value, kumi::result::cat_t<Tup1, Tup2>>>>
     operator()(Tup1 const& t1, Tup2 const& t2) const noexcept
     { return EVE_DISPATCH_CALL(t1, t2); }
-
-
 
     EVE_CALLABLE_OBJECT(welford_cosine_similarity_t, welford_cosine_similarity_);
   };
@@ -230,34 +157,51 @@ namespace eve
   namespace detail
   {
 
-    template<floating_value T1,  floating_value T2, callable_options O>
-    EVE_FORCEINLINE constexpr auto
-    welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T1 t1, T2 t2) noexcept
+    template<kumi::non_empty_product_type PT1, kumi::non_empty_product_type PT2, callable_options O>
+    EVE_FORCEINLINE constexpr auto welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, PT1 f, PT2 s) noexcept
+    requires (kumi::as_tuple_t<PT1>::size() == kumi::as_tuple_t<PT2>::size())
     {
-      using r_t = detail::welford_cosine_similarity_result<common_value_t<T1, T2>>;
-      if constexpr(O::contains(widen))
-      {
-        return welford_cosine_similarity(eve::upgrade(t1), eve::upgrade(t2));
+      if constexpr(O::contains(widen)) {
+        auto up = [](auto tup){
+          auto upg = [](auto t){return eve::upgrade(t); };
+          return kumi::map(upg, tup);
+        };
+        return welford_cosine_similarity[o.drop(widen)](up(f), up(s));
       }
       else
       {
-        return r_t(eve::sqr(t1), eve::sqr(t2), t1*t2, eve::sign(t1*t2));
+        using Tup1 = kumi::as_tuple_t<PT1>;
+        using Tup2 = kumi::as_tuple_t<PT2>;
+        using r1_t = kumi::apply_traits_t<eve::common_value, Tup1>;
+        using r2_t = kumi::apply_traits_t<eve::common_value, Tup2>;
+        using r_t =  eve::common_value_t<r1_t, r2_t>;
+
+        auto nsim = welford_cosine_similarity_result<r_t>();
+        auto doit = [&nsim, &f, &s](){
+          auto wcs_step = [&nsim](auto xnyn)
+          {
+            auto [xn, yn] = xnyn;
+            auto rxn = xn;
+            auto ryn = yn;
+            nsim.sumx2 = eve::fam(nsim.sumx2, rxn, rxn);
+            nsim.sumy2 = eve::fam(nsim.sumy2, ryn, ryn);
+            nsim.sumxy = eve::fam(nsim.sumxy, rxn, ryn);
+            return nsim;
+          };
+          auto r = kumi::map([](auto xs,  auto ys){return eve::zip(r_t(xs), r_t(ys)); }, f, s);
+          kumi::apply([wcs_step](auto ... xsys){((wcs_step(xsys)), ...); }, r);
+          nsim.cosine_similarity = nsim.sumxy*eve::rsqrt(nsim.sumx2*nsim.sumy2);
+          return nsim;
+        };
+        return  doit();
       }
     }
 
-    // this call computes the welford cosine_similarity from a sequence of floating values xs then ys parameters
+    // This call compute the join cosine_similarity from previously computed welford cosine similarities
     template<typename... Ts, callable_options O>
-    EVE_FORCEINLINE constexpr auto
-    welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, Ts... args) noexcept
-    requires( (sizeof...(Ts) > 0) &&
-              (sizeof...(Ts)%2 == 0) &&
-              (simd_value<eve::common_value_t<Ts...>> ||
-               (scalar_value<eve::common_value_t<Ts...>>  && (sizeof...(Ts) < 2*wide<common_value_t<Ts...>>::size()))
-              )
-            )
+    EVE_FORCEINLINE constexpr auto welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, Ts... args) noexcept
+    requires((sizeof...(Ts) > 1) && (detail::is_welford_cosine_similarity_result_v<Ts> && ...))
     {
-      constexpr auto siz = sizeof...(Ts)/2;
-      using r_t =  eve::common_value_t<Ts...>;
       if constexpr(O::contains(widen))
       {
         auto up_it = [](auto a){
@@ -268,107 +212,97 @@ namespace eve
       }
       else
       {
-        auto[f,s]   = kumi::split(kumi::make_tuple(args...), kumi::index<siz>);
-        auto nsim = welford_cosine_similarity_result<r_t>();
-        auto doit = [&nsim, &f, &s](){
-          auto welford_cosine_similaritystep = [&nsim](auto xnyn)
-          {
-            auto [xn, yn] = xnyn;
-            nsim.sumx2 = eve::fam(nsim.sumx2, xn, xn);
-            nsim.sumy2 = eve::fam(nsim.sumy2, yn, yn);
-            nsim.sumxy = eve::fam(nsim.sumxy, xn, yn);
-            return nsim;
-          };
-          auto r = kumi::map([](auto xs,  auto ys){return eve::zip(xs, ys); }, f, s);
-          kumi::apply([welford_cosine_similaritystep](auto ... xsys){((welford_cosine_similaritystep(xsys)), ...); }, r);
-          nsim.cosine_similarity = nsim.sumxy*eve::rsqrt(nsim.sumx2*nsim.sumy2);
-          return nsim;
-        };
-        return  doit();
-      }
-    }
-
-    // This call compute the join cosine_similarity from previously computed welford cosine similarities
-    template<typename T, typename... Ts, callable_options O>
-    EVE_FORCEINLINE constexpr auto welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T t, Ts... args) noexcept
-    requires((sizeof...(Ts)!= 0) && detail::is_welford_cosine_similarity_result_v<T> && (detail::is_welford_cosine_similarity_result_v<Ts> && ...))
-    {
-      if constexpr(O::contains(widen))
-      {
-        auto up_it = [](auto a){
-          if constexpr(requires { a.up(); }) return a.up();
-          else return eve::upgrade(a);
-        };
-        return welford_cosine_similarity[o.drop(widen)](up_it(t), up_it(args)...);
-      }
-      else
-      {
-        T nsim = t;
+        using r_t = typename detail::welford_cosine_similarity_result<common_value_t<detail::internal_welford_cosine_similarity_t<Ts>...>>;
+        using v_t = r_t::type;
+        auto nsim = welford_cosine_similarity_result<v_t>();
+//        return nsim;
         auto doit = [ & ](){
-          auto welford_cosine_similaritystep = [&](auto sim)
+          auto wcs_step = [&](auto sim)
           {
             nsim.sumx2 += sim.sumx2;
             nsim.sumy2 += sim.sumy2;
             nsim.sumxy += sim.sumxy;
             return nsim;
           };
-          ((nsim = welford_cosine_similaritystep(args)),...);
+          ((nsim = wcs_step(args)),...);
           nsim.cosine_similarity = nsim.sumxy*eve::rsqrt(nsim.sumx2*nsim.sumy2);
           return nsim;
         };
         return  doit();
       }
     }
-
-    // This call 'scalarize' a welford cosine_similarity
-    template<typename T, callable_options O>
-    EVE_FORCEINLINE constexpr auto welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T t) noexcept
-    requires(detail::is_welford_cosine_similarity_result_v<T>)
-    {
-      auto scalarize = []<typename U>(U w){
-        using e_t =  element_type_t<typename U::type>;
-        auto getit = [w](auto i){return welford_cosine_similarity_result<e_t>(w.sumx2.get(i), w.sumy2.get(i), w.sumxy.get(i), w.cosine_similarity.get(i)); };
-        return kumi::generate<w.mxy.size()>(getit);
-      };
-      if constexpr(scalar_value<typename T::type>)
-        return t;
-      else
-      {
-        auto tpsc =  scalarize(t);
-        return kumi::apply([o](auto... m){return welford_cosine_similarity[o](m...); }, tpsc);
-      }
-    }
-
-    // This call treat the case where all entries are scalar values and numerous enough by chunking
-    template<scalar_value ... Ts, callable_options O>
-    EVE_FORCEINLINE constexpr auto
-    welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, Ts const &... args) noexcept
-    requires( (sizeof...(Ts) > 0) && (sizeof...(Ts)%2 == 0) && (sizeof...(Ts) >= 2*wide<common_value_t<Ts...>>::size()))
-    {
-      auto scalarize = []<typename T>(T w){
-        using e_t  =  element_type_t<typename T::type>;
-        auto getit = [w](auto i){return welford_cosine_similarity_result<e_t>(w.sumx2.get(i), w.sumy2.get(i), w.sumxy.get(i), w.cosine_similarity.get(i)); };
-        return kumi::generate<w.sumxy.size()>(getit);
-      };
-
-      using r_t =  common_value_t<Ts...>;
-      auto tup = kumi::make_tuple(args...);
-      constexpr auto siz = sizeof...(Ts)/2;
-      auto [tup1, tup2] = kumi::split(tup,  kumi::index<siz>);
-      auto head1 = as_wides(eve::zero(eve::as<r_t>()), tup1);
-      auto head2 = as_wides(eve::zero(eve::as<r_t>()), tup2);
-      auto wc = eve::welford_cosine_similarity[o](head1, head2);
-      auto swc = scalarize(wc);
-      auto wsim1 = kumi::apply([](auto...m){return welford_cosine_similarity(m...);}, swc);
-      return wsim1;
-    }
-
-
-    template< kumi::non_empty_product_type T1,  kumi::non_empty_product_type T2, callable_options O>
-    EVE_FORCEINLINE constexpr auto
-    welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T1 t1, T2 t2) noexcept
-    {
-      return kumi::apply([o](auto... m){return welford_cosine_similarity[o](m...); }, kumi::cat(t1, t2));
-    }
   }
 }
+//         using r_t = eve::common_value_t<detail::internal_welford_cosine_similarity_t<Ts>, ...> > ;
+
+//         auto nsim = welford_cosine_similarity_result<r_t>();
+//         auto doit = [ & ](){
+//           auto wcs_step = [&](auto sim)
+//           {
+//             nsim.sumx2 += sim.sumx2;
+//             nsim.sumy2 += sim.sumy2;
+//             nsim.sumxy += sim.sumxy;
+//             return nsim;
+//           };
+//           ((nsim = wcs_step(args)),...);
+//           nsim.cosine_similarity = nsim.sumxy*eve::rsqrt(nsim.sumx2*nsim.sumy2);
+//           return nsim;
+//         };
+//         return  doit();
+//       }
+//     }
+
+
+
+// //     // This call 'scalarize' a welford cosine_similarity
+// //     template<typename T, callable_options O>
+// //     EVE_FORCEINLINE constexpr auto welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T t) noexcept
+// //     requires(detail::is_welford_cosine_similarity_result_v<T>)
+// //     {
+// //       auto scalarize = []<typename U>(U w){
+// //         using e_t =  element_type_t<typename U::type>;
+// //         auto getit = [w](auto i){return welford_cosine_similarity_result<e_t>(w.sumx2.get(i), w.sumy2.get(i), w.sumxy.get(i), w.cosine_similarity.get(i)); };
+// //         return kumi::generate<w.mxy.size()>(getit);
+// //       };
+// //       if constexpr(scalar_value<typename T::type>)
+// //         return t;
+// //       else
+// //       {
+// //         auto tpsc =  scalarize(t);
+// //         return kumi::apply([o](auto... m){return welford_cosine_similarity[o](m...); }, tpsc);
+// //       }
+// //     }
+
+// //     // This call treat the case where all entries are scalar values and numerous enough by chunking
+// //     template<scalar_value ... Ts, callable_options O>
+// //     EVE_FORCEINLINE constexpr auto
+// //     welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, Ts const &... args) noexcept
+// //     requires( (sizeof...(Ts) > 0) && (sizeof...(Ts)%2 == 0) && (sizeof...(Ts) >= 2*wide<common_value_t<Ts...>>::size()))
+// //     {
+// //       auto scalarize = []<typename T>(T w){
+// //         using e_t  =  element_type_t<typename T::type>;
+// //         auto getit = [w](auto i){return welford_cosine_similarity_result<e_t>(w.sumx2.get(i), w.sumy2.get(i), w.sumxy.get(i), w.cosine_similarity.get(i)); };
+// //         return kumi::generate<w.sumxy.size()>(getit);
+// //       };
+
+// //       using r_t =  common_value_t<Ts...>;
+// //       auto tup = kumi::make_tuple(args...);
+// //       constexpr auto siz = sizeof...(Ts)/2;
+// //       auto [tup1, tup2] = kumi::split(tup,  kumi::index<siz>);
+// //       auto head1 = as_wides(eve::zero(eve::as<r_t>()), tup1);
+// //       auto head2 = as_wides(eve::zero(eve::as<r_t>()), tup2);
+// //       auto wc = eve::welford_cosine_similarity[o](head1, head2);
+// //       auto swc = scalarize(wc);
+// //       auto wsim1 = kumi::apply([](auto...m){return welford_cosine_similarity(m...);}, swc);
+// //       return wsim1;
+// //     }
+
+
+// //     template< kumi::non_empty_product_type T1,  kumi::non_empty_product_type T2, callable_options O>
+// //     EVE_FORCEINLINE constexpr auto
+// //     welford_cosine_similarity_(EVE_REQUIRES(cpu_), O const & o, T1 t1, T2 t2) noexcept
+// //     {
+// //       return kumi::apply([o](auto... m){return welford_cosine_similarity[o](m...); }, kumi::cat(t1, t2));
+// //     }
+// //   }
+// }
