8bdb7ec5f71ff0c8ee817bc689e651104f70df8f
jtlap <jtlapreste@gmail.com>
2025-11-05 19:55:45 +0100

    exponent ldexp

--- include/eve/module/core/regular/exponent.hpp
+++ include/eve/module/core/regular/exponent.hpp
@@ -20,6 +20,7 @@
 #include <eve/module/core/regular/is_not_finite.hpp>
 #include <eve/module/core/regular/logical_not.hpp>
 #include <eve/module/core/regular/logical_or.hpp>
+#include <eve/module/core/regular/sub.hpp>
 #include <eve/arch/platform.hpp>
 
 namespace eve
@@ -29,7 +30,7 @@ namespace eve
   struct exponent_t : elementwise_callable<exponent_t, Options, raw_option>
   {
     template<eve::value T>
-    constexpr EVE_FORCEINLINE as_integer_t<T> operator()(T v) const noexcept
+    constexpr EVE_FORCEINLINE as_integer_t<T, signed> operator()(T v) const noexcept
     { return EVE_DISPATCH_CALL(v); }
 
     EVE_CALLABLE_OBJECT(exponent_t, exponent_);
@@ -85,10 +86,11 @@ namespace eve
   namespace detail
   {
     template<floating_value T, callable_options O>
-    constexpr as_integer_t<T>  exponent_(EVE_REQUIRES(cpu_), O const&, T const& a) noexcept
+    constexpr as_integer_t<T, signed>  exponent_(EVE_REQUIRES(cpu_), O const&, T const& a) noexcept
     {
-      auto z = bit_and(exponentmask(as<T>()), a);
-      auto x = (z >> nbmantissabits(eve::as<T>()));
+      using i_t =  as_integer_t<T>;
+      i_t z = bit_and(exponentmask(as<T>()), a);
+      i_t x = eve::bit_shr(z, nbmantissabits(eve::as<T>()));
       return sub[is_nez(a)](x, maxexponent(eve::as<T>()));
     }
   }
--- include/eve/module/core/regular/impl/ldexp.hpp
+++ include/eve/module/core/regular/impl/ldexp.hpp
@@ -70,11 +70,11 @@ constexpr auto ldexp_(EVE_REQUIRES(cpu_), O const& o, T a, U b)
   else
   {
     using elt_t = element_type_t<T>;
-    using i_t   = as_integer_t<T>;
+    using i_t   = as_integer_t<T, signed>;
     using shf_t = detail::conditional_t<simd_value<T>, T, elt_t>;
 
     auto  bb   = convert(b, as<element_type_t<i_t>>{});
-    auto  ik   = bb + maxexponent(as<shf_t>());
+    i_t   ik   = bb + maxexponent(as<shf_t>());
           ik <<= nbmantissabits(as<shf_t>());
     return a * bit_cast(ik, as<as_wide_as_t<T,U>>());
   }
