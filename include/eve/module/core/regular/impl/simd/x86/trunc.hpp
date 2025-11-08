ff490f1f8b8631733bb6acde7b4e7307ce4c205b
jtlap <jtlapreste@gmail.com>
2025-11-08 08:35:42 +0100

    typo

--- include/eve/module/core/regular/impl/simd/x86/trunc.hpp
+++ include/eve/module/core/regular/impl/simd/x86/trunc.hpp
@@ -27,7 +27,7 @@ namespace eve::detail
     constexpr auto c = categorize<wide<T, N>>();
     if  constexpr (match(c, category::float16))
     {
-      return trunc.behavior(cpu_{}, o, a);
+      return trunc.behavior(cpu_{}, o, a0);
     }
     else if  constexpr(!O::contains(almost))
     {
