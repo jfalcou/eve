d8e922e8f9c842d4c4722e18b3f4d4b3d659c0ba
SadiinsoSnowfall <sadiinso@gmail.com>
2025-11-08 09:38:19 +0100

    optimization for non-AVX512-FP16 x86 targets

--- include/eve/module/core/regular/impl/simd/x86/is_nez.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_nez.hpp
@@ -16,7 +16,7 @@ namespace eve::detail
 {
   template<floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE logical<wide<T, N>> is_nez_(EVE_REQUIRES(avx512_),
-                                              O          const &,
+                                              O          const &opts,
                                               wide<T, N> const &a) noexcept
   requires x86_abi<abi_t<T, N>>
   {
@@ -34,7 +34,7 @@ namespace eve::detail
     else if constexpr( c == category::float32x4 ) return ~s_t {_mm_fpclass_ps_mask(a, f)};
     else if constexpr (match(c, category::float16))
     {
-      if      constexpr(!detail::supports_fp16_vector_ops) return apply_fp16_as_fp32(is_nez, a);
+      if      constexpr(!detail::supports_fp16_vector_ops) return is_nez.behavior(cpu_{}, opts, a);
       else if constexpr( c == category::float16x32 )       return ~s_t {_mm512_fpclass_ph_mask(a, f)};
       else if constexpr( c == category::float16x16 )       return ~s_t {_mm256_fpclass_ph_mask(a, f)};
       else if constexpr( c == category::float16x8  )       return ~s_t {_mm_fpclass_ph_mask(a, f)};
