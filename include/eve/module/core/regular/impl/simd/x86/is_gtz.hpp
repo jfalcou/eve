220a88c81df729395ccbd71d4aa943187374a879
SadiinsoSnowfall <sadiinso@gmail.com>
2025-11-11 12:20:33 +0100

    fix unused param

--- include/eve/module/core/regular/impl/simd/x86/is_gtz.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_gtz.hpp
@@ -16,7 +16,7 @@ namespace eve::detail
 {
   template<floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE logical<wide<T, N>> is_gtz_(EVE_REQUIRES(avx512_),
-                                              O          const &opts,
+                                              O          const &,
                                               wide<T, N> const &a) noexcept
   requires x86_abi<abi_t<T, N>>
   {
--- include/eve/module/core/regular/impl/simd/x86/is_lez.hpp
+++ include/eve/module/core/regular/impl/simd/x86/is_lez.hpp
@@ -17,7 +17,7 @@ namespace eve::detail
 {
   template<floating_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE logical<wide<T, N>> is_lez_(EVE_REQUIRES(avx512_),
-                                              O          const &opts,
+                                              O          const &,
                                               wide<T, N> const &a) noexcept
   requires x86_abi<abi_t<T, N>>
   {
