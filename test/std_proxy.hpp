2a42eb1e1fff1e42f4a0bae4faeff15dbc67705b
SadiinsoSnowfall <sadiinso@gmail.com>
2025-11-13 17:11:55 +0100

    fix std_proxy

--- test/std_proxy.hpp
+++ test/std_proxy.hpp
@@ -21,34 +21,34 @@
 template <typename T>
 constexpr T std_floor(T x)
 {
-  if constexpr (std::same_as<T, eve::float16_t>) return std::floor(static_cast<float>(x));
+  if constexpr (std::same_as<T, eve::float16_t>) return static_cast<eve::float16_t>(std::floor(static_cast<float>(x)));
   else                                           return std::floor(x);
 }
 
 template <typename T>
 constexpr T std_ceil(T x)
 {
-  if constexpr (std::same_as<T, eve::float16_t>) return std::ceil(static_cast<float>(x));
+  if constexpr (std::same_as<T, eve::float16_t>) return static_cast<eve::float16_t>(std::ceil(static_cast<float>(x)));
   else                                           return std::ceil(x);
 }
 
 template <typename T>
 constexpr T std_trunc(T x)
 {
-  if constexpr (std::same_as<T, eve::float16_t>) return std::trunc(static_cast<float>(x));
+  if constexpr (std::same_as<T, eve::float16_t>) return static_cast<eve::float16_t>(std::trunc(static_cast<float>(x)));
   else                                           return std::trunc(x);
 }
 
 template <typename T>
 constexpr T std_nearbyint(T x)
 {
-  if constexpr (std::same_as<T, eve::float16_t>) return std::nearbyint(static_cast<float>(x));
+  if constexpr (std::same_as<T, eve::float16_t>) return static_cast<eve::float16_t>(std::nearbyint(static_cast<float>(x)));
   else                                           return std::nearbyint(x);
 }
 
 template <typename T>
 constexpr T std_abs(T x)
 {
-  if constexpr (std::same_as<T, eve::float16_t>) return std::abs(static_cast<float>(x));
+  if constexpr (std::same_as<T, eve::float16_t>) return static_cast<eve::float16_t>(std::abs(static_cast<float>(x)));
   else                                           return std::abs(x);
 }
