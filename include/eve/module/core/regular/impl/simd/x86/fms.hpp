17b3031da67bdaaf712656a6085013b4ef76511d
jtlap <jtlapreste@gmail.com>
2025-11-15 16:19:14 +0100

    fma fms

--- include/eve/module/core/regular/impl/simd/x86/fms.hpp
+++ include/eve/module/core/regular/impl/simd/x86/fms.hpp
@@ -66,12 +66,20 @@ namespace eve::detail
     {
       if      constexpr( cat == category::float64x8 )  return _mm512_fmsub_pd(a, b, c);
       else if constexpr( cat == category::float32x16 ) return _mm512_fmsub_ps(a, b, c);
+      else  if constexpr ( match(cat, category::float16))
+      {
+        if      constexpr (!detail::supports_fp16_vector_ops) return apply_fp16_as_fp32(fms, a, b, c);
+        else if constexpr (cat == category::float16x32)        return _mm512_fmsub_ph(a, b, c);
+        else if constexpr (cat == category::float16x16)        return _mm256_fmsub_ph(a, b, c);
+        else if constexpr (cat == category::float16x8)         return _mm_fmsub_ph(a, b, c);
+      }
       else if constexpr( supports_fma3 )
       {
         if      constexpr( cat == category::float64x4 ) return _mm256_fmsub_pd(a, b, c);
         else if constexpr( cat == category::float64x2 ) return _mm_fmsub_pd(a, b, c);
         else if constexpr( cat == category::float32x8 ) return _mm256_fmsub_ps(a, b, c);
         else if constexpr( cat == category::float32x4 ) return _mm_fmsub_ps(a, b, c);
+        else return fms.behavior(cpu_{}, opts, a, b, c);
       }
       else return fma(a, b, -c);
     }
@@ -130,6 +138,13 @@ namespace eve::detail
       else if constexpr( cx == category::float64x4 ) return _mm256_mask_fmsub_pd(v, m, w, x);
       else if constexpr( cx == category::float32x4 ) return _mm_mask_fmsub_ps(v, m, w, x);
       else if constexpr( cx == category::float64x2 ) return _mm_mask_fmsub_pd(v, m, w, x);
+      else if constexpr ( match(cx, category::float16))
+      {
+        if      constexpr (!detail::supports_fp16_vector_ops) return apply_fp16_as_fp32_masked(sub, v, m, w, x);
+        else if constexpr (cx == category::float16x32)        return _mm512_mask_fmsub_ph(v, m, w, x);
+        else if constexpr (cx == category::float16x16)        return _mm256_mask_fmsub_ph(v, m, w, x);
+        else if constexpr (cx == category::float16x8)         return _mm_mask_fmsub_ph(v, m, w, x);
+      }
       // No rounding issue with integers, so we just mask over regular FMS
       else                                          return if_else(mask, eve::fms(v, w, x), v);
     }
