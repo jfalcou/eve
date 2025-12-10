bfde0e0412c60a9d880e0cd938389f52548a72c0
jtlap <jtlapreste@gmail.com>
2025-12-10 15:06:10 +0100

    expx2 zxpmx2

--- test/unit/module/math/expmx2.cpp
+++ test/unit/module/math/expmx2.cpp
@@ -46,7 +46,7 @@ TTS_CASE_WITH("Check behavior of exp on wide",
                     [](auto e) -> v_t
                     {
                       long double le = e;
-                      return std_exp(-le * le);
+                      return eve::convert(std_exp(-le * le), eve::as<v_t>());
                     },
                     a0),
                 200);
--- test/unit/module/math/expx2.cpp
+++ test/unit/module/math/expx2.cpp
@@ -45,7 +45,7 @@ TTS_CASE_WITH("Check behavior of exp on wide",
                     [](auto e) -> v_t
                     {
                       long double le = e;
-                      return std_exp(le * le);
+                      return eve::convertstd_exp(le * le), eve::as<v_t>());
                     },
                     a0),
                 200);
