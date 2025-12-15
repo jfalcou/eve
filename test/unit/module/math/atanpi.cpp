e88564bb96b7abfd03066e7fee6334f667693d5b
jtlap <jtlapreste@gmail.com>
2025-12-15 15:12:46 +0100

    atanxxx

--- test/unit/module/math/atanpi.cpp
+++ test/unit/module/math/atanpi.cpp
@@ -6,7 +6,7 @@
 **/
 //==================================================================================================
 #include "test.hpp"
-
+#include "std_proxy.hpp"
 #include <eve/module/core.hpp>
 #include <eve/module/math.hpp>
 
@@ -15,7 +15,7 @@
 //==================================================================================================
 // Types tests
 //==================================================================================================
-TTS_CASE_TPL("Check return types of atanpi", eve::test::simd::ieee_reals)
+TTS_CASE_TPL("Check return types of atanpi", eve::test::simd::ieee_reals_wf16)
 <typename T>(tts::type<T>)
 {
   using v_t = eve::element_type_t<T>;
@@ -28,16 +28,16 @@ TTS_CASE_TPL("Check return types of atanpi", eve::test::simd::ieee_reals)
 // atanpi  tests
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of atanpi on wide",
-              eve::test::simd::ieee_reals,
+              eve::test::simd::ieee_reals_wf16,
               tts::generate(tts::randoms(eve::valmin, eve::valmax), tts::randoms(-1.0, 1.0)))
 <typename T>(T const& a0, T const& a1)
 {
   using v_t = eve::element_type_t<T>;
 
   TTS_ULP_EQUAL(
-      eve::atanpi(a0), tts::map([](auto e) -> v_t { return eve::radinpi(std::atan(e)); }, a0), 2);
+    eve::atanpi(a0), tts::map([](auto e) -> v_t { return static_cast<v_t>(eve::radinpi(std_atan(e))); }, a0), 2);
   TTS_ULP_EQUAL(
-      eve::atanpi(a1), tts::map([](auto e) -> v_t { return eve::radinpi(std::atan(e)); }, a1), 2);
+    eve::atanpi(a1), tts::map([](auto e) -> v_t { return static_cast<v_t>(eve::radinpi(std_atan(e))); }, a1), 2);
 };
 
 
@@ -45,10 +45,10 @@ TTS_CASE_WITH("Check behavior of atanpi on wide",
 // Tests for masked atanpi
 //==================================================================================================
 TTS_CASE_WITH("Check behavior of eve::masked(eve::atanpi)(eve::wide)",
-              eve::test::simd::ieee_reals,
+              eve::test::simd::ieee_reals_wf16,
               tts::generate(tts::randoms(eve::valmin, eve::valmax),
               tts::logicals(0, 3)))
-<typename T, typename M>(T const& a0, 
+<typename T, typename M>(T const& a0,
                          M const& mask)
 {
   TTS_IEEE_EQUAL(eve::atanpi[mask](a0),
