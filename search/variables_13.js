57c019079c5230f9681ea51b330f4cd6f9dd698e
jfalcou <jfalcou@users.noreply.github.com>
2026-02-19 19:17:13 +0000

    deploy: 1c96a03a7c8fce907fbcd203867e1b83ec7ba55c

--- search/variables_13.js
+++ search/variables_13.js
@@ -1,16 +1,14 @@
 var searchData=
 [
   ['tan_0',['tan',['../group__math__trig_gaf92ef4ab7d8bd5a527db4d94bcebbdf7.html#gaf92ef4ab7d8bd5a527db4d94bcebbdf7',1,'eve']]],
-  ['tand_1',['tand',['../group__math__trig_ga768026667ea866e5968189c2dc7855a4.html#ga768026667ea866e5968189c2dc7855a4',1,'eve']]],
-  ['tanh_2',['tanh',['../group__math__hyper_ga32818e7959c1a884302b9f05ba8b8797.html#ga32818e7959c1a884302b9f05ba8b8797',1,'eve']]],
-  ['tanpi_3',['tanpi',['../group__math__trig_ga5eae3a44e735470ea64d1dca960eae25.html#ga5eae3a44e735470ea64d1dca960eae25',1,'eve']]],
-  ['tchebytchev_4',['tchebytchev',['../group__polynomial_ga5262bc83278d307e1c920cc1cb608838.html#ga5262bc83278d307e1c920cc1cb608838',1,'eve']]],
-  ['tgamma_5',['tgamma',['../group__special_gab6f03be6923b910c56c4b49af92e5ad5.html#gab6f03be6923b910c56c4b49af92e5ad5',1,'eve']]],
-  ['three_5ffma_6',['three_fma',['../group__core__accuracy_ga8af2528a0e1737b2cf9729e99950aa5b.html#ga8af2528a0e1737b2cf9729e99950aa5b',1,'eve']]],
-  ['trigamma_7',['trigamma',['../group__special_ga3bdbbd165da55f74928f2b06709234a2.html#ga3bdbbd165da55f74928f2b06709234a2',1,'eve']]],
-  ['true_5f_8',['true_',['../group__core__constants_gac1255364700394f5b94065c935208f42.html#gac1255364700394f5b94065c935208f42',1,'eve']]],
-  ['trunc_9',['trunc',['../group__core__arithmetic_gafde17ae255fef25fc9015dabc599c701.html#gafde17ae255fef25fc9015dabc599c701',1,'eve']]],
-  ['two_5fadd_10',['two_add',['../group__core__accuracy_ga300f96ea06042ecfd42f3e3e17787882.html#ga300f96ea06042ecfd42f3e3e17787882',1,'eve']]],
-  ['two_5fprod_11',['two_prod',['../group__core__accuracy_ga5ef9f1355859ff017142b92d4eb5516f.html#ga5ef9f1355859ff017142b92d4eb5516f',1,'eve']]],
-  ['twotonmb_12',['twotonmb',['../group__core__constants_gacefed0e3b9ee16507cf649601408be2a.html#gacefed0e3b9ee16507cf649601408be2a',1,'eve']]]
+  ['tanh_1',['tanh',['../group__math__hyper_ga32818e7959c1a884302b9f05ba8b8797.html#ga32818e7959c1a884302b9f05ba8b8797',1,'eve']]],
+  ['tchebytchev_2',['tchebytchev',['../group__polynomial_ga5262bc83278d307e1c920cc1cb608838.html#ga5262bc83278d307e1c920cc1cb608838',1,'eve']]],
+  ['tgamma_3',['tgamma',['../group__special_gab6f03be6923b910c56c4b49af92e5ad5.html#gab6f03be6923b910c56c4b49af92e5ad5',1,'eve']]],
+  ['three_5ffma_4',['three_fma',['../group__core__accuracy_ga8af2528a0e1737b2cf9729e99950aa5b.html#ga8af2528a0e1737b2cf9729e99950aa5b',1,'eve']]],
+  ['trigamma_5',['trigamma',['../group__special_ga3bdbbd165da55f74928f2b06709234a2.html#ga3bdbbd165da55f74928f2b06709234a2',1,'eve']]],
+  ['true_5f_6',['true_',['../group__core__constants_gac1255364700394f5b94065c935208f42.html#gac1255364700394f5b94065c935208f42',1,'eve']]],
+  ['trunc_7',['trunc',['../group__core__arithmetic_gafde17ae255fef25fc9015dabc599c701.html#gafde17ae255fef25fc9015dabc599c701',1,'eve']]],
+  ['two_5fadd_8',['two_add',['../group__core__accuracy_ga300f96ea06042ecfd42f3e3e17787882.html#ga300f96ea06042ecfd42f3e3e17787882',1,'eve']]],
+  ['two_5fprod_9',['two_prod',['../group__core__accuracy_ga5ef9f1355859ff017142b92d4eb5516f.html#ga5ef9f1355859ff017142b92d4eb5516f',1,'eve']]],
+  ['twotonmb_10',['twotonmb',['../group__core__constants_gacefed0e3b9ee16507cf649601408be2a.html#gacefed0e3b9ee16507cf649601408be2a',1,'eve']]]
 ];
