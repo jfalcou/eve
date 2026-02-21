676a5385a5522180b6ad8d10ad6884acfd488097
jtlap <jtlapreste@gmail.com>
2026-02-21 09:57:00 +0100

    two_sqrt_approx

--- include/eve/module/core/regular/two_sqrt_approx.hpp
+++ include/eve/module/core/regular/two_sqrt_approx.hpp
@@ -19,12 +19,10 @@ namespace eve
   template<typename Options>
   struct two_sqrt_approx_t : elementwise_callable<two_sqrt_approx_t, Options>
   {
-    template<eve::floating_value T, eve::floating_value U>
-    requires(eve::same_lanes_or_scalar<T, U>)
-      constexpr EVE_FORCEINLINE
-    zipped<common_value_t<T,U>,common_value_t<T,U>> operator()(T a, U b) const
+    template<eve::floating_value T> constexpr EVE_FORCEINLINE
+    zipped<T, T> operator()(T a) const
     {
-      return EVE_DISPATCH_CALL(a,b);
+      return EVE_DISPATCH_CALL(a);
     }
 
     EVE_CALLABLE_OBJECT(two_sqrt_approx_t, two_sqrt_approx_);
@@ -35,7 +33,7 @@ namespace eve
 //! @{
 //!   @var two_sqrt_approx
 //!   @brief Computes the [elementwise](@ref glossary_elementwise)
-//!   pair of  sqrtision and error,
+//!   pair of square root and error,
 //!
 //!   @groupheader{Header file}
 //!
@@ -48,20 +46,20 @@ namespace eve
 //!   @code
 //!   namespace eve
 //!   {
-//!      constexpr auto two_sqrt_approx(floating_value auto x, floating_value auto y) noexcept;
+//!      constexpr auto two_sqrt_approx(floating_value auto x) noexcept;
 //!   }
 //!   @endcode
 //!
 //!   **Parameters**
 //!
-//!     * `x`, `y`:  [floating arguments](@ref eve::floating_value).
+//!     * `x`:  [floating arguments](@ref eve::floating_value).
 //!
 //!   **Return value**
 //!
 //!     Computes [elementwise](@ref glossary_elementwise) a pair of values `[a,e]` such that:
-//!       * `a` is `x*y`
-//!       * `e` is a value such that `a`\f$\oplus\f$`e` is an approximation in doubled precision to `x`\f$\osqrt\f$`y`,
-//!          where \f$\oplus\f$ (resp. \f$\osqrt\f$) adds (resp. sqrtides) its two parameters with
+//!       * `a` is `eve::sqrt(x)`
+//!       * `e` is a value such that `a`\f$\oplus\f$`e` is an approximation in doubled precision to `\f$\sqrt\f$`x`,
+//!          where \f$\oplus\f$ (resp. \f$\sqrt\f$) adds (resp. take the square roots of) its parameters with
 //!          infinite precision.
 //!
 //!  @groupheader{Example}
@@ -75,7 +73,7 @@ namespace eve
   namespace detail
   {
     template<typename T, callable_options O>
-    constexpr EVE_FORCEINLINE auto two_sqrt_approx_(EVE_REQUIRES(cpu_), O const&, T x, T y)
+    constexpr EVE_FORCEINLINE auto two_sqrt_approx_(EVE_REQUIRES(cpu_), O const&, T x)
     {
       auto r0 = eve::sqrt(x);
       auto e0 = if_else(is_not_finite(r0), zero, eve::fma[pedantic](-r0, r0, x)/(r0+r0));
--- test/doc/core/two_sqrt_approx.cpp
+++ test/doc/core/two_sqrt_approx.cpp
@@ -1,85 +1,21 @@
-// //================================================================================================
-// /*
-//   EVE - Expressive Vector Engine
-//   Copyright : EVE Project Contributors
-//   SPDX-License-Identifier: BSL-1.0
-// */
-// //================================================================================================
-// #pragma once
+// revision 1
+#include <eve/module/core.hpp>
+#include <iostream>
+#include <iomanip>
 
-// #include <eve/arch.hpp>
-// #include <eve/traits/overload.hpp>
-// #include <eve/module/core/decorator/core.hpp>
-// #include <eve/module/core/regular/zip.hpp>
-// #include <eve/module/core/regular/fms.hpp>
-// #include <iostream>
+int main()
+{
+  eve::wide wf0 = {3.0f, 2.5f, 32.7f, 1.0f};
 
-// namespace eve
-// {
-//   template<typename Options>
-//   struct two_sqrt_approx_t : elementwise_callable<two_sqrt_approx_t, Options>
-//   {
-//     template<eve::floating_value T, eve::floating_value U>
-//     requires(eve::same_lanes_or_scalar<T, U>)
-//       constexpr EVE_FORCEINLINE
-//     zipped<common_value_t<T,U>,common_value_t<T,U>> operator()(T a, U b) const
-//     {
-//       return EVE_DISPATCH_CALL(a,b);
-//     }
+  std::cout << "<- wf0                = " << wf0 << "\n";
 
-//     EVE_CALLABLE_OBJECT(two_sqrt_approx_t, two_sqrt_approx_);
-//   };
+  std::cout << "-> two_sqrt_approx(wf0) = " << eve::two_sqrt_approx(wf0) << "\n";
 
-// //================================================================================================
-// //! @addtogroup core_accuracy
-// //! @{
-// //!   @var two_sqrt_approx
-// //!   @brief Computes the [elementwise](@ref glossary_elementwise)
-// //!   pair of  sqrtision and error,
-// //!
-// //!   @groupheader{Header file}
-// //!
-// //!   @code
-// //!   #include <eve/module/core.hpp>
-// //!   @endcode
-// //!
-// //!   @groupheader{Callable Signatures}
-// //!
-// //!   @code
-// //!   namespace eve
-// //!   {
-// //!      constexpr auto two_sqrt_approx(floating_value auto x, floating_value auto y) noexcept;
-// //!   }
-// //!   @endcode
-// //!
-// //!   **Parameters**
-// //!
-// //!     * `x`, `y`:  [floating arguments](@ref eve::floating_value).
-// //!
-// //!   **Return value**
-// //!
-// //!     Computes [elementwise](@ref glossary_elementwise) a pair of values `[a,e]` such that:
-// //!       * `a` is `x*y`
-// //!       * `e` is a value such that `a`\f$\oplus\f$`e` is an approximation in doubled precision to `x`\f$\osqrt\f$`y`,
-// //!          where \f$\oplus\f$ (resp. \f$\osqrt\f$) adds (resp. sqrtides) its two parameters with
-// //!          infinite precision.
-// //!
-// //!  @groupheader{Example}
-// //!  @godbolt{doc/core/two_sqrt_approx.cpp}
-// //================================================================================================
-//   inline constexpr auto two_sqrt_approx = functor<two_sqrt_approx_t>;
-// //================================================================================================
-// //! @}
-// //================================================================================================
-
-//   namespace detail
-//   {
-//     template<typename T, callable_options O>
-//     constexpr EVE_FORCEINLINE auto two_sqrt_approx_(EVE_REQUIRES(cpu_), O const&, T x, T y)
-//     {
-//       auto r0 = eve::sqrt(x);
-//       auto e0 = if_else(is_not_finite(r0), zero, eve::fma[pedantic](-s, s, x)/(s+s)
-//       return eve::zip(r0,e0);
-//     }
-//   }
-// }
+  auto [d, e] = eve::two_sqrt_approx(3.0f);
+  std::cout << std::setprecision(15);
+  std::cout << double(d) << " + " << double(e) << std::endl;
+  std::cout << double(e)+double(d) << std::endl;
+  std::cout << eve::sqrt(3.0) << std::endl;
+  std::cout << eve::sqrt(3.0f) <<  std::endl;
+  std::cout << eve::sqrt(3.0) - (double(e)+double(d))<< std::endl;
+}
