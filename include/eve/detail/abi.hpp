d0642006ca8320bea57e1b900a3a2a19b1d1e9f1
Nxirda <93188097+Nxirda@users.noreply.github.com>
2026-02-25 23:18:58 +0100

    Ensure wrong C++ version is fatal (#2316)
    Co-authored-by: Nxirda <Nxirda@nothing.com>

--- include/eve/detail/abi.hpp
+++ include/eve/detail/abi.hpp
@@ -9,11 +9,13 @@
 
 #if defined(_MSC_VER)
 #    if _MSVC_LANG < 202002L
-#        error "Eve requires C++20 or higher. Use /std:c++20 or higher to enable C++20 features."
+#        error "Eve C++ version error"
+#        include "Eve requires C++20 or higher. Use /std:c++20 or higher to enable C++20 features."
 #    endif
 #else
 #    if __cplusplus < 202002L
-#        error "Eve requires C++20 or higher. Use -std=c++20 or higher to enable C++20 features."
+#        error "Eve C++ version error"
+#        include "Eve requires C++20 or higher. Use -std=c++20 or higher to enable C++20 features."
 #    endif
 #endif
 
