d3264f24921525757b0eaf58386dcfe8cd28de5d
Joel Falcou <joel.falcou@lri.fr>
2026-03-09 13:32:03 +0100

    One day, one day, I will push everything at once

--- cmake/toolchain/gcc15.x86.cmake
+++ cmake/toolchain/gcc15.x86.cmake
@@ -3,7 +3,7 @@
 ##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
-set(CMAKE_C_COMPILER    gcc-14  )
-set(CMAKE_CXX_COMPILER  g++-14  )
+set(CMAKE_C_COMPILER    gcc-15  )
+set(CMAKE_CXX_COMPILER  g++-15  )
 
 set(CMAKE_CXX_FLAGS     "-Wno-psabi -DEVE_NO_FORCEINLINE ${EVE_OPTIONS}" )
