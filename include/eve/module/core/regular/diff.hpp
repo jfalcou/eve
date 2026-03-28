92dd33d03a544418e40287f395d64c0da12c1a62
jtlap <jtlapreste@gmail.com>
2026-03-28 13:17:34 +0100

    diff only tuples  widen ok

--- include/eve/module/core/regular/diff.hpp
+++ include/eve/module/core/regular/diff.hpp
@@ -11,23 +11,24 @@
 #include <eve/traits/overload.hpp>
 #include <eve/module/core/decorator/core.hpp>
 #include <eve/traits/updown.hpp>
+#include <iostream>
 
 namespace eve
 {
 
   template<typename Options>
-  struct diff_t : callable<diff_t, Options>
+  struct diff_t : callable<diff_t, Options, widen_option>
   {
 
     template<eve::non_empty_product_type Tup>
     requires(eve::same_lanes_or_scalar_tuple<Tup>)
-      EVE_FORCEINLINE kumi::result::iota_t<kumi::size_v<Tup>-1, kumi::apply_traits_t<eve::common_value, Tup>> constexpr
+      EVE_FORCEINLINE kumi::result::iota_t<kumi::size_v<Tup>-1, eve::upgrade_if_t<Options, kumi::apply_traits_t<eve::common_value, Tup>>> constexpr
     operator()(Tup const& t) const noexcept
     { return EVE_DISPATCH_CALL(t); }
 
     template<size_t N, eve::non_empty_product_type Tup>
     requires(eve::same_lanes_or_scalar_tuple<Tup>)
-      EVE_FORCEINLINE constexpr  kumi::result::iota_t<kumi::size_v<Tup>-N, kumi::apply_traits_t<eve::common_value, Tup>>
+      EVE_FORCEINLINE kumi::result::iota_t<kumi::size_v<Tup>-N, eve::upgrade_if_t<Options, kumi::apply_traits_t<eve::common_value, Tup>>> constexpr
     operator()(kumi::index_t<N>, Tup t) const noexcept
     { return EVE_DISPATCH_CALL(kumi::index_t<N>{}, t); }
 
@@ -84,16 +85,32 @@ namespace eve
     template<eve::non_empty_product_type PT , callable_options O>
     EVE_NOINLINE constexpr auto
     diff_(EVE_REQUIRES(cpu_), O const &, PT x) noexcept
+    requires(!O::contains(widen))
     {
+      //std::cout << "!widen" << std::endl;
       using r_t = kumi::apply_traits_t<common_value, PT>;
       auto xx =  kumi::map([](auto m){ return r_t(m); }, x);
+      return kumi::map(eve::sub, kumi::pop_back(xx), kumi::pop_front(xx));
+    }
+
+    template<eve::non_empty_product_type PT , callable_options O>
+    EVE_NOINLINE constexpr auto
+    diff_(EVE_REQUIRES(cpu_), O const &, PT x) noexcept
+    requires(O::contains(widen))
+    {
+      //std::cout << "widen" << std::endl;
+      using r_t = kumi::apply_traits_t<common_value, PT>;
+      using ur_t = eve::upgrade_t<r_t>;
+      auto xx =  kumi::map([](auto m){ return ur_t(m); }, x);
       return kumi::map(eve::sub, kumi::pop_back(xx), kumi::pop_front(xx));
     }
 
     template<size_t N, eve::non_empty_product_type PT , callable_options O>
     EVE_NOINLINE constexpr auto
     diff_(EVE_REQUIRES(cpu_), O const &, kumi::index_t<N>, PT & x) noexcept
+    requires(!O::contains(widen))
     {
+      //std::cout << "!widen index" << std::endl;
       using r_t = kumi::apply_traits_t<common_value, PT>;
       auto xx =  kumi::map([](auto m){ return r_t(m); }, x);
       if constexpr(N >= kumi::size_v<PT>)
@@ -105,6 +122,18 @@ namespace eve
         if constexpr(N == 1)             return diff(xx);
         else                             return diff(kumi::index_t<N-1>{}, diff(xx));
       }
+    }
+
+    template<size_t N, eve::non_empty_product_type PT , callable_options O>
+    EVE_NOINLINE constexpr auto
+    diff_(EVE_REQUIRES(cpu_), O const & o, kumi::index_t<N> n, PT & x) noexcept
+    requires(O::contains(widen))
+    {
+      //std::cout << "widen index" << std::endl;
+      using r_t = kumi::apply_traits_t<common_value, PT>;
+      using ur_t = eve::upgrade_t<r_t>;
+      auto xx =  kumi::map([](auto m){ return ur_t(m); }, x);
+      return diff[o.drop(widen)](n, xx);
     }
   }
 }
--- test/doc/core/diff.cpp
+++ test/doc/core/diff.cpp
@@ -13,9 +13,9 @@ EVE_FORCEINLINE constexpr  kumi::result::iota_t<sizeof...(Ts)-N, eve::common_val
 
 int main()
 {
-
+  std::cout << std::hexfloat << std::endl;
   {
-    kumi::tuple tt{1.5000, 2.2500, 3.3750, 5.0625, 7.5938, 11.3906, 17.0859, 25.6289};
+    kumi::tuple tt{1.5000e-5, 2.2500, 3.3750, 5.0625, 7.0, 11.0, 17.0, 25.0};
     std::cout << " < - tt                        = " << tt << std::endl;
     std::cout << " -> diff(index_t<0>, tt)       = " << eve::diff(kumi::index_t<0>{}, tt) << std::endl;
     std::cout << " -> diff(tt)                   = " << eve::diff(tt) << std::endl;
@@ -23,10 +23,11 @@ int main()
     std::cout << " -> diff(index_t<2>, tt)       = " << eve::diff(kumi::index_t<2>{}, tt) << std::endl;
     std::cout << " -> diff(index_t<7>, tt)       = " << eve::diff(kumi::index_t<7>{}, tt) << std::endl;
     std::cout << " -> diff(index_t<8>, tt)       = " << eve::diff(kumi::index_t<8>{}, tt) << std::endl;
+    std::cout << "float"  << 8*sizeof(kumi::get<0>(eve::diff(kumi::index_t<1>{}, tt))) << std::endl;
 
   }
   {
-    kumi::tuple tt{1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.5938f, 11.3906f, 17.0859f, 25.6289f};
+    kumi::tuple tt{1.5000e-5f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f};
     std::cout << " < - tt                        = " << tt << std::endl;
     std::cout << " -> diff(index_t<0>, tt)       = " << eve::diff(kumi::index_t<0>{}, tt) << std::endl;
     std::cout << " -> diff(tt)                   = " << eve::diff(tt) << std::endl;
@@ -34,11 +35,25 @@ int main()
     std::cout << " -> diff(index_t<2>, tt)       = " << eve::diff(kumi::index_t<2>{}, tt) << std::endl;
     std::cout << " -> diff(index_t<7>, tt)       = " << eve::diff(kumi::index_t<7>{}, tt) << std::endl;
     std::cout << " -> diff(index_t<8>, tt)       = " << eve::diff(kumi::index_t<8>{}, tt) << std::endl;
+    std::cout << "float"  << 8*sizeof(kumi::get<0>(eve::diff(kumi::index_t<1>{}, tt))) << std::endl;
+  }
+  using eve::widen;
+  {
+    kumi::tuple tt{1.5000e-5f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f};
+    std::cout << " < - tt                        = " << tt << std::endl;
+    std::cout << " -> diff[widen](index_t<0>, tt)       = " << eve::diff[widen](kumi::index_t<0>{}, tt) << std::endl;
+    std::cout << " -> diff[widen](tt)                   = " << eve::diff[widen](tt) << std::endl;
+    std::cout << " -> diff[widen](index_t<1>, tt)       = " << eve::diff[widen](kumi::index_t<1>{}, tt) << std::endl;
+    std::cout << " -> diff[widen](index_t<2>, tt)       = " << eve::diff[widen](kumi::index_t<2>{}, tt) << std::endl;
+    std::cout << " -> diff[widen](index_t<7>, tt)       = " << eve::diff[widen](kumi::index_t<7>{}, tt) << std::endl;
+    std::cout << " -> diff[widen](index_t<8>, tt)       = " << eve::diff[widen](kumi::index_t<8>{}, tt) << std::endl;
+    std::cout << "float"  << 8*sizeof(kumi::get<0>(eve::diff[widen](kumi::index_t<1>{}, tt))) << std::endl;
+
   }
   {
-//    std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << mydiff(kumi::index_t<2>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.5938f, 11.3906f, 17.0859f, 25.6289f) << std::endl;
-//    std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << eve::diff(kumi::index_t<2>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.5938f, 11.3906f, 17.0859f, 25.6289f) << std::endl;
-//    std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << eve::diff(1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.5938f, 11.3906f, 17.0859f, 25.6289f) << std::endl;
-//    std::cout << " -> diff[eve::widen](index_t<8>, ...)       = " << eve::diff[eve::widen](kumi::index_t<8>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.5938f, 11.3906f, 17.0859f, 25.6289f) << std::endl;
+//   std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << mydiff(kumi::index_t<2>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f) << std::endl;
+//    std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << eve::diff(kumi::index_t<2>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f) << std::endl;
+//    std::cout << " -> diff[eve::widen](index_t<0>, ...)       = " << eve::diff(1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f) << std::endl;
+//    std::cout << " -> diff[eve::widen](index_t<8>, ...)       = " << eve::diff[eve::widen](kumi::index_t<8>{}, 1.5000f, 2.2500f, 3.3750f, 5.0625f, 7.0f, 11.0f, 17.0f, 25.0f) << std::endl;
   }
 }
