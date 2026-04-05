81e8532b680fd4efadd3c1ff635269e68cf35e76
jfalcou <jfalcou@users.noreply.github.com>
2026-04-05 15:19:17 +0000

    deploy: 67cf671eadc6739367d53d67b5187733a4e17186

--- search/variables_3.js
+++ search/variables_3.js
@@ -6,11 +6,12 @@ var searchData=
   ['deinterleave_5fgroups_3',['deinterleave_groups',['../group__core_ga5c2b47962e4523e5ca348a7e3034032b.html#ga5c2b47962e4523e5ca348a7e3034032b',1,'eve']]],
   ['deinterleave_5fgroups_5fshuffle_4',['deinterleave_groups_shuffle',['../group__core__simd_ga275315d7cc9b3cd8f5e063136d4990d1.html#ga275315d7cc9b3cd8f5e063136d4990d1',1,'eve']]],
   ['dense_5foutput_5',['dense_output',['../group__algo__traits_gab450c355e4ef74d7473519d8b5bdff24.html#gab450c355e4ef74d7473519d8b5bdff24',1,'eve::algo']]],
-  ['diff_5fof_5fprod_6',['diff_of_prod',['../group__core__accuracy_gab21bf1ebf628243f28a10f05ce66f820.html#gab21bf1ebf628243f28a10f05ce66f820',1,'eve']]],
-  ['digamma_7',['digamma',['../group__special_ga35bd17562d2457033901ba4a6a112808.html#ga35bd17562d2457033901ba4a6a112808',1,'eve']]],
-  ['dist_8',['dist',['../group__core__arithmetic_ga0278a079dfdeb50fcb31e27b3ac191f9.html#ga0278a079dfdeb50fcb31e27b3ac191f9',1,'eve']]],
-  ['div_9',['div',['../group__core__arithmetic_gaf14eacd69db8a64fecf63346475c37fb.html#gaf14eacd69db8a64fecf63346475c37fb',1,'eve']]],
-  ['div_5f180_10',['div_180',['../group__math__trig_gae38f095b02721ec8d4e23bf613a3ddba.html#gae38f095b02721ec8d4e23bf613a3ddba',1,'eve']]],
-  ['divisible_5fby_5fcardinal_11',['divisible_by_cardinal',['../group__algo__traits_ga01b72d95d26f28c2c8715af53bef8f53.html#ga01b72d95d26f28c2c8715af53bef8f53',1,'eve::algo']]],
-  ['dot_12',['dot',['../group__core__arithmetic_ga9629bc8a3ce2a95e45bd49e118605156.html#ga9629bc8a3ce2a95e45bd49e118605156',1,'eve']]]
+  ['diff_6',['diff',['../group__core__arithmetic_ga057eabc3676fe1e9370bd201a21e97d7.html#ga057eabc3676fe1e9370bd201a21e97d7',1,'eve']]],
+  ['diff_5fof_5fprod_7',['diff_of_prod',['../group__core__accuracy_gab21bf1ebf628243f28a10f05ce66f820.html#gab21bf1ebf628243f28a10f05ce66f820',1,'eve']]],
+  ['digamma_8',['digamma',['../group__special_ga35bd17562d2457033901ba4a6a112808.html#ga35bd17562d2457033901ba4a6a112808',1,'eve']]],
+  ['dist_9',['dist',['../group__core__arithmetic_ga0278a079dfdeb50fcb31e27b3ac191f9.html#ga0278a079dfdeb50fcb31e27b3ac191f9',1,'eve']]],
+  ['div_10',['div',['../group__core__arithmetic_gaf14eacd69db8a64fecf63346475c37fb.html#gaf14eacd69db8a64fecf63346475c37fb',1,'eve']]],
+  ['div_5f180_11',['div_180',['../group__math__trig_gae38f095b02721ec8d4e23bf613a3ddba.html#gae38f095b02721ec8d4e23bf613a3ddba',1,'eve']]],
+  ['divisible_5fby_5fcardinal_12',['divisible_by_cardinal',['../group__algo__traits_ga01b72d95d26f28c2c8715af53bef8f53.html#ga01b72d95d26f28c2c8715af53bef8f53',1,'eve::algo']]],
+  ['dot_13',['dot',['../group__core__arithmetic_ga9629bc8a3ce2a95e45bd49e118605156.html#ga9629bc8a3ce2a95e45bd49e118605156',1,'eve']]]
 ];
