695d3e5b8dd510b28079ac5d38f1c6e9c3c86a18
SadiinsoSnowfall <sadiinso@gmail.com>
2026-04-07 14:22:08 +0200

    change map call

--- include/eve/traits/overload/impl/strict_elementwise.hpp
+++ include/eve/traits/overload/impl/strict_elementwise.hpp
@@ -40,14 +40,14 @@ namespace eve
     {
       constexpr bool has_implementation          = requires{ func_t::deferred_call(a, o, x, xs...); };
       constexpr bool has_emulated_implementation = requires{ func_t::deferred_call(emulated_{}, o, x, xs...); };
-      constexpr bool supports_map_no_conversion  = requires{ this->derived().map(x, xs...); };
+      constexpr bool supports_map_no_conversion  = requires{ this->map(x, xs...); };
       constexpr bool any_emulated                = (has_emulated_abi_v<T> || ... || has_emulated_abi_v<Ts>);
       constexpr bool any_aggregated              = (has_aggregated_abi_v<T> || ... || has_aggregated_abi_v<Ts>);
 
 
       if      constexpr(any_aggregated)                              return aggregate(this->derived(), x, xs...);
       else if constexpr(any_emulated && has_emulated_implementation) return func_t::deferred_call(emulated_{}, o, x, xs...);
-      else if constexpr(any_emulated && supports_map_no_conversion)  return this->derived().map(x, xs...);
+      else if constexpr(any_emulated && supports_map_no_conversion)  return this->map(x, xs...);
       else if constexpr(has_implementation)                          return func_t::deferred_call(a, o, x, xs...);
       else                                                           return detail::ignore{};
     }
