83e1dc65ad416005ba1d8345892e61dc58efebad
SadiinsoSnowfall <sadiinso@gmail.com>
2026-04-11 09:47:38 +0200

    sve fix

--- test/unit/regression/shuffle_down.cpp
+++ test/unit/regression/shuffle_down.cpp
@@ -12,8 +12,8 @@
 // regression test for selection of broadcast_group where Size < Cardinal
 TTS_CASE("Shuffle broadcast_group down select")
 {
-    eve::wide<char, eve::fixed<32>> w = [](auto i) { return i; };
-    eve::wide<char, eve::fixed<4>> res = eve::shuffle(w,eve::fix_pattern<4>(eve::as_pattern([](auto i, auto) { return i + 8; })));
-    eve::wide<char, eve::fixed<4>> ref{8, 9, 10, 11};
+    eve::wide<unsigned char, eve::fixed<32>> w = [](auto i) { return i; };
+    eve::wide<unsigned char, eve::fixed<4>> res = eve::shuffle(w,eve::fix_pattern<4>(eve::as_pattern([](auto i, auto) { return i + 8; })));
+    eve::wide<unsigned char, eve::fixed<4>> ref{8, 9, 10, 11};
     TTS_EQUAL(res, ref);
 };
