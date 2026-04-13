26ce5db74dcb6c1318c2ff740b6af5440f2f529b
jtlap <jtlapreste@gmail.com>
2026-04-13 19:48:20 +0200

    doc update

--- include/eve/module/core/regular/unfold.hpp
+++ include/eve/module/core/regular/unfold.hpp
@@ -20,40 +20,40 @@ namespace eve
     template<typename ...Ts>
     struct cardinal_sum
     {
-      static constexpr auto value = ((cardinal_v<Ts>) + ... + 0 ); 
+      static constexpr auto value = ((cardinal_v<Ts>) + ... + 0 );
       using type = std::integral_constant<std::size_t,value>;
     };
-    
+
     template<typename T>
     using return_type = eve::element_type_t<eve::upgrade_if_t<Options,T>>;
-    
+
     template<value... Ts>
     using result = kumi::result::fill_t<
       cardinal_sum<Ts...>::value
       , return_type<eve::common_value_t<Ts...>>>;
-    
+
     template<product_type T>
     using tuple_result = kumi::result::fill_t<
       kumi::apply_traits_t<cardinal_sum, T>::value
       , return_type<kumi::apply_traits_t<eve::common_value, T>>>;
-    
+
     template<value T, value ...Ts>
     EVE_FORCEINLINE constexpr result<T, Ts...>
     operator()(T t, Ts ... ts) const noexcept
     {
       return EVE_DISPATCH_CALL(t, ts...);
     }
-    
+
     template<non_empty_product_type T>
     EVE_FORCEINLINE constexpr tuple_result<T>
     operator()(T t) const noexcept
     {
       return EVE_DISPATCH_CALL(t);
     }
-    
+
     EVE_CALLABLE_OBJECT(unfold_t, unfold_);
-  }; 
-  
+  };
+
 //================================================================================================
 //! @addtogroup core_arithmetic
 //! @{
@@ -73,6 +73,7 @@ namespace eve
 //!   {
 //!      // Regular overloads
 //!      constexpr auto unfold(eve::value auto ... xs)                noexcept; // 1
+//!      constexpr auto unfold(non_empty_product_type tup)            noexcept; // 2
 //!
 //!      // Semantic options
 //!      constexpr auto unfold[widen](/*any of the above overloads*/) noexcept; // 2
@@ -83,11 +84,13 @@ namespace eve
 //!
 //!     * `x`: [value](@ref eve::value) arguments.
 //!     * `xs...`: [values](@ref eve::value) arguments.
+//!     * `tup`: kumi tuple of values.
 //!
 //!    **Return value**
 //!
 //!     1. return a kumi tuple of scalar values of the elements of all `xs` converted to the element type of the common value of the `xs`.
-//!     2. same of 1. or 2. but upgrading the elements of the result.
+//!     2. same as 1., using the tuple elements.
+//!     3. same of 1. or 2., but upgrading the elements of the result.
 //!
 //!  @groupheader{Example}
 //!  @godbolt{doc/core/unfold.cpp}
