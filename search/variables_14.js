9423a19bffc977b190481ffeb60da2e09b698d19
jfalcou <jfalcou@users.noreply.github.com>
2026-04-16 12:53:24 +0000

    deploy: ec88b0449442b2dc663d87cf277dc7e8723ccfc5

--- search/variables_14.js
+++ search/variables_14.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['ulpdist_0',['ulpdist',['../group__core__internal_ga5b73df953d6c6313eeae28925eb8f19c.html#ga5b73df953d6c6313eeae28925eb8f19c',1,'eve']]],
   ['unalign_1',['unalign',['../group__memory_ga62462e7b23d8d092781d5561a9d4c90f.html#ga62462e7b23d8d092781d5561a9d4c90f',1,'eve']]],
-  ['unroll_2',['unroll',['../group__algo__traits_gaa0edcdf11a934c3303c8dbac175f5340.html#gaa0edcdf11a934c3303c8dbac175f5340',1,'eve::algo']]]
+  ['unfold_2',['unfold',['../group__core__arithmetic_gab00b0403241b47b4e0a7f3e2d8be4cfd.html#gab00b0403241b47b4e0a7f3e2d8be4cfd',1,'eve']]],
+  ['unroll_3',['unroll',['../group__algo__traits_gaa0edcdf11a934c3303c8dbac175f5340.html#gaa0edcdf11a934c3303c8dbac175f5340',1,'eve::algo']]]
 ];
