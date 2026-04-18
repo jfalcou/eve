9e010d8ffc9c121bad3dff9bec31f534be561a76
jtlap <jtlapreste@gmail.com>
2026-04-18 15:04:38 +0200

    cumsum cumprod cumtrapz

--- test/doc/core/cumprod.cpp
+++ test/doc/core/cumprod.cpp
@@ -1,15 +1,21 @@
 // revision 0
 #include <eve/module/core.hpp>
 #include <iostream>
-
+#include <bit>
 
 int main()
 {
-  kumi::tuple t{1.0, 2.0, 3.0, 4.0};
-  std::cout << t << std::endl;
-  std::cout << eve::cumprod(t) << std::endl;
-  std::cout << eve::cumprod(1.0, 2.0, 3.0, 4.0) << std::endl;
-  std::cout << eve::cumprod(t) << std::endl;
+  kumi::tuple t{1.0f, 2.0f, 3.0f, 10000.0f};
+  std::cout << "t                                    " << t                                    << std::endl;
+  std::cout << "eve::cumprod(t)                      " << eve::cumprod(t)                      << std::endl;
+  std::cout << "eve::cumprod(1.0, 2.0, 3.0, 10000.0) " << eve::cumprod(1.0, 2.0, 3.0, 10000.0) << std::endl;
+  std::cout << "eve::cumprod(t)                      " << eve::cumprod(t)                      << std::endl;
+  std::cout << "eve::cumprod[eve::widen](t)          " << eve::cumprod[eve::widen](t)          << std::endl;
+  std::array<std::int16_t, 4> a{1, 2, 3, 10000};
+  auto ta = std::bit_cast<kumi::result::fill_t<4, std::int16_t>>(a);
+  std::cout << "eve::cumprod[eve::saturated](ta)     " << eve::cumprod[eve::saturated](ta)    << std::endl;
+  std::cout << "eve::cumprod(ta)                     " << eve::cumprod(ta)                    << std::endl;
+  std::cout << "eve::cumprod[eve::widen](ta)         " << eve::cumprod[eve::widen](ta)        << std::endl;
 
 
 };
--- test/doc/core/cumsum.cpp
+++ test/doc/core/cumsum.cpp
@@ -5,12 +5,16 @@
 
 int main()
 {
-  kumi::tuple t{1.0, 2.0, 3.0, 4.0};
-  std::cout << t << std::endl;
-  std::cout << eve::cumsum(t) << std::endl;
-  std::cout << eve::cumsum(1.0, 2.0, 3.0, 4.0) << std::endl;
-  std::cout << eve::cumsum(t) << std::endl;
-
+  kumi::tuple t{1.0f, 2.0f, 30000.0f, 10000.0f};
+  std::cout << "t                                   " << t                                    << std::endl;
+  std::cout << "eve::cumsum(t)                      " << eve::cumsum(t)                      << std::endl;
+  std::cout << "eve::cumsum(1.0, 2.0, 3.0, 10000.0) " << eve::cumsum(1.0, 2.0, 30000.0, 10000.0) << std::endl;
+  std::cout << "eve::cumsum(t)                      " << eve::cumsum(t)                      << std::endl;
+  std::cout << "eve::cumsum[eve::widen](t)          " << eve::cumsum[eve::widen](t)          << std::endl;
+  std::array<std::int16_t, 4> a{1, 2, 30000, 10000};
+  auto ta = std::bit_cast<kumi::result::fill_t<4, std::int16_t>>(a);
+  std::cout << "eve::cumsum[eve::saturated](ta)     " << eve::cumsum[eve::saturated](ta)    << std::endl;
+  std::cout << "eve::cumsum(ta)                     " << eve::cumsum(ta)                    << std::endl;
+  std::cout << "eve::cumsum[eve::widen](ta)         " << eve::cumsum[eve::widen](ta)        << std::endl;
 
 };
-
