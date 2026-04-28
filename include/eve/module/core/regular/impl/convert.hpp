09950aa322036e57adb8ea310ed14bdaec5bc913
SadiinsoSnowfall <sadiinso@gmail.com>
2026-04-28 19:50:25 +0200

    fix saturated convert for emulated float16s

--- include/eve/module/core/regular/impl/convert.hpp
+++ include/eve/module/core/regular/impl/convert.hpp
@@ -42,16 +42,16 @@ namespace eve::_
 
   template<callable_options O, typename U, typename N>
   EVE_FORCEINLINE auto convert_(EVE_REQUIRES(cpu_), O const&, wide<eve::float16_t, N> v, as<U>) noexcept
-    requires (!_::supports_fp16_vector_conversion && !std::same_as<U, eve::float16_t>)
+    requires (!_::supports_fp16_vector_conversion && !std::same_as<U, eve::float16_t> && !O::contains(saturated))
   {
     // Because we currently only have conversion routine through floats, we make sure that we convert to/from chunks of
     // proper cardinality to keep the code size of `emulated_simd_fp16_to_fp32` under control.
-    if constexpr( has_aggregated_abi_v<wide<float,N>> )
+    if constexpr( has_aggregated_abi_v<wide<float, N>> )
     {
       using card_t = expected_cardinal_t<float>;
-      using base  = typename wide<eve::float16_t, N>::template rescale<card_t>;
-      auto parts  = kumi::map([](auto m) { return std::bit_cast<base>(m); }, kumi::chunks<card_t::value>(v.storage()));
-      auto cvt    = kumi::map([](auto p) { return convert(p, as<U>{}); }, parts);
+      using base   = typename wide<eve::float16_t, N>::template rescale<card_t>;
+      auto parts   = kumi::map([](auto m) { return std::bit_cast<base>(m); }, kumi::chunks<card_t::value>(v.storage()));
+      auto cvt     = kumi::map([](auto p) { return convert(p, as<U>{}); }, parts);
       return kumi::apply([&](auto... m) { return wide<U, N>{m...}; }, cvt);
     }
     else
@@ -62,7 +62,7 @@ namespace eve::_
 
   template<callable_options O, typename T, typename N>
   EVE_FORCEINLINE auto convert_(EVE_REQUIRES(cpu_), O const&, wide<T, N> v, as<eve::float16_t>) noexcept
-    requires (!_::supports_fp16_vector_conversion && !std::same_as<T, eve::float16_t>)
+    requires (!_::supports_fp16_vector_conversion && !std::same_as<T, eve::float16_t> && !O::contains(saturated))
   {
     return _::emulated_simd_fp32_to_fp16(convert(v, as<float>{}));
   }
