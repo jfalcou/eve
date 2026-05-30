6e75d87cff57456fc76f9a69596c18dcf06b8bfb
SadiinsoSnowfall <sadiinso@gmail.com>
2026-05-30 18:26:25 +0200

    fix

--- include/eve/detail/function/simd/common/movemask.hpp
+++ include/eve/detail/function/simd/common/movemask.hpp
@@ -23,11 +23,11 @@ namespace eve::_
     static_assert ( !has_aggregated_abi_v<wide<T, N>>, "movemask is an internal function that does not make sense for aggregated" );
 
     std::uint64_t res = 0;
-    for (std::uint64_t i = 0; i != (std::uint64_t) N; ++i) {
+    for (std::uint64_t i = 0; i != static_cast<std::uint64_t>(N); ++i) {
       std::uint64_t elem = v.get(i) ? 1 : 0;
       res |= elem << i;
     }
-    if constexpr (N <= 32)  return std::pair{(std::uint32_t) res, eve::lane<1>};
+    if constexpr (N <= 32)  return std::pair{static_cast<std::uint32_t>(res), eve::lane<1>};
     else                    return std::pair{res, eve::lane<1>};
   }
 
