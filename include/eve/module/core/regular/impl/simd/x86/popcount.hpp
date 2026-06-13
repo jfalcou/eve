c242c6d4f721ce90989a934aeff1954b9ed64c17
Joel Falcou <joel.falcou@lri.fr>
2026-06-13 23:30:14 +0200

    Remove recursion

--- include/eve/module/core/regular/impl/simd/x86/popcount.hpp
+++ include/eve/module/core/regular/impl/simd/x86/popcount.hpp
@@ -25,32 +25,20 @@ namespace eve::_
     else if constexpr (sizeof(T) == 8 && supports_avx512vl && supports_avx512popcntdq_) return _mm_popcnt_epi64(x);
     else if constexpr (current_api >= eve::ssse3)
     {
-      if constexpr (sizeof(T) == 1)
-      {
-        wide<T,fixed<16>> const lut = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
-        wide<T,N> nibbles{15};
-        wide<T,N> lo_count{_mm_shuffle_epi8(lut, x & nibbles)};
-        wide<T,N> hi_count{_mm_shuffle_epi8(lut, _mm_and_si128(_mm_srli_epi16(x,4), nibbles))};
-        return lo_count + hi_count;
-      }
-      else if constexpr (sizeof(T) == 2)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint8_t, eve::fixed<16>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm_maddubs_epi16(byte_counts, one(tgt))};
-      }
-      else if constexpr (sizeof(T) == 4)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint16_t, eve::fixed<8>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm_madd_epi16(byte_counts, one(tgt))};
-      }
-      else// if constexpr (sizeof(T) == 8)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint8_t, eve::fixed<16>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm_sad_epu8(byte_counts, _mm_setzero_si128())};
-      }
+      __m128i lut  = _mm_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+      __m128i mask = _mm_set1_epi8(0x0F);
+
+      __m128i lo_count = _mm_shuffle_epi8(lut, _mm_and_si128(x, mask));
+      __m128i hi_count = _mm_shuffle_epi8(lut, _mm_and_si128(_mm_srli_epi16(x, 4), mask));
+      __m128i count8   = _mm_add_epi8(lo_count, hi_count);
+
+      if constexpr (sizeof(T) == 1) return wide<T,N>{count8};
+      if constexpr (sizeof(T) == 8) return wide<T,N>{_mm_sad_epu8(count8, _mm_setzero_si128())};
+
+      __m128i count16 = _mm_maddubs_epi16(count8, _mm_set1_epi8(1));
+
+      if constexpr (sizeof(T) == 2) return wide<T,N>{count16};
+      if constexpr (sizeof(T) == 4) return wide<T,N>{_mm_madd_epi16(count16, _mm_set1_epi16(1))};
     }
     else
     {
@@ -60,7 +48,7 @@ namespace eve::_
 
   /////////////////////////////////////////////////////////////////////////////
   // 256 bits
-  template<unsigned_scalar_value T, typename N, callable_options O>
+template<unsigned_scalar_value T, typename N, callable_options O>
   EVE_FORCEINLINE auto popcount_(EVE_REQUIRES(avx_), O const& o, wide<T, N> x) noexcept
     requires std::same_as<abi_t<T, N>, x86_256_>
   {
@@ -70,41 +58,28 @@ namespace eve::_
     else if constexpr (sizeof(T) == 8 && supports_avx512vl && supports_avx512popcntdq_) return _mm256_popcnt_epi64(x);
     else if constexpr (current_api >= eve::avx2)
     {
-      if constexpr (sizeof(T) == 1)
-      {
-        // Repeat LUT bit counts to fit 32bits
-        wide<T,fixed<32>> const lut = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
-                                      , 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4
-                                      };
-        wide<T,N> nibbles{15};
-        wide<T,N> lo_count{_mm256_shuffle_epi8(lut, x & nibbles)};
-        wide<T,N> hi_count{_mm256_shuffle_epi8(lut, _mm256_and_si256(_mm256_srli_epi16(x,4), nibbles))};
-        return lo_count + hi_count;
-      }
-      else if constexpr (sizeof(T) == 2)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint8_t, eve::fixed<32>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm256_maddubs_epi16(byte_counts, one(tgt))};
-      }
-      else if constexpr (sizeof(T) == 4)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint16_t, eve::fixed<16>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm256_madd_epi16(byte_counts, one(tgt))};
-      }
-      else// if constexpr (sizeof(T) == 8)
-      {
-        constexpr as<typename wide<T,N>::template rebind<std::uint8_t, eve::fixed<32>>> tgt = {};
-        auto byte_counts = popcount(bit_cast(x,tgt));
-        return wide<T,N>{_mm256_sad_epu8(byte_counts, _mm256_setzero_si256())};
-      }
+      // We broadcast the 128bits LUT as it generates better codegen than using _mm256_setr_epi8
+      __m128i lut_128 = _mm_setr_epi8(0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4);
+      __m256i lut     = _mm256_broadcastsi128_si256(lut_128);
+      __m256i mask    = _mm256_set1_epi8(0x0F);
+
+      __m256i lo_count = _mm256_shuffle_epi8(lut, _mm256_and_si256(x, mask));
+      __m256i hi_count = _mm256_shuffle_epi8(lut, _mm256_and_si256(_mm256_srli_epi16(x, 4), mask));
+      __m256i count8   = _mm256_add_epi8(lo_count, hi_count);
+
+      if constexpr (sizeof(T) == 1) return wide<T,N>{count8};
+      if constexpr (sizeof(T) == 8) return wide<T,N>{_mm256_sad_epu8(count8, _mm256_setzero_si256())};
+
+      __m256i count16 = _mm256_maddubs_epi16(count8, _mm256_set1_epi8(1));
+
+      if constexpr (sizeof(T) == 2) return wide<T,N>{count16};
+      if constexpr (sizeof(T) == 4) return wide<T,N>{_mm256_madd_epi16(count16, _mm256_set1_epi16(1))};
     }
     else
     {
       auto[l,h] = x.slice();
-      return wide<T,N>(popcount[o](l),popcount[o](h));
-     }
+      return wide<T,N>(popcount[o](l), popcount[o](h));
+    }
   }
 
   /////////////////////////////////////////////////////////////////////////////
