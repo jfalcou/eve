c0001d3a9596f042897c3b1f131147622657f129
jtlap <jtlapreste@gmail.com>
2026-06-16 09:48:15 +0200

    tan for full circle raw fast

--- benchmarks/module/math/tan/full_circle/tan.hpp
+++ benchmarks/module/math/tan/full_circle/tan.hpp
@@ -19,8 +19,10 @@ int main()
   auto std__tan = [](auto x){return std::tan(x);};
 
   eve::bench::experiment xp;
-  run<EVE_VALUE>(EVE_NAME(std__tan) , xp, std__tan , arg0);
-  run<EVE_VALUE>(EVE_NAME(tan[eve::full_circle]) , xp, eve::tan[eve::full_circle] , arg0);
-  run<EVE_TYPE> (EVE_NAME(tan[eve::full_circle]) , xp, eve::tan[eve::full_circle] , arg0);
+  run<EVE_VALUE>(EVE_NAME(std__tan     ) , xp, std__tan , arg0);
+  run<EVE_VALUE>(EVE_NAME(tan full     ) , xp, eve::tan[eve::full_circle] , arg0);
+  run<EVE_TYPE> (EVE_NAME(tan full     ) , xp, eve::tan[eve::full_circle] , arg0);
+  run<EVE_TYPE> (EVE_NAME(tan full_raw ) , xp, eve::tan[eve::full_circle][eve::raw] , arg0);
+  run<EVE_TYPE> (EVE_NAME(tan full_fast) , xp, eve::tan[eve::full_circle][eve::fast] , arg0);
 
 }
