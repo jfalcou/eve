1b9ed035db31947c155fbd1a8c6f0df534cb49ac
Joel Falcou <joel.falcou@lri.fr>
2026-08-27 16:50:12 +0200

    Fix RVV headers relying on include order (#2371)

--- include/eve/detail/function/simd/riscv/slice.hpp
+++ include/eve/detail/function/simd/riscv/slice.hpp
@@ -7,9 +7,10 @@
 //==================================================================================================
 #pragma once
 
+#include <eve/arch/riscv/rvv_utils.hpp>
 #include <eve/detail/category.hpp>
 #include <eve/detail/implementation.hpp>
-#include <eve/module/core/regular/simd_cast.hpp>
+#include <eve/forward.hpp>
 
 // Return first or second part of vector.
 
@@ -28,7 +29,7 @@ requires rvv_abi<abi_t<T, N>>
   constexpr auto in_lmul  = rvv_lmul_v<T, N>;
   constexpr auto out_lmul = rvv_lmul_v<T, typename N::split_type>;
   if constexpr( in_lmul == out_lmul ) return a.storage();
-  else { return simd_cast(a, as<wide<T, typename N::split_type>> {}); }
+  else { return call_simd_cast(a, as<wide<T, typename N::split_type>> {}); }
 }
 
 template<callable_options O, typename T, typename N>
@@ -44,7 +45,7 @@ requires rvv_abi<abi_t<T, N>>
   else
   {
     // we need to lower lmul - call lmul trunc.
-    return simd_cast(res, as<wide<T, typename N::split_type>> {});
+    return call_simd_cast(res, as<wide<T, typename N::split_type>> {});
   }
 }
 
@@ -53,7 +54,7 @@ EVE_FORCEINLINE logical<wide<T, typename N::split_type>>
                 slice_(EVE_REQUIRES(rvv_), O const&, logical<wide<T, N>> a, lower_slice_t) noexcept
 requires rvv_abi<abi_t<T, N>>
 {
-  return simd_cast(a, as<logical<wide<T, typename N::split_type>>> {});
+  return call_simd_cast(a, as<logical<wide<T, typename N::split_type>>> {});
 }
 
 template<callable_options O, typename T, typename N>
@@ -64,7 +65,7 @@ requires rvv_abi<abi_t<T, N>>
   auto bits_slice = a.bits().slice(upper_slice_t {});
   // TODO: could be improved when `simd_cast` for RISC-V will support casts between logical-wide.
   auto neq = bits_slice != 0;
-  return simd_cast(neq, as<logical<wide<T, typename N::split_type>>> {});
+  return call_simd_cast(neq, as<logical<wide<T, typename N::split_type>>> {});
 }
 
 //================================================================================================
