48f2f3b26265e1a966e21960cbd189a7c0dabab5
Nxirda <Nxirda@users.noreply.github.com>
2026-09-04 17:36:59 +0200

    fix shuffle driver usage of old system

--- include/eve/detail/shuffle_v2/simd/common/shuffle_v2_driver.hpp
+++ include/eve/detail/shuffle_v2/simd/common/shuffle_v2_driver.hpp
@@ -430,7 +430,7 @@ struct shuffle_v2_driver_bundle
     return kumi::map(
         []<typename Field, typename... Fields>(kumi::tuple<Field, Fields...> field_res)
         {
-          using T1 = typename Bundle::template rescale<Field::width_type::value>;
+          using T1 = typename Bundle::template rescale<Field::size()>;
           return T1 {field_res};
         },
         field_results);
