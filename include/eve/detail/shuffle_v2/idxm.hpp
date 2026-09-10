fe847939e94576454b4a4c8a59f80b70ea3a4619
Joel Falcou <joel.falcou@lri.fr>
2026-09-10 08:03:15 +0200

    Fix #2397 - Name the array type at the add_shuffle_levels call site (#2401)

--- include/eve/detail/shuffle_v2/idxm.hpp
+++ include/eve/detail/shuffle_v2/idxm.hpp
@@ -1280,14 +1280,14 @@ add_shuffle_levels(std::span<const std::ptrdiff_t> ls)
   return base + use_masks;
 }
 
-// In C++26 the {} can be used with spans, so it's not needed anymore
-#if __cplusplus <= 202302L
 constexpr auto
-add_shuffle_levels(std::array<std::ptrdiff_t, 3> ls)
+add_shuffle_levels(std::integral auto a, std::integral auto... as)
 {
-  return add_shuffle_levels(std::span(ls));
+  return add_shuffle_levels ( std::array{ static_cast<std::ptrdiff_t>(a)
+                                        , static_cast<std::ptrdiff_t>(as)...
+                                        }
+                            );
 }
-#endif
 
 template<std::ptrdiff_t... ls>
 constexpr auto
